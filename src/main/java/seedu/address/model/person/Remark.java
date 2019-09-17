ca36f897456cb49902c00d8dae29b110a1e14879
Open Close <interfiancee@gmail.com>
2019-09-17 23:17:30 +0800

    Add Remark

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,31 @@
+package seedu.address.model.person;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable
+ */
+public class Remark {
+
+    public final String value;
+
+    public Remark(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof Remark // instanceof handles nulls
+            && value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
