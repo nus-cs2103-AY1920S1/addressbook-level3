9f9db515258ee0bd100077ad52a8b31443c406fe
AhmadHatziq <e0031550@u.nus.edu>
2019-09-19 01:47:50 +0800

    Commit to salvage Address code into main

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -20,7 +20,7 @@ public class Person {
     private final Phone phone;
     private final Email email;
 
-    // Data fields
+    // Data  fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
 
