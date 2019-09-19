917b8d0ae484bf49090cd445700f97b0fc7691cd
AeroSpectr3 <32993514+AeroSpectr3@users.noreply.github.com>
2019-09-19 16:12:34 +0800

    implemented NUSmod API websocket

--- src/main/java/seedu/address/websocket/NusmodApi.java
+++ src/main/java/seedu/address/websocket/NusmodApi.java
@@ -14,61 +14,154 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * NUSMods API websocket
+ * Refer to https://api.nusmods.com/v2/#/ for more detailed information
+ */
 public class NusmodApi {
-    private static final String BASEURL = "https://api.nusmods.com/v2";
-    private static final String BASEURL2 = "http://api.nusmods.com/v2";
+    private final String BASEURL = "https://api.nusmods.com/v2";
+    private final String BASEURL2 = "http://api.nusmods.com/v2";
 
-    private static final String ACAD_YEAR = "/2018-2019";
-    private static final String MODULES = "/modules";
-    private static final String SLASH = "/";
-    private static final String JSON_EXTENTION = ".json";
+    private final String ACAD_YEAR = "/2018-2019";
+    private final String MODULES = "/modules";
+    private final String SLASH = "/";
+    private final String JSON_EXTENTION = ".json";
+    private final String MODULE_LIST = "/moduleList";
+    private final String MODULE_INFO = "/moduleInfo";
+    private final String SEMESTERS = "/semesters";
+    private final String VENUES = "/venues";
+    private final String VENUE_INFO = "/venueInformation";
 
     public NusmodApi(){
 
     }
 
+    /**
+     * Returns a JSONObject of summaries of all modules in ACAD_YEAR
+     * @return JSONObject
+     */
+    public JSONObject getModuleList(){
+        JSONObject obj = null;
+        try{
+            URL query = new URL(BASEURL + ACAD_YEAR + MODULE_LIST + JSON_EXTENTION);
+            System.out.println(query.toString());
+
+            obj = parseQuery(query);
+        } catch(MalformedURLException mue){
+            System.out.println("Invalid URL");
+        }
+        return obj;
+    }
+
+    /**
+     * Returns a JSONObject of detailed information about all modules in ACED_YEAR
+     * @return
+     */
+    public JSONObject getModuleInfo(){
+        JSONObject obj = null;
+        try{
+            URL query = new URL(BASEURL + ACAD_YEAR + MODULE_INFO + JSON_EXTENTION);
+            System.out.println(query.toString());
+
+            obj = parseQuery(query);
+        } catch(MalformedURLException mue){
+            System.out.println("Invalid URL");
+        }
+        return obj;
+    }
+
+    /**
+     * Returns a JSONObject of all information about moduleCode in ACAD_YEAR
+     * @param moduleCode    code of the specific module
+     * @return JSONObject representing information of the module
+     */
     public JSONObject getModules(String moduleCode){
-        String output = "";
-        try {
-            // Generate URL
+        JSONObject obj = null;
+        try{
             URL query = new URL(BASEURL + ACAD_YEAR + MODULES + SLASH + moduleCode + JSON_EXTENTION);
             System.out.println(query.toString());
 
-            HttpsURLConnection conn = establishHttpsConnection(query);
+            obj = parseQuery(query);
+        } catch(MalformedURLException mue){
+            System.out.println("Invalid URL");
+        }
+        return obj;
+    }
 
-            if(conn == null){
-                System.out.println("Unable to establish connection, returning null object");
-                return null;
-            } else {
-                try{
-                    int responsecode = conn.getResponseCode();
-                    if(responsecode != 200){
-                        throw new RuntimeException("Connection Error!\n"
-                                + "HttpsResponseCode: " + responsecode
-                        );
-                    } else {
-                        try (BufferedReader br = new BufferedReader(new InputStreamReader(query.openStream()))) {
-                            String line;
+    /**
+     * Returns a JSONObject of summaries of all venues for the current semester
+     * @param semester current semmester: 1 -> sem 1
+     *                                    2 -> sem 2
+     *                                    3 -> special term 1
+     *                                    4 -> special term 2
+     * @return JSONObject representing the venues
+     */
+    public JSONObject getVenues(String semester){
+        JSONObject obj = null;
+        try{
+            URL query = new URL(BASEURL + ACAD_YEAR + SEMESTERS + semester + VENUES + JSON_EXTENTION);
+            System.out.println(query.toString());
 
-                            while((line = br.readLine()) != null) {
+            obj = parseQuery(query);
+        } catch(MalformedURLException mue){
+            System.out.println("Invalid URL");
+        }
+        return obj;
+    }
+
+    /**
+     * Returns a JSONObject of detailed infomation of all venues
+     * @param semester current semmester: 1 -> sem 1
+     *                                    2 -> sem 2
+     *                                    3 -> special term 1
+     *                                    4 -> special term 2
+     * @return JSONObject representing the venues
+     */
+    public JSONObject getVenueInformation(String semester){
+        JSONObject obj = null;
+        try{
+            URL query = new URL(BASEURL + ACAD_YEAR + SEMESTERS + semester + VENUE_INFO + JSON_EXTENTION);
+            System.out.println(query.toString());
+
+            obj = parseQuery(query);
+        } catch(MalformedURLException mue){
+            System.out.println("Invalid URL");
+        }
+        return obj;
+    }
+
+    private JSONObject parseQuery(URL query){
+        String output = "";
+        HttpsURLConnection conn = establishHttpsConnection(query);
+
+        if(conn == null){
+            System.out.println("Unable to establish connection, returning null object");
+            return null;
+        } else {
+            try{
+                int responsecode = conn.getResponseCode();
+                if(responsecode != 200){
+                    throw new RuntimeException("Connection Error! "
+                            + "HttpsResponseCode: " + responsecode
+                    );
+                } else {
+                    try (BufferedReader br = new BufferedReader(new InputStreamReader(query.openStream()))) {
+                        String line;
+                        while((line = br.readLine()) != null) {
                                 output += line;
                                 System.out.println(line);
-                            }
-                        } catch (IOException ioe) {
-                            System.out.println("invalid argument");
-                            output += "Invalid Module Code";
                         }
+                    } catch (IOException ioe) {
+                        System.out.println("invalid argument");
+                        output += "Invalid Module Code";
                     }
-
-                } catch (IOException ioe){
-                    System.out.println("Connection error");
                 }
+            } catch (IOException ioe){
+                System.out.println("Connection error");
             }
-            conn.disconnect();
-
-        } catch (MalformedURLException mue) {
-            System.out.println("invalid url");
         }
+        conn.disconnect();
+
         return parser(output);
     }
 
