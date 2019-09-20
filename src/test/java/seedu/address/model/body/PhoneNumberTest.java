fad97a571aa21df1dcf0d97c3f64829c35636769
ambervoong <voongyuxuan@gmail.com>
2019-09-20 13:09:28 +0800

    Remove empty line.

--- src/test/java/seedu/address/model/body/PhoneNumberTest.java
+++ src/test/java/seedu/address/model/body/PhoneNumberTest.java
@@ -1,6 +1,5 @@
 package seedu.address.model.body;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
