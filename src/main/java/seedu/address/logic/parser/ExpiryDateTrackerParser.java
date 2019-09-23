be89e15e33e30510da3c7e02331862b725c63876
xiaoyu <tkgs.liu.xiaoyu@gmail.com>
2019-09-23 14:26:56 +0800

    Add Check

--- src/main/java/seedu/address/logic/parser/ExpiryDateTrackerParser.java
+++ src/main/java/seedu/address/logic/parser/ExpiryDateTrackerParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.CheckCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
@@ -73,6 +74,9 @@ public class ExpiryDateTrackerParser {
 
         case SortCommand.COMMAND_WORD:
             return new SortCommandParser().parse(arguments);
+
+        case CheckCommand.COMMAND_WORD:
+            return new CheckCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
