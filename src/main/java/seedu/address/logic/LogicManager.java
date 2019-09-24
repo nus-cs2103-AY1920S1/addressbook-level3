aa569b58c59df82dde81bc0c5a5b8a8d50370f4d
AeroSpectr3 <32993514+AeroSpectr3@users.noreply.github.com>
2019-09-24 21:34:48 +0800

    fixed small checkstyle issues

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -48,11 +48,11 @@ public class LogicManager implements Logic {
             throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
         }
 
-        try{
+        try {
             storage.savePersonList(model.getPersonList());
             storage.saveGroupList(model.getGroupList());
             storage.savePersonToGroupMappingList(model.getPersonToGroupMappingList());
-        } catch (IOException ioe){
+        } catch (IOException ioe) {
             logger.severe("Unable to save data");
         }
 
