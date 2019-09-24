ef14e67267f76e494d143e78f66d72e380eedfa6
ShuTingY <shuting_y@u.nus.edu>
2019-09-25 01:23:32 +0800

    throw an exception

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -8,17 +9,24 @@ import seedu.address.model.Model;
 public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "r/ Likes to swim.";
 
-//
-//    private final Index targetIndex;
-//    public RemarkCommand(Index index) {
-//        this.targetIndex = index;
-//
-//    }
-    public RemarkCommand() {}
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from remark");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
+
+//    @Override
+//    public CommandResult execute(Model model) {
+//
+//        return new CommandResult("Hello from remark");
+//    }
 }
