760309417f714154079668b285f60c4b2d398be3
shufa <shufa17@gmail.com>
2019-09-24 16:17:24 +0800

    add enum Status

--- src/main/java/seedu/address/model/events/Status.java
+++ src/main/java/seedu/address/model/events/Status.java
@@ -1,5 +1,19 @@
 package seedu.address.model.events;
 
-//TODO: Stub models for now
-public class Status {
+public enum Status {
+
+    WAITING("patient is waiting now"),
+    MISSED("patient is waiting now"),
+    ACK("patient is arrived"),
+    SETTLE("patient is ");
+
+    private String statusMess;
+
+    Status(String status) {
+        this.statusMess = status;
+    }
+
+    public String getStatusMess() {
+        return statusMess;
+    }
 }
