2314853689107034d6e82461a678a9577833c23f
LimYiJie <e0325578@u.nus.edu>
2019-09-26 01:44:26 +0800

    Group-schedule visualisation completed

--- src/main/java/seedu/address/model/person/ScheduleStub.java
+++ src/main/java/seedu/address/model/person/ScheduleStub.java
@@ -6,19 +6,57 @@ import java.util.ArrayList;
 
 public class ScheduleStub {
     private ArrayList<ArrayList<Pair<Integer, Integer>>> schedule;
-    public ScheduleStub() {
+    public ScheduleStub(int version) {
         schedule = new ArrayList<ArrayList<Pair<Integer, Integer>>>();
-        for (int i = 0; i < 7; i++) {
-            ArrayList<Pair<Integer, Integer>> daySchedule = new ArrayList<Pair<Integer, Integer>>();
-            if (i % 2 == 1) {
-                daySchedule.add(new Pair(1100, 1300));
-                daySchedule.add(new Pair(1500, 1600));
+        if (version == 1) {
+            for (int i = 0; i < 7; i++) {
+                ArrayList<Pair<Integer, Integer>> daySchedule = new ArrayList<Pair<Integer, Integer>>();
+                if (i % 2 == 1) {
+                    daySchedule.add(new Pair(1100, 1300));
+                    daySchedule.add(new Pair(1500, 1600));
+                    daySchedule.add(new Pair(1600, 1700));
+                } else {
+                    daySchedule.add(new Pair(800, 900));
+                    daySchedule.add(new Pair(1330, 1430));
+                }
+                schedule.add(daySchedule);
             }
-            schedule.add(daySchedule);
+        } else if (version == 2) {
+            for (int i = 0; i < 7; i++) {
+                ArrayList<Pair<Integer, Integer>> daySchedule = new ArrayList<Pair<Integer, Integer>>();
+                if (i % 2 == 1) {
+                    daySchedule.add(new Pair(900, 1000));
+                    daySchedule.add(new Pair(1100, 1300));
+                } else {
+                    daySchedule.add(new Pair(1200, 1400));
+                    daySchedule.add(new Pair(1800, 1900));
+                }
+                schedule.add(daySchedule);
+            }
         }
     }
 
     public ArrayList<Pair<Integer, Integer>> getDaySchedule(int dayNumber) {
         return schedule.get(dayNumber - 1);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        } else if (other instanceof ScheduleStub) {
+            boolean result = true;
+            for (int i = 0; i < 7; i++) {
+                ArrayList<Pair<Integer, Integer>> temp = schedule.get(i);
+                for (int j = 0; j < temp.size(); j++) {
+                    Pair<Integer, Integer> p = temp.get(j);
+                    result = result && (p.getKey() == ((ScheduleStub) other).getDaySchedule(i + 1).get(j).getKey())
+                            && (p.getValue() == ((ScheduleStub) other).getDaySchedule(i + 1).get(j).getValue());
+                }
+            }
+            return result;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/seedu/address/ui/ScheduleView.java
+++ src/main/java/seedu/address/ui/ScheduleView.java
@@ -4,26 +4,36 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import seedu.address.model.person.ScheduleStub;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ScheduleView extends Application {
+    //Schedule to be received from logic MUST be in chronological order from Monday -> Friday and events must be
+    //in chronological order as well.
     private ArrayList<String> dayNames = new ArrayList<String>(List.of("Sunday", "Monday", "Tuesday",
             "Wednesday", "Thursday", "Friday", "Saturday"));
-    private GridPane scheduleView = new GridPane();
-    private ArrayList<StackPane> timeslotStackPanes = new ArrayList<StackPane>();
-    private int oneHourLength = 50;
+    private ArrayList<String> listOfColors = new ArrayList<String>(List.of("darkred", "navy", "darkgreen",
+            "darkorange", "lightslategray", "orchid", "teal", "darkmagenta"));
+    private GridPane scheduleView;
+    private ScheduleStub schedule;
+    private ArrayList<StackPane> dayTimeslotStackPanes = new ArrayList<StackPane>();
+    private int oneHourLength = 48;
     private int preferredWidth = 100;
-    private int blockWidth = 20;
+    private double blockWidth = 100;
     private int startTime = 8;
     private int endTime = 20;
+
+    public ScheduleView initialise() {
+        scheduleView = new GridPane();
+        scheduleView.setStyle("-fx-border-width: 2; -fx-border-color: black;");
+        return this;
+    }
 
     private void initialiseHeaders() {
         //initialise headers
@@ -48,10 +58,9 @@ public class ScheduleView extends Application {
         }
         //timeslot headers
         for (int j = startTime; j < endTime; j++) {
-            //Region is used for border and background color control.
             Region timeslotLabelContainer = new Region();
             timeslotLabelContainer.setPrefSize(preferredWidth, oneHourLength);
-            timeslotLabelContainer.setStyle("-fx-background-color: grey; -fx-border-color: white;");
+            timeslotLabelContainer.setStyle("-fx-background-color: white; -fx-border-color: white;");
             Text timeslotText = new Text(j * 100 + "");
             timeslotText.setTranslateX(preferredWidth / 3);
             StackPane stack = new StackPane();
@@ -79,9 +88,6 @@ public class ScheduleView extends Application {
                 timeslotMinorRegion1.setStyle("-fx-border-color: lightgrey; -fx-border-style: dashed none none dashed");
                 timeslotMinorRegion1.setPrefSize(preferredWidth, oneHourLength / 2.0);
                 timeslotMinorRegion.getChildren().addAll(offset, timeslotMinorRegion1);
-                //Line timeslotMinorRegion = new Line(0.0, oneHourLength / 2, preferredWidth, oneHourLength/2);
-                //timeslotMinorRegion.getStrokeDashArray().addAll(2d);
-                //timeslotMinorRegion.setStyle("-fx-stroke: silver;");
                 if (k == endTime - 1) {
                     timeslotMajorRegion.setPrefSize(preferredWidth, oneHourLength / 2);
                     timeslotRegion.getChildren().addAll(timeslotMajorRegion);
@@ -93,7 +99,7 @@ public class ScheduleView extends Application {
                 }
             }
             stackPane.getChildren().add(timeslotContainer);
-            timeslotStackPanes.add(stackPane);
+            dayTimeslotStackPanes.add(stackPane);
             scheduleView.add(stackPane, l + 1, 1, 1, endTime - startTime);
         }
     }
@@ -113,6 +119,7 @@ public class ScheduleView extends Application {
         int minutes = durationMinutes % 60;
         double heightOfTimeslot = hours * oneHourLength + (minutes / 60.0) * oneHourLength;
         result.setPrefSize(blockWidth, heightOfTimeslot);
+        //result.setMaxWidth(blockWidth); setsMaxWidth of busy timeslot.
         result.setStyle("-fx-background-color: " + color + "; -fx-border-width: 2; -fx-background-radius: 5;");
         return result;
     }
@@ -123,39 +130,105 @@ public class ScheduleView extends Application {
         return hours * 60 + minutes;
     }
 
-    public void initialise(ScheduleStub scheduleStub) {
+    private VBox getDayVBoxOfIndividualSchedule(ArrayList<Pair<Integer, Integer>> daySchedule, String color) {
+        VBox timeslotContainer = new VBox();
+        timeslotContainer.setStyle("-fx-padding: 0 2 0 2; -fx-border-width: 2;");
+        timeslotContainer.getChildren().add(makeEmptyTimeslot(30));
+        int originalTimeStamp = startTime * 100;
+        for (int j = 0; j < daySchedule.size(); j++) {
+            Pair<Integer, Integer> timeslot = daySchedule.get(j);
+            int startTime = (int) timeslot.getKey();
+            int endTime = (int) timeslot.getValue();
+            Region busyTimeslot = makeColouredTimeslot(getTimeDifference(startTime, endTime), color);
+            if (originalTimeStamp != startTime) {
+                int timeUntilBusy = getTimeDifference(originalTimeStamp, startTime);
+                Region freeTimeslot = makeEmptyTimeslot(timeUntilBusy);
+                timeslotContainer.getChildren().add(freeTimeslot);
+            }
+            timeslotContainer.getChildren().add(busyTimeslot);
+            originalTimeStamp = endTime;
+        }
+        return timeslotContainer;
+    }
+
+    private ArrayList<String> generateColorList(int groupSize) {
+        ArrayList<String> colors = new ArrayList<String>();
+        HashSet<String> colorChecker = new HashSet<String>();
+        int i = 0;
+        while (i < groupSize) {
+            int index = (int) (Math.random() * (listOfColors.size() - 1));
+            if (colorChecker.contains(listOfColors.get(index))) {
+                continue;
+            }
+            colors.add(listOfColors.get(index));
+            colorChecker.add(listOfColors.get(index));
+            i++;
+        }
+        return colors;
+    }
+
+    public GridPane showIndividualSchedule(ScheduleStub scheduleStub, String color) {
         for (int i = 1; i <= 7; i++) {
             ArrayList<Pair<Integer, Integer>> daySchedule = scheduleStub.getDaySchedule(i);
-            StackPane dayStackPane = timeslotStackPanes.get(i - 1);
-            VBox timeslotContainer = new VBox();
-            timeslotContainer.setStyle("-fx-padding: 0");
-            timeslotContainer.getChildren().add(makeEmptyTimeslot(30));
-            int originalTimeStamp = startTime * 100;
-            for (int j = 0; j < daySchedule.size(); j++) {
-                Pair<Integer, Integer> timeslot = daySchedule.get(j);
-                int startTime = (int) timeslot.getKey();
-                int endTime = (int) timeslot.getValue();
-                Region busyTimeslot = makeColouredTimeslot(getTimeDifference(startTime, endTime), "red");
-                if (originalTimeStamp != startTime) {
-                    int timeUntilBusy = getTimeDifference(originalTimeStamp, startTime);
-                    Region freeTimeslot = makeEmptyTimeslot(timeUntilBusy);
-                    timeslotContainer.getChildren().add(freeTimeslot);
-                }
-                timeslotContainer.getChildren().add(busyTimeslot);
-                originalTimeStamp = endTime;
-            }
+            //Sunday -> 0, Monday -> 1.. for dayStackPane.
+            StackPane dayStackPane = dayTimeslotStackPanes.get(i - 1);
+            VBox timeslotContainer = getDayVBoxOfIndividualSchedule(daySchedule, color);
             dayStackPane.getChildren().add(timeslotContainer);
+        }
+        return scheduleView;
+    }
+
+    public GridPane showGroupSchedule(ArrayList<ScheduleStub> schedules) {
+        //Assign colors to each schedule.
+        //Draw VBox of each individual's schedule.
+        //Put VBoxes of all individuals' timeslot for the day into HBox.
+        //Put HBox into dayStackPane.
+        //Loop to next day.
+        ArrayList<String> colors = generateColorList(schedules.size());
+        blockWidth = blockWidth / (schedules.size());
+        for (int i = 0; i < dayTimeslotStackPanes.size(); i++) {
+            StackPane dayStackPane = dayTimeslotStackPanes.get(i);
+            HBox groupTimeslot = new HBox();
+            for (int j = 0; j < schedules.size(); j++) {
+                ScheduleStub currIndividualSchedule = schedules.get(j);
+                ArrayList<Pair<Integer, Integer>> daySchedule = currIndividualSchedule.getDaySchedule(i + 1);
+                VBox dayScheduleVBox = getDayVBoxOfIndividualSchedule(daySchedule, colors.get(j));
+                HBox.setHgrow(dayScheduleVBox, Priority.ALWAYS);
+                groupTimeslot.getChildren().add(dayScheduleVBox);
+            }
+            dayStackPane.getChildren().add(groupTimeslot);
+        }
+        return scheduleView;
+    }
+
+    public ScheduleStub getSchedule() {
+        return this.schedule;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        } else {
+            if (other instanceof ScheduleView) {
+                return ((ScheduleView) other).getSchedule().equals(this.schedule);
+            } else {
+                return false;
+            }
         }
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        initialise();
         initialiseHeaders();
         initialiseTableCells();
-        ScheduleStub ss = new ScheduleStub();
-        initialise(ss);
+        ScheduleStub s1 = new ScheduleStub(1);
+        ScheduleStub s2 = new ScheduleStub(2);
         ScrollPane mainLayout = new ScrollPane();
-        mainLayout.setContent(scheduleView);
+        //mainLayout.setContent(showIndividualSchedule(s1, "darkblue"));
+        mainLayout.setContent(showGroupSchedule(new ArrayList<ScheduleStub>(List.of(s1, s2))));
+        //mainLayout.setContent(scheduleView);
         mainLayout.setFitToHeight(true);
         mainLayout.setFitToWidth(true);
         Scene scene = new Scene(mainLayout);
