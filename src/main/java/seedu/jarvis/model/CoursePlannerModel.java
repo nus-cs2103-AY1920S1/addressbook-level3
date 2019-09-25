bd5e3f52dc83a2deb66946d2e2ef67cefd8485ff
ryanYtan <tanyuryan@gmail.com>
2019-09-25 19:41:23 +0800

    Add javadocs to CoursePlannerModel

--- src/main/java/seedu/jarvis/model/CoursePlannerModel.java
+++ src/main/java/seedu/jarvis/model/CoursePlannerModel.java
@@ -1,5 +1,21 @@
 package seedu.jarvis.model;
 
+import seedu.jarvis.model.course.Course;
+
 public interface CoursePlannerModel {
-    void addModule(Course asd);
+    /**
+     * Adds a module.
+     */
+    void addModule(Course course);
+
+    /**
+     * Deletes the given course.
+     * The course must exist within the user's list.
+     */
+    void deleteModule(Course course);
+
+    /**
+     * Checks if the user can take this course.
+     */
+    boolean canTakeModule(Course course);
 }
