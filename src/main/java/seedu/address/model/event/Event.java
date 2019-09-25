3870d3c844460f10810e001972624fc1a2ba6c59
DivineDX <chia.dexun97@gmail.com>
2019-09-25 12:20:52 +0800

    Add Skeleton for Event Objects

---
+++ src/main/java/seedu/address/model/event/Event.java
@@ -0,0 +1,35 @@
+package seedu.address.model.event;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+
+public class Event {
+
+    //Identity Fields
+    private final EventID id;
+    private final EventName name;
+
+    //Data Fields
+    private final EventVenue venue;
+    private final EventHoursNeeded hoursNeeded;
+    private final EventManpowerNeeded manpowerNeeded;
+    private final EventStartDate startDate;
+    private final EventEndDate endDate;
+    private final EventManpowerAllocatedList manpowerAllocatedList;
+    private final Set<Tag> tags = new HashSet<>();
+
+    public Event(EventID id, EventName name, EventVenue venue, EventHoursNeeded hoursNeeded,
+                 EventManpowerNeeded manpowerNeeded, EventStartDate startDate, EventEndDate endDate, Set<Tag> tags) {
+        this.id = id;
+        this.name = name;
+        this.venue = venue;
+        this.hoursNeeded = hoursNeeded;
+        this.manpowerNeeded = manpowerNeeded;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.manpowerAllocatedList = new EventManpowerAllocatedList();
+        this.tags.addAll(tags);
+    }
+}
