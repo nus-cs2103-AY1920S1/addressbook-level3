c539bca11dd88b3ba9f7e731a7824f644fd0fea3
Chee Yuan <chong-1997@hotmail.com>
2019-09-25 15:25:55 +0800

    Add remarkCommand without test

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class RemarkCommandParser implements Parser<RemarkCommand> {
+    public static final Prefix PREFIX_REMARK = new Prefix("r/");
+
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        Remark remark = ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+
+        return new RemarkCommand(index, remark);
+    }
+}
