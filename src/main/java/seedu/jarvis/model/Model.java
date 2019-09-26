a43585e5aa4a0aeb6a608b06a38c418f5bf67995
marcfyk <marcjfyk@gmail.com>
2019-09-26 16:54:32 +0800

    Move predicate from Model to AddressModel

--- src/main/java/seedu/jarvis/model/Model.java
+++ src/main/java/seedu/jarvis/model/Model.java
@@ -1,17 +1,11 @@
 package seedu.jarvis.model;
 
-import java.util.function.Predicate;
-
 import seedu.jarvis.commons.core.GuiSettings;
-import seedu.jarvis.model.person.Person;
 
 /**
  * The API of the Model component.
  */
 public interface Model extends AddressModel, HistoryModel {
-    /** {@code Predicate} that always evaluate to true */
-    Predicate<Person> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
-
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
      */
