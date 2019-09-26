2ce875d05157ac3397ce41b2ceb80e145b2f9361
chanjunren <chanjunren@gmail.com>
2019-09-26 19:29:12 +0800

    Revert "Removed address class and usages"
    This reverts commit e41fb7f32a3b51edf1e2b92c5881513730248ff8.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -21,16 +21,18 @@ public class Person {
     private final Email email;
 
     // Data fields
+    private final Address address;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, tags);
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
+        this.address = address;
         this.tags.addAll(tags);
     }
 
@@ -44,6 +46,10 @@ public class Person {
 
     public Email getEmail() {
         return email;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 
     /**
@@ -86,13 +92,14 @@ public class Person {
         return otherPerson.getName().equals(getName())
                 && otherPerson.getPhone().equals(getPhone())
                 && otherPerson.getEmail().equals(getEmail())
+                && otherPerson.getAddress().equals(getAddress())
                 && otherPerson.getTags().equals(getTags());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, tags);
+        return Objects.hash(name, phone, email, address, tags);
     }
 
     @Override
@@ -103,6 +110,8 @@ public class Person {
                 .append(getPhone())
                 .append(" Email: ")
                 .append(getEmail())
+                .append(" Address: ")
+                .append(getAddress())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
         return builder.toString();
