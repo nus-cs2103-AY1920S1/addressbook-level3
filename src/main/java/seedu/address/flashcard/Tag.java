7fbdf252d11d5cdf331f2e16188add9c4f428456
Akimatsu98 <e0322951@u.nus.edu>
2019-09-26 10:13:09 +0800

    Refactored code and added javadoc

--- src/main/java/seedu/address/flashcard/Tag.java
+++ src/main/java/seedu/address/flashcard/Tag.java
@@ -1,57 +1,90 @@
-package seedu.address.model.tag;
+package seedu.address.flashcard;
 
 import java.util.ArrayList;
 
+/**
+ * A class that represents each individual tagged flashcard set
+ */
 public class Tag {
-    private ArrayList<Flashcard> cardlist;
+    private ArrayList<Flashcard> cardList;
     private String name;
-    private Statistics stats;
 
+    /**
+     * Constructor for a tag object.
+     *
+     * @param _name the name of the tag to be initialized.
+     */
     public Tag(String _name) {
         this.name = _name;
-        stats = new Statistics();
         cardList = new ArrayList<>();
     }
 
+    /**
+     * Give a list of all the flashcards under this tag.
+     *
+     * @return a list of all flashcards.
+     */
     public ArrayList<Flashcard> getFlashcards() {
         ArrayList<Flashcard> resultList = new ArrayList<>();
-        resultList.addAll(cardlist);
+        resultList.addAll(cardList);
         return resultList;
     }
 
+    /**
+     * Remove a particular flashcard from the tag based on the flashcard ID.
+     *
+     * @param id the ID of the flashcard to be removed.
+     */
     public void deleteFlashcard(ID id) {
         ArrayList<Flashcard> newList = new ArrayList<>();
-        newList.addAll(cardlist);
+        newList.addAll(cardList);
         for (Flashcard item: newList) {
             if (item.getId() == id.getId()) { // there are two different getId() and ID stored as int
                 newList.remove(item);
             }
         }
-        this.cardlist = newList;
+        this.cardList = newList;
     }
 
+    /**
+     * Add a new flashcard to the tag.
+     *
+     * @param c the flashcard to be added.
+     */
     public void addFlashcard(Flashcard c) {
         ArrayList<Flashcard> newList = new ArrayList<>();
-        newList.addAll(cardlist);
+        newList.addAll(cardList);
         newList.add(c);
-        this.cardlist = newList;
+        this.cardList = newList;
     }
 
+    /**
+     * Inform the name of the tag.
+     *
+     * @return name of the tag.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Edit the name of the tag to a new one.
+     *
+     * @param newName the new name of the tag.
+     */
     public void setName(String newName) {
         this.name = newName;
     }
 
-    public Statistics getStatistics() {
-        return stats;
-    }
-
+    /**
+     * Check whether the tag contains a particular flashcard.
+     *
+     * @param c the flashcard to be checked.
+     * @return whether the flashcard exists.
+     */
     public boolean contains(Flashcard c) {
         boolean isContain = false;
-        for (Flashcard item: cardlist) {
+        for (Flashcard item: cardList) {
             if (item.equals(c)) {
                 isContain = true;
             }
@@ -59,6 +92,12 @@ public class Tag {
         return isContain;
     }
 
+    /**
+     * Check if two tags are equivalent to each other.
+     *
+     * @param other the other tag to be tested.
+     * @return a boolean variable that informs whether the two tags are the same.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
--- src/main/java/seedu/address/flashcard/TagManager.java
+++ src/main/java/seedu/address/flashcard/TagManager.java
@@ -1,14 +1,28 @@
-package seedu.address.model.tag;
+package seedu.address.flashcard;
 
 import java.util.ArrayList;
 
+/**
+ *  A class for managing flashcard tagged sets.
+ */
 public class TagManager {
     private ArrayList<Tag> allTags;
 
+    /**
+     * Constructor for TagManager.
+     *
+     * @param _allTags a list of all the tagged sets.
+     */
     public TagManager(ArrayList<Tag> _allTags) {
         this.allTags = _allTags;
     }
 
+    /**
+     * Identify all the relevant tags whose names contain the keyword.
+     *
+     * @param keyword the user input keyword for searching.
+     * @return a list of all relevant tags.
+     */
     public ArrayList<Tag> findTag(String keyword) {
         ArrayList<Tag> resultList = new ArrayList<>();
         for (Tag item : allTags) {
@@ -19,12 +33,23 @@ public class TagManager {
         return resultList;
     }
 
-    public ArrayList<Tag> getTags(String keyword) {
+    /**
+     * Give all the tags currently available.
+     *
+     * @return a list of all the tags.
+     */
+    public ArrayList<Tag> getTags() {
         ArrayList<Tag> resultList = new ArrayList<>();
         resultList.addAll(allTags);
         return resultList;
     }
 
+    /**
+     * Edit the name of a tag to a new name.
+     *
+     * @param tagName the original tag name.
+     * @param newName the new tag name.
+     */
     public void setTagName(String tagName, String newName) {
         Tag cur = null;
         for (Tag item : allTags) {
@@ -35,6 +60,11 @@ public class TagManager {
         cur.setName(newName);
     }
 
+    /**
+     * Add a new tag to the current tag list.
+     *
+     * @param name name of the new tag.
+     */
     public void addTag(String name) {
         Tag t = new Tag(name);
         ArrayList<Tag> newTags = new ArrayList<>();
@@ -43,6 +73,11 @@ public class TagManager {
         this.allTags = newTags;
     }
 
+    /**
+     * Remove a specific tag from the current tag list.
+     *
+     * @param name the name of tag that is to be deleted.
+     */
     public void deleteTag(String name) {
         ArrayList<Tag> newTags = new ArrayList<>();
         newTags.addAll(allTags);
@@ -54,6 +89,12 @@ public class TagManager {
         this.allTags = newTags;
     }
 
+    /**
+     * Check whether there exists a tag with the given name.
+     *
+     * @param name the name of the tag to be searched.
+     * @return a boolean variable that informs either true or false
+     */
     public boolean contains(String name) {
         boolean isContain = false;
         for (Tag item : allTags) {
