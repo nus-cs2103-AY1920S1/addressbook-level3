03255864cb55d025d03c29dfe7429a743a14b1c0
ShawnCX <shawnlum123@gmail.com>
2019-09-28 01:38:56 +0800

    Adds Schedule manager for patient appointments

---
+++ src/main/java/seedu/address/model/PatientSchedule.java
@@ -0,0 +1,124 @@
+package seedu.address.model;
+
+import java.util.List;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.events.Event;
+import seedu.address.model.events.UniqueEventList;
+
+import static java.util.Objects.requireNonNull;
+
+public class PatientSchedule implements ReadOnlySchedule {
+
+    private final UniqueEventList events;
+
+    /*
+     * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
+     * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
+     *
+     * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
+     *   among constructors.
+     */
+    {
+        events = new UniqueEventList();
+    }
+
+    public PatientSchedule() { }
+
+    /**
+     * Creates an PatientSchedule using the Events in the {@code toBeCopied}
+     */
+    public PatientSchedule(ReadOnlySchedule toBeCopied) {
+        this();
+        resetData(toBeCopied);
+    }
+
+    //// list overwrite operations
+
+    /**
+     * Replaces the contents of the event list with {@code events}.
+     * {@code events} must not contain duplicate events.
+     */
+    public void setEvents(List<Event> events) {
+        this.events.setEvents(events);
+    }
+
+    /**
+     * Resets the existing data of this {@code PatientSchedule} with {@code newData}.
+     */
+    public void resetData(ReadOnlySchedule newData) {
+        requireNonNull(newData);
+
+        setEvents(newData.getEventList());
+    }
+
+    //// event-level operations
+
+    /**
+     * Returns true if a event with the same identity as {@code event} exists in the address book.
+     */
+    public boolean hasEvent(Event event) {
+        requireNonNull(event);
+        return events.contains(event);
+    }
+
+    /**
+     * Returns true if an exact {@code event} exists in the address book.
+     */
+    public boolean hasExactEvent(Event event) {
+        requireNonNull(event);
+        return events.containsExact(event);
+    }
+
+    /**
+     * Adds a event to the address book.
+     * The event must not already exist in the address book.
+     */
+    public void addEvent(Event p) {
+        events.add(p);
+    }
+
+    /**
+     * Replaces the given event {@code target} in the list with {@code editedEvent}.
+     * {@code target} must exist in the address book.
+     * The event identity of {@code editedEvent} must not be the same as another existing event in the address book.
+     */
+    public void setEvent(Event target, Event editedEvent) {
+        requireNonNull(editedEvent);
+
+        events.setEvent(target, editedEvent);
+    }
+
+    /**
+     * Removes {@code key} from this {@code PatientSchedule}.
+     * {@code key} must exist in the address book.
+     */
+    public void removeEvent(Event key) {
+        events.remove(key);
+    }
+
+    //// util methods
+
+    @Override
+    public String toString() {
+        return events.asUnmodifiableObservableList().size() + " events";
+        // TODO: refine later
+    }
+
+    @Override
+    public ObservableList<Event> getEventList() {
+        return events.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof PatientSchedule // instanceof handles nulls
+            && events.equals(((PatientSchedule) other).events));
+    }
+
+    @Override
+    public int hashCode() {
+        return events.hashCode();
+    }
+}
