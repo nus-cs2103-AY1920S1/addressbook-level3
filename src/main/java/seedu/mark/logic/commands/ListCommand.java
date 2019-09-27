bc5fd9a57263a695c4bff21e9f1ee4f369722b9f
dorcastan <dorcas.tann@gmail.com>
2019-09-27 10:02:31 +0800

    Refactor: Logic (Edit, Find and List Commands)
    Rename bookmarks to persons.

--- src/main/java/seedu/mark/logic/commands/ListCommand.java
+++ src/main/java/seedu/mark/logic/commands/ListCommand.java
@@ -6,13 +6,13 @@ import static seedu.mark.model.Model.PREDICATE_SHOW_ALL_BOOKMARKS;
 import seedu.mark.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all bookmarks in the bookmark manager to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all bookmarks";
 
 
     @Override
