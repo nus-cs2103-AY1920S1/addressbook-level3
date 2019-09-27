9fc6890291c68687fb2570d15b3cd3120cf9326c
Tishya Khanna <tishya.khanna01@gmail.com>
2019-09-28 02:09:41 +0800

    Test

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -20,6 +20,7 @@ import javafx.application.Application;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Test");
         Application.launch(MainApp.class, args);
     }
 }
