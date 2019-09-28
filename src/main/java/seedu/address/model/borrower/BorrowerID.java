f0adfcf21bb167baacd77c218dc5f16dd0acb2a9
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-09-28 17:18:19 +0800

    Add Loan class

---
+++ src/main/java/seedu/address/model/borrower/BorrowerID.java
@@ -0,0 +1,11 @@
+package seedu.address.model.borrower;
+
+public class BorrowerID {
+
+    private String id;
+
+    // TODO
+    public BorrowerID(String id) {
+        this.id = id;
+    }
+}
--- src/main/java/seedu/address/model/loan/Loan.java
+++ src/main/java/seedu/address/model/loan/Loan.java
@@ -1,23 +1,59 @@
 package seedu.address.model.loan;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.LocalDate;
+
+import seedu.address.model.book.SerialNumber;
+import seedu.address.model.borrower.BorrowerID;
+
 /**
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Loan {
 
-    private String id = "13245";
+    private final SerialNumber bookSerialNumber;
+    private final BorrowerID borrowerID;
+    private final LocalDate startDate;
+    private final LocalDate dueDate;
 
-    public Loan (String id) {
-        this.id = id;
+    public Loan(SerialNumber bookSerialNumber, BorrowerID borrowerID, LocalDate startDate, LocalDate dueDate) {
+        requireAllNonNull(bookSerialNumber, borrowerID, startDate, dueDate);
+        this.bookSerialNumber = bookSerialNumber;
+        this.borrowerID = borrowerID;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
     }
 
-    public Loan() {
-
+    public SerialNumber getBookSerialNumber() {
+        return bookSerialNumber;
     }
 
-    public String getId() {
-        return id;
+    public BorrowerID getBorrowerID() {
+        return borrowerID;
     }
 
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return true; // TODO
+    }
+
+    @Override
+    public int hashCode() {
+        return 0; // TODO
+    }
+
+    @Override
+    public String toString() {
+        return ""; // TODO
+    }
 }
