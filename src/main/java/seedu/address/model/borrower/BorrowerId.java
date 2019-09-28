7b3d7ae2741c2d9d62c64fb6e3020c185e425af9
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-09-28 18:10:05 +0800

    Change JsonAdaptedLoan

---
+++ src/main/java/seedu/address/model/borrower/BorrowerId.java
@@ -0,0 +1,18 @@
+package seedu.address.model.borrower;
+
+// TODO
+public class BorrowerId {
+
+    public static final String MESSAGE_CONSTRAINTS = "TODO"; // TODO
+    public final String value;
+
+    // TODO
+    public BorrowerId(String borrowerId) {
+        value = borrowerId;
+    }
+
+    // TODO
+    public static boolean isValidBorrowerId(String borrowerId) {
+        return true; // TODO
+    }
+}
--- src/main/java/seedu/address/model/loan/Loan.java
+++ src/main/java/seedu/address/model/loan/Loan.java
@@ -3,9 +3,10 @@ package seedu.address.model.loan;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import seedu.address.model.book.SerialNumber;
-import seedu.address.model.borrower.BorrowerID;
+import seedu.address.model.borrower.BorrowerId;
 
 /**
  * Represents a Person in the address book.
@@ -14,14 +15,14 @@ import seedu.address.model.borrower.BorrowerID;
 public class Loan {
 
     private final SerialNumber bookSerialNumber;
-    private final BorrowerID borrowerID;
+    private final BorrowerId borrowerId;
     private final LocalDate startDate;
     private final LocalDate dueDate;
 
-    public Loan(SerialNumber bookSerialNumber, BorrowerID borrowerID, LocalDate startDate, LocalDate dueDate) {
-        requireAllNonNull(bookSerialNumber, borrowerID, startDate, dueDate);
+    public Loan(SerialNumber bookSerialNumber, BorrowerId borrowerId, LocalDate startDate, LocalDate dueDate) {
+        requireAllNonNull(bookSerialNumber, borrowerId, startDate, dueDate);
         this.bookSerialNumber = bookSerialNumber;
-        this.borrowerID = borrowerID;
+        this.borrowerId = borrowerId;
         this.startDate = startDate;
         this.dueDate = dueDate;
     }
@@ -30,8 +31,8 @@ public class Loan {
         return bookSerialNumber;
     }
 
-    public BorrowerID getBorrowerID() {
-        return borrowerID;
+    public BorrowerId getBorrowerId() {
+        return borrowerId;
     }
 
     public LocalDate getStartDate() {
@@ -44,16 +45,37 @@ public class Loan {
 
     @Override
     public boolean equals(Object other) {
-        return true; // TODO
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Loan)) {
+            return false;
+        }
+
+        Loan otherLoan = (Loan) other;
+        return otherLoan.getBookSerialNumber().equals(bookSerialNumber)
+                && otherLoan.getBorrowerId().equals(borrowerId)
+                && otherLoan.getStartDate().equals(startDate)
+                && otherLoan.getDueDate().equals(dueDate);
     }
 
     @Override
     public int hashCode() {
-        return 0; // TODO
+        return Objects.hash(bookSerialNumber, borrowerId, startDate, dueDate);
     }
 
     @Override
     public String toString() {
-        return ""; // TODO
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Book Serial Number: ")
+                .append(bookSerialNumber)
+                .append(" Borrower ID: ")
+                .append(borrowerId)
+                .append(" Loaned from ")
+                .append(startDate)
+                .append(" to ")
+                .append(dueDate);
+        return builder.toString();
     }
 }
