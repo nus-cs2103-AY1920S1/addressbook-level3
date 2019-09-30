a88ee974d0757b3a838bb49eff662817227a5566
ShawnCX <shawnlum123@gmail.com>
2019-09-30 18:53:26 +0800

    Adds DateTime class for Events

---
+++ src/main/java/seedu/address/model/events/DateTime.java
@@ -0,0 +1,49 @@
+package seedu.address.model.events;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.util.Objects.requireNonNull;
+
+public class DateTime {
+
+    public static final String DATETIME_FORMAT = "dd MM yyyy HHmm";
+    public static final String MESSAGE_CONSTRAINTS =
+        "date time must be follow the format of'" + DATETIME_FORMAT + "'.";
+    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATETIME_FORMAT);
+    private final Date time;
+
+    public DateTime(Date date) {
+        requireNonNull(date);
+        time = date;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    /**
+     * Parses a simple date format as described by {@code DATETIME_FORMAT}, else returns null.
+     *
+     * @param dateString a simple date format
+     *
+     * @return a {@code DateTime} if valid, otherwise, returns null.
+     */
+    public static DateTime tryParseSimpleDateFormat(String dateString) {
+        DateTime parsedDateTime;
+        try {
+            Date parseDate = DATE_FORMATTER.parse(dateString);
+            parsedDateTime = new DateTime(parseDate);
+        } catch (ParseException ex) {
+            parsedDateTime = null;
+        }
+        return parsedDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return DATE_FORMATTER.format(time);
+    }
+
+}
--- src/main/java/seedu/address/model/events/Timing.java
+++ src/main/java/seedu/address/model/events/Timing.java
@@ -3,25 +3,20 @@ package seedu.address.model.events;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
 
-//TODO: Stub models for now
 /**
  * Represents a Date time in the schedule.
- * Guarantees: immutable; is valid as declared in {@link #isValidTiming(Date, Date)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidTiming(DateTime, DateTime)}
  */
 public class Timing {
 
     public static final String MESSAGE_CONSTRAINTS =
-        "The event start datetime must be before the end datetime.";
-    public static final String DATETIME_FORMAT = "dd MMM yyyy HHmm";
-    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATETIME_FORMAT);
+        "The event start timing must be before the end timing.";
 
-    private final Date startTiming;
-    private final Date endTiming;
+    private final DateTime startTiming;
+    private final DateTime endTiming;
 
     /**
      * Constructs a {@code Timing}.
@@ -29,7 +24,7 @@ public class Timing {
      * @param startTiming A valid dateTime describing the start of event.
      * @param endTiming   A valid dateTime describing the end of event.
      */
-    public Timing(Date startTiming, Date endTiming) {
+    public Timing(DateTime startTiming, DateTime endTiming) {
         requireAllNonNull(startTiming, endTiming);
         checkArgument(isValidTiming(startTiming, endTiming), MESSAGE_CONSTRAINTS);
 
@@ -38,61 +33,32 @@ public class Timing {
     }
 
     /**
-     * Parses a simple date format as described by {@code DATETIME_FORMAT}, else returns null.
-     *
-     * @param dateString a simple date format
-     *
-     * @return a date if valid, otherwise, returns null.
-     */
-    private static Date parseSimpleDateFormat(String dateString) {
-        Date parsedDate;
-        try {
-            parsedDate = DATE_FORMATTER.parse(dateString);
-        } catch (ParseException ex) {
-            parsedDate = null;
-        }
-        return parsedDate;
-    }
-
-    private static String toString(Date date) {
-        return DATE_FORMATTER.format(date);
-    }
-
-    /**
      * Returns true if the start dateTime is before the end dateTime.
      */
     public static boolean isValidTiming(String testStart, String testEnd) {
-        Date startDate = parseSimpleDateFormat(testStart);
-        Date endDate = parseSimpleDateFormat(testEnd);
+        DateTime startDate = DateTime.tryParseSimpleDateFormat(testStart);
+        DateTime endDate = DateTime.tryParseSimpleDateFormat(testEnd);
         return isValidTiming(startDate, endDate);
     }
 
     /**
      * Returns true if the start dateTime is before the end dateTime.
      */
-    public static boolean isValidTiming(Date testStart, Date testEnd) {
-        return testStart != null && testEnd != null && testStart.before(testEnd);
+    public static boolean isValidTiming(DateTime testStart, DateTime testEnd) {
+        return testStart != null && testEnd != null && testStart.getTime().before(testEnd.getTime());
     }
 
-    public Date getStartTiming() {
+    public DateTime getStartTime() {
         return startTiming;
     }
 
-    public Date getEndTiming() {
+    public DateTime getEndTime() {
         return endTiming;
-    }
-
-    public String toStartTimingString() {
-        return toString(startTiming);
-    }
-
-    public String toEndTimingString() {
-        return toString(endTiming);
     }
 
     @Override
     public String toString() {
-        return String.format("%s - %s", toStartTimingString(), toEndTimingString());
+        return String.format("%s - %s", startTiming.toString(), endTiming.toString());
     }
 
     @Override
@@ -106,8 +72,8 @@ public class Timing {
         }
 
         Timing otherTiming = (Timing) other;
-        return otherTiming.getStartTiming().equals(getStartTiming())
-            && otherTiming.getEndTiming().equals(getEndTiming());
+        return otherTiming.getStartTime().equals(getStartTime())
+            && otherTiming.getEndTime().equals(getEndTime());
     }
 
     @Override
