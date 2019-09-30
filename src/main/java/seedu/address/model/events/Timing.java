1b7fc1aa80d10ee4f18ea3979e565ff7a762a653
ShawnCX <shawnlum123@gmail.com>
2019-09-30 11:43:40 +0800

    Adds Timing model for Events

--- src/main/java/seedu/address/model/events/Timing.java
+++ src/main/java/seedu/address/model/events/Timing.java
@@ -3,6 +3,8 @@ package seedu.address.model.events;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
 
@@ -15,6 +17,9 @@ public class Timing {
 
     public static final String MESSAGE_CONSTRAINTS =
         "The event start datetime must be before the end datetime.";
+    public static final String DATETIME_FORMAT = "dd MMM yyyy HHmm";
+    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATETIME_FORMAT);
+
     private final Date startTiming;
     private final Date endTiming;
 
@@ -33,10 +38,40 @@ public class Timing {
     }
 
     /**
+     * Parses a simple date format as described by {@code DATETIME_FORMAT}, else returns null.
+     *
+     * @param dateString a simple date format
+     *
+     * @return a date if valid, otherwise, returns null.
+     */
+    private static Date parseSimpleDateFormat(String dateString) {
+        Date parsedDate;
+        try {
+            parsedDate = DATE_FORMATTER.parse(dateString);
+        } catch (ParseException ex) {
+            parsedDate = null;
+        }
+        return parsedDate;
+    }
+
+    private static String toString(Date date) {
+        return DATE_FORMATTER.format(date);
+    }
+
+    /**
+     * Returns true if the start dateTime is before the end dateTime.
+     */
+    public static boolean isValidTiming(String testStart, String testEnd) {
+        Date startDate = parseSimpleDateFormat(testStart);
+        Date endDate = parseSimpleDateFormat(testEnd);
+        return isValidTiming(startDate, endDate);
+    }
+
+    /**
      * Returns true if the start dateTime is before the end dateTime.
      */
     public static boolean isValidTiming(Date testStart, Date testEnd) {
-        return testStart.before(testEnd);
+        return testStart != null && testEnd != null && testStart.before(testEnd);
     }
 
     public Date getStartTiming() {
@@ -47,9 +82,17 @@ public class Timing {
         return endTiming;
     }
 
+    public String toStartTimingString() {
+        return toString(startTiming);
+    }
+
+    public String toEndTimingString() {
+        return toString(endTiming);
+    }
+
     @Override
     public String toString() {
-        return "SOME DATETIME"; //TODO:
+        return String.format("%s - %s", toStartTimingString(), toEndTimingString());
     }
 
     @Override
