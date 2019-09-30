e9c2b316854642c022c0a1e4795f2f777c1b9795
Kalsyc <klassiclegend@gmail.com>
2019-09-30 18:35:01 +0800

    Add TagCommand and TagCommandParser outline

---
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.TagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class TagCommandParser implements Parser<TagCommand> {
+    @Override
+    public TagCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
