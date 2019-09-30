6841673d4636dda814efd742206f2155d1bee8c2
walnads <s10158595@connect.np.edu.sg>
2019-09-30 15:02:40 +0800

    Revert "Remove all instances of address"
    This reverts commit 5e1abfd36d07a4f508e11d4db75ed375d1c7d091.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -47,6 +47,7 @@ public class PersonCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
+        address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
