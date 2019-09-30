f7d6ca2480c96df459afd223376593b438f58aba
Christabel <50402053+belbel98@users.noreply.github.com>
2019-09-30 10:34:19 +0800

    Tutorial adding command (#9)
    * Update .gitignore
    
    * Enhance RemarkCommand
    
    Completed steps up till this step on the tutorial
    
    * Add Remark class to the model
    
    * Add placeholder element for remark to the UI
    
    * Modify Person to support Remark field
    
    * Update Storage
    
    Add Remark field to JsonAdaptedPerson class
    
    * Finalise UI - Connect Remark field to PersonCard
    
    * Modify RemarkCommand execute() method

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -22,17 +22,20 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Remark remark;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address,
+                  Remark remark,Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.remark = remark;
         this.tags.addAll(tags);
     }
 
@@ -50,6 +53,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Remark getRemark() {
+        return remark;
     }
 
     /**
@@ -112,6 +119,8 @@ public class Person {
                 .append(getEmail())
                 .append(" Address: ")
                 .append(getAddress())
+                .append(" Remark: ")
+                .append(getRemark())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
         return builder.toString();
