aae50199f773c058d15004cf3a54caf6b6c258f8
eugene teu <eugeneteu@gmail.com>
2019-10-01 14:41:05 +0800

    encapsulate logic inside TabPanel controller class

--- src/main/java/seedu/address/ui/TabPanel.java
+++ src/main/java/seedu/address/ui/TabPanel.java
@@ -12,9 +12,20 @@ public class TabPanel extends UiPart<Region> {
     private static final String FXML = "TabPanel.fxml";
 
     private PersonListPanel personListPanel;
+    private PersonListPanel personListPanel2;
+    private PersonListPanel personListPanel3;
+    private PersonListPanel personListPanel4;
 
     @FXML
-    private StackPane personListPanelPlaceholder;
+    private StackPane phoneListPanelPlaceholder;
+
+    @FXML
+    private StackPane customerListPanelPlaceholder;
+
+    @FXML
+    private StackPane orderListPanelPlaceholder;
+    @FXML
+    private StackPane scheduleListPanelPlaceholder;
 
     @FXML
     private TabPane tabPanel;
@@ -22,6 +33,23 @@ public class TabPanel extends UiPart<Region> {
     public TabPanel(Logic logic) {
         super(FXML);
         personListPanel = new PersonListPanel(logic.getFilteredPersonList());
-        personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+        customerListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+
+        personListPanel2 = new PersonListPanel(logic.getFilteredPersonList());
+        phoneListPanelPlaceholder.getChildren().add(personListPanel2.getRoot());
+
+        personListPanel3 = new PersonListPanel(logic.getFilteredPersonList());
+        orderListPanelPlaceholder.getChildren().add(personListPanel3.getRoot());
+
+        personListPanel4 = new PersonListPanel(logic.getFilteredPersonList());
+        scheduleListPanelPlaceholder.getChildren().add(personListPanel4.getRoot());
+
+
+        tabPanel.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
     }
+
+
+
+
+
 }
