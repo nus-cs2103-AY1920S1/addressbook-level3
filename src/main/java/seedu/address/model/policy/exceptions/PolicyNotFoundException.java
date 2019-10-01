31b9f90fb754ac1dac4c73d309f4319e5f538674
chowyiyin <yiyin.chow@gmail.com>
2019-10-01 23:12:12 +0800

    Add exceptions for policies

---
+++ src/main/java/seedu/address/model/policy/exceptions/PolicyNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.policy.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified policy.
+ */
+public class PolicyNotFoundException extends RuntimeException {}
