45634f10f535ed188bfd0b49b76d9771a5129097
chowyiyin <yiyin.chow@gmail.com>
2019-10-01 23:11:38 +0800

    Create policy class and classes for fields

---
+++ src/main/java/seedu/address/model/policy/StartAge.java
@@ -0,0 +1,57 @@
+package seedu.address.model.policy;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Policy's start age elibility in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAge(String)}
+ */
+public class StartAge {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Ages should only contain numbers.";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{N}] ]*";
+
+    public final String age;
+    /**
+     * Constructs a {@code StartAge}.
+     *
+     * @param age A valid age.
+     */
+    public StartAge(String age) {
+        requireNonNull(age);
+        checkArgument(isValidAge(age), MESSAGE_CONSTRAINTS);
+        this.age = age;
+    }
+
+    /**
+     * Returns true if a given string is a valid coverage declaration.
+     */
+    public static boolean isValidAge(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof StartAge // instanceof handles nulls
+                && age.equals(((StartAge) other).age)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return age.hashCode();
+    }
+
+}
---
+++ src/main/java/seedu/address/model/policy/UniquePolicyList.java
@@ -0,0 +1,138 @@
+package seedu.address.model.policy;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.person.Person;
+import seedu.address.model.policy.exceptions.DuplicatePolicyException;
+import seedu.address.model.policy.exceptions.PolicyNotFoundException;
+
+/**
+ * A list of policies that enforces uniqueness between its elements and does not allow nulls.
+ * A policy is considered unique by comparing using {@code Person#isSamePolicy(Policy)}. As such, adding and updating of
+ * policies uses Policy#isSamePolicy(Policy) for equality so as to ensure that the policy being added or updated is
+ * unique in terms of identity in the UniquePolicyList. However, the removal of a policy uses Policy#equals(Object) so
+ * as to ensure that the policy with exactly the same fields will be removed.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see seedu.address.model.policy.Policy#isSamePolicy(seedu.address.model.policy.Policy)
+ */
+public class UniquePolicyList implements Iterable<Policy> {
+
+    private final ObservableList<Policy> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Policy> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent person as the given argument.
+     */
+    public boolean contains(Policy toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::isSamePolicy);
+    }
+
+    /**
+     * Adds a policy to the list.
+     * The person must not already exist in the list.
+     */
+    public void add(Policy toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicatePolicyException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the policy {@code target} in the list with {@code editedPolicy}.
+     * {@code target} must exist in the list.
+     * The policy identity of {@code editedPolicy} must not be the same as another existing policy in the list.
+     */
+    public void setPolicy(Policy target, Policy editedPolicy) {
+        requireAllNonNull(target, editedPolicy);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new PolicyNotFoundException();
+        }
+
+        if (!target.isSamePolicy(editedPolicy) && contains(editedPolicy)) {
+            throw new DuplicatePolicyException();
+        }
+
+        internalList.set(index, editedPolicy);
+    }
+
+    /**
+     * Removes the equivalent policy from the list.
+     * The policy must exist in the list.
+     */
+    public void remove(Policy toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new PolicyNotFoundException();
+        }
+    }
+
+    public void setPolicies(UniquePolicyList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code policies}.
+     * {@code policies} must not contain duplicate policies.
+     */
+    public void setPersons(List<Policy> policies) {
+        requireAllNonNull(policies);
+        if (!policiesAreUnique(policies)) {
+            throw new DuplicatePolicyException();
+        }
+
+        internalList.setAll(policies);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Policy> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Policy> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UniquePolicyList // instanceof handles nulls
+                        && internalList.equals(((UniquePolicyList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    /**
+     * Returns true if {@code policies} contains only unique policies.
+     */
+    private boolean policiesAreUnique(List<Policy> policies) {
+        for (int i = 0; i < policies.size() - 1; i++) {
+            for (int j = i + 1; j < policies.size(); j++) {
+                if (policies.get(i).isSamePolicy(policies.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
