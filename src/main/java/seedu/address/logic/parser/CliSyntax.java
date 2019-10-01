34537178be36d8165516f511c0c91f9f733d1450
chowyiyin <yiyin.chow@gmail.com>
2019-10-01 23:08:57 +0800

    Add prefixes for policy and NRIC, Date of Birth and Policy To do: should remove policy prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,9 +7,21 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_NRIC = new Prefix("ic/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_DATE_OF_BIRTH = new Prefix("dob/");
+    public static final Prefix PREFIX_POLICY = new Prefix("pol/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
+    public static final Prefix PREFIX_DESCRIPTION = new Prefix ("d/");
+    public static final Prefix PREFIX_COVERAGE = new Prefix ("c/");
+    public static final Prefix PREFIX_DAYS = new Prefix("days/");
+    public static final Prefix PREFIX_MONTHS = new Prefix("months/");
+    public static final Prefix PREFIX_YEARS = new Prefix("years/");
+    public static final Prefix PREFIX_PRICE = new Prefix ("p/");
+    public static final Prefix PREFIX_START_AGE = new Prefix ("sa/");
+    public static final Prefix PREFIX_END_AGE = new Prefix("ea/");
+    public static final Prefix PREFIX_CRITERIA = new Prefix("cr/");
 }
