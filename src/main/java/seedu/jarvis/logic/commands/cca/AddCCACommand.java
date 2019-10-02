8e695154c64a770fde6f2b7d5ca08e1c48049960
therealbuyer <yekai.tan@gmail.com>
2019-10-02 21:38:28 +0800

    add AddCcaCommand

---
+++ src/main/java/seedu/jarvis/logic/commands/cca/AddCCACommand.java
@@ -0,0 +1,26 @@
+package seedu.jarvis.logic.commands.cca;
+
+import seedu.jarvis.logic.commands.Command;
+import seedu.jarvis.logic.commands.CommandResult;
+import seedu.jarvis.logic.commands.exceptions.CommandException;
+import seedu.jarvis.model.Model;
+
+public class AddCcaCommand extends Command {
+
+    public static final String COMMAND_WORD = "cca";
+
+    @Override
+    public boolean hasInverseExecution() {
+        return false;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Hello from CCA command");
+    }
+
+    @Override
+    public CommandResult executeInverse(Model model) throws CommandException {
+        return null;
+    }
+}
