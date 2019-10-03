e504b86991a659ebff56dafa610c7ddf25869ede
Waynefong <waynefong0401@gmail.com>
2019-10-03 23:04:24 +0800

    Sample test class added for Expense.java

---
+++ src/test/java/seedu/address/model/record/ExpenseTest.java
@@ -0,0 +1,16 @@
+package seedu.address.model.record;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseTest {
+
+    @Test
+    void getPrintableMsg() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
