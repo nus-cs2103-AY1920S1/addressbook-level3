51d435fd8a81856ac1d6526958a815b1e72362af
weikiat97 <jerryloh97@hotmail.com>
2019-10-03 13:58:51 +0800

    Removed trailing whitespaces.

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -45,7 +45,7 @@ public class MainApp extends Application {
     protected Storage storage;
     protected Model model;
     protected Config config;
-    
+
     @Override
     public void init() throws Exception {
         logger.info("=============================[ Initializing AddressBook ]===========================");
