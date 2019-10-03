a37191e1476b68041ff08ae7add2528f57da4ee4
weikiat97 <jerryloh97@hotmail.com>
2019-10-03 13:49:52 +0800

    Removed whitespace.

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -45,7 +45,6 @@ public class MainApp extends Application {
     protected Storage storage;
     protected Model model;
     protected Config config;
-
     
     @Override
     public void init() throws Exception {
