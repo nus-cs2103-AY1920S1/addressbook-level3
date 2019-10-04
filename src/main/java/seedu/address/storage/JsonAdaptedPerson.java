8747699c417ba1a6d520866fd4eb0d5460117473
chowyiyin <yiyin.chow@gmail.com>
2019-10-04 15:50:24 +0800

    Add JsonAdaptedPolicy to JsonAdaptedPerson and add criteria to JsonAdaptedPolicy

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -11,11 +11,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.DateOfBirth;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.policy.Policy;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -66,7 +68,7 @@ class JsonAdaptedPerson {
         phone = source.getPhone().value;
         email = source.getEmail().value;
         address = source.getAddress().value;
-        dateOfBirth = source.getDateOfBirth().toString();
+        dateOfBirth = source.getDateOfBirth().value;
         policies.addAll(source.getPolicies().stream()
                 .map(JsonAdaptedPolicy::new)
                 .collect(Collectors.toList()));
@@ -82,8 +84,14 @@ class JsonAdaptedPerson {
      */
     public Person toModelType() throws IllegalValueException {
         final List<Tag> personTags = new ArrayList<>();
+        final List<Policy> policyList = new ArrayList<>();
+
         for (JsonAdaptedTag tag : tagged) {
             personTags.add(tag.toModelType());
+        }
+
+        for (JsonAdaptedPolicy policy : policies) {
+            policyList.add(policy.toModelType());
         }
 
         if (name == null) {
@@ -98,7 +106,7 @@ class JsonAdaptedPerson {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Nric.class.getSimpleName()));
         }
         if (!Nric.isValidNric(nric)) {
-            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+            throw new IllegalValueException(Nric.MESSAGE_CONSTRAINTS);
         }
         final Nric modelNric = new Nric(nric);
 
@@ -126,8 +134,18 @@ class JsonAdaptedPerson {
         }
         final Address modelAddress = new Address(address);
 
+        if (dateOfBirth == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, DateOfBirth.class.getSimpleName()));
+        }
+        if (!DateOfBirth.isValidDateOfBirth(dateOfBirth)) {
+            throw new IllegalValueException(DateOfBirth.MESSAGE_CONSTRAINTS);
+        }
+        final DateOfBirth modelDateOfBirth = new DateOfBirth(dateOfBirth);
+
+        final Set<Policy> modelPolicies = new HashSet<>(policyList);
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+        return new Person(modelName, modelNric, modelPhone, modelEmail, modelAddress, modelDateOfBirth,
+                modelPolicies, modelTags);
     }
 
 }
--- src/main/java/seedu/address/storage/JsonAdaptedPolicy.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPolicy.java
@@ -19,8 +19,11 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.policy.Coverage;
 import seedu.address.model.policy.Description;
+import seedu.address.model.policy.EndAge;
 import seedu.address.model.policy.Policy;
 import seedu.address.model.policy.PolicyName;
+import seedu.address.model.policy.Price;
+import seedu.address.model.policy.StartAge;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -36,6 +39,7 @@ class JsonAdaptedPolicy {
     private final String price;
     private final String startAge;
     private final String endAge;
+    private final List<JsonAdaptedTag> criteria = new ArrayList<>();
     private final List<JsonAdaptedTag> tagged = new ArrayList<>();
 
     /**
@@ -45,6 +49,7 @@ class JsonAdaptedPolicy {
     public JsonAdaptedPolicy(@JsonProperty("name") String name, @JsonProperty("description") String description,
                              @JsonProperty("coverage") String coverage, @JsonProperty("price") String price,
                              @JsonProperty("start age") String startAge, @JsonProperty("end age") String endAge,
+                             @JsonProperty("criteria") List<JsonAdaptedTag> criteria,
                              @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
         this.name = name;
         this.description = description;
@@ -52,6 +57,9 @@ class JsonAdaptedPolicy {
         this.price = price;
         this.startAge = startAge;
         this.endAge = endAge;
+        if (criteria != null) {
+            this.criteria.addAll(criteria);
+        }
         if (tagged != null) {
             this.tagged.addAll(tagged);
         }
@@ -79,8 +87,13 @@ class JsonAdaptedPolicy {
      */
     public Policy toModelType() throws IllegalValueException {
         final List<Tag> policyTags = new ArrayList<>();
+        final List<Tag> criteriaTags = new ArrayList<>();
         for (JsonAdaptedTag tag : tagged) {
             policyTags.add(tag.toModelType());
+        }
+
+        for (JsonAdaptedTag criteria : criteria) {
+            criteriaTags.add(criteria.toModelType());
         }
 
         if (name == null) {
@@ -102,29 +115,39 @@ class JsonAdaptedPolicy {
         if (coverage == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Coverage.class.getSimpleName()));
         }
-        if (!Coverage.isValidCoverage(coverage) {
+        if (!Coverage.isValidCoverage(coverage)) {
             throw new IllegalValueException(Coverage.MESSAGE_CONSTRAINTS);
         }
         final Coverage modelCoverage = new Coverage(coverage);
 
-        if (email == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+        if (price == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Price.class.getSimpleName()));
         }
-        if (!Email.isValidEmail(email)) {
-            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
+        if (!Price.isValidPrice(price)) {
+            throw new IllegalValueException(Price.MESSAGE_CONSTRAINTS);
         }
-        final Email modelEmail = new Email(email);
+        final Price modelPrice = new Price(price);
 
-        if (address == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
+        if (startAge == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, StartAge.class.getSimpleName()));
         }
-        if (!Address.isValidAddress(address)) {
-            throw new IllegalValueException(Address.MESSAGE_CONSTRAINTS);
+        if (!StartAge.isValidAge(startAge)) {
+            throw new IllegalValueException(StartAge.MESSAGE_CONSTRAINTS);
         }
-        final Address modelAddress = new Address(address);
+        final StartAge modelStartAge = new StartAge(startAge);
 
-        final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+        if (endAge == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, EndAge.class.getSimpleName()));
+        }
+        if (!EndAge.isValidAge(endAge)) {
+            throw new IllegalValueException(EndAge.MESSAGE_CONSTRAINTS);
+        }
+        final EndAge modelEndAge = new EndAge(endAge);
+
+        final Set<Tag> modelCriteria = new HashSet<>(criteriaTags);
+        final Set<Tag> modelTags = new HashSet<>(policyTags);
+        return new Policy(modelName, modelDescription, modelCoverage, modelPrice, modelStartAge, modelEndAge,
+                modelCriteria, modelTags);
     }
 
 }
