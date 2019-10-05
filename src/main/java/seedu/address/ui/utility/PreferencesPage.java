d35b02f2d5d57ac92066a40147bf099ef76247f9
Ang Ze Yu <angzeyu@gmail.com>
2019-10-05 17:58:09 +0800

    Changes : - Start work on PreferencesPage ui - Create ToggleFormItem ui and class

--- src/main/java/seedu/address/ui/utility/PreferencesPage.java
+++ src/main/java/seedu/address/ui/utility/PreferencesPage.java
@@ -1,5 +1,6 @@
 package seedu.address.ui.utility;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import seedu.address.logic.Logic;
 import seedu.address.model.Model;
@@ -8,15 +9,28 @@ import seedu.address.ui.template.Page;
 
 public class PreferencesPage extends Page<AnchorPane> {
 
-    private static final String FXML = "PreferencesPage.fxml";
+    private static final String FXML = "common/PreferencesPage.fxml";
 
     public PreferencesPage(MainWindow mainWindow, Logic logic, Model model) {
         super(FXML, mainWindow, logic, model);
+
     }
 
     /**
      * Fills up all the placeholders of this window.
      */
     public void fillPage() {
+    }
+
+    @FXML
+    private void handleEditPrefsDone() {
+        //String commandText = DoneEditPrefsCommand.COMMAND_WORD;
+        //mainWindow.executeGuiCommand(commandText);
+    }
+
+    @FXML
+    private void handleEditPrefsCancel() {
+        //String commandText = CancelEditPrefsCommand.COMMAND_WORD;
+        //mainWindow.executeGuiCommand(commandText);
     }
 }
