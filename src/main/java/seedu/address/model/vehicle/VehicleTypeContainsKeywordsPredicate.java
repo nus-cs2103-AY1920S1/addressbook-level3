78585f2d3cd97a3881b8eea0d68fbf90dab4c27d
Mandy Wang <shunflower320@gmail.com>
2019-10-06 22:05:17 +0800

    adding new line

--- src/main/java/seedu/address/model/vehicle/VehicleTypeContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/vehicle/VehicleTypeContainsKeywordsPredicate.java
@@ -28,3 +28,4 @@ public class VehicleTypeContainsKeywordsPredicate implements Predicate<Vehicle> 
                 && keywords.equals(((VehicleTypeContainsKeywordsPredicate) other).keywords)); // state check
     }
 }
+
