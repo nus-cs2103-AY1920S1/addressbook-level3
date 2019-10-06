74c45d328444c442b29bf598fb9f1bb0b8674f00
kxyoke <kxyoke@gmail.com>
2019-10-06 17:23:53 +0800

    Add address bar and home button for embedded browser. - Address bar text field supports out of focus reverting to website url and on focus select-all-text. - Need to link address bar to new webpage - Need to add parser to facilitate google search of non websites. (If not, give warning that url given is invalid and suggest/redirect to google.) - Need to reformat gui (including google button)

--- src/main/java/seedu/mark/ui/BrowserPanel.java
+++ src/main/java/seedu/mark/ui/BrowserPanel.java
@@ -10,17 +10,25 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker.State;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import seedu.mark.MainApp;
 import seedu.mark.commons.core.LogsCenter;
 
+/**
+ * The Browser Panel of Mark.
+ */
 public class BrowserPanel extends UiPart<Region> {
 
     public static final URL DEFAULT_PAGE =
-            requireNonNull(MainApp.class.getResource(FXML_FILE_FOLDER + "default.html"));
-    //public static final URL GOOGLE_SEARCH = new URL("google.com.sg");
+            requireNonNull(MainApp.class
+                    .getResource(FXML_FILE_FOLDER + "default.html"));
 
     private static final String FXML = "BrowserPanel.fxml";
 
@@ -28,14 +36,50 @@ public class BrowserPanel extends UiPart<Region> {
 
     @FXML
     private WebView browser;
+    @FXML
+    private TextField addressBar;
+    @FXML
+    private ImageView googleLogo;
+    @FXML
+    private Button googleButton;
+
     private WebEngine webEngine;
+    private String currentPageUrl;
 
     public BrowserPanel() {
         super(FXML);
 
-        //browser = new WebView(); //dunnid cus fxml loading will init i think
+        loadGuiAddress();
+        loadGuiGoogleButton();
+        loadGuiBrowser();
+
+        loadPage("https://google.com.sg");
+    }
+
+    private void loadGuiAddress() {
+        addressBar.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            Platform.runLater(() -> {
+                if (newValue) {
+                    logger.info("Address bar status: onfocus");
+                    addressBar.selectAll();
+                } else {
+                    logger.info("Address bar status: outfocus");
+                    if (!currentPageUrl.equals(addressBar.getText())) {
+                        addressBar.setText(currentPageUrl);
+                    }
+                }
+            });
+        });
+    }
+
+    private void loadGuiGoogleButton() {
+        googleLogo.setImage(new Image(
+                MainWindow.class.getResourceAsStream("/images/gLogo.png")
+        ));
+    }
+
+    private void loadGuiBrowser() {
         webEngine = browser.getEngine();
-        //webEngine.setJavaScriptEnabled(true);
 
         webEngine.getLoadWorker()
                 .stateProperty()
@@ -46,33 +90,31 @@ public class BrowserPanel extends UiPart<Region> {
                                                 State oldValue,
                                                 State newValue) {
 
-                                String pageUrl = webEngine.getLocation();
+                                currentPageUrl = webEngine.getLocation();
                                 if (newValue == State.FAILED) {
                                     logger.info("browser: unable to connect to internet");
                                     loadDefaultPage();
-                                    //logger.info("loading default page now");
-                                    //tryLoadingAgain(pageUrl);
                                 }
+                                showAddressOnAddressBar(currentPageUrl);
                             }
                         }
                 );
-        //webEngine.load("http://google.com.sg");
-        //loadDefaultPage();
-        loadPage("https://google.com.sg");
     }
 
+    ////////////////////// MAIN METHODS /////////////////////////
+
+    /**
+     * Loads page with url on the webview.
+     * @param url valid url
+     */
     public void loadPage(String url) {
+        assert checkUrlValid(url) : "invalid url passed to webEngine";
+
         Platform.runLater(() -> webEngine.load(url));
     }
 
-    private void tryLoadingAgain(String url) {
-        try {
-            Thread.sleep(2000);
-            loadPage(url);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-        }
+    private void showAddressOnAddressBar(String url) {
+        addressBar.setText(url);
     }
 
     /**
@@ -82,4 +124,49 @@ public class BrowserPanel extends UiPart<Region> {
         loadPage(DEFAULT_PAGE.toExternalForm());
     }
 
+    /**
+     * Changes webview to site based on input entered in the address bar.
+     */
+    @FXML
+    private void handleAddressBarInput() {
+        //if address legit then load
+        //if not legit h
+        String input = addressBar.getText();
+        logger.info("address bar read: " + input);
+
+        logger.info("Is given input a valid URL? " + checkUrlValid(input));
+        //dunnid clear input
+    }
+
+    /**
+     * Checks if given url is a valid url.
+     * @param url suspected url
+     * @return true if url is valid; else false.
+     */
+    private boolean checkUrlValid(String url) {
+        //check if have protocol in front
+        //if true then test out by creating a url and catching malinformedurlexception?
+        return true;
+    }
+
+    /**
+     * Parses input into a valid url.
+     * If the input is simply lacking a protocol, http:// is prepended.
+     * Otherwise, the input is passed into google search.
+     * @param input non-url input
+     * @return a valid url based on given input.
+     */
+    private String parseNonUrl(String input) {
+        //if is url without protocol, add protocol http://
+        //else google search it... how?
+        return "";
+    }
+
+    /**
+     * Changes webview to google search page.
+     */
+    @FXML
+    private void handleGotoGoogle() {
+        loadPage("https://google.com.sg");
+    }
 }
