b0d709417efef2741eac76eb8e107bbf543c7835
Mandy Wang <shunflower320@gmail.com>
2019-10-06 22:49:32 +0800

    resolving checkstyle

--- src/main/java/seedu/address/model/vehicle/exceptions/DuplicateVehicleException.java
+++ src/main/java/seedu/address/model/vehicle/exceptions/DuplicateVehicleException.java
@@ -1,8 +1,8 @@
 package seedu.address.model.vehicle.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Vehicles (Vehicles are considered duplicates if they have the same
- * identity).
+ * Signals that the operation will result in duplicate Vehicles
+ * (Vehicles are considered duplicates if they have the same identity).
  */
 public class DuplicateVehicleException extends RuntimeException {
     public DuplicateVehicleException() {
