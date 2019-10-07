61caf9b89db1f57947c40ab38280d8d5f155c7a6
Lim Daekoon <daekoon_lim@hotmail.com>
2019-10-07 11:33:49 +0800

    Add CreateActivity command

---
+++ src/main/java/seedu/address/logic/commands/CreateActivityCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Command to create a new Activity.
+ */
+
+public class CreateActivityCommand extends Command {
+    public static final String COMMAND_WORD="activity";
+
+    public CommandResult execute(Model model) {
+        return new CommandResult("Hello from CreateActivity");
+    }
+}
