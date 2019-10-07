960f43cfae64087eddc887214dcdb1b7cf14c28b
dingyuchen <dingyuchen@gmail.com>
2019-10-07 18:04:02 +0800

    create incomplete class for transaction

--- src/main/java/seedu/address/model/BankAccount.java
+++ src/main/java/seedu/address/model/BankAccount.java
@@ -1,6 +1,10 @@
 package seedu.address.model;
 
+import java.util.ArrayList;
+
 public class BankAccount {
     private int income;
     private int outflow;
+    private ArrayList<Transaction> transactions;
+
 }
