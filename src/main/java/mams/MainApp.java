1957a4737596e0c54617fd96b1bdeb53c275fc47
SendorasLeft <35860599+SendorasLeft@users.noreply.github.com>
2019-10-07 14:48:00 +0800

    Update MainApp.java

--- src/main/java/mams/MainApp.java
+++ src/main/java/mams/MainApp.java
@@ -48,7 +48,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing Mams ]===========================");
+        logger.info("=============================[ Initializing MAMS ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
