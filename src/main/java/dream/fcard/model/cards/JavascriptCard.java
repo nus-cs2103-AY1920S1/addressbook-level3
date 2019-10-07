262b02a833beaa517616b540edda56cfe9be9730
AHaliq <liqo12@hotmail.com>
2019-10-07 21:05:50 +0800

    write skeleton for JavascriptCard

--- src/main/java/dream/fcard/model/cards/JavascriptCard.java
+++ src/main/java/dream/fcard/model/cards/JavascriptCard.java
@@ -1,4 +1,35 @@
 package dream.fcard.model.cards;
 
-public class JavascriptCard {
+import dream.fcard.util.json.jsontypes.JSONValue;
+import javafx.scene.Node;
+
+public class JavascriptCard implements FlashCard {
+
+    protected String front;
+    protected String output;
+
+    public JavascriptCard(String frontString, String outputString) {
+        front = frontString;
+        output = outputString;
+    }
+
+    @Override
+    public JSONValue toJSON() {
+        return null;
+    }
+
+    @Override
+    public Node renderFront() {
+        return null;
+    }
+
+    @Override
+    public Node renderBack() {
+        return null;
+    }
+
+    @Override
+    public Boolean evaluate(String in) {
+        return null;
+    }
 }
---
+++ src/test/java/dream/fcard/model/cards/JavascriptCardTest.java
@@ -0,0 +1,14 @@
+package dream.fcard.model.cards;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class JavascriptCardTest {
+
+    @Test
+    void testEvaluate() {
+        //JavascriptCard card = new JavascriptCard("Echo 123", "123");
+        //assertEquals(true, card.evaluate("console.log('123');"));
+    }
+}
