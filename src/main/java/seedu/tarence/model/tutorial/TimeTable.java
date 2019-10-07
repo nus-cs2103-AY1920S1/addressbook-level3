AY1920S1-CS2103-T14-2/main

Added Attendance, missing tests for Tutorial and associated subclasses.

src/main/java/seedu/tarence/model/tutorial/TimeTable.java
@@ -10,26 +10,23 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * Represents a TimeTable.
- * Guarantees: details are present and not null, field values are validated, immutable.
+ * Represents a TimeTable for a Tutorial. Guarantees: details are present and not null, field
+ * values are validated, immutable.
  */
 public class TimeTable {
     private final Duration duration;
     private final DayOfWeek day;
-    private final LocalTime time;
-    private final List<Integer> weeks;
+    private final LocalTime startTime;
+    private final List<Week> weeks;
 
     /**
      * Every field must be present and not null.
      */
-    public TimeTable(DayOfWeek day, LocalTime time, List<Integer> weeks, Duration duration) {
-        requireAllNonNull(day, time, weeks, duration);
+    public TimeTable(DayOfWeek day, LocalTime startTime,
+            List<Week> weeks, Duration duration) {
+        requireAllNonNull(day, startTime, weeks, duration);
         this.day = day;
-        this.time = time;
-
-        for (Integer week : weeks) {
-            assert(week >= 1 && week <= 13);
-        }
+        this.startTime = startTime;
         this.weeks = weeks;
         this.duration = duration;
 
@@ -39,11 +36,11 @@ public class TimeTable {
         return day;
     }
 
-    public LocalTime getTime() {
-        return time;
+    public LocalTime getStartTime() {
+        return startTime;
     }
 
-    public List<Integer> getWeeks() {
+    public List<Week> getWeeks() {
         return weeks;
     }
 
@@ -52,7 +49,7 @@ public class TimeTable {
     }
 
     /**
-     * Returns true if both timetables have the same identity and data fields.
+     * Returns true if both timetables have the same identity or data fields.
      */
     @Override
     public boolean equals(Object other) {
@@ -66,7 +63,7 @@ public class TimeTable {
 
         TimeTable otherTimeTable = (TimeTable) other;
         return otherTimeTable.getDay().equals(getDay())
-                && otherTimeTable.getTime().equals(getTime())
+                && otherTimeTable.getStartTime().equals(getStartTime())
                 && otherTimeTable.getWeeks().equals(getWeeks())
                 && otherTimeTable.getDuration().equals(getDuration());
     }
@@ -74,7 +71,7 @@ public class TimeTable {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(day, time, weeks, duration);
+        return Objects.hash(day, startTime, weeks, duration);
     }
 
     @Override
@@ -83,8 +80,8 @@ public class TimeTable {
         builder.append(getWeeks())
                 .append(" Day: ")
                 .append(getDay())
-                .append(" Time: ")
-                .append(getTime())
+                .append(" Start Time: ")
+                .append(getStartTime())
                 .append(" Weeks: ")
                 .append(getWeeks())
                 .append(" Duration: ")

src/test/java/seedu/tarence/model/tutorial/TimeTableTest.java
@@ -1,0 +1,48 @@
+package seedu.tarence.model.tutorial;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.tarence.testutil.Assert.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.tarence.testutil.TimeTableBuilder;
+
+public class TimeTableTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TimeTable(null, null, null, null));
+    }
+
+    @Test
+    public void equals() {
+        // same day, startTime, weeks, duration
+        TimeTable timetable = new TimeTableBuilder().build();
+        TimeTable timetableCopy = new TimeTableBuilder(timetable).build();
+        assertTrue(timetable.equals(timetableCopy));
+
+        // different day
+        TimeTable timetableDiffDay = new TimeTableBuilder(timetable).withDayOfWeek("FRIDAY").build();
+        assertFalse(timetable.equals(timetableDiffDay));
+
+        // different start time
+        TimeTable timetableDiffStartTime = new TimeTableBuilder(timetable).withStartTime("00:00:00").build();
+        assertFalse(timetable.equals(timetableDiffStartTime));
+
+        // different weeks
+        List<Week> weeks = new ArrayList<>();
+        weeks.add(new Week(1));
+        weeks.add(new Week(3));
+        weeks.add(new Week(5));
+        TimeTable timetableDiffWeeks = new TimeTableBuilder(timetable).withWeeks(weeks).build();
+        assertFalse(timetable.equals(timetableDiffWeeks));
+
+        // different duration
+        TimeTable timetableDiffDuration = new TimeTableBuilder(timetable).withDuration(0).build();
+        assertFalse(timetable.equals(timetableDiffDuration));
+    }
+}

src/test/java/seedu/tarence/model/tutorial/TutorialTest.java
@@ -1,0 +1,78 @@
+package seedu.tarence.model.tutorial;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.tarence.testutil.Assert.assertThrows;
+import static seedu.tarence.testutil.TypicalStudents.ALICE;
+import static seedu.tarence.testutil.TypicalTutorials.CS1020_LAB01;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.tarence.model.student.Student;
+import seedu.tarence.testutil.TutorialBuilder;
+
+public class TutorialTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Tutorial(null, null, null, null, null, null, null));
+    }
+
+    @Test
+    public void isSameTutorial() {
+        // same name, timetable and modcode
+        Tutorial copy = new TutorialBuilder(CS1020_LAB01).build();
+        assertTrue(CS1020_LAB01.isSameTutorial(copy));
+
+        // different timetable
+        TimeTable timetable = new TimeTable(DayOfWeek.FRIDAY, LocalTime.MAX, new ArrayList<>(), Duration.ZERO);
+        Tutorial diffTimeTable = new TutorialBuilder(CS1020_LAB01).withTimeTable(timetable).build();
+        assertFalse(CS1020_LAB01.isSameTutorial(diffTimeTable));
+
+        // different name
+        Tutorial diffTutName = new TutorialBuilder(CS1020_LAB01).withTutName("CS1020_LAB02").build();
+        assertFalse(CS1020_LAB01.isSameTutorial(diffTutName));
+
+        // different students
+        List<Student> students = new ArrayList<>();
+        students.add(ALICE);
+        Tutorial diffStudents = new TutorialBuilder(CS1020_LAB01).withStudents(students).build();
+        assertTrue(CS1020_LAB01.isSameTutorial(diffStudents));
+
+        // different modcode
+        Tutorial diffModCode = new TutorialBuilder(CS1020_LAB01).withModCode("CS2040").build();
+        assertFalse(CS1020_LAB01.isSameTutorial(diffModCode));
+    }
+
+    @Test
+    public void equals() {
+        // same name and students
+        Tutorial copy = new TutorialBuilder(CS1020_LAB01).build();
+        assertTrue(CS1020_LAB01.equals(copy));
+
+        // different timetable
+        TimeTable timetable = new TimeTable(DayOfWeek.FRIDAY, LocalTime.MAX, new ArrayList<>(), Duration.ZERO);
+        Tutorial diffTimeTable = new TutorialBuilder(CS1020_LAB01).withTimeTable(timetable).build();
+        assertTrue(CS1020_LAB01.equals(diffTimeTable));
+
+        // different name
+        Tutorial diffTutName = new TutorialBuilder(CS1020_LAB01).withTutName("CS1020_LAB02").build();
+        assertFalse(CS1020_LAB01.equals(diffTutName));
+
+        // different students
+        List<Student> students = new ArrayList<>();
+        students.add(ALICE);
+        Tutorial diffStudents = new TutorialBuilder(CS1020_LAB01).withStudents(students).build();
+        assertFalse(CS1020_LAB01.equals(diffStudents));
+
+        // different modcode
+        Tutorial diffModCode = new TutorialBuilder(CS1020_LAB01).withModCode("CS2040").build();
+        assertTrue(CS1020_LAB01.equals(diffModCode));
+    }
+}

src/test/java/seedu/tarence/testutil/TutorialBuilder.java
@@ -12,6 +12,7 @@ import seedu.tarence.model.student.Student;
 import seedu.tarence.model.tutorial.TimeTable;
 import seedu.tarence.model.tutorial.TutName;
 import seedu.tarence.model.tutorial.Tutorial;
+import seedu.tarence.model.tutorial.Week;
 
 
 /**
@@ -23,25 +24,27 @@ public class TutorialBuilder {
     public static final String DEFAULT_TUTNAME = "T01";
     public static final int DEFAULT_DURATION = 100;
     public static final String DEFAULT_DAY = "MONDAY";
-    public static final String DEFAULT_TIME = "12:00:00";
-    public static final List<Integer> DEFAULT_WEEKS = new ArrayList<>();
+    public static final String DEFAULT_STARTTIME = "12:00:00";
+    public static final List<Week> DEFAULT_WEEKS = new ArrayList<>();
     public static final List<Student> DEFAULT_STUDENTS = new ArrayList<>();
 
     private ModCode modCode;
     private Duration duration;
     private DayOfWeek day;
-    private LocalTime time;
-    private List<Integer> weeks;
+    private LocalTime startTime;
+    private List<Week> weeks;
     private TutName tutName;
+    private List<Student> students;
 
     public TutorialBuilder() {
         modCode = new ModCode(DEFAULT_MODCODE);
         duration = Duration.ofMinutes(DEFAULT_DURATION);
         day = DayOfWeek.valueOf(DEFAULT_DAY);
-        time = LocalTime.parse(DEFAULT_TIME, DateTimeFormatter.ISO_TIME);
-        DEFAULT_WEEKS.add(1);
+        startTime = LocalTime.parse(DEFAULT_STARTTIME, DateTimeFormatter.ISO_TIME);
+        DEFAULT_WEEKS.add(new Week(1));
         weeks = DEFAULT_WEEKS;
         tutName = new TutName(DEFAULT_TUTNAME);
+        students = DEFAULT_STUDENTS;
     }
 
     /**
@@ -51,9 +54,10 @@ public class TutorialBuilder {
         this.modCode = tutorialToCopy.getModCode();
         this.duration = tutorialToCopy.getTimeTable().getDuration();
         this.day = tutorialToCopy.getTimeTable().getDay();
-        this.time = tutorialToCopy.getTimeTable().getTime();
+        this.startTime = tutorialToCopy.getTimeTable().getStartTime();
         this.weeks = tutorialToCopy.getTimeTable().getWeeks();
         this.tutName = tutorialToCopy.getTutName();
+        this.students = tutorialToCopy.getStudents();
     }
 
     /**
@@ -70,7 +74,7 @@ public class TutorialBuilder {
     public TutorialBuilder withTimeTable(TimeTable timeTable) {
         this.duration = timeTable.getDuration();
         this.day = timeTable.getDay();
-        this.time = timeTable.getTime();
+        this.startTime = timeTable.getStartTime();
         this.weeks = timeTable.getWeeks();
         return this;
     }
@@ -83,8 +87,17 @@ public class TutorialBuilder {
         return this;
     }
 
+
+    /**
+     * Sets the {@code List<Student>} of the {@code Tutorial} that we are building.
+     */
+    public TutorialBuilder withStudents(List<Student> students) {
+        this.students = students;
+        return this;
+    }
+
     public Tutorial build() {
-        return new Tutorial(tutName, day, time, weeks, duration, DEFAULT_STUDENTS, modCode);
+        return new Tutorial(tutName, day, startTime, weeks, duration, students, modCode);
     }
 
 }

nus-cs2103-AY1920S1/addressbook-level3

src/main/java/seedu/tarence/model/tutorial/TimeTable.java
@@ -6,44 +6,40 @@ import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalTime;
 
-import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 /**
- * Represents a TimeTable.
- * Guarantees: details are present and not null, field values are validated, immutable.
+ * Represents a TimeTable for a Tutorial. Guarantees: details are present and not null, field
+ * values are validated, immutable.
  */
 public class TimeTable {
     private final Duration duration;
     private final DayOfWeek day;
-    private final LocalTime time;
-    private final List<Integer> weeks;
+    private final LocalTime startTime;
+    private final Set<Week> weeks;
 
     /**
      * Every field must be present and not null.
      */
-    public TimeTable(DayOfWeek day, LocalTime time, List<Integer> weeks, Duration duration) {
-        requireAllNonNull(day, time, weeks, duration);
+    public TimeTable(DayOfWeek day, LocalTime startTime,
+            Set<Week> weeks, Duration duration) {
+        requireAllNonNull(day, startTime, weeks, duration);
         this.day = day;
-        this.time = time;
-
-        for (Integer week : weeks) {
-            assert(week >= 1 && week <= 13);
-        }
+        this.startTime = startTime;
         this.weeks = weeks;
         this.duration = duration;
-
     }
 
     public DayOfWeek getDay() {
         return day;
     }
 
-    public LocalTime getTime() {
-        return time;
+    public LocalTime getStartTime() {
+        return startTime;
     }
 
-    public List<Integer> getWeeks() {
+    public Set<Week> getWeeks() {
         return weeks;
     }
 
@@ -52,7 +48,7 @@ public class TimeTable {
     }
 
     /**
-     * Returns true if both timetables have the same identity and data fields.
+     * Returns true if both timetables have the same identity or data fields.
      */
     @Override
     public boolean equals(Object other) {
@@ -66,7 +62,7 @@ public class TimeTable {
 
         TimeTable otherTimeTable = (TimeTable) other;
         return otherTimeTable.getDay().equals(getDay())
-                && otherTimeTable.getTime().equals(getTime())
+                && otherTimeTable.getStartTime().equals(getStartTime())
                 && otherTimeTable.getWeeks().equals(getWeeks())
                 && otherTimeTable.getDuration().equals(getDuration());
     }
@@ -74,7 +70,7 @@ public class TimeTable {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(day, time, weeks, duration);
+        return Objects.hash(day, startTime, weeks, duration);
     }
 
     @Override
@@ -83,8 +79,8 @@ public class TimeTable {
         builder.append(getWeeks())
                 .append(" Day: ")
                 .append(getDay())
-                .append(" Time: ")
-                .append(getTime())
+                .append(" Start Time: ")
+                .append(getStartTime())
                 .append(" Weeks: ")
                 .append(getWeeks())
                 .append(" Duration: ")

src/test/java/seedu/tarence/model/tutorial/TimeTableTest.java
@@ -1,0 +1,46 @@
+package seedu.tarence.model.tutorial;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.tarence.testutil.Assert.assertThrows;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.tarence.testutil.TimeTableBuilder;
+
+public class TimeTableTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TimeTable(null, null, null, null));
+    }
+
+    @Test
+    public void equals() {
+        // same day, startTime, weeks, duration
+        TimeTable timetable = new TimeTableBuilder().build();
+        TimeTable timetableCopy = new TimeTableBuilder(timetable).build();
+        assertTrue(timetable.equals(timetableCopy));
+
+        // different day
+        TimeTable timetableDiffDay = new TimeTableBuilder(timetable).withDayOfWeek("FRIDAY").build();
+        assertFalse(timetable.equals(timetableDiffDay));
+
+        // different start time
+        TimeTable timetableDiffStartTime = new TimeTableBuilder(timetable).withStartTime("00:00:00").build();
+        assertFalse(timetable.equals(timetableDiffStartTime));
+
+        // different weeks
+        Set<Week> weeks = new TreeSet<>(Arrays.asList(new Week(1), new Week(3), new Week(5)));
+        TimeTable timetableDiffWeeks = new TimeTableBuilder(timetable).withWeeks(weeks).build();
+        assertFalse(timetable.equals(timetableDiffWeeks));
+
+        // different duration
+        TimeTable timetableDiffDuration = new TimeTableBuilder(timetable).withDuration(0).build();
+        assertFalse(timetable.equals(timetableDiffDuration));
+    }
+}

src/test/java/seedu/tarence/model/tutorial/TutorialTest.java
@@ -1,0 +1,79 @@
+package seedu.tarence.model.tutorial;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.tarence.testutil.Assert.assertThrows;
+import static seedu.tarence.testutil.TypicalStudents.ALICE;
+import static seedu.tarence.testutil.TypicalTutorials.CS1020_LAB01;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.tarence.model.student.Student;
+import seedu.tarence.testutil.TutorialBuilder;
+
+public class TutorialTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Tutorial(null, null, null, null, null, null, null));
+    }
+
+    @Test
+    public void isSameTutorial() {
+        // same name, timetable and modcode
+        Tutorial copy = new TutorialBuilder(CS1020_LAB01).build();
+        assertTrue(CS1020_LAB01.isSameTutorial(copy));
+
+        // different timetable
+        TimeTable timetable = new TimeTable(DayOfWeek.FRIDAY, LocalTime.MAX, new TreeSet<>(), Duration.ZERO);
+        Tutorial diffTimeTable = new TutorialBuilder(CS1020_LAB01).withTimeTable(timetable).build();
+        assertFalse(CS1020_LAB01.isSameTutorial(diffTimeTable));
+
+        // different name
+        Tutorial diffTutName = new TutorialBuilder(CS1020_LAB01).withTutName("CS1020_LAB02").build();
+        assertFalse(CS1020_LAB01.isSameTutorial(diffTutName));
+
+        // different students
+        List<Student> students = new ArrayList<>();
+        students.add(ALICE);
+        Tutorial diffStudents = new TutorialBuilder(CS1020_LAB01).withStudents(students).build();
+        assertTrue(CS1020_LAB01.isSameTutorial(diffStudents));
+
+        // different modcode
+        Tutorial diffModCode = new TutorialBuilder(CS1020_LAB01).withModCode("CS2040").build();
+        assertFalse(CS1020_LAB01.isSameTutorial(diffModCode));
+    }
+
+    @Test
+    public void equals() {
+        // same name and students
+        Tutorial copy = new TutorialBuilder(CS1020_LAB01).build();
+        assertTrue(CS1020_LAB01.equals(copy));
+
+        // different timetable
+        TimeTable timetable = new TimeTable(DayOfWeek.FRIDAY, LocalTime.MAX, new TreeSet<>(), Duration.ZERO);
+        Tutorial diffTimeTable = new TutorialBuilder(CS1020_LAB01).withTimeTable(timetable).build();
+        assertFalse(CS1020_LAB01.equals(diffTimeTable));
+
+        // different name
+        Tutorial diffTutName = new TutorialBuilder(CS1020_LAB01).withTutName("CS1020_LAB02").build();
+        assertFalse(CS1020_LAB01.equals(diffTutName));
+
+        // different students
+        List<Student> students = new ArrayList<>();
+        students.add(ALICE);
+        Tutorial diffStudents = new TutorialBuilder(CS1020_LAB01).withStudents(students).build();
+        assertFalse(CS1020_LAB01.equals(diffStudents));
+
+        // different modcode
+        Tutorial diffModCode = new TutorialBuilder(CS1020_LAB01).withModCode("CS2040").build();
+        assertFalse(CS1020_LAB01.equals(diffModCode));
+    }
+}

src/test/java/seedu/tarence/testutil/TutorialBuilder.java
@@ -6,12 +6,15 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import seedu.tarence.model.module.ModCode;
 import seedu.tarence.model.student.Student;
 import seedu.tarence.model.tutorial.TimeTable;
 import seedu.tarence.model.tutorial.TutName;
 import seedu.tarence.model.tutorial.Tutorial;
+import seedu.tarence.model.tutorial.Week;
 
 
 /**
@@ -23,25 +26,27 @@ public class TutorialBuilder {
     public static final String DEFAULT_TUTNAME = "T01";
     public static final int DEFAULT_DURATION = 100;
     public static final String DEFAULT_DAY = "MONDAY";
-    public static final String DEFAULT_TIME = "12:00:00";
-    public static final List<Integer> DEFAULT_WEEKS = new ArrayList<>();
+    public static final String DEFAULT_STARTTIME = "12:00:00";
+    public static final Set<Week> DEFAULT_WEEKS = new TreeSet<>();
     public static final List<Student> DEFAULT_STUDENTS = new ArrayList<>();
 
     private ModCode modCode;
     private Duration duration;
     private DayOfWeek day;
-    private LocalTime time;
-    private List<Integer> weeks;
+    private LocalTime startTime;
+    private Set<Week> weeks;
     private TutName tutName;
+    private List<Student> students;
 
     public TutorialBuilder() {
         modCode = new ModCode(DEFAULT_MODCODE);
         duration = Duration.ofMinutes(DEFAULT_DURATION);
         day = DayOfWeek.valueOf(DEFAULT_DAY);
-        time = LocalTime.parse(DEFAULT_TIME, DateTimeFormatter.ISO_TIME);
-        DEFAULT_WEEKS.add(1);
+        startTime = LocalTime.parse(DEFAULT_STARTTIME, DateTimeFormatter.ISO_TIME);
+        DEFAULT_WEEKS.add(new Week(1));
         weeks = DEFAULT_WEEKS;
         tutName = new TutName(DEFAULT_TUTNAME);
+        students = DEFAULT_STUDENTS;
     }
 
     /**
@@ -51,9 +56,10 @@ public class TutorialBuilder {
         this.modCode = tutorialToCopy.getModCode();
         this.duration = tutorialToCopy.getTimeTable().getDuration();
         this.day = tutorialToCopy.getTimeTable().getDay();
-        this.time = tutorialToCopy.getTimeTable().getTime();
+        this.startTime = tutorialToCopy.getTimeTable().getStartTime();
         this.weeks = tutorialToCopy.getTimeTable().getWeeks();
         this.tutName = tutorialToCopy.getTutName();
+        this.students = tutorialToCopy.getStudents();
     }
 
     /**
@@ -70,7 +76,7 @@ public class TutorialBuilder {
     public TutorialBuilder withTimeTable(TimeTable timeTable) {
         this.duration = timeTable.getDuration();
         this.day = timeTable.getDay();
-        this.time = timeTable.getTime();
+        this.startTime = timeTable.getStartTime();
         this.weeks = timeTable.getWeeks();
         return this;
     }
@@ -83,8 +89,16 @@ public class TutorialBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code List<Student>} of the {@code Tutorial} that we are building.
+     */
+    public TutorialBuilder withStudents(List<Student> students) {
+        this.students = students;
+        return this;
+    }
+
     public Tutorial build() {
-        return new Tutorial(tutName, day, time, weeks, duration, DEFAULT_STUDENTS, modCode);
+        return new Tutorial(tutName, day, startTime, weeks, duration, students, modCode);
     }
 
 }