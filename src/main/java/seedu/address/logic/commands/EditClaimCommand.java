91273fd72103f06c1c96f9b5c790d7389c838b3b
weigenie <ieeweigen@hotmail.com>
2019-10-08 01:09:02 +0800

    Added EditClaimCommand

---
+++ src/main/java/seedu/address/logic/commands/EditClaimCommand.java
@@ -0,0 +1,226 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CLAIMS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.claim.Amount;
+import seedu.address.model.claim.Claim;
+import seedu.address.model.claim.Description;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Edits the details of an existing person in the address book.
+ */
+public class EditClaimCommand extends Command {
+
+    public static final String COMMAND_WORD = "edit_claim";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the claim "
+            + "by the index number used in the displayed claim list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_DESCRIPTION + "DESCRIPTION] "
+            + "[" + PREFIX_CASH + "CASH AMOUNT] "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_PHONE + "91234567 "
+            + PREFIX_EMAIL + "johndoe@example.com";
+
+    public static final String MESSAGE_EDIT_CLAIM_SUCCESS = "Edited Claim: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_CLAIM = "This claim already exists in the claim list.";
+
+    private final Index index;
+    private final EditClaimDescriptor editClaimDescriptor;
+
+    /**
+     * @param index of the person in the filtered person list to edit
+     * @param editPersonDescriptor details to edit the person with
+     */
+    public EditClaimCommand(Index index, EditClaimDescriptor editClaimDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editClaimDescriptor);
+
+        this.index = index;
+        this.editClaimDescriptor = new EditClaimDescriptor(editClaimDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Claim> lastShownList = model.getFilteredClaimList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CLAIM_DISPLAYED_INDEX);
+        }
+
+        Claim claimToEdit = lastShownList.get(index.getZeroBased());
+        Claim editedClaim = createEditedClaim(claimToEdit, editClaimDescriptor);
+
+        if (!claimToEdit.isSameClaim(editedClaim) && model.hasClaim(editedClaim)) {
+            throw new CommandException(MESSAGE_DUPLICATE_CLAIM);
+        }
+
+        model.setClaim(claimToEdit, editedClaim);
+        model.updateFilteredClaimList(PREDICATE_SHOW_ALL_CLAIMS);
+        return new CommandResult(String.format(MESSAGE_EDIT_CLAIM_SUCCESS, editedClaim));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Claim createEditedClaim(Claim claimToEdit, EditClaimDescriptor editClaimDescriptor) {
+        assert claimToEdit != null;
+
+        Description updatedDescription = editClaimDescriptor.getDescription().orElse(claimToEdit.getName());
+        Amount updatedAmount = editClaimDescriptor.getAmount().orElse(claimToEdit.getAmount());
+        Name updatedName = editClaimDescriptor.getName().orElse(claimToEdit.getName());
+        Phone updatedPhone = editClaimDescriptor.getPhone().orElse(claimToEdit.getPhone());
+        Set<Tag> updatedTags = editClaimDescriptor.getTags().orElse(claimToEdit.getTags());
+
+        return new Claim(updatedDescription, updatedAmount, updatedName, updatedPhone, updatedTags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditClaimCommand)) {
+            return false;
+        }
+
+        // state check
+        EditClaimCommand e = (EditClaimCommand) other;
+        return index.equals(e.index)
+                && editClaimDescriptor.equals(e.editClaimDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class EditClaimDescriptor {
+        private Description description;
+        private Amount amount;
+        private Name name;
+        private Phone phone;
+        private Set<Tag> tags;
+
+        public EditClaimDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public EditClaimDescriptor(EditClaimDescriptor toCopy) {
+            setDescription(toCopy.description);
+            setAmount(toCopy.amount);
+            setName(toCopy.name);
+            setPhone(toCopy.phone);
+            setTags(toCopy.tags);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(description, amount, name, phone, tags);
+        }
+
+        public void setDescription(Description description) {
+            this.description = description;
+        }
+
+        public Optional<Description> getDescription() {
+            return Optional.ofNullable(description);
+        }
+
+        public void setAmount(Amount amount) {
+            this.amount = amount;
+        }
+
+        public Optional<Amount> getAmount() {
+            return Optional.ofNullable(amount);
+        }
+
+        public void setName(Name name) {
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public void setPhone(Phone phone) {
+            this.phone = phone;
+        }
+
+        public Optional<Phone> getPhone() {
+            return Optional.ofNullable(phone);
+        }
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<Tag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditClaimDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditClaimDescriptor e = (EditClaimDescriptor) other;
+
+            return getDescription().equals(e.getDescription())
+                    && getAmount().equals(e.getAmount())
+                    && getName().equals(e.getName())
+                    && getPhone().equals(e.getPhone())
+                    && getTags().equals(e.getTags());
+        }
+    }
+}
--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -15,6 +15,9 @@ public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Person> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
 
+    /** {@code Predicate} that always evaluate to true */
+    Predicate<Claim> PREDICATE_SHOW_ALL_CLAIMS = unused -> true;
+
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
      */
