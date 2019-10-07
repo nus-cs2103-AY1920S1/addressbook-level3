c9867991c3d59d61758c04f539a6cce519f77120
AHaliq <liqo12@hotmail.com>
2019-10-07 20:34:04 +0800

    implemented FlashCard interface

--- src/main/java/dream/fcard/model/cards/FlashCard.java
+++ src/main/java/dream/fcard/model/cards/FlashCard.java
@@ -1,4 +1,32 @@
 package dream.fcard.model.cards;
 
-public class FlashCard {
+import dream.fcard.util.json.jsontypes.JSONValue;
+import javafx.scene.Node;
+
+public interface FlashCard {
+
+    /**
+     * Returns a JSONObject representation of this flashcard.
+     * @return  JSONObject
+     */
+    JSONValue toJSON();
+
+    /**
+     * Returns render of front of this flash card.
+     * @return  JavaFX Node
+     */
+    Node renderFront();
+
+    /**
+     * Returns render of back of this flash card
+     * @return  JavaFX Node
+     */
+    Node renderBack();
+
+    /**
+     * Evaluate if the input matches the card
+     * @param in    input
+     * @return      true if its a valid match
+     */
+    Boolean evaluate(String in);
 }
