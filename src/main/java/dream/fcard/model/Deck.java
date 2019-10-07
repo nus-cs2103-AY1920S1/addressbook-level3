80905fe5eba7ed59208c525ccd511fb97cb06566
AHaliq <liqo12@hotmail.com>
2019-10-07 21:06:03 +0800

    skeleton for Deck

--- src/main/java/dream/fcard/model/Deck.java
+++ src/main/java/dream/fcard/model/Deck.java
@@ -1,4 +1,39 @@
 package dream.fcard.model;
 
+import dream.fcard.model.cards.FlashCard;
+import dream.fcard.util.json.jsontypes.JSONValue;
+import java.util.ArrayList;
+import javafx.scene.Node;
+
 public class Deck {
+
+    private ArrayList<FlashCard> cards;
+
+    public Deck () {
+        cards = new ArrayList<>();
+    }
+
+    public Deck(ArrayList<FlashCard> initialCards) {
+        cards = initialCards;
+    }
+
+    public JSONValue toJSON() {
+        return null;
+    }
+
+    /**
+     * Render all the cards of the deck in a list.
+     * @return  node of list
+     */
+    public Node renderListView() {
+        return null;
+    }
+
+    /**
+     * Render tile icon of this deck to display alongside other decks.
+     * @return  node of tile
+     */
+    public Node renderTileView() {
+        return null;
+    }
 }
