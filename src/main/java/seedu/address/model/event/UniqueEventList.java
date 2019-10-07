6b8cb081db471c0d0887738f3b2dc121fe7e70b5
calvincxz <cxingzhu@hotmail.com>
2019-10-07 19:56:13 +0800

    correction v2

--- src/main/java/seedu/address/model/event/UniqueEventList.java
+++ src/main/java/seedu/address/model/event/UniqueEventList.java
@@ -3,13 +3,10 @@ package seedu.address.model.event;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Iterator;
 import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 
