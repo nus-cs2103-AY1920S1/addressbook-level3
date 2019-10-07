d9f805595e18de76e2ac70bb9e770f7b1a742fd5
AHaliq <liqo12@hotmail.com>
2019-10-07 20:51:17 +0800

    implemented frontbackcard

--- src/main/java/dream/fcard/model/cards/FrontBackCard.java
+++ src/main/java/dream/fcard/model/cards/FrontBackCard.java
@@ -1,4 +1,35 @@
 package dream.fcard.model.cards;
 
-public class FrontBackCard {
+import dream.fcard.util.json.jsontypes.JSONValue;
+import javafx.scene.Node;
+
+public class FrontBackCard implements FlashCard {
+
+    protected String back;
+    protected String front;
+
+    public FrontBackCard(String frontString, String backString) {
+        back = backString;
+        front = frontString;
+    }
+
+    @Override
+    public JSONValue toJSON() {
+        return null;
+    }
+
+    @Override
+    public Node renderFront() {
+        return null;
+    }
+
+    @Override
+    public Node renderBack() {
+        return null;
+    }
+
+    @Override
+    public Boolean evaluate(String in) {
+        return in.equals(back);
+    }
 }
