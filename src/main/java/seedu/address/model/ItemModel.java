87387a7e2488d6bd7bbd2f8cce247cc08621c1b4
Siang Hwee <siang_hwee@hotmail.com>
2019-10-09 01:52:11 +0800

    Create interface to contain the API for the model

--- src/main/java/seedu/address/model/ItemModel.java
+++ src/main/java/seedu/address/model/ItemModel.java
@@ -1,102 +1,65 @@
 package seedu.address.model;
 
-import seedu.address.model.item.EventList;
+import java.nio.file.Path;
+
+import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.item.Item;
 import seedu.address.model.item.ItemList;
-import seedu.address.model.item.ReminderList;
-import seedu.address.model.item.TaskList;
 
 /**
- * Represents the model for ELISA
+ * The API of the Model component.
  */
-public class ItemModel {
-    private ItemList allItems;
-    private TaskList taskList;
-    private EventList eventList;
-    private ReminderList reminderList;
-    // The list to be used for visualizing in the Ui
-    private ItemList visualList;
-
-    public ItemModel() {
-        this.allItems = new ItemList();
-        this.taskList = new TaskList();
-        this.eventList = new EventList();
-        this.reminderList = new ReminderList();
-        this.visualList = taskList;
-    }
+public interface ItemModel {
 
     /**
-     * Adds an item to the respective list. All items will be added to the central list.
-     * It will also be added to the respective list depending on whether it is a task, event or a reminder.
-     * @param item the item to be added to the program
+     * Replaces user prefs data with the data in {@code userPrefs}.
      */
-    public void add (Item item) {
-        allItems.add(item);
-        if (item.isTask()) {
-            taskList.add(item);
-        }
-
-        if (item.isEvent()) {
-            eventList.add(item);
-        }
-
-        if (item.isReminder()) {
-            reminderList.add(item);
-        }
-    }
+    void setUserPrefs(ReadOnlyUserPrefs userPrefs);
 
     /**
-     * Adds an item to a specific list
-     * @param item the item to be added to the list
-     * @param il the list the item is to be added to
+     * Returns the user prefs.
      */
-    public void add (Item item, ItemList il) {
-        il.add(item);
-    }
+    ReadOnlyUserPrefs getUserPrefs();
 
     /**
-     * Remove an item from the current list.
-     * @param index the item to be removed from the current list
-     * @return the item that was removed
+     * Returns the user prefs' GUI settings.
      */
-    public Item remove(int index) {
-        Item item = visualList.remove(index);
-        if (visualList instanceof TaskList) {
-            taskList.remove(item);
-        } else if (visualList instanceof EventList) {
-            eventList.remove(item);
-        } else if (visualList instanceof ReminderList) {
-            reminderList.remove(item);
-        } else {
-            // never reached here as there are only three variants for the visualList
-        }
-
-        return item;
-    }
+    GuiSettings getGuiSettings();
 
     /**
-     * Deletes an item from the program.
-     * @param index the index of the item to be deleted.
-     * @return the item that was deleted from the program
+     * Sets the user prefs' GUI settings.
      */
-    public Item delete(int index) {
-        Item item = visualList.remove(index);
-        allItems.remove(item);
-        taskList.remove(item);
-        eventList.remove(item);
-        reminderList.remove(item);
-        return item;
-    }
-
-    public ItemList getVisualList() {
-        return this.visualList;
-    }
+    void setGuiSettings(GuiSettings guiSettings);
 
     /**
-     * Set the new item list to be the visualization list.
-     * @param il the item list to be visualized
+     * Returns the user prefs' address book file path.
      */
-    public void setVisualList(ItemList il) {
-        this.visualList = il;
-    }
+    Path getAddressBookFilePath();
+
+    /**
+     * Sets the user prefs' address book file path.
+     */
+    void setAddressBookFilePath(Path addressBookFilePath);
+
+    /**
+     * Replaces address book data with the data in {@code addressBook}.
+     */
+    void setAddressBook(ReadOnlyAddressBook addressBook);
+
+    /** Returns the AddressBook */
+    ReadOnlyAddressBook getAddressBook();
+
+    public void addItem(Item item);
+
+    public void setItem(Item item, Item newItem);
+
+    public Item removeItem(int index);
+
+    public Item deleteItem(int index);
+
+    public ItemList getVisualList();
+
+    public void setVisualList(ItemList il);
+
+
 }
