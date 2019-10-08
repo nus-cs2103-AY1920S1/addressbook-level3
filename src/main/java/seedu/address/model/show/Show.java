3fa43e3546b748cf29521f7d1945fc6bfa31734a
jcjjjared <jcjjjared@gmail.com>
2019-10-08 21:15:37 +0800

    Fix lexicographical order of imports

--- src/main/java/seedu/address/model/show/Show.java
+++ src/main/java/seedu/address/model/show/Show.java
@@ -4,8 +4,8 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Collections;
 import java.util.Objects;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 import seedu.address.model.actor.Actor;
 
