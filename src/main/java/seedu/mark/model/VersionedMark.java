183e815c3417e0cf269724b8d7bfa3b4e770d801
Na-Nazhou <nazhou.na9@gmail.com>
2019-10-08 20:20:37 +0800

    Add Redo, Undo command and VersionedMark

---
+++ src/main/java/seedu/mark/model/VersionedMark.java
@@ -0,0 +1,91 @@
+package seedu.mark.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@code Mark} that keeps track of its own history.
+ */
+public class VersionedMark extends Mark {
+
+    private final List<ReadOnlyMark> markStateList;
+    private int currentStatePointer;
+
+    public VersionedMark(ReadOnlyMark initialState) {
+        super(initialState);
+
+        markStateList = new ArrayList<>();
+        markStateList.add(new Mark(initialState));
+        currentStatePointer = 0;
+    }
+
+    /**
+     * Appends a copy of the current {@code Mark} state to the end of the state list.
+     * Undone states after the current state pointer are removed from the state list.
+     */
+    public void commit() {
+        removeStatesAfterCurrentPointer();
+        markStateList.add(new Mark(this));
+        currentStatePointer++;
+    }
+
+    private void removeStatesAfterCurrentPointer() {
+        markStateList.subList(currentStatePointer + 1, markStateList.size()).clear();
+    }
+
+    /**
+     * Restores the mark to its previous state.
+     */
+    public void undo() {
+        if (!canUndo()) {
+            throw new RuntimeException("Current state pointer at start of markState list, unable to undo.");
+        }
+        currentStatePointer--;
+        resetData(markStateList.get(currentStatePointer));
+    }
+
+    /**
+     * Restores the mark to its previously undone state.
+     */
+    public void redo() {
+        if (!canRedo()) {
+            throw new RuntimeException("Current state pointer at end of markState list, unable to redo.");
+        }
+        currentStatePointer++;
+        resetData(markStateList.get(currentStatePointer));
+    }
+
+    /**
+     * Returns true if {@code undo()} has mark states to undo.
+     */
+    public boolean canUndo() {
+        return currentStatePointer > 0;
+    }
+
+    /**
+     * Returns true if {@code redo()} has mark states to redo.
+     */
+    public boolean canRedo() {
+        return currentStatePointer < markStateList.size() - 1;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof VersionedMark)) {
+            return false;
+        }
+
+        VersionedMark otherVersionedMark = (VersionedMark) other;
+
+        // state check
+        return super.equals(otherVersionedMark)
+                && markStateList.equals(otherVersionedMark.markStateList)
+                && currentStatePointer == otherVersionedMark.currentStatePointer;
+    }
+}
