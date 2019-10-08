7a00f9f545202c851e65c6da1fc44a53602f7d80
Lim Daekoon <daekoon_lim@hotmail.com>
2019-10-09 00:42:47 +0800

    Modify activity command to throw command not implemented yet error

--- src/main/java/seedu/address/logic/commands/ActivityCommand.java
+++ src/main/java/seedu/address/logic/commands/ActivityCommand.java
@@ -8,9 +8,13 @@ import seedu.address.model.Model;
  */
 
 public class ActivityCommand extends Command {
-    public static final String COMMAND_WORD="activity";
+    public static final String COMMAND_WORD = "activity";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a new Activity.\n"
+            + "Parameters: \n"
+            + "Example: Activity ";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Activity command not implemented yet";
 
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from CreateActivity");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
