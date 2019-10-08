624bf7c03a3150ff3caa07ed9c837958ba371d3e
atharvjoshi <atharvjoshi@ymail.com>
2019-10-08 15:01:02 +0800

    Add JavaFX support for Vehicle Model.
    - Create VehicleCard and VehicleListPanel and relevant fxml files.
    - Change GUI layout by splitting panes in main window.
    - Refine Vehicle model by adding Availability class as attribute.

---
+++ src/main/java/seedu/address/model/vehicle/Availability.java
@@ -0,0 +1,65 @@
+package seedu.address.model.vehicle;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a vehicle's availability in the IMS.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAvailability(String)}
+ */
+public class Availability {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Availability is either 'AVAILABLE' or 'BUSY'";
+
+    // Vehicle availability status tags
+    public static final String VEHICLE_AVAILABLE_TAG = "AVAILABLE";
+    public static final String VEHICLE_BUSY_TAG = "BUSY";
+
+    public String availability;
+    /**
+     * Constructs an {@code Availability}.
+     *
+     * @param d A valid Availability tag.
+     */
+    public Availability(String d) {
+        requireNonNull(d);
+        checkArgument(isValidAvailability(d), MESSAGE_CONSTRAINTS);
+         this.availability = d.toUpperCase();
+    }
+
+    /**
+     * Returns true if a given string is a valid Availability.
+     */
+    public static boolean isValidAvailability(String test) {
+        return test.equalsIgnoreCase(VEHICLE_AVAILABLE_TAG) || test.equalsIgnoreCase(VEHICLE_BUSY_TAG);
+    }
+
+    /**
+     * Sets availability to given availability.
+     * @param updatedAvailability updated availability
+     */
+    public void setAvailability(Availability updatedAvailability) {
+        this.availability = updatedAvailability.availability;
+    }
+
+    @Override
+    public String toString() {
+        return availability;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Availability // instanceof handles nulls
+                && availability.equals(((Availability) other).availability)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return availability.hashCode();
+    }
+
+}
+
+
---
+++ src/main/java/seedu/address/ui/VehicleCard.java
@@ -0,0 +1,66 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.vehicle.Vehicle;
+
+/**
+ * An UI component that displays information of a {@code Vehicle}.
+ */
+public class VehicleCard extends UiPart<Region> {
+
+    private static final String FXML = "VehicleListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Vehicle vehicle;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label id;
+    @FXML
+    private Label vehicleNumber;
+    @FXML
+    private Label vehicleType;
+    @FXML
+    private Label district;
+    @FXML
+    private Label availability;
+
+    public VehicleCard(Vehicle vehicle, int displayedIndex) {
+        super(FXML);
+        this.vehicle = vehicle;
+        id.setText(displayedIndex + ". ");
+        vehicleNumber.setText(vehicle.getVehicleNumber().vehicleNumber);
+        vehicleType.setText(vehicle.getVehicleType().vehicleType);
+        district.setText(vehicle.getDistrict().district);
+        availability.setText(vehicle.getAvailability().availability);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof VehicleCard)) {
+            return false;
+        }
+
+        // state check
+        VehicleCard card = (VehicleCard) other;
+        return id.getText().equals(card.id.getText())
+                && vehicle.equals(card.vehicle);
+    }
+}
