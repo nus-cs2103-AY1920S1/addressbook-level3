028828facc6e15d60b99dd925def68032ada6db5
limhawjia <hawjiaa@gmail.com>
2019-10-08 20:27:17 +0800

    Add models for test case and user program

---
+++ src/main/java/com/dukeacademy/model/UserProgram.java
@@ -0,0 +1,12 @@
+package com.dukeacademy.model;
+
+/**
+ * Represents a user's submission for a question.
+ */
+public class UserProgram {
+    private String sourceCode;
+
+    public String getSourceCodeAsString() {
+        return this.sourceCode;
+    }
+}
