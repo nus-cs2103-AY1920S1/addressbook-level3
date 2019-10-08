34a2f554224068aa74584dd6106afac92d711c51
berttwm <berttwm@gmail.com>
2019-10-08 17:56:39 +0800

    Tentative SplitTransaction and SplitCommand created. (Not completed)

--- src/main/java/seedu/address/logic/commands/SplitCommand.java
+++ src/main/java/seedu/address/logic/commands/SplitCommand.java
@@ -1,4 +1,32 @@
 package seedu.address.logic.commands;
 
-public class SplitCommand {
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.transaction.Transaction;
+
+import static java.util.Objects.requireNonNull;
+
+public class SplitCommand extends Command {
+
+    public static final String COMMAND_WORD = "split";
+
+    public static final String MESSAGE_SUCCESS = "Split amount successful";
+    Transaction transaction;
+
+    /*
+    incomplete class, is SplitCommand considered a transaction?
+     */
+    public SplitCommand(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        model.addTransaction(transaction);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, transaction));
+    }
+
 }
--- src/main/java/seedu/address/model/transaction/SplitTransaction.java
+++ src/main/java/seedu/address/model/transaction/SplitTransaction.java
@@ -1,4 +1,23 @@
 package seedu.address.model.transaction;
 
-public class SplitTransaction {
+import java.util.Date;
+
+public class SplitTransaction extends Transaction{
+
+    public int numOfSplits;
+
+    /*
+     *
+     * @param numOfSplits will define the number of ways the same amount is going to be divided into
+     */
+    public SplitTransaction(Amount amount, Date date, int numOfSplits) {
+        super(amount, date);
+        this.numOfSplits = numOfSplits;
+    }
+
+    @Override
+    public Amount handleBalance(Amount balance){
+        Amount myNewBalance = balance.divideAmount(numOfSplits);
+        return myNewBalance;
+    }
 }
