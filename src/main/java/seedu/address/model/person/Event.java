eea6fca90e7fa056bc2b97380de359a35f461a3f
blimyj <blimyj21@gmail.com>
2019-10-09 02:03:09 +0800

    Implement Event class

--- src/main/java/seedu/address/model/person/Event.java
+++ src/main/java/seedu/address/model/person/Event.java
@@ -1,46 +1,109 @@
 package seedu.address.model.person;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * Represents an Item's Event in ELISA.
- * Guarantees: immutable; is valid as declared in {@link #isValidEvent(String)}
+ * Duration of Event is 0 by default.
+ * Priority of Event is medium by default.
+ * Guarantees: immutable;
  */
 public class Event {
 
-    public final LocalDateTime deadLine;
+    public final LocalDateTime startDateTime;
+    public final LocalDateTime endDateTime;
+    
+    //Duration chosen over Period as Events are unlikely to exceed a day.
+    public final Duration duration;
+    public final Priority priority;
 
     /**
-     * Constructs an {@code Email}.
+     * Constructs an {@code Event}.
      *
-     * @param email A valid email address.
+     * @param startDateTimeString A valid LocalDateTime String that denotes the start of the event.
      */
-    public Email(String email) {
-        requireNonNull(email);
-        checkArgument(isValidEmail(email), MESSAGE_CONSTRAINTS);
-        value = email;
+    public Event(String startDateTimeString, Duration duration, Priority priority) throws IllegalArgumentException{
+        requireNonNull(startDateTimeString);
+        
+        if (duration != null) {
+            this.duration = duration;
+        } else {
+            this.duration = Duration.ZERO;
+        }
+
+        if (priority != null) {
+            this.priority = priority;
+        } else {
+            this.priority = Priority.MEDIUM;
+        }
+        
+        
+        try {
+            //Check validity of startDateTimeString by parsing
+            this.startDateTime = LocalDateTime.parse(startDateTimeString);
+            this.endDateTime = startDateTime.plus(duration);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("DateTimeString Invalid! It should follow the ISO_LOCAL_DATE_TIME format:" +
+                    " YYYY-MM-DDTHH:MM. Eg.2019-10-09T02:00");
+        }
+    }
+    
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+    
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
     }
 
-    /**
-     * Returns if a given string is a valid email.
-     */
-    public static boolean isValidEmail(String test) {
-        return test.matches(VALIDATION_REGEX);
+    public Duration getDuration() {
+        return duration;
     }
 
+    public Priority getPriority() {
+        return priority;
+    }
+    
     @Override
     public String toString() {
-        return value;
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Start DateTime: ")
+                .append(getStartDateTime().toString())
+                .append(" End DateTime: ")
+                .append(getEndDateTime().toString())
+                .append(" Duration: ")
+                .append(getDuration().toString())
+                .append(" Priority: ")
+                .append(getPriority().toString());
+        return builder.toString();
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Email // instanceof handles nulls
-                && value.equals(((Email) other).value)); // state check
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Item)) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+        return otherEvent.getStartDateTime().equals(getStartDateTime())
+                && otherEvent.getEndDateTime().equals(getEndDateTime())
+                && otherEvent.getDuration().equals(getDuration())
+                && otherEvent.getPriority().equals(getPriority());
+                
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return Objects.hash(startDateTime, endDateTime, duration, priority);
     }
 
 }
---
+++ src/main/java/seedu/address/model/person/Priority.java
@@ -0,0 +1,5 @@
+package seedu.address.model.person;
+
+public enum Priority {
+    HIGH, MEDIUM, LOW;
+}
