5cd041571913ac83725441d6ef9613d75a5f8e63
zhixianggg <zhixiangg.o@gmail.com>
2019-10-08 15:03:16 +0800

    Rename test files

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.PersonBuilder;
 
-public class CustomerPhonePhoneNameContainsKeywordsPredicateTest {
+public class NameContainsKeywordsPredicateTest {
 
     @Test
     public void equals() {
