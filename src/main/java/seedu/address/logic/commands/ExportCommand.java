25dad5f59f99ae8c0900cd1828c85754e0fb544d
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-10-08 19:44:14 +0800

    Minor changes to ExportCommand such that program is able to run

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,11 +1,9 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.category.Category;
 import seedu.address.model.util.FilePath;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FILE_PATH;
 
@@ -17,13 +15,13 @@ public class ExportCommand extends Command {
 
     public static final String COMMAND_WORD = "export";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports some or all of your flashcards into a "
-            + "document file, for easy sharing or use as a cheat sheet.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports your flashcards into a document file, "
+            + "for easy sharing or use as a cheat sheet.\n"
             + "Parameters: "
             + PREFIX_CATEGORY + "CATEGORY "
             + PREFIX_FILE_PATH + "FILE_PATH\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_CATEGORY + "CS2105"
+            + PREFIX_CATEGORY + "CS2105 "
             + PREFIX_FILE_PATH + "C:\\Users\\damithc\\Documents\\CS2105_Cheat_Sheet.docx";
 
     private final Category category;
@@ -37,11 +35,14 @@ public class ExportCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         // TODO
-        requireNonNull(model);
+        /*requireNonNull(model);
         model.updateFilteredFlashCardList(predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_FLASHCARD_LISTED_OVERVIEW,
                         model.getFilteredFlashCardList().size()));
+
+         */
+        return new CommandResult("marmalade ni sugar song peanuts and bitter step");
     }
 
     @Override
