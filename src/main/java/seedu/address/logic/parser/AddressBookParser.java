4be654258a19d00a1b7df33855bb15f106d2008e
ShuTingY <shuting_y@u.nus.edu>
2019-10-09 17:38:09 +0800

    - stylecheck

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,8 +15,8 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EndTestCommand;
 import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCategoryCommand;
 import seedu.address.logic.commands.ExportCommand;
+import seedu.address.logic.commands.FindCategoryCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 
