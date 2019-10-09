dd3624e417b395fcda85e05d70175b7c71b6cbd1
1nefootstep <sheuxiang@gmail.com>
2019-10-09 17:36:59 +0800

    Update TimeSlotUnavailableException javadocs

--- src/main/java/seedu/address/model/day/exceptions/TimeSlotUnavailableException.java
+++ src/main/java/seedu/address/model/day/exceptions/TimeSlotUnavailableException.java
@@ -1,7 +1,7 @@
 package seedu.address.model.day.exceptions;
 
 /**
- * Signals that the operation is unable to continue because there is an overlap in time-slot.
+ * Signals that the operation is unable to continue because there is an overlap in time slot.
  */
 public class TimeSlotUnavailableException extends RuntimeException {
     public TimeSlotUnavailableException() {
