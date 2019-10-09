d490fff3ec9e835ce8c58966a3a81df4b00f45f9
EvonDong <e0322896@u.nus.edu>
2019-10-09 12:47:30 +0800

    Solve conflicts

--- src/main/java/seedu/address/inventory/logic/Logic.java
+++ src/main/java/seedu/address/inventory/logic/Logic.java
@@ -1,15 +1,10 @@
 package seedu.address.inventory.logic;
 
-import seedu.address.inventory.commands.CommandResult;
 import seedu.address.transaction.util.TransactionList;
 
 public interface Logic {
 
     CommandResult execute(String commandText) throws Exception;
 
-<<<<<<< HEAD
     TransactionList getInventoryList() throws Exception;
-=======
-    TransactionList getTransactionList() throws Exception;
->>>>>>> Implemented some Inventory classes
 }
