de2db823a3bb705783e43e73387175e90177bca7
robytanama <tanamaroby@gmail.com>
2019-10-09 10:58:42 +0800

    Fix the prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,7 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
     public static final Prefix PREFIX_CATEGORY = new Prefix("c/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_OPENING_HOURS = new Prefix("oh/");
+    public static final Prefix PREFIX_OPENING_HOURS = new Prefix("o/");
     public static final Prefix PREFIX_RESTRICTIONS = new Prefix("r/");
 
 }
