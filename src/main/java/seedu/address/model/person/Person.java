c64490101138625762b6f19e95f566acd9bc269e
Yen-Peng <46236142+Yen-Peng@users.noreply.github.com>
2019-10-09 23:42:14 +0800

    Amend comments (#81)

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -5,7 +5,7 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import java.util.Objects;
 
 /**
- * Represents a Person in the address book.
+ * Represents a Person in ORGANice.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Person {
