2c250f6660658535ff15a42f770debc3eff4269a
Wong Shun Min <wongsm7@hotmail.com>
2019-10-09 22:04:01 +0800

    Fix style checks

--- src/main/java/seedu/address/logic/commands/EnqueueCommand.java
+++ src/main/java/seedu/address/logic/commands/EnqueueCommand.java
@@ -1,12 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.common.CommandResult;
 import seedu.address.logic.commands.common.ReversibleCommand;
