0a18de826bfb28ac5eef655d1126d1ccd4c2343f
Gi Hun Ko <rhrlgns96@gmail.com>
2019-10-09 16:41:29 +0800

    adding skeleton command for tab command.

---
+++ src/main/java/com/typee/logic/commands/TabCommand.java
@@ -0,0 +1,14 @@
+package com.typee.logic.commands;
+
+import com.typee.logic.commands.exceptions.CommandException;
+import com.typee.model.Model;
+
+/**
+ * Changes the tab/menu of the application window.
+ */
+public class TabCommand extends Command {
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+}
