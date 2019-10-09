f95da862a7b98c19d57582af391b777efd112f3c
EvonDong <e0322896@u.nus.edu>
2019-10-09 19:15:24 +0800

    Clean up conflicts

--- src/main/java/seedu/address/person/logic/LogicManager.java
+++ src/main/java/seedu/address/person/logic/LogicManager.java
@@ -29,13 +29,16 @@ public class LogicManager implements Logic {
     private final AddressBookParser addressBookParser;
     private final seedu.address.transaction.logic.Logic transactionLogic;
     private final seedu.address.reimbursement.logic.Logic reimbursementLogic;
+    private final seedu.address.cashier.logic.Logic cashierLogic;
 
     public LogicManager(Model model, Storage storage, seedu.address.transaction.logic.Logic transactionLogic,
-                        seedu.address.reimbursement.logic.Logic reimbursementLogic) {
+                        seedu.address.reimbursement.logic.Logic reimbursementLogic,
+                        seedu.address.cashier.logic.Logic cashierLogic) {
         this.model = model;
         this.storage = storage;
         this.transactionLogic = transactionLogic;
         this.reimbursementLogic = reimbursementLogic;
+        this.cashierLogic = cashierLogic;
         addressBookParser = new AddressBookParser();
     }
 
@@ -45,7 +48,7 @@ public class LogicManager implements Logic {
 
         CommandResult commandResult;
         Command command = addressBookParser.parseCommand(commandText);
-        commandResult = command.execute(model, transactionLogic, reimbursementLogic);
+        commandResult = command.execute(model, transactionLogic, reimbursementLogic, cashierLogic);
 
         try {
             storage.saveAddressBook(model.getAddressBook());
