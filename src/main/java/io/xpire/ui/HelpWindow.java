42c780baea41e5e54f740b2f001258bfbe264c2f
xiaoyu <tkgs.liu.xiaoyu@gmail.com>
2019-10-09 18:08:29 +0800

    Update User Guide link

--- src/main/java/io/xpire/ui/HelpWindow.java
+++ src/main/java/io/xpire/ui/HelpWindow.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL =
-            "https://github.com/AY1920S1-CS2103T-F11-2/main/blob/master/docs/UserGuide.adoc";
+            "https://ay1920s1-cs2103t-f11-2.github.io/main/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
