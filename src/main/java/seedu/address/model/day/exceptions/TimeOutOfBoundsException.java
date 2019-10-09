3855832b0ef29b347e9f8a7437a96cba54af81e5
1nefootstep <sheuxiang@gmail.com>
2019-10-09 09:06:37 +0800

    Add Day class
    And all the exceptions that day class is dependent on

---
+++ src/main/java/seedu/address/model/day/exceptions/TimeOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.day.exceptions;
+
+/**
+ * Signals that the operation is out of bounds from the time-slots (goes beyond 12 midnight).
+ */
+public class TimeOutOfBoundsException extends RuntimeException {
+}
