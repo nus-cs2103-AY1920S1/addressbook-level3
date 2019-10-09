bd43ccf3a06736acdc177104d3c79df94f3be243
mannggoo <yuhuilim1999@gmail.com>
2019-10-09 23:51:25 +0800

    Bug fix

---
+++ src/main/java/seedu/address/model/item/DeleteLater.java
@@ -0,0 +1,10 @@
+package seedu.address.model.item;
+
+public class DeleteLater {
+    /**
+     * Constructor
+     */
+    public DeleteLater() {
+        System.out.println("Screw this");
+    }
+}
--- src/main/java/seedu/address/model/item/deleteLater.java
+++
@@ -1,7 +0,0 @@
-package seedu.address.model.item;
-
-public class deleteLater {
-    public deleteLater() {
-        System.out.println("Screw this");
-    }
-}
