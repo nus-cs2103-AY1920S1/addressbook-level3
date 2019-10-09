22ca2691f742079bd342d0ec811dd96d60c29927
1nefootstep <sheuxiang@gmail.com>
2019-10-09 08:59:22 +0800

    Add DurationInHalfHour class

---
+++ src/main/java/seedu/address/commons/core/time/DurationInHalfHours.java
@@ -0,0 +1,13 @@
+package seedu.address.commons.core.time;
+
+public class DurationInHalfHours {
+    private final int numberOfHalfHour;
+
+    public DurationInHalfHours(int numberOfHalfHour) {
+        this.numberOfHalfHour = numberOfHalfHour;
+    }
+
+    public int getNumberOfHalfHour() {
+        return numberOfHalfHour;
+    }
+}
