005f5cc2ed63b87d8eafc093e647e159ab2c4ad4
choongyx <yongxin.choong98@gmail.com>
2019-10-10 01:21:17 +0800

    Add comments to test

--- src/test/java/seedu/address/logic/commands/StatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatsCommandTest.java
@@ -10,6 +10,9 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for StatsCommand.
+ */
 class StatsCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
