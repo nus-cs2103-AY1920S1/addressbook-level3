f9f76851558936bea47dcab466668c1c30865dad
TSAI, HSIAO-HAN <sunnyhome3224@gmail.com>
2019-10-09 19:32:22 +0800

    formatting

--- src/test/java/seedu/mark/model/reminder/NoteTest.java
+++ src/test/java/seedu/mark/model/reminder/NoteTest.java
@@ -1,7 +1,7 @@
 package seedu.mark.model.reminder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NoteTest {
-
 }
