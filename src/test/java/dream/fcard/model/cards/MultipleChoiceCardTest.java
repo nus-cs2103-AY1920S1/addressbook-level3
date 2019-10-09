776ff3f7c7ee5f321bcc549b0e3b60184e9d6482
huiminlim <e0036379@u.nus.edu>
2019-10-09 22:18:49 +0800

    Edited: checkstyle

--- src/test/java/dream/fcard/model/cards/MultipleChoiceCardTest.java
+++ src/test/java/dream/fcard/model/cards/MultipleChoiceCardTest.java
@@ -1,10 +1,10 @@
 package dream.fcard.model.cards;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MultipleChoiceCardTest {
 
