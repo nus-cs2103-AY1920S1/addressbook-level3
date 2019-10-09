a7f109451b6d29d30923a23655654527b72baf52
jcjjjared <jcjjjared@gmail.com>
2019-10-09 19:40:50 +0800

    Added model classes for watchlist

---
+++ src/main/java/seedu/address/model/ReadOnlyUserPrefs1.java
@@ -0,0 +1,16 @@
+package seedu.address.model;
+
+import java.nio.file.Path;
+
+import seedu.address.commons.core.GuiSettings;
+
+/**
+ * Unmodifiable view of user prefs.
+ */
+public interface ReadOnlyUserPrefs1 {
+
+    GuiSettings getGuiSettings();
+
+    Path getWatchListFilePath();
+
+}
