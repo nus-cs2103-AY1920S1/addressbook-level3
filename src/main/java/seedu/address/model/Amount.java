8032a2d434b6a6cc31e1aa2a5613fb19b8d34334
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-11 00:10:04 +0800

    Move Amount and Description out of transaction package

--- src/main/java/seedu/address/model/Amount.java
+++ src/main/java/seedu/address/model/Amount.java
@@ -1,4 +1,4 @@
-package seedu.address.model.transaction;
+package seedu.address.model;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
