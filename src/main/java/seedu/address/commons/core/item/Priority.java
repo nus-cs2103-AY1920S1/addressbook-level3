2fecd0e987850e98b2fe7c95e0c368c0f8ab1850
Siang Hwee <siang_hwee@hotmail.com>
2019-10-10 16:16:09 +0800

    Add method to parse a string to a priority

--- src/main/java/seedu/address/commons/core/item/Priority.java
+++ src/main/java/seedu/address/commons/core/item/Priority.java
@@ -1,8 +1,23 @@
 package seedu.address.commons.core.item;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+
 /**
  * Available priority levels for tasks and events.
  */
 public enum Priority {
     HIGH, MEDIUM, LOW;
+
+    public static Priority parse(String status) throws IllegalValueException {
+        switch(status) {
+            case "HIGH":
+                return Priority.HIGH;
+            case "MEDIUM":
+                return Priority.MEDIUM;
+            case "LOW":
+                return Priority.LOW;
+            default:
+                throw new IllegalValueException("Priority not recognized");
+        }
+    }
 }
