81daaaf6e2bd974b5b477e93b9f6f98b6b326553
Ni Tianzhen <nitianzhen1210@gmail.com>
2019-10-10 21:40:55 +0800

    Add tags to encrypted files

--- src/main/java/seedu/address/model/file/EncryptedFile.java
+++ src/main/java/seedu/address/model/file/EncryptedFile.java
@@ -2,7 +2,12 @@ package seedu.address.model.file;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
 
 /**
  * Represents an Encrypted File in SecureIT.
@@ -12,11 +17,14 @@ public class EncryptedFile {
     private final FilePath filePath;
     private final EncryptedAt encryptedAt;
 
-    public EncryptedFile(FileName fileName, FilePath filePath, EncryptedAt encryptedAt) {
-        requireAllNonNull(fileName, filePath, encryptedAt);
+    private final Set<Tag> tags = new HashSet<>();
+
+    public EncryptedFile(FileName fileName, FilePath filePath, EncryptedAt encryptedAt, Set<Tag> tags) {
+        requireAllNonNull(fileName, filePath, encryptedAt, tags);
         this.fileName = fileName;
         this.filePath = filePath;
         this.encryptedAt = encryptedAt;
+        this.tags.addAll(tags);
     }
 
     public FileName getFileName() {
@@ -31,6 +39,14 @@ public class EncryptedFile {
         return encryptedAt;
     }
 
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -43,11 +59,26 @@ public class EncryptedFile {
 
         EncryptedFile otherFile = (EncryptedFile) other;
         return otherFile.getFileName().equals(getFileName())
-                && otherFile.getFilePath().equals(getFilePath());
+                && otherFile.getFilePath().equals(getFilePath())
+                && otherFile.getEncryptedAt().equals(getEncryptedAt())
+                && otherFile.getTags().equals(getTags());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fileName, filePath);
+        return Objects.hash(fileName, filePath, encryptedAt, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getFileName())
+                .append(" Path: ")
+                .append(getFilePath())
+                .append(" Encrypted at: ")
+                .append(getEncryptedAt())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
     }
 }
