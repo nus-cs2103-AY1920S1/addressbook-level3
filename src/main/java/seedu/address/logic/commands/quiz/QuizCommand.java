ab86d4e1040133fbf4cd62aa0e00a1d32f5db103
fabbbbbbyy <fabianchia97@gmail.com>
2019-10-10 23:20:26 +0800

    Add Quiz Command

---
+++ src/main/java/seedu/address/logic/commands/quiz/QuizCommand.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands.quiz;
+
+import seedu.address.logic.commands.Command;
+
+/**
+ * Represents a question command.
+ */
+public abstract class QuizCommand extends Command {
+
+    public static final String COMMAND_WORD = "quiz";
+
+}
