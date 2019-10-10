61651e746210ebac6e46029545a839dba693f4c5
chowyiwai <yiwai.chow99@gmail.com>
2019-10-10 21:24:27 +0800

    Add tag commands to (1) view all tags, (2) view default tags, (3) view modules with specific tags, (4) view tags of a specific module

--- src/main/java/seedu/address/logic/commands/tag/AddTagCommand.java
+++ src/main/java/seedu/address/logic/commands/tag/AddTagCommand.java
@@ -3,19 +3,24 @@ package seedu.address.logic.commands.tag;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.Iterator;
+
+import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.module.UniqueModuleList;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.exceptions.ModuleNotFoundException;
 import seedu.address.model.studyplan.StudyPlan;
-import seedu.address.model.tag.DefaultTagType;
+import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.tag.UserTag;
 
 /**
  * Adds a tag to a module.
  */
-public class AddTagCommand {
+public class AddTagCommand extends Command {
 
     public static final String COMMAND_WORD = "tag";
 
@@ -29,9 +34,11 @@ public class AddTagCommand {
     public static final String MESSAGE_SUCCESS_TAG_ADDED = "New tag created: %1$s \n" + "Tag added to module";
     public static final String MESSAGE_SUCCESS = "Tag added to module";
     public static final String MESSAGE_EXISTING_TAG = "This tag is already attached to this module";
+    public static final String MESSAGE_INVALID_MODULE_CODE = "The provided module code is invalid";
 
     private final String tagName;
     private final String moduleCode;
+    private boolean newTagCreated = false;
 
     /**
      * Creates an {@code AddTagCommand} to add a tag with the given name to the module of the given module code.
@@ -50,16 +57,60 @@ public class AddTagCommand {
         StudyPlan activeStudyPlan = model.getActiveStudyPlan();
         UniqueTagList uniqueTagList = activeStudyPlan.getTags();
 
-        if (!uniqueTagList.getMapTags().containsKey(tagName)) {
-            UserTag toCreate = new UserTag(tagName);
-            uniqueTagList.addUserTag(toCreate);
-            return new CommandResult(String.format(MESSAGE_SUCCESS_TAG_ADDED, toCreate));
+        Tag toAdd = getTagToAdd(uniqueTagList);
+        Module module;
+        try {
+            module = getTargetModule(activeStudyPlan);
+        } catch (ModuleNotFoundException exception) {
+            throw new CommandException(MESSAGE_INVALID_MODULE_CODE);
         }
 
-        UniqueModuleList uniqueModuleList = activeStudyPlan.getModules();
+        if (moduleContainsTag(module, toAdd)) {
+            throw new CommandException(MESSAGE_EXISTING_TAG);
+        }
 
-        return new CommandResult(String.format(MESSAGE_SUCCESS, tagName));
+        module.addTag(toAdd);
+
+        if (newTagCreated) {
+            return new CommandResult(String.format(MESSAGE_SUCCESS_TAG_ADDED, toAdd));
+        }
+
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
+    private boolean moduleContainsTag(Module module, Tag tag) {
+        return module.getTags().contains(tag);
+    }
+
+    private Module getTargetModule(StudyPlan activeStudyPlan) throws ModuleNotFoundException {
+        UniqueModuleList uniqueModuleList = activeStudyPlan.getModules();
+        Iterator<Module> moduleIterator = uniqueModuleList.iterator();
+        while (moduleIterator.hasNext()) {
+            Module currentModule = moduleIterator.next();
+            if (currentModule.getModuleCode().toString().equals(moduleCode)) {
+                return currentModule;
+            }
+        }
+        throw new ModuleNotFoundException();
+    }
+
+    private Tag getTagToAdd(UniqueTagList uniqueTagList) {
+        boolean tagExists = uniqueTagList.getMapTags().containsKey(tagName);
+        Tag toAdd;
+        if (!tagExists) {
+            toAdd = createNewTag(tagName, uniqueTagList);
+        } else {
+            Tag existingTag = uniqueTagList.getMapTags().get(tagName);
+            toAdd = existingTag;
+        }
+        return toAdd;
+    }
+
+    private UserTag createNewTag(String tagName, UniqueTagList uniqueTagList) {
+        UserTag toCreate = new UserTag(tagName);
+        uniqueTagList.addUserTag(toCreate);
+        newTagCreated = true;
+        return toCreate;
+    }
 
 }
