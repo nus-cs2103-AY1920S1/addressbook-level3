100c7af0402af87140ea6eb7854a756f0a52e74d
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-11 00:24:20 +0800

    Change javadocs to use "description" instead of "name" in Description.java

--- src/main/java/seedu/address/model/Description.java
+++ src/main/java/seedu/address/model/Description.java
@@ -32,7 +32,7 @@ public class Description {
     }
 
     /**
-     * Returns true if a given string is a valid name.
+     * Returns true if a given string is a valid description.
      */
     public static boolean isValidDescription(String test) {
         return test.matches(VALIDATION_REGEX);
