c4463913db7f044e784a6e1ade809072b91c59b1
huiminlim <e0036379@u.nus.edu>
2019-10-10 22:09:33 +0800

    Edit: checkstyle

--- src/main/java/dream/fcard/model/cards/MultipleChoiceCard.java
+++ src/main/java/dream/fcard/model/cards/MultipleChoiceCard.java
@@ -2,9 +2,9 @@ package dream.fcard.model.cards;
 
 import java.util.ArrayList;
 
-import dream.fcard.util.json.jsontypes.JsonValue;
-
 import dream.fcard.model.exceptions.ChoiceNotFoundException;
+
+import dream.fcard.util.json.jsontypes.JsonValue;
 
 import javafx.scene.Node;
 
