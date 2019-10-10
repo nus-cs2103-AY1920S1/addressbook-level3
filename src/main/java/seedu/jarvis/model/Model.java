15e4b9505359e3e1cd5464087f28634e10828f16
marcfyk <marcjohnfong@gmail.com>
2019-10-10 14:18:28 +0800

    Update Model and Storage interfaces

--- src/main/java/seedu/jarvis/model/Model.java
+++ src/main/java/seedu/jarvis/model/Model.java
@@ -4,7 +4,7 @@ package seedu.jarvis.model;
  * The API of the {@code Model} component.
  * This extends all the different models for all the features.
  * {@code AddressModel} is the API for AddressBook interactions.
- * {@code UserPrefsModel} is the API for User preferences and GUI settings.
+ * {@code UserPrefsModel} is the API for User Preferences and GUI settings.
  * {@code HistoryModel} is the API for undoing and redoing commands.
  */
 public interface Model extends AddressModel, UserPrefsModel, HistoryModel {}
--- src/main/java/seedu/jarvis/storage/Storage.java
+++ src/main/java/seedu/jarvis/storage/Storage.java
@@ -2,5 +2,8 @@ package seedu.jarvis.storage;
 
 /**
  * API of the Storage component.
+ * This extends al the different models for all the features.
+ * {@code AddressBookStorage} is the API for AddressBook.
+ * {@code UserPrefsStorage} is the API for User Preferences and GUI settings.
  */
 public interface Storage extends AddressBookStorage, UserPrefsStorage {}
