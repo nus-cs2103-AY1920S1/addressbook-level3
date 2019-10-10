9c13f4f1a98e751079099ca97812c2f6d495653a
marcfyk <marcjohnfong@gmail.com>
2019-10-10 22:00:34 +0800

    Add HistoryManagerStorage interface and skeleton of JsonHistoryManagerStorage

---
+++ src/main/java/seedu/jarvis/storage/history/JsonHistoryManagerStorage.java
@@ -0,0 +1,83 @@
+package seedu.jarvis.storage.history;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.jarvis.commons.exceptions.DataConversionException;
+import seedu.jarvis.commons.util.FileUtil;
+import seedu.jarvis.model.history.HistoryManager;
+
+
+/**
+ * A class to access {@code HistoryManager} data stored as a text file on the hard disk.
+ */
+public class JsonHistoryManagerStorage implements HistoryManagerStorage {
+
+    private Path filepath;
+
+    public JsonHistoryManagerStorage(Path filepath) {
+        this.filepath = filepath;
+    }
+
+    public Path getHistoryManagerFilePath() {
+        return filepath;
+    }
+
+    /**
+     * Returns {@code HistoryManager} data.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @return {@code HistoryManager} data, or {@code Optional.empty()} if storage file is not found.
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException             if there was any problem when reading from the storage.
+     */
+    @Override
+    public Optional<HistoryManager> readHistoryManager() throws DataConversionException, IOException {
+        return readHistoryManager(filepath);
+    }
+
+    /**
+     * Returns {@code HistoryManager} data.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @param filePath {@code Path} to read {@code HistoryManager} data.
+     * @return {@code HistoryManager} data, or {@code Optional.empty()} if storage file is not found.
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException             if there was any problem when reading from the storage.
+     */
+    @Override
+    public Optional<HistoryManager> readHistoryManager(Path filePath) throws DataConversionException, IOException {
+        requireNonNull(filePath);
+        return Optional.empty();
+    }
+
+    /**
+     * Saves the given {@link HistoryManager} to the storage.
+     *
+     * @param historyManager {@code HistoryManager} to be saved, which cannot be null.
+     * @throws IOException If there was any problem writing to the file.
+     */
+    @Override
+    public void saveHistoryManager(HistoryManager historyManager) throws IOException {
+        saveHistoryManager(historyManager, filepath);
+    }
+
+    /**
+     * Saves the given {@link HistoryManager} to the storage.
+     *
+     * @param historyManager {@code HistoryManager} to be saved, which cannot be null.
+     * @param filePath       {@code Path} to read {@code HistoryManager} data.
+     * @throws IOException If there was any problem writing to the file.
+     */
+    @Override
+    public void saveHistoryManager(HistoryManager historyManager, Path filePath) throws IOException {
+        requireNonNull(historyManager);
+        requireNonNull(filePath);
+
+        FileUtil.createIfMissing(filePath);
+    }
+
+}
