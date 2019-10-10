d1fb339f19e78c6e03229ba5a72c5af61be538e2
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-10-11 01:31:26 +0800

    Add Parser for AssignPolicyCommand

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -17,8 +17,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
     // Policy prefix
-    public static final Prefix PREFIX_DESCRIPTION = new Prefix ("d/");
-    public static final Prefix PREFIX_COVERAGE = new Prefix ("c/");
+    public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
+    public static final Prefix PREFIX_COVERAGE = new Prefix("c/");
     public static final Prefix PREFIX_DAYS = new Prefix("days/");
     public static final Prefix PREFIX_MONTHS = new Prefix("months/");
     public static final Prefix PREFIX_YEARS = new Prefix("years/");
@@ -26,6 +26,10 @@ public class CliSyntax {
     public static final Prefix PREFIX_START_AGE = new Prefix ("sa/");
     public static final Prefix PREFIX_END_AGE = new Prefix("ea/");
     public static final Prefix PREFIX_CRITERIA = new Prefix("cr/");
+
+    // Prefixes for assign/unassign policy commands
+    public static final Prefix PREFIX_POLICY_INDEX = new Prefix("polindex/");
+    public static final Prefix PREFIX_PERSON_INDEX = new Prefix("perindex/");
 
     public static final Prefix PREFIX_MERGE = new Prefix("m/");
 }
