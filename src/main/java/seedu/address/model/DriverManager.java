15a9d30172426bd25c6de3d6d493be8945c05596
cheeyou <youchee1999@gmail.com>
2019-10-10 16:54:00 +0800

    Revised Driver

--- src/main/java/seedu/address/model/DriverManager.java
+++ src/main/java/seedu/address/model/DriverManager.java
@@ -1,12 +1,27 @@
 package seedu.address.model;
 
 import seedu.address.model.person.Driver;
+import seedu.address.model.person.Person;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Represents the in-memory model of the drivers' data.
+ */
 public class DriverManager extends ModelManager{
-    private Driver driver;
-
-    public DriverManager(Driver driver) {
+    public DriverManager() {
         super();
-        this.driver = driver;
+    }
+
+    public void addDriver(Driver driver) {
+        super.addPerson(driver);
+    }
+
+    public void deleteDriver(Driver target) {
+        super.deletePerson((Person)target);
+    }
+
+    public void setDriver(Driver target, Driver editedDriver) {
+        super.setPerson((Person)target, (Person)editedDriver);
     }
 }
--- src/main/java/seedu/address/model/person/Driver.java
+++ src/main/java/seedu/address/model/person/Driver.java
@@ -1,26 +1,54 @@
 package seedu.address.model.person;
 
+import seedu.address.model.person.exceptions.SchedulingException;
 import seedu.address.model.tag.Tag;
 
 import java.util.Set;
 
+/**
+ * Represents a Driver available for work.
+ * Gurantees: details are present and not null, field values are validated, id is immutable.
+ */
 public class Driver extends Person {
-    private AvailabilityManager allAssignedTimeSlots;
-    private int id;
-    static int IDCOUNT = 1;
 
+    //Identity fields
+    private final int id;
+
+    //data fields
+    static int IDCOUNT = 1;
+    private Schedule schedule;
+
+    /**
+     * Every field must be present and not null.
+     */
     public Driver (Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
-        allAssignedTimeSlots = new AvailabilityManager();
+        schedule = new Schedule();
         id = IDCOUNT;
         IDCOUNT++;
     }
 
-    public boolean addTimeSlot(Duration TimeSlot) {
-        if (allAssignedTimeSlots.isAvailable(TimeSlot)) {
-           allAssignedTimeSlots.addTimeSlot(TimeSlot);
-        }
-        return allAssignedTimeSlots.isAvailable(TimeSlot);
+    public int getId() {
+        return id;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * Returns a string representation of the driver, with identity fields visible to the user.
+     *
+     * @return string representation of driver
+     */
+    @Override
+    public String toString() {
+        StringBuilder driverBuilder = new StringBuilder();
+        driverBuilder.append(" Driver stats: \n")
+                    .append(" id: ")
+                    .append(getId())
+                    .append(super.toString());
+        return driverBuilder.toString();
     }
 
 }
