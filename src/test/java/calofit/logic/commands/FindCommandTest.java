38466d25240a6d0399d75fd3b0ce2a782d08eb1e
NgWenJunFelix <felixng97@gmail.com>
2019-10-10 09:09:41 +0800

    Change expected test result to fit our new food data for Calofit

--- src/test/java/calofit/logic/commands/FindCommandTest.java
+++ src/test/java/calofit/logic/commands/FindCommandTest.java
@@ -63,13 +63,12 @@ public class FindCommandTest {
 
     @Test
     public void execute_multipleKeywords_multipleDishesFound() {
-        String expectedMessage = String.format(MESSAGE_DISHES_LISTED_OVERVIEW, 3);
-        NameContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
+        String expectedMessage = String.format(MESSAGE_DISHES_LISTED_OVERVIEW, 1);
+        NameContainsKeywordsPredicate predicate = preparePredicate("spaghetti");
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredDishList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(
-                TypicalDishes.CHICKEN_RICE, TypicalDishes.CHEESE_BAKED_RICE, TypicalDishes.APPLE_PIE),
+        assertEquals(Arrays.asList(TypicalDishes.SPAGHETTI),
                 model.getFilteredDishList());
     }
 
