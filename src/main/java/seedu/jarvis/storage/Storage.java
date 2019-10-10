a349f5fe7bcc319969426f3a38ed4f0faae8f97b
marcfyk <marcjohnfong@gmail.com>
2019-10-10 14:14:02 +0800

    Remove redundant overrides in Storage

--- src/main/java/seedu/jarvis/storage/Storage.java
+++ src/main/java/seedu/jarvis/storage/Storage.java
@@ -1,32 +1,6 @@
 package seedu.jarvis.storage;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Optional;
-
-import seedu.jarvis.commons.exceptions.DataConversionException;
-import seedu.jarvis.model.ReadOnlyAddressBook;
-import seedu.jarvis.model.ReadOnlyUserPrefs;
-import seedu.jarvis.model.UserPrefs;
-
 /**
- * API of the Storage component
+ * API of the Storage component.
  */
-public interface Storage extends AddressBookStorage, UserPrefsStorage {
-
-    @Override
-    Optional<UserPrefs> readUserPrefs() throws DataConversionException, IOException;
-
-    @Override
-    void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException;
-
-    @Override
-    Path getAddressBookFilePath();
-
-    @Override
-    Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException, IOException;
-
-    @Override
-    void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
-
-}
+public interface Storage extends AddressBookStorage, UserPrefsStorage {}
