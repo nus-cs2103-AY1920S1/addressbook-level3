a8181036e0617d1c8fe1ad0967784305a716271c
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-10-10 12:58:48 +0800

    add javadoc for ExportCommand

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -2,7 +2,12 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.util.CsvUtil;
 import seedu.address.model.Model;
+
+/**
+ * Exports Person data from the AddressBook
+ */
 
 public class ExportCommand extends Command {
 
@@ -14,6 +19,7 @@ public class ExportCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
