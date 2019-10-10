a3f57e0725103c725139ecd1562ef60030c2398c
caesarpjz <caesarpjz@gmail.com>
2019-10-10 22:27:36 +0800

    Fixed errors.

--- src/main/java/seedu/address/logic/commands/earnings/AddEarningsCommand.java
+++ src/main/java/seedu/address/logic/commands/earnings/AddEarningsCommand.java
@@ -5,7 +5,7 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import static seedu.address.logic.parser.CliSyntax.*;
 
 /**
@@ -30,17 +30,17 @@ public class AddEarningsCommand extends Command {
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Add earnings command not implemented yet";
     public static final String MESSAGE_ARGUMENTS = "Date: %1$10s, Module: %2$s, Amount: $ %3$.2f"; // For date, maybe can use this instead --> %tm/%td/%ty
 
-    private final String date;
-    private final String module;
-    private final double amount;
+//    private final String date;
+//    private final String module;
+//    private final double amount;
 
-    public AddEarningsCommand(String date, String module, double amount) {
-        requireAllNonNull(date, module, amount);
-
-        this.date = date;
-        this.module = module;
-        this.amount = amount;
-    }
+//    public AddEarningsCommand(String date, String module, double amount) {
+//        requireAllNonNull(date, module, amount);
+//
+//        this.date = date;
+//        this.module = module;
+//        this.amount = amount;
+//    }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
@@ -48,24 +48,24 @@ public class AddEarningsCommand extends Command {
         //throw new CommandException(String.format(MESSAGE_ARGUMENTS, date, module, amount));
     }
 
-    @Override
-    public boolean equals(Object other) {
-        // short circuit if same object
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof AddEarningsCommand)) {
-            return false;
-        }
-
-        // state check
-        AddEarningsCommand e = (AddEarningsCommand) other;
-        return date.equals(e.date)
-                && module.equals(e.module)
-                && amount == e.amount;
-    }
+//    @Override
+//    public boolean equals(Object other) {
+//        // short circuit if same object
+//        if (other == this) {
+//            return true;
+//        }
+//
+//        // instanceof handles nulls
+//        if (!(other instanceof AddEarningsCommand)) {
+//            return false;
+//        }
+//
+//        // state check
+//        AddEarningsCommand e = (AddEarningsCommand) other;
+//        return date.equals(e.date)
+//                && module.equals(e.module)
+//                && amount == e.amount;
+//    }
 
     // AT PARSE UNIT INPUT!!!!!!!!!!!!!!!!!!!!!!
 }
