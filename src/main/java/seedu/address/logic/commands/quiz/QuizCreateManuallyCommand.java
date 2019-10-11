2e80c01674261a3799f771b2c1121e6aaba23283
fabbbbbbyy <fabianchia97@gmail.com>
2019-10-11 11:59:06 +0800

    Add newline

--- src/main/java/seedu/address/logic/commands/quiz/QuizCreateManuallyCommand.java
+++ src/main/java/seedu/address/logic/commands/quiz/QuizCreateManuallyCommand.java
@@ -84,4 +84,5 @@ public class QuizCreateManuallyCommand extends QuizCommand {
         return this.quizID.equals(e.quizID);
     }
 
+
 }
--- src/main/java/seedu/address/logic/commands/quiz/QuizGetQuestionsAndAnswersCommand.java
+++ src/main/java/seedu/address/logic/commands/quiz/QuizGetQuestionsAndAnswersCommand.java
@@ -46,4 +46,5 @@ public class QuizGetQuestionsAndAnswersCommand extends QuizCommand {
                     + message;
     }
 
+    
 }
