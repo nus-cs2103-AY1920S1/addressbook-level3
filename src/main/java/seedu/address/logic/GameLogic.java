a64918b35372986e2e517a31c2ecc4cd09a6faa4
sreesubbash <33561308+sreesubbash@users.noreply.github.com>
2019-10-11 10:40:27 +0800

    [FIX] imports with switchmode package

--- src/main/java/seedu/address/logic/GameLogic.java
+++ src/main/java/seedu/address/logic/GameLogic.java
@@ -3,7 +3,7 @@ package seedu.address.logic;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.game.GuessCommand;
-import seedu.address.logic.commands.StartCommand;
+import seedu.address.logic.commands.switchmode.StartCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.game.Game;
--- src/main/java/seedu/address/logic/parser/StartCommandParser.java
+++ src/main/java/seedu/address/logic/parser/StartCommandParser.java
@@ -2,8 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.logic.commands.GuessCommand;
-import seedu.address.logic.commands.StartCommand;
+import seedu.address.logic.commands.game.GuessCommand;
+import seedu.address.logic.commands.switchmode.StartCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
