e7107e6f36bfc64124524debafcac9f3723cce51
ryanty <tanyuryan@gmail.com>
2019-10-11 17:53:18 +0800

    add CourseNotFoundException class

---
+++ src/main/java/seedu/jarvis/commons/exceptions/CourseNotFoundException.java
@@ -0,0 +1,11 @@
+package seedu.jarvis.commons.exceptions;
+
+public class CourseNotFoundException extends Exception {
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+
+    public CourseNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
