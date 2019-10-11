6a20a220dff6bbc0b3f96d3118ca32a5c142a55f
kelvinnharris <kelvinnharris@gmail.com>
2019-10-12 00:21:40 +0800

    Add ExpiryDate class, ExpiryDateTest class and ExpiryDate prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,6 +7,7 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_EXPIRY_DATE = new Prefix("e/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_AMOUNT = new Prefix("a/");
 
