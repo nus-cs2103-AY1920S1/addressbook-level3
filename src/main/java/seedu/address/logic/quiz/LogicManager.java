856b5e9e054a1dec520481672458d070a13cdacb
Sacchirro Victor <sacchirro@yahoo.com>
2019-10-11 21:21:00 +0800

    First step of division of code

---
+++ src/main/java/seedu/address/logic/quiz/LogicManager.java
@@ -0,0 +1,82 @@
+package seedu.address.logic.quiz;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.quiz.commands.Command;
+import seedu.address.logic.quiz.commands.CommandResult;
+import seedu.address.logic.quiz.commands.exceptions.CommandException;
+import seedu.address.logic.quiz.parser.AddressBookParser;
+import seedu.address.logic.quiz.parser.exceptions.ParseException;
+import seedu.address.model.quiz.Model;
+import seedu.address.model.quiz.ReadOnlyAddressBook;
+import seedu.address.model.quiz.person.Person;
+import seedu.address.storage.quiz.Storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
+/**
+ * The main LogicManager of the app.
+ */
+public class LogicManager implements Logic {
+    public static final String FILE_OPS_ERROR_MESSAGE = "Could not save data to file: ";
+    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
+
+    private final Model model;
+    private final Storage storage;
+    private final AddressBookParser addressBookParser;
+
+    public LogicManager(Model model, Storage storage) {
+        this.model = model;
+        this.storage = storage;
+        addressBookParser = new AddressBookParser();
+    }
+
+    @Override
+    public CommandResult execute(String commandText) throws CommandException, ParseException {
+        //Logging, safe to ignore
+        logger.info("----------------[USER COMMAND][" + commandText + "]");
+
+        CommandResult commandResult;
+        //Parse user input from String to a Command
+        Command command = addressBookParser.parseCommand(commandText);
+        commandResult = command.execute(model);
+
+        try {
+            //We can deduce that the previous line of code modifies model in some way
+            // since it's being stored here.
+            storage.saveAddressBook(model.getAddressBook());
+        } catch (IOException ioe) {
+            throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
+        }
+
+        return commandResult;
+    }
+
+    @Override
+    public ReadOnlyAddressBook getAddressBook() {
+        return model.getAddressBook();
+    }
+
+    @Override
+    public ObservableList<Person> getFilteredPersonList() {
+        return model.getFilteredPersonList();
+    }
+
+    @Override
+    public Path getAddressBookFilePath() {
+        return model.getAddressBookFilePath();
+    }
+
+    @Override
+    public GuiSettings getGuiSettings() {
+        return model.getGuiSettings();
+    }
+
+    @Override
+    public void setGuiSettings(GuiSettings guiSettings) {
+        model.setGuiSettings(guiSettings);
+    }
+}
