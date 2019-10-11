fc9874deb23187cde4a623a9ba76bb93b2f0e09d
chanjunren <chanjunren@gmail.com>
2019-10-11 12:47:38 +0800

    Removed isImageFlashcard method

--- src/main/java/seedu/address/model/flashcard/Flashcard.java
+++ src/main/java/seedu/address/model/flashcard/Flashcard.java
@@ -38,13 +38,6 @@ public class Flashcard {
     public Answer getAnswer() {
         return answer;
     }
-    
-    /**
-     * Returns whether the flashcard contains a question in the form of a image file.
-     */
-    public boolean isImageFlashcard() {
-        return false;
-    }
 
     /**
      * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
