16d00dd370286bf7e60e5eb1fe805c50b004b122
Raikonen <choozeyuan97@gmail.com>
2019-10-11 22:54:46 +0800

    Undo wrong refactoring

--- src/main/java/seedu/savenus/logic/commands/AddCommand.java
+++ src/main/java/seedu/savenus/logic/commands/AddCommand.java
@@ -64,6 +64,7 @@ public class AddCommand extends Command {
             throw new CommandException(MESSAGE_DUPLICATE_FOOD);
         }
         model.addFood(toAdd);
+
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
