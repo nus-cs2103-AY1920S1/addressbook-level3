a30a35d6b5f504bd5e041db7362584c50563abc2
AHaliq <liqo12@hotmail.com>
2019-10-12 17:24:57 +0800

    made schema final

--- src/main/java/dream/fcard/logic/storage/Schema.java
+++ src/main/java/dream/fcard/logic/storage/Schema.java
@@ -1,13 +1,19 @@
 package dream.fcard.logic.storage;
 
+/**
+ * Collection of string constants used to define json files understood by the program
+ */
 public class Schema {
 
-    public static String TYPE_FIELD = "type";
-    public static String FRONT_FIELD = "front";
-    public static String BACK_FIELD = "back";
-    public static String CHOICES_FIELD = "choices";
+    public static final String DECK_NAME = "name";
+    public static final String DECK_CARDS = "cards";
 
-    public static String FRONT_BACK_TYPE = "front-back";
-    public static String JAVASCRIPT_TYPE = "javascript";
-    public static String MULTIPLE_CHOICE_TYPE = "multiple-choice";
+    public static final String TYPE_FIELD = "type";
+    public static final String FRONT_FIELD = "front";
+    public static final String BACK_FIELD = "back";
+    public static final String CHOICES_FIELD = "choices";
+
+    public static final String FRONT_BACK_TYPE = "front-back";
+    public static final String JAVASCRIPT_TYPE = "javascript";
+    public static final String MULTIPLE_CHOICE_TYPE = "multiple-choice";
 }
