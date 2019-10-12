c368fab1d2c5e719fd069ebdac828576a0beb22f
AHaliq <liqo12@hotmail.com>
2019-10-12 11:26:28 +0800

    implemented JsonWrongValueException

--- src/main/java/dream/fcard/util/json/exceptions/JsonWrongValueException.java
+++ src/main/java/dream/fcard/util/json/exceptions/JsonWrongValueException.java
@@ -1,7 +1,12 @@
 package dream.fcard.util.json.exceptions;
 
+import dream.fcard.util.json.jsontypes.JsonValueTypes;
+
 /**
- * ...
+ * Exception when access wrong value of a JsonValue object.
  */
-public class JsonWrongValueException {
+public class JsonWrongValueException extends Exception {
+    JsonWrongValueException(JsonValueTypes expected, JsonValueTypes got) {
+        super("Expecting " + expected + ", however got " + got);
+    }
 }
