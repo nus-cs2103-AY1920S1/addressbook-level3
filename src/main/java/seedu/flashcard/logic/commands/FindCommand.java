0d0b882bda232be44af8e92d1979ec75b7cc1597
Liang Boyuan <dedicus545@gmail.com>
2019-10-12 22:25:17 +0800

    ExitCommand, Find Command, HelpCommand Structure

--- src/main/java/seedu/flashcard/logic/commands/FindCommand.java
+++ src/main/java/seedu/flashcard/logic/commands/FindCommand.java
@@ -6,6 +6,18 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
+    // TODO: Specify the message of usage of add command.
+    //  This will be displayed to the user when the help command is called
+    public final String MESSAGE_USAGE = "";
+
+    private final String findMessage;
+
+    public FindCommand(String findMessage) {
+        this.findMessage = findMessage;
+    }
+
+    // TODO: implement the following execution method for add command, beware to generate a good command result.
+    // TODO: write corresponding tests to test out this execution methods.
     @Override
     public CommandResult execute(FlashcardList flashcardList) {
         return null;
--- src/main/java/seedu/flashcard/logic/commands/HelpCommand.java
+++ src/main/java/seedu/flashcard/logic/commands/HelpCommand.java
@@ -9,6 +9,8 @@ public class HelpCommand extends Command {
     // TODO: Specify the message of usage of help command.
     public static final String MESSAGE_USAGE = "";
 
+    // TODO: implement the following execution method for help command, beware to generate a good command result.
+    // TODO: write corresponding tests to test out this execution methods.
     @Override
     public CommandResult execute(FlashcardList flashcardList) {
         return null;
