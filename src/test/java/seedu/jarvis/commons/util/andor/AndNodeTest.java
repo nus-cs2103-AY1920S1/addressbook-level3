141124b2eda622049cb414421db72f6d028948ba
ryanty <tanyuryan@gmail.com>
2019-10-12 16:30:01 +0800

    move and rename AndOrStubsUtil to CourseTestUtil

--- src/test/java/seedu/jarvis/commons/util/andor/AndNodeTest.java
+++ src/test/java/seedu/jarvis/commons/util/andor/AndNodeTest.java
@@ -3,8 +3,8 @@ package seedu.jarvis.commons.util.andor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.jarvis.commons.util.andor.AndOrStubsUtil.CHILDREN;
-import static seedu.jarvis.commons.util.andor.AndOrStubsUtil.CourseStub;
+import static seedu.jarvis.testutil.course.CourseTestUtil.CHILDREN;
+import static seedu.jarvis.testutil.course.CourseTestUtil.CourseStub;
 
 import java.util.List;
 
--- src/test/java/seedu/jarvis/commons/util/andor/AndOrNodeTest.java
+++ src/test/java/seedu/jarvis/commons/util/andor/AndOrNodeTest.java
@@ -2,7 +2,7 @@ package seedu.jarvis.commons.util.andor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.jarvis.commons.util.andor.AndOrStubsUtil.CourseStub;
+import static seedu.jarvis.testutil.course.CourseTestUtil.CourseStub;
 
 import org.junit.jupiter.api.Test;
 
