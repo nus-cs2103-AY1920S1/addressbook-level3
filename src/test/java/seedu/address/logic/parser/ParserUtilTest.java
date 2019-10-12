64f550155b8be7f907aad7e085755f427dd75c30
chowyiyin <yiyin.chow@gmail.com>
2019-10-12 17:54:37 +0800

    Add tests to ParserUtil for new Person fields and Policy fields

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -15,24 +15,45 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.DateOfBirth;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Nric;
 import seedu.address.model.person.Phone;
+import seedu.address.model.policy.Coverage;
+import seedu.address.model.policy.Description;
+import seedu.address.model.policy.EndAge;
+import seedu.address.model.policy.Price;
+import seedu.address.model.policy.StartAge;
 import seedu.address.model.tag.Tag;
 
 public class ParserUtilTest {
     private static final String INVALID_NAME = "R@chel";
+    private static final String INVALID_NRIC = "J001";
     private static final String INVALID_PHONE = "+651234";
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
-    private static final String INVALID_TAG = "#friend";
+    private static final String INVALID_DATE_OF_BIRTH = "13/10/2019";
+    private static final String INVALID_DESCRIPTION = "";
+    private static final String INVALID_COVERAGE = "2 months";
+    private static final String INVALID_PRICE = "50SGD";
+    private static final String INVALID_START_AGE = "twenty";
+    private static final String INVALID_END_AGE = "twenty";
+    private static final String INVALID_TAG = "#smoker";
 
     private static final String VALID_NAME = "Rachel Walker";
+    private static final String VALID_NRIC = "S987654A";
     private static final String VALID_PHONE = "91234567";
     private static final String VALID_ADDRESS = "123 Main Street #0505";
     private static final String VALID_EMAIL = "rachel@example.com";
-    private static final String VALID_TAG_1 = "friend";
-    private static final String VALID_TAG_2 = "neighbour";
+    private static final String VALID_DATE_OF_BIRTH = "13.10.2019";
+    private static final String VALID_DESCRIPTION = "Insurance for health";
+    private static final String VALID_COVERAGE = "months/2";
+    private static final String VALID_PRICE = "$50";
+    private static final String VALID_START_AGE = "20";
+    private static final String VALID_END_AGE = "20";
+    private static final String VALID_TAG_1 = "smoker";
+    private static final String VALID_TAG_2 = "diabetic";
 
     private static final String WHITESPACE = " \t\r\n";
 
@@ -77,6 +98,29 @@ public class ParserUtilTest {
         String nameWithWhitespace = WHITESPACE + VALID_NAME + WHITESPACE;
         Name expectedName = new Name(VALID_NAME);
         assertEquals(expectedName, ParserUtil.parseName(nameWithWhitespace));
+    }
+
+    @Test
+    public void parseNric_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseNric((String) null));
+    }
+
+    @Test
+    public void parseNric_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseNric(INVALID_NRIC));
+    }
+
+    @Test
+    public void parseNric_validValueWithoutWhitespace_returnsNric() throws Exception {
+        Nric expectedNric = new Nric(VALID_NRIC);
+        assertEquals(expectedNric, ParserUtil.parseNric(VALID_NRIC));
+    }
+
+    @Test
+    public void parseNric_validValueWithWhitespace_returnsTrimmedNric() throws Exception {
+        String nricWithWhitespace = WHITESPACE + VALID_NRIC + WHITESPACE;
+        Nric expectedNric = new Nric(VALID_NRIC);
+        assertEquals(expectedNric, ParserUtil.parseNric(nricWithWhitespace));
     }
 
     @Test
@@ -146,6 +190,144 @@ public class ParserUtilTest {
         String emailWithWhitespace = WHITESPACE + VALID_EMAIL + WHITESPACE;
         Email expectedEmail = new Email(VALID_EMAIL);
         assertEquals(expectedEmail, ParserUtil.parseEmail(emailWithWhitespace));
+    }
+
+    @Test
+    public void parseDateOfBirth_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseDateOfBirth((String) null));
+    }
+
+    @Test
+    public void parseDateOfBirth_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseDateOfBirth(INVALID_DATE_OF_BIRTH));
+    }
+
+    @Test
+    public void parseDateOfBirth_validValueWithoutWhitespace_returnsDateOfBirth() throws Exception {
+        DateOfBirth expectedDateOfBirth = new DateOfBirth(VALID_DATE_OF_BIRTH);
+        assertEquals(expectedDateOfBirth, ParserUtil.parseDateOfBirth(VALID_DATE_OF_BIRTH));
+    }
+
+    @Test
+    public void parseDateOfBirth_validValueWithWhitespace_returnsTrimmedDateOfBirth() throws Exception {
+        String dateOfBirthWithWhitespace = WHITESPACE + VALID_DATE_OF_BIRTH + WHITESPACE;
+        DateOfBirth expectedDateOfBirth = new DateOfBirth(VALID_DATE_OF_BIRTH);
+        assertEquals(expectedDateOfBirth, ParserUtil.parseDateOfBirth(dateOfBirthWithWhitespace));
+    }
+
+    @Test
+    public void parseDescription_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseDescription((String) null));
+    }
+
+    @Test
+    public void parseDescription_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseDescription(INVALID_DESCRIPTION));
+    }
+
+    @Test
+    public void parseDescription_validValueWithoutWhitespace_returnsDescription() throws Exception {
+        Description expectedDescription = new Description(VALID_DESCRIPTION);
+        assertEquals(expectedDescription, ParserUtil.parseDescription(VALID_DESCRIPTION));
+    }
+
+    @Test
+    public void parseDescription_validValueWithWhitespace_returnsTrimmedDescription() throws Exception {
+        String descriptionWithWhitespace = WHITESPACE + VALID_DESCRIPTION + WHITESPACE;
+        Description expectedDescription = new Description(VALID_DESCRIPTION);
+        assertEquals(expectedDescription, ParserUtil.parseDescription(descriptionWithWhitespace));
+    }
+
+    @Test
+    public void parseCoverage_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseCoverage((String) null));
+    }
+
+    @Test
+    public void parseCoverage_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseCoverage(INVALID_COVERAGE));
+    }
+
+    @Test
+    public void parseCoverage_validValueWithoutWhitespace_returnsCoverage() throws Exception {
+        Coverage expectedCoverage = new Coverage(VALID_COVERAGE);
+        assertEquals(expectedCoverage, ParserUtil.parseCoverage(VALID_COVERAGE));
+    }
+
+    @Test
+    public void parseCoverage_validValueWithWhitespace_returnsTrimmedCoverage() throws Exception {
+        String coverageWithWhitespace = WHITESPACE + VALID_COVERAGE + WHITESPACE;
+        Coverage expectedCoverage = new Coverage(VALID_COVERAGE);
+        assertEquals(expectedCoverage, ParserUtil.parseCoverage(coverageWithWhitespace));
+    }
+
+    @Test
+    public void parsePrice_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parsePrice((String) null));
+    }
+
+    @Test
+    public void parsePrice_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parsePrice(INVALID_PRICE));
+    }
+
+    @Test
+    public void parsePrice_validValueWithoutWhitespace_returnsPrice() throws Exception {
+        Price expectedPrice = new Price(VALID_PRICE);
+        assertEquals(expectedPrice, ParserUtil.parsePrice(VALID_PRICE));
+    }
+
+    @Test
+    public void parsePrice_validValueWithWhitespace_returnsTrimmedPrice() throws Exception {
+        String priceWithWhitespace = WHITESPACE + VALID_PRICE + WHITESPACE;
+        Price expectedPrice = new Price(VALID_PRICE);
+        assertEquals(expectedPrice, ParserUtil.parsePrice(priceWithWhitespace));
+    }
+
+    @Test
+    public void parseStartAge_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseStartAge((String) null));
+    }
+
+    @Test
+    public void parseStartAge_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseStartAge(INVALID_START_AGE));
+    }
+
+    @Test
+    public void parseStartAge_validValueWithoutWhitespace_returnsStartAge() throws Exception {
+        StartAge expectedStartAge = new StartAge(VALID_START_AGE);
+        assertEquals(expectedStartAge, ParserUtil.parseStartAge(VALID_START_AGE));
+    }
+
+    @Test
+    public void parseStartAge_validValueWithWhitespace_returnsTrimmedStartAge() throws Exception {
+        String startAgeWithWhitespace = WHITESPACE + VALID_START_AGE + WHITESPACE;
+        StartAge expectedStartAge = new StartAge(VALID_START_AGE);
+        assertEquals(expectedStartAge, ParserUtil.parseStartAge(startAgeWithWhitespace));
+    }
+
+    @Test
+    public void parseEndAge_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseEndAge((String) null));
+    }
+
+    @Test
+    public void parseEndAge_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseEmail(INVALID_END_AGE));
+    }
+
+    @Test
+    public void parseEndAge_validValueWithoutWhitespace_returnsEndAge() throws Exception {
+        EndAge expectedEndAge = new EndAge(VALID_END_AGE);
+        assertEquals(expectedEndAge, ParserUtil.parseEndAge(VALID_END_AGE));
+    }
+
+    @Test
+    public void parseEndAge_validValueWithWhitespace_returnsTrimmedEndAge() throws Exception {
+        String endAgeWithWhitespace = WHITESPACE + VALID_END_AGE + WHITESPACE;
+        EndAge expectedEndAge = new EndAge(VALID_END_AGE);
+        assertEquals(expectedEndAge, ParserUtil.parseEndAge(VALID_END_AGE));
     }
 
     @Test
