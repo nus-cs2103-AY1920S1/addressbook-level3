f98f26bc1ec1e4729517d8f6e47ad13add60ca3b
ryanty <tanyuryan@gmail.com>
2019-10-12 12:42:16 +0800

    replace methods in CoursePlannerModel for v1.2 features

--- src/main/java/seedu/jarvis/model/course/CoursePlannerModel.java
+++ src/main/java/seedu/jarvis/model/course/CoursePlannerModel.java
@@ -1,23 +1,11 @@
 package seedu.jarvis.model.course;
 
-
 /**
  * The API of Course Planner component.
  */
 public interface CoursePlannerModel {
     /**
-     * Adds a module.
+     * Looks up a course.
      */
-    void addModule(Course course);
-
-    /**
-     * Deletes the given course.
-     * The course must exist within the user's list.
-     */
-    void deleteModule(Course course);
-
-    /**
-     * Checks if the user can take this course.
-     */
-    boolean canTakeModule(Course course);
+    void lookUpCourse(Course course);
 }
