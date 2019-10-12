5c482338c263051b07e413a11f050151294183e8
Siang Hwee <siang_hwee@hotmail.com>
2019-10-13 03:45:24 +0800

    Remove unused dependencies

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -7,7 +7,6 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ItemStorage;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.item.ItemList;
 
 /**
