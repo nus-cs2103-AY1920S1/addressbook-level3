9ef08c40898b1c2990f120f7993236e12654a3ad
Kalsyc <klassiclegend@gmail.com>
2019-10-13 02:17:18 +0800

    Edit UserGuide

--- src/main/java/io/xpire/model/tag/Tag.java
+++ src/main/java/io/xpire/model/tag/Tag.java
@@ -5,7 +5,7 @@ import static java.util.Objects.requireNonNull;
 import io.xpire.commons.util.AppUtil;
 
 /**
- * Represents a Tag in the expiry date tracker.
+ * Represents a Tag in the expiry date tracker. Tags are in Sentence-Case when parsed in.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
  */
 public class Tag {
