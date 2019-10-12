50e51039a73a6ecceab5d7b51667aefee83a5254
seris370 <1812563232@qq.com>
2019-10-13 04:54:57 +0800

    add ListPlanCommand

---
+++ src/main/java/seedu/algobase/logic/commands/ListPlanCommand.java
@@ -0,0 +1,25 @@
+package seedu.algobase.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.algobase.model.Model.PREDICATE_SHOW_ALL_PLANS;
+
+import seedu.algobase.model.Model;
+
+
+/**
+ * Lists all plans in the algobase to the user.
+ */
+public class ListPlanCommand extends Command {
+
+    public static final String COMMAND_WORD = "lsplan";
+
+    public static final String MESSAGE_SUCCESS = "Listed all plans";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPlanList(PREDICATE_SHOW_ALL_PLANS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
