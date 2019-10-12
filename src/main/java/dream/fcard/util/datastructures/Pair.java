0c04d85bbe84843776543d964a9a62a92013d792
AHaliq <liqo12@hotmail.com>
2019-10-12 12:40:08 +0800

    implemented pair type

---
+++ src/main/java/dream/fcard/util/datastructures/Pair.java
@@ -0,0 +1,12 @@
+package dream.fcard.util.datastructures;
+
+public class Pair<U, T> {
+
+    public U fst;
+    public T snd;
+
+    public Pair(U a, T b) {
+        fst = a;
+        snd = b;
+    }
+}
