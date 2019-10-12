54b2e59b2534d88429f0500ad6e78ad0d246b726
Liu Zechu <liuzechu2013@gmail.com>
2019-10-12 17:59:44 +0800

    minor changes

--- src/test/java/seedu/address/storage/JsonAdaptedStudyPlanTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedStudyPlanTest.java
@@ -3,6 +3,7 @@ package seedu.address.storage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.storage.JsonAdaptedStudyPlan.MISSING_FIELD_MESSAGE_FORMAT;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalStudyPlans.SP_1;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,24 +14,19 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 
 public class JsonAdaptedStudyPlanTest {
-    private static final String INVALID_NAME = "R@chel";
-    private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_ADDRESS = " ";
-    private static final String INVALID_EMAIL = "example.com";
-    private static final String INVALID_TAG = "#friend";
+    private static final String INVALID_SEMESTER_NAME = "HELLO";
+    private static final String INVALID_TAG = "计算机";
 
-    private static final String VALID_NAME = BENSON.getName().toString();
-    private static final String VALID_PHONE = BENSON.getPhone().toString();
-    private static final String VALID_EMAIL = BENSON.getEmail().toString();
-    private static final String VALID_ADDRESS = BENSON.getAddress().toString();
-    private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
+    private static final String VALID_NAME = SP_1.getName().toString();
+
+    private static final List<JsonAdaptedTag> VALID_TAGS = SP_1.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
 
     @Test
     public void toModelType_validStudyPlanDetails_returnsStudyPlan() throws Exception {
-        JsonAdaptedStudyPlan studyPlan = new JsonAdaptedStudyPlan(BENSON);
-        assertEquals(BENSON, studyPlan.toModelType());
+        JsonAdaptedStudyPlan studyPlan = new JsonAdaptedStudyPlan(SP_1);
+        assertEquals(SP_1, studyPlan.toModelType());
     }
 
     @Test
