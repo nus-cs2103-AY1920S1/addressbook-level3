1e5be08fff0bf9284827fa72e989970cf64c9913
AHaliq <liqo12@hotmail.com>
2019-10-12 12:29:33 +0800

    implemented json values, exception and string parse

--- src/main/java/dream/fcard/util/json/JsonParser.java
+++ src/main/java/dream/fcard/util/json/JsonParser.java
@@ -4,4 +4,13 @@ package dream.fcard.util.json;
  * ...
  */
 public class JsonParser {
+
+    /**
+     * Replace instances of " and n as escape characters for formatting as json string.
+     * @param str   string to format
+     * @return      formatted string
+     */
+    public static String formatStringForJson(String str) {
+        return "\"" + str.replaceAll("\"", "\\\\\"").replaceAll("\n", "\\\\\n") + "\"";
+    }
 }
--- src/main/java/dream/fcard/util/json/exceptions/JsonWrongValueException.java
+++ src/main/java/dream/fcard/util/json/exceptions/JsonWrongValueException.java
@@ -6,7 +6,7 @@ import dream.fcard.util.json.jsontypes.JsonValueTypes;
  * Exception when access wrong value of a JsonValue object.
  */
 public class JsonWrongValueException extends Exception {
-    JsonWrongValueException(JsonValueTypes expected, JsonValueTypes got) {
+    public JsonWrongValueException(JsonValueTypes expected, JsonValueTypes got) {
         super("Expecting " + expected + ", however got " + got);
     }
 }
