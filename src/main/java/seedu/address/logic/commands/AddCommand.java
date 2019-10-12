a10f6a4a97db2b7852f5bc5cc11f8f5ecd04ebd9
chowyiyin <yiyin.chow@gmail.com>
2019-10-12 14:18:46 +0800

    Remove pol and tag prefixes from user guide and AddCommand. execute() now throws DuplicatePersonWithoutMergePromptException if compulsory fields are equal and only throws DuplicatePersonWithMergePromptException if any compulsory field is different.

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -7,10 +7,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import seedu.address.logic.commands.exceptions.DuplicatePersonException;
+import seedu.address.logic.commands.exceptions.DuplicatePersonWithMergeException;
+import seedu.address.logic.commands.exceptions.DuplicatePersonWithoutMergeException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
@@ -29,22 +28,19 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
             + PREFIX_DATE_OF_BIRTH + "DATE OF BIRTH "
-            + "[" + PREFIX_POLICY + "POLICY]...\n"
-            + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_NRIC + "S000001J "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_DATE_OF_BIRTH + "12.12.1912 "
-            + PREFIX_POLICY + "Senior Care "
-            + PREFIX_TAG + "diabetic "
-            + PREFIX_TAG + "elderly ";
+            + PREFIX_DATE_OF_BIRTH + "12.12.1912 ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book\n";
+    public static final String MESSAGE_INPUT_INFORMATION_HEADER = "Your input:\n%1$s";
     public static final String DUPLICATE_PERSON_MERGE_PROMPT = "Do you wish to edit this person's profile?";
+    public static final String NEW_LINE = "\n";
 
     private final Person toAdd;
 
@@ -57,19 +53,50 @@ public class AddCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) throws DuplicatePersonException {
+    public CommandResult execute(Model model) throws DuplicatePersonWithoutMergeException,
+            DuplicatePersonWithMergeException {
         requireNonNull(model);
 
         if (model.hasPerson(toAdd)) {
-            StringBuilder exceptionMessage = new StringBuilder();
-            exceptionMessage.append(MESSAGE_DUPLICATE_PERSON);
-            exceptionMessage.append(model.getPerson(toAdd).toString() + "\n");
-            exceptionMessage.append(DUPLICATE_PERSON_MERGE_PROMPT);
-            throw new DuplicatePersonException(exceptionMessage.toString());
+            String exceptionMessage = "";
+            if (toAdd.hasEqualCompulsoryFields(model.getPerson(toAdd))) {
+                exceptionMessage = generateExceptionMessageWithoutMergePrompt(model.getPerson(toAdd));
+                throw new DuplicatePersonWithoutMergeException(exceptionMessage);
+            } else {
+                exceptionMessage = generateExceptionMessageWithMergePrompt(model.getPerson(toAdd));
+                throw new DuplicatePersonWithMergeException(exceptionMessage);
+            }
         }
 
         model.addPerson(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    /**
+     * Generates an exception message with a merge prompt.
+     * @param original Original person in the addressbook.
+     * @return The exception message to be thrown.
+     */
+    public String generateExceptionMessageWithMergePrompt(Person original) {
+        StringBuilder exceptionMessage = new StringBuilder();
+        exceptionMessage.append(MESSAGE_DUPLICATE_PERSON);
+        exceptionMessage.append(original.toString() + NEW_LINE);
+        exceptionMessage.append(String.format(MESSAGE_INPUT_INFORMATION_HEADER, toAdd.toString()) + NEW_LINE);
+        exceptionMessage.append(DUPLICATE_PERSON_MERGE_PROMPT);
+        return exceptionMessage.toString();
+    }
+
+    /**
+     * Generates an exception message without a merge prompt. This method is used if the input person has all the same
+     * compulsory data fields as the original person.
+     * @param original Original person in the addressbook.
+     * @return The exception message to be thrown.
+     */
+    public String generateExceptionMessageWithoutMergePrompt(Person original) {
+        StringBuilder exceptionMessage = new StringBuilder();
+        exceptionMessage.append(MESSAGE_DUPLICATE_PERSON);
+        exceptionMessage.append(original.toString());
+        return exceptionMessage.toString();
     }
 
     @Override
