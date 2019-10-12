4fad2eef9e3ad2f1f1b3eab6cf4120890efe8b27
ryanty <tanyuryan@gmail.com>
2019-10-12 13:49:36 +0800

    implement LookUpCommandParser

---
+++ src/main/java/seedu/jarvis/logic/parser/course/LookUpCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.jarvis.logic.parser.course;
+
+import static seedu.jarvis.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.jarvis.logic.parser.CliSyntax.PREFIX_COURSE;
+
+import seedu.jarvis.commons.exceptions.CourseNotFoundException;
+import seedu.jarvis.commons.util.CourseUtil;
+import seedu.jarvis.logic.commands.course.LookUpCommand;
+import seedu.jarvis.logic.parser.ArgumentMultimap;
+import seedu.jarvis.logic.parser.ArgumentTokenizer;
+import seedu.jarvis.logic.parser.Parser;
+import seedu.jarvis.logic.parser.exceptions.ParseException;
+import seedu.jarvis.model.course.Course;
+
+import java.util.Optional;
+
+public class LookUpCommandParser implements Parser<LookUpCommand> {
+    @Override
+    public LookUpCommand parse(String args) throws ParseException {
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args, PREFIX_COURSE);
+        Optional<String> arg = argumentMultimap.getValue(PREFIX_COURSE);
+
+        if (arg.isEmpty()) {
+            throw new ParseException(String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, LookUpCommand.MESSAGE_USAGE));
+        }
+
+        Course course;
+        try {
+            course = CourseUtil.getCourse(arg.get());
+        } catch (CourseNotFoundException e) {
+            throw new ParseException(arg.get() + " could not be found!");
+        }
+        return new LookUpCommand(course);
+    }
+}
