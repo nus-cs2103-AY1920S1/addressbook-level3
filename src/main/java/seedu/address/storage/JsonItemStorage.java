bb674af3d34fc59b6c1a60b01337032d094d187e
Siang Hwee <siang_hwee@hotmail.com>
2019-10-13 03:48:51 +0800

    Edit code to comply with checkstyle

--- src/main/java/seedu/address/storage/JsonItemStorage.java
+++ src/main/java/seedu/address/storage/JsonItemStorage.java
@@ -5,15 +5,15 @@ import static java.util.Objects.requireNonNull;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.model.ItemStorage;
-import seedu.address.model.ReadOnlyAddressBook;
 
 /**
  * A class that contains all the JSON representation of the string
  */
-public class JsonItemStorage implements ItemListStorage{
+public class JsonItemStorage implements ItemListStorage {
     public static final String MESSAGE_DUPLICATE_ITEM = "Items list contains duplicate items";
 
     private Path itemListFilePath;
@@ -31,15 +31,21 @@ public class JsonItemStorage implements ItemListStorage{
 
 
     /**
-     * Saves the given {@link ReadOnlyAddressBook} to the storage.
+     * Saves the given {@link ItemStorage} to the storage.
      * @param itemStorage cannot be null.
      * @throws IOException if there was any problem writing to the file.
      */
-    public void saveItemStorage(ItemStorage itemStorage) throws IOException{
+    public void saveItemStorage(ItemStorage itemStorage) throws IOException {
         saveItemStorage(itemStorage, itemListFilePath);
     }
 
-    public void saveItemStorage(ItemStorage itemStorage, Path filePath) throws IOException{
+    /**
+     * Saves the given {@link ItemStorage} to the storage.
+     * @param itemStorage cannot be null.
+     * @param filePath the path of the save file. Cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    public void saveItemStorage(ItemStorage itemStorage, Path filePath) throws IOException {
         requireNonNull(itemStorage);
         requireNonNull(filePath);
 
@@ -53,7 +59,7 @@ public class JsonItemStorage implements ItemListStorage{
      * @throws IllegalValueException if there were any data constraints violated.
      * @throws IOException if there are any problem with reading from the string.
      */
-    public ItemStorage toModelType() throws IllegalValueException, IOException {
+    public ItemStorage toModelType() throws IOException, DataConversionException {
         String jsonString = FileUtil.readFromFile(itemListFilePath);
         return ItemStorage.fromJson(jsonString);
     }
