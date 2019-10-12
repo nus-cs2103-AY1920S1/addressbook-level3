87b31484f628fb449f0da7afd1d6ed8c8e74fb83
marcfyk <marcjohnfong@gmail.com>
2019-10-12 21:19:55 +0800

    Add TypicalCommands

---
+++ src/test/java/seedu/jarvis/testutil/history/TypicalCommands.java
@@ -0,0 +1,42 @@
+package seedu.jarvis.testutil.history;
+
+import static seedu.jarvis.testutil.address.TypicalPersons.*;
+
+import seedu.jarvis.logic.commands.Command;
+import seedu.jarvis.logic.commands.address.AddAddressCommand;
+import seedu.jarvis.model.history.HistoryManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A utility class containing {@code Command} objects to be used in tests.
+ */
+public class TypicalCommands {
+    public static final Command ADD_AMY = new AddAddressCommand(AMY);
+    public static final Command ADD_BENSON = new AddAddressCommand(BENSON);
+    public static final Command ADD_CARL = new AddAddressCommand(CARL);
+    public static final Command ADD_DANIEL = new AddAddressCommand(DANIEL);
+    public static final Command ADD_ELLE = new AddAddressCommand(ELLE);
+    public static final Command ADD_FIONA = new AddAddressCommand(FIONA);
+    public static final Command ADD_GEORGE = new AddAddressCommand(GEORGE);
+
+    private TypicalCommands() {} // prevents instantiation
+
+
+    /**
+     * Returns a {@code HistoryManager} with the history of commands to add persons to the typical AddressBook.
+     */
+    public static HistoryManager getTypicalHistoryManager() {
+        HistoryManager historyManager = new HistoryManager();
+        getTypicalCommands().forEach(historyManager::rememberExecutedCommand);
+        return historyManager;
+    }
+
+    public static List<Command> getTypicalCommands() {
+        return new ArrayList<>(Arrays.asList(ADD_AMY, ADD_BENSON, ADD_CARL, ADD_DANIEL, ADD_ELLE, ADD_FIONA,
+                ADD_GEORGE));
+    }
+
+}
