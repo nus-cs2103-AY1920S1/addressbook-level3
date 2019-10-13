15f8c41325847f369677661e31d1eec88e0dfc54
TSAI, HSIAO-HAN <sunnyhome3224@gmail.com>
2019-10-13 10:17:23 +0800

    add set method

--- src/main/java/seedu/mark/model/reminder/ReminderAssociation.java
+++ src/main/java/seedu/mark/model/reminder/ReminderAssociation.java
@@ -16,20 +16,49 @@ public class ReminderAssociation {
 
     public void addReminder(Bookmark bookmark, Reminder reminder) {
         requireAllNonNull(bookmark, reminder);
+
         if (association.containsKey(bookmark)) {
             throw new ExistReminderException();
         }
+
         association.put(bookmark, reminder);
     }
 
     public void deleteReminder(Reminder reminder) {
         requireAllNonNull(reminder);
         Bookmark bookmark = reminder.getBookmark();
+
         if (!association.containsKey(bookmark)) {
             throw new BookmarkContainNoReminderException();
         }
+
         if(!association.remove(bookmark, reminder)) {
             throw new ReminderNotFoundException();
         }
+    }
+
+    public void setReminder(Reminder targetReminder, Reminder edittedReminder) {
+        requireAllNonNull(targetReminder, edittedReminder);
+        Bookmark bookmark = targetReminder.getBookmark();
+
+        if (!association.containsKey(bookmark)) {
+            throw new BookmarkContainNoReminderException();
+        }
+
+        if (!association.replace(bookmark, targetReminder, edittedReminder)) {
+            throw new ReminderNotFoundException();
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return association.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReminderAssociation // instanceof handles nulls
+                && association.equals(((ReminderAssociation) other).association));
     }
 }
