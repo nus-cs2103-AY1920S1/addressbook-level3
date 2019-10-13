3c654a8331bb45f400faef77ea074dd6d6565604
Sam-limyr <nacuniel@gmail.com>
2019-10-13 17:14:47 +0800

    Adapted test utilties to allow testing of Note indexes.

---
+++ src/test/java/seedu/address/testutil/TypicalNotes.java
@@ -0,0 +1,64 @@
+package seedu.address.testutil;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.note.Note;
+import seedu.address.model.person.Person;
+
+/**
+ * A utility class containing a list of {@code Person} objects to be used in tests.
+ */
+public class TypicalNotes {
+
+    public static final Note SAMPLE = new NoteBuilder().withTitle("Sample Title").withContent("Sample Content").build();
+    public static final Note PIPELINE = new NoteBuilder().withTitle("Pipelining Definition")
+            .withContent("Pipelining is the process of making a single processor run multiple instructions " 
+                    + "simultaneously.").build();
+    
+/*
+    // Manually added
+    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
+            .withEmail("stefan@example.com").withAddress("little india").build();
+    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
+            .withEmail("hans@example.com").withAddress("chicago ave").build();
+
+    // Manually added - Person's details found in {@code CommandTestUtil}
+    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
+            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
+    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
+            .build();
+*/
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalNotes() {} // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical notes.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Note note : getTypicalNotes()) {
+            ab.addNote(note);
+        }
+        return ab;
+    }
+
+    public static List<Note> getTypicalNotes() {
+        return new ArrayList<>(Arrays.asList(SAMPLE, PIPELINE));
+    }
+}
