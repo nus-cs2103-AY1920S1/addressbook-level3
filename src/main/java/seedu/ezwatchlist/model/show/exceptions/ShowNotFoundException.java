30b0e6d9d1baa5c21ae5814576a2dd754e2efa6d
Chiang Jiajun Jared <50407763+jcjjjared@users.noreply.github.com>
2019-10-13 19:15:38 +0800

    Revert "Branch caleb v1.2"

--- src/main/java/seedu/ezwatchlist/model/show/exceptions/ShowNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package seedu.ezwatchlist.model.show.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified show.
- */
-public class ShowNotFoundException extends RuntimeException {
-}
