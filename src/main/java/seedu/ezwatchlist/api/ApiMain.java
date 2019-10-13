6e56e5f1addc6ecf8a662b23f2578d2c9b46c5f7
heze8 <46834785+heze8@users.noreply.github.com>
2019-10-13 17:58:41 +0800

    Added rudimentary spellcheck classes for Michelle to take a look Added one MovieCard that has the same format as PersonCard so that we can start working on the interface changes Tweaked some of the API methods Refactored the name of the package from adress to ezwatchlist Added a movieInfromation class in the api package but might be unnecesarry. Could perhaps change the in-house movie class to be similar to that

---
+++ src/main/java/seedu/ezwatchlist/api/ApiMain.java
@@ -0,0 +1,132 @@
+package seedu.ezwatchlist.api;
+
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.TvResultsPage;
+import info.movito.themoviedbapi.model.Artwork;
+import info.movito.themoviedbapi.model.Credits;
+import info.movito.themoviedbapi.model.Genre;
+import info.movito.themoviedbapi.model.MovieDb;
+import info.movito.themoviedbapi.model.config.TmdbConfiguration;
+import info.movito.themoviedbapi.model.core.MovieResultsPage;
+import info.movito.themoviedbapi.model.keywords.Keyword;
+import info.movito.themoviedbapi.model.people.PersonCast;
+
+import javax.imageio.ImageIO;
+
+import info.movito.themoviedbapi.model.tv.TvEpisode;
+import info.movito.themoviedbapi.model.tv.TvSeason;
+import info.movito.themoviedbapi.model.tv.TvSeries;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import seedu.ezwatchlist.model.show.Date;
+import seedu.ezwatchlist.model.show.Description;
+import seedu.ezwatchlist.model.show.Episode;
+import seedu.ezwatchlist.model.show.Movie;
+import seedu.ezwatchlist.model.show.Name;
+import seedu.ezwatchlist.model.show.RunningTime;
+import seedu.ezwatchlist.model.show.TvShow;
+
+/**
+ * Main class for the API to connect to the server
+ */
+public class ApiMain {
+    //API key is to connect with the tmdb server.
+    private final static String API_KEY = "44ed1d7975d7c699743229199b1fc26e";
+    private final TmdbApi ApiCall;
+
+    public ApiMain() {
+        ApiCall = new TmdbApi(API_KEY);
+    }
+
+    public List<Movie> getMovieByName(String name) {
+        MovieResultsPage page = ApiCall.getSearch().searchMovie(name, null, null, true, null);
+        ArrayList<Movie> movies = new ArrayList<>();
+        for (MovieDb m : page.getResults()) {
+            movies.add(new Movie(new Name(m.getTitle()), new Description(m.getTagline()), false, new Date(m.getReleaseDate()),
+                    new RunningTime(m.getRuntime()), null));
+        }
+        return movies;
+    }
+
+    public List<TvShow> getTvShowByName(String name) {
+        TvResultsPage page = ApiCall.getSearch().searchTv(name, null, null);
+        ArrayList<TvShow> tvShows = new ArrayList<>();
+
+        for (TvSeries tv : page.getResults()) {
+            List<TvSeason> seasons = tv.getSeasons();
+            ArrayList<seedu.ezwatchlist.model.show.TvSeason> seasonsList = new ArrayList<>();
+            Credits credits = tv.getCredits();
+            List<PersonCast> cast = credits.getCast();
+            PersonCast personCast = cast.get(0);
+            personCast.getName();
+            for (TvSeason tvSeason: seasons) {
+                List<TvEpisode> episodes = tvSeason.getEpisodes();
+                ArrayList<Episode> episodeList = new ArrayList<>();
+                for (TvEpisode episode: episodes) {
+                    episodeList.add(new seedu.ezwatchlist.model.show.Episode(new Name(episode.getName()), episode.getEpisodeNumber()));
+                }
+                seedu.ezwatchlist.model.show.TvSeason tvS =
+                        new seedu.ezwatchlist.model.show.TvSeason(tvSeason.getSeasonNumber(), episodes.size(),
+                                episodeList);
+                seasonsList.add(tvS);
+            }
+
+            tvShows.add(new TvShow(new Name(tv.getName()), new Description(tv.getOverview()), false,
+                    new Date(tv.getFirstAirDate()),
+                    new RunningTime(tv.getEpisodeRuntime().get(0)), null, 0,
+                    tv.getNumberOfEpisodes(), seasonsList));
+        }
+        return tvShows;
+    }
+
+
+
+    /**
+     * test function
+     * @param args
+     * @throws IOException
+
+    public static void main(String[] args) throws IOException {
+        TmdbApi tmdbApi = new TmdbApi(API_KEY);
+        TmdbMovies movies = tmdbApi.getMovies();
+        MovieDb movie = movies.getMovie(5353, null, TmdbMovies.MovieMethod.similar, TmdbMovies.MovieMethod.keywords, TmdbMovies.MovieMethod.credits, TmdbMovies.MovieMethod.images);
+        System.out.println(movie.getOriginalTitle());
+        p2( movie.getSimilarMovies());
+        List<Artwork> artworks = movie.getImages();
+        String filePath = artworks.get(0).getFilePath();
+        TmdbConfiguration configuration = tmdbApi.getConfiguration();
+        final String baseUrl = configuration.getBaseUrl() + "w500";
+        BufferedImage img = ImageIO.read(new URL(baseUrl + filePath));
+        Graphics g = img.getGraphics();
+        g.drawImage(img, 0, 0, null);
+
+        //keywords
+        List<Keyword> keywordList = movie.getKeywords();
+        p(keywordList);
+        List<Genre> genres = movie.getGenres();
+        System.out.println("genres");
+        p(genres);
+        System.out.println("casts");
+        List<PersonCast> cast = movie.getCast();
+        p(cast);
+        System.out.println("crew");
+        p(movie.getCrew());
+    }
+
+    public static <T> void p (List<T> s) {
+        for (T t : s)
+            System.out.println(t);
+    }
+
+    public static void p2 (List<MovieDb> l) {
+        for (MovieDb m : l) {
+            System.out.println(m.getOriginalTitle());
+        }
+    }
+    */
+}
---
+++ src/main/java/seedu/ezwatchlist/model/WatchList.java
@@ -0,0 +1,120 @@
+package seedu.ezwatchlist.model;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import javafx.collections.ObservableList;
+import seedu.ezwatchlist.model.show.Show;
+import seedu.ezwatchlist.model.show.UniqueShowList;
+
+/**
+ * Wraps all data at the address-book level
+ * Duplicates are not allowed (by .isSamePerson comparison)
+ */
+public class WatchList implements ReadOnlyWatchList {
+
+    private final UniqueShowList shows;
+
+    /*
+     * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
+     * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
+     *
+     * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
+     *   among constructors.
+     */
+    {
+        shows = new UniqueShowList();
+    }
+
+    public WatchList() {}
+
+    /**
+     * Creates an WatchList using the Shows in the {@code toBeCopied}
+     */
+    public WatchList(ReadOnlyWatchList toBeCopied) {
+        this();
+        resetData(toBeCopied);
+    }
+
+    //// list overwrite operations
+
+    /**
+     * Replaces the contents of the watchlist with {@code shows}.
+     * {@code shows} must not contain duplicate shows.
+     */
+    public void setShows(List<Show> shows) {
+        this.shows.setShows(shows);
+    }
+
+    /**
+     * Resets the existing data of this {@code WatchList} with {@code newData}.
+     */
+    public void resetData(ReadOnlyWatchList newData) {
+        requireNonNull(newData);
+
+        setShows(newData.getShowList());
+    }
+
+    //// show-level operations
+
+    /**
+     * Returns true if a show with the same identity as {@code show} exists in the WatchList.
+     */
+    public boolean hasShow(Show show) {
+        requireNonNull(show);
+        return shows.contains(show);
+    }
+
+    /**
+     * Adds a show to the watchlist.
+     * The show must not already exist in the watchlist.
+     */
+    public void addShow(Show s) {
+        shows.add(s);
+    }
+
+    /**
+     * Replaces the given show {@code target} in the list with {@code editedShow}.
+     * {@code target} must exist in the watchlist.
+     * The show identity of {@code editedShow} must not be the same as another existing show in the watchlist.
+     */
+    public void setShow(Show target, Show editedShow) {
+        requireNonNull(editedShow);
+
+        shows.setShow(target, editedShow);
+    }
+
+    /**
+     * Removes {@code key} from this {@code WatchList}.
+     * {@code key} must exist in the watchlist.
+     */
+    public void removeShow(Show key) {
+        shows.remove(key);
+    }
+
+    //// util methods
+
+    @Override
+    public String toString() {
+        return shows.asUnmodifiableObservableList().size() + " shows";
+        // TODO: refine later
+    }
+
+    @Override
+    public ObservableList<Show> getShowList() {
+        return shows.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof WatchList // instanceof handles nulls
+                && shows.equals(((WatchList) other).shows));
+    }
+
+    @Override
+    public int hashCode() {
+        return shows.hashCode();
+    }
+}
