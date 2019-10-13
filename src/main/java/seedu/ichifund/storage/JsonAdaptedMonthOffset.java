cc204b55df3e496d37e1a06b90186b11b107b041
leeyiyuan <leeyiyuan@hotmail.com>
2019-10-13 16:44:04 +0800

    Add JsonAdaptedCategory

--- src/main/java/seedu/ichifund/storage/JsonAdaptedMonthOffset.java
+++ src/main/java/seedu/ichifund/storage/JsonAdaptedMonthOffset.java
@@ -34,7 +34,7 @@ class JsonAdaptedMonthOffset {
     }
 
     /**
-     * Converts this Jackson-friendly adapted tag object into the model's {@code MonthOffset} object.
+     * Converts this Jackson-friendly adapted month offset object into the model's {@code MonthOffset} object.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted month offset.
      */
