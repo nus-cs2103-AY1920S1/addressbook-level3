190ecd67af8100808ba28854cd0ff904ee919e5b
huiminlim <e0036379@u.nus.edu>
2019-10-13 23:42:06 +0800

    Added: State test

---
+++ src/test/java/dream/fcard/model/StateTest.java
@@ -0,0 +1,21 @@
+package dream.fcard.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import dream.fcard.model.exceptions.IndexNotFoundException;
+
+class StateTest {
+    State s = new State();
+
+    @Test
+    void getDeckIndex_validIndex_returnValidIndex() {
+
+        //s.addDeck(new Deck());
+    }
+
+}
