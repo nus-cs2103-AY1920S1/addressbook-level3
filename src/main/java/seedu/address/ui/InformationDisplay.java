ca1950b6bca5b77440c5113000c6134d322a2487
ErwinChan <erwinchan1997@gmail.com>
2019-10-13 17:39:59 +0800

    Updated with changes to font

--- src/main/java/seedu/address/ui/InformationDisplay.java
+++ src/main/java/seedu/address/ui/InformationDisplay.java
@@ -4,12 +4,12 @@ import static seedu.address.model.util.SampleDataUtil.getTagSet;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.*;
+// to be removed
 
 
 public class InformationDisplay extends UiPart<Region> {
