46cff10fd6fefc478f6db42fa51079d9931a6cfb
Sam-limyr <nacuniel@gmail.com>
2019-10-13 20:41:23 +0800

    Updated AddressBookParser to include ListNoteCommand.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,6 +17,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListNoteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -69,6 +70,9 @@ public class AddressBookParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListNoteCommand.COMMAND_WORD:
+            return new ListNoteCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
