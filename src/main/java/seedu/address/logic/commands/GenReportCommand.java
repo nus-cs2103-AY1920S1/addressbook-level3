0cee6bd68da3cffd802a1f89c35e69195eb7b079
Bernice Chio <bernicechiohuiyin@gmail.com>
2019-10-13 22:35:26 +0800

    Fix checkstyle violation

--- src/main/java/seedu/address/logic/commands/GenReportCommand.java
+++ src/main/java/seedu/address/logic/commands/GenReportCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.entity.IdentificationNumber;
