84fee44fcbc1d0b6677f59d794bf27a9c0329497
Siang Hwee <siang_hwee@hotmail.com>
2019-10-13 16:51:10 +0800

    Enable find command to be recognized by the parser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,10 +1,9 @@
 package seedu.address.logic.parser;
 
-import java.util.Arrays;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -17,20 +16,16 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String keywords, String empty) throws ParseException {
-        /*
-        String trimmedArgs = args.trim();
+
+        String trimmedArgs = keywords.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String[] keywordArray = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
-
-         */
-        String[] dummyList = {"dummy"};
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(dummyList)));
+        return new FindCommand(keywordArray);
     }
 
 }
