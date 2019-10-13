0c5d5cdd6cbda385b436e93db96a33fab4ac58db
chowyiwai <yiwai.chow99@gmail.com>
2019-10-13 12:18:04 +0800

    Modify Duplicate Tag Exception

--- src/main/java/seedu/address/model/tag/exceptions/DuplicateTagException.java
+++ src/main/java/seedu/address/model/tag/exceptions/DuplicateTagException.java
@@ -4,7 +4,7 @@ package seedu.address.model.tag.exceptions;
  * Signals that the operation would result in duplicate tags (Tags are considered duplicates if they have the same
  * name).
  */
-public class DuplicateTagException extends RuntimeException {
+public class DuplicateTagException extends InvalidTagException {
 
     public DuplicateTagException() {
         super("Operation would result in duplicate tags");
