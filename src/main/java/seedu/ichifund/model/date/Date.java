a852a8c5abcd08b69ca5522109d57eaf33cc3be8
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-13 16:40:54 +0800

    Add Date

---
+++ src/main/java/seedu/ichifund/model/date/Date.java
@@ -0,0 +1,9 @@
+package seedu.ichifund.model.date;
+
+
+/**
+ * Represents a Transaction in IchiFund.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Date {
+}
