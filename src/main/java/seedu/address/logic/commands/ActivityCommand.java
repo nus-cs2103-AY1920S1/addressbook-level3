fd68c2e08181e35b11c781d20f7e5810615644fd
Lim Daekoon <daekoon_lim@hotmail.com>
2019-10-14 02:20:31 +0800

    Improve ActivityCommand#execute to be more clear and readable

--- src/main/java/seedu/address/logic/commands/ActivityCommand.java
+++ src/main/java/seedu/address/logic/commands/ActivityCommand.java
@@ -51,37 +51,46 @@ public class ActivityCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        String warning = "";
-        String success = "";
+        StringBuilder warningMessage = new StringBuilder();
+        StringBuilder successMessage = new StringBuilder();
 
         List<String> keywords;
         ArrayList<Person> findResult;
         ArrayList<Integer> participantIds = new ArrayList<Integer>();
         Person toAddPerson;
         Integer id;
+
+        // This loop adds participants one by one using keyword matching
+        // For each participant argument passed through, the argument is broken up into keywords
+        // which are then used to search through the address book.
+        // Participant will only be added if the keyword has a unique match.
         for (String searchTerm : participants) {
+
             keywords = Arrays.asList(searchTerm.split(" "));
             NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(keywords);
             findResult = model.findPerson(predicate);
+
+            // Non-unique match (0 or more than 1) - this argument is skipped
             if (findResult.size() != 1) {
-                warning += String.format(WARNING_SEARCH_RESULTS, searchTerm, findResult.size());
+                updateWarningNotSingleMatch(warningMessage, searchTerm, findResult.size());
                 continue;
             }
+
             toAddPerson = findResult.get(0);
             id = toAddPerson.hashCode();
+
+            // Person already in this activity - this person is not added
             if (participantIds.contains(id)) {
-                warning += String.format(WARNING_DUPLICATE_PERSON, toAddPerson.getName());
+                updateWarningPersonInActivity(warningMessage, toAddPerson);
                 continue;
             }
-            if (participantIds.size() > 0) {
-                success += ", ";
-            }
             participantIds.add(id);
-            success += toAddPerson.getName();
+            updateSuccessMessage(successMessage, toAddPerson);
         }
+
         Activity toAdd = new Activity(title, participantIds.toArray(new Integer[participantIds.size()]));
         model.addActivity(toAdd);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd, success, warning));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd, successMessage, warningMessage));
     }
 
     @Override
@@ -100,5 +109,34 @@ public class ActivityCommand extends Command {
         ActivityCommand e = (ActivityCommand) other;
         return title.equals(e.title)
                 && participants.equals(e.participants);
+    }
+
+    /**
+     * @param stringBuilder StringBuilder that contains the success message
+     * @param person Person that got just added
+     *
+     */
+    private void updateSuccessMessage(StringBuilder stringBuilder, Person person) {
+        if (stringBuilder.length() > 0) {
+            stringBuilder.append(", ");
+        }
+        stringBuilder.append(person.getName());
+    }
+
+    /**
+     * @param stringBuilder StringBuilder that contains the warning message
+     * @param searchTerm Search term that was used to find a person
+     * @param matchCount Number of valid matches
+     */
+    private void updateWarningNotSingleMatch(StringBuilder stringBuilder, String searchTerm, int matchCount) {
+        stringBuilder.append(String.format(WARNING_SEARCH_RESULTS, searchTerm, matchCount));
+    }
+
+    /**
+     * @param stringBuilder StringBuilder that contains the warning message
+     * @param person Person object that attempted to be added in but failed as it is already in the participant list
+     */
+    private void updateWarningPersonInActivity(StringBuilder stringBuilder, Person person) {
+        stringBuilder.append(String.format(WARNING_DUPLICATE_PERSON, person.getName()));
     }
 }
