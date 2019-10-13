a16d95644978cf6ecc32aad641423ec15755b44f
seahlynn <e0324409@u.nus.edu>
2019-10-14 00:32:52 +0800

    Edited to fix checkstyle

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,11 +10,10 @@ public class CliSyntax {
     public static final Prefix PREFIX_TASK_INDEX = new Prefix("ti/");
     public static final Prefix PREFIX_TASK_STATUS = new Prefix("s/");
     public static final Prefix PREFIX_TASK_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_MEMBER_NAME = new Prefix("mn/");
-    public static final Prefix PREFIX_MEMBER_INDEX = new Prefix("mi/");
     public static final Prefix PREFIX_DEADLINE = new Prefix("at/");
 
     /* Prefix definitions for Member */
+    public static final Prefix PREFIX_MEMBER_NAME = new Prefix("mn/");
     public static final Prefix PREFIX_MEMBER_ID = new Prefix("mi/");
     public static final Prefix PREFIX_MEMBER_TAG = new Prefix("mt/");
 
