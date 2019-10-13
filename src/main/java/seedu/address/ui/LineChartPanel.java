821ad8c0ef146ec45a10658aa1090104abf6eff7
dalisc <42921826+dalisc@users.noreply.github.com>
2019-10-13 14:34:16 +0800

    Manually test line chart on worker date of birth (arbitrary)

--- src/main/java/seedu/address/ui/LineChartPanel.java
+++ src/main/java/seedu/address/ui/LineChartPanel.java
@@ -1,8 +1,6 @@
 package seedu.address.ui;
 
-import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
-import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
@@ -16,10 +14,9 @@ import seedu.address.model.entity.worker.Worker;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.Executors;
+import java.util.HashMap;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -30,11 +27,13 @@ public class LineChartPanel extends UiPart<Region> {
     private ScheduledExecutorService scheduledExecutorService;
     private ObservableList<Worker> bodyList;
     // this is used to display time in HH:mm:ss format
-    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d");
     final CategoryAxis xAxis = new CategoryAxis(); // we are gonna plot against time
     final NumberAxis yAxis = new NumberAxis();
     final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
     final int WINDOW_SIZE = 10;
+    private XYChart.Series<String, Number> series = new XYChart.Series<>();
+    private HashMap<String, Number> freqByDate = new HashMap<>();
 
     @FXML
     private TextArea resultDisplay;
@@ -51,9 +50,9 @@ public class LineChartPanel extends UiPart<Region> {
 
         //creating the line chart with two axis created above
         lineChart.setTitle("Bodies Admitted");
+        lineChart.setAnimated(false);
 
         //defining a series to display data
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
         series.setName("Data Series");
 
         // add series to chart
@@ -61,45 +60,36 @@ public class LineChartPanel extends UiPart<Region> {
 
         bodyList.addListener((ListChangeListener<Worker>) c -> {
             while (c.next()) {
-                Date now = new Date();
+
                 if (c.wasAdded()) {
-                    series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), 10));
+                    Date now = c.getAddedSubList().get(0).getDateOfBirth();
+                    String dateOnXAxis = simpleDateFormat.format(now);
+                    Number oldFreq = freqByDate.getOrDefault(dateOnXAxis,0);
+                    int newFreq = oldFreq.intValue() + 1;
+                    freqByDate.put(dateOnXAxis, newFreq);
                 }
+
                 if (c.wasRemoved()) {
-                    series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), 5));
+                    Date now = c.getRemoved().get(0).getDateOfBirth();
+                    String dateOnXAxis = simpleDateFormat.format(now);
+                    Number oldFreq = freqByDate.getOrDefault(dateOnXAxis,0);
+                    int newFreq = oldFreq.intValue() - 1;
+                    freqByDate.put(dateOnXAxis, newFreq);
                 }
-                if (series.getData().size() > WINDOW_SIZE) {
-                    series.getData().remove(0);
-                }
+                reinitialiseChart();
             }
         });
 
-//        // setup a scheduled executor to periodically put data into the chart
-//        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-//
-//        // put dummy data onto graph per second
-//        scheduledExecutorService.scheduleAtFixedRate(() -> {
-//            // get a random integer between 0-10
-//            Integer random = ThreadLocalRandom.current().nextInt(10);
-//
-//            // Update the chart
-//            Platform.runLater(() -> {
-//                // get current time
-//                Date now = new Date();
-//                // put random number with current time
-//                for (int i = 0; i < bodyList.size(); i++) {
-//                    series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), random));
-//                }
-//                // cut the chart off after specific length
-//                if (series.getData().size() > WINDOW_SIZE) {
-//                    series.getData().remove(0);
-//                }
-//            });
-//        }, 0, 1, TimeUnit.MINUTES);
         return lineChart;
     }
 
-    public void shutDownScheduledExecutor() {
-        this.scheduledExecutorService.shutdownNow();
+    public void reinitialiseChart() {
+        series.getData().clear();
+        freqByDate.forEach((date, freq) -> {
+            series.getData().add(new XYChart.Data<>(date, freq));
+            if (series.getData().size() > WINDOW_SIZE) {
+                series.getData().remove(0);
+            }
+        });
     }
 }
