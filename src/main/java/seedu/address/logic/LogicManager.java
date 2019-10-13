130bd189a5750f2a0efa11f858f42dea51507eac
Zhang Dongjun <dexterzhang430@hotmail.com>
2019-10-13 20:32:55 +0800

    Fix order of static variable definition

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -23,8 +23,8 @@ import seedu.address.storage.Storage;
  */
 public class LogicManager implements Logic {
     public static final String FILE_OPS_ERROR_MESSAGE = "Could not save data to file: ";
-    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
     private static boolean isQuiz = false;
+    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
 
     private final Model model;
     private final Storage storage;
