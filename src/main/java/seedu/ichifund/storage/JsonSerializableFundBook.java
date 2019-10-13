393ab464ce56a31d1f46e23010e3edd0d7bc5b04
Eugene Lim <nexolute@gmail.com>
2019-10-14 01:03:23 +0800

    Modify JsonSerializableFundBook to serialize Budget

--- src/main/java/seedu/ichifund/storage/JsonSerializableFundBook.java
+++ src/main/java/seedu/ichifund/storage/JsonSerializableFundBook.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 import seedu.ichifund.commons.exceptions.IllegalValueException;
 import seedu.ichifund.model.FundBook;
 import seedu.ichifund.model.ReadOnlyFundBook;
+import seedu.ichifund.model.budget.Budget;
 import seedu.ichifund.model.person.Person;
 
 /**
@@ -20,15 +21,19 @@ import seedu.ichifund.model.person.Person;
 class JsonSerializableFundBook {
 
     public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
+    public static final String MESSAGE_DUPLICATE_BUDGET = "Budgets list contains duplicate budget(s).";
 
     private final List<JsonAdaptedPerson> persons = new ArrayList<>();
+    private final List<JsonAdaptedBudget> budgets = new ArrayList<>();
 
     /**
-     * Constructs a {@code JsonSerializableFundBook} with the given persons.
+     * Constructs a {@code JsonSerializableFundBook} with the given person and budget.
      */
     @JsonCreator
-    public JsonSerializableFundBook(@JsonProperty("persons") List<JsonAdaptedPerson> persons) {
+    public JsonSerializableFundBook(@JsonProperty("persons") List<JsonAdaptedPerson> persons,
+                                    @JsonProperty("budgets") List<JsonAdaptedBudget> budgets) {
         this.persons.addAll(persons);
+        this.budgets.addAll(budgets);
     }
 
     /**
@@ -38,6 +43,7 @@ class JsonSerializableFundBook {
      */
     public JsonSerializableFundBook(ReadOnlyFundBook source) {
         persons.addAll(source.getPersonList().stream().map(JsonAdaptedPerson::new).collect(Collectors.toList()));
+        budgets.addAll(source.getBudgetList().stream().map(JsonAdaptedBudget::new).collect(Collectors.toList()));
     }
 
     /**
@@ -53,6 +59,14 @@ class JsonSerializableFundBook {
                 throw new IllegalValueException(MESSAGE_DUPLICATE_PERSON);
             }
             fundBook.addPerson(person);
+        }
+
+        for (JsonAdaptedBudget jsonAdaptedBudget : budgets) {
+            Budget budget = jsonAdaptedBudget.toModelType();
+            if (fundBook.hasBudget(budget)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_BUDGET);
+            }
+            fundBook.addBudget(budget);
         }
         return fundBook;
     }
