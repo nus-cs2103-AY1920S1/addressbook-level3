8d30650262c2358f7110a93648f79291c7f1120a
Siang Hwee <siang_hwee@hotmail.com>
2019-10-13 15:10:04 +0800

    Store all items in item storage

--- src/main/java/seedu/address/model/ItemStorage.java
+++ src/main/java/seedu/address/model/ItemStorage.java
@@ -1,6 +1,7 @@
 package seedu.address.model;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,13 +10,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import seedu.address.commons.core.item.Item;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.JsonUtil;
-import seedu.address.model.item.ItemList;
 
 /**
  * The central storage of all the items in the program.
  */
 public class ItemStorage {
-    private ItemList items = new ItemList();
+    private ArrayList<Item> items = new ArrayList<>();
 
     /**
      * Adds an item to the item list.
@@ -29,12 +29,64 @@ public class ItemStorage {
      * Retrieve the item list.
      * @return the item list.
      */
-    public ItemList getItems() {
+    public ArrayList<Item> getItems() {
         return this.items;
     }
 
+    /**
+     * Gets the item at the index.
+     * @param index the index of the item to be retrieved.
+     * @return the item at that index.
+     */
+    public Item get(int index) {
+        return items.get(index);
+    }
+
+    /**
+     * Return the size of the storage.
+     * @return the size of the storage as an integer.
+     */
+    public int size() {
+        return items.size();
+    }
+
+    /**
+     * Remove the item from within the storage.
+     * @param item the item to be removed from the storage.
+     * @return
+     */
+    public Item remove(Item item) {
+        items.remove(item);
+        return item;
+    }
+
+    /**
+     * Returns the index of the item in the storage. If the item is not in the storage,
+     * a index of -1 is returned.
+     * @param item the item to be searched for.
+     * @return the index of the item in the storage.
+     */
+    public int indexOf(Item item) {
+        return items.indexOf(item);
+    }
+
+    /**
+     * Set the item at the specific index to a new item.
+     * @param index the index of the item to be replaced.
+     * @param newItem the item to replace the old item.
+     * @return
+     */
+    public Item setItem(int index, Item newItem) {
+        return items.set(index, newItem);
+    }
+
+    /**
+     * Converts the storage into a JSON string.
+     * @return the JSON representation of the storage.
+     * @throws JsonProcessingException
+     */
     public String toJson() throws JsonProcessingException {
-        return JsonUtil.toJsonString(items.getList());
+        return JsonUtil.toJsonString(items);
     }
 
     /**
