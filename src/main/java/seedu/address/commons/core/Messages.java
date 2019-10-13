fcb3b52a0c599476eb58c3fd9dfb83d71b7cf6bc
Sam-limyr <nacuniel@gmail.com>
2019-10-14 00:05:42 +0800

    Implemented TitleMatchesKeywordsPredicate for matching exact titles.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,5 +10,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_INVALID_NOTE_DISPLAYED_INDEX = "The note index provided is invalid!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_NOTES_LISTED_OVERVIEW = "%1$d notes listed!";
+    public static final String MESSAGE_MATCHING_NOTE_FOUND = "Note matching the title exists.";
+    public static final String MESSAGE_NO_MATCHING_NOTE_FOUND = "No note with that title exists!";
 
 }
