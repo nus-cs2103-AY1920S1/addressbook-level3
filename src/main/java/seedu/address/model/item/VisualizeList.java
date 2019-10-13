40862474b2ccd71a5af95edb62cb3cfec272c68f
Siang Hwee <siang_hwee@hotmail.com>
2019-10-13 15:11:03 +0800

    Make Visualize List extends from Modifiable Observable List

--- src/main/java/seedu/address/model/item/VisualizeList.java
+++ src/main/java/seedu/address/model/item/VisualizeList.java
@@ -2,15 +2,16 @@ package seedu.address.model.item;
 
 import java.util.ArrayList;
 
+import javafx.collections.ModifiableObservableListBase;
 import seedu.address.commons.core.item.Item;
 
 /**
  * An object to hold items. Parent class for TaskList, EventList and ReminderList.
  */
-public class ItemList {
+public abstract class VisualizeList extends ModifiableObservableListBase<Item> {
     protected ArrayList<Item> list;
 
-    public ItemList() {
+    public VisualizeList() {
         this.list = new ArrayList<>();
     }
 
@@ -18,13 +19,18 @@ public class ItemList {
      * Add an item into the list.
      * @param item the item to be added into the list
      */
-    public void add(Item item) {
-        list.add(item);
+    public boolean add(Item item) {
+        return list.add(item);
+    }
+
+    @Override
+    public void doAdd(int index, Item item) {
+        list.add(index, item);
     }
 
     /**
      * Get the list of the item list.
-     * @return an ArrayList of all the items in the ItemList
+     * @return an ArrayList of all the items in the VisualizeList
      */
     public ArrayList<Item> getList() {
         return this.list;
@@ -47,30 +53,17 @@ public class ItemList {
         list.remove(item);
     }
 
-    /**
-     * Sorts the items in the list by their lexicographical order.
-     * @return the item list in the sorted order
-     */
-    public ItemList sort() {
-        ItemList il = new ItemList();
-        for (Item item : list) {
-            il.add(item);
-        }
-
-        il.list.sort((item1, item2) -> item1.getItemDescription().getDescription()
-                .compareTo(item2.getItemDescription().getDescription()));
-        return il;
+    @Override
+    public Item doSet(int index, Item item) {
+        return list.set(index, item);
     }
 
-    /**
-     * Main method to find a search string.
-     * @param searchString the string to be searched for within the description
-     * @return a new ItemList containing all the items that match the criteria.
-     */
-    public ItemList find(String searchString) {
-        ItemList il = new ItemList();
-        return find(searchString, il);
+    @Override
+    public Item doRemove(int index) {
+        return list.remove(index);
     }
+
+    public abstract VisualizeList find(String searchString);
 
     /**
      * Helper function to find an item based on their description.
@@ -78,7 +71,7 @@ public class ItemList {
      * @param il the item list that will hold the items that contain the string within its description
      * @return the item list that was given with the found items added
      */
-    protected ItemList find(String searchString, ItemList il) {
+    protected VisualizeList find(String searchString, VisualizeList il) {
         for (Item i : list) {
             if (i.getItemDescription().getDescription().toLowerCase().contains(searchString.toLowerCase())) {
                 il.add(i);
@@ -117,12 +110,20 @@ public class ItemList {
         if (this == other) {
             return true;
         } else {
-            if (!(other instanceof ItemList)) {
+            if (!(other instanceof VisualizeList)) {
                 return false;
             }
 
-            ItemList otherIl = (ItemList) other;
+            VisualizeList otherIl = (VisualizeList) other;
             return this.list.equals(otherIl.list);
         }
     }
+
+    /**
+     * Sorts the items in the list.
+     * @return the item list in the sorted order
+     */
+    public abstract VisualizeList sort();
+
+
 }
