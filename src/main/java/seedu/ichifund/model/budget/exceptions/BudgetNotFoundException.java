f8b2f9c927aa12c873acbfbad114c200b1350b01
Eugene Lim <nexolute@gmail.com>
2019-10-13 16:13:35 +0800

    Add exceptions relating to Budget model

---
+++ src/main/java/seedu/ichifund/model/budget/exceptions/BudgetNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.ichifund.model.budget.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified budget.
+ */
+public class BudgetNotFoundException extends RuntimeException {}
