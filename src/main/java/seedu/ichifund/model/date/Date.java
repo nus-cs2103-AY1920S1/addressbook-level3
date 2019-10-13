4bfb94116630003b297e6a525872b4ca39f86cc9
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-13 20:13:27 +0800

    Add toString to Date

--- src/main/java/seedu/ichifund/model/date/Date.java
+++ src/main/java/seedu/ichifund/model/date/Date.java
@@ -55,4 +55,9 @@ public class Date implements Comparable<Date> {
     public int compareTo(Date other) {
         return this.date.compareTo(other.date);
     }
+
+    @Override
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
 }
