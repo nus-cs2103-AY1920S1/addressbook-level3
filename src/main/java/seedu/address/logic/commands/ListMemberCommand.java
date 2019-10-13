2e8dad97dd10359d4440ea1127a91b2bf6512585
seahlynn <e0324409@u.nus.edu>
2019-10-14 00:54:29 +0800

    Made changes for checkstyle

--- src/main/java/seedu/address/logic/commands/ListMemberCommand.java
+++ src/main/java/seedu/address/logic/commands/ListMemberCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_MEMBERS;
+
 import seedu.address.model.Model;
 
 /**
