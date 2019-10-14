20dd0850886bbacc4873388ae5cc89711e5800fc
Tee Hao Wei <54080836+thwnus@users.noreply.github.com>
2019-10-15 05:26:30 +0800

    Add a command to evaluate a script

---
+++ src/main/java/seedu/address/logic/commands/EvalCommand.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.script.ScriptManager;
+import seedu.address.logic.script.exceptions.ScriptException;
+import seedu.address.model.Model;
+
+/**
+ * Evaluates a script. The script has access to the full model.
+ */
+public class EvalCommand extends Command {
+    public static final String COMMAND_WORD = "eval";
+
+    public static final String MESSAGE_NO_RESULT = "Script succeeded with no result.";
+    public static final String MESSAGE_SCRIPT_EXCEPTION = "Exception thrown during script evaluation\n%1$s";
+
+    private final String script;
+
+    public EvalCommand(String script) {
+        this.script = script;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        try {
+            Object result = ScriptManager.evaluateScript(script, model);
+            if (result == null) {
+                return new CommandResult(MESSAGE_NO_RESULT);
+            } else {
+                return new CommandResult(result.toString());
+            }
+        } catch (ScriptException se) {
+            throw new CommandException(String.format(MESSAGE_SCRIPT_EXCEPTION, se.getMessage()), se);
+        }
+    }
+}
