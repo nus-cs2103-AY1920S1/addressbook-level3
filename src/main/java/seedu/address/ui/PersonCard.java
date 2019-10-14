d6c2de560ab22f9ec2c21ea0f14305b2a7a9191b
ngswbryan <46245100+ngswbryan@users.noreply.github.com>
2019-10-14 15:39:29 +0800

    updated checkstyle for imports

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -2,10 +2,10 @@ package seedu.address.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 
 /**
