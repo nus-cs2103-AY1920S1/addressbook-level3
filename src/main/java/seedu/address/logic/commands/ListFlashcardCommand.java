7e4d52451df0a8663a7a6839b02196ba0c7fd6f4
Sahilgat <nisgat@live.com>
2019-10-14 23:29:59 +0800

    Update List_Flashcard command to display flashcards

--- src/main/java/seedu/address/logic/commands/ListFlashcardCommand.java
+++ src/main/java/seedu/address/logic/commands/ListFlashcardCommand.java
@@ -4,6 +4,9 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_FLASHCARDS;
 
 import seedu.address.model.Model;
+import seedu.address.model.flashcard.Flashcard;
+
+import java.util.List;
 
 /**
  * Lists all flashcards in the StudyBuddy application to the user.
@@ -18,7 +21,21 @@ public class ListFlashcardCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredFlashcardList(PREDICATE_SHOW_ALL_FLASHCARDS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        model.updateFilteredFlashcardList(PREDICATE_SHOW_ALL_FLASHCARDS); //Still needed?
+        List<Flashcard> lastShownList = model.getFilteredFlashcardList();
+
+        //Builds list display of flashcard (Index. Title - Question)
+        StringBuilder sb = new StringBuilder();
+        int size = lastShownList.size();
+        for(int i = 1; i <= size; i++) {
+            Flashcard curr_flashcard = lastShownList.get(i - 1);
+            sb.append(i + ". ");
+            sb.append(curr_flashcard.getTitle() + " - ");
+            sb.append(curr_flashcard.getQuestion());
+            if(i != size) {
+                sb.append("\n");
+            }
+        }
+        return new CommandResult(sb.toString());
     }
 }
