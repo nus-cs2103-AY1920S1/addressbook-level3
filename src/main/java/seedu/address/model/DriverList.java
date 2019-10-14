c75dc0f4f5013f986d0d2baed8311182523df4ba
cheeyou <youchee1999@gmail.com>
2019-10-14 22:00:04 +0800

    more comments

--- src/main/java/seedu/address/model/DriverList.java
+++ src/main/java/seedu/address/model/DriverList.java
@@ -19,22 +19,46 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
  */
 public class DriverList extends UniquePersonList {
 
+    /**
+     * Check the presence of a Driver in the DriverList.
+     */
     public boolean containsDriver(Driver toCheck) {
         return contains(toCheck);
     }
 
+    /**
+     * Adds a Driver to the DriverList
+     *
+     * @param toAdd driver to be added to DriverList
+     */
     public void addDriver(Driver toAdd) {
         super.add(toAdd);
     }
 
+    /**
+     * Removes a Driver from the DriverList
+     *
+     * @param toRemove Driver to be removed from DriverList
+     */
     public void removeDriver(Driver toRemove) {
         super.remove(toRemove);
     }
 
+    /**
+     * Replaces an existing Driver from the DriverList with another Driver
+     *
+     * @param target Driver to be replaced
+     * @param editedDriver new Driver
+     */
     public void setDriver(Driver target, Driver editedDriver) {
         super.setPerson((Person)target, (Person)editedDriver);
     }
 
+    /**
+     * Replace the entire DriverList with a new DriverList
+     *
+     * @param replacement new DriverList
+     */
     public void setDrivers(DriverList replacement) {
         setPersons(replacement);
     }
