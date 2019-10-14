dd519daa87c1bb4e0d2f7ec91a2a5293ab40dc4b
JasmineYeo_A0185321L <jas.mint98@gmail.com>
2019-10-14 12:24:25 +0800

    Added javadoc for travis

--- src/main/java/seedu/address/logic/commands/AddCheatSheetCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCheatSheetCommand.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
--- src/main/java/seedu/address/logic/parser/AddCheatSheetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCheatSheetCommandParser.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import java.util.HashSet;
 import java.util.Set;
