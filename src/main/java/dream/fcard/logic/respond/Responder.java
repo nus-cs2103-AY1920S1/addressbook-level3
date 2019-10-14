e63ae01798c3d202732207f3646592b4a3e31b95
Taha <taha.jailani@gmail.com>
2019-10-14 16:05:11 +0800

    Fixed checkstyle errors.

--- src/main/java/dream/fcard/logic/respond/Responder.java
+++ src/main/java/dream/fcard/logic/respond/Responder.java
@@ -6,6 +6,13 @@ import dream.fcard.model.State;
  * Interface to take in user input and execute program behaviour.
  */
 public class Responder {
+    /**
+     * Takes in user input and the current state of the program. Uses the user input to decide which is the appropriate
+     * responseFunc to call.
+     *
+     * @param input A String representing the user input.
+     * @param state The current State of the program.
+     */
     public static void takeInput(String input, State state) {
         for (Responses r : Responses.values()) {
             if (r.call(input, state)) {
--- src/main/java/dream/fcard/logic/respond/Responses.java
+++ src/main/java/dream/fcard/logic/respond/Responses.java
@@ -8,43 +8,88 @@ import dream.fcard.model.State;
 enum Responses {
     HELP("(?i)^(help)?(\\s)*(command/[\\w\\p{Punct}]+)?(\\s)*", (commandInput, programState) -> {
         System.out.println("Current command is HELP");
+        /*Print out "Available commands are:\n" +
+                "Help [Command]\n" +
+                "Import filepath/FILEPATH\n" +
+                "Export filepath/FILEPATH\n" +
+                "Stats [deck/DECK_NAME]\n" +
+                "View [deck/DECK_NAME]\n" +
+                "Create deck/DECK_NAME\n" +
+                "Test [duration/TIME_LIMIT_ALLOWED] deck/DECK_NAME\n" +
+                "Exit\n" +
+                "Edit deck/DECK_NAME action/DESIRED_ACTION [index/CARD_INDEX] [front/NEW_FRONT_TEXT]" +
+                "[back/NEW_BACK_TEXT]\n";
+         */
         return true; // capture is valid, end checking other commands
     }),
     IMPORT("(?i)^(import)?(\\s)+(filepath/[\\w\\p{Punct}]+){1}(\\s)*", (commandInput, programState) -> {
         System.out.println("Current command is IMPORT");
+        //Deck newDeck = *get deck from filepath*;
+        //programState.getDecks().add(newDeck);
         return true; // capture is valid, end checking other commands
     }),
     EXPORT("(?i)^(export)?(\\s)+(filepath/[\\w\\p{Punct}]+){1}(\\s)*", (commandInput, programState) -> {
         System.out.println("Current command is EXPORT");
+        //Deck newDeck = programState.getDecks().get(*whichever deck*);
+        //*Export newDeck to filepath*
         return true; // capture is valid, end checking other commands
     }),
     STATS("(?i)^(stats)?(\\s)*(deck/[\\w\\p{Punct}]+)?(\\s)*", (commandInput, programState) -> {
         System.out.println("Current command is STATS");
+        // ArrayList<Deck> allDecks = programState.getDecks();
+        // String inputName = *name of deck to find*;
+        // for (Deck curr : allDecks) {
+        //      if(curr.getName().equals(inputName) {
+        //         System.out.println(curr.getStats());
+        //      }
+        // }
         return true; // capture is valid, end checking other commands
     }),
     VIEW("(?i)^(view)?(\\s)*(deck/[\\w\\p{Punct}]+)?(\\s)*", (commandInput, programState) -> {
         System.out.println("Current command is VIEW");
+        // ArrayList<Deck> allDecks = programState.getDecks();
+        //  String inputName = *name of deck to find*;
+        //  for (Deck curr : allDecks) {
+        //      if(curr.getName().equals(inputName) {
+        //          curr.viewDeck();
+        //      }
+        //  }
+
         return true; // capture is valid, end checking other commands
     }),
     CREATE("(?i)^(create)?(\\s)+(deck/[\\w\\p{Punct}]+){1}(\\s)*", (commandInput, programState) -> {
         System.out.println("Current command is CREATE");
+        // String inputName = *name of deck to find*;
+        // Deck newDeck = new Deck(inputName);
+        // programState.getDecks().add(newDeck);
         return true; // capture is valid, end checking other commands
     }),
-    TEST("(?i)^(test)?(\\s)+(duration/[\\w\\p{Punct}]+)*(deck/[\\w\\p{Punct}]+){1}(\\s)*", (
+    TEST("(?i)^(test)?(\\s)+(duration/[\\w\\p{Punct}]+)?(deck/[\\w\\p{Punct}]+){1}(\\s)*", (
             commandInput, programState) -> {
         System.out.println("Current command is TEST");
+        // ArrayList<Deck> allDecks = programState.getDecks();
+        // String inputName = *name of deck to find*;
+        // Deck testDeck;
+        // for (Deck curr : allDecks) {
+        //      if(curr.getName().equals(inputName) {
+        //          testDeck = curr;
+        //      }
+        // }
+        // *Initiate test with Test Deck*
         return true; // capture is valid, end checking other commands
     }),
     EXIT("(?i)^(exit)?", (commandInput, programState) -> {
         System.out.println("Current command is EXIT");
+        // Exit from application
         return true; // capture is valid, end checking other commands
     }),
     EDIT("(?i)^(edit)?(\\s)+(deck/[\\w\\p{Punct}]+){1}(action/[\\w\\p{Punct}]+){1}(index/[\\w\\p{Punct}]+)?"
             + "(front/[\\w\\p{Punct}]+)?(back/[\\w\\p{Punct}]+)?(\\s)*", (
             commandInput, programState) -> {
-        System.out.println("Current command is EDIT");
-        return true; // capture is valid, end checking other commands
-    });
+                System.out.println("Current command is EDIT");
+                // Will plan an implement soon (a bit busy now lol)
+                return true; // capture is valid, end checking other commands
+            });
 
     private String regex;
     private ResponseFunc func;
