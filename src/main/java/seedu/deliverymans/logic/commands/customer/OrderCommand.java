207103a69571e5b6baf12436c81734c17c61efbd
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-10-14 16:03:37 +0800

    Added working but unoptimized implementation of adding order command in customer context

--- src/main/java/seedu/deliverymans/logic/commands/customer/OrderCommand.java
+++ src/main/java/seedu/deliverymans/logic/commands/customer/OrderCommand.java
@@ -1,18 +1,54 @@
 package seedu.deliverymans.logic.commands.customer;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.deliverymans.logic.parser.CliSyntax.PREFIX_CUSTOMER;
+import static seedu.deliverymans.logic.parser.CliSyntax.PREFIX_DELIVERYMAN;
+import static seedu.deliverymans.logic.parser.CliSyntax.PREFIX_ORDER;
+import static seedu.deliverymans.logic.parser.CliSyntax.PREFIX_RESTAURANT;
+
 import seedu.deliverymans.logic.commands.Command;
 import seedu.deliverymans.logic.commands.CommandResult;
 import seedu.deliverymans.logic.commands.exceptions.CommandException;
 import seedu.deliverymans.model.Model;
+import seedu.deliverymans.model.order.Order;
 
 /**
- * (to be added)
+ * Order command
  */
 public class OrderCommand extends Command {
     public static final String COMMAND_WORD = "order";
 
+    public static final String MESSAGE_ADD_SUCCESS = "Added order: %1$s";
+
+    public static final String MESSAGE_ADD_ORDER_USAGE = COMMAND_WORD
+            + ": Adds an order to the manager. "
+            + "Parameters: "
+            + PREFIX_ORDER + "ORDER "
+            + "[" + PREFIX_CUSTOMER + "CUSTOMER]\n"
+            + "[" + PREFIX_RESTAURANT + "RESTAURANT]\n"
+            + "[" + PREFIX_DELIVERYMAN + "DELIVERYMAN]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_CUSTOMER + "Sam "
+            + PREFIX_RESTAURANT + "KFC "
+            + PREFIX_DELIVERYMAN + "Deliveryman #1337";
+
+    private final Order toAdd;
+
+    public OrderCommand(Order toAdd) {
+        requireNonNull(toAdd);
+        this.toAdd = toAdd;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        model.addOrder(toAdd);
+        return new CommandResult(String.format(MESSAGE_ADD_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof OrderCommand // instanceof handles nulls
+                && toAdd.equals(((OrderCommand) other).toAdd));
     }
 }
--- src/main/java/seedu/deliverymans/logic/parser/CliSyntax.java
+++ src/main/java/seedu/deliverymans/logic/parser/CliSyntax.java
@@ -10,7 +10,18 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_REMARK = new Prefix("r/");
+    public static final Prefix PREFIX_REMARK = new Prefix("rr/");
+    public static final Prefix PREFIX_CUSTOMER = new Prefix("c/");
+    public static final Prefix PREFIX_DELIVERYMAN = new Prefix("d/");
+    public static final Prefix PREFIX_RESTAURANT = new Prefix("r/");
+
+    // Prefix for universal commands
+
+
+    // Prefix for customer commands
     public static final Prefix PREFIX_ORDER = new Prefix("o/");
 
+    // Prefix for deliveryman commands
+
+    // Prefix for restaurant commands
 }
