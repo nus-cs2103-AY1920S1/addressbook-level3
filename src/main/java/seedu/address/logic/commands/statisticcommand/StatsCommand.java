a6994d59c0c26eceb8e304795aa888f1c254f222
eugene teu <eugeneteu@gmail.com>
2019-10-14 13:07:34 +0800

    string fix

--- src/main/java/seedu/address/logic/commands/statisticcommand/StatsCommand.java
+++ src/main/java/seedu/address/logic/commands/statisticcommand/StatsCommand.java
@@ -16,7 +16,11 @@ public class StatsCommand extends Command {
 
     public static final String COMMAND_WORD = "generate-s";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": generates statistic for ";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": generates statistic for PROFIT/COST/REVENUE"
+            + " Parameters: s/{Type of stat} d1/{starting date (YYYY.MM.DD)} "
+            + "d2/{ending date}\n"
+            + "Example: generate-s s/REVENUE d1/2018.12.13 d2/2019.11.13";
 
     private final Calendar startingDate;
 
@@ -35,7 +39,6 @@ public class StatsCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         StatsPayload payload = new StatsPayload(startingDate, endingDate, statisticType);
-        String statMessage = MESSAGE_USAGE + statisticType.toString();
-        return new CommandResult(statMessage, payload, UiChange.STATS);
+        return new CommandResult(MESSAGE_USAGE, payload, UiChange.STATS);
     }
 }
