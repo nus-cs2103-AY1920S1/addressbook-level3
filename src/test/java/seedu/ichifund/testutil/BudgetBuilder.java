c232b7a905286eadb18a63634df04854ae7c9b20
Eugene Lim <nexolute@gmail.com>
2019-10-14 17:22:03 +0800

    Modify BudgetBuilder for test

--- src/test/java/seedu/ichifund/testutil/BudgetBuilder.java
+++ src/test/java/seedu/ichifund/testutil/BudgetBuilder.java
@@ -3,6 +3,9 @@ package seedu.ichifund.testutil;
 import seedu.ichifund.model.Amount;
 import seedu.ichifund.model.Description;
 import seedu.ichifund.model.budget.Budget;
+import seedu.ichifund.model.date.Month;
+import seedu.ichifund.model.date.Year;
+import seedu.ichifund.model.transaction.Category;
 
 /**
  * A utility class to help with building Budget objects.
@@ -11,13 +14,22 @@ public class BudgetBuilder {
 
     public static final String DEFAULT_DESCRIPTION = "Saving for my future";
     public static final String DEFAULT_AMOUNT = "13.37";
+    public static final String DEFAULT_MONTH = "12";
+    public static final String DEFAULT_YEAR = "2012";
+    public static final String DEFAULT_CATEGORY = "food";
 
     private Description description;
     private Amount amount;
+    private Month month;
+    private Year year;
+    private Category category;
 
     public BudgetBuilder() {
         description = new Description(DEFAULT_DESCRIPTION);
         amount = new Amount(DEFAULT_AMOUNT);
+        month = new Month(DEFAULT_MONTH);
+        year = new Year(DEFAULT_YEAR);
+        category = new Category(DEFAULT_CATEGORY);
     }
 
     /**
@@ -26,6 +38,9 @@ public class BudgetBuilder {
     public BudgetBuilder(Budget budgetToCopy) {
         description = budgetToCopy.getDescription();
         amount = budgetToCopy.getAmount();
+        month = budgetToCopy.getMonth();
+        year = budgetToCopy.getYear();
+        category = budgetToCopy.getCategory();
     }
 
     /**
@@ -45,7 +60,7 @@ public class BudgetBuilder {
     }
 
     public Budget build() {
-        return new Budget(description, amount);
+        return new Budget(description, amount, month, year, category);
     }
 
 }
