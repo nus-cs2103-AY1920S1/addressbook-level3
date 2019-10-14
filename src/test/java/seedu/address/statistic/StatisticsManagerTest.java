9a5a16c4f4cba4cf5862d417046d587b6969a5b7
eugene teu <eugeneteu@gmail.com>
2019-10-14 14:42:11 +0800

    tests for default Stats methods

--- src/test/java/seedu/address/statistic/StatisticsManagerTest.java
+++ src/test/java/seedu/address/statistic/StatisticsManagerTest.java
@@ -9,7 +9,9 @@ import javafx.collections.ObservableList;
 
 import seedu.address.model.ReadOnlyDataBook;
 import seedu.address.model.order.Order;
+import seedu.address.model.phone.Phone;
 import seedu.address.testutil.TypicalOrders;
+import seedu.address.testutil.TypicalPhones;
 
 
 public class StatisticsManagerTest {
@@ -25,6 +27,25 @@ public class StatisticsManagerTest {
         String testResult = this.statisticModule.calculateTotalRevenueOnCompleted(orderBookStub);
         assertEquals(actualResult, testResult);
     }
+
+    @Test
+    public void execute_calculateTotalProfitOnCompleted() {
+        OrderBookStub orderBookStub = new OrderBookStub();
+        PhoneBookStub phoneBookStub = new PhoneBookStub();
+        String actualResult = "1540.12";
+        String testResult = this.statisticModule.calculateTotalProfitOnCompleted(orderBookStub, phoneBookStub);
+        assertEquals(actualResult, testResult);
+    }
+
+    @Test
+    public void execute_calculateTotalCostOnCompleted() {
+        OrderBookStub orderBookStub = new OrderBookStub();
+        PhoneBookStub phoneBookStub = new PhoneBookStub();
+        String actualResult = "1100.0";
+        String testResult = this.statisticModule.calculateTotalCostOnCompleted(orderBookStub, phoneBookStub);
+        assertEquals(actualResult, testResult);
+    }
+
 
     /**
      * Stub method to build a dummy order book for use here
@@ -43,6 +64,28 @@ public class StatisticsManagerTest {
         @Override
         public ObservableList<Order> getList() {
             return orders;
+        }
+    }
+
+    /**
+     * Stub method to build a dummy order book for use here
+     */
+    private static class PhoneBookStub implements ReadOnlyDataBook<Phone> {
+        private final ObservableList<Phone> phones = FXCollections.observableArrayList();
+
+        PhoneBookStub() {
+            ObservableList<Phone> listOfTypicalPhones = FXCollections.observableArrayList();
+            listOfTypicalPhones.add(TypicalPhones.IPHONEONE);
+            listOfTypicalPhones.add(TypicalPhones.IPHONETWO);
+            listOfTypicalPhones.add(TypicalPhones.ANDROIDONE);
+            listOfTypicalPhones.add(TypicalPhones.IPHONETWO);
+
+            this.phones.setAll(listOfTypicalPhones);
+        }
+
+        @Override
+        public ObservableList<Phone> getList() {
+            return phones;
         }
     }
 
