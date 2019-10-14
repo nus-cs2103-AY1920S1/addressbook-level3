94a3e12a9b05096bfc36b20fe70346791c871de5
klementtan <klementtan@gmail.com>
2019-10-15 01:38:59 +0800

    Resolve check style

--- src/test/java/seedu/address/logic/internal/gmaps/SanitizeLocationTest.java
+++ src/test/java/seedu/address/logic/internal/gmaps/SanitizeLocationTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.net.ConnectException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
