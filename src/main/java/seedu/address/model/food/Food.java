732f665e34ec05d2775f48b5bb0a1267ae6eea6a
ManasVegi <manasvegi@gmail.com>
2019-10-14 18:55:08 +0800

    Remove tags from the parent Food class

--- src/main/java/seedu/address/model/food/Food.java
+++ src/main/java/seedu/address/model/food/Food.java
@@ -19,17 +19,14 @@ public class Food {
     private final Name name;
     private final Amount amount;
 
-    // Data fields
-    private final Set<Tag> tags = new HashSet<>();
-
     /**
      * Every field must be present and not null.
      */
-    public Food(Name name, Amount amount, Set<Tag> tags) {
-        requireAllNonNull(name, tags);
+    public Food(Name name, Amount amount) {
+        requireAllNonNull(name);
         this.name = name;
         this.amount = amount;
-        this.tags.addAll(tags);
+        //this.tags.addAll(tags);
     }
 
     public Name getName() {
@@ -40,13 +37,13 @@ public class Food {
         return amount;
     }
 
-    /**
-     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
-     * if modification is attempted.
-     */
-    public Set<Tag> getTags() {
-        return Collections.unmodifiableSet(tags);
-    }
+//    /**
+//     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+//     * if modification is attempted.
+//     */
+//    public Set<Tag> getTags() {
+//        return Collections.unmodifiableSet(tags);
+//    }
 
     /**
      * Returns true if both persons of the same name have at least one other identity field that is the same.
@@ -64,15 +61,15 @@ public class Food {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, tags);
+        return Objects.hash(name, amount);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append(getName())
-                .append(" Tags: ");
-        getTags().forEach(builder::append);
+        builder.append(getName());
+                //.append(" Tags: ");
+        //getTags().forEach(builder::append);
         return builder.toString();
     }
 
