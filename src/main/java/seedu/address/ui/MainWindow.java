d0a183af8e1064e1e59efb8faa34298cce24398d
honhaochen <haochenhon@hotmail.com>
2019-10-14 22:02:14 +0800

    for travis

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -95,7 +95,6 @@ public class MainWindow extends UiPart<Stage> implements Page {
 
     /**
      * Sets the accelerator of a MenuItem.
-     * 
      * @param keyCombination the KeyCombination value of the accelerator
      */
     private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
