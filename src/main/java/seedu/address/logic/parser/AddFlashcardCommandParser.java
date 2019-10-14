cdb347d8acbc56519eef02cde6fa2b5cf8ed1013
Sahilgat <nisgat@live.com>
2019-10-14 13:12:06 +0800

    Fix checkstyle issues for Travis

--- src/main/java/seedu/address/logic/parser/AddFlashcardCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddFlashcardCommandParser.java
@@ -3,8 +3,8 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ANSWER;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_QUESTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FLASHCARD_TITLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_QUESTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
