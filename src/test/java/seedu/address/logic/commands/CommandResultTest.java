efe398028dee09b336858f4e09eceea0c5989690
Q-gabe <gabriel.ong@u.nus.edu>
2019-10-14 12:49:34 +0800

    chore: update command tests to reflect new CommandResult constructor changes

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -14,7 +14,7 @@ public class CommandResultTest {
 
         // same values -> returns true
         assertTrue(commandResult.equals(new CommandResult("feedback")));
-        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, false, false)));
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, false, false, false)));
 
         // same object -> returns true
         assertTrue(commandResult.equals(commandResult));
@@ -54,7 +54,7 @@ public class CommandResultTest {
 
     @Test
     public void getters() {
-        CommandResult commandResult = new CommandResult("feedback", false, false, false, false);
+        CommandResult commandResult = new CommandResult("feedback", false, false, false, false, false);
 
         assertEquals(commandResult.isAddVisit(), false);
         assertEquals(commandResult.isDeleteVisit(), false);
