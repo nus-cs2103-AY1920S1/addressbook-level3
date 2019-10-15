cfadc8fdc91e643a8151b20c2dca992d260da901
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-15 17:56:35 +0800

    Add javadocs for TransactionCard

--- src/main/java/seedu/ichifund/ui/TransactionCard.java
+++ src/main/java/seedu/ichifund/ui/TransactionCard.java
@@ -6,6 +6,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.ichifund.model.transaction.Transaction;
 
+/**
+ * An UI component that displays information of a {@code Transaction}.
+ */
 public class TransactionCard extends UiPart<Region> {
     private static final String FXML = "TransactionListCard.fxml";
 
