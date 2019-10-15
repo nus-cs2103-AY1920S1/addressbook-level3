102802d38f5a861dbadd3250222a65b21845937e
Lee Liak Ghee <liakify@gmail.com>
2019-10-16 01:05:24 +0800

    Redo Context and changed it from enum to class, add tests

--- src/main/java/seedu/address/model/Context.java
+++ src/main/java/seedu/address/model/Context.java
@@ -1,36 +1,102 @@
 package seedu.address.model;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import seedu.address.model.activity.Activity;
 import seedu.address.model.person.Person;
 
-
 /**
- * Represents an application context.
+ * Represents an immutable application context.
  */
-public enum Context {
-    MAIN,
-    VIEW_CONTACT,
-    VIEW_ACTIVITY,
-    LIST_CONTACT,
-    LIST_ACTIVITY;
+public class Context {
 
-    private final Optional<Object> model;
-
-    Context() {
-        model = Optional.empty();
+    /**
+     * Represents the various types of contexts that can exist.
+     */
+    enum Type {
+        MAIN,
+        VIEW_CONTACT,
+        VIEW_ACTIVITY,
+        LIST_CONTACT,
+        LIST_ACTIVITY;
     }
 
-    Context(Object model) {
-        this.model = Optional.ofNullable(model);
+    private final Optional<Object> object;
+    private final Type type;
+
+    /**
+     * Default constructor where context type is MAIN.
+     */
+    Context() {
+        object = Optional.empty();
+        type = Type.MAIN;
+    }
+
+    /**
+     * Constructor for a VIEW_ACTIVITY context.
+     */
+    Context(Activity activity) {
+        requireNonNull(activity);
+        object = Optional.ofNullable(activity);
+        type = Type.VIEW_ACTIVITY;
+    }
+
+    /**
+     * Constructor for a VIEW_CONTACT context.
+     */
+    Context(Person person) {
+        requireNonNull(person);
+        object = Optional.ofNullable(person);
+        type = Type.VIEW_CONTACT;
+    }
+
+    /**
+     * Constructor for a LIST_ACTIVITY context.
+     */
+    Context(Activity... activityList) {
+        requireNonNull(activityList);
+        ArrayList<Activity> arr = new ArrayList<>();
+        for (Activity a : activityList) {
+            arr.add(a);
+        }
+        object = Optional.ofNullable(arr);
+        type = Type.LIST_ACTIVITY;
+    }
+
+    /**
+     * Constructor for a LIST_CONTACT context.
+     */
+    Context(Person... personList) {
+        requireNonNull(personList);
+        ArrayList<Person> arr = new ArrayList<>();
+        for (Person p : personList) {
+            arr.add(p);
+        }
+        object = Optional.ofNullable(arr);
+        type = Type.LIST_CONTACT;
+    }
+
+    public Type getType() {
+        return type;
     }
 
     public Optional<Activity> getActivity() {
-        return model.filter(x -> this == Context.VIEW_ACTIVITY).map(x->(Activity) x);
+        return object.filter(x -> type == Type.VIEW_ACTIVITY).map(x->(Activity) x);
     }
 
     public Optional<Person> getContact() {
-        return model.filter(x -> this == Context.VIEW_CONTACT).map(x->(Person) x);
+        return object.filter(x -> type == Type.VIEW_CONTACT).map(x->(Person) x);
+    }
+
+    public Optional<List<Activity>> getActivityList() {
+        return object.filter(x -> type == Type.LIST_ACTIVITY).map(x->(List<Activity>) x);
+    }
+
+    public Optional<List<Person>> getContactList() {
+        return object.filter(x -> type == Type.LIST_CONTACT).map(x->(List<Person>) x);
     }
 }
