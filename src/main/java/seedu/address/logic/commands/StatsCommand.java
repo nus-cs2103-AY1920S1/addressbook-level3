d87e9205285327a2a62950620e28b64b6d9d4b7b
choongyx <yongxin.choong98@gmail.com>
2019-10-16 01:54:47 +0800

    Update StatsCommand Logic and Test to enable calculation of total cost

--- src/main/java/seedu/address/logic/commands/StatsCommand.java
+++ src/main/java/seedu/address/logic/commands/StatsCommand.java
@@ -1,6 +1,11 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.address.model.Model;
+import seedu.address.model.spending.Spending;
 
 /**
  * Displays statistics of the user's spending.
@@ -8,9 +13,19 @@ import seedu.address.model.Model;
 public class StatsCommand extends Command {
 
     public static final String COMMAND_WORD = "stats";
+    public static final String MESSAGE_SUCCESS = "Statistics of all spending displayed below.";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult("ST2334 is the best :)");
+        requireNonNull(model);
+
+        List<Spending> lastShownList = model.getFilteredSpendingList();
+
+        double totalCost = 0;
+        for (Spending i: lastShownList){
+            totalCost += Double.parseDouble(i.getPhone().toString());
+        }
+
+        return new CommandResult(MESSAGE_SUCCESS + "\nTotal Cost: $" + String.format("%.2f", totalCost));
     }
 }
--- src/test/java/seedu/address/logic/commands/StatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatsCommandTest.java
@@ -1,14 +1,20 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalSpendings.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalSpendings.ALICE;
+import static seedu.address.testutil.TypicalSpendings.BENSON;
+import static seedu.address.testutil.TypicalSpendings.CARL;
+import static seedu.address.testutil.TypicalSpendings.DANIEL;
+import static seedu.address.testutil.TypicalSpendings.ELLE;
+import static seedu.address.testutil.TypicalSpendings.FIONA;
+import static seedu.address.testutil.TypicalSpendings.GEORGE;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for StatsCommand.
@@ -18,14 +24,17 @@ class StatsCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_displayStats_success() {
+    public void execute_displayStatsForAll_success() {
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-        StatsCommand statsCommand = new StatsCommand();
+        double totalCost = Double.parseDouble(ALICE.getPhone().toString()) + Double.parseDouble(BENSON.getPhone().toString())
+            + Double.parseDouble(CARL.getPhone().toString()) + Double.parseDouble(DANIEL.getPhone().toString())
+            + Double.parseDouble(ELLE.getPhone().toString()) + Double.parseDouble(FIONA.getPhone().toString())
+            + Double.parseDouble(GEORGE.getPhone().toString());
 
-        String expectedMessage = String.format("ST2334 is the best :)");
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-
-        assertCommandSuccess(statsCommand, model , expectedMessage, expectedModel);
+        String expectedMessage = StatsCommand.MESSAGE_SUCCESS + "\nTotal Cost: $" + String.format("%.2f", totalCost);
+        System.out.println(expectedMessage);
+        assertCommandSuccess(new StatsCommand(), model , expectedMessage, expectedModel);
     }
 
 }
