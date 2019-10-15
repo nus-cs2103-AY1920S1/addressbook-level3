94f0ce1a0e195792bd9160011ace169e4ae9c2ff
chen-xi-cx <chenxi_95@hotmail.com>
2019-10-15 23:04:04 +0800

    Attempt to fix travis check error

--- src/test/java/seedu/address/logic/commands/AverageCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AverageCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.AverageCommand.MESSAGE_NO_RECORD;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,15 +10,11 @@ import seedu.address.logic.commands.exceptions.AverageType;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.RecordBook;
-import seedu.address.model.UserPrefs;
 import seedu.address.model.record.RecordType;
-import seedu.sgm.model.food.FoodMap;
 
 public class AverageCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new FoodMap(), new RecordBook());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new FoodMap(),
-            new RecordBook());
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
 
     @Test
     public void execute_zeroRecordType_throwsCommandException() {
