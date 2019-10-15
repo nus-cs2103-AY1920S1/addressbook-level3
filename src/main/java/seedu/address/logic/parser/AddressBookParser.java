a822d574c3e2e2e8f0463013947bf06a246fec1b
shuiyao-sg <shuiyao@u.nus.edu>
2019-10-16 01:17:21 +0800

    Implement delete command for task

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -21,6 +21,7 @@ import seedu.address.logic.commands.statistics.GetQnsCommand;
 import seedu.address.logic.commands.statistics.GetReportCommand;
 import seedu.address.logic.commands.statistics.GetStatisticsCommand;
 import seedu.address.logic.commands.task.AddTaskForNoteCommand;
+import seedu.address.logic.commands.task.DeleteTaskCommand;
 import seedu.address.logic.commands.task.DoneTaskCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.parser.questionparser.AddQuestionCommandParser;
@@ -29,6 +30,7 @@ import seedu.address.logic.parser.statistics.GetQnsCommandParser;
 import seedu.address.logic.parser.statistics.GetReportCommandParser;
 import seedu.address.logic.parser.statistics.GetStatisticsCommandParser;
 import seedu.address.logic.parser.task.AddTaskForNoteCommandParser;
+import seedu.address.logic.parser.task.DeleteTaskCommandParser;
 import seedu.address.logic.parser.task.DoneTaskCommandParser;
 
 /**
@@ -102,6 +104,9 @@ public class AddressBookParser {
 
         case DoneTaskCommand.COMMAND_WORD:
             return new DoneTaskCommandParser().parse(arguments);
+
+        case DeleteTaskCommand.COMMAND_WORD:
+            return new DeleteTaskCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
