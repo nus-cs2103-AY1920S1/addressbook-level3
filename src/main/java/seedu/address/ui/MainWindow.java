62d0a762e73cc847335bb5598a9486ff790a7a59
dawo5010 <me@dawo.me>
2019-10-15 14:23:31 +0800

    Removed unnecessary UI classes and some code.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -215,10 +215,6 @@ public class MainWindow extends UiPart<Stage> {
         primaryStage.hide();
     }
 
-    //public PersonListPanel getPersonListPanel() {
-    //    return personListPanel;
-    //}
-
     /**
      * Executes the command and returns the result.
      *
