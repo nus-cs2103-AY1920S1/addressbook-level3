82300762edac33f1a2e859a110107e89fa3c84a3
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-15 16:53:37 +0800

    Add getFilteredTransactionList to Logic and LogicManager

--- src/main/java/seedu/ichifund/logic/LogicManager.java
+++ src/main/java/seedu/ichifund/logic/LogicManager.java
@@ -16,6 +16,7 @@ import seedu.ichifund.model.Model;
 import seedu.ichifund.model.ReadOnlyFundBook;
 import seedu.ichifund.model.budget.Budget;
 import seedu.ichifund.model.person.Person;
+import seedu.ichifund.model.transaction.Transaction;
 import seedu.ichifund.storage.Storage;
 
 /**
@@ -60,6 +61,11 @@ public class LogicManager implements Logic {
     @Override
     public ObservableList<Person> getFilteredPersonList() {
         return model.getFilteredPersonList();
+    }
+
+    @Override
+    public ObservableList<Transaction> getFilteredTransactionList() {
+        return model.getFilteredTransactionList();
     }
 
     @Override
