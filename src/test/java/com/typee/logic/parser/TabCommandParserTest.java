45369b65ac44fd6ab4d9d61cd7def8514ece8426
Gihun Ko <rhrlgns96@gmail.com>
2019-10-15 15:29:27 +0800

    adds skeleton unit test case for TabCommandParser

---
+++ src/test/java/com/typee/logic/parser/TabCommandParserTest.java
@@ -0,0 +1,14 @@
+package com.typee.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TabCommandParserTest {
+    private TabCommandParser parser = new TabCommandParser();
+
+    @Test
+    void parse_all_menus_success() {
+        
+    }
+}
