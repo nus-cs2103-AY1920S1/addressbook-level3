0bb620c77508229a23cfe49164f71e09e0a73b3d
TanZhanMing <e0349703@u.nus.edu>
2019-10-15 16:26:49 +0800

    Add basic budget functionality pending the integration of 'branch-integration-claims' branch.

---
+++ src/main/java/seedu/address/logic/commands/BudgetCommand.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+import seedu.address.model.budget.Budget;
+//import seedu.address.model.claim.Claim;
+//import seedu.address.model.income.Income;
+//import java.util.List;
+
+public class BudgetCommand extends Command {
+
+    public static final String COMMAND_WORD = "budget";
+
+    @Override
+    public CommandResult execute(Model model) {
+        //List<Claim> claimList = model.getFilteredClaimList();
+        //List<Income> incomeList = model.getFilteredIncomeList();
+
+        String message;
+
+        //Budget budget = new Budget(claimList, incomeList);
+        Budget budget = new Budget();   //placeholder
+        budget.calculateBudget();
+
+        message = "Total income: "
+                + budget.getTotalIncome()
+                + "\nTotal claim value: "
+                + budget.getTotalExpenses()
+                + "\nBudget: "
+                + budget.getBudgetAmount();
+
+        if (budget.isOverBudget()) {
+            message += "\nWarning, you are over budget!";
+        }
+
+        return new CommandResult(message,false,false);
+    }
+}
