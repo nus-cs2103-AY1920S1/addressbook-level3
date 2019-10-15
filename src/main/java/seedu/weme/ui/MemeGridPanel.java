ccfa139324bf11f125c848162d8f25ca4b2c88bc
Mo Zongran <mozongran@gmail.com>
2019-10-16 01:48:33 +0800

    Remove unfinished listener

--- src/main/java/seedu/weme/ui/MemeGridPanel.java
+++ src/main/java/seedu/weme/ui/MemeGridPanel.java
@@ -2,7 +2,6 @@ package seedu.weme.ui;
 
 import java.util.logging.Logger;
 
-import javafx.collections.MapChangeListener;
 import org.controlsfx.control.GridCell;
 import org.controlsfx.control.GridView;
 
@@ -30,9 +29,6 @@ public class MemeGridPanel extends UiPart<Region> {
         memeGridView.setItems(memeList);
         memeGridView.setCellFactory(listView -> new MemeGridViewCell());
         this.likeData = likeData;
-        likeData.addListener((MapChangeListener<String, Integer>) change -> {
-
-        });
     }
 
     /**
