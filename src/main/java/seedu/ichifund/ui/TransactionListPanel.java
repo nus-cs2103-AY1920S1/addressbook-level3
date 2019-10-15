d9a2ac0478d718e2cbc377cc89d29eca2a74f08e
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-15 16:51:53 +0800

    Add TransactionListPanel and associated fxml

---
+++ src/main/java/seedu/ichifund/ui/TransactionListPanel.java
@@ -0,0 +1,46 @@
+package seedu.ichifund.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.ichifund.commons.core.LogsCenter;
+import seedu.ichifund.model.transaction.Transaction;
+
+/**
+ * Panel containing the list of transactions.
+ */
+public class TransactionListPanel extends UiPart<Region> {
+    private static final String FXML = "TransactionListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TransactionListPanel.class);
+
+    @FXML
+    private ListView<Transaction> transactionListView;
+
+    public TransactionListPanel(ObservableList<Transaction> transactionList) {
+        super(FXML);
+        transactionListView.setItems(transactionList);
+        transactionListView.setCellFactory(listView -> new TransactionListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Transaction} using a {@code TransactionCard}.
+     */
+    class TransactionListViewCell extends ListCell<Transaction> {
+        @Override
+        protected void updateItem(Transaction transaction, boolean empty) {
+            super.updateItem(transaction, empty);
+
+            if (empty || transaction == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new TransactionCard(transaction, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
