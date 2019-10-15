b3a29cc6f0dae18d9f576cd7b65ceddcda309baf
robytanama <tanamaroby@gmail.com>
2019-10-15 22:14:27 +0800

    Carrying the team test coveralls with better checkstyle

--- src/test/java/seedu/savenus/MainAppTest.java
+++ src/test/java/seedu/savenus/MainAppTest.java
@@ -1,17 +1,15 @@
 package seedu.savenus;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.savenus.commons.core.Version;
 
-class MainAppTest {
+public class MainAppTest {
 
-    MainApp mainApp = new MainApp();
+    private MainApp mainApp = new MainApp();
 
     @Test
     public void correctVersion() {
--- src/test/java/seedu/savenus/logic/LogicManagerTest.java
+++ src/test/java/seedu/savenus/logic/LogicManagerTest.java
@@ -1,7 +1,6 @@
 package seedu.savenus.logic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.savenus.commons.core.Messages.MESSAGE_INVALID_FOOD_DISPLAYED_INDEX;
 import static seedu.savenus.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
