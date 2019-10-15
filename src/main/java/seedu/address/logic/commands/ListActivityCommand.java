18b98fe7fcfdf3aabf145762a7292d8a8e9d4d7d
KxxMxxx <kai_1997_ming@hotmail.com>
2019-10-16 06:49:07 +0800

    implement ListActivity
    implementation of ListDayCommand left undone because of unsure access to list of days

--- src/main/java/seedu/address/logic/commands/ListActivityCommand.java
+++ src/main/java/seedu/address/logic/commands/ListActivityCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_ACTIVITIES;
 
 import seedu.address.model.Model;
 
@@ -16,7 +17,7 @@ public class ListActivityCommand extends ListCommand {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        //to be implemented
+        model.updateFilteredActivityList(PREDICATE_SHOW_ALL_ACTIVITIES);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
--- src/main/java/seedu/address/logic/commands/ListContactCommand.java
+++ src/main/java/seedu/address/logic/commands/ListContactCommand.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONTACTS;
 
 import seedu.address.model.Model;
 
@@ -10,14 +10,14 @@ import seedu.address.model.Model;
  */
 public class ListContactCommand extends ListCommand {
 
-    public static final String SECOND_COMMAND_WORD = "person";
+    public static final String SECOND_COMMAND_WORD = "contact";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all contacts";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredContactList(PREDICATE_SHOW_ALL_CONTACTS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
