e838b06dffee9207446cee65cf34a734156b8190
Kenneth Fung <fungchenyu@rocketmail.com>
2019-10-15 12:15:30 +0800

    Fix missing import in AddCommandParserTest

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -20,6 +20,7 @@ import static seedu.address.testutil.TypicalPersons.BOB;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.parser.commandparsers.AddCommandParser;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
