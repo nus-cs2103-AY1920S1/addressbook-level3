05020b11c6d6835b7b8cfc48cdf5dcd53206dced
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-10-16 01:19:20 +0800

    Fix regression in Spending

--- src/main/java/seedu/address/model/spending/Spending.java
+++ src/main/java/seedu/address/model/spending/Spending.java
@@ -27,7 +27,7 @@ public class Spending {
      * Every field must be present and not null.
      */
     public Spending(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, tags);
+        requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
