390034c387f26564ad38fc1239c208290948ab6f
weiboon <lumweiboon@gmail.com>
2019-10-15 21:24:50 +0800

    String Similarity Comparision

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -65,4 +65,46 @@ public class StringUtil {
             return false;
         }
     }
+    /**
+     * Returns true if {@code requiredSimilarityPercentage} is less than the actual similarity between
+     * {@code string1} and {@code string2}
+     * @throws NullPointerException if {@code string1} or {@code string2} is null.
+     */
+    public static boolean isSimilarWord(String string1, String string2, Double requiredSimilarityPercentage) {
+        requireNonNull(string1);
+        requireNonNull(string2);
+
+        Double similarityPercentage = (1.0 / calculateLevenshteinDistance(string1, string2));
+        return similarityPercentage >= requiredSimilarityPercentage;
+    }
+
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
+    }
+
+    private static int calculateLevenshteinDistance(String string1, String string2) {
+        int[][] dp = new int[string1.length() + 1][string2.length() + 1];
+
+        for (int i = 0; i <= string1.length(); i++) {
+            for (int j = 0; j <= string2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1] + costOfSubstitution(string1.charAt(i - 1), string2.charAt(j - 1)),
+                            dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[string1.length()][string2.length()];
+    }
 }
