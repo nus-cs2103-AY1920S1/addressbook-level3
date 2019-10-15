f9e7b69e36d8404e79107bc94d04670321d71aa3
Caleb Goh Ee Gen <sonicheze8@gmail.com>
2019-10-15 13:03:37 +0800

    Added Poster Class

---
+++ src/main/java/seedu/ezwatchlist/model/show/Poster.java
@@ -0,0 +1,26 @@
+package seedu.ezwatchlist.model.show;
+
+import javafx.scene.image.Image;
+
+import java.net.URL;
+
+/**
+ * Represents a Show's name in the watchlist.
+ */
+public class Poster {
+    private static final String PLACEHOLDER_IMAGE = "/images/poster-placeholder.png";
+    public Image image;
+
+
+    /**
+     * Constructs a {@code Poster}.
+     *
+     * @param image A valid image.
+     */
+    public Poster() {
+    }
+
+    public boolean setImage(String imageUrl) {
+        image = new Image(imageUrl);
+    }
+}
