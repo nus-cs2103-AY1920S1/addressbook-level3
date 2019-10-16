4205c148e6453b0a6c45c5b1361103de35d7f6ed
Bill Chee <billchee@comp.nus.edu.sg>
2019-10-16 23:35:47 +0800

    Add StageCommand

--- src/main/java/seedu/weme/logic/parser/MemeParser.java
+++ src/main/java/seedu/weme/logic/parser/MemeParser.java
@@ -12,6 +12,8 @@ import seedu.weme.logic.commands.MemeDeleteCommand;
 import seedu.weme.logic.commands.MemeEditCommand;
 import seedu.weme.logic.commands.MemeFindCommand;
 import seedu.weme.logic.commands.MemeListCommand;
+import seedu.weme.logic.commands.MemeStageCommand;
+
 import seedu.weme.logic.parser.exceptions.ParseException;
 
 /**
@@ -54,6 +56,9 @@ public class MemeParser extends WemeParser {
 
         case MemeListCommand.COMMAND_WORD:
             return new MemeListCommand();
+
+        case MemeStageCommand.COMMAND_WORD:
+            return new MemeStageCommandParser().parse(arguments);
 
         default:
             return super.parseCommand(userInput);
