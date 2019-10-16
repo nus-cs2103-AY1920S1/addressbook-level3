033c8be36e8df96f94b2599f1114108a3a916d99
Jiang Yuxin <jyx11011@gmail.com>
2019-10-16 23:34:52 +0800

    Add event and reminder command.

---
+++ src/main/java/seedu/address/logic/commands/ReminderCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CALENDAR_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CALENDAR_REPETITION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATETIME;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.calendar.CalendarEntry;
+
+public class ReminderCommand extends Command {
+    public static final String COMMAND_WORD = "reminder";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a reminder to the calendar. "
+            + "Parameters: "
+            + PREFIX_CALENDAR_DESCRIPTION + "DESCRIPTION "
+            + PREFIX_DATETIME + "DATETIME "
+            + "[" + PREFIX_CALENDAR_REPETITION + "REPETITION]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_CALENDAR_DESCRIPTION + "Insulin injection "
+            + PREFIX_DATETIME + "31/5/2020 16:00 "
+            + PREFIX_CALENDAR_REPETITION + "everyday";
+
+    public static final String MESSAGE_SUCCESS = "New reminder added: %1$s";
+    public static final String MESSAGE_DUPLICATE_REMINDER = "This reminder already exists in the calendar";
+
+    private final CalendarEntry toAdd;
+
+    /**
+     * Creates an ReminderCommand to add the specified {@code Reminder}
+     */
+    public ReminderCommand(CalendarEntry calendarEntry) {
+        requireNonNull(calendarEntry);
+        toAdd = calendarEntry;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        /*
+        if (model.hasRecord(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_RECORD);
+        }
+
+        model.addRecord(toAdd);
+        */
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd.toString()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReminderCommand // instanceof handles nulls
+                && toAdd.equals(((ReminderCommand) other).toAdd));
+    }
+}
