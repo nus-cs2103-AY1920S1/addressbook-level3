f17a2842c27fc171297f003b7187a9314215ff70
ckb055 <chenkaibin7@gmail.com>
2019-10-16 17:17:44 +0800

    Latest Commits

---
+++ src/main/java/seedu/address/logic/commands/GlobalTagFilterCommand.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+import seedu.address.model.StudyBuddyItemContainsTagPredicate;
+import seedu.address.model.flashcard.FlashcardContainsTagPredicate;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.GLOBAL_TAG_FILTER;
+
+public class GlobalTagFilterCommand {
+
+    public static final String COMMAND_WORD = GLOBAL_TAG_FILTER;
+
+    public static final String MESSAGE_USAGE = "filters every studyBuddy item by a tag." +
+            "example usage : globaltagfilter cs2100";
+
+    public static final String FILTER_TAG_MESSAGE_SUCCESS = "Filter all by tag(s) : ";
+
+    private String[] tagKeywords;
+
+    private final StudyBuddyItemContainsTagPredicate tagPredicate;
+
+    /**
+     * Constructor for filter by tag.
+     * @param predicate to test on an note object to see if it has the tag.
+     * @param tagKeywords the tags provided by user input to test on the note.
+     */
+    public GlobalTagFilterCommand(StudyBuddyItemContainsTagPredicate predicate, String[] tagKeywords) {
+        this.tagPredicate = predicate;
+        this.tagKeywords = tagKeywords;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        return new CommandResult(FILTER_TAG_MESSAGE_SUCCESS + FilterByTagCommand.displayTagKeywords(tagKeywords));
+    }
+}
