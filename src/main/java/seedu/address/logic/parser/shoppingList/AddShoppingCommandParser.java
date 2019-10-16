7512f7bbca5aa6ade0d78cf13adf0c1b0d9c5f8b
ManasVegi <manasvegi@gmail.com>
2019-10-16 13:46:28 +0800

    Implement Shopping Parser classes

---
+++ src/main/java/seedu/address/logic/parser/shoppingList/AddShoppingCommandParser.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.parser.shoppingList;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.shoppingList.AddShoppingCommand;
+import seedu.address.logic.parser.*;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.food.*;
+
+/**
+ * Parses input arguments and creates a new AddCommand object
+ */
+public class AddShoppingCommandParser implements Parser<AddShoppingCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     * @return
+     */
+    public AddShoppingCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_AMOUNT);
+
+        //TO BE CHANGED LATER SINCE SHOULD BE ABLE TO EDIT ONLY AMOUNT
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        Amount amount = ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get());
+
+        ShoppingItem food = new ShoppingItem(name, amount);
+
+        return new AddShoppingCommand(food);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
---
+++ src/main/java/seedu/address/logic/parser/shoppingList/BoughtShoppingCommandParser.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.parser.shoppingList;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.shoppingList.BoughtShoppingCommand;
+import seedu.address.logic.commands.shoppingList.EditShoppingCommand;
+import seedu.address.logic.commands.shoppingList.BoughtShoppingCommand.BoughtShoppingItemDescriptor;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new EditShoppingCommand object
+ */
+public class BoughtShoppingCommandParser implements Parser<BoughtShoppingCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the BoughtShoppingCommand
+     * and returns an BoughtShoppingCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public BoughtShoppingCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_EXPIRY_DATE, PREFIX_AMOUNT);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditShoppingCommand.MESSAGE_USAGE), pe);
+        }
+
+        BoughtShoppingItemDescriptor boughtShoppingItemDescriptor = new BoughtShoppingItemDescriptor();
+        if (argMultimap.getValue(PREFIX_EXPIRY_DATE).isPresent()) {
+            boughtShoppingItemDescriptor.setExpiryDate(
+                    ParserUtil.parseExpiryDate(argMultimap.getValue(PREFIX_EXPIRY_DATE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_AMOUNT).isPresent()) {
+            boughtShoppingItemDescriptor.setAmount(ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get()));
+        }
+
+        return new BoughtShoppingCommand(index, boughtShoppingItemDescriptor);
+    }
+}
