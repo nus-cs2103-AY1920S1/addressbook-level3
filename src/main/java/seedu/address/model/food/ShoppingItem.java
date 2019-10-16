c0733ae1388eadd46bf6d6b722dc9a7f1abc340e
ManasVegi <manasvegi@gmail.com>
2019-10-16 13:44:36 +0800

    Change ShoppingItem class for better functionality

--- src/main/java/seedu/address/model/food/ShoppingItem.java
+++ src/main/java/seedu/address/model/food/ShoppingItem.java
@@ -9,16 +9,41 @@ public class ShoppingItem extends Food{
 
     private final boolean bought;
     private final boolean urgent;
+    private final ExpiryDate expiryDate;
+
+    //empty constructor to be removed after Model integration
+    public ShoppingItem() {
+        super(new Name("ALICE"), new Amount("21"));
+        this.bought = false;
+        this.urgent = false;
+        this.expiryDate = null;
+    }
 
     public ShoppingItem(Name name, Amount amount) {
         super(name, amount);
         bought = false;
         urgent = false;
+        this.expiryDate = null;
     }
-    public ShoppingItem(Name name, Amount amount, boolean isBought, boolean isUrgent) {
+
+    public ShoppingItem(Name name, Amount amount, boolean isUrgent) {
         super(name, amount);
-        bought = isBought;
-        urgent = isUrgent;
+        this.bought = false;
+        this.urgent = isUrgent;
+        this.expiryDate = null;
+    }
+
+    /**
+     * Create a new Shopping item with expiry date when it is marked as bought.
+     * @param name Name of shopping item to be added to grocery list
+     * @param amount Amount of the item
+     * @param expiryDate expiry date of the item when it is added to the grocery list
+     */
+    public ShoppingItem(Name name, Amount amount, ExpiryDate expiryDate) {
+        super(name, amount);
+        this.bought = true;
+        this.urgent = false;
+        this.expiryDate = expiryDate;
     }
 
     public boolean isBought() {
@@ -29,18 +54,22 @@ public class ShoppingItem extends Food{
         return this.urgent;
     }
 
-    public ShoppingItem setBought(boolean bought) {
-        return new ShoppingItem(this.getName(), this.getAmount(), bought, this.urgent);
+    public ExpiryDate getExpiryDate() {
+        return this.expiryDate;
+    }
+
+    public ShoppingItem setBought(ExpiryDate expiryDate) {
+        return new ShoppingItem(this.getName(), this.getAmount(), expiryDate);
     }
 
     public ShoppingItem setUrgent(boolean urgent) {
-        return new ShoppingItem(this.getName(), this.getAmount(), this.bought, urgent);
+        return new ShoppingItem(this.getName(), this.getAmount(), urgent);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(this.getName(), this.getAmount(), this.isBought(), this.isUrgent());
+        return Objects.hash(this.getName(), this.getAmount(), this.isBought(), this.isUrgent(), this.expiryDate);
     }
 
     @Override
