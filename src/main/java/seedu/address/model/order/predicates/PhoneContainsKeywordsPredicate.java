e1bdfe70912c2a90ea16ea72eca704f2d8eddb5d
zhixianggg <zhixiangg.o@gmail.com>
2019-10-16 16:19:23 +0800

    Add CustomerContainsKeywordsPredicate and PhoneContainsKeywordsPredicate

--- src/main/java/seedu/address/model/order/predicates/PhoneContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/order/predicates/PhoneContainsKeywordsPredicate.java
@@ -19,7 +19,13 @@ public class PhoneContainsKeywordsPredicate implements Predicate<Order> {
     @Override
     public boolean test(Order order) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(order.getPhone().toString(), keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(order.getPhone().getSerialNumber().toString(), keyword)
+                        || StringUtil.containsWordIgnoreCase(order.getPhone().getBrand().toString(), keyword)
+                        || StringUtil.containsWordIgnoreCase(order.getPhone().getCapacity().toString(), keyword)
+                        || StringUtil.containsWordIgnoreCase(order.getPhone().getColour().toString(), keyword)
+                        || StringUtil.containsWordIgnoreCase(order.getPhone().getCost().toString(), keyword)
+                        || StringUtil.containsWordIgnoreCase(order.getPhone().getIdentityNumber().toString(), keyword)
+                        || StringUtil.containsWordIgnoreCase(order.getPhone().getPhoneName().toString(), keyword));
     }
 
     @Override
