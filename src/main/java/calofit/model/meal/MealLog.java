b430678573f4e159cf6b88ec97d9481fdf35560b
NgWenJunFelix <felixng97@gmail.com>
2019-10-16 22:36:43 +0800

    Update JavaDocs

--- src/main/java/calofit/model/meal/MealLog.java
+++ src/main/java/calofit/model/meal/MealLog.java
@@ -9,6 +9,8 @@ import javafx.collections.ObservableList;
 
 /**
  * Represents all meals tracked by the application.
+ * Contains the original list of all meals input by the user.
+ * Stores other lists of meals that are generated based on the original list of meals.
  */
 public class MealLog {
     private List<Meal> mealLog = new ArrayList<>();
@@ -17,17 +19,28 @@ public class MealLog {
     private ObservableList<Meal> todayMeals = observableMeals.filtered(MealLog::isMealToday);
     private ObservableList<Meal> currentMonthMeals = observableMeals.filtered(MealLog::isMealThisMonth);
 
+    /**
+     * Checks if the Meal object is created today.
+     * @param meal the Meal to be tested
+     * @return the boolean representing whether the meal is created today.
+     */
     private static boolean isMealToday(Meal meal) {
         return meal.getTimestamp()
             .getDateTime().toLocalDate()
             .equals(LocalDate.now());
     }
 
+    /**
+     * Checks if the Meal object is created this month.
+     * @param meal is the Meal to be tested
+     * @return the boolean representing whether the Meal is created in this month.
+     */
     private static boolean isMealThisMonth(Meal meal) {
         return meal.getTimestamp()
                 .getDateTime().toLocalDate().getMonth()
                 .equals(LocalDate.now().getMonth());
     }
+
     /**
      * Get a list of meals eaten by the user.
      * @return Meal list
@@ -54,10 +67,18 @@ public class MealLog {
         return observableMeals.remove(meal);
     }
 
+    /**
+     * Gets the list of meals eaten by the user today.
+     * @return the filtered Meal List that checks the TimeStamp of each Meal.
+     */
     public ObservableList<Meal> getTodayMeals() {
         return todayMeals;
     }
 
+    /**
+     * Gets the list of meals eaten by the user this month.
+     * @return the filtered Meal List that checks the TimeStamp of each Meal.
+     */
     public ObservableList<Meal> getCurrentMonthMeals() {
         return currentMonthMeals;
     }
