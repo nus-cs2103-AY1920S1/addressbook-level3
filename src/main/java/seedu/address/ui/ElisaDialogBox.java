5b50a1173a8ffe02efb982854370b8d36bf23d4c
Icesiolz <Icesiolz@users.noreply.github.com>
2019-10-16 10:32:16 +0800

    Change Elisa dialog color and clicking of tabs --> show command

--- src/main/java/seedu/address/ui/ElisaDialogBox.java
+++ src/main/java/seedu/address/ui/ElisaDialogBox.java
@@ -1,21 +1,11 @@
 package seedu.address.ui;
 
 import java.io.IOException;
-import java.util.Collections;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 
 /**
  * This class is the Ui for UserDialogBox
