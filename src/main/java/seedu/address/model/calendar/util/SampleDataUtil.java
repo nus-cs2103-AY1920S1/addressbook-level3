560d8536ab8d43fb71c4c2457d9d59bc86581e41
joannasara <joannasarasusan@gmail.com>
2019-10-16 19:52:02 +0800

    Update sample data

--- src/main/java/seedu/address/model/calendar/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/calendar/util/SampleDataUtil.java
@@ -19,27 +19,9 @@ import seedu.address.model.calendar.tag.TaskTag;
 public class SampleDataUtil {
     public static Task[] getSamplePersons() {
         return new Task[] {
-            new Task(new TaskTitle("Alex Yeoh"), new TaskTime("87438807"),
-                    new TaskDescription("alexyeoh@example.com"),
-                    new TaskPlace("Blk 30 Geylang Street 29, #06-40"), getTagSet("friends")),
-            new Task(new TaskTitle("Bernice Yu"), new TaskTime("99272758"),
-                    new TaskDescription("berniceyu@example.com"),
-                    new TaskPlace("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    getTagSet("colleagues", "friends")),
-            new Task(new TaskTitle("Charlotte Oliveiro"), new TaskTime("93210283"),
-                    new TaskDescription("charlotte@example.com"),
-                    new TaskPlace("Blk 11 Ang Mo Kio Street 74, #11-04"), getTagSet("neighbours")),
-            new Task(new TaskTitle("David Li"), new TaskTime("91031282"),
-                    new TaskDescription("lidavid@example.com"),
-                    new TaskPlace("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("family")),
-            new Task(new TaskTitle("Irfan Ibrahim"), new TaskTime("92492021"),
-                    new TaskDescription("irfan@example.com"),
-                    new TaskPlace("Blk 47 Tampines Street 20, #17-35"), getTagSet("classmates")),
-            new Task(new TaskTitle("Roy Balakrishnan"), new TaskTime("92624417"),
-                    new TaskDescription("royb@example.com"),
-                    new TaskPlace("Blk 45 Aljunied Street 85, #11-31"),
-                    getTagSet("colleagues"))
+            new Task(new TaskTitle("CS2100 Assignment"), new TaskTime("31 December 2019"),
+                    new TaskDescription("Hardcopy only"),
+                    new TaskPlace("Blk 30 Geylang Street 29, #06-40"), getTagSet("CS2100"))
         };
     }
 
