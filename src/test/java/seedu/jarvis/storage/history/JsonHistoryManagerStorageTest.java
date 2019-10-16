5c2fd315b3b4d3f6c482dce05ed33f0a30d56321
marcfyk <marcjohnfong@gmail.com>
2019-10-16 22:58:13 +0800

    Update tests for JsonHistoryManagerStorage

--- src/test/java/seedu/jarvis/storage/history/JsonHistoryManagerStorageTest.java
+++ src/test/java/seedu/jarvis/storage/history/JsonHistoryManagerStorageTest.java
@@ -3,6 +3,7 @@ package seedu.jarvis.storage.history;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.jarvis.testutil.Assert.assertThrows;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
@@ -13,12 +14,24 @@ import org.junit.jupiter.api.io.TempDir;
 import seedu.jarvis.commons.exceptions.DataConversionException;
 import seedu.jarvis.model.history.HistoryManager;
 
+/**
+ * Tests the behaviour of {@code JsonHistoryManagerStorage}.
+ */
 public class JsonHistoryManagerStorageTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data",
             "JsonHistoryManagerStorageTest");
 
     @TempDir
     public Path testFolder;
+
+    private void saveAddressBook(HistoryManager historyManager, String filePath) {
+        try {
+            new JsonHistoryManagerStorage(Paths.get(filePath))
+                    .saveHistoryManager(historyManager, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
 
     private Optional<HistoryManager> readHistoryManager(String filePath) throws Exception {
         return new JsonHistoryManagerStorage(Paths.get(filePath)).readHistoryManager(
