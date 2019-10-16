8b30e62d8851d8e512246da4bfbdd72a3e958974
Cheryl <cheryl.nqj@gmail.com>
2019-10-16 21:26:58 +0800

    Fix travisbot checkstyle issues

--- src/test/java/seedu/address/logic/commands/ListIncidentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListIncidentCommandTest.java
@@ -2,8 +2,8 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ENTITY;
 import static seedu.address.testutil.TypicalEntities.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ENTITY;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
