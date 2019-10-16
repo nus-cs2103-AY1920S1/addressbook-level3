666088b7a0ebe3bff8a43370487b7704be155488
LimYiJie <e0325578@u.nus.edu>
2019-10-16 22:32:02 +0800

    Merged changes from upstream repo

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,7 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("tag/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
-
+    public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
     public static final Prefix PREFIX_LINK = new Prefix("l/");
     public static final Prefix PREFIX_SEMESTER = new Prefix("s/");
     public static final Prefix PREFIX_ACAD_YEAR = new Prefix("ay/");
