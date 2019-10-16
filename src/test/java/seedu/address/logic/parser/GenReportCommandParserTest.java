2e526f695df23787e946bf79c4e48c6a3ac272da
Bernice Chio <bernicechiohuiyin@gmail.com>
2019-10-16 12:59:17 +0800

    Fix checkstyle violation

--- src/test/java/seedu/address/logic/parser/GenReportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/GenReportCommandParserTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIdentificationNumbers.FIRST_BODY_ID_NUM;
 
 import org.junit.jupiter.api.Test;
