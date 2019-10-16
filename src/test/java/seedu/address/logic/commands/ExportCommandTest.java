8026cf69802fc722465753d6255c981529bac478
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-10-16 15:28:40 +0800

    Remove a platform-dependent test that fails on UNIX (but passes on Windows)

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -4,13 +4,10 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_EXPORT_IO_EXCEPTION;
-import static seedu.address.logic.commands.CommandTestUtil.INVALID_FILE_PATH;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CATEGORY_HISTORY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CATEGORY_LOCATION;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FILE_PATH_1;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FILE_PATH_2;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalFlashCards.getTypicalAddressBook;
 
@@ -29,12 +26,10 @@ import seedu.address.model.category.Category;
  */
 public class ExportCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     private FilePath firstFilePath = new FilePath(VALID_FILE_PATH_1);
     private FilePath secondFilePath = new FilePath(VALID_FILE_PATH_2);
     private FilePath thirdFilePath = new FilePath(VALID_FILE_PATH_2);
-    private FilePath invalidFilePath = new FilePath(INVALID_FILE_PATH);
     private Category firstCategory = new Category(VALID_CATEGORY_HISTORY);
     private Category secondCategory = new Category(VALID_CATEGORY_LOCATION);
     private Category thirdCategory = new Category(VALID_CATEGORY_LOCATION);
@@ -43,8 +38,6 @@ public class ExportCommandTest {
     private ExportCommand secondCommand = new ExportCommand(secondCategory, secondFilePath);
     private ExportCommand thirdCommand = new ExportCommand(thirdCategory, thirdFilePath);
     private ExportCommand fourthCommand = new ExportCommand(firstCategory, secondFilePath);
-
-    private ExportCommand invalidFilePathCommand = new ExportCommand(firstCategory, invalidFilePath);
 
     @Test
     public void equals() {
@@ -66,15 +59,6 @@ public class ExportCommandTest {
 
         // returns false if both fields not the same
         assertFalse(firstCommand.equals(secondCommand));
-    }
-
-    @Test
-    public void execute_invalidFilePath_throwsException() {
-        String expectedMessage = MESSAGE_EXPORT_IO_EXCEPTION;
-        assertCommandFailure(invalidFilePathCommand, model, expectedMessage);
-        assertFalse(
-                isFilePresent(invalidFilePath)
-        );
     }
 
     @Test
