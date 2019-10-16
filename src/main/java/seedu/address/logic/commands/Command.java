cccf163e6bd7230de23d75c2955cff6d065b71fd
rongrongrr <jingrong.contact@gmail.com>
2019-10-16 23:25:45 +0800

    Comment out draft code to fix Travis Failure

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -18,13 +18,13 @@ public abstract class Command {
      */
     public abstract CommandResult execute(Model model) throws CommandException;
 
-    /**
-     * Executes the command and returns the result message.
-     *
-     * @param model {@code Model} which the command should operate on.
-     * @param history {@code CommandHistory} which the command should operate on.
-     * @return feedback message of the operation result for display
-     * @throws CommandException If an error occurs during command execution.
+    /*
+     Executes the command and returns the result message.
+
+     @param model {@code Model} which the command should operate on.
+     @param history {@code CommandHistory} which the command should operate on.
+     @return feedback message of the operation result for display
+     @throws CommandException If an error occurs during command execution.
      */
 
     //public abstract CommandResult execute(Model model, CommandHistory history) throws CommandException;
