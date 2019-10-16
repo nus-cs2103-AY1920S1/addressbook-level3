066bbd7ac22e8d2303efb3775e2be2ce7c920f13
zhixianggg <zhixiangg.o@gmail.com>
2019-10-16 14:53:52 +0800

    Add ContactNumberContainsKeywordsPredicate

---
+++ src/main/java/seedu/address/model/customer/predicates/ContactNumberContainsKeywordsPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.customer.predicates;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.customer.Customer;
+
+/**
+ * Tests that a {@code Customer}'s {@code Name} matches any of the keywords given.
+ */
+public class ContactNumberContainsKeywordsPredicate implements Predicate<Customer> {
+    private final List<String> keywords;
+
+    public ContactNumberContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Customer customer) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(customer.getContactNumber().value, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ContactNumberContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((ContactNumberContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
