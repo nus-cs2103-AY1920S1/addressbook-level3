5bc8d1e43924f08a2e1871709efc00b208e3febf
hellodommy <dominique@u.nus.edu>
2019-10-16 23:50:15 +0800

    Add JavaDoc comments

--- src/main/java/seedu/address/model/performance/Event.java
+++ src/main/java/seedu/address/model/performance/Event.java
@@ -4,10 +4,18 @@ import java.util.TreeMap;
 
 import seedu.address.model.person.Person;
 
+/**
+ * Events are types of activities that can measure performance. Examples include '50m breaststroke' or '100m freestyle'.
+ */
 public class Event {
+
     private String name;
     private TreeMap<Person, PerformanceEntry> performances;
 
+    /**
+     * Creates a type of event that stores the members and their respective timings (performance) for this event.
+     * @param name Name of this event.
+     */
     public Event(String name) {
         this.name = name;
         this.performances = new TreeMap<>();
--- src/main/java/seedu/address/model/performance/PerformanceEntry.java
+++ src/main/java/seedu/address/model/performance/PerformanceEntry.java
@@ -1,14 +1,27 @@
 package seedu.address.model.performance;
 
+/**
+ * Under each event, each member will have a PerformanceEntry that contains their performance timing and the date
+ * that they took
+ */
 public class PerformanceEntry {
     private String date;
     private double time;
 
+    /**
+     * Creates a PerformanceEntry for a member under an event.
+     * @param date Date this entry was taken.
+     * @param time Timing of this entry.
+     */
     public PerformanceEntry(String date, double time) {
         this.date = date;
         this.time = time;
     }
 
+    /**
+     * Retrieves the timing of this PerformanceEntry.
+     * @return Timing of this PerformanceEntry.
+     */
     public double getTime() {
         return time;
     }
