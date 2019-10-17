49d15e8dd9cef6a85af8f7d49009075b8f092f6a
jun-ha0 <e0309667@u.nus.edu>
2019-10-17 16:59:49 +0800

    Implement typing game skeleton using JavaFX

---
+++ src/main/java/com/typee/ui/game/StartWindow.java
@@ -0,0 +1,28 @@
+package com.typee.ui.game;
+
+import com.typee.ui.UiPart;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+
+/**
+ * Controller class which handles Start window.
+ */
+public class StartWindow extends UiPart<Region> {
+
+    public static final String FXML = "StartWindow.fxml";
+
+    @FXML
+    private AnchorPane startWindow;
+
+    public StartWindow() {
+        super(FXML);
+    }
+
+    @FXML
+    private void handleStart() {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.show();
+    }
+}
