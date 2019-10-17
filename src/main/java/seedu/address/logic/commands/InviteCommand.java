14431d033d917cb804aeaff46f5c4ffd85961e63
JohnNzj <johnng1@hotmail.com>
2019-10-18 01:14:48 +0800

    Modify Messages, InviteCommand & InviteCommandParser class

--- src/main/java/seedu/address/logic/commands/InviteCommand.java
+++ src/main/java/seedu/address/logic/commands/InviteCommand.java
@@ -2,30 +2,75 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARTICIPANT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.ActivityBook;
 import seedu.address.model.Model;
+import seedu.address.model.activity.Activity;
+import seedu.address.model.activity.Title;
+import seedu.address.model.person.Person;
 
 public class InviteCommand extends Command {
 
     public static final String COMMAND_WORD = "invite";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Invites participant(s) into current viewed Activity.\n"
+            + "Parameters: "
+            + "[" + PREFIX_PARTICIPANT + "PARTICIPANT]...\n"
+            + "Example: invite p/Ben p/David";
 
+    public static final String MESSAGE_INVITE_PERSON_SUCCESS = "Invited Person: %1$s";
+//    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This contact already exists in the activity.";
+
+    private final Index index;
     private final List<String> participants;
 
-    public InviteCommand(List<String> participants) {
-        requireAllNonNull(participants);
+    public InviteCommand(Index index, List<String> participants) {
+        requireNonNull(index);
+        requireNonNull(participants);
+        this.index = index;
         this.participants = participants;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        ActivityBook book = model.getActivityBook();
+        List<Activity> activityList = book.getActivityList();
+
+        if (index.getZeroBased() >= activityList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_ACTIVITY_DISPLAYED_INDEX);
+        }
+
+        Activity activityToInvite = activityList.get(index.getZeroBased());
+        Title title = activityToInvite.getTitle();
+        ArrayList<Integer> participantIds = activityToInvite.getParticipantIds();
 
 
+        //////
+
+
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof InviteCommand // instanceof handles nulls
+                && participants.equals(((InviteCommand) other).participants));
     }
 
 }
--- src/main/java/seedu/address/logic/parser/InviteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/InviteCommandParser.java
@@ -1,6 +1,14 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARTICIPANT;
+
+import java.util.List;
 import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ActivityCommand;
 
 import seedu.address.logic.commands.InviteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -8,7 +16,25 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class InviteCommandParser implements Parser<InviteCommand> {
 
     public InviteCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_PARTICIPANT);
 
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, InviteCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_PARTICIPANT)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, InviteCommand.MESSAGE_USAGE));
+        }
+
+        List<String> participants = argMultimap.getAllValues(PREFIX_PARTICIPANT);
+
+        return new InviteCommand(index, participants);
     }
 
     /**
