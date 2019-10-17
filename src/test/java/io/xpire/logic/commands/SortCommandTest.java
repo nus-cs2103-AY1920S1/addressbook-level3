a8249c87d673b4b86d8d442ce8e08f4af411abec
febee99 <febee99@gmail.com>
2019-10-17 23:33:00 +0800

    Remove SortCommandTest updates from this PR

--- src/test/java/io/xpire/logic/commands/SortCommandTest.java
+++ src/test/java/io/xpire/logic/commands/SortCommandTest.java
@@ -1,8 +1,5 @@
 package io.xpire.logic.commands;
 
-import static io.xpire.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static io.xpire.logic.commands.CommandTestUtil.showItemAtIndex;
-import static io.xpire.testutil.TypicalIndexes.INDEX_FIRST_ITEM;
 import static io.xpire.testutil.TypicalItems.getTypicalExpiryDateTracker;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import io.xpire.model.Model;
 import io.xpire.model.ModelManager;
 import io.xpire.model.UserPrefs;
-import org.junit.jupiter.api.Test;
 
 
 /**
@@ -28,16 +24,4 @@ public class SortCommandTest {
         model = new ModelManager(getTypicalExpiryDateTracker(), new UserPrefs());
         expectedModel = new ModelManager(model.getXpire(), new UserPrefs());
     }
-
-    @Test
-    public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ViewCommand(), model, ViewCommand.MESSAGE_SUCCESS, expectedModel);
-    }
-
-    @Test
-    public void execute_listIsFiltered_showsEverything() {
-        showItemAtIndex(model, INDEX_FIRST_ITEM);
-        assertCommandSuccess(new ViewCommand(), model, ViewCommand.MESSAGE_SUCCESS, expectedModel);
-    }
-
 }
