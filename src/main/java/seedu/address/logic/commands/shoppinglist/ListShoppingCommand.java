2ab1e55323b090d224947622ffc66dcd636c35ce
ManasVegi <manasvegi@gmail.com>
2019-10-17 22:41:52 +0800

    Make ShoppingCommands work with new model

--- src/main/java/seedu/address/logic/commands/shoppinglist/ListShoppingCommand.java
+++ src/main/java/seedu/address/logic/commands/shoppinglist/ListShoppingCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands.shoppinglist;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SHOPPING_ITEMS;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
@@ -19,7 +20,7 @@ public class ListShoppingCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        //model.updateFilteredGroceryItemList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredShoppingList(PREDICATE_SHOW_ALL_SHOPPING_ITEMS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
