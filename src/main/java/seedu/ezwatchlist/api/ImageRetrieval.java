2aeaa2eb72f4667b0651ca77e8789e7441291d29
tswuxia <tswuxia@gmail.com>
2019-10-17 21:23:50 +0800

    Trying to fix travis failure for ApiMain.java

--- src/main/java/seedu/ezwatchlist/api/ImageRetrieval.java
+++ src/main/java/seedu/ezwatchlist/api/ImageRetrieval.java
@@ -1,11 +1,5 @@
 package seedu.ezwatchlist.api;
 
-import info.movito.themoviedbapi.TmdbApi;
-import info.movito.themoviedbapi.model.config.TmdbConfiguration;
-import seedu.ezwatchlist.model.Model;
-import seedu.ezwatchlist.model.UserPrefs;
-import seedu.ezwatchlist.storage.UserPrefsStorage;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.model.config.TmdbConfiguration;
+
+/**
+ * A class for the image retrieval.
+ */
 public class ImageRetrieval {
     private static final String DEFAULT_FILE_SIZE = "w500";
     private final String IMAGE_CACHE_LOCATION;
