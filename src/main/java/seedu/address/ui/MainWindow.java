AY1920S1-CS2103T-F11-3/main

Integrate UI with password model

src/main/java/seedu/address/ui/MainWindow.java
@@ -32,6 +32,7 @@ public class MainWindow extends UiPart<Stage> {
 
     // Independent Ui parts residing in this Ui container
     private PersonListPanel personListPanel;
+    private PasswordListPanel objectListPanel;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
 
@@ -43,6 +44,9 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane personListPanelPlaceholder;
+
+    @FXML
+    private StackPane objectListPanelPlaceholder;
 
     @FXML
     private StackPane resultDisplayPlaceholder;
@@ -119,6 +123,24 @@ public class MainWindow extends UiPart<Stage> {
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
     }
+
+    /**
+     * Fills up all the placeholders of this window.
+     */
+    void fillInnerPartsObject() {
+        objectListPanel = new PasswordListPanel(logic.getFilteredPasswordList());
+        objectListPanelPlaceholder.getChildren().add(objectListPanel.getRoot());
+
+        resultDisplay = new ResultDisplay();
+        resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
+
+        StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
+        statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
+
+        CommandBox commandBox = new CommandBox(this::executeCommand);
+        commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+    }
+
 
     /**
      * Sets the default size based on {@code guiSettings}.

nus-cs2103-AY1920S1/addressbook-level3

src/main/java/seedu/address/ui/MainWindow.java
@@ -35,6 +35,7 @@ public class MainWindow extends UiPart<Stage> {
     private FileListPanel fileListPanel;
     private CardListPanel cardListPanel;
     private NoteListPanel noteListPanel;
+    private PasswordListPanel passwordListPanel;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
     private EditObjectWindow editObjectWindow;
@@ -46,6 +47,9 @@ public class MainWindow extends UiPart<Stage> {
     private MenuItem helpMenuItem;
 
     @FXML
+    private StackPane objectListPanelPlaceholder;
+
+    @FXML
     private StackPane personListPanelPlaceholder;
 
     @FXML
@@ -55,7 +59,7 @@ public class MainWindow extends UiPart<Stage> {
     private StackPane noteListPanelPlaceholder;
 
     @FXML
-    private StackPane objectListPanelPlaceholder;
+    private StackPane passwordListPanelPlaceholder;
 
     @FXML
     private StackPane resultDisplayPlaceholder;
@@ -124,7 +128,7 @@ public class MainWindow extends UiPart<Stage> {
     void fillInnerParts() {
         fillInnerPartsWithMode();
         personListPanel = new PersonListPanel(logic.getFilteredPersonList());
-        objectListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+        passwordListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
         fillInnerPartsWithMode();
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
@@ -153,6 +157,9 @@ public class MainWindow extends UiPart<Stage> {
             noteListPanel = new NoteListPanel(logic.getFilteredNoteList());
             objectListPanelPlaceholder.getChildren().add(noteListPanel.getRoot());
             break;
+        case "password":
+            passwordListPanel = new PasswordListPanel(logic.getFilteredPasswordList());
+            objectListPanelPlaceholder.getChildren().add(noteListPanel.getRoot());
         default:
             personListPanel = new PersonListPanel(logic.getFilteredPersonList());
             objectListPanelPlaceholder.getChildren().add(personListPanel.getRoot());