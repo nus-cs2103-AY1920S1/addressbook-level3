2ef4b899defbfe24cc7cfc0d4ae4de8a1183b3da
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-10-17 20:50:51 +0800

    Create new DirectoryPath class

---
+++ src/main/java/seedu/address/model/export/DirectoryPath.java
@@ -0,0 +1,66 @@
+//@@author LeowWB
+
+package seedu.address.model.export;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Represents a directory path.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDirectoryPath(String)}
+ */
+public class DirectoryPath {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Directory path may only consist of alphanumeric characters, spaces, and the following characters:\n"
+                    + "~\\/-_!:[]()\n";
+
+    /*
+     * The following characters are allowed (in addition to alphanumeric):
+     * ~\/-_!:[]()
+     * Space is allowed.
+     */
+    public static final String VALIDATION_REGEX = "[~\\w\\-!:\\[\\]()/\\\\ ]+\\";
+
+    private final Path path;
+
+    /**
+     * Constructs a {@code DirectoryPath}.
+     *
+     * @param directoryPath A valid DirectoryPath.
+     */
+    public DirectoryPath(String directoryPath) {
+        requireNonNull(directoryPath);
+        checkArgument(isValidDirectoryPath(directoryPath), MESSAGE_CONSTRAINTS);
+        path = Paths.get(directoryPath);
+    }
+
+    /**
+     * Returns true if a given string is a valid DirectoryPath.
+     */
+    public static boolean isValidDirectoryPath(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return path.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DirectoryPath // instanceof handles nulls
+                && path.equals(((DirectoryPath) other).path)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return path.hashCode();
+    }
+
+}
