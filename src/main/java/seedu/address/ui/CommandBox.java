1e8272680a83742933d88f800945c61e9e6f84ac
sreesubbash <33561308+sreesubbash@users.noreply.github.com>
2019-10-17 14:37:38 +0800

    Add listener to detect changes in command box

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -26,6 +26,9 @@ public class CommandBox extends UiPart<Region> {
         this.commandExecutor = commandExecutor;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+        commandTextField.textProperty().addListener((observable, oldCommand, newCommand) -> {
+            System.out.println("command changing from " + oldCommand + " to " + newCommand);
+        });
     }
 
 
