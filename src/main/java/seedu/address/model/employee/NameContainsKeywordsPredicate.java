dcadf3090ca3ef8bb7e5596bb9a3c5432ebb771c
DivineDX <chia.dexun97@gmail.com>
2019-10-18 17:26:07 +0800

    Rename file/package

---
+++ src/main/java/seedu/address/model/employee/NameContainsKeywordsPredicate.java
@@ -0,0 +1,31 @@
+package seedu.address.model.employee;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Employee}'s {@code EmployeeName} matches any of the keywords given.
+ */
+public class NameContainsKeywordsPredicate implements Predicate<Employee> {
+    private final List<String> keywords;
+
+    public NameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Employee employee) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(employee.getEmployeeName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
