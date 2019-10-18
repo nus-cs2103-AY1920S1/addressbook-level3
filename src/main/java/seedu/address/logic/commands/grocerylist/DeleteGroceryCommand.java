847fae2e21805bda24b37d23e23667134232cfed
zhangxuan97 <zxrox@yahoo.com.sg>
2019-10-18 16:10:01 +0800

    Modify delete command: only move to waste list if amount is not fully used

--- src/main/java/seedu/address/logic/commands/grocerylist/DeleteGroceryCommand.java
+++ src/main/java/seedu/address/logic/commands/grocerylist/DeleteGroceryCommand.java
@@ -10,6 +10,7 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.food.Amount;
 import seedu.address.model.food.GroceryItem;
 
 /**
@@ -42,10 +43,13 @@ public class DeleteGroceryCommand extends Command {
         }
 
         GroceryItem groceryItemToDelete = lastShownList.get(targetIndex.getZeroBased());
-
-        model.addWasteItem(groceryItemToDelete);
+        boolean isFullyUsed = groceryItemToDelete.isEmpty();
+        if (!isFullyUsed) {
+            model.addWasteItem(groceryItemToDelete);
+        }
 
         model.deleteGroceryItem(groceryItemToDelete);
+        
         CommandResult result = new CommandResult(String.format(MESSAGE_DELETE_GROCERY_ITEM_SUCCESS,
                 groceryItemToDelete));
         result.setWastelistCommand();
