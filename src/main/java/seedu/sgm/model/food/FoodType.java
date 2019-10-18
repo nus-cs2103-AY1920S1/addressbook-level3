f1e2bdf96e0b4202e284deb332f93db1ad486b07
ZhangHuafan <e0261883@u.nus.edu>
2019-10-18 23:46:02 +0800

    Add Json file for food

--- src/main/java/seedu/sgm/model/food/FoodType.java
+++ src/main/java/seedu/sgm/model/food/FoodType.java
@@ -14,12 +14,32 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Represents all useful food types for categorizing food recommendations.
  */
 public enum FoodType {
-    NON_STARCHY_VEGETABLE,
-    STARCHY_VEGETABLE,
-    FRUIT,
-    PROTEIN,
-    SNACK,
-    MEAL;
+    NON_STARCHY_VEGETABLE("Non-starchy vegetable", ABBR_NON_STARCHY_VEGETABLE),
+    STARCHY_VEGETABLE("Starchy vegetable", ABBR_STARCHY_VEGETABLE),
+    FRUIT("Fruit", ABBR_FRUIT),
+    PROTEIN("Protein", ABBR_PROTEIN),
+    SNACK("Snack", ABBR_SNACK),
+    MEAL("Meal", ABBR_MEAL);
+
+    private String type;
+    private String shortHand;
+
+    public static final String MESSAGE_CONSTRAINTS = "Food types should only be one of the following"
+        + "nsv(Non-starchy vegetable); sv(Starchy vegetable); f(Fruit); p(Protein); s(Snack); m(Meal) ";
+
+    FoodType(String type, String shortHand) {
+        this.type = type;
+        this.shortHand = shortHand;
+    }
+
+
+    public String getTypeName() {
+        return type;
+    }
+
+    public String getShortHand() {
+        return shortHand;
+    }
 
     public static FoodType getFrom(String shortHandType) throws ParseException {
         switch (shortHandType.toLowerCase()) {
@@ -45,5 +65,4 @@ public enum FoodType {
             throw new ParseException("Invalid food type");
         }
     }
-
 }
