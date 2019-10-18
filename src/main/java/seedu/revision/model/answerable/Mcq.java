5f222907d1d3c430fff96c9470ae92f943ac7382
Wilfred Bradley Tan <wilfredbtan@gmail.com>
2019-10-18 10:10:42 +0800

    v1.2

--- src/main/java/seedu/revision/model/answerable/Mcq.java
+++ src/main/java/seedu/revision/model/answerable/Mcq.java
@@ -2,6 +2,7 @@ package seedu.revision.model.answerable;
 
 import seedu.revision.model.category.Category;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import static seedu.revision.commons.util.CollectionUtil.requireAllNonNull;
@@ -24,6 +25,30 @@ public class Mcq extends Answerable {
     //TODO: Update functionality
     public boolean isCorrect() {
         return true;
+    }
+
+    public String toString() {
+        String correctAnswers;
+        String wrongAnswers;
+        if (wrongAnswerSet.isEmpty()) {
+            correctAnswers = "Correct Answers: " + Arrays.toString(correctAnswerSet.toArray());
+            wrongAnswers = "";
+        } else {
+            correctAnswers = "Correct Answers: " + Arrays.toString(correctAnswerSet.toArray());
+            wrongAnswers = "Wrong Answers: " + Arrays.toString(wrongAnswerSet.toArray());
+        }
+
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Question: ")
+                .append(getQuestion())
+                .append(" Answers: ")
+                .append(correctAnswers)
+                .append(wrongAnswers)
+                .append(" Difficulty: ")
+                .append(getDifficulty())
+                .append(" Categories: ");
+        getCategories().forEach(builder::append);
+        return builder.toString();
     }
 
 }
