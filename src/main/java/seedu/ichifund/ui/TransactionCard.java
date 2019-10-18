c88e4335722e9ef17a7c9502602e7f4baaa6a795
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-19 03:12:42 +0800

    Update TransactionCard GUI for smaller cells, repositioned fields, conditional amount colour

--- src/main/java/seedu/ichifund/ui/TransactionCard.java
+++ src/main/java/seedu/ichifund/ui/TransactionCard.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Paint;
 import seedu.ichifund.model.transaction.Transaction;
 
 /**
@@ -34,8 +35,7 @@ public class TransactionCard extends UiPart<Region> {
     private Label date;
     @FXML
     private Label category;
-    @FXML
-    private Label transactionType;
+
 
     public TransactionCard(Transaction transaction, int displayedIndex) {
         super(FXML);
@@ -43,9 +43,13 @@ public class TransactionCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         description.setText(transaction.getDescription().toString());
         amount.setText("$" + transaction.getAmount().toString());
-        date.setText(transaction.getDate().toString());
+        date.setText(transaction.getDate().toFullString());
         category.setText(transaction.getCategory().toString());
-        transactionType.setText(transaction.getTransactionType().toString());
+        if(transaction.isExpenditure()) {
+            amount.setTextFill(Paint.valueOf("#ff7675"));
+        } else {
+            amount.setTextFill(Paint.valueOf("#81ecec"));
+        }
     }
 
     @Override
