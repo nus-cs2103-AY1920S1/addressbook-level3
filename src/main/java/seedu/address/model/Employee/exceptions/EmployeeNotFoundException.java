b5d41886c807f7201c42b5a4a5621ae906d0f398
DivineDX <chia.dexun97@gmail.com>
2019-10-18 17:43:06 +0800

    Rename package

--- src/main/java/seedu/address/model/Employee/exceptions/EmployeeNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.employee.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified employee.
- */
-public class EmployeeNotFoundException extends RuntimeException {}
