7dc03631c35201b3b29a9bfa837e4a4b7004577b
joloong <joelloong@gmail.com>
2019-10-19 16:57:10 +0800

    Build FilterCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.transaction.TransactionContainsTagsPredicate;
+
+
+public class FilterCommandParserTest {
+
+    private FilterCommandParser parser = new FilterCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFilterCommand() {
+        // no leading and trailing whitespaces
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new TransactionContainsTagsPredicate("Alice"));
+        assertParseSuccess(parser, "Alice", expectedFilterCommand);
+
+        // multiple whitespaces between keywords
+        // assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFilterCommand);
+    }
+
+}
