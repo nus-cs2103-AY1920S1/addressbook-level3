f48dafdada6706226c4074a591d276b529f62482
joloong <joelloong@gmail.com>
2019-10-19 16:49:21 +0800

    Add one more test case for FilterCommandTest

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -3,9 +3,14 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_TRANSACTIONS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalTransactions.getTypicalBankAccount;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -45,5 +50,22 @@ public class FilterCommandTest {
 
         // different person -> returns false
         assertFalse(filterFirstCommand.equals(filterSecondCommand));
+    }
+
+    @Test
+    public void execute_zeroKeywords_noTransactionFound() {
+        String expectedMessage = String.format(MESSAGE_TRANSACTIONS_LISTED_OVERVIEW, 0);
+        TransactionContainsTagsPredicate predicate = preparePredicate(" ");
+        FilterCommand command = new FilterCommand(predicate);
+        expectedModel.updateFilteredTransactionList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredTransactionList());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code NameContainsKeywordsPredicate}.
+     */
+    private TransactionContainsTagsPredicate preparePredicate(String userInput) {
+        return new TransactionContainsTagsPredicate(userInput);
     }
 }
