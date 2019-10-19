1ea1fa4b6c62fccd47666f8259acf6a0048efde5
jeongyh99 <jeong.yuhan@gmail.com>
2019-10-20 03:14:48 +0800

    GUI Improvements

--- src/main/java/seedu/address/model/statistics/Statistics.java
+++ src/main/java/seedu/address/model/statistics/Statistics.java
@@ -9,8 +9,10 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 public class Statistics {
 
-    protected HashMap<String, HashMap<String, Double>> data; //mapping of name to map of {subject, scores}
+    private HashMap<String, HashMap<String, Double>> data; //mapping of name to map of {subject, scores}
     private ArrayList<StudentStat> studentWeightedScores;
+    private HashMap<Integer, Integer> scoreCounters;
+    private HashMap<String, Integer> gradeGroupings;
     private int totalStudents;
     private double min;
     private double max;
@@ -27,29 +29,72 @@ public class Statistics {
         requireAllNonNull(data);
         this.data = data;
         this.studentWeightedScores = new ArrayList<>();
+        this.gradeGroupings = new HashMap<>();
+        this.scoreCounters = new HashMap<>();
+        initializeData();
         processData();
     }
 
-//    public Statistics(HashMap<String, HashMap<String, Double>> data) {
-//        requireAllNonNull(data);
-//        this.data = data;
-//        this.studentWeightedScores = new ArrayList<>();
-//        processData();
-//    }
+    public void initializeData() {
+        gradeGroupings.put("aboveEighty", 0);
+        gradeGroupings.put("seventies", 0);
+        gradeGroupings.put("sixties", 0);
+        gradeGroupings.put("fifties", 0);
+        gradeGroupings.put("belowFifty", 0);
+    }
 
     public void processData() {
         data.forEach((name, subjectScoreMap) -> studentWeightedScores.add(new StudentStat(name, subjectScoreMap)));
         studentWeightedScores.sort((s1, s2) -> s1.weightedScore >= s2.weightedScore ? 1 : -1);
+        studentWeightedScores.stream().forEach(x -> sortIntoGrade(x));
+        studentWeightedScores
+                .stream()
+                .mapToInt(studentStat -> (int) studentStat.weightedScore)
+                .forEach(score -> allocateDistribution(score));
+
         DescriptiveStatistics statsGenerator = new DescriptiveStatistics();
         studentWeightedScores.stream().forEach((dataValue) -> statsGenerator.addValue(dataValue.weightedScore));
-
         totalStudents = (int) statsGenerator.getN();
         min = statsGenerator.getMin();
         max = statsGenerator.getMax();
         mean = statsGenerator.getMean();
         median = statsGenerator.getPercentile(50);
         standardDev = statsGenerator.getStandardDeviation();
+    }
 
+    private void allocateDistribution(int score) {
+        if (scoreCounters.containsKey(score)) {
+            scoreCounters.put(score, scoreCounters.get(score) + 1);
+        }
+        else {
+            scoreCounters.put(score, 1);
+        }
+    }
+
+    public HashMap<Integer, Integer> getFrequencyDistribution() {
+        return scoreCounters;
+    }
+
+    private void sortIntoGrade(StudentStat studentStat) {
+        if (studentStat.weightedScore >= 80) {
+            gradeGroupings.put("aboveEighty", gradeGroupings.get("aboveEighty") + 1);
+        }
+        else if (studentStat.weightedScore >= 70) {
+            gradeGroupings.put("seventies", gradeGroupings.get("seventies") + 1);
+        }
+        else if (studentStat.weightedScore >= 60) {
+            gradeGroupings.put("sixties", gradeGroupings.get("sixties") + 1);
+        }
+        else if (studentStat.weightedScore >= 50) {
+            gradeGroupings.put("fifties", gradeGroupings.get("fifties") + 1);
+        }
+        else {
+            gradeGroupings.put("belowFifty", gradeGroupings.get("belowFifty") + 1);
+        }
+    }
+
+    public HashMap<String, Integer> getGradeGroupings() {
+        return gradeGroupings;
     }
 
     public void setData(HashMap<String, HashMap<String, Double>> data) {
@@ -80,7 +125,7 @@ public class Statistics {
         return totalStudents;
     }
 
-    class StudentStat {
+    private class StudentStat {
         protected String name;
         protected double weightedScore;
         protected HashMap<String, Double> subjectScoreMap;
@@ -96,5 +141,27 @@ public class Statistics {
             double total = subjectScoreMap.values().stream().mapToDouble(score -> Double.valueOf(score)).sum();
             weightedScore = total / noSubjects;
         }
+    }
+
+    class ScoreCounter {
+        double score;
+        double count;
+
+        public ScoreCounter(double score) {
+            this.score = score;
+            count = 0;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) {
+                return true;
+            }
+            else if (other instanceof ScoreCounter) {
+                return ((ScoreCounter) other).score == this.score;
+            }
+            return false;
+        }
+
     }
 }
--- src/main/java/seedu/address/ui/StatisticsCard.java
+++ src/main/java/seedu/address/ui/StatisticsCard.java
@@ -14,6 +14,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.statistics.Statistics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * An UI component that displays information of {@code Statistics}.
  */
@@ -52,18 +55,22 @@ public class StatisticsCard extends UiPart<Region> {
         totalStudents.setText(String.valueOf(this.stat.getTotalStudents()));
         mean.setText((String.format("%.2f", this.stat.getMean())));
         median.setText((String.format("%.2f",this.stat.getMedian())));
+        min.setText((String.format("%.2f", this.stat.getMin())));
+        max.setText((String.format("%.2f", this.stat.getMax())));
+        standardDev.setText((String.format("%.2f", this.stat.getStandardDev())));
         setPieChart();
         setLineChart();
     }
 
     public void setPieChart() {
+        HashMap<String, Integer> gradeGroupings = stat.getGradeGroupings();
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
-                        new PieChart.Data("A", 10),
-                        new PieChart.Data("B", 20),
-                        new PieChart.Data("C", 20),
-                        new PieChart.Data("D", 20),
-                        new PieChart.Data("Ungraded", 30));
+                        new PieChart.Data(">= 80", gradeGroupings.get("aboveEighty")),
+                        new PieChart.Data("70 ~ 79", gradeGroupings.get("seventies")),
+                        new PieChart.Data("60 ~ 69", gradeGroupings.get("sixties")),
+                        new PieChart.Data("50 ~ 59", gradeGroupings.get("fifties")),
+                        new PieChart.Data("< 50", gradeGroupings.get("belowFifty")));
         distributionChart.setData(pieChartData);
         distributionChart.setTitle("Weighted Grade Distribution");
         distributionChart.setLegendSide(Side.LEFT);
@@ -74,31 +81,33 @@ public class StatisticsCard extends UiPart<Region> {
         NumberAxis xAxis = (NumberAxis) lineChart.getXAxis();
         NumberAxis yAxis = (NumberAxis) lineChart.getYAxis();
 
-        xAxis.setLabel("Num Of Students(s)");
-        yAxis.setLabel("Weighted Average Score");
+        yAxis.setLabel("Num Of Students(s)");
+        xAxis.setLabel("Weighted Average Score");
+        xAxis.setForceZeroInRange(false);
 
         lineChart.setTitle("Score Frequency Distribution");
         //defining a series
         XYChart.Series series = new XYChart.Series();
         //populating the series with data
-        series.getData().add(new XYChart.Data(1, 23));
-        series.getData().add(new XYChart.Data(2, 14));
-        series.getData().add(new XYChart.Data(3, 15));
-        series.getData().add(new XYChart.Data(4, 24));
-        series.getData().add(new XYChart.Data(5, 34));
-        series.getData().add(new XYChart.Data(6, 36));
-        series.getData().add(new XYChart.Data(7, 22));
-        series.getData().add(new XYChart.Data(8, 45));
-        series.getData().add(new XYChart.Data(9, 43));
-        series.getData().add(new XYChart.Data(10, 17));
-        series.getData().add(new XYChart.Data(11, 29));
-        series.getData().add(new XYChart.Data(12, 25));
+        HashMap<Integer, Integer> frequencyDistribution = stat.getFrequencyDistribution();
+        for (Map.Entry<Integer, Integer> entry : frequencyDistribution.entrySet()) {
+            series.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+        }
 
-//        Scene scene  = new Scene(lineChart,800,600);
+//        series.getData().add(new XYChart.Data(1, 23));
+//        series.getData().add(new XYChart.Data(2, 14));
+//        series.getData().add(new XYChart.Data(3, 15));
+//        series.getData().add(new XYChart.Data(4, 24));
+//        series.getData().add(new XYChart.Data(5, 34));
+//        series.getData().add(new XYChart.Data(6, 36));
+//        series.getData().add(new XYChart.Data(7, 22));
+//        series.getData().add(new XYChart.Data(8, 45));
+//        series.getData().add(new XYChart.Data(9, 43));
+//        series.getData().add(new XYChart.Data(10, 17));
+//        series.getData().add(new XYChart.Data(11, 29));
+//        series.getData().add(new XYChart.Data(12, 25));
+
         lineChart.getData().add(series);
     }
 
-    public void updateData(ObservableList<Statistics> stat) {
-        postStat(stat);
-    }
 }
