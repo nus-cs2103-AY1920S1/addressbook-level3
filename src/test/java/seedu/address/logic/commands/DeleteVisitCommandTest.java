565578952415f0c51cc00246a17aee443c70cce2
SQwQ <33057619+SQwQ@users.noreply.github.com>
2019-10-19 17:25:24 +0800

    fix: fixed broken test

--- src/test/java/seedu/address/logic/commands/DeleteVisitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteVisitCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
@@ -9,6 +10,7 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -35,7 +37,16 @@ public class DeleteVisitCommandTest {
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(firstPerson, editedPerson);
 
-        assertCommandSuccess(deleteVisitCommand, model, expectedMessage, expectedModel);
+        CommandResult expectedCommandResult =
+                new CommandResult(expectedMessage, editedPerson.getVisitList().getObservableRecords());
+
+        try {
+            CommandResult result = deleteVisitCommand.execute(model);
+            assertEquals(expectedCommandResult, result);
+            assertEquals(expectedModel, model);
+        } catch (CommandException ce) {
+            throw new AssertionError("Execution of command should not fail.", ce);
+        }
     }
 
     @Test
