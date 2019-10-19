037fbdd5c2c61e4a8cfbbd888a916ad6e22b167b
Joshua Seet <joshu5797@gmail.com>
2019-10-19 18:09:26 +0800

    Fixed checkstyle

--- src/test/java/seedu/address/logic/commands/AddFinSecCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddFinSecCommandTest.java
@@ -4,11 +4,14 @@ import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 import javafx.collections.ObservableList;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.FinSec;
