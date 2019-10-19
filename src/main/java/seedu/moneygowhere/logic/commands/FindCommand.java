b08a021405cfd2ea04c976fdfea16587d8b21695
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-10-20 02:23:43 +0800

    Updated find command

--- src/main/java/seedu/moneygowhere/logic/commands/FindCommand.java
+++ src/main/java/seedu/moneygowhere/logic/commands/FindCommand.java
@@ -2,9 +2,13 @@ package seedu.moneygowhere.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 import seedu.moneygowhere.commons.core.Messages;
 import seedu.moneygowhere.model.Model;
-import seedu.moneygowhere.model.spending.NameContainsKeywordsPredicate;
+import seedu.moneygowhere.model.spending.Spending;
 
 /**
  * Finds and lists all spending in MoneyGoWhere whose name contains any of the argument keywords.
@@ -16,27 +20,42 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all spending entries whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Not all fields are necessary, but they are combined together to form a query.\n"
+            + "Parameters: n/KEYWORD [MORE_KEYWORDS]...  d/DATE_RANGE c/COST_RANGE r/REMARK t/TAG\n"
+            + "Example: " + COMMAND_WORD + " n/chicken rice d/1/1/2019 to 2/1/2019 c/1.50-5 r/tasty t/food";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final List<Predicate<Spending>> predicates;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    public FindCommand(List<Predicate<Spending>> predicates) {
+        this.predicates = predicates;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredSpendingList(predicate);
+
+        Optional<Predicate<Spending>> predicate = predicates.stream().reduce(Predicate::and);
+
+        if (predicate.isEmpty()) {
+            model.updateFilteredSpendingList(failed -> false);
+            return new CommandResult(
+                    String.format(Messages.MESSAGE_SPENDINGS_LISTED_OVERVIEW, model.getFilteredSpendingList().size()));
+        }
+
+        model.updateFilteredSpendingList(predicate.get());
         return new CommandResult(
                 String.format(Messages.MESSAGE_SPENDINGS_LISTED_OVERVIEW, model.getFilteredSpendingList().size()));
+    }
+
+    @Override
+    public int hashCode() {
+        return predicates.hashCode();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                && predicates.equals(((FindCommand) other).predicates)); // state check
     }
 }
--- src/main/java/seedu/moneygowhere/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/moneygowhere/logic/parser/FindCommandParser.java
@@ -1,12 +1,28 @@
 package seedu.moneygowhere.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.moneygowhere.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.moneygowhere.logic.parser.CliSyntax.PREFIX_COST;
+import static seedu.moneygowhere.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.moneygowhere.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.moneygowhere.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.moneygowhere.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.moneygowhere.logic.commands.FindCommand;
 import seedu.moneygowhere.logic.parser.exceptions.ParseException;
+import seedu.moneygowhere.model.spending.Cost;
+import seedu.moneygowhere.model.spending.CostInRangePredicate;
+import seedu.moneygowhere.model.spending.Date;
+import seedu.moneygowhere.model.spending.DateInRangePredicate;
 import seedu.moneygowhere.model.spending.NameContainsKeywordsPredicate;
+import seedu.moneygowhere.model.spending.RemarkContainsKeywordsPredicate;
+import seedu.moneygowhere.model.spending.Spending;
+import seedu.moneygowhere.model.tag.TagPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -19,15 +35,51 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_DATE, PREFIX_COST, PREFIX_REMARK, PREFIX_TAG);
+
+        List<Predicate<Spending>> predicates = new ArrayList<>();
+
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            String name = argMultimap.getValue(PREFIX_NAME).get();
+            String[] nameKeywords = name.split("\\s+");
+            predicates.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        }
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            List<Date> dates = ParserUtil.parseDates(argMultimap.getAllValues(PREFIX_DATE));
+
+            if (dates.size() < 2) {
+                throw new ParseException(Date.MESSAGE_CONSTRAINTS);
+            }
+            predicates.add(new DateInRangePredicate(dates.get(0), dates.get(1)));
+        }
+        if (argMultimap.getValue(PREFIX_COST).isPresent()) {
+            List<Cost> costs = ParserUtil.parseCosts(argMultimap.getAllValues(PREFIX_COST));
+
+            double min = Double.parseDouble(costs.get(0).value);
+            double max = Double.parseDouble(costs.get(1).value);
+
+            if (costs.size() != 2 || min > max) {
+                throw new ParseException(Cost.MESSAGE_CONSTRAINTS);
+            }
+
+            predicates.add(new CostInRangePredicate(costs.get(0), costs.get(1)));
+        }
+        if (argMultimap.getValue(PREFIX_REMARK).isPresent()) {
+            String name = argMultimap.getValue(PREFIX_REMARK).get();
+            String[] nameKeywords = name.split("\\s+");
+            predicates.add(new RemarkContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        }
+        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            String tag = argMultimap.getValue(PREFIX_TAG).get();
+            predicates.add(new TagPredicate(tag));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        if (predicates.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(predicates);
     }
-
 }
