23a49dfdf46e5cdf46f1fc17489bf4ca7e7510ed
chowyiyin <yiyin.chow@gmail.com>
2019-10-19 14:42:55 +0800

    Add expand person and expand policy command

---
+++ src/main/java/seedu/address/logic/commands/ExpandPersonCommand.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Expands the selected person.
+ */
+public class ExpandPersonCommand extends Command {
+
+    public static final String COMMAND_WORD = "expandperson";
+
+    public static final String MESSAGE_SUCCESS = "Expanded: %1$s";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
+            + "by the index number used in the displayed person list. \n"
+            + "Example: " + COMMAND_WORD + " 1 ";
+
+    private final Index index;
+
+    public ExpandPersonCommand(Index index) {
+        this.index = index;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToExpand = lastShownList.get(index.getZeroBased());
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, personToExpand), personToExpand);
+    }
+}
---
+++ src/main/java/seedu/address/logic/commands/ExpandPolicyCommand.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_POLICIES;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.policy.Policy;
+
+/**
+ * Expands the selected policy.
+ */
+public class ExpandPolicyCommand extends Command {
+
+    public static final String COMMAND_WORD = "expandpolicy";
+
+    public static final String MESSAGE_SUCCESS = "Expanded: %1$s";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
+            + "by the index number used in the displayed person list. \n"
+            + "Example: " + COMMAND_WORD + " 1 ";
+
+    private final Index index;
+
+    public ExpandPolicyCommand(Index index) {
+        this.index = index;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Policy> lastShownList = model.getFilteredPolicyList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_POLICY_DISPLAYED_INDEX);
+        }
+
+        Policy policyToExpand = lastShownList.get(index.getZeroBased());
+        model.updateFilteredPolicyList(PREDICATE_SHOW_ALL_POLICIES);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, policyToExpand), policyToExpand);
+    }
+}
