226b9619876ad0c58a116b60eb659419e8c7fd63
ryanty <tanyuryan@gmail.com>
2019-10-19 15:55:08 +0800

    add AddCourseCommand case to JarvisParser

--- src/main/java/seedu/jarvis/logic/parser/JarvisParser.java
+++ src/main/java/seedu/jarvis/logic/parser/JarvisParser.java
@@ -19,6 +19,7 @@ import seedu.jarvis.logic.commands.address.ListAddressCommand;
 import seedu.jarvis.logic.commands.cca.AddCcaCommand;
 import seedu.jarvis.logic.commands.cca.DeleteCcaCommand;
 import seedu.jarvis.logic.commands.cca.EditCcaCommand;
+import seedu.jarvis.logic.commands.course.AddCourseCommand;
 import seedu.jarvis.logic.commands.course.LookUpCommand;
 import seedu.jarvis.logic.commands.finance.RemoveInstallmentCommand;
 import seedu.jarvis.logic.commands.finance.SetInstallmentCommand;
@@ -100,6 +101,9 @@ public class JarvisParser {
 
         case LookUpCommand.COMMAND_WORD:
             return new LookUpCommandParser().parse(arguments);
+
+        case AddCourseCommand.COMMAND_WORD:
+            return new AddCourseCommandParser().parse(arguments);
 
         case AddCcaCommand.COMMAND_WORD:
             return new AddCcaCommandParser().parse(arguments);
