60ae2b3f2a06ed5fe1be993f2f1f4370113dba4c
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-10-19 23:00:21 +0800

    Added parsed date field in Date

--- src/main/java/seedu/moneygowhere/model/spending/Date.java
+++ src/main/java/seedu/moneygowhere/model/spending/Date.java
@@ -2,6 +2,8 @@ package seedu.moneygowhere.model.spending;
 
 import static java.util.Objects.requireNonNull;
 
+import java.time.LocalDate;
+
 import seedu.moneygowhere.commons.util.DateUtil;
 import seedu.moneygowhere.logic.parser.exceptions.ParseException;
 
@@ -9,13 +11,13 @@ import seedu.moneygowhere.logic.parser.exceptions.ParseException;
  * Represents a Spending's Date in MoneyGoWhere.
  * Guarantees: immutable; is valid as declared in {@link #isValidDate(String)}
  */
-public class Date {
+public class Date implements Comparable<Date> {
 
 
     public static final String MESSAGE_CONSTRAINTS =
             "Date numbers can be today, yesterday, tomorrow or a formal date DD/MM/YYYY.";
-    //public static final String VALIDATION_REGEX = "([0-9]{1,2})([/\\-])([0-9]{1,2})([/\\-])([0-9]{4})";
     public final String value;
+    public final LocalDate dateValue;
 
     /**
      * Constructs a {@code Date}.
@@ -25,14 +27,13 @@ public class Date {
     public Date(String date) {
         requireNonNull(date);
 
-        java.util.Date parsedDate;
         try {
-            parsedDate = DateUtil.parseDate(date);
+            dateValue = DateUtil.parseDate(date);
         } catch (ParseException e) {
             throw new IllegalArgumentException(MESSAGE_CONSTRAINTS);
         }
 
-        value = DateUtil.formatDate(parsedDate);
+        value = DateUtil.formatDate(dateValue);
     }
 
     /**
@@ -57,6 +58,11 @@ public class Date {
     @Override
     public int hashCode() {
         return value.hashCode();
+    }
+
+    @Override
+    public int compareTo(Date other) {
+        return dateValue.compareTo(other.dateValue);
     }
 
 }
