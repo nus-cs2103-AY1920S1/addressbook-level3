bfc31344a609b57309971bc184eea6c01ca14c57
hua-lun <chuahualun@gmail.com>
2019-10-20 00:24:42 +0800

    Add sample data util file for dashboard

---
+++ src/main/java/seedu/address/model/util/DashboardSampleDataUtil.java
@@ -0,0 +1,34 @@
+package seedu.address.model.util;
+
+import seedu.address.model.dashboard.DashboardRecords;
+import seedu.address.model.dashboard.ReadOnlyDashboard;
+import seedu.address.model.dashboard.components.Dashboard;
+import seedu.address.model.dashboard.components.DashboardName;
+import seedu.address.model.diary.DiaryRecords;
+import seedu.address.model.diary.ReadOnlyDiary;
+import seedu.address.model.diary.components.Diary;
+import seedu.address.model.diary.components.DiaryName;
+
+/**
+ * Contains utility methods for populating {@code DashboardRecords} with sample data.
+ */
+public class DashboardSampleDataUtil {
+    public static Dashboard[] getSampleDashboards() {
+        return new Dashboard[] {
+                new Dashboard(new DashboardName("Asian Cuisines")),
+                new Dashboard(new DashboardName("Healthy Living")),
+                new Dashboard(new DashboardName("Meat Lovers")),
+                new Dashboard(new DashboardName("Vegan Diet")),
+                new Dashboard(new DashboardName("One Week Slimming")),
+                new Dashboard(new DashboardName("Core Exercises")),
+        };
+    }
+
+    public static ReadOnlyDashboard getSampleDashboardRecords() {
+        DashboardRecords sampleDc = new DashboardRecords();
+        for (Dashboard sampleDashboard : getSampleDashboards()) {
+            sampleDc.addDashboard(sampleDashboard);
+        }
+        return sampleDc;
+    }
+}
