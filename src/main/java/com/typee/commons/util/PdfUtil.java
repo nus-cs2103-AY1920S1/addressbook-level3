865ffc2c4c27e844549c8fcff1247d91f5928c72
Gi Hun Ko <rhrlgns96@gmail.com>
2019-10-19 11:08:01 +0800

    creates a skeleton Report class and PdfUtil class for pdf generation

---
+++ src/main/java/com/typee/commons/util/PdfUtil.java
@@ -0,0 +1,33 @@
+package com.typee.commons.util;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.typee.model.report.Report;
+
+/**
+ * Generates report of engagement with a specified template.
+ */
+public class PdfUtil {
+    /**
+     * Generates a {@code Report} in .pdf format and opens the file.
+     */
+    public void generateDocument(Report report) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+
+        document.add(chunk);
+        document.close();
+    }
+}
