5ceee1acd8117751aecda51574fa8956ccee3cfe
sreesubbash <33561308+sreesubbash@users.noreply.github.com>
2019-10-19 11:00:54 +0800

    Working command box

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -2,11 +2,15 @@ package seedu.address.ui;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+// import org.controlsfx.control.textfield.TextFields;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
@@ -21,14 +25,37 @@ public class CommandBox extends UiPart<Region> {
     @FXML
     private TextField commandTextField;
 
+    @FXML
+    private MenuBar commandMenuField;
+
+    @FXML
+    private ComboBox<String> commandComboField;
+
+    private Menu temp;
+    //private ObservableList<String> list = FXCollections.observableArrayList("app", "game", "load");
+
     public CommandBox(CommandExecutor commandExecutor) {
         super(FXML);
         this.commandExecutor = commandExecutor;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+        //String[] prompts = {"load", "home", "start"};
+        //TextFields.bindAutoCompletion(commandTextField, prompts);
         commandTextField.textProperty().addListener((observable, oldCommand, newCommand) -> {
             System.out.println("command changing from " + oldCommand + " to " + newCommand);
+            if (oldCommand.equals("") && newCommand.equals("s")) {
+                commandTextField.setText("start ");
+                // commandTextField.positionCaret(6);
+            }
+
+            if (oldCommand.equals("start ") && newCommand.equals("start")) {
+                commandTextField.setText("");
+
+            }
         });
+        temp = new Menu("Menu 1");
+        commandMenuField.getMenus().add(temp);
+        commandComboField.getItems().add("app");
     }
 
 
@@ -37,6 +64,7 @@ public class CommandBox extends UiPart<Region> {
      */
     @FXML
     private void handleCommandEntered() {
+        commandMenuField.getMenus().remove(temp);
         try {
             commandExecutor.execute(commandTextField.getText());
         } catch (CommandException | ParseException e) {
