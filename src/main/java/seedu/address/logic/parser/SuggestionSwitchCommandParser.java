b4983e9867a39533f6a7b2f2cbb463c71bc73004
chowyiyin <yiyin.chow@gmail.com>
2019-10-20 13:05:49 +0800

    Add parsers for suggestion and suggestionswitch commands

--- src/main/java/seedu/address/logic/parser/SuggestionSwitchCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SuggestionSwitchCommandParser.java
@@ -1,34 +1,13 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE_OF_BIRTH;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_OFF;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ON;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.stream.Stream;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.SuggestionCommand;
 import seedu.address.logic.commands.SuggestionSwitchCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.DateOfBirth;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Nric;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.policy.Policy;
-import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -38,7 +17,8 @@ public class SuggestionSwitchCommandParser implements Parser<SuggestionSwitchCom
     /**
      * Parses the given {@code String} of arguments in the context of the SuggestionSwitchCommand
      * and returns an SuggestionSwitchCommand object for execution.
-     * @throws seedu.address.logic.parser.exceptions.ParseException if the user input does not conform the expected format
+     * @throws seedu.address.logic.parser.exceptions.ParseException if the user input does not conform the expected
+     * format
      */
     public SuggestionSwitchCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
