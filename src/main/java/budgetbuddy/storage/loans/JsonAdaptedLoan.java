4599940ad034719edf372ef80f1a275fc09cc3fb
Kenneth Fung <fungchenyu@rocketmail.com>
2019-10-21 00:59:59 +0800

    Add more safety checks for reading JsonAdaptedLoan

--- src/main/java/budgetbuddy/storage/loans/JsonAdaptedLoan.java
+++ src/main/java/budgetbuddy/storage/loans/JsonAdaptedLoan.java
@@ -66,27 +66,91 @@ public class JsonAdaptedLoan {
      * @throws IllegalValueException If any data constraints were violated in the adapted loan.
      */
     public Loan toModelType() throws IllegalValueException {
-        if (!Name.isValidName(personName)) {
-            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
-        }
-        if (!Direction.contains(direction)) {
-            throw new IllegalValueException(Direction.MESSAGE_CONSTRAINTS);
-        }
-        if (!Amount.isValidAmount(amount)) {
-            throw new IllegalValueException(Amount.MESSAGE_CONSTRAINTS);
-        }
-        // TODO Check date validity.
-        if (!Description.isValidDescription(description)) {
-            throw new IllegalValueException(Description.MESSAGE_CONSTRAINTS);
-        }
-        if (!Status.contains(status)) {
-            throw new IllegalValueException(Status.MESSAGE_CONSTRAINTS);
-        }
+        checkName();
+        checkDirection();
+        checkAmount();
+        checkDate();
+        checkDescription();
+        checkStatus();
 
         Person person = new Person(new Name(personName), new LoanList());
         Loan loan = new Loan(person, Direction.valueOf(direction), new Amount(amount),
                 date, new Description(description), Status.valueOf(status));
         person.addLoan(loan);
         return loan;
+    }
+
+    /**
+     * Checks that adapted name can be converted into model's {@code Name} object.
+     * @throws IllegalValueException If adapted object cannot be converted.
+     */
+    private void checkName() throws IllegalValueException {
+        if (personName == null) {
+            throw new IllegalValueException(
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Person.class.getSimpleName()));
+        }
+        if (!Name.isValidName(personName)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+    }
+
+    /**
+     * Checks that adapted direction can be converted into model's {@code Direction} object.
+     * @throws IllegalValueException If adapted object cannot be converted.
+     */
+    private void checkDirection() throws IllegalValueException {
+        if (direction == null) {
+            throw new IllegalValueException(
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Direction.class.getSimpleName()));
+        }
+        if (!Direction.contains(direction)) {
+            throw new IllegalValueException(Direction.MESSAGE_CONSTRAINTS);
+        }
+    }
+
+    /**
+     * Checks that adapted amount can be converted into model's {@code Amount} object.
+     * @throws IllegalValueException If adapted object cannot be converted.
+     */
+    private void checkAmount() throws IllegalValueException {
+        if (!Amount.isValidAmount(amount)) {
+            throw new IllegalValueException(Amount.MESSAGE_CONSTRAINTS);
+        }
+    }
+
+    /**
+     * Checks that adapted date can be converted into model's {@code Date} object.
+     * @throws IllegalValueException If adapted object cannot be converted.
+     */
+    private void checkDate() throws IllegalValueException {
+        // TODO Check validity of date.
+    }
+
+    /**
+     * Checks that adapted description can be converted into model's {@code Description} object.
+     * @throws IllegalValueException If adapted object cannot be converted.
+     */
+    private void checkDescription() throws IllegalValueException {
+        if (description == null) {
+            throw new IllegalValueException(
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName()));
+        }
+        if (!Description.isValidDescription(description)) {
+            throw new IllegalValueException(Description.MESSAGE_CONSTRAINTS);
+        }
+    }
+
+    /**
+     * Checks that adapted status can be converted into model's {@code Status} object.
+     * @throws IllegalValueException If adapted object cannot be converted.
+     */
+    private void checkStatus() throws IllegalValueException {
+        if (status == null) {
+            throw new IllegalValueException(
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Status.class.getSimpleName()));
+        }
+        if (!Status.contains(status)) {
+            throw new IllegalValueException(Status.MESSAGE_CONSTRAINTS);
+        }
     }
 }
