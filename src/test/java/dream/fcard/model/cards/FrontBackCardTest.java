57218ba9a2f9dd4de0856c9214a20c19c755fa7b
huiminlim <e0036379@u.nus.edu>
2019-10-20 22:08:39 +0800

    Fix checkstyle

--- src/test/java/dream/fcard/model/cards/FrontBackCardTest.java
+++ src/test/java/dream/fcard/model/cards/FrontBackCardTest.java
@@ -2,12 +2,13 @@ package dream.fcard.model.cards;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import dream.fcard.model.exceptions.IndexNotFoundException;
 import org.junit.jupiter.api.Test;
 
 public class FrontBackCardTest {
 
     @Test
-    void testEvaluate() {
+    void testEvaluate() throws IndexNotFoundException {
         FrontBackCard card = new FrontBackCard("asd", "123");
         assertEquals(true, card.evaluate("123"));
         assertEquals(false, card.evaluate("abc"));
