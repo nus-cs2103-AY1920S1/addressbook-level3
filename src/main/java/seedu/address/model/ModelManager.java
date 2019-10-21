616d28184a2a53ae58ee8225d5f22aee67a1490a
1nefootstep <sheuxiang@gmail.com>
2019-10-21 17:43:24 +0800

    Import Optional in ModelManager

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -6,6 +6,7 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
