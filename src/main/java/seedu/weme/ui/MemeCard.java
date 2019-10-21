e94daf5386207c2727b91749dffaf11d679e98d1
Mo Zongran <mozongran@gmail.com>
2019-10-21 22:27:17 +0800

    Remove debugging line

--- src/main/java/seedu/weme/ui/MemeCard.java
+++ src/main/java/seedu/weme/ui/MemeCard.java
@@ -46,7 +46,6 @@ public class MemeCard extends UiPart<Region> {
         this.meme = meme;
         id.setText(displayedIndex + "");
         display.setImage(new Image(meme.getFilePath().toUrl().toString()));
-        System.out.println(meme.getFilePath().toUrl().toString());
         description.setText(meme.getDescription().value);
         meme.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
