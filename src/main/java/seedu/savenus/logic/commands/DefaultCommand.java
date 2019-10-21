30c8b32f44b0ca49559707fe7f4a29394040388d
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-10-21 17:37:29 +0800

    Edit DefaultCommand

--- src/main/java/seedu/savenus/logic/commands/DefaultCommand.java
+++ src/main/java/seedu/savenus/logic/commands/DefaultCommand.java
@@ -6,8 +6,8 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
 import seedu.savenus.logic.commands.exceptions.CommandException;
 import seedu.savenus.model.Model;
-import seedu.savenus.model.sorter.DefaultComparator;
 import seedu.savenus.model.food.Food;
+import seedu.savenus.model.sorter.DefaultComparator;
 
 /**
  * Sorts all the foods in the $aveNUS menu based on their default ordering, which is:
