52d6ed359e1d65607bac5dab68789aefc96a866d
Raikonen <choozeyuan97@gmail.com>
2019-10-21 13:52:04 +0800

    Add tests

---
+++ src/test/java/seedu/savenus/logic/parser/TopUpCommandParserTest.java
@@ -0,0 +1,31 @@
+package seedu.savenus.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.savenus.logic.commands.TopUpCommand;
+
+import java.math.BigDecimal;
+
+import static seedu.savenus.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.savenus.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.savenus.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class TopUpCommandParserTest {
+    private TopUpCommandParser parser = new TopUpCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsTopUpCommand() {
+        assertParseSuccess(parser, "100", new TopUpCommand(new BigDecimal(100)));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "abcd asd",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, TopUpCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_empty_throwsParseException() {
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, TopUpCommand.MESSAGE_USAGE));
+    }
+}
