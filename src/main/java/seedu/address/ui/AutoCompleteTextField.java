a08b3773c8b84ca48d0ead44a3d5a319aa502cd2
uberSaiyan <yanwl.note@gmail.com>
2019-10-21 23:22:54 +0800

    Remove unused imports

--- src/main/java/seedu/address/ui/AutoCompleteTextField.java
+++ src/main/java/seedu/address/ui/AutoCompleteTextField.java
@@ -13,8 +13,6 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
