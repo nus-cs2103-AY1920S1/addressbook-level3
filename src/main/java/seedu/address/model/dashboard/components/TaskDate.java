be28cffe10710c1e88b4ffdb4d3e12f9d0a82fb0
hua-lun <chuahualun@gmail.com>
2019-10-21 21:37:50 +0800

    Update dashboard to support date inputs

---
+++ src/main/java/seedu/address/model/dashboard/components/TaskDate.java
@@ -0,0 +1,66 @@
+package seedu.address.model.dashboard.components;
+
+import java.time.LocalDate;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class TaskDate {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Date entered is invalid, should only contain numbers, " +
+                    "expressed in day/month/year";
+    public final String day;
+    public final String month;
+    public final String year;
+
+    /**
+     * Constructs a {@TaskDate}
+     * @param date A valid date
+     */
+    public TaskDate(String date) {
+        requireNonNull(date);
+        checkArgument(isValidTaskDate(date), MESSAGE_CONSTRAINTS);
+        String[] d = date.split("/");
+        day = d[0];
+        month = d[1];
+        year = d[2];
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    /**
+     * Returns true if given string is a valid date
+     */
+    public static boolean isValidTaskDate(String test) {
+        String[] t = test.split("/");
+        int d = Integer.parseInt(t[0]);
+        int m = Integer.parseInt(t[1]);
+        int y = Integer.parseInt(t[2]);
+        boolean validDay = d > 0 && d < 31;
+        boolean validMonth = m > 0 && m < 13;
+        boolean validYear = y > 2018;
+        if(validDay && validMonth && validYear) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getDay() + "/" + getMonth() + "/" + getYear();
+    }
+
+
+}
