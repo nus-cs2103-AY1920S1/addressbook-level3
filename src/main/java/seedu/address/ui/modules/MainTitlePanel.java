d14d226e1664e93334593e963213a2933bc3223c
jasc <jasonsathyacitro@gmail.com>
2019-10-21 16:03:29 +0800

    Make title screen show play count this week

--- src/main/java/seedu/address/ui/modules/MainTitlePanel.java
+++ src/main/java/seedu/address/ui/modules/MainTitlePanel.java
@@ -1,11 +1,17 @@
 package seedu.address.ui.modules;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.util.StringConverter;
 import seedu.address.commons.util.AppUtil;
 import seedu.address.model.globalstatistics.GlobalStatistics;
 import seedu.address.model.globalstatistics.WeeklyPlayed;
@@ -13,7 +19,13 @@ import seedu.address.statistics.WordBankStatistics;
 import seedu.address.ui.AvatarImage;
 import seedu.address.ui.UiPart;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MainTitlePanel extends UiPart<Region> {
 
@@ -33,6 +45,9 @@ public class MainTitlePanel extends UiPart<Region> {
     private Label favoriteWordBankText;
 
     @FXML
+    private StackPane weeklyBarChartPlaceholder;
+
+    @FXML
     private ImageView avatarImageView;
 
     public MainTitlePanel(GlobalStatistics globalStats, Optional<WordBankStatistics> maxWbStats,
@@ -49,7 +64,37 @@ public class MainTitlePanel extends UiPart<Region> {
         playedTimesText.setText(playedTimes + "");
         timesText.setText(playedTimes == 1 ? " time" : " times");
 
+        favoriteWordBankText.setText(" - ");
         maxWbStats.ifPresent(statistics -> favoriteWordBankText.setText(statistics.getWordBankName()));
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        yAxis.setTickLabelFormatter(new StringConverter<>() { // set to only showing integer values
+            @Override
+            public String toString(Number object) {
+                if (object.intValue() != object.doubleValue()) {
+                    return "";
+                }
+                return object.intValue() + "";
+            }
+
+            @Override
+            public Number fromString(String string) {
+                Number val = Double.parseDouble(string);
+                return val.intValue();
+            }
+        });
+        BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+        WeeklyPlayed weeklyPlayed = globalStats.getWeeklyPlayed();
+        XYChart.Series<String, Number> dataSeries = new XYChart.Series<>();
+        List<Map.Entry<DayOfWeek, Integer>> sortedData = new ArrayList<>(weeklyPlayed.getNumPlayed().entrySet());
+        sortedData.sort(Comparator.comparing(Map.Entry::getKey));
+        for (Map.Entry<DayOfWeek, Integer> entry : sortedData) {
+            dataSeries.getData().add(new XYChart.Data<>(entry.getKey().toString().substring(0, 3),
+                    entry.getValue()));
+        }
+        barChart.getData().add(dataSeries);
+        weeklyBarChartPlaceholder.getChildren().add(barChart);
 
         Image avatarImage = AvatarImage.get(avatarId);
         avatarImageView.setImage(avatarImage);
