6604c0c2dfe9cb7433f2adb1085ddac181de6346
Kevin Lim <lyskevin@gmail.com>
2019-10-21 12:24:48 +0800

    Refactor ClearCommandTest

--- src/test/java/com/typee/logic/commands/ClearCommandTest.java
+++ src/test/java/com/typee/logic/commands/ClearCommandTest.java
@@ -1,10 +1,17 @@
 package com.typee.logic.commands;
 
-//import static com.typee.testutil.TypicalPersons.getTypicalAddressBook;
+import static com.typee.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static com.typee.testutil.TypicalEngagements.getTypicalEngagementList;
+
+import org.junit.jupiter.api.Test;
+
+import com.typee.model.EngagementList;
+import com.typee.model.Model;
+import com.typee.model.ModelManager;
+import com.typee.model.UserPrefs;
 
 public class ClearCommandTest {
 
-    /*
     @Test
     public void execute_emptyAddressBook_success() {
         Model model = new ModelManager();
@@ -16,14 +23,12 @@ public class ClearCommandTest {
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
-        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model model = new ModelManager(getTypicalEngagementList(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalEngagementList(), new UserPrefs());
         expectedModel.setHistoryManager(new EngagementList());
         expectedModel.saveEngagementList();
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
-
-     */
 }
