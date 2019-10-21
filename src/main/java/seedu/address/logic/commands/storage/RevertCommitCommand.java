9fa220ff96cfd1112ea11a5ff18cba4be19dd69d
Liu Zechu <liuzechu2013@gmail.com>
2019-10-21 21:05:26 +0800

    Change revert command message

--- src/main/java/seedu/address/logic/commands/storage/RevertCommitCommand.java
+++ src/main/java/seedu/address/logic/commands/storage/RevertCommitCommand.java
@@ -19,7 +19,7 @@ public class RevertCommitCommand extends Command {
             + "Parameters: PLAN_INDEX.COMMIT_NUMBER (both must be non-negative integers)\n"
             + "Example: " + COMMAND_WORD + " 1.3";
 
-    public static final String MESSAGE_SUCCESS = "Here's your study plan for this commit. All later commits discarded";
+    public static final String MESSAGE_SUCCESS = "Here's your study plan for this commit. New revert commit created.";
     public static final String MESSAGE_NOT_ACTIVE_STUDY_PLAN = "The study plan index must be the active one!";
     public static final String MESSAGE_NO_SUCH_COMMIT = "The commit index you've entered is invalid!";
     public static final String MESSAGE_NO_MORE_STUDYPLAN = "You don't have any study plan currently. Create now!";
