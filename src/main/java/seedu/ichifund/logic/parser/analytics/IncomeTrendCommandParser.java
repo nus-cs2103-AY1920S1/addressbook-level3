967980bb6ea2294b1dfbc34696efb1dc2d4fb133
Alaete <yu.wanzhi.olivia@dhs.sg>
2019-10-22 22:33:59 +0800

    Add logic for income & balance trends

---
+++ src/main/java/seedu/ichifund/logic/parser/analytics/IncomeTrendCommandParser.java
@@ -0,0 +1,43 @@
+package seedu.ichifund.logic.parser.analytics;
+
+import static seedu.ichifund.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.ichifund.logic.parser.CliSyntax.PREFIX_YEAR;
+
+import java.util.Optional;
+
+import seedu.ichifund.logic.commands.analytics.IncomeTrendCommand;
+import seedu.ichifund.logic.parser.ArgumentMultimap;
+import seedu.ichifund.logic.parser.ArgumentTokenizer;
+import seedu.ichifund.logic.parser.Parser;
+import seedu.ichifund.logic.parser.ParserUtil;
+import seedu.ichifund.logic.parser.exceptions.ParseException;
+import seedu.ichifund.model.date.Year;
+
+/**
+ * Parses input arguments and creates a new ExpenditureTrendCommand object
+ */
+public class IncomeTrendCommandParser implements Parser<IncomeTrendCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ExpenditureTrendCommand
+     * and returns an ExpenditureTrendCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public IncomeTrendCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_YEAR);
+
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, IncomeTrendCommand.MESSAGE_USAGE));
+        }
+        Year year = null;
+
+        if (argMultimap.getValue(PREFIX_YEAR).isPresent()) {
+            year = ParserUtil.parseYear(argMultimap.getValue(PREFIX_YEAR).get());
+        }
+
+        Optional<Year> optionalYear = Optional.ofNullable(year);
+
+        return new IncomeTrendCommand(optionalYear);
+    }
+}
