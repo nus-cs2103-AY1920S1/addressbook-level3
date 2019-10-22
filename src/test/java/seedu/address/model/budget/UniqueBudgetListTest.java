9668816ad471d47121e663bdf3360cdf01fafc17
anqichen9856 <chenanqi9856@gmail.com>
2019-10-22 21:28:27 +0800

    Add unit tests for UniqueBudgetList

---
+++ src/test/java/seedu/address/model/budget/UniqueBudgetListTest.java
@@ -0,0 +1,88 @@
+package seedu.address.model.budget;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalBudgets.SCHOOL;
+import static seedu.address.testutil.TypicalBudgets.OUTSIDE_SCHOOL;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.budget.exceptions.DuplicateBudgetException;
+
+public class UniqueBudgetListTest {
+    private final UniqueBudgetList uniqueBudgetList = new UniqueBudgetList();
+
+    @Test
+    public void contains_nullBudget_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueBudgetList.contains(null));
+    }
+
+    @Test
+    public void contains_budgetNotInList_returnsFalse() {
+        assertFalse(uniqueBudgetList.contains(SCHOOL));
+    }
+
+    @Test
+    public void contains_budgetInList_returnsTrue() {
+        uniqueBudgetList.add(SCHOOL);
+        assertTrue(uniqueBudgetList.contains(SCHOOL));
+    }
+
+    @Test
+    public void add_nullBudget_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueBudgetList.add(null));
+    }
+
+    @Test
+    public void add_duplicateBudget_throwsDuplicateBudgetException() {
+        uniqueBudgetList.add(SCHOOL);
+        assertThrows(DuplicateBudgetException.class, () -> uniqueBudgetList.add(SCHOOL));
+    }
+
+    @Test
+    public void setBudgets_nullUniqueBudgetList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueBudgetList.setBudgets((UniqueBudgetList) null));
+    }
+
+    @Test
+    public void setBudgets_uniqueBudgetList_replacesOwnListWithProvidedUniqueBudgetList() {
+        uniqueBudgetList.add(SCHOOL);
+        UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
+        expectedUniqueBudgetList.add(OUTSIDE_SCHOOL);
+        uniqueBudgetList.setBudgets(expectedUniqueBudgetList);
+        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
+    }
+
+    @Test
+    public void setBudgets_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueBudgetList.setBudgets((List<Budget>) null));
+    }
+
+    @Test
+    public void setBudgets_list_replacesOwnListWithProvidedList() {
+        uniqueBudgetList.add(SCHOOL);
+        List<Budget> BudgetList = Collections.singletonList(OUTSIDE_SCHOOL);
+        uniqueBudgetList.setBudgets(BudgetList);
+        UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
+        expectedUniqueBudgetList.add(OUTSIDE_SCHOOL);
+        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
+    }
+
+    @Test
+    public void setBudgets_listWithDuplicateBudgets_throwsDuplicateBudgetException() {
+        List<Budget> listWithDuplicateBudgets = Arrays.asList(SCHOOL, SCHOOL);
+        assertThrows(DuplicateBudgetException.class, () -> uniqueBudgetList.setBudgets(listWithDuplicateBudgets));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+                -> uniqueBudgetList.asUnmodifiableObservableList().remove(0));
+    }
+}
