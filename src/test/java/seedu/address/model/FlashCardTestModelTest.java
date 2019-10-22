9a013932b149b7d6e24ca60a87e5dc0866ab816a
Leonard Tay <leonardtay748@gmail.com>
2019-10-22 19:08:33 +0800

    edit FlashCardTestModelTest.java: remove model from getQuestion( )

--- src/test/java/seedu/address/model/FlashCardTestModelTest.java
+++ src/test/java/seedu/address/model/FlashCardTestModelTest.java
@@ -30,9 +30,9 @@ public class FlashCardTestModelTest {
     }
 
     @Test
-    public void getQuestion_validTestList_success(Model model) {
+    public void getQuestion_validTestList_success() {
         flashCardTestModel = new FlashCardTestModel(testList);
         String expectedOutput = "0 + 10";
-        assertEquals(expectedOutput, flashCardTestModel.getQuestion(model));
+        assertEquals(expectedOutput, flashCardTestModel.getQuestion());
     }
 }
