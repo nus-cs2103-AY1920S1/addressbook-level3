5e0d1ce07f1ed5a7f1d0f508ddca33d5836697d6
Icesiolz <43022818+Icesiolz@users.noreply.github.com>
2019-10-22 12:02:54 +0800

    Update UpCommand.java

--- src/main/java/seedu/address/logic/commands/UpCommand.java
+++ src/main/java/seedu/address/logic/commands/UpCommand.java
@@ -26,4 +26,8 @@ public class UpCommand extends ScrollCommand {
         }
         return new UpCommandResult(MESSAGE_SUCCESS, pane);
     }
+    
+    public String getCommandWord() {
+        return this.COMMAND_WORD;
+    }
 }
