e34be9cbb24fec960c6bc9f7dcbea96a41c9d33b
dorcastan <dorcas.tann@gmail.com>
2019-10-22 13:37:05 +0800

    Add: Not-parameters for AutotagCommand
    Tag bookmarks based on keywords not found in name or URL.

--- src/main/java/seedu/mark/logic/parser/CliSyntax.java
+++ src/main/java/seedu/mark/logic/parser/CliSyntax.java
@@ -6,12 +6,14 @@ package seedu.mark.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
+    public static final Prefix PREFIX_FOLDER = new Prefix("f/");
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_URL = new Prefix("u/");
+    public static final Prefix PREFIX_NOT_NAME = new Prefix("nn/");
+    public static final Prefix PREFIX_NOT_URL = new Prefix("nu/");
+    public static final Prefix PREFIX_NOTE = new Prefix("n/");
+    public static final Prefix PREFIX_PARENT_FOLDER = new Prefix("p/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_FOLDER = new Prefix("f/");
-    public static final Prefix PREFIX_PARENT_FOLDER = new Prefix("p/");
-    public static final Prefix PREFIX_NOTE = new Prefix("n/");
     public static final Prefix PREFIX_TIME = new Prefix("t/");
+    public static final Prefix PREFIX_URL = new Prefix("u/");
 }
