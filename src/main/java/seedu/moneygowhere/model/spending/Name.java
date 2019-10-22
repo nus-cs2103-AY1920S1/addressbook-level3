7fefa288a412c3184a443f943537c9b25b94bfac
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-10-23 03:40:27 +0800

    Implement Comparable on fields

--- src/main/java/seedu/moneygowhere/model/spending/Name.java
+++ src/main/java/seedu/moneygowhere/model/spending/Name.java
@@ -7,7 +7,7 @@ import static seedu.moneygowhere.commons.util.AppUtil.checkArgument;
  * Represents a Spending's name in the MoneyGoWhere list.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
-public class Name {
+public class Name implements Comparable<Name> {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
@@ -56,4 +56,8 @@ public class Name {
         return fullName.hashCode();
     }
 
+    @Override
+    public int compareTo(Name o) {
+        return fullName.compareTo(o.fullName);
+    }
 }
