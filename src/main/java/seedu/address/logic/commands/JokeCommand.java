aa62ee44901b9d18ea0c1804d0e1c3837f657b57
lrchema <hema@Hemas-MacBook-Pro-2.local>
2019-10-22 16:11:55 +0800

    Remove getCommandWord from joke command

--- src/main/java/seedu/address/logic/commands/JokeCommand.java
+++ src/main/java/seedu/address/logic/commands/JokeCommand.java
@@ -23,8 +23,4 @@ public class JokeCommand extends Command {
         return new CommandResult(jokeList.getJoke());
     }
 
-    @Override
-    public String getCommandWord() {
-        return COMMAND_WORD;
-    }
 }
