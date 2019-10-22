9873ee902ff0142e9adeae85f5a2588a348bc128
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-10-22 20:11:59 +0800

    Add tests for DirectoryPath

---
+++ src/test/java/seedu/address/model/export/DirectoryPathTest.java
@@ -0,0 +1,50 @@
+//@@author LeowWB
+
+package seedu.address.model.export;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class DirectoryPathTest {
+
+    @Test
+    public void directoryPath_invalidPath_throwsException() {
+        String[] invalidDirectoryPathStrings = {
+                "file.docx",
+                "/file.docx/",
+                "\\file.docx\\"
+        };
+
+        for (String invalidDirectoryPathString : invalidDirectoryPathStrings) {
+            assertThrows(
+                    IllegalArgumentException.class,
+                    () -> new DirectoryPath(
+                            invalidDirectoryPathString
+                    )
+            );
+        }
+    }
+
+    @Test
+    public void directoryPath_validPath_success() {
+        String[] validDirectoryPathStrings = {
+                "directory",
+                "/directory/",
+                "\\directory\\",
+                "\\b c\\d",
+                "/b c/d"
+                "C:\\Users\\User\\Desktop\\",
+                "~/Desktop/"
+        };
+
+        for (String validDirectoryPathString : validDirectoryPathStrings) {
+            try {
+                new DirectoryPath(validDirectoryPathString);
+            } catch (IllegalArgumentException e) {
+                fail("Valid directory file path was not recognized as being valid");
+            }
+        }
+    }
+}
