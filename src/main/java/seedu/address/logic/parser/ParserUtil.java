e567944e037dc38f01da77bffcf8c8c3ea78c9d5
halfwhole <41856541+halfwhole@users.noreply.github.com>
2019-10-23 22:41:42 +0800

    Add parsing for prereq trees (#197)

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -4,13 +4,19 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_INDEX;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_SEMESTER;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.PrereqLeaf;
+import seedu.address.model.PrereqNode;
+import seedu.address.model.PrereqTree;
 import seedu.address.model.semester.SemesterName;
 import seedu.address.model.tag.Tag;
 
@@ -88,5 +94,61 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+
+    /**
+     * Parses the prerequisite tree from a given string.
+     * @param s Given string representing the prerequisite tree. Either the empty string, a module code, or (OP _ _).
+     * @return Prerequisite tree
+     */
+    public static PrereqTree parsePrereqTree(String s) {
+        if ("".equals(s)) {
+            return null;
+        } else if (s.charAt(0) != '(') {
+            return new PrereqLeaf(s);
+        }
+
+        // Split the string into list of items
+        String removeBrackets = s.substring(1, s.length() - 1);
+        String[] operatorOperands = removeBrackets.split(" ", 2);
+        String operator = operatorOperands[0];
+        String operandsString = operatorOperands[1];
+        List<String> operands = splitOperands(operandsString);
+        List<PrereqTree> children = operands.stream()
+                .map(operand -> parsePrereqTree(operand))
+                .collect(Collectors.toList());
+        return new PrereqNode(operator, children);
+    }
+
+    /**
+     * Splits the String of operands into its logical groupings.
+     * Example: "CS1 (AND (OR CS2 CS3) CS4) CS5" => ["CS1", "(AND (OR CS2 CS3) CS4)", "CS5"]
+     *
+     * @param operands String that represents the operands all together
+     * @return List of Strings, where each represents a single operand to be further parsed
+     */
+    public static List<String> splitOperands(String operands) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int balance = 0;
+        for (char c : operands.toCharArray()) {
+            sb.append(c);
+            if (c == ' ' && balance == 0) {
+                String trimmed = sb.toString().trim();
+                if (!"".equals(trimmed)) {
+                    list.add(trimmed);
+                }
+                sb.setLength(0);
+            } else if (c == '(') {
+                balance++;
+            } else if (c == ')' && --balance == 0) {
+                list.add(sb.toString().trim());
+                sb.setLength(0);
+            }
+        }
+        if (sb.length() > 0) {
+            list.add(sb.toString());
+        }
+        return list;
     }
 }
