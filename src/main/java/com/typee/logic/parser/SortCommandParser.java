bd9c92e1252a629cc63625076dbf1dd6df9f771f
brebeek <huyc1998@gmail.com>
2019-10-24 00:32:09 +0800

    Redesign sort command format

--- src/main/java/com/typee/logic/parser/SortCommandParser.java
+++ src/main/java/com/typee/logic/parser/SortCommandParser.java
@@ -5,14 +5,52 @@ import com.typee.logic.commands.SortCommand;
 import com.typee.logic.parser.exceptions.ParseException;
 
 public class SortCommandParser implements Parser<SortCommand> {
+
+    private String trimmedArgs;
     @Override
     public SortCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
+        trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
 
-        return new SortCommand(ParserUtil.parseComparator(trimmedArgs));
+        return new SortCommand(ParserUtil.parseComparator(parseOrder()));
     }
+
+    private String parseOrder() throws ParseException {
+        switch (Order.valueOf(trimmedArgs.toUpperCase())) {
+
+        case STARTA:
+            return "START_TIME";
+
+        case STARTD:
+            return "START_TIME_REVERSE";
+
+        case ENDA:
+            return "END_TIME";
+
+        case ENDD:
+            return "END_TIME_REVERSE";
+
+        case NAMEA:
+            return "ALPHABETICAL";
+
+        case NAMED:
+            return "ALPHABETICAL_REVERSE";
+
+        case PRIORITYA:
+            return "PRIORITY";
+
+        case PRIORITYD:
+            return "PRIORITY_REVERSE";
+
+        default:
+            throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+        }
+    }
+}
+
+enum Order {
+    STARTA, STARTD, ENDA, ENDD, NAMEA, NAMED, PRIORITYA, PRIORITYD
 }
