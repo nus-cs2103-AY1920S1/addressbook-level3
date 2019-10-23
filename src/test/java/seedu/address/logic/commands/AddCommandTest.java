2e5fb513c9dfdf4fa8420b7101f96d72c8f1bc0d
ShuTingY <shuting_y@u.nus.edu>
2019-10-23 12:40:09 +0800

    -Refactor names

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -48,7 +48,7 @@ public class AddCommandTest {
     public void execute_duplicateFlashCard_throwsCommandException() {
         FlashCard validFlashCard = new FlashCardBuilder().build();
         AddCommand addCommand = new AddCommand(validFlashCard);
-        ModelStub modelStub = new ModelStubWithPerson(validFlashCard);
+        ModelStub modelStub = new ModelStubWithFlashCard(validFlashCard);
 
         assertThrows(CommandException.class,
                 AddCommand.MESSAGE_DUPLICATE_FLASHCARD, () -> addCommand.execute(modelStub));
@@ -275,10 +275,10 @@ public class AddCommandTest {
     /**
      * A Model stub that contains a single flashCard.
      */
-    private class ModelStubWithPerson extends ModelStub {
+    private class ModelStubWithFlashCard extends ModelStub {
         private final FlashCard flashCard;
 
-        ModelStubWithPerson(FlashCard flashCard) {
+        ModelStubWithFlashCard(FlashCard flashCard) {
             requireNonNull(flashCard);
             this.flashCard = flashCard;
         }
