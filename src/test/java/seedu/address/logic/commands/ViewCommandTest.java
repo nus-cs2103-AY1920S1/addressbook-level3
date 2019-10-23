10cac6011e338c323d14405e3c826c10bb9a0914
joloong <joelloong@gmail.com>
2019-10-23 22:21:10 +0800

    Simple test for ViewCommand

---
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.ViewCommand.MESSAGE_SUCCESS;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.ui.tab.Tab;
+
+
+public class ViewCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_view_success() {
+        CommandResult expectedCommandResult =
+            new CommandResult(MESSAGE_SUCCESS, false, false, Tab.BUDGET);
+        assertCommandSuccess(new ViewCommand(Tab.BUDGET), model, expectedCommandResult, expectedModel);
+    }
+}
