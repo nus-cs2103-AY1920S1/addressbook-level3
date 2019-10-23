0c08bc697015ee81f7898b12896421effc0e0685
nattanyz <natalietan8@gmail.com>
2019-10-24 01:31:49 +0800

    change FlashCardDisplay to extend HBox instead of VBox. add TextFlow around flashCardText to allow wrapping.

--- src/main/java/dream/fcard/gui/FlashCardDisplay.java
+++ src/main/java/dream/fcard/gui/FlashCardDisplay.java
@@ -1,18 +1,21 @@
 package dream.fcard.gui;
 
 import javafx.geometry.Insets;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-public class FlashCardDisplay extends VBox {
+public class FlashCardDisplay extends HBox {
     private Text flashCardText;
 
     public FlashCardDisplay(String text) {
         super();
-        this.setMaxWidth(USE_COMPUTED_SIZE);
+//        this.setMaxWidth();
+//        this.setMinHeight();
 
-        // todo: figure out how to get card to maintain max width, min height
+        // todo: must figure out how to get card to maintain max width, min height
 
         this.setPadding(new Insets(10));
         this.setStyle("-fx-background-color:" + GuiSettings.getPrimaryUIColour() +";");
@@ -24,7 +27,9 @@ public class FlashCardDisplay extends VBox {
         flashCardText.setFill(Color.web(GuiSettings.getPrimaryTextColour()));
         flashCardText.setWrappingWidth(this.getWidth());
 
+        TextFlow textFlow = new TextFlow(flashCardText);
+
         // add flashCardText to card
-        this.getChildren().add(flashCardText);
+        this.getChildren().add(textFlow);
     }
 }
