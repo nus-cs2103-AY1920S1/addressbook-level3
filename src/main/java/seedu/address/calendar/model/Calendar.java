ffcbbc41195384ab35c309383db4178dab9d8afc
Lin Wei <linwei@Lins-MacBook-Pro-2.local>
2019-10-23 23:45:31 +0800

    Create UML diagrams

--- src/main/java/seedu/address/calendar/model/Calendar.java
+++ src/main/java/seedu/address/calendar/model/Calendar.java
@@ -5,14 +5,13 @@ import java.util.List;
 
 public class Calendar {
     private Month monthShown;
-    private Year currentYear;
     private boolean hasVisibleUpdates;
     private List<Event> events;
 
     public Calendar() {
         java.util.Calendar currentDate = java.util.Calendar.getInstance();
 
-        this.currentYear = new Year(currentDate.get(java.util.Calendar.YEAR));
+        Year currentYear = new Year(currentDate.get(java.util.Calendar.YEAR));
 
         int currentUnformattedMonth = currentDate.get(java.util.Calendar.MONTH);
         MonthOfYear currentMonth = MonthOfYear.convertJavaMonth(currentUnformattedMonth);
