885d51fdf20cf60db37c34c52d78fbb675556696
Huang Weijie <h_weijie@yahoo.com>
2019-10-24 04:14:37 +0800

    Wrote some comments

--- src/main/java/seedu/billboard/model/statistics/generators/BreakdownGenerator.java
+++ src/main/java/seedu/billboard/model/statistics/generators/BreakdownGenerator.java
@@ -11,7 +11,6 @@ import seedu.billboard.model.statistics.formats.ExpenseBreakdown;
 import seedu.billboard.model.statistics.formats.FilledExpenseBreakdown;
 import seedu.billboard.model.tag.Tag;
 
-
 /**
  * Stateless class to generate a breakdown of expenses. Methods here are guaranteed to have no external
  * side effects, or depend on external state.
@@ -26,15 +25,26 @@ public class BreakdownGenerator implements StatisticsGenerator<ExpenseBreakdown>
         return new FilledExpenseBreakdown(breakdown);
     }
 
+    /**
+     * Adds an expense to a map of tags to lists of expenses by using its tags as keys. If there is an already
+     * existing key, the expense is instead merged into the list of expenses under that key.
+     */
     private void combineMapAndExpense(Map<Tag, List<Expense>> map, Expense expense) {
         expense.getTags()
                 .forEach(tag -> map.merge(tag, List.of(expense), this::concatList));
     }
 
+    /**
+     * Combines the key value pairs of map2 into map1. If there are duplicate keys, the two lists are merged into
+     * a new list with all the values of the old list.
+     */
     private <K, V> void combineMapOfLists(Map<K, List<V>> map1, Map<K, List<V>> map2) {
-        map1.forEach((key, list) -> map2.merge(key, list, this::concatList));
+        map2.forEach((key, list) -> map1.merge(key, list, this::concatList));
     }
 
+    /**
+     * Concatenates two lists and returns the result list.
+     */
     private <T> List<T> concatList(List<T> list1, List<T> list2) {
         return Stream.concat(list1.stream(), list2.stream())
                 .collect(Collectors.toList());
