d93e843c42fb4de8d9433ff892f174bca8302e57
eugene teu <eugeneteu@gmail.com>
2019-10-23 14:37:26 +0800

    modify build.gradle

--- src/test/java/seedu/address/ui/UiPartExtension.java
+++ src/test/java/seedu/address/ui/UiPartExtension.java
@@ -11,7 +11,7 @@ import javafx.scene.Scene;
  * Provides an isolated stage to test an individual {@code UiPart}.
  */
 public class UiPartExtension extends StageExtension {
-    private static final String[] CSS_FILES = {"view/DarkTheme.css", "view/Extensions.css"};
+    private static final String[] CSS_FILES = {"view/SMLTheme.css", "view/Extensions.css"};
 
     public void setUiPart(final UiPart<? extends Parent> uiPart) {
         try {
