0d242d677e4309dee9f969762fe4642b624e7d2a
Bernice Chio <bernicechiohuiyin@gmail.com>
2019-10-23 18:32:06 +0800

    Fix checkstyle violation

--- src/test/java/seedu/address/report/ReportGeneratorTest.java
+++ src/test/java/seedu/address/report/ReportGeneratorTest.java
@@ -1,11 +1,11 @@
 package seedu.address.report;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalBodies.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
