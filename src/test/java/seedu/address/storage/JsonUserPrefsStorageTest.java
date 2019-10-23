048575e4c3b93e1fd413af06834080b4cbbd7a5b
ManasVegi <manasvegi@gmail.com>
2019-10-24 03:46:39 +0800

    Fix an issue in testing due to new json file name

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setGroceryListFilePath(Paths.get("addressbook.json"));
+        userPrefs.setGroceryListFilePath(Paths.get("data\\grocerylist.json"));
         return userPrefs;
     }
 
