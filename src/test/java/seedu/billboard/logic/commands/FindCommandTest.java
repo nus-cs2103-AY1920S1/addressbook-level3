00b16a035a1727b3a569f223d28cec2003991d26
xinmingzh <xinming.zh@gmail.com>
2019-10-24 12:59:14 +0800

    Fix checkstyletest

--- src/test/java/seedu/billboard/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/billboard/logic/commands/FindCommandTest.java
@@ -11,7 +11,6 @@ import static seedu.billboard.testutil.TypicalExpenses.getTypicalBillboard;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
