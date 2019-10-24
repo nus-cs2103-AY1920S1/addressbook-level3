c7930beab76c44e5d43b7f507410911fb4257cb3
ckb055 <chenkaibin7@gmail.com>
2019-10-24 15:16:02 +0800

    Fixed syntax for filtering tags to tag/

--- src/main/java/seedu/address/logic/commands/cheatsheet/FilterCheatSheetByTagCommand.java
+++ src/main/java/seedu/address/logic/commands/cheatsheet/FilterCheatSheetByTagCommand.java
@@ -7,24 +7,24 @@ import java.util.ArrayList;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.FilterByTagCommand;
 import seedu.address.model.Model;
 import seedu.address.model.cheatsheet.CheatSheet;
 import seedu.address.model.cheatsheet.CheatSheetContainsTagPredicate;
 
 /**
- * Command to filter cheatsheets(s) with the related tag(s).
+ * Command to filter cheatsheet(s) with the related tag(s).
  */
-public class FilterCheatSheetByTagCommand extends Command implements FilterByTagCommand {
+public class FilterCheatSheetByTagCommand extends Command {
 
     public static final String COMMAND_WORD = FILTER;
 
-    public static final String MESSAGE_USAGE = "filter by tags. Find all related cheatsheets with the specified \n"
-            + "tags. Example : filter [cheatsheet] [cs2103t]";
+    public static final String MESSAGE_USAGE = "filter by tags. Find all " +
+            "related cheatsheets with the specified \n"
+            + "tags. Example : filter tag/cheatsheet tag/cs2103t";
 
     public static final String FILTER_TAG_MESSAGE_SUCCESS = "Filter cheatsheets by tag(s) : ";
 
-    private String[] tagKeywords;
+    private ArrayList<String> tagKeywords;
 
     private final CheatSheetContainsTagPredicate tagPredicate;
 
@@ -33,9 +33,27 @@ public class FilterCheatSheetByTagCommand extends Command implements FilterByTag
      * @param predicate to test on an note object to see if it has the tag.
      * @param tagKeywords the tags provided by user input to test on the note.
      */
-    public FilterCheatSheetByTagCommand(CheatSheetContainsTagPredicate predicate, String[] tagKeywords) {
+    public FilterCheatSheetByTagCommand(CheatSheetContainsTagPredicate predicate, ArrayList<String> tagKeywords) {
         this.tagPredicate = predicate;
         this.tagKeywords = tagKeywords;
+    }
+
+    /**
+     * To display to the user which tags he/she indicated
+     * @return a string of the tags indicated
+     */
+
+    public String showTagQueries() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tagKeywords.size(); i++) {
+            if (i != tagKeywords.size() - 1) {
+                sb.append(tagKeywords.get(i))
+                        .append(", ");
+            } else {
+                sb.append(tagKeywords.get(i));
+            }
+        }
+        return sb.toString();
     }
 
     @Override
@@ -49,7 +67,7 @@ public class FilterCheatSheetByTagCommand extends Command implements FilterByTag
             sb.append("\n");
         }
         return new CommandResult(FILTER_TAG_MESSAGE_SUCCESS
-                + "\n" + FilterByTagCommand.displayTagKeywords(tagKeywords)
+                + "\n" + showTagQueries()
                 + "\n" + sb.toString());
     }
 }
