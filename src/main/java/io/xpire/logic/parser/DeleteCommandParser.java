b1303ca03ca2b1b22f20b10593a0502b3f27a7af
Liu Xiaoyu <51201915+xiaoyu-nus@users.noreply.github.com>
2019-10-24 23:53:31 +0800

    URL diagram for SetReminder Command (#108)

--- src/main/java/io/xpire/logic/parser/DeleteCommandParser.java
+++ src/main/java/io/xpire/logic/parser/DeleteCommandParser.java
@@ -45,8 +45,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     }
 
     private boolean containsQuantity(String[] args) {
-        return (args.length == 2)
-                ? isIntegerType(args[1]) : false;
+        return (args.length == 2) && isIntegerType(args[1]);
     }
 
     /**
