86a857f5c347e0cef41713769796e1d7a69b4cbb
charliechoong <e0202817@u.nus.edu>
2019-10-24 21:25:59 +0800

    Clear checkstyle errors

--- src/main/java/seedu/deliverymans/ui/UnavailableDeliverymenListPanel.java
+++ src/main/java/seedu/deliverymans/ui/UnavailableDeliverymenListPanel.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
