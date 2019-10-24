837e5b613f1dfbb48410f013274000e64763a5a1
marcfyk <marcjohnfong@gmail.com>
2019-10-24 12:19:48 +0800

    add JsonAdaptedEquipment and tests

---
+++ src/main/java/seedu/jarvis/storage/cca/JsonAdaptedEquipment.java
@@ -0,0 +1,38 @@
+package seedu.jarvis.storage.cca;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import seedu.jarvis.commons.exceptions.IllegalValueException;
+import seedu.jarvis.model.cca.Equipment;
+
+/**
+ * Jackson-friendly version of {@link Equipment}.
+ */
+public class JsonAdaptedEquipment {
+
+    private final String equipmentName;
+
+    /**
+     * Constructs a {@code JsonAdaptedEquipment} with the given {@code Equipment}.
+     */
+    @JsonCreator
+    public JsonAdaptedEquipment(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    public JsonAdaptedEquipment(Equipment equipment) {
+        equipmentName = equipment.equipmentName;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted equipment object into the model's {@code Equipment} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted equipment.
+     */
+    public Equipment toModelType() throws IllegalValueException {
+        if (!Equipment.isValidEquipmentName(equipmentName)) {
+            throw new IllegalValueException(Equipment.MESSAGE_CONSTRAINTS);
+        }
+        return new Equipment(equipmentName);
+    }
+}
