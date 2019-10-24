c5f109d5dd84371c8e84c3bd681e56591bec5fb9
Wang Shiyao <furytoar@gmail.com>
2019-10-25 01:18:36 +0800

    fix: FindCommandParserTest

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,6 +1,13 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_AUTHOR_BOOK_1;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GENRE_ACTION;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GENRE_FICTION;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SERIAL_NUMBER_BOOK_1;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AUTHOR;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GENRE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SERIAL_NUMBER;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -17,6 +24,8 @@ public class FindCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         assertParseFailure(parser, SPACES,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
@@ -25,11 +34,35 @@ public class FindCommandParserTest {
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
-                new FindCommand(new BookPredicate().setTitle("Alice Bob"));
-        assertParseSuccess(parser, FindCommand.COMMAND_WORD + " t/Alice Bob", expectedFindCommand);
+                new FindCommand(new BookPredicate()
+                        .setTitle("Harry Potter")
+                        .setAuthor(VALID_AUTHOR_BOOK_1)
+                        .setSerialNumber(VALID_SERIAL_NUMBER_BOOK_1)
+                        .setGenres(VALID_GENRE_FICTION, VALID_GENRE_ACTION)
+                        .setLoanState(Flag.LOANED));
+
+        assertParseSuccess(parser, FindCommand.COMMAND_WORD
+                + " t/Harry Potter" + " "
+                + PREFIX_AUTHOR + VALID_AUTHOR_BOOK_1 + " "
+                + PREFIX_SERIAL_NUMBER + VALID_SERIAL_NUMBER_BOOK_1 + " "
+                + PREFIX_GENRE + VALID_GENRE_FICTION + " "
+                + PREFIX_GENRE + VALID_GENRE_ACTION + " "
+                + "-loaned", expectedFindCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, FindCommand.COMMAND_WORD + SPACES + " t/ Alice Bob ", expectedFindCommand);
+        assertParseSuccess(parser, FindCommand.COMMAND_WORD
+                + SPACES + " t/ Harry Potter "
+                + PREFIX_AUTHOR + SPACES + VALID_AUTHOR_BOOK_1 + " "
+                + PREFIX_SERIAL_NUMBER + SPACES + VALID_SERIAL_NUMBER_BOOK_1 + " "
+                + PREFIX_GENRE + SPACES + VALID_GENRE_FICTION + " "
+                + PREFIX_GENRE + VALID_GENRE_ACTION + " "
+                + SPACES + "-loaned", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_duplicateLoanState_throwsParseException() {
+        assertParseFailure(parser, FindCommand.COMMAND_WORD + " -loaned -available",
+                FindCommand.MESSAGE_LOANSTATE_CONSTRAINTS);
     }
 
 }
