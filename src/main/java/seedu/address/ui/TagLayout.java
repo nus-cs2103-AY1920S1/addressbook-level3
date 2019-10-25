6ba7ea963d2629f8e220e0c180940bff3eb3f934
shawnlsj97 <slsj1997@gmail.com>
2019-10-26 00:41:57 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/ui/TagLayout.java
+++ src/main/java/seedu/address/ui/TagLayout.java
@@ -6,7 +6,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 
-public class TagLayout extends UiPart<Region>{
+/**
+ * Represents a tag for an athlete.
+ */
+public class TagLayout extends UiPart<Region> {
 
     private static final String FXML = "TagLayout.fxml";
 
