69226f0569dddd5708434b92386591e2d80ee1c0
joloong <joelloong@gmail.com>
2019-10-25 11:22:38 +0800

    Add test case for Messages again

--- src/test/java/seedu/address/commons/core/MessagesTest.java
+++ src/test/java/seedu/address/commons/core/MessagesTest.java
@@ -8,8 +8,11 @@ public class MessagesTest {
 
     @Test
     public void messagesAreAllCorrect() {
-        assertEquals(Messages.MESSAGE_UNKNOWN_COMMAND, "Unknown command");
-        assertEquals(Messages.MESSAGE_INVALID_COMMAND_FORMAT, "Invalid command format! \n%1$s");
-        assertEquals(Messages.MESSAGE_TRANSACTIONS_LISTED_OVERVIEW, "%1$d transactions listed!");
+        assertEquals(Messages.getMessageUnknownCommand(), "Unknown command");
+        assertEquals(Messages.getMessageInvalidCommandFormat(), "Invalid command format! \n%1$s");
+        assertEquals(Messages.getMessageTransactionsListedOverview(), "%1$d transactions listed!");
+        assertEquals(Messages.getMessageInvalidTransactionDisplayedIndex(),
+            "The transaction index provided is invalid");
+        assertEquals(Messages.getMessageInvalidBudgetDisplayedIndex(), "The budget index provided is invalid");
     }
 }
