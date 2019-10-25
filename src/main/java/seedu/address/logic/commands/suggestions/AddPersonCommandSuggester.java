6001f42222e96135ce4f7dc70ed29876f05c1ce8
aidilfbk <e0303295@u.nus.edu>
2019-10-25 09:09:08 +0800

    Remove redundant use of Collections.unmodifiableList

--- src/main/java/seedu/address/logic/commands/suggestions/AddPersonCommandSuggester.java
+++ src/main/java/seedu/address/logic/commands/suggestions/AddPersonCommandSuggester.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands.suggestions;
 
-import java.util.Collections;
 import java.util.List;
 
 import seedu.address.logic.commands.suggestions.stateless.NullSuggester;
@@ -11,7 +10,7 @@ import seedu.address.logic.parser.Prefix;
  * Provides suggestions for the {@link Prefix}es of the {@link seedu.address.logic.commands.AddPersonCommand}.
  */
 public class AddPersonCommandSuggester extends NullSuggester {
-    public static final List<Prefix> SUPPORTED_PREFIXES = Collections.unmodifiableList(List.of(
+    public static final List<Prefix> SUPPORTED_PREFIXES = List.of(
             CliSyntax.PREFIX_NAME
-    ));
+    );
 }
