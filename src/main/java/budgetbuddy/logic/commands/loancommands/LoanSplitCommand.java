d2cce4b715b7b2905dd3be5693ab1b0eb47945fc
Kenneth Fung <fungchenyu@rocketmail.com>
2019-10-25 23:18:34 +0800

    Revamp split algorithm

--- src/main/java/budgetbuddy/logic/commands/loancommands/LoanSplitCommand.java
+++ src/main/java/budgetbuddy/logic/commands/loancommands/LoanSplitCommand.java
@@ -6,18 +6,16 @@ import static budgetbuddy.logic.parser.CliSyntax.PREFIX_AMOUNT;
 import static budgetbuddy.logic.parser.CliSyntax.PREFIX_PERSON;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import budgetbuddy.logic.commands.Command;
 import budgetbuddy.logic.commands.CommandCategory;
 import budgetbuddy.logic.commands.CommandResult;
 import budgetbuddy.logic.commands.exceptions.CommandException;
+import budgetbuddy.model.LoansManager;
 import budgetbuddy.model.Model;
 import budgetbuddy.model.loan.LoanSplit;
 import budgetbuddy.model.person.Person;
@@ -41,13 +39,15 @@ public class LoanSplitCommand extends Command {
                     + PREFIX_AMOUNT + "10 90 0";
 
     public static final String MESSAGE_SUCCESS = "Loans split.";
+
     public static final String MESSAGE_PERSON_AMOUNT_NUMBERS_MISMATCH =
             "The number of persons does not match the number of payments.";
     public static final String MESSAGE_DUPLICATE_PERSONS = "Names of persons entered must be unique.";
+
     public static final String MESSAGE_INVALID_TOTAL = "Total amount must be more than zero.";
     public static final String MESSAGE_ALREADY_SPLIT = "The amounts have already been split equally.";
 
-    private HashMap<Person, Amount> personAmountHash;
+    private HashMap<Person, Amount> personAmountMap;
 
     /**
      * Constructs a hash map with a person as the key and the amount they paid as the value.
@@ -66,9 +66,9 @@ public class LoanSplitCommand extends Command {
             throw new CommandException(MESSAGE_DUPLICATE_PERSONS);
         }
 
-        this.personAmountHash = new HashMap<Person, Amount>();
+        this.personAmountMap = new HashMap<Person, Amount>();
         for (int i = 0; i < persons.size(); i++) {
-            personAmountHash.put(persons.get(i), amounts.get(i));
+            personAmountMap.put(persons.get(i), amounts.get(i));
         }
     }
 
@@ -76,68 +76,75 @@ public class LoanSplitCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireAllNonNull(model, model.getLoansManager());
 
-        long totalAmount = personAmountHash.values().stream()
+        long totalAmount = personAmountMap.values().stream()
                 .map(Amount::toLong)
                 .reduce(Long::sum)
-                .orElse(0L);
+                .orElseThrow(() -> new CommandException(MESSAGE_INVALID_TOTAL));
 
-        if (totalAmount <= 0) {
-            throw new CommandException(MESSAGE_INVALID_TOTAL);
+        long perPerson = totalAmount / personAmountMap.size();
+
+        List<Participant> participants = new ArrayList<Participant>();
+        for (Map.Entry<Person, Amount> personAmountEntry : personAmountMap.entrySet()) {
+            long amountPaid = personAmountEntry.getValue().toLong();
+            long balance = amountPaid - perPerson;
+            participants.add(new Participant(personAmountEntry.getKey(), balance));
         }
 
-        long perPerson = totalAmount / personAmountHash.size();
+        if (participants.stream().allMatch(p -> p.getBalance() >= 0)) {
+            throw new CommandException(MESSAGE_ALREADY_SPLIT);
+        }
 
-        List<Map.Entry<Person, Amount>> personsOverpaid = personAmountHash.entrySet().stream()
-                .filter(personAmountEntry -> personAmountEntry.getValue().toLong() > perPerson)
-                .collect(Collectors.toList());
-        List<Map.Entry<Person, Amount>> personsUnderpaid = personAmountHash.entrySet().stream()
-                .filter(personAmountEntry -> personAmountEntry.getValue().toLong() < perPerson)
-                .collect(Collectors.toList());
+        SortBalanceIncreasing balanceIncreasing = new SortBalanceIncreasing();
+        List<LoanSplit> splitList = calculateSplitList(participants, balanceIncreasing);
 
-        List<LoanSplit> splitList = getLoanSplitList(personsOverpaid, personsUnderpaid, perPerson);
-
-        model.getLoansManager().setSplitList(splitList);
-        return new CommandResult(model.getLoansManager().getSplitList().toString(), CommandCategory.LOAN_SPLIT);
+        LoansManager loansManager = model.getLoansManager();
+        loansManager.setSplitList(splitList);
+        return new CommandResult(loansManager.getSplitList().toString(), CommandCategory.LOAN_SPLIT);
     }
 
     /**
-     * Returns a list of {@code LoanSplit} to replace the existing one in the loans manager.
-     * Each {@code LoanSplit} contains information about a creditor, debtor, and amount owed between the two.
-     * @param personsOverpaidList The persons who over-paid, relative to the correct amount {@code perPerson}.
-     * @param personsUnderpaidList The persons who under-paid, relative to the correct amount {@code perPerson}.
-     * @param perPerson The correct that should be paid per person for the entire payment.
-     * @throws CommandException If an error occurs during the calculations.
+     * Constructs a list of {@code LoanSplit} objects, indicating which person owes which person what amount.
+     * @param participants The list of particpants to calculate the debts from.
+     * @param balanceIncreasing A {@code Comparator} to sort participants in order of increasing balance.
+     * @return The list of {@code LoanSplit} objects.
      */
-    private List<LoanSplit> getLoanSplitList(
-            List<Map.Entry<Person, Amount>> personsOverpaidList,
-            List<Map.Entry<Person, Amount>> personsUnderpaidList, long perPerson) throws CommandException {
-        requireAllNonNull(personsOverpaidList, personsUnderpaidList, perPerson);
+    private List<LoanSplit> calculateSplitList(
+            List<Participant> participants, SortBalanceIncreasing balanceIncreasing) {
+        requireAllNonNull(participants, balanceIncreasing);
 
-        long numPersonsUnderpaid = personsUnderpaidList.size();
+        List<LoanSplit> splitList = new ArrayList<LoanSplit>();
+        while (participants.size() > 1) {
+            participants.sort(balanceIncreasing); // participants MUST be sorted in the order of increasing balance
 
-        /*
-        This function acts on a person who over-paid, returning a stream of LoanSplit objects.
-        Each LoanSplit object in the stream indicates who owes them money (and how much).
-         */
-        Function<Map.Entry<Person, Amount>, Stream<LoanSplit>> calculateLoanSplit = personOverpaidEntry -> {
-            List<LoanSplit> loanSplitList = new ArrayList<LoanSplit>();
-            for (Map.Entry<Person, Amount> personUnderpaidEntry : personsUnderpaidList) {
-                long creditorPaid = personOverpaidEntry.getValue().toLong();
-                long debtorPaid = personUnderpaidEntry.getValue().toLong();
-                long amountOwed = ((creditorPaid - perPerson) / numPersonsUnderpaid) - debtorPaid;
-                loanSplitList.add(new LoanSplit(
-                        personUnderpaidEntry.getKey(),
-                        personOverpaidEntry.getKey(),
-                        new Amount(amountOwed)));
+            Participant debtor = participants.get(0);
+            Participant creditor = participants.get(participants.size() - 1);
+
+            // transfer money between the biggest debtor and biggest creditor
+            long amountTransferred = 0;
+            long debtorBalancePositive = debtor.getBalance() * -1;
+            if (debtorBalancePositive >= creditor.getBalance()) {
+                amountTransferred = creditor.getBalance();
+                debtor.setBalance(debtor.getBalance() + creditor.getBalance());
+                creditor.setBalance(0);
+            } else if (debtorBalancePositive < creditor.getBalance()) {
+                amountTransferred = debtorBalancePositive;
+                creditor.setBalance(creditor.getBalance() - debtor.getBalance());
+                debtor.setBalance(0);
             }
-            return loanSplitList.stream();
-        };
 
-        return personsOverpaidList
-                .stream()
-                .map(calculateLoanSplit)
-                .flatMap(Function.identity())
-                .collect(Collectors.toList());
+            if (debtor.getBalance() == 0) {
+                participants.remove(0);
+            }
+            if (!participants.isEmpty() && creditor.getBalance() == 0) {
+                participants.remove(participants.size() - 1);
+            }
+
+            if (amountTransferred != 0) {
+                splitList.add(new LoanSplit(debtor.getPerson(), creditor.getPerson(), new Amount(amountTransferred)));
+            }
+        }
+
+        return splitList;
     }
 
     @Override
@@ -151,6 +158,63 @@ public class LoanSplitCommand extends Command {
         }
 
         LoanSplitCommand otherCommand = (LoanSplitCommand) other;
-        return personAmountHash.equals(otherCommand.personAmountHash);
+        return personAmountMap.equals(otherCommand.personAmountMap);
+    }
+
+    /**
+     * A utility class that holds a {@code Person} and their {@code balance} relative to the total pool of money.
+     * This class is used only for the calculations involved in splitting a group payment.
+     */
+    private class Participant {
+
+        private final Person person;
+        private long balance;
+
+        public Participant(Person person, long balance) {
+            this.person = person;
+            this.balance = balance;
+        }
+
+        public Person getPerson() {
+            return person;
+        }
+
+        public long getBalance() {
+            return balance;
+        }
+
+        public void setBalance(long balance) {
+            this.balance = balance;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("<%s, %d>", person, balance);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+
+            if (!(other instanceof Participant)) {
+                return false;
+            }
+
+            Participant otherParticipant = (Participant) other;
+            return person.equals(otherParticipant.person)
+                    && balance == otherParticipant.balance;
+        }
+    }
+
+    /**
+     * A {@code Comparator} to sort participants in order of increasing balance.
+     */
+    public static class SortBalanceIncreasing implements Comparator<Participant> {
+        @Override
+        public int compare(Participant p1, Participant p2) {
+            return (int) (p1.getBalance() - p2.getBalance());
+        }
     }
 }
