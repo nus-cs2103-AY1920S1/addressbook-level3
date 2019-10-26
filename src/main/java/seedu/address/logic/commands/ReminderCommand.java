71983fbffb77ff32a1d6a99713e2e0ca7e26edba
ReignOfComputer <roc@reignofcomputer.com>
2019-10-27 02:00:23 +0800

    fix: checkstyle

--- src/main/java/seedu/address/logic/commands/ReminderCommand.java
+++ src/main/java/seedu/address/logic/commands/ReminderCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DAYS;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
