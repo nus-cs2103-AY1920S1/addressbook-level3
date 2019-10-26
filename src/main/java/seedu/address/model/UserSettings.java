7a594b7ba2b317d9fa880d803224e450738c1ffb
abhinav <ambhinav@yahoo.com>
2019-10-26 14:31:50 +0800

    implemented settings in model

---
+++ src/main/java/seedu/address/model/UserSettings.java
@@ -0,0 +1,98 @@
+package seedu.address.model;
+
+import static java.util.Objects.requireNonNull;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+import seedu.address.model.settings.ClockFormat;
+import seedu.address.model.settings.Theme;
+
+/**
+ * Represents user settings of +Work.
+ */
+public class UserSettings implements ReadOnlyUserSettings {
+
+    private Path userSettingsFilePath = Paths.get("data", "plusworksettings.json");
+
+    // default settings
+    private Theme theme = Theme.DARK;
+    private ClockFormat clockFormat = ClockFormat.TWENTY_FOUR;
+
+    /**
+     * Creates a {@code UserSettings} with default values.
+     */
+    public UserSettings() {};
+
+    /**
+     * Creates a {@code UserSettings} with the settings in {@code userSettings}
+     */
+    public UserSettings(ReadOnlyUserSettings userSettings) {
+        this();
+        resetData(userSettings);
+    }
+
+    public void resetData(ReadOnlyUserSettings newUserSettings) {
+        requireNonNull(newUserSettings);
+        setUserSettingsFilePath(newUserSettings.getUserSettingsFilePath());
+    }
+
+    @Override
+    public Path getUserSettingsFilePath() {
+        return userSettingsFilePath;
+    }
+
+    public void setUserSettingsFilePath(Path newUserSettingsFilePath) {
+        requireNonNull(userSettingsFilePath);
+        userSettingsFilePath = newUserSettingsFilePath;
+    }
+
+    @Override
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme newTheme) {
+        theme = newTheme;
+    }
+
+    @Override
+    public ClockFormat getClockFormat() {
+        return clockFormat;
+    }
+
+    public void setClockFormat(ClockFormat newClockFormat) {
+        clockFormat = newClockFormat;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof UserSettings)) { //this handles null as well.
+            return false;
+        }
+
+        UserSettings o = (UserSettings) other;
+
+        return theme.equals(o.theme)
+                && clockFormat.equals(o.clockFormat)
+                && userSettingsFilePath.equals(o.userSettingsFilePath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userSettingsFilePath);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Current theme : " + theme.getDisplayName());
+        sb.append("Current clockFormat : " + clockFormat.getDisplayName());
+        sb.append("\nLocal data file location : " + userSettingsFilePath);
+        return sb.toString();
+    }
+}
