9339d53f6b08e43f01eba8ef75d716beb136b1d1
Leonard Tay <leonardtay748@gmail.com>
2019-10-26 19:24:18 +0800

    Trim StatsCommand to omit displaying statistic numbers.

--- src/main/java/seedu/address/logic/commands/StatsCommand.java
+++ src/main/java/seedu/address/logic/commands/StatsCommand.java
@@ -12,43 +12,14 @@ import seedu.address.model.flashcard.RatingContainsKeywordPredicate;
 public class StatsCommand extends Command {
 
     public static final String COMMAND_WORD = "stats";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays the number of GOOD, HARD & EASY flashcards ";
-
-    private final RatingContainsKeywordPredicate predicateGood = new RatingContainsKeywordPredicate("good");
-    private final RatingContainsKeywordPredicate predicateHard = new RatingContainsKeywordPredicate("hard");
-    private final RatingContainsKeywordPredicate predicateEasy = new RatingContainsKeywordPredicate("easy");
+    public static final String MESSAGE_USAGE = "Displays the number of GOOD, HARD & EASY FlashCards";
 
     ////////// Should return a CommandResult with the statistics ////////////////
     @Override
     public CommandResult execute(Model model)  {
         requireNonNull(model);
 
-        int numGood = 0;
-        int numHard = 0;
-        int numEasy = 0;
-
-        model.updateFilteredFlashCardList(predicateGood);
-        numGood = model.getFilteredFlashCardList().size();
-        model.updateFilteredFlashCardList(predicateHard);
-        numHard = model.getFilteredFlashCardList().size();
-        model.updateFilteredFlashCardList(predicateEasy);
-        numEasy = model.getFilteredFlashCardList().size();
-
-        int[] stats = model.getStats();
-
-        StringBuilder sb = new StringBuilder("STATISTICS" + System.getProperty("line.separator"));
-        sb.append(System.getProperty("line.separator"));
-        sb.append("Total: ");
-        sb.append("Good (" + numGood + ")  ");
-        sb.append("Hard (" + numHard + ")  ");
-        sb.append("Easy (" + numEasy + ")" + System.getProperty("line.separator"));
-        sb.append(System.getProperty("line.separator"));
-        sb.append("Completed in test: " );
-        sb.append("Good (" + stats[0] + ")  ");
-        sb.append("Hard (" + stats[1] + ")  ");
-        sb.append("Easy (" + stats[2] + ")  ");
-
-        return new CommandResult(sb.toString(), false, true, false);
+        return new CommandResult(MESSAGE_USAGE, false, true, false);
     }
 
 }
