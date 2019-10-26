30b39103d2b09e709bd3e79f0427f5c984727240
Ryan Tan Yu <tanyuryan@gmail.com>
2019-10-26 16:47:34 +0800

    fix trailing whitespace error

--- src/main/java/seedu/jarvis/model/ModelManager.java
+++ src/main/java/seedu/jarvis/model/ModelManager.java
@@ -693,7 +693,7 @@ public class ModelManager implements Model {
     public ObservableList<Task> getFilteredTaskList() {
         return planner.getFilteredTaskList();
     }
-  
+
     /**
      * Marks a {@code Task} at the specified {@code Index} as done
      * @param i {@code Index} of the {@code Task} to be marked as done
