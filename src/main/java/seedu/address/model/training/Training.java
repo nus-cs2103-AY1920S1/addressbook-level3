4508f6fa0388636967a7d22f0b43cf664b5bd466
Jeun Hoe Poon <e0325449@u.nus.edu>
2019-10-27 03:12:57 +0800

    Add tests for Training

--- src/main/java/seedu/address/model/training/Training.java
+++ src/main/java/seedu/address/model/training/Training.java
@@ -28,6 +28,7 @@ public class Training {
     public HashMap<Person, Boolean> getTrainingAttendance() {
         return this.trainingAttendance;
     }
+
     public boolean hasPerson(Person person) {
         return this.trainingAttendance.containsKey(person);
     }
@@ -35,8 +36,9 @@ public class Training {
     public boolean hasPersonAttended(Person person) {
         return this.trainingAttendance.get(person);
     }
+
     /**
-     * Returns a list of person who has attended this training
+     * Returns a list of Person who has attended this training
      */
     public List<Person> getAttended() {
         List<Person> listOfAttended = new ArrayList<>();
@@ -48,5 +50,20 @@ public class Training {
             }
         }
         return listOfAttended;
+    }
+
+    /**
+     * Returns a list of Person who has attended this training
+     */
+    public List<Person> getAbsentees() {
+        List<Person> listOfAbsentees = new ArrayList<>();
+        for (Map.Entry<Person, Boolean> set: this.trainingAttendance.entrySet()) {
+            Person name = set.getKey();
+            Boolean hasAttended = set.getValue();
+            if (!hasAttended) {
+                listOfAbsentees.add(name);
+            }
+        }
+        return listOfAbsentees;
     }
 }
--- src/test/java/seedu/address/model/training/TrainingTest.java
+++ src/test/java/seedu/address/model/training/TrainingTest.java
@@ -9,50 +9,144 @@ import static seedu.address.testutil.TypicalPersons.DANIEL;
 import static seedu.address.testutil.TypicalPersons.ELLE;
 import static seedu.address.testutil.TypicalPersons.FIONA;
 import static seedu.address.testutil.TypicalPersons.GEORGE;
+import static seedu.address.testutil.TypicalPersons.HOON;
+import static seedu.address.testutil.TypicalPersons.IDA;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.date.AthletickDate;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.TypicalTraining;
 
 class TrainingTest {
 
     @Test
     public void test_validDate_returnTrue() {
-        String date = new Date().toString();
-        Training training = new Training(date, new TypicalTraining().getTraining());
-        assertTrue(date.equals(training.getDate()));
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            Training training = new Training(date, new TypicalTraining().getTraining());
+            assertTrue(date.equals(training.getDate()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void test_validTrainingAttendance_returnTrue() {
-        String date = new Date().toString();
-        TypicalTraining trainingGroup = new TypicalTraining();
-        Training training = new Training(date, trainingGroup.getTraining());
-        assertTrue(trainingGroup.getTraining().equals(training.getTrainingAttendance()));
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
+            assertTrue(trainingGroup.getTraining().equals(training.getTrainingAttendance()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void test_personHasAttendance_returnTrue() {
-        String date = new Date().toString();
-        TypicalTraining trainingGroup = new TypicalTraining();
-        Training training = new Training(date, trainingGroup.getTraining());
+    public void test_personExists() {
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
 
-        assertTrue(training.getPersonAttendance(ALICE));
-        assertTrue(training.getPersonAttendance(BENSON));
-        assertTrue(training.getPersonAttendance(ELLE));
+            assertTrue(training.hasPerson(ALICE));
+            assertTrue(training.hasPerson(BENSON));
+            assertTrue(training.hasPerson(CARL));
+            assertTrue(training.hasPerson(DANIEL));
+            assertTrue(training.hasPerson(ELLE));
+            assertTrue(training.hasPerson(FIONA));
+            assertTrue(training.hasPerson(GEORGE));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void test_personHasNoAttendance_returnFalse() {
-        String date = new Date().toString();
-        TypicalTraining trainingGroup = new TypicalTraining();
-        Training training = new Training(date, trainingGroup.getTraining());
+    public void test_personDoesNotExists() {
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
 
-        assertFalse(training.getPersonAttendance(CARL));
-        assertFalse(training.getPersonAttendance(GEORGE));
-        assertFalse(training.getPersonAttendance(FIONA));
-        assertFalse(training.getPersonAttendance(DANIEL));
+            assertFalse(training.hasPerson(HOON));
+            assertFalse(training.hasPerson(IDA));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_personAttended() {
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
+
+            assertTrue(training.hasPersonAttended(ALICE));
+            assertTrue(training.hasPersonAttended(BENSON));
+            assertTrue(training.hasPersonAttended(ELLE));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_personAbsent() {
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
+
+            assertFalse(training.hasPersonAttended(CARL));
+            assertFalse(training.hasPersonAttended(GEORGE));
+            assertFalse(training.hasPersonAttended(FIONA));
+            assertFalse(training.hasPersonAttended(DANIEL));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_getAttended() {
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
+
+            List<Person> attended = training.getAttended();
+            List<Person> selfCreatedAttended = new ArrayList<>();
+            selfCreatedAttended.add(ALICE);
+            selfCreatedAttended.add(BENSON);
+            selfCreatedAttended.add(ELLE);
+
+            assertTrue(attended.containsAll(selfCreatedAttended));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_getAbsent() {
+        try {
+            AthletickDate date = new AthletickDate("20012019");
+            TypicalTraining trainingGroup = new TypicalTraining();
+            Training training = new Training(date, trainingGroup.getTraining());
+
+            List<Person> absentees = training.getAbsentees();
+            List<Person> selfCreatedAbsentees = new ArrayList<>();
+            selfCreatedAbsentees.add(CARL);
+            selfCreatedAbsentees.add(GEORGE);
+            selfCreatedAbsentees.add(FIONA);
+            selfCreatedAbsentees.add(DANIEL);
+
+            assertTrue(absentees.containsAll(selfCreatedAbsentees));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
