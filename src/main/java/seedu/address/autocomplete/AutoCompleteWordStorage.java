7ea3ffb9e3737b8a77978187a93c77debb3c8cf4
Francis <francislow_6@hotmail.com>
2019-10-26 23:04:46 +0800

    Remove hardcoding some commands and object words

--- src/main/java/seedu/address/autocomplete/AutoCompleteWordStorage.java
+++ src/main/java/seedu/address/autocomplete/AutoCompleteWordStorage.java
@@ -2,11 +2,28 @@ package seedu.address.autocomplete;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.ExportAllCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ImportReplaceCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.UndoCommand;
 
 /**
  * Class that initialise and stores all list
  */
 public class AutoCompleteWordStorage {
+    public static final String VISITOBJECTWORD = "visit";
+    public static final String PATIENTOBJECTWORD = "pat";
+    public static final String APPLICATIONOBJECTWORD = "app";
+    public static final String MEDOBJECTWORD = "med";
+    public static final String MEDCONOBJECTWORD = "medcon";
+    public static final String APPOINTMENTBJECTWORD = "appt";
+
     private ObservableList<AutoCompleteWord> oListAllObjectWord;
     private ObservableList<AutoCompleteWord> oListAllCommandWord;
     private ObservableList<AutoCompleteWord> oListAllPrefixWord;
@@ -22,25 +39,25 @@ public class AutoCompleteWordStorage {
      */
     private ObservableList<AutoCompleteWord> initAllCommandWordList() {
         ObservableList<AutoCompleteWord> oListAllCommandWord = FXCollections.observableArrayList();
-        oListAllCommandWord.add(new CommandWord("visit", "end", false, false));
-        oListAllCommandWord.add(new CommandWord("visit", "start", false, true));
-        oListAllCommandWord.add(new CommandWord("visit", "now-show", false, false));
-        oListAllCommandWord.add(new CommandWord("visit", "now-update", false, true));
+        oListAllCommandWord.add(new CommandWord(VISITOBJECTWORD, "end", false, false));
+        oListAllCommandWord.add(new CommandWord(VISITOBJECTWORD, "start", false, true));
+        oListAllCommandWord.add(new CommandWord(VISITOBJECTWORD, "now-show", false, false));
+        oListAllCommandWord.add(new CommandWord(VISITOBJECTWORD, "now-update", false, true));
 
 
 
-        oListAllCommandWord.add(new CommandWord("patient", "list", false, false));
-        oListAllCommandWord.add(new CommandWord("patient", "find", false, true));
-        oListAllCommandWord.add(new CommandWord("patient", "view", true, false));
-        oListAllCommandWord.add(new CommandWord("patient", "add-medcon", false, true));
-        oListAllCommandWord.add(new CommandWord("app", "undo", false, false));
-        oListAllCommandWord.add(new CommandWord("app", "redo", false, false));
-        oListAllCommandWord.add(new CommandWord("app", "import-replace", false, true));
-        oListAllCommandWord.add(new CommandWord("app", "export-all", false, false));
-        oListAllCommandWord.add(new CommandWord("app", "help", false, false));
-        oListAllCommandWord.add(new CommandWord("medcon", "delete", true, false));
-        oListAllCommandWord.add(new CommandWord("med", "add", false, true));
-        oListAllCommandWord.add(new CommandWord("appt", "edit", true, true));
+        oListAllCommandWord.add(new CommandWord(PATIENTOBJECTWORD, ListCommand.COMMAND_WORD, false, false));
+        oListAllCommandWord.add(new CommandWord(PATIENTOBJECTWORD, FindCommand.COMMAND_WORD, false, true));
+        oListAllCommandWord.add(new CommandWord(PATIENTOBJECTWORD, "view", true, false));
+        oListAllCommandWord.add(new CommandWord(PATIENTOBJECTWORD, "add-medcon", false, true));
+        oListAllCommandWord.add(new CommandWord(APPLICATIONOBJECTWORD, UndoCommand.COMMAND_WORD, false, false));
+        oListAllCommandWord.add(new CommandWord(APPLICATIONOBJECTWORD, RedoCommand.COMMAND_WORD, false, false));
+        oListAllCommandWord.add(new CommandWord(APPLICATIONOBJECTWORD, ImportReplaceCommand.COMMAND_WORD, false, true));
+        oListAllCommandWord.add(new CommandWord(APPLICATIONOBJECTWORD, ExportAllCommand.COMMAND_WORD, false, false));
+        oListAllCommandWord.add(new CommandWord(APPLICATIONOBJECTWORD, HelpCommand.COMMAND_WORD, false, false));
+        oListAllCommandWord.add(new CommandWord(MEDCONOBJECTWORD, DeleteCommand.COMMAND_WORD, true, false));
+        oListAllCommandWord.add(new CommandWord(MEDOBJECTWORD, AddCommand.COMMAND_WORD, false, true));
+        oListAllCommandWord.add(new CommandWord(APPOINTMENTBJECTWORD, EditCommand.COMMAND_WORD, true, true));
 
         return oListAllCommandWord;
     }
@@ -50,21 +67,21 @@ public class AutoCompleteWordStorage {
      */
     private ObservableList<AutoCompleteWord> initAllPrefixWordList() {
         ObservableList<AutoCompleteWord> oListAllPrefixWord = FXCollections.observableArrayList();
-        oListAllPrefixWord.add(new PrefixWord("visit", "start", "p/"));
-        oListAllPrefixWord.add(new PrefixWord("visit", "now-update", "t/"));
-        oListAllPrefixWord.add(new PrefixWord("visit", "now-update", "d/"));
-        oListAllPrefixWord.add(new PrefixWord("visit", "now-update", "ud/"));
-        oListAllPrefixWord.add(new PrefixWord("visit", "now-update", "r/"));
+        oListAllPrefixWord.add(new PrefixWord(VISITOBJECTWORD, "start", "p/"));
+        oListAllPrefixWord.add(new PrefixWord(VISITOBJECTWORD, "now-update", "t/"));
+        oListAllPrefixWord.add(new PrefixWord(VISITOBJECTWORD, "now-update", "d/"));
+        oListAllPrefixWord.add(new PrefixWord(VISITOBJECTWORD, "now-update", "ud/"));
+        oListAllPrefixWord.add(new PrefixWord(VISITOBJECTWORD, "now-update", "r/"));
 
 
 
 
-        oListAllPrefixWord.add(new PrefixWord("patient", "add-medcon", "t/"));
-        oListAllPrefixWord.add(new PrefixWord("patient", "find", "t/"));
-        oListAllPrefixWord.add(new PrefixWord("medcon", "find", "c/"));
-        oListAllPrefixWord.add(new PrefixWord("patient", "add-medcon", "c/"));
-        oListAllPrefixWord.add(new PrefixWord("med", "add", "testt/"));
-        oListAllPrefixWord.add(new PrefixWord("appt", "edit", "c/"));
+        oListAllPrefixWord.add(new PrefixWord(PATIENTOBJECTWORD, "add-medcon", "t/"));
+        oListAllPrefixWord.add(new PrefixWord(PATIENTOBJECTWORD, FindCommand.COMMAND_WORD, "t/"));
+        oListAllPrefixWord.add(new PrefixWord(MEDCONOBJECTWORD, FindCommand.COMMAND_WORD, "c/"));
+        oListAllPrefixWord.add(new PrefixWord(PATIENTOBJECTWORD, "add-medcon", "c/"));
+        oListAllPrefixWord.add(new PrefixWord(MEDOBJECTWORD, AddCommand.COMMAND_WORD, "testt/"));
+        oListAllPrefixWord.add(new PrefixWord(APPOINTMENTBJECTWORD, EditCommand.COMMAND_WORD, "c/"));
 
         return oListAllPrefixWord;
     }
@@ -74,12 +91,12 @@ public class AutoCompleteWordStorage {
      */
     private ObservableList<AutoCompleteWord> initAllObjectWordList() {
         ObservableList<AutoCompleteWord> oListAllObjectWord = FXCollections.observableArrayList();
-        oListAllObjectWord.add(new ObjectWord("patient"));
-        oListAllObjectWord.add(new ObjectWord("med"));
-        oListAllObjectWord.add(new ObjectWord("medcon"));
-        oListAllObjectWord.add(new ObjectWord("app"));
-        oListAllObjectWord.add(new ObjectWord("visit"));
-        oListAllObjectWord.add(new ObjectWord("appt"));
+        oListAllObjectWord.add(new ObjectWord(PATIENTOBJECTWORD));
+        oListAllObjectWord.add(new ObjectWord(MEDOBJECTWORD));
+        oListAllObjectWord.add(new ObjectWord(MEDCONOBJECTWORD));
+        oListAllObjectWord.add(new ObjectWord(APPLICATIONOBJECTWORD));
+        oListAllObjectWord.add(new ObjectWord(VISITOBJECTWORD));
+        oListAllObjectWord.add(new ObjectWord(APPOINTMENTBJECTWORD));
 
         return oListAllObjectWord;
     }
