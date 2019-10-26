fd328a6654e48e95d9040a44e82cd3178ba3ab68
sreesubbash <33561308+sreesubbash@users.noreply.github.com>
2019-10-26 10:39:04 +0800

    fix styles 1

--- src/main/java/seedu/address/logic/parser/load/RemoveCommandParser.java
+++ src/main/java/seedu/address/logic/parser/load/RemoveCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import seedu.address.logic.commands.loadcommands.CreateCommand;
 import seedu.address.logic.commands.loadcommands.RemoveCommand;
 import seedu.address.logic.parser.Parser;
-import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
