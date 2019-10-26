5dae27e89e981b6f581ff28e217dcabe3e68eacf
linyutinglyt <e0332737@u.nus.edu>
2019-10-26 16:27:38 +0800

    declare throwing of commandexception at method name

--- src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -15,11 +15,11 @@ public class CommandParserTestUtil {
      * Asserts that the parsing of {@code userInput} by {@code parser} is successful and the command created
      * equals to {@code expectedCommand}.
      */
-    public static void assertParseSuccess(Parser parser, String userInput, Command expectedCommand) {
+    public static void assertParseSuccess(Parser parser, String userInput, Command expectedCommand) throws CommandException {
         try {
             Command command = parser.parse(userInput);
             assertEquals(expectedCommand, command);
-        } catch (ParseException | CommandException pe) {
+        } catch (ParseException pe) {
             throw new IllegalArgumentException("Invalid userInput.", pe);
         }
     }
@@ -28,11 +28,11 @@ public class CommandParserTestUtil {
      * Asserts that the parsing of {@code userInput} by {@code parser} is unsuccessful and the error message
      * equals to {@code expectedMessage}.
      */
-    public static void assertParseFailure(Parser parser, String userInput, String expectedMessage) {
+    public static void assertParseFailure(Parser parser, String userInput, String expectedMessage) throws CommandException {
         try {
             parser.parse(userInput);
             throw new AssertionError("The expected ParseException was not thrown.");
-        } catch (ParseException | CommandException pe) {
+        } catch (ParseException pe) {
             assertEquals(expectedMessage, pe.getMessage());
         }
     }
