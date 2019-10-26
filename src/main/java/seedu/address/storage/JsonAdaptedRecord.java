1bec9a181e21e4e80013f4a60e869f1d464dd132
hellodommy <dominique@u.nus.edu>
2019-10-26 23:54:20 +0800

    Add Json support for a Record

---
+++ src/main/java/seedu/address/storage/JsonAdaptedRecord.java
@@ -0,0 +1,56 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.performance.Record;
+
+/**
+ * Jackson-friendly version of {@link Record}.
+ */
+public class JsonAdaptedRecord {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Record's %s field is missing!";
+
+    private final String date;
+    private final String timing;
+
+    /**
+     * Constructs a {@code JsonAdaptedRecord} with the given record details.
+     */
+    @JsonCreator
+    public JsonAdaptedRecord(@JsonProperty("date") String date, @JsonProperty("timing") String timing) {
+        this.date = date;
+        this.timing = timing;
+    }
+
+    /**
+     * Converts a given {@code Record} into this class for Jackson use.
+     */
+    public JsonAdaptedRecord(Record source) {
+        date = source.getDate();
+        timing = source.getTiming();
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted record object into the model's {@code Record} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted record.
+     */
+    public Record toModelType() throws IllegalValueException {
+        if (date == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, "date"));
+        }
+
+        final String modelDate = date;
+
+        if (timing == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, "timing"));
+        }
+
+        final String modelTiming = date;
+
+        return new Record(modelDate, modelTiming);
+    }
+}
