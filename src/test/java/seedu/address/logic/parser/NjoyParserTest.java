07794b5cc6d823d9b98ed1a2b250163af2327e31
jeongyh99 <jeong.yuhan@gmail.com>
2019-10-26 15:30:09 +0800

    Fix test checkstyle

--- src/test/java/seedu/address/logic/parser/NjoyParserTest.java
+++ src/test/java/seedu/address/logic/parser/NjoyParserTest.java
@@ -2,7 +2,10 @@ package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.core.Messages.*;
+import static seedu.address.commons.core.Messages.EXCEL_FILE_ILLEGAL_INPUT;
+import static seedu.address.commons.core.Messages.EXCEL_FILE_NOT_FOUND;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
