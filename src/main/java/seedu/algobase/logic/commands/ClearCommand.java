70c0058235b053ba40332fd8162edea9807e8f54
Lu Wenqi <e0261893@u.nus.edu>
2019-10-26 22:50:39 +0800

    fix checkstyle issues

--- src/main/java/seedu/algobase/logic/commands/ClearCommand.java
+++ src/main/java/seedu/algobase/logic/commands/ClearCommand.java
@@ -11,10 +11,10 @@ import seedu.algobase.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_USAGE = COMMAND_WORD +
-            "Clears all entries from AlgoBase.\n" +
-            "Example: \n" +
-            COMMAND_WORD;
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + "Clears all entries from AlgoBase.\n"
+            + "Example: \n"
+            + COMMAND_WORD;
     public static final String MESSAGE_SUCCESS = "AlgoBase has been cleared!";
 
 
