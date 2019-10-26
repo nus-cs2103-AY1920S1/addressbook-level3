7ab2f4b2802207032f5934da39cb2a9d9f77f2a5
Sheu Xiang <sheuxiang@gmail.com>
2019-10-26 18:27:44 +0800

    Add AccommodationBuilder to TestUtil (#107)
    * Add AccommodationBuilder
    
    * Modify from ContactBuilder to AccommodationBuilder
    
    * Change the way Contact is added
    
    * Add contact get/set to AccommodationBuilder
    
    * Rename static variables

---
+++ src/test/java/seedu/address/testutil/accommodation/AccommodationBuilder.java
@@ -0,0 +1,97 @@
+package seedu.address.testutil.accommodation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.contact.Contact;
+import seedu.address.model.field.Address;
+import seedu.address.model.field.Name;
+import seedu.address.model.itineraryitem.accommodation.Accommodation;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
+import seedu.address.testutil.ContactBuilder;
+
+/**
+ * A utility class to help with building Accommodation objects.
+ */
+public class AccommodationBuilder {
+
+    public static final String DEFAULT_NAME = "Alice Pauline Home";
+    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+
+    public static final String DEFAULT_ACCOMMODATION_NAME = "Sam Smith";
+    public static final String DEFAULT_ACCOMMODATION_PHONE = "91170081";
+    public static final String DEFAULT_ACCOMMODATION_EMAIL = "sam1987@gmail.com";
+    public static final String DEFAULT_ACCOMMODATION_ADDRESS = "456, Bukit Batok st 53, #03-21";
+
+    public static final String[] DEFAULT_ACCOMMODATION_TAGS = {"Jurong", "Cool", "Cheap"};
+
+    private Name name;
+    private Address address;
+    private Contact contact;
+    private Set<Tag> tags;
+
+    public AccommodationBuilder() {
+        name = new Name(DEFAULT_NAME);
+        address = new Address(DEFAULT_ADDRESS);
+        contact = new ContactBuilder().withName(DEFAULT_ACCOMMODATION_NAME).withPhone(DEFAULT_ACCOMMODATION_PHONE)
+                .withEmail(DEFAULT_ACCOMMODATION_EMAIL).withAddress(DEFAULT_ACCOMMODATION_ADDRESS).build();
+        tags = SampleDataUtil.getTagSet(DEFAULT_ACCOMMODATION_TAGS);
+    }
+
+    /**
+     * Initializes the AccommodationBuilder with the data of {@code contactToCopy}.
+     */
+    public AccommodationBuilder(Accommodation accommodationToCopy) {
+        name = accommodationToCopy.getName();
+        address = accommodationToCopy.getAddress();
+        contact = accommodationToCopy.getContact().orElse(null);
+        tags = new HashSet<>(accommodationToCopy.getTags());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Contact} that we are building.
+     */
+    public AccommodationBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Address} of the {@code Contact} that we are building.
+     */
+    public AccommodationBuilder withAddress(String address) {
+        this.address = new Address(address);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Contact} that we are building.
+     */
+    public AccommodationBuilder withContact(String name, String phone, String email, String address, String... tags) {
+        this.contact = new ContactBuilder().withName(name).withEmail(email)
+                .withPhone(phone).withAddress(address).withTags(tags).build();
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Contact} that we are building.
+     */
+    public AccommodationBuilder withContact(Contact contact) {
+        this.contact = contact;
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Contact} that we are building.
+     */
+    public AccommodationBuilder withTags(String ... tags) {
+        this.tags = SampleDataUtil.getTagSet(tags);
+        return this;
+    }
+
+    public Accommodation build() {
+        return new Accommodation(name, address, contact, tags);
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/accommodation/TypicalAccommodations.java
@@ -0,0 +1,71 @@
+package seedu.address.testutil.accommodation;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.itineraryitem.accommodation.Accommodation;
+import seedu.address.testutil.TypicalContacts;
+
+/**
+ * A utility class containing a list of {@code Contact} objects to be used in tests.
+ */
+public class TypicalAccommodations {
+
+    public static final Accommodation ALICE = new AccommodationBuilder().withName("Alice Pauline")
+            .withAddress("123, Jurong West Ave 6, #08-111").withContact(TypicalContacts.ALICE)
+            .withTags("Jurong", "Cool", "Cheap").build();
+    public static final Accommodation BENSON = new AccommodationBuilder().withName("Benson Meier Home")
+            .withAddress("311, Clementi Ave 2, #02-25")
+            .withContact(TypicalContacts.BENSON).withTags("Near Mrt", "Cozy").build();
+    public static final Accommodation CARL = new AccommodationBuilder().withName("Carl Kurz Home")
+            .withContact(TypicalContacts.CARL).withAddress("wall street").build();
+    public static final Accommodation DANIEL = new AccommodationBuilder().withName("Daniel Meier Home")
+            .withContact(TypicalContacts.DANIEL).withAddress("10th street").withTags("friends").build();
+    public static final Accommodation ELLE = new AccommodationBuilder().withName("Elle Meyer Home")
+            .withContact(TypicalContacts.ELLE).withAddress("michegan ave").build();
+    public static final Accommodation FIONA = new AccommodationBuilder().withName("Fiona Kunz Home")
+            .withContact(TypicalContacts.FIONA).withAddress("little tokyo").build();
+    public static final Accommodation GEORGE = new AccommodationBuilder().withName("George Best Home")
+            .withContact(TypicalContacts.GEORGE).withAddress("4th street").build();
+
+    // Manually added
+    public static final Accommodation HOON = new AccommodationBuilder().withName("Hoon Meier")
+            .withContact(TypicalContacts.HOON).withAddress("little india").build();
+    public static final Accommodation IDA = new AccommodationBuilder().withName("Ida Mueller")
+            .withContact(TypicalContacts.IDA).withAddress("chicago ave").build();
+
+    // Manually added - Contact's details found in {@code CommandTestUtil}
+    public static final Accommodation AMY = new AccommodationBuilder().withName(VALID_NAME_AMY)
+            .withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
+    public static final Accommodation BOB = new AccommodationBuilder().withName(VALID_NAME_BOB)
+            .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalAccommodations() {} // prevents instantiation
+
+    /**
+     * Returns an {@code Model} with all the typical accommodations.
+     */
+    public static Model getTypicalAccommodations() {
+        Model ab = new ModelManager();
+        for (Accommodation accommodation : getTypicalContacts()) {
+            ab.addAccommodation(accommodation);
+        }
+        return ab;
+    }
+
+    public static List<Accommodation> getTypicalContacts() {
+        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
+    }
+}
