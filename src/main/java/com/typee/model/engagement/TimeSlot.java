83348cf8b1ae4e164c63c8fc1fda836e5c0c16bc
uggi121 <sudharshanmadhavan@outlook.com>
2019-10-27 15:39:54 +0800

    Add timeslot

---
+++ src/main/java/com/typee/model/engagement/TimeSlot.java
@@ -0,0 +1,14 @@
+package com.typee.model.engagement;
+
+import java.time.LocalDateTime;
+
+public class TimeSlot {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
+        assert startTime !=
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
