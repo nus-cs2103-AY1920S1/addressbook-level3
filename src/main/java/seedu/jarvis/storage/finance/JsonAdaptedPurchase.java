e4707719dd9c6974a595c656c7ecd592934784dd
marcfyk <marcjohnfong@gmail.com>
2019-10-27 14:54:08 +0800

    Add JsonAdaptedPurchase and tests

--- src/main/java/seedu/jarvis/storage/finance/JsonAdaptedPurchase.java
+++ src/main/java/seedu/jarvis/storage/finance/JsonAdaptedPurchase.java
@@ -6,6 +6,7 @@ import seedu.jarvis.commons.exceptions.IllegalValueException;
 import seedu.jarvis.model.finance.purchase.Purchase;
 import seedu.jarvis.model.finance.purchase.PurchaseDescription;
 import seedu.jarvis.model.finance.purchase.PurchaseMoneySpent;
+import seedu.jarvis.storage.JsonAdapter;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -13,7 +14,7 @@ import java.time.format.DateTimeParseException;
 /**
  * Jackson-friendly version of {@link Purchase}.
  */
-public class JsonAdaptedPurchase {
+public class JsonAdaptedPurchase implements JsonAdapter<Purchase> {
 
     public static final String MESSAGE_INVALID_ATTRIBUTES = "Invalid purchase attributes.";
 
@@ -21,6 +22,13 @@ public class JsonAdaptedPurchase {
     private final String moneySpent;
     private final String date;
 
+    /**
+     * Constructs a {@code JsonAdaptedPurchase} with the given purchase details.
+     *
+     * @param description {@code PurchaseDescription} of the purchase.
+     * @param moneySpent {@code PurchaseMoneySpent} of the purchase.
+     * @param date {@code LocalDate} of the purchase.
+     */
     @JsonCreator
     public JsonAdaptedPurchase(@JsonProperty("description") String description,
                                @JsonProperty("moneySpent") String moneySpent, @JsonProperty("date") String date) {
@@ -29,12 +37,24 @@ public class JsonAdaptedPurchase {
         this.date = date;
     }
 
+    /**
+     * Converts a given {@code Purchase} into this class for Jackson use.
+     *
+     * @param purchase {@code Purchase} to be used to construct the {@code JsonAdaptedPurchase}.
+     */
     public JsonAdaptedPurchase(Purchase purchase) {
         description = purchase.getDescription().purchaseDescription;
         moneySpent = purchase.getMoneySpent().toString();
         date = purchase.getDateOfPurchase().format(Purchase.getDateFormat());
     }
 
+    /**
+     * Converts this Jackson-friendly adapted {@code Purchase} object into the model's {@code Purchase} object.
+     *
+     * @return {@code Purchase} of the Jackson-friendly adapted {@code Purchase}.
+     * @throws IllegalValueException If there were any data constraints violated in the adapted {@code Purchase}.
+     */
+    @Override
     public Purchase toModelType() throws IllegalValueException {
         boolean isValidDescription = description != null && PurchaseDescription.isValidDescription(description);
         boolean isValidMoneySpent = moneySpent != null && PurchaseMoneySpent.isValidAmount(moneySpent);
