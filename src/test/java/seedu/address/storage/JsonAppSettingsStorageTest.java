c90c570d59853234581250760211beae7a329cdc
chrischenhui <e0322956@u.nus.edu>
2019-10-27 23:54:25 +0800

    Fix some test

--- src/test/java/seedu/address/storage/JsonAppSettingsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonAppSettingsStorageTest.java
@@ -29,11 +29,6 @@ public class JsonAppSettingsStorageTest {
         assertThrows(NullPointerException.class, () -> readUserPrefs(null));
     }
 
-    private Optional<UserPrefs> readUserPrefs(String userPrefsFileInTestDataFolder) throws DataConversionException {
-        Path prefsFilePath = addToTestDataPathIfNotNull(userPrefsFileInTestDataFolder);
-        return new JsonUserPrefsStorage(prefsFilePath).readUserPrefs(prefsFilePath);
-    }
-
     @Test
     public void readUserPrefs_missingFile_emptyResult() throws DataConversionException {
         assertFalse(readUserPrefs("NonExistentFile.json").isPresent());
@@ -44,38 +39,10 @@ public class JsonAppSettingsStorageTest {
         assertThrows(DataConversionException.class, () -> readUserPrefs("NotJsonFormatUserPrefs.json"));
     }
 
-    private Path addToTestDataPathIfNotNull(String userPrefsFileInTestDataFolder) {
-        return userPrefsFileInTestDataFolder != null
-                ? TEST_DATA_FOLDER.resolve(userPrefsFileInTestDataFolder)
-                : null;
-    }
-
-    //    @Test
-    //    public void readUserPrefs_fileInOrder_successfullyRead() throws DataConversionException {
-    //        UserPrefs expected = getTypicalUserPrefs();
-    //        UserPrefs actual = readUserPrefs("TypicalUserPref.json").get();
-    //        assertEquals(expected, actual);
-    //    }
-
     @Test
     public void readUserPrefs_valuesMissingFromFile_defaultValuesUsed() throws DataConversionException {
         UserPrefs actual = readUserPrefs("EmptyUserPrefs.json").get();
         assertEquals(new UserPrefs(), actual);
-    }
-
-    //    @Test
-    //    public void readUserPrefs_extraValuesInFile_extraValuesIgnored() throws DataConversionException {
-    //        UserPrefs expected = getTypicalUserPrefs();
-    //        UserPrefs actual = readUserPrefs("ExtraValuesUserPref.json").get();
-    //
-    //        assertEquals(expected, actual);
-    //    }
-
-    private UserPrefs getTypicalUserPrefs() {
-        UserPrefs userPrefs = new UserPrefs();
-        userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setDataFilePath(Paths.get("addressbook.json"));
-        return userPrefs;
     }
 
     @Test
@@ -86,18 +53,6 @@ public class JsonAppSettingsStorageTest {
     @Test
     public void saveUserPrefs_nullFilePath_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> saveUserPrefs(new UserPrefs(), null));
-    }
-
-    /**
-     * Saves {@code userPrefs} at the specified {@code prefsFileInTestDataFolder} filepath.
-     */
-    private void saveUserPrefs(UserPrefs userPrefs, String prefsFileInTestDataFolder) {
-        try {
-            new JsonUserPrefsStorage(addToTestDataPathIfNotNull(prefsFileInTestDataFolder))
-                    .saveUserPrefs(userPrefs);
-        } catch (IOException ioe) {
-            throw new AssertionError("There should not be an error writing to the file", ioe);
-        }
     }
 
     @Test
@@ -121,4 +76,50 @@ public class JsonAppSettingsStorageTest {
         assertEquals(original, readBack);
     }
 
+    //    @Test
+    //    public void readUserPrefs_extraValuesInFile_extraValuesIgnored() throws DataConversionException {
+    //        UserPrefs expected = getTypicalUserPrefs();
+    //        UserPrefs actual = readUserPrefs("ExtraValuesUserPref.json").get();
+    //
+    //        assertEquals(expected, actual);
+    //    }
+
+    //    @Test
+    //    public void readUserPrefs_fileInOrder_successfullyRead() throws DataConversionException {
+    //        UserPrefs expected = getTypicalUserPrefs();
+    //        UserPrefs actual = readUserPrefs("TypicalUserPref.json").get();
+    //        assertEquals(expected, actual);
+    //    }
+    
+    /**
+     * Saves {@code userPrefs} at the specified {@code prefsFileInTestDataFolder} filepath.
+     */
+    private void saveUserPrefs(UserPrefs userPrefs, String prefsFileInTestDataFolder) {
+        try {
+            new JsonUserPrefsStorage(addToTestDataPathIfNotNull(prefsFileInTestDataFolder))
+                    .saveUserPrefs(userPrefs);
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file", ioe);
+        }
+    }
+
+    private UserPrefs getTypicalUserPrefs() {
+        UserPrefs userPrefs = new UserPrefs();
+        userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
+        userPrefs.setDataFilePath(Paths.get("addressbook.json"));
+        return userPrefs;
+    }
+
+
+    private Optional<UserPrefs> readUserPrefs(String userPrefsFileInTestDataFolder) throws DataConversionException {
+        Path prefsFilePath = addToTestDataPathIfNotNull(userPrefsFileInTestDataFolder);
+        return new JsonUserPrefsStorage(prefsFilePath).readUserPrefs(prefsFilePath);
+    }
+
+
+    private Path addToTestDataPathIfNotNull(String userPrefsFileInTestDataFolder) {
+        return userPrefsFileInTestDataFolder != null
+                ? TEST_DATA_FOLDER.resolve(userPrefsFileInTestDataFolder)
+                : null;
+    }
 }
