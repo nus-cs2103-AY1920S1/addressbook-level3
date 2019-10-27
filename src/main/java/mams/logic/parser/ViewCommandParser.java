9ed83eae6b2cfad603ef28a9dd68609c4e5cceec
Whitebells <sky.lasts.mirror@gmail.com>
2019-10-27 12:43:02 +0800

    Refactored ViewCommand

--- src/main/java/mams/logic/parser/ViewCommandParser.java
+++ src/main/java/mams/logic/parser/ViewCommandParser.java
@@ -5,9 +5,6 @@ import static mams.logic.parser.CliSyntax.PREFIX_APPEAL;
 import static mams.logic.parser.CliSyntax.PREFIX_MODULE;
 import static mams.logic.parser.CliSyntax.PREFIX_STUDENT;
 
-import java.util.stream.Stream;
-
-import mams.commons.core.index.Index;
 import mams.logic.commands.ViewCommand;
 import mams.logic.parser.exceptions.ParseException;
 
@@ -25,38 +22,30 @@ public class ViewCommandParser {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_APPEAL, PREFIX_MODULE, PREFIX_STUDENT);
 
-        if (areAllPrefixesAbsent(argMultimap, PREFIX_APPEAL, PREFIX_MODULE, PREFIX_STUDENT)) {
+        if (argMultimap.areAllPrefixesAbsent(PREFIX_APPEAL, PREFIX_MODULE, PREFIX_STUDENT)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
         }
 
         try {
-            Index appealIndex = null;
-            Index moduleIndex = null;
-            Index studentIndex = null;
-
+            ViewCommand.ViewCommandParameters parameters = new ViewCommand.ViewCommandParameters();
             if (argMultimap.getValue(PREFIX_APPEAL).isPresent()) {
-                appealIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_APPEAL).get());
+                parameters.setAppealIndex(ParserUtil.parseIndex(argMultimap.getValue(PREFIX_APPEAL).get()));
             }
 
             if (argMultimap.getValue(PREFIX_MODULE).isPresent()) {
-                moduleIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_MODULE).get());
+                parameters.setModuleIndex(ParserUtil.parseIndex(argMultimap.getValue(PREFIX_MODULE).get()));
             }
 
             if (argMultimap.getValue(PREFIX_STUDENT).isPresent()) {
-                studentIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_STUDENT).get());
+                parameters.setStudentIndex(ParserUtil.parseIndex(argMultimap.getValue(PREFIX_STUDENT).get()));
             }
-            return new ViewCommand(appealIndex, moduleIndex, studentIndex);
+
+            assert parameters.isAtLeastOneParameterPresent() : "Assertion failed: ViewCommandParameter has no params"
+                    + "even after parsing";
+            return new ViewCommand(parameters);
         } catch (ParseException e) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
         }
 
-    }
-
-    /**
-     * Returns true if all of the prefixes are not present in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean areAllPrefixesAbsent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isEmpty());
     }
 }
