a18e3c44184388547878234b834b084e960ae3a5
nattanyz <natalietan8@gmail.com>
2019-10-27 23:29:00 +0800

    more checkstyle fixes

--- src/test/java/dream/fcard/gui/GuiTest.java
+++ src/test/java/dream/fcard/gui/GuiTest.java
@@ -10,7 +10,7 @@ import dream.fcard.model.cards.FrontBackCard;
 /** Tests for Gui. */
 public class GuiTest {
     /** Verify that flashcards with long text scroll in the expected manner. */
-    public static void gui_displayFlashCardWithLongText_ShowScrollBar() {
+    public static void renderFront_longText_showScrollBar() {
         String longStringForTesting = "Lorem ipsum dolor sit amet, "
             + "consectetur adipscing elit. Aliquam lacinia, nunc quis dictum consectetur, "
             + "erat nulla molestie turpis, quis finibus justo ipsum a justo. Cras quis ullamcorper "
@@ -28,22 +28,22 @@ public class GuiTest {
     }
 
     /** Verify that a node can be appended to the scrollable pane. */
-    public static void gui_addNodeToScrollablePane() {
+    public static void displayInScrollablePane() {
 
     }
 
     /** Verify that a node can replace currently existing nodes in the scrollable pane. */
-    public static void gui_replaceNodesInScrollablePane() {
+    public static void replaceInScrollablePane() {
 
     }
 
     /** Verify that the GUI can display an error message. */
-    public static void gui_displayErrorMessage() {
+    public static void showError() {
         Gui.showError("Command not recognised :(");
     }
 
     /** Verify that the GUI can display a deck of cards. */
-    public static void gui_displayDeck() {
+    public static void displayDeck_validDeck_showDeckInGui() {
         ArrayList<FlashCard> cards = new ArrayList<>();
         String frontString = "Lorem ipsum dolor sit amet, "
             + "consectetur adipscing elit. Aliquam lacinia, nunc quis dictum consectetur, "
