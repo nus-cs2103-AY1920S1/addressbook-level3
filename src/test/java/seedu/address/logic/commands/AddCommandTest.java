47a2715c8c8e5636941a3083c0756fb0a3fb8c19
chanjunren <chanjunren@gmail.com>
2019-10-27 19:09:12 +0800

    Fixed checkstyle errors in AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -46,9 +46,9 @@ public class AddCommandTest {
 
         CommandResult commandResult = new AddCommand(validPerson).execute(modelStub);
 
-    assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validPerson), commandResult.getFeedbackToUser());
-    assertEquals(Arrays.asList(validPerson), modelStub.personsAdded);
-}
+        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validPerson), commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(validPerson), modelStub.personsAdded);
+    }
 
     @Test
     public void execute_duplicatePerson_throwsCommandException() {
