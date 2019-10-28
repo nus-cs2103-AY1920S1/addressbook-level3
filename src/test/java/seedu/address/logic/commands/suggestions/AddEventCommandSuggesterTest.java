0c3d2175781febb5c1f51bff3362c7fdafa38bd8
aidilfbk <e0303295@u.nus.edu>
2019-10-28 22:53:59 +0800

    Add test cases for various Suggesters

---
+++ src/test/java/seedu/address/logic/commands/suggestions/AddEventCommandSuggesterTest.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands.suggestions;
+
+class AddEventCommandSuggesterTest extends SuggesterImplTester {
+
+    protected AddEventCommandSuggesterTest() throws ReflectiveOperationException {
+        super(AddEventCommandSuggester.class);
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/suggestions/EditPersonCommandSuggesterTest.java
@@ -0,0 +1,64 @@
+package seedu.address.logic.commands.suggestions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import seedu.address.logic.parser.ArgumentList;
+import seedu.address.logic.parser.CliSyntax;
+import seedu.address.logic.parser.CommandArgument;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.model.person.PersonDescriptor;
+import seedu.address.model.tag.Tag;
+import seedu.address.testutil.personutil.TypicalPersonDescriptor;
+
+class EditPersonCommandSuggesterTest extends EditCommandSuggesterTest {
+    static final PersonDescriptor KNOWN_PERSON = TypicalPersonDescriptor.ALICE;
+
+    protected EditPersonCommandSuggesterTest() throws ReflectiveOperationException {
+        super(EditPersonCommandSuggester.class, PrefixEditType.PERSON);
+    }
+
+    /**
+     * Provides the {@link Arguments} that represent the currently known values of the {@code KNOWN_PERSON}'s details
+     * (e.g. their name, phone number etc)
+     *
+     * @return A stream with each value containing the {@link Prefix} and the corresponding expected value.
+     */
+    static Stream<Arguments> knownPersonPrefixesAndValueGenerator() {
+        final Collection<String> tags = KNOWN_PERSON.getTags()
+                .stream()
+                .map(Tag::toString)
+                .collect(Collectors.toUnmodifiableList());
+
+        return Stream.of(
+                arguments(CliSyntax.PREFIX_NAME, List.of(KNOWN_PERSON.getName().toString())),
+                arguments(CliSyntax.PREFIX_PHONE, List.of(KNOWN_PERSON.getPhone().toString())),
+                arguments(CliSyntax.PREFIX_EMAIL, List.of(KNOWN_PERSON.getEmail().toString())),
+                arguments(CliSyntax.PREFIX_ADDRESS, List.of(KNOWN_PERSON.getAddress().toString())),
+                arguments(CliSyntax.PREFIX_REMARK, List.of(KNOWN_PERSON.getRemark().toString())),
+                arguments(CliSyntax.PREFIX_TAG, tags)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("knownPersonPrefixesAndValueGenerator")
+    void getSuggestions_exactPersonChangingPrefixBlankValue_currentPersonPrefixValue(
+            final Prefix prefix, final Collection<String> expectedSuggestions) {
+        final ArgumentList argumentList = argumentListOf(
+                new CommandArgument(CliSyntax.PREFIX_EDIT, 0, KNOWN_PERSON.getName().toString()),
+                new CommandArgument(prefix, 1, "")
+        );
+
+        final List<String> actualSuggestions = getSuggestions(argumentList, argumentList.get(1));
+        assertEquals(expectedSuggestions, actualSuggestions);
+    }
+}
