021a63470946de9dd4b88237818b723b88657451
sreesubbash <33561308+sreesubbash@users.noreply.github.com>
2019-10-28 18:10:01 +0800

    ParserManager and related classes have javadocs

--- src/main/java/seedu/address/logic/parser/ClassPair.java
+++ src/main/java/seedu/address/logic/parser/ClassPair.java
@@ -8,15 +8,28 @@ public class ClassPair {
     private Class command;
     private Class parser;
 
+    /**
+     * A simple constructor to create a pair of classes
+     * @param command
+     * @param parser
+     */
     public ClassPair(Class command, Class parser) {
         this.command = command;
         this.parser = parser;
     }
 
+    /**
+     * Gets command class from pair.
+     * @return Command Class
+     */
     public Class getCommand() {
         return this.command;
     }
 
+    /**
+     * Gets parser class from pair.
+     * @return Parser Class
+     */
     public Class getParser() {
         return this.parser;
     }
