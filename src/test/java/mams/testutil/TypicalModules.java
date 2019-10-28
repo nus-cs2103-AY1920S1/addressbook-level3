d59bf54dba0636d7f1a9c83668e780a3d0bb0588
cheongliyuan <e0008064@u.nus.edu>
2019-10-28 18:53:10 +0800

    remove some stuff

--- src/test/java/mams/testutil/TypicalModules.java
+++ src/test/java/mams/testutil/TypicalModules.java
@@ -16,17 +16,17 @@ public class TypicalModules {
             .withModuleCode("CS1010").withModuleName("Programming Methodology")
             .withModuleDescription("test1").withLecturerName("Tung Kum Hoe Anthony")
             .withTimeSlot("1,2,45,46,47").withQuota("100")
-            .withTags("A6614133H,A1748370H").build();
+            .withTags("A6614133H").build();
     public static final Module CS1231 = new ModuleBuilder()
             .withModuleCode("CS1231").withModuleName("Discrete Structures")
             .withModuleDescription("test2").withLecturerName("Terence Sim")
             .withTimeSlot("1,2,45,46,47").withQuota("150")
-            .withTags("A6614133H,A1748370H").build();
+            .withTags("A6614133H").build();
     public static final Module CS1020 = new ModuleBuilder()
             .withModuleCode("CS1020").withModuleName("Data Structures and Algorithms Is")
             .withModuleDescription("test3").withLecturerName("Tan Sun Teck")
             .withTimeSlot("21,20,57,58,59").withQuota("200")
-            .withTags("A5802229H,A1748370H,A5524592H").build();
+            .withTags("A5802229").build();
 
     private TypicalModules() {} // prevents instantiation
 
