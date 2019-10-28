1517074433e7c64cbd11e5821c01a108e92ae673
MauriceTXS <maurice456123@gmail.com>
2019-10-28 17:57:15 +0800

    Basic function of notification working.

--- src/main/java/calofit/logic/commands/NotificationCommand.java
+++ src/main/java/calofit/logic/commands/NotificationCommand.java
@@ -8,22 +8,39 @@ public class NotificationCommand extends Command{
     public static final String COMMAND_WORD = "notification";
 
     public static final String MESSAGE_BREAKFAST = "Looks like you have missed your breakfast," +
-        "Please take some time out to eat your breakfast!";
+        " please take some time out to eat your breakfast!";
 
     public static final String MESSAGE_LUNCH = "Looks like you have missed your lunch," +
-        "Please take some time out to eat your lunch!";
+        " please take some time out to eat your lunch!";
 
     public static final String MESSAGE_DINNER = "Looks like you have missed your dinner," +
-        "Please take some time out to eat your dinner!";
+        " please take some time out to eat your dinner!";
+
+    public static final String MESSAGE_SUCCESS = "You have taken all the needed meals. Good job!";
 
 
     @Override
     public CommandResult execute(Model model) {
-
         Notification notification = new Notification();
 
-        notification.checkBreakfast(model.getMealLog().getMeals().get(0).getTimestamp());
+        switch (model.getMealLog().getTodayMeals().size()) {
+        case 0:
+            if (!notification.eatenBreakfast()) {
+                return new CommandResult(MESSAGE_BREAKFAST);
+            }
+            break;
+        default:
+            if (!notification.eatenLunch(model.getMealLog().getTodayMeals().get(
+                model.getMealLog().getTodayMeals().size() - 1).getTimestamp())) {
+                return new CommandResult(MESSAGE_LUNCH);
+            } else {
+                if (!notification.eatenDinner(model.getMealLog().getTodayMeals().get(
+                    model.getMealLog().getTodayMeals().size() - 1).getTimestamp())) {
+                    return new CommandResult(MESSAGE_DINNER);
+                }
+            }
+        }
 
-        return new CommandResult(MESSAGE_BREAKFAST);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
--- src/main/java/calofit/model/util/Notification.java
+++ src/main/java/calofit/model/util/Notification.java
@@ -3,23 +3,51 @@ package calofit.model.util;
 import java.time.LocalDateTime;
 
 public class Notification {
-    private Timestamp currDateTime;
 
-    /*Notification(LocalDateTime currDateTime) {
-        this.currDateTime = currDateTime;
-    }*/
+    LocalDateTime currDateTime;
 
-    public boolean checkBreakfast(Timestamp currDateTime) {
-        System.out.println(currDateTime.getDateTime());
-        return false;
+    public Notification () {
+        currDateTime = LocalDateTime.now();
     }
 
-    public boolean checkLunch() {
-        return false;
+    public boolean eatenBreakfast() {
+        if (currDateTime.getHour() > 10) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean checkDinner() {
-        return false;
+    public boolean eatenLunch(Timestamp dishDateTime) {
+        if(currDateTime.getDayOfYear() != dishDateTime.getDateTime().getDayOfYear()) {
+            return false;
+        }
+
+        if (currDateTime.getHour() < 14 || currDateTime.getHour() > 19) {
+            return true;
+        }
+
+        if (dishDateTime.getDateTime().getHour() > 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean eatenDinner(Timestamp dishDateTime) {
+        if(currDateTime.getDayOfYear() != dishDateTime.getDateTime().getDayOfYear()) {
+            return false;
+        }
+
+        if (currDateTime.getHour() <= 20) {
+            return true;
+        }
+
+        if (dishDateTime.getDateTime().getHour() > 16) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
