375eee380b6411f77444ebdf076d139b2afb85a3
marcfyk <marcjohnfong@gmail.com>
2019-10-28 16:38:57 +0800

    Add JsonAdaptedSetMonthlyLimitCommand

---
+++ src/test/java/seedu/jarvis/storage/history/commands/course/JsonAdaptedSetMonthlyLimitCommandTest.java
@@ -0,0 +1,33 @@
+package seedu.jarvis.storage.history.commands.course;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.jarvis.logic.commands.finance.SetMonthlyLimitCommand;
+import seedu.jarvis.model.finance.MonthlyLimit;
+import seedu.jarvis.storage.finance.JsonAdaptedSetMonthlyLimitCommand;
+
+/**
+ * Tests the behaviour of {@code SetMonthlyLimitCommand}.
+ */
+public class JsonAdaptedSetMonthlyLimitCommandTest {
+
+    @Test
+    public void toModelType_nonNullOriginalLimit_returnsSetMonthlyLimitCommand() throws Exception {
+        SetMonthlyLimitCommand setMonthlyLimitCommand = new SetMonthlyLimitCommand(new MonthlyLimit("20"),
+                new MonthlyLimit("10"));
+        JsonAdaptedSetMonthlyLimitCommand jsonAdaptedSetMonthlyLimitCommand = new JsonAdaptedSetMonthlyLimitCommand(
+                setMonthlyLimitCommand);
+        assertEquals(setMonthlyLimitCommand, jsonAdaptedSetMonthlyLimitCommand.toModelType());
+    }
+
+    @Test
+    public void toModelType_nullOriginalLimit_returnsSetMonthlyLimitCommand() throws Exception {
+        SetMonthlyLimitCommand setMonthlyLimitCommand = new SetMonthlyLimitCommand(new MonthlyLimit("20"), null);
+        JsonAdaptedSetMonthlyLimitCommand jsonAdaptedSetMonthlyLimitCommand = new JsonAdaptedSetMonthlyLimitCommand(
+                setMonthlyLimitCommand);
+        assertEquals(setMonthlyLimitCommand, jsonAdaptedSetMonthlyLimitCommand.toModelType());
+    }
+
+}
