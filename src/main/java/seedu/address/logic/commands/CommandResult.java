be1957fe79d09f6511a8e66c8164c5c4843a44ad
1nefootstep <sheuxiang@gmail.com>
2019-10-28 16:58:17 +0800

    Add UiFocus enum

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.UiFocus;
+
 import static java.util.Objects.requireNonNull;
 
 import java.util.Objects;
@@ -17,21 +19,40 @@ public class CommandResult {
     /** The application should exit. */
     private final boolean exit;
 
+    private final UiFocus uiFocus;
+
     /**
      * Constructs a {@code CommandResult} with the specified fields.
      */
-    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
+    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit, UiFocus uiFocus) {
         this.feedbackToUser = requireNonNull(feedbackToUser);
         this.showHelp = showHelp;
         this.exit = exit;
+        this.uiFocus = uiFocus;
+    }
+
+    /**
+     * Constructs a {@code CommandResult} with the specified fields,
+     * but with uiFocus in its default value of null.
+     */
+    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
+        this(feedbackToUser, showHelp, exit, null);
     }
 
     /**
      * Constructs a {@code CommandResult} with the specified {@code feedbackToUser},
-     * and other fields set to their default value.
+     * and other fields set to their default values.
      */
     public CommandResult(String feedbackToUser) {
-        this(feedbackToUser, false, false);
+        this(feedbackToUser, false, false, null);
+    }
+
+    /**
+     * Constructs a {@code CommandResult} with the specified {@code feedbackToUser}
+     * and {@code uiFocus, while other fields are set to their default values.
+     */
+    public CommandResult(String feedbackToUser, UiFocus uiFocus) {
+        this(feedbackToUser, false, false, requireNonNull(uiFocus));
     }
 
     public String getFeedbackToUser() {
---
+++ src/main/java/seedu/address/logic/commands/exceptions/UiFocus.java
@@ -0,0 +1,5 @@
+package seedu.address.logic.commands.exceptions;
+
+public enum UiFocus {
+    AGENDA
+}
