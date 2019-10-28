727ebb784e9dac9ab9d28ce6ae79d9f11f96036f
Joshua Seet <joshu5797@gmail.com>
2019-10-29 07:43:18 +0800

    Added EditIncomeCommandParserTest and DeleteIncomeCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/DeleteIncomeCommandParserTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DeleteIncomeCommand;
+
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the DeleteIncomeCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteContactCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
+public class DeleteIncomeCommandParserTest {
+    private DeleteIncomeCommandParser parser = new DeleteIncomeCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsIncomeCommand() {
+        assertParseSuccess(parser, "1", new DeleteIncomeCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteIncomeCommand.MESSAGE_USAGE));
+    }
+}
