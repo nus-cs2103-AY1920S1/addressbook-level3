a539139f1892ad2d1cafa623d953d225d280fabc
charliechoong <e0202817@u.nus.edu>
2019-10-28 16:17:30 +0800

    Create basic DeliveryRecord class

--- src/main/java/seedu/deliverymans/model/deliveryman/DeliveryRecord.java
+++ src/main/java/seedu/deliverymans/model/deliveryman/DeliveryRecord.java
@@ -1,13 +1,75 @@
 package seedu.deliverymans.model.deliveryman;
 
-import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import seedu.deliverymans.model.Name;
 
 /**
  * Represents the delivery record of a deliveryman.
  */
 public class DeliveryRecord {
 
-    private Time timeJoined;
+    private Name name;
+    private LocalDateTime timeJoined;
     private int noOrdersCompleted;
+
+    public DeliveryRecord(Name name) {
+        this.name = name;
+        timeJoined = LocalDateTime.now();
+        noOrdersCompleted = 0;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public LocalDateTime getTimeJoined() {
+        return timeJoined;
+    }
+
+    public int getNoOrdersCompleted() {
+        return noOrdersCompleted;
+    }
+
+    /**
+     * Returns true if both persons have the same identity. Data fields need not be same.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof DeliveryRecord)) {
+            return false;
+        }
+
+        DeliveryRecord otherRecord = (DeliveryRecord) other;
+        return otherRecord.getName().equals(getName())
+                && otherRecord.getTimeJoined().equals(getTimeJoined())
+                && otherRecord.getNoOrdersCompleted() == getNoOrdersCompleted();
+
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(" Name: ")
+                .append(getName() + "\n")
+                .append(" Time Joined")
+                .append(getTimeJoined() + "\n")
+                .append(" No.completed orders: ")
+                .append(getNoOrdersCompleted());
+        return builder.toString();
+    }
+
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, timeJoined, noOrdersCompleted);
+    }
 
 }
