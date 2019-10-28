994643848feca44f9f5fda0af900a56e2d64e3a1
pohlinwei <linwei@Lins-MacBook-Pro-2.local>
2019-10-28 17:39:30 +0800

    Remove comments

--- src/main/java/seedu/address/calendar/model/event/EventQuery.java
+++ src/main/java/seedu/address/calendar/model/event/EventQuery.java
@@ -22,41 +22,7 @@ public class EventQuery extends Interval<Date, EventQuery> {
     public static boolean isValidEventTime(Date startDate, Date endDate) {
         return startDate.compareTo(endDate) <= 0;
     }
-    /*
-    public boolean isEndsAfter(Date date) {
-        // todo: change to endDate
-        return endDate.compareTo(date) > 0;
-    }
 
-    public boolean isStartsAfter(Date date) {
-        return startDate.compareTo(date) > 0;
-    }
-
-    public boolean contains(Date date) {
-        boolean isStartBeforeOrAt = startDate.compareTo(date) <= 0;
-        boolean isEndsAfterOrAt = endDate.compareTo(date) >= 0;
-        return isStartBeforeOrAt && isEndsAfterOrAt;
-    }
-
-    public Date getStart() {
-        return startDate;
-    }
-
-    public Date getEnd() {
-        return endDate;
-    }
-
-    public int compareTo(Interval<Date, EventQuery> other) {
-        Date otherStartDate = other.getStart();
-        int startCompare = startDate.compareTo(otherStartDate);
-        if (startCompare != 0) {
-            return startCompare;
-        }
-
-        Date otherEndDate = other.getEnd();
-        int endCompare = endDate.compareTo(otherEndDate);
-        return endCompare;
-    }*/
     @Override
     public String toString() {
         boolean isOneDayLong = startDate.equals(endDate);
