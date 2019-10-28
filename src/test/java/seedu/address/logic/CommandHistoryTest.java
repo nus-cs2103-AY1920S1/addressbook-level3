3aef46b9a403eb50d917d2eae10105e5ed827ce1
zhixianggg <zhixiangg.o@gmail.com>
2019-10-28 22:32:41 +0800

    Add CommandHistoryTest

--- src/test/java/seedu/address/logic/CommandHistoryTest.java
+++ src/test/java/seedu/address/logic/CommandHistoryTest.java
@@ -1,6 +1,9 @@
 package seedu.address.logic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 
@@ -8,11 +11,11 @@ import org.junit.jupiter.api.Test;
 
 public class CommandHistoryTest {
 
-    public CommandHistory commandHistory = CommandHistory.getCommandHistory();
+    private CommandHistory commandHistory = CommandHistory.getCommandHistory();
 
     @Test
     public void constructor_withCommandHistory_copiesCommandHistory() {
-        final CommandHistory commandHistoryWithAddCustomer = commandHistory;
+        CommandHistory commandHistoryWithAddCustomer = commandHistory;
         commandHistoryWithAddCustomer.add("add-c");
 
         assertEquals(commandHistoryWithAddCustomer, new CommandHistory(commandHistoryWithAddCustomer));
@@ -20,13 +23,54 @@ public class CommandHistoryTest {
 
     @Test
     public void add() {
-        final String validCommand = "clear";
-        final String invalidCommand = "adds Boy";
+        String validCommand = "clear";
+        String invalidCommand = "adds Boy";
 
         commandHistory.clear();
         commandHistory.add(validCommand);
         commandHistory.add(invalidCommand);
         assertEquals(Arrays.asList(validCommand, invalidCommand), commandHistory.getCommandHistoryList());
+    }
+
+    @Test
+    public void equals() {
+        commandHistory.clear();
+        CommandHistory commandHistoryWithAdd = new CommandHistory(commandHistory);
+        commandHistoryWithAdd.add("add");
+        CommandHistory commandHistoryWithAddCopy = new CommandHistory(commandHistory);
+        commandHistoryWithAddCopy.add("add");
+        CommandHistory emptyCommandHistory = new CommandHistory(commandHistory);
+
+        // same object -> returns true
+        assertTrue(commandHistoryWithAdd.equals(commandHistoryWithAdd));
+
+        // same values -> returns true
+        assertTrue(commandHistoryWithAdd.equals(commandHistoryWithAddCopy));
+
+        // null -> returns false
+        assertFalse(commandHistoryWithAdd.equals(null));
+
+        // different types -> returns false
+        assertFalse(commandHistoryWithAdd.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(commandHistoryWithAdd.equals(emptyCommandHistory));
+    }
+
+    @Test
+    public void hashcode() {
+        commandHistory.clear();
+        CommandHistory commandHistoryWithAdd = new CommandHistory(commandHistory);
+        commandHistoryWithAdd.add("add");
+        CommandHistory commandHistoryWithAddCopy = new CommandHistory(commandHistory);
+        commandHistoryWithAddCopy.add("add");
+        CommandHistory emptyCommandHistory = new CommandHistory(commandHistory);
+
+        // same values -> returns same hashcode
+        assertEquals(commandHistoryWithAdd.hashCode(), commandHistoryWithAddCopy.hashCode());
+
+        // different values -> returns different hashcode
+        assertNotEquals(commandHistoryWithAdd.hashCode(), emptyCommandHistory.hashCode());
     }
 
 }
