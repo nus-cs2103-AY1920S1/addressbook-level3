7d0abcc1b8e91ec6867f62ee641857a17154f18e
joloong <joelloong@gmail.com>
2019-10-29 11:11:30 +0800

    Add class diagram for filter

--- src/main/java/seedu/address/model/transaction/TransactionContainsCategoriesPredicate.java
+++ src/main/java/seedu/address/model/transaction/TransactionContainsCategoriesPredicate.java
@@ -1,25 +1,28 @@
 package seedu.address.model.transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
+import seedu.address.model.category.Category;
 
 /**
  * Tests that a {@code Transaction}'s {@code Category} matches the {@code Category} given.
  */
 public class TransactionContainsCategoriesPredicate implements Predicate<BankAccountOperation> {
 
-    private final List<String> keyCategories;
+    private final List<Category> keyCategories;
 
     public TransactionContainsCategoriesPredicate(List<String> keyCategories) {
-        this.keyCategories = keyCategories;
+        this.keyCategories = new ArrayList<>();
+        keyCategories.forEach(category -> this.keyCategories.add(new Category(category)));
     }
 
     @Override
     public boolean test(BankAccountOperation transaction) {
         return keyCategories.stream()
-                .anyMatch(keyCategory -> isCategoryInsideCategories(transaction, keyCategory));
+            .anyMatch(keyCategory -> isCategoryInsideCategories(transaction, keyCategory));
     }
 
     /**
@@ -28,17 +31,18 @@ public class TransactionContainsCategoriesPredicate implements Predicate<BankAcc
      * @param transaction {@code Transaction} to be checked.
      * @param keyCategory Category to be found in {@code Transaction}
      */
-    private boolean isCategoryInsideCategories(BankAccountOperation transaction, String keyCategory) {
+    private boolean isCategoryInsideCategories(BankAccountOperation transaction, Category keyCategory) {
         return transaction
-                .getCategories()
-                .stream()
-                .anyMatch(category -> StringUtil.containsWordIgnoreCase(category.getCategoryName(), keyCategory));
+            .getCategories()
+            .stream()
+            .anyMatch(category ->
+                StringUtil.containsWordIgnoreCase(category.getCategoryName(), keyCategory.getCategoryName()));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof TransactionContainsCategoriesPredicate // instanceof handles nulls
-                && keyCategories.equals(((TransactionContainsCategoriesPredicate) other).keyCategories)); // state check
+            || (other instanceof TransactionContainsCategoriesPredicate // instanceof handles nulls
+            && keyCategories.equals(((TransactionContainsCategoriesPredicate) other).keyCategories)); // state check
     }
 }
