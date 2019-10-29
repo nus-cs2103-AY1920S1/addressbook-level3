9426cea9ed64d96d54b30c511c9b87791ae2c3c0
ErwinChan <erwinchan1997@gmail.com>
2019-10-30 01:27:14 +0800

    Updated Userguide.adoc

--- src/main/java/seedu/address/model/person/Photo.java
+++ src/main/java/seedu/address/model/person/Photo.java
@@ -3,6 +3,8 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.io.File;
+
 import javafx.scene.image.Image;
 
 /**
@@ -11,26 +13,29 @@ import javafx.scene.image.Image;
  */
 public class Photo {
 
+
     public static final String MESSAGE_CONSTRAINTS = "Image filepath should be of the format local-part.png "
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and underscores.\n"
             + "2. This is followed by a '.' and only the image extension 'png' is allowed. ";
     public static final String VALIDATION_REGEX = "^[\\w]+(\\.(?i)(png))$";
-    private static final String IMAGE_DIRECTORY = "/images/";
-
-    public final String value;
+    private static final String IMAGE_DIRECTORY = "images/";
     public final Image photo;
+    public final String value;
+    private final File imageFile;
 
     public Photo() {
         value = "default.png";
-        photo = new Image(this.getClass().getResourceAsStream(IMAGE_DIRECTORY + value));
+        imageFile = new File(IMAGE_DIRECTORY + value);
+        photo = new Image("file://" + imageFile.toURI().getPath());
     }
 
     public Photo(String image) {
         requireNonNull(image);
         checkArgument(isValidFilePath(image), MESSAGE_CONSTRAINTS);
         value = image;
-        photo = new Image(this.getClass().getResourceAsStream(IMAGE_DIRECTORY + image));
+        imageFile = new File(IMAGE_DIRECTORY + value);
+        photo = new Image("file://" + imageFile.toURI().getPath());
     }
 
     public static boolean isValidFilePath(String test) {
