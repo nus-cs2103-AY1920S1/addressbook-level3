9a3cf8f13d10bb9fa639a828f75b18b1a9fff62e
tswuxia <tswuxia@gmail.com>
2019-10-29 19:39:56 +0800

    Added favourite genre feature. Changed tag colour.

--- src/main/java/seedu/ezwatchlist/statistics/Statistics.java
+++ src/main/java/seedu/ezwatchlist/statistics/Statistics.java
@@ -1,7 +1,15 @@
 package seedu.ezwatchlist.statistics;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import seedu.ezwatchlist.model.Model;
+import seedu.ezwatchlist.model.show.Genres;
 import seedu.ezwatchlist.model.show.Show;
 import seedu.ezwatchlist.model.show.UniqueShowList;
 
@@ -28,5 +36,33 @@ public class Statistics {
             forgotten.add(watchlist.get(2));
         }
         return forgotten.asUnmodifiableObservableList();
+    }
+
+    /**
+     * Gets the favourite genre of the user.
+     * @return an observable list of genres strings
+     */
+    public ObservableList<String> getFavouriteGenre() {
+        HashMap<String, Integer> genreRecords = new HashMap<>();
+
+        model.getWatchList().getShowList().stream().forEach(show -> {
+            show.getGenres().stream().forEach(genre -> {
+                if (genreRecords.containsKey(genre)) {
+                    genreRecords.put(genre, genreRecords.get(genre) + 1);
+                } else {
+                    genreRecords.put(genre, new Integer(1));
+                }
+            });
+        });
+
+        List<String> keyList = new ArrayList<>();
+        keyList.addAll(genreRecords.keySet());
+        Collections.sort(keyList, (key1, key2) -> genreRecords.get(key2) - genreRecords.get(key1));
+
+        ObservableList<String> favouriteGenres = FXCollections.observableArrayList();
+        for (int i = 0; i < 3 && i < keyList.size(); i++) {
+            favouriteGenres.add(keyList.get(i));
+        }
+        return favouriteGenres;
     }
 }
--- src/main/java/seedu/ezwatchlist/ui/StatisticsPanel.java
+++ src/main/java/seedu/ezwatchlist/ui/StatisticsPanel.java
@@ -2,9 +2,12 @@ package seedu.ezwatchlist.ui;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.ezwatchlist.model.show.Show;
 
 /**
@@ -12,21 +15,31 @@ import seedu.ezwatchlist.model.show.Show;
  */
 public class StatisticsPanel extends UiPart<Region> {
     private static final String FXML = "StatisticsPanel.fxml";
-    //private ObservableList<Show> forgotten;
 
     @FXML
     private ListView<Show> forgottenView;
+    @FXML
+    private HBox favouriteGenres;
+    @FXML
+    private VBox forgottenPlaceHolder;
 
-    public StatisticsPanel(ObservableList<Show> forgotten) {
+    public StatisticsPanel(ObservableList<Show> forgotten, ObservableList<String> favourite) {
         super(FXML);
-        forgottenView.setItems(forgotten);
-        forgottenView.setCellFactory(listView -> new ForgottenViewCell());
+        if (forgotten.size() > 0) {
+            forgottenView.setItems(forgotten);
+            forgottenView.setCellFactory(listView -> new StatisticsViewCell());
+        } else {
+            forgottenPlaceHolder.getChildren().add(new Label("You currently do not have any forgotten entry!"));
+        }
+
+
+        favourite.stream().forEach(genre -> favouriteGenres.getChildren().add(new Label(genre)));
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Show} using a {@code ShowCard}.
      */
-    class ForgottenViewCell extends ListCell<Show> {
+    class StatisticsViewCell extends ListCell<Show> {
         @Override
         protected void updateItem(Show show, boolean empty) {
             super.updateItem(show, empty);
