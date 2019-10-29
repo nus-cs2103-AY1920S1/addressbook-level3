787b7865206aa0bf79b507eb128174e054ee8168
ckb055 <chenkaibin7@gmail.com>
2019-10-29 21:50:31 +0800

    Added more test files

---
+++ src/test/java/seedu/address/logic/parser/cheatsheet/AddCheatSheetCommandParserTest.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser.cheatsheet;
+
+import seedu.address.logic.parser.person.AddCommandParser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddCheatSheetCommandParserTest {
+    private AddCheatSheetCommandParser parser = new AddCheatSheetCommandParser();
+
+
+}
