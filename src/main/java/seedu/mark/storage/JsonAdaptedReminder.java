da7c373676d7ccb5c6b56c45d1482883d560b111
TSAI, HSIAO-HAN <sunnyhome3224@gmail.com>
2019-10-30 02:48:39 +0800

    reminder storage

--- src/main/java/seedu/mark/storage/JsonAdaptedReminder.java
+++ src/main/java/seedu/mark/storage/JsonAdaptedReminder.java
@@ -3,7 +3,14 @@ package seedu.mark.storage;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import seedu.mark.commons.exceptions.IllegalValueException;
+import seedu.mark.model.bookmark.Url;
+import seedu.mark.model.reminder.Note;
 import seedu.mark.model.reminder.Reminder;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * JSON friendly version of {@link Reminder}.
@@ -11,6 +18,10 @@ import seedu.mark.model.reminder.Reminder;
 public class JsonAdaptedReminder {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Reminder's %s field is missing!";
+    private static final String DATE_FORMATTER = "dd/MM/yyyy HHmm";
+    protected static final String MESSAGE_INVALID_TIME_FORMAT =
+            "Invalid time format! Please use the following format: " + DATE_FORMATTER;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
 
     private final String note;
     private final String time;
@@ -36,4 +47,54 @@ public class JsonAdaptedReminder {
         url = source.getUrl().toString();
     }
 
+    /**
+     * Converts this Jackson-friendly adapted reminder object into the model's {@code Reminder} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted reminder.
+     */
+    public Reminder toModelType() throws IllegalValueException {
+
+        if (note == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Note.class.getSimpleName()));
+        }
+        if (!Note.isValidNote(note)) {
+            throw new IllegalValueException(Note.MESSAGE_CONSTRAINTS);
+        }
+        final Note modelNote = new Note(note);
+
+        if (url == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Url.class.getSimpleName()));
+        }
+        if (!Url.isValidUrl(url)) {
+            throw new IllegalValueException(Url.MESSAGE_CONSTRAINTS);
+        }
+        final Url modelUrl = new Url(url);
+
+        if (time == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, LocalDateTime.class.getSimpleName()));
+        }
+
+        final LocalDateTime modelTime = getModelTime(time);
+
+        return new Reminder(modelUrl, modelTime, modelNote);
+    }
+
+    /**
+     * Parses a {@code String time} into a {@code LocalDateTime}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws IllegalValueException if the given {@code time} is invalid.
+     */
+    public static LocalDateTime getModelTime(String time) throws IllegalValueException {
+        LocalDateTime getTime;
+
+        try {
+            getTime = LocalDateTime.parse(time, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalValueException(MESSAGE_INVALID_TIME_FORMAT);
+        }
+
+        return getTime;
+    }
 }
+
--- src/main/java/seedu/mark/storage/JsonSerializableMark.java
+++ src/main/java/seedu/mark/storage/JsonSerializableMark.java
@@ -1,6 +1,7 @@
 package seedu.mark.storage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,12 +9,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
+import javafx.collections.ObservableList;
 import seedu.mark.commons.exceptions.IllegalValueException;
 import seedu.mark.model.Mark;
 import seedu.mark.model.ReadOnlyMark;
 import seedu.mark.model.bookmark.Bookmark;
 import seedu.mark.model.bookmark.Folder;
+import seedu.mark.model.bookmark.Url;
 import seedu.mark.model.folderstructure.FolderStructure;
+import seedu.mark.model.reminder.Reminder;
+import seedu.mark.model.reminder.ReminderAssociation;
 
 /**
  * An Immutable Mark that is serializable to JSON format.
@@ -24,18 +29,23 @@ class JsonSerializableMark {
     public static final String MESSAGE_DUPLICATE_BOOKMARK = "Bookmark list contains duplicate bookmark(s).";
     public static final String MESSAGE_DUPLICATE_FOLDER = "There are duplicate folder(s).";
     public static final String MESSAGE_NO_ROOT_FOLDER = "The root folder is missing.";
+    public static final String MESSAGE_DUPLICATE_OR_NOT_EXIST_REMINDER =
+            "Cannot find the bookmark for reminder or bookmark has duplicate reminder.";
 
     private final List<JsonAdaptedBookmark> bookmarks = new ArrayList<>();
     private final JsonAdaptedFolderStructure folderStructure;
+    private final List<JsonAdaptedReminder> reminders = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonSerializableMark} with the given bookmarks.
      */
     @JsonCreator
     public JsonSerializableMark(@JsonProperty("bookmarks") List<JsonAdaptedBookmark> bookmarks,
-        @JsonProperty("folderStructure") JsonAdaptedFolderStructure folderStructure) {
+        @JsonProperty("folderStructure") JsonAdaptedFolderStructure folderStructure,
+                                @JsonProperty("reminders") List<JsonAdaptedReminder> reminders) {
         this.bookmarks.addAll(bookmarks);
         this.folderStructure = folderStructure;
+        this.reminders.addAll(reminders);
     }
 
     /**
@@ -48,6 +58,9 @@ class JsonSerializableMark {
         bookmarks.addAll(source.getBookmarkList().stream().map(JsonAdaptedBookmark::new)
             .collect(Collectors.toList()));
         folderStructure = new JsonAdaptedFolderStructure(source.getFolderStructure());
+        reminders.addAll(source.getReminderList().stream()
+                .map(JsonAdaptedReminder::new)
+                .collect(Collectors.toList()));
     }
 
     /**
@@ -72,7 +85,41 @@ class JsonSerializableMark {
             throw new IllegalValueException(MESSAGE_DUPLICATE_FOLDER);
         }
         mark.setFolderStructure(modelFolderStructure);
+
+        List<Reminder> reminderList = new ArrayList<>();
+        for (JsonAdaptedReminder jsonAdaptedReminder : reminders) {
+            Reminder reminder = jsonAdaptedReminder.toModelType();
+            reminderList.add(reminder);
+        }
+
+        setReminderAssociation(mark, reminderList);
         return mark;
+    }
+
+    private void setReminderAssociation(Mark mark, List<Reminder> reminders) throws IllegalValueException {
+        ObservableList<Bookmark> bookmarks = mark.getBookmarkList();
+        HashMap<Url, Bookmark> bookmarkHashMap = new HashMap<>();
+        ReminderAssociation reminderAssociation = new ReminderAssociation();
+        for (int i = 0; i < bookmarks.size(); i++) {
+            Bookmark bookmark = bookmarks.get(i);
+            Url url = bookmark.getUrl();
+
+            bookmarkHashMap.put(url, bookmark);
+        }
+
+        for (int i = 0; i < reminders.size(); i++) {
+            Reminder reminder = reminders.get(i);
+            Url url = reminder.getUrl();
+            if (!bookmarkHashMap.containsKey(url)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_OR_NOT_EXIST_REMINDER);
+            }
+
+            Bookmark bookmark = bookmarkHashMap.get(url);
+            reminderAssociation.addReminder(bookmark, reminder);
+            bookmarkHashMap.remove(url);
+        }
+
+        mark.setReminderAssociation(reminderAssociation);
     }
 
 }
