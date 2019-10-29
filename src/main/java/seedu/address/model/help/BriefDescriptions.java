9c32694ea77ce645def6c1fc9bb01b075aa95be7
TanZhanMing <e0349703@u.nus.edu>
2019-10-29 12:43:23 +0800

    Update javadocs to reveal only relevant information

--- src/main/java/seedu/address/model/help/BriefDescriptions.java
+++ src/main/java/seedu/address/model/help/BriefDescriptions.java
@@ -1,22 +1,6 @@
 package seedu.address.model.help;
 
-import seedu.address.logic.commands.AddClaimCommand;
-import seedu.address.logic.commands.AddContactCommand;
-import seedu.address.logic.commands.AddIncomeCommand;
-import seedu.address.logic.commands.ApproveClaimCommand;
-import seedu.address.logic.commands.BudgetCommand;
-import seedu.address.logic.commands.CheckCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.DeleteContactCommand;
-import seedu.address.logic.commands.DeleteIncomeCommand;
-import seedu.address.logic.commands.EditClaimCommand;
-import seedu.address.logic.commands.EditContactCommand;
-import seedu.address.logic.commands.EditIncomeCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.GotoCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.RejectClaimCommand;
+import seedu.address.logic.commands.*;
 
 /**
  * Contains a brief descriptions of all the commands in FinSec
@@ -87,6 +71,12 @@ public class BriefDescriptions {
 
         case "exit":
             return ExitCommand.MESSAGE_USAGE;
+
+        case "sort":
+            return SortCommand.MESSAGE_USAGE;
+
+        case "reverse":
+            return SortReverseCommand.MESSAGE_USAGE;
 
         default: return "Command should be self-explanatory with no additional parameters.\n"
                 + "Use 'type/guide' if you really want to know the exact details "
