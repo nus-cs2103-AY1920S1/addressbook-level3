ae5da502acaee083da51fa1a0faa431b5554dace
Sheryl Tay <sheryltayyyy@gmail.com>
2019-10-29 23:54:50 +0800

    removed debugging statements

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -251,9 +251,7 @@ public class ParserUtil {
             date = sdf.parse(trimmedTime);
             calendar.setTime(date);
             Calendar currCalendar = Calendar.getInstance();
-            System.out.println(calendar.compareTo(currCalendar));
             if (calendar.compareTo(currCalendar) < 0) {
-                System.out.println("hihihi");
                 throw new ParseException(Time.MESSAGE_TIME_CONSTRAINT);
             }
         } catch (java.text.ParseException e) {
