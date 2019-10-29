be5c40c0403ba621eae8813b9f74b7e639bb0a5a
Neo Si Hao <neosihao@gmail.com>
2019-10-29 12:36:34 +0800

    added AnswerChecker Class

---
+++ src/main/java/seedu/revision/model/answerable/AnswerChecker.java
@@ -0,0 +1,50 @@
+package seedu.revision.model.answerable;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class AnswerChecker {
+
+    private static StanfordCoreNLP pipeline = Pipeline.getPipeline();
+
+    static List<String> POSKeywords = List.of("CC", "CD", "NN", "NNS", "JJ");
+
+    public AnswerChecker() {
+    }
+
+    private static List<CoreLabel> analyseAnswerForKeywords(String answer) {
+        CoreDocument coreDocument = new CoreDocument(answer);
+        pipeline.annotate(coreDocument);
+
+        List<CoreLabel> coreLabelList = coreDocument.tokens(); // tokenize each word
+
+        Iterator<CoreLabel> iter = coreLabelList.iterator();
+        while (iter.hasNext()) {
+            CoreLabel coreLabel = iter.next();
+            String pos = coreLabel.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            if (!POSKeywords.parallelStream().anyMatch(pos::contains)) {
+                iter.remove();
+            }
+        }
+
+        for (CoreLabel coreLabel : coreLabelList) {
+            coreLabel.setOriginalText(coreLabel.lemma());
+        }
+
+        return coreLabelList;
+    }
+
+    public static boolean check(String userInput, String correctAnswer) {
+        if (new HashSet<>(analyseAnswerForKeywords(userInput)).equals(new HashSet<>(analyseAnswerForKeywords(correctAnswer)))) {
+            return true;
+        }
+
+        return true;
+    }
+}
