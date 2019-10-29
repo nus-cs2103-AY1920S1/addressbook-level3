70bd8ff2c434ddd95738affe014d861c824cccac
Marcus Ong <marcus.ong404@gmail.com>
2019-10-30 00:04:20 +0800

    Cache Fix (#158)
    * Implement caching
    
    * Add ModuleSummaryList, ImportMods
    * Refactor Cache paths
    
    * Implement importmods in an internal program
    * Add nusmods json files
    * Refactor FileUtil to add newline when writing to file
    * Fix NusmodsParser.parseWeeks logic
    
    * Fix checkstyle, add logging for initialization of nusmodsdata
    
    * Fix testutil to consider that FileUtil.writeToFile appends newline to EOF now
    
    * refactoring ImportMods
    
    * Change to use model.findPerson in AddNusMod(s)Command
    
    * Fix lesson's day not being considered when generating timeslots
    
    * Extract module-event mapping methods from command to ModuleEventMappingUtil
    
    * Modify addmods to include exam dates
    
    * Modify addmods to check for clashing events and trigger UI update
    
    * Add check for module exam before attempting to generate timeslot
    
    * Modify Timeslot.isClash to return false on exact edge overlaps
    
    * Improve ImportMods performance
    
    * Improve error checking in NusModsParser
    * Convert Week.type to enum
    
    * Improve code quality
    
    * Remove unused imports
    
    * merge master
    
    * Revert "merge master"
    
    This reverts commit 25656ff78d08ccbd204f4f89ca3ef72269fb0758.
    
    * Refactor and add error checks
    * remove acadYear/semesterNo flags for addmod command (disallow user to specify year/semester since it's not well supported yet)
    * refactor SemesterNo to enum
    
    * remove unused import
    
    * Fix SemesterNo enum equality checks
    
    * Fix cacheTest
    
    * Fix EOF
    
    * hotfix cache resolution when JAR-ed
    
    * fix checkstyle
    
    * Fix test case

--- src/test/java/seedu/address/websocket/CacheFileNamesTest.java
+++ src/test/java/seedu/address/websocket/CacheFileNamesTest.java
@@ -7,25 +7,25 @@ import org.junit.jupiter.api.Test;
 class CacheFileNamesTest {
     @Test
     void testStaticVariable() {
-        assertEquals("src/main/resources/ApiResponseCache/NusModsApi/academic_calendar.json",
+        assertEquals("/ApiResponseCache/NusModsApi/academic_calendar.json",
                 CacheFileNames.ACADEMIC_CALENDAR);
-        assertEquals("src/main/resources/ApiResponseCache/", CacheFileNames.CACHE_FOLDER_PATH);
-        assertEquals("src/main/resources/ApiResponseCache/GoogleMapsApi/DistanceMatrix.json",
+        assertEquals("/ApiResponseCache/", CacheFileNames.CACHE_FOLDER_PATH);
+        assertEquals("/ApiResponseCache/GoogleMapsApi/DistanceMatrix.json",
                 CacheFileNames.GMAPS_DISTANCE_MATRIX_PATH);
         assertEquals("GoogleMapsApi/", CacheFileNames.GMAPS_FOLDER_PATH);
-        assertEquals("src/main/resources/ApiResponseCache/GoogleMapsApi/GmapsImages/",
+        assertEquals("/ApiResponseCache/GoogleMapsApi/GmapsImages/",
                 CacheFileNames.GMAPS_IMAGE_DIR);
-        assertEquals("src/main/resources/ApiResponseCache/GoogleMapsApi/Places.json",
+        assertEquals("/ApiResponseCache/GoogleMapsApi/Places.json",
                 CacheFileNames.GMAPS_PLACES_PATH);
-        assertEquals("src/main/resources/ApiResponseCache/NusModsApi/holidays.json",
+        assertEquals("/ApiResponseCache/NusModsApi/holidays.json",
                 CacheFileNames.HOLIDAYS);
-        assertEquals("src/main/resources/ApiResponseCache/NusModsApi/modules.json",
+        assertEquals("/ApiResponseCache/NusModsApi/modules.json",
                 CacheFileNames.MODULES);
-        assertEquals("src/main/resources/ApiResponseCache/NusModsApi/modules_summary.json",
+        assertEquals("/ApiResponseCache/NusModsApi/modules_summary.json",
                 CacheFileNames.MODULES_SUMMARY);
         assertEquals("NusModsApi/",
                 CacheFileNames.NUSMODS_FOLDER_PATH);
-        assertEquals("src/main/resources/ApiResponseCache/NusModsApi/Venues.json",
+        assertEquals("/ApiResponseCache/NusModsApi/Venues.json",
                 CacheFileNames.VENUES_FULL_PATH);
     }
 }
