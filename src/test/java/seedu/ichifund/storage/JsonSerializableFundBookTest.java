c79c9f6bf8ee90a82ba53ace1ae1393f962a2049
Eugene Lim <nexolute@gmail.com>
2019-10-29 14:08:16 +0000

    Fix wrong test data filename

--- src/test/java/seedu/ichifund/storage/JsonSerializableFundBookTest.java
+++ src/test/java/seedu/ichifund/storage/JsonSerializableFundBookTest.java
@@ -11,27 +11,27 @@ import org.junit.jupiter.api.Test;
 import seedu.ichifund.commons.exceptions.IllegalValueException;
 import seedu.ichifund.commons.util.JsonUtil;
 import seedu.ichifund.model.FundBook;
-import seedu.ichifund.testutil.TypicalPersons;
+import seedu.ichifund.testutil.TypicalFundBook;
 
 public class JsonSerializableFundBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableFundBookTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsFundBook.json");
-    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonFundBook.json");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonFundBook.json");
+    private static final Path TYPICAL_FUNDBOOK_FILE = TEST_DATA_FOLDER.resolve("typicalFundBook.json");
+    private static final Path INVALID_FUNDBOOK_FILE = TEST_DATA_FOLDER.resolve("invalidFundBook.json");
+    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicateFundBook.json");
 
     @Test
-    public void toModelType_typicalPersonsFile_success() throws Exception {
-        JsonSerializableFundBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
+    public void toModelType_typicalFundBookFile_success() throws Exception {
+        JsonSerializableFundBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_FUNDBOOK_FILE,
                 JsonSerializableFundBook.class).get();
         FundBook fundBookFromFile = dataFromFile.toModelType();
-        FundBook typicalPersonsFundBook = TypicalPersons.getTypicalFundBook();
-        assertEquals(fundBookFromFile, typicalPersonsFundBook);
+        FundBook typicalFundBook = TypicalFundBook.getTypicalFundBook();
+        assertEquals(fundBookFromFile, typicalFundBook);
     }
 
     @Test
-    public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
-        JsonSerializableFundBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
+    public void toModelType_invalidFundBookFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableFundBook dataFromFile = JsonUtil.readJsonFile(INVALID_FUNDBOOK_FILE,
                 JsonSerializableFundBook.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
