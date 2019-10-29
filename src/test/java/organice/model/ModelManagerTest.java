e21d13e37e2a7bb4cb3eb6a2256b3d3bf9394a12
Chow Jia Ying <chowjiaying211@gmail.com>
2019-10-29 18:27:07 +0800

    Remove redundant persons from TypicalPerson (#149)
    * Remove Benson from TypicalPersons
    
    * Remove Persons from TypicalPersons
    
    * Change test cases to remove redundant persons

--- src/test/java/organice/model/ModelManagerTest.java
+++ src/test/java/organice/model/ModelManagerTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static organice.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static organice.testutil.Assert.assertThrows;
 import static organice.testutil.TypicalPersons.DOCTOR_ALICE;
-import static organice.testutil.TypicalPersons.DOCTOR_BENSON;
 import static organice.testutil.TypicalPersons.DONOR_IRENE_DONOR;
 import static organice.testutil.TypicalPersons.PATIENT_IRENE;
 
@@ -174,7 +173,7 @@ public class ModelManagerTest {
 
     @Test
     public void equals() {
-        AddressBook addressBook = new AddressBookBuilder().withPerson(DOCTOR_ALICE).withPerson(DOCTOR_BENSON).build();
+        AddressBook addressBook = new AddressBookBuilder().withPerson(DOCTOR_ALICE).withPerson(PATIENT_IRENE).build();
         AddressBook differentAddressBook = new AddressBook();
         UserPrefs userPrefs = new UserPrefs();
 
