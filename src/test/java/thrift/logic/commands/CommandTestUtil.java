cd1888a4a4e54c7cd42e8c7b84eade9e34525b52
Oen Qi Han, Kenson <54096011+kensonoenqh@users.noreply.github.com>
2019-10-29 19:10:53 +0800

    Kenson added tests for listcommandparsertest (#214)
    * Added a few tests to ListCommandParserTest
    
    * write more tests for listcommandparser
    
    * Added few new tests
    
    * added a test in listcommandparser
    
    * Added more new tests.
    
    * removed unneeded exception.
    
    * add more tests

--- src/test/java/thrift/logic/commands/CommandTestUtil.java
+++ src/test/java/thrift/logic/commands/CommandTestUtil.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static thrift.logic.parser.CliSyntax.PREFIX_DATE;
 import static thrift.logic.parser.CliSyntax.PREFIX_INDEX;
+import static thrift.logic.parser.CliSyntax.PREFIX_MONTH;
 import static thrift.logic.parser.CliSyntax.PREFIX_NAME;
 import static thrift.logic.parser.CliSyntax.PREFIX_OCCURRENCE;
 import static thrift.logic.parser.CliSyntax.PREFIX_REMARK;
@@ -46,6 +47,8 @@ public class CommandTestUtil {
     public static final String VALID_TAG_LUNCH = "Lunch";
     public static final String VALID_TAG_BRUNCH = "Brunch";
     public static final String VALID_TAG_ACCESSORY = "Accessory";
+    public static final String VALID_MONTH_JAN_19 = "01/2019";
+
 
     public static final String DESC_BURSARY = " " + PREFIX_NAME + VALID_DESCRIPTION_BURSARY;
     public static final String DESC_LAKSA = " " + PREFIX_NAME + VALID_DESCRIPTION_LAKSA;
@@ -62,6 +65,7 @@ public class CommandTestUtil {
     public static final String TAG_BRUNCH = " " + PREFIX_TAG + VALID_TAG_BRUNCH;
     public static final String TAG_AIRPODS = " " + PREFIX_TAG + VALID_TAG_ACCESSORY;
     public static final String INDEX_TOKEN = " " + PREFIX_INDEX;
+    public static final String MONTH_JAN_19 = " " + PREFIX_MONTH + VALID_MONTH_JAN_19;
     public static final String OCCURRENCE_TOKEN = " " + PREFIX_OCCURRENCE;
 
     public static final String INVALID_VALUE = " " + PREFIX_VALUE + ".00"; // missing whole number
---
+++ src/test/java/thrift/logic/parser/ListCommandParserTest.java
@@ -0,0 +1,59 @@
+package thrift.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static thrift.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static thrift.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.jupiter.api.Test;
+
+import thrift.logic.commands.CommandTestUtil;
+import thrift.logic.commands.ListCommand;
+
+public class ListCommandParserTest {
+
+    private static final String MESSAGE_INVALID_FORMAT =
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE);
+
+    private static final String MESSAGE_INVALID_DATE_FORMAT = "Month should be in the following format: MM/yyyy."
+            + ", where MM is an integer between 01-12";
+
+    private ListCommandParser parser = new ListCommandParser();
+
+    @Test
+    public void parse_invalidPreamble_failure() {
+        // invalid non empty preamble before valid prefix
+        assertParseFailure(parser, CommandTestUtil.PREAMBLE_NON_EMPTY
+                + CommandTestUtil.VALID_MONTH_JAN_19, MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", MESSAGE_INVALID_FORMAT);
+
+        assertParseFailure(parser, " r/invalidprefix", MESSAGE_INVALID_FORMAT);
+
+        assertParseFailure(parser, " invalidpreamble" + "m/10/2019", MESSAGE_INVALID_FORMAT);
+
+        //month not within specified range or not in proper syntax
+        assertParseFailure(parser, " m/13/2019", MESSAGE_INVALID_DATE_FORMAT);
+
+        assertParseFailure(parser, " m/00/2019", MESSAGE_INVALID_DATE_FORMAT);
+
+        assertParseFailure(parser, " m/012019", MESSAGE_INVALID_DATE_FORMAT);
+
+        assertParseFailure(parser, " m/1/2019", MESSAGE_INVALID_DATE_FORMAT);
+    }
+
+    @Test
+    public void parse_optionalFieldsMissing_success() {
+        // no month prefix and argument, (list all transactions)
+        assertDoesNotThrow(() -> parser.parse(""));
+    }
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        // includes month prefix, list transactions by some specific month (jan 2019).
+        assertDoesNotThrow(() -> parser.parse(CommandTestUtil.MONTH_JAN_19));
+    }
+
+}
