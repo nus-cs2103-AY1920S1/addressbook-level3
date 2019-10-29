ac29d7f6c8906250c264b95523a388809b0888c9
ckb055 <chenkaibin7@gmail.com>
2019-10-29 22:06:23 +0800

    Fix TravisCheckstyle

--- src/test/java/seedu/address/logic/parser/cheatsheet/AddCheatSheetCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/cheatsheet/AddCheatSheetCommandParserTest.java
@@ -1,9 +1,5 @@
 package seedu.address.logic.parser.cheatsheet;
 
-import seedu.address.logic.parser.person.AddCommandParser;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class AddCheatSheetCommandParserTest {
     private AddCheatSheetCommandParser parser = new AddCheatSheetCommandParser();
 
