ed4fc10f05577c86c3b5530414c3456aaaafb3fb
Mandy Wang <shunflower320@gmail.com>
2019-10-29 13:12:04 +0800

    adding tests for vnum n vtype

--- src/test/java/seedu/address/logic/commands/FindVehiclesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindVehiclesCommandTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_VEHICLES_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalVehicles.V1;
+import static seedu.address.testutil.TypicalVehicles.V2;
+import static seedu.address.testutil.TypicalVehicles.V3;
 import static seedu.address.testutil.TypicalVehicles.V4;
 import static seedu.address.testutil.TypicalVehicles.getTypicalIncidentManager;
 
@@ -21,6 +23,10 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.vehicle.District;
 import seedu.address.model.vehicle.DistrictKeywordsPredicate;
+import seedu.address.model.vehicle.VNumKeywordsPredicate;
+import seedu.address.model.vehicle.VTypeKeywordsPredicate;
+import seedu.address.model.vehicle.VehicleNumber;
+import seedu.address.model.vehicle.VehicleType;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindVehiclesCommand}.
@@ -59,9 +65,21 @@ public class FindVehiclesCommandTest {
     @Test
     public void execute_zeroKeywords_noVehiclesFound() {
         String expectedMessage = String.format(MESSAGE_VEHICLES_LISTED_OVERVIEW, 0);
-        DistrictKeywordsPredicate predicate = prepareDistrictPredicate(" ");
-        FindVehiclesCommand command = new FindVehiclesCommand(predicate);
-        expectedModel.updateFilteredVehicleList(predicate);
+        DistrictKeywordsPredicate dPredicate = prepareDistrictPredicate(" ");
+        FindVehiclesCommand command = new FindVehiclesCommand(dPredicate);
+        expectedModel.updateFilteredVehicleList(dPredicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredVehicleList());
+
+        VTypeKeywordsPredicate vTypePredicate = prepareVTypePredicate(" ");
+        command = new FindVehiclesCommand(vTypePredicate);
+        expectedModel.updateFilteredVehicleList(vTypePredicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredVehicleList());
+
+        VNumKeywordsPredicate vNumKeywordsPredicate = prepareVNumPredicate(" ");
+        command = new FindVehiclesCommand(vNumKeywordsPredicate);
+        expectedModel.updateFilteredVehicleList(vNumKeywordsPredicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertEquals(Collections.emptyList(), model.getFilteredVehicleList());
     }
@@ -69,15 +87,31 @@ public class FindVehiclesCommandTest {
     @Test
     public void execute_multipleKeywords_multipleVehiclesFound() {
         String expectedMessage = String.format(MESSAGE_VEHICLES_LISTED_OVERVIEW, 2);
-        DistrictKeywordsPredicate predicate = prepareDistrictPredicate("1 2");
-        FindVehiclesCommand command = new FindVehiclesCommand(predicate);
-        expectedModel.updateFilteredVehicleList(predicate);
+        DistrictKeywordsPredicate dPredicate = prepareDistrictPredicate("1 2");
+        FindVehiclesCommand command = new FindVehiclesCommand(dPredicate);
+        expectedModel.updateFilteredVehicleList(dPredicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertEquals(Arrays.asList(V1, V4), model.getFilteredVehicleList());
+
+        expectedMessage = String.format(MESSAGE_VEHICLES_LISTED_OVERVIEW, 2);
+        VTypeKeywordsPredicate vTypePredicate = prepareVTypePredicate("Ambulance");
+        command = new FindVehiclesCommand(vTypePredicate);
+        expectedModel.updateFilteredVehicleList(vTypePredicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(V1, V2), model.getFilteredVehicleList());
+
+        expectedMessage = String.format(MESSAGE_VEHICLES_LISTED_OVERVIEW, 3);
+        VNumKeywordsPredicate vNumPredicate = prepareVNumPredicate("2");
+        command = new FindVehiclesCommand(vNumPredicate);
+        expectedModel.updateFilteredVehicleList(vNumPredicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(V1, V2, V3), model.getFilteredVehicleList());
     }
 
     /**
      * Parses {@code userInput} into a {@code DistrictKeywordsPredicate}.
+     * @param userInput
+     * @return
      */
     private DistrictKeywordsPredicate prepareDistrictPredicate(String userInput) {
         List<String> splittedD = Arrays.asList(userInput.split("\\s+"));
@@ -86,5 +120,25 @@ public class FindVehiclesCommandTest {
             districts.add(new District(Integer.valueOf(s)));
         }
         return new DistrictKeywordsPredicate(districts);
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code VTypeKeywordsPredicate}.
+     * @param userInput
+     * @return
+     */
+    private VTypeKeywordsPredicate prepareVTypePredicate(String userInput) {
+        VehicleType vType = new VehicleType(userInput);
+        return new VTypeKeywordsPredicate(vType);
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code VTypeKeywordsPredicate}.
+     * @param userInput
+     * @return
+     */
+    private VNumKeywordsPredicate prepareVNumPredicate(String userInput) {
+        VehicleNumber vNum = new VehicleNumber(userInput);
+        return new VNumKeywordsPredicate(vNum);
     }
 }
