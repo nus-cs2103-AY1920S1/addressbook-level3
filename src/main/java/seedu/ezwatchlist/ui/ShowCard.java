70e03b949e79a2884b8bf59e60e15df86a15a992
tswuxia <tswuxia@gmail.com>
2019-10-29 15:46:12 +0800

    Fixed travis check style

--- src/main/java/seedu/ezwatchlist/ui/ShowCard.java
+++ src/main/java/seedu/ezwatchlist/ui/ShowCard.java
@@ -10,8 +10,8 @@ import javafx.scene.control.Label;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import seedu.ezwatchlist.api.exceptions.OnlineConnectionException;
 import seedu.ezwatchlist.logic.commands.exceptions.CommandException;
