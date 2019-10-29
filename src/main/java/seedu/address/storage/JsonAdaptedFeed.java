b228a057d705f5dc039e1bc03c80fb0bfc13a4cd
Daryl Chan <6095637+dvrylc@users.noreply.github.com>
2019-10-29 23:46:28 +0800

    Add missing serializable `FeedPost` (#103)

--- src/main/java/seedu/address/storage/JsonAdaptedFeed.java
+++ src/main/java/seedu/address/storage/JsonAdaptedFeed.java
@@ -2,6 +2,7 @@ package seedu.address.storage;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -19,7 +20,7 @@ class JsonAdaptedFeed {
 
     private final String name;
     private final String address;
-    private final Set<FeedPost> posts = new HashSet<>();
+    private final Set<JsonAdaptedFeedPost> posts = new HashSet<>();
 
     /**
      * Constructs a {@code JsonAdaptedFeed} with the given feed details.
@@ -27,7 +28,7 @@ class JsonAdaptedFeed {
     @JsonCreator
     public JsonAdaptedFeed(@JsonProperty("name") String name,
                            @JsonProperty("address") String address,
-                           @JsonProperty("posts") Set<FeedPost> posts) {
+                           @JsonProperty("posts") Set<JsonAdaptedFeedPost> posts) {
         this.name = name;
         this.address = address;
         this.posts.addAll(posts);
@@ -39,7 +40,7 @@ class JsonAdaptedFeed {
     public JsonAdaptedFeed(Feed source) {
         name = source.getName();
         address = source.getAddress();
-        this.posts.addAll(source.getPosts());
+        this.posts.addAll(source.getPosts().stream().map(JsonAdaptedFeedPost::new).collect(Collectors.toSet()));
     }
 
     /**
@@ -55,7 +56,15 @@ class JsonAdaptedFeed {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, "address"));
         }
 
-        return new Feed(name, address, posts);
+        Feed feed = new Feed(name, address);
+
+        Set<FeedPost> feedPosts = new HashSet<>();
+        for (JsonAdaptedFeedPost feedPost : this.posts) {
+            feedPosts.add(feedPost.toModelType());
+        }
+        feed.setPosts(feedPosts);
+
+        return feed;
     }
 
 }
