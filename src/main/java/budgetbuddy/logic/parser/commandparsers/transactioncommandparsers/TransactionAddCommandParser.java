ea32f925c8aee7680c23a743a1f8ffdbff2a03ed
cambrian-dk <daryl.kohyikai@gmail.com>
2019-10-29 11:11:06 +0800

    Fixed incorrect static method usage

--- src/main/java/budgetbuddy/logic/parser/commandparsers/transactioncommandparsers/TransactionAddCommandParser.java
+++ src/main/java/budgetbuddy/logic/parser/commandparsers/transactioncommandparsers/TransactionAddCommandParser.java
@@ -18,13 +18,14 @@ import budgetbuddy.logic.parser.CommandParser;
 import budgetbuddy.logic.parser.CommandParserUtil;
 import budgetbuddy.logic.parser.Prefix;
 import budgetbuddy.logic.parser.exceptions.ParseException;
-import budgetbuddy.model.AccountsManager;
 import budgetbuddy.model.account.Account;
 import budgetbuddy.model.attributes.Category;
 import budgetbuddy.model.attributes.Description;
 import budgetbuddy.model.attributes.Direction;
+import budgetbuddy.model.attributes.Name;
 import budgetbuddy.model.transaction.Amount;
 import budgetbuddy.model.transaction.Transaction;
+import budgetbuddy.model.transaction.TransactionList;
 
 /**
  * Parses input and creates a new TransactionAddCommand
