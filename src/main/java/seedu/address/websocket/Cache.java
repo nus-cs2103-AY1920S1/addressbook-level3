5f5ea9545429438ca59ca2cfdebaa019a44e66b9
klementtan <klementtan@gmail.com>
2019-10-29 13:21:25 +0800

    Remove useless code

--- src/main/java/seedu/address/websocket/Cache.java
+++ src/main/java/seedu/address/websocket/Cache.java
@@ -69,12 +69,7 @@ public class Cache {
         if (optionalObject.isPresent()) {
             jsonObject = (JSONObject) optionalObject.get();
         }
-
-        if (jsonObject.containsKey(key)) {
-            jsonObject.remove(key);
-        }
-        jsonObject.put(key, value);
-
+        jsonObject.put(key, value);\
         save(jsonObject, filePath);
     }
 
