352a531b4a1f550bc16bfa8e9d4867420c95566f
Neo Si Hao <neosihao@gmail.com>
2019-10-29 12:35:07 +0800

    added StanfordCoreNLP Pipeline class

---
+++ src/main/java/seedu/revision/model/answerable/Pipeline.java
@@ -0,0 +1,26 @@
+package seedu.revision.model.answerable;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.Properties;
+
+public class Pipeline {
+
+    private static Properties properties;
+    private static String propertiesName = "tokenize, ssplit, pos, lemma, ner, parse, sentiment";
+    private static StanfordCoreNLP pipeline;
+
+    private Pipeline() { }
+
+    static {
+        properties = new Properties();
+        properties.setProperty("annotators", propertiesName);
+    }
+
+    public static StanfordCoreNLP getPipeline() {
+        if(pipeline == null) {
+            pipeline = new StanfordCoreNLP(properties);
+        }
+        return pipeline;
+    }
+}
