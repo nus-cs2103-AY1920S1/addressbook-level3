ba983b24ec02dc5d2817b21c14e66af4395f2ef4
Whitebells <sky.lasts.mirror@gmail.com>
2019-10-29 22:54:22 +0800

    Fixed HelpWindow bug for real this time

--- src/main/java/mams/ui/HelpWindow.java
+++ src/main/java/mams/ui/HelpWindow.java
@@ -1,12 +1,16 @@
 package mams.ui;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.Random;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import mams.commons.core.LogsCenter;
 
@@ -21,6 +25,17 @@ public class HelpWindow extends UiPart<Stage> {
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
+    public static final String YELLOW_LABEL_STYLE_CLASS = "prefix_yellow";
+    public static final String PURPLE_LABEL_STYLE_CLASS = "prefix_purple";
+    public static final String RED_LABEL_STYLE_CLASS = "prefix_red";
+    public static final String SKY_BLUE_LABEL_CLASS = "prefix_sky_blue";
+    public static final String PINK_LABEL_STYLE_CLASS = "prefix_pink";
+    public static final String GREEN_LABEL_STYLE_CLASS = "prefix_green";
+
+    public static final List<String> LABEL_STYLES = Arrays.asList(YELLOW_LABEL_STYLE_CLASS,
+            PURPLE_LABEL_STYLE_CLASS, RED_LABEL_STYLE_CLASS, SKY_BLUE_LABEL_CLASS,
+            PINK_LABEL_STYLE_CLASS, GREEN_LABEL_STYLE_CLASS);
+
     @FXML
     private Button copyButton;
 
@@ -34,6 +49,7 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
+        helpMessage.setText(HELP_MESSAGE);
     }
 
     /**
@@ -65,7 +81,21 @@ public class HelpWindow extends UiPart<Stage> {
         logger.fine("Showing help page about the application.");
         getRoot().show();
         getRoot().centerOnScreen();
-        helpMessage.setText(HELP_MESSAGE);
+        changeHelpMessageStyle(); // see #changeHelpMessageStyle() for why this is needed.
+    }
+
+    /**
+     * Changes the background fill for the helpMessage {@code Label} to a random colour. The reason
+     * why this is needed is due to a bug on JavaFX on some computers where the help window will
+     * not sometimes not load on re-opening until an event is fired (or until something "prompts" the framework).
+     * By changing the Label style randomly on each re-open, we prevent this from happening. Besides,
+     * it also looks nice.
+     */
+    public void changeHelpMessageStyle() {
+        helpMessage.getStyleClass().removeAll(LABEL_STYLES);
+        Random random = new Random();
+        String randomStyleClass = LABEL_STYLES.get(random.nextInt(LABEL_STYLES.size()));
+        helpMessage.getStyleClass().add(randomStyleClass);
     }
 
     /**
@@ -87,6 +117,9 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public void focus() {
         getRoot().requestFocus();
+        // unlike #close(), this is not explicitly needed here (see #changeHelpMessageStyle)
+        // just purely for the visual effects.
+        changeHelpMessageStyle();
     }
 
     /**
@@ -98,5 +131,20 @@ public class HelpWindow extends UiPart<Stage> {
         final ClipboardContent url = new ClipboardContent();
         url.putString(USERGUIDE_URL);
         clipboard.setContent(url);
+    }
+
+    /**
+     * Handles {@code KeyEvent} for the help window.
+     */
+    @FXML
+    private void handleKeyPress(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case ESCAPE:
+                keyEvent.consume();
+                hide();
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/main/java/mams/ui/HistoryWindow.java
+++ src/main/java/mams/ui/HistoryWindow.java
@@ -153,5 +153,4 @@ public class HistoryWindow extends UiPart<Stage> {
             break;
         }
     }
-
 }
