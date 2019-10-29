e6077708386e9a23c985a87f40e4a69591c9924c
klementtan <klementtan@gmail.com>
2019-10-29 19:39:16 +0800

    Resolve test case

--- src/main/java/seedu/address/websocket/util/UrlUtil.java
+++ src/main/java/seedu/address/websocket/util/UrlUtil.java
@@ -17,7 +17,7 @@ import seedu.address.commons.util.StringUtil;
  */
 public class UrlUtil {
 
-    private static String gmapsApiKey = "AIzaSyAfef376nLjbnArYyPCRkAc0VcJo1br7xo";
+    private static String gmapsApiKey = "";
 
     /**
      * Generates a URL object from a String.
