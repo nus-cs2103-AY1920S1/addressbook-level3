0ef2498e655eb7329e43947e9e88ca3f09e0cae4
SQwQ <33057619+SQwQ@users.noreply.github.com>
2019-10-30 01:39:00 +0800

    docs: UG fix: checkstyle

--- src/main/java/seedu/address/ui/EmptyVisitList.java
+++ src/main/java/seedu/address/ui/EmptyVisitList.java
@@ -5,8 +5,6 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 
