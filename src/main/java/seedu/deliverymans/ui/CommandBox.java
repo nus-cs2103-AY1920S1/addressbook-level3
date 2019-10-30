42ac3a1745cbcbd3286a93abf70cfdde1656a9dc
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-10-30 21:30:10 +0800

    Fixed checkstyle errors part 2 and minor edits to autocomplete feature

--- src/main/java/seedu/deliverymans/ui/CommandBox.java
+++ src/main/java/seedu/deliverymans/ui/CommandBox.java
@@ -54,6 +54,7 @@ public class CommandBox extends UiPart<Region> {
     private void handleKeyPressed() {
         LinkedList<String> temp = trieManager.getAutoCompleteResults(commandTextField.getText(),
                 MainWindow.currentContext);
+        temp.sort(String::compareToIgnoreCase);
         if (!temp.isEmpty()) {
             for (String s : temp) {
                 System.out.println(s);
