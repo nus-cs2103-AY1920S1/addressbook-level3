ec9d9cd33f9d1f0cb177b42cf2b9e60b45aa9253
victorvic54 <sacchirro@yahoo.com>
2019-10-30 16:44:34 +0800

    Fixing checkstyle to pass travis

--- src/test/java/seedu/address/logic/quiz/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/quiz/commands/ExitCommandTest.java
@@ -1,21 +1,21 @@
-//package seedu.address.logic.quiz.commands;
-//
-//import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.address.logic.commands.CommandResult;
-//import seedu.address.logic.commands.ExitCommand;
-//
-//
-//public class ExitCommandTest {
-//    private Model model = new ModelManager();
-//    private Model expectedModel = new ModelManager();
-//
-//    @Test
-//    public void execute_exit_success() {
-//        CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
-//        assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
-//    }
-//}
+package seedu.address.logic.quiz.commands;
+
+import static seedu.address.logic.quiz.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.quiz.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.quiz.Model;
+import seedu.address.model.quiz.ModelQuizManager;
+
+
+public class ExitCommandTest {
+    private Model model = new ModelQuizManager();
+    private Model expectedModel = new ModelQuizManager();
+
+    @Test
+    public void execute_exit_success() {
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+        assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
