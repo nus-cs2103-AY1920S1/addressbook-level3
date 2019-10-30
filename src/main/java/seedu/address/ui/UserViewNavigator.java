fc6b967b32e59562a38e295c88e00fccd9b16a6d
seahlynn <e0324409@u.nus.edu>
2019-10-30 18:23:50 +0800

    Ui for list

--- src/main/java/seedu/address/ui/UserViewNavigator.java
+++ src/main/java/seedu/address/ui/UserViewNavigator.java
@@ -61,7 +61,8 @@ public class UserViewNavigator {
      * @param logic to access task data
      */
     public void loadTaskListView(Logic logic) {
-        TaskListPanel taskListPanel = new TaskListPanel(logic.getFilteredTaskList());
+        TaskListPanel taskListPanel = new TaskListPanel(logic.getFilteredTaskList(),
+                logic.getProjectDashboard().getMemberList(), logic.getProjectDashboard().getTasMemMappingList());
         userViewController.setUserView(taskListPanel);
     }
 
--- src/main/java/seedu/address/ui/views/IndivTaskCard.java
+++ src/main/java/seedu/address/ui/views/IndivTaskCard.java
@@ -1,24 +1,25 @@
 package seedu.address.ui.views;
 
 import java.util.Comparator;
+import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
+import seedu.address.commons.util.DateTimeUtil;
 import seedu.address.model.member.Member;
+import seedu.address.model.task.Task;
 import seedu.address.ui.UiPart;
 
 /**
  * An UI component that displays information of a {@code Task}.
  */
-public class MemberCard extends UiPart<Region> {
+public class IndivTaskCard extends UiPart<Region> {
 
-    private static final String FXML = "MemberListCard.fxml";
+    private static final String FXML = "IndivTaskCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -28,7 +29,7 @@ public class MemberCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on ProjectDashboard level 4</a>
      */
 
-    public final Member member;
+    public final Task task;
 
     @FXML
     private HBox cardPane;
@@ -36,33 +37,42 @@ public class MemberCard extends UiPart<Region> {
     private Label name;
     @FXML
     private Label id;
+
     @FXML
-    private Label displayId;
-    @FXML
-    private ImageView imageView;
+    private Label deadline;
     @FXML
     private FlowPane tags;
     @FXML
-    private Text listTasks;
+    private Label membersAllocated;
+    @FXML
+    private Text members;
 
-    public MemberCard(Member member, int displayedIndex) {
+    public IndivTaskCard(Task task, int displayedIndex, List<Member> memberList) {
         super(FXML);
-        this.member = member;
+        this.task = task;
         id.setText(displayedIndex + ". ");
-        displayId.setText("Member ID: " + member.getId().getDisplayId());
-        name.setText(member.getName().fullName);
-        member.getTags().stream()
+        name.setText(task.getName().fullName);
+        task.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
 
-        if (member.getImage() == null) {
-            imageView.setImage(new Image(this.getClass().getResourceAsStream("/images/DaUser.png")));
+        if (task.hasDeadline()) {
+            deadline.setText("Due on: " + DateTimeUtil.displayDateTime(task.getDeadline()));
         } else {
-            imageView.setImage(member.getImage());
+            deadline.setText("No deadline set");
         }
 
-        imageView.setFitHeight(120);
-        imageView.setFitWidth(120);
+        String listOfMembers = "";
+
+        for (int i = 0; i < memberList.size(); i++) {
+            listOfMembers += (i+1) + ". " + memberList.get(i).toString() + "\n";
+        }
+
+        members.setText(listOfMembers);
+    }
+
+    public Label getDeadlineLabel() {
+        return deadline;
     }
 
     @Override
@@ -73,14 +83,13 @@ public class MemberCard extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof TaskCard)) {
+        if (!(other instanceof IndivTaskCard)) {
             return false;
         }
 
         // state check
-        MemberCard card = (MemberCard) other;
+        IndivTaskCard card = (IndivTaskCard) other;
         return id.getText().equals(card.id.getText())
-                && member.equals(card.member);
+                && task.equals(card.task);
     }
 }
-
