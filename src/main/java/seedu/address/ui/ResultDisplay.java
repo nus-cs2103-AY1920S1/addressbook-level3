53e50437025189b5d975da266f388c82feb87ac7
hoholyin <holyin97@gmail.com>
2019-10-30 22:56:59 +0800

    Scrollpane for result display

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 
@@ -16,7 +16,7 @@ public class ResultDisplay extends UiPart<Region> {
     private static final String FXML = "ResultDisplay.fxml";
 
     @FXML
-    private AnchorPane resultDisplayPlaceholder;
+    private ScrollPane resultDisplayPlaceholder;
 
     @FXML
     private Label resultDisplay;
