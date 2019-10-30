a196a23dd017430b13a10fe56ecd3743c1609d12
ZiHawkEye <yewkaizhe@hotmail.com>
2019-10-31 00:29:40 +0800

    Update user guide

--- src/main/java/seedu/tarence/logic/commands/HelpCommand.java
+++ src/main/java/seedu/tarence/logic/commands/HelpCommand.java
@@ -10,12 +10,9 @@ import seedu.tarence.storage.Storage;
 public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
-
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
-
     private static final String[] COMMAND_SYNONYMS = {COMMAND_WORD.toLowerCase()};
 
     @Override
