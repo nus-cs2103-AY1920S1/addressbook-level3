aad8b49823bccc45fc6d1800a9d00188ffaa769e
bakwxh <omega1653@gmail.com>
2019-10-30 20:08:23 +0800

    Add model tests for Recipe

---
+++ src/test/java/dukecooks/model/recipe/FatsTest.java
@@ -0,0 +1,41 @@
+package dukecooks.model.recipe;
+
+import static dukecooks.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import dukecooks.model.recipe.components.Fats;
+
+public class FatsTest {
+    
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Fats(null));
+    }
+
+    @Test
+    public void constructor_invalidFats_throwsIllegalArgumentException() {
+        String invalidFats = "";
+        assertThrows(IllegalArgumentException.class, () -> new Fats(invalidFats));
+    }
+
+    @Test
+    public void isValidFats() {
+        // null fats number
+        assertThrows(NullPointerException.class, () -> Fats.isValidFats(null));
+
+        // invalid fats numbers
+        assertFalse(Fats.isValidFats("")); // empty string
+        assertFalse(Fats.isValidFats(" ")); // spaces only
+        assertFalse(Fats.isValidFats("fats")); // non-numeric
+        assertFalse(Fats.isValidFats("9011p041")); // alphabets within digits
+        assertFalse(Fats.isValidFats("9312 1534")); // spaces within digits
+
+        // valid fats numbers
+        assertTrue(Fats.isValidFats("911")); // exactly 3 numbers
+        assertTrue(Fats.isValidFats("93121534"));
+        assertTrue(Fats.isValidFats("124293842033123")); // long fats numbers
+    }
+}
---
+++ src/test/java/dukecooks/model/recipe/ProteinTest.java
@@ -0,0 +1,41 @@
+package dukecooks.model.recipe;
+
+import static dukecooks.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import dukecooks.model.recipe.components.Protein;
+
+public class ProteinTest {
+    
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Protein(null));
+    }
+
+    @Test
+    public void constructor_invalidProtein_throwsIllegalArgumentException() {
+        String invalidProtein = "";
+        assertThrows(IllegalArgumentException.class, () -> new Protein(invalidProtein));
+    }
+
+    @Test
+    public void isValidProtein() {
+        // null protein number
+        assertThrows(NullPointerException.class, () -> Protein.isValidProtein(null));
+
+        // invalid protein numbers
+        assertFalse(Protein.isValidProtein("")); // empty string
+        assertFalse(Protein.isValidProtein(" ")); // spaces only
+        assertFalse(Protein.isValidProtein("protein")); // non-numeric
+        assertFalse(Protein.isValidProtein("9011p041")); // alphabets within digits
+        assertFalse(Protein.isValidProtein("9312 1534")); // spaces within digits
+
+        // valid protein numbers
+        assertTrue(Protein.isValidProtein("911")); // exactly 3 numbers
+        assertTrue(Protein.isValidProtein("93121534"));
+        assertTrue(Protein.isValidProtein("124293842033123")); // long protein numbers
+    }
+}
