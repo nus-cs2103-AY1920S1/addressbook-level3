f490e3fad922db561ac5d45544dab3227f6d563e
SimNee <chua_sim_nee@hotmail.com>
2019-10-31 02:20:58 +0800

    checkstyle

--- src/test/java/organice/logic/commands/AddCommandTest.java
+++ src/test/java/organice/logic/commands/AddCommandTest.java
@@ -10,9 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
-
 import organice.logic.commands.exceptions.CommandException;
-
 import organice.model.AddressBook;
 import organice.model.ModelStub;
 import organice.model.ReadOnlyAddressBook;
@@ -20,7 +18,6 @@ import organice.model.person.Doctor;
 import organice.model.person.Donor;
 import organice.model.person.Patient;
 import organice.model.person.Person;
-import organice.model.person.exceptions.PersonNotFoundException;
 import organice.testutil.DoctorBuilder;
 import organice.testutil.DonorBuilder;
 import organice.testutil.PatientBuilder;
