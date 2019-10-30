37602bcfc980a1ba164a21eb9dc84d203e3a1a08
EvonDong <e0322896@u.nus.edu>
2019-10-30 11:24:48 +0800

    Solve conflict

--- src/main/java/seedu/address/cashier/storage/StorageManager.java
+++ src/main/java/seedu/address/cashier/storage/StorageManager.java
@@ -30,6 +30,7 @@ public class StorageManager implements Storage {
         this.transactionLogic = transactionLogic;
     }
 
+    
     /*public StorageManager(File iFile, File tFile, seedu.address.person.model.Model personModel) {
         this.iFile = iFile;
         this.tFile = tFile;
