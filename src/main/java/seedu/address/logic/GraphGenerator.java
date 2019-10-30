6ee8d7ef6caec9be091ae9c6c25e8c6a4962003f
uberSaiyan <yanwl.note@gmail.com>
2019-10-30 14:01:03 +0800

    Intermediate

--- src/main/java/seedu/address/logic/GraphGenerator.java
+++ src/main/java/seedu/address/logic/GraphGenerator.java
@@ -31,8 +31,11 @@ import seedu.address.model.schedule.Schedule;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -56,118 +59,47 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 class GraphGenerator {
 
-    private static final List<String> commandWords = Arrays.asList(
-            "add-c", "add-o", "add-p", "add-s",
-            "delete-c", "delete-p", "delete-s",
-            "edit-c", "edit-o", "edit-p", "edit-s",
-            "switch-c", "switch-o", "switch-p", "switch-s",
-            "list", "cancel", "exit", "help",
-            "generate-s"
-    );
-
     private final Model model;
-
-    private Graph addCustomerGraph;
-    private Graph addPhoneGraph;
-    private Graph addOrderGraph;
-    private Graph statisticsGraph;
+    private final Map<String, Graph> graphs;
 
     GraphGenerator(Model model) {
         this.model = model;
+        graphs = new HashMap<>();
 
-        setAddCustomerGraph();
-        setAddPhoneGraph();
-        setAddOrderGraph();
-        setStatisticsGraph();
-    }
-
-    private void setAddCustomerGraph() {
-        List<Customer> customerList = model.getCustomerBook().getList();
-        Node<Customer> addCustomerStartNode = new AddCustomerStartNode(Collections.emptyList());
-        Node<Customer> customerContactNumberNode = new CustomerContactNumberNode(customerList);
-        Node<Customer> customerEmailNode = new CustomerEmailNode(customerList);
-        Node<Customer> customerNameNode = new CustomerNameNode(customerList);
-        Node<Customer> customerTagNode = new CustomerTagNode(customerList);
-        addCustomerGraph = new Graph(addCustomerStartNode, Arrays.asList(
-                new Edge(PREFIX_NAME, addCustomerStartNode, customerNameNode),
-                new Edge(PREFIX_CONTACT, customerNameNode, customerContactNumberNode),
-                new Edge(PREFIX_EMAIL, customerContactNumberNode, customerEmailNode),
-                new Edge(PREFIX_TAG, customerEmailNode, customerTagNode),
-                new Edge(PREFIX_TAG, customerTagNode, customerTagNode)
-        ));
-    }
-
-    private void setAddPhoneGraph() {
-        List<Phone> phoneList = model.getPhoneBook().getList();
-        Node<Phone> addPhoneStartNode = new AddPhoneStartNode(phoneList);
-        Node<Phone> phoneIdentityNumberNode = new PhoneIdentityNumberNode(phoneList);
-        Node<Phone> phoneSerialNumberNode = new PhoneSerialNumberNode(phoneList);
-        Node<Phone> phoneNameNode = new PhoneNameNode(phoneList);
-        Node<Phone> phoneBrandNode = new PhoneBrandNode(phoneList);
-        Node<Phone> phoneCapacityNode = new PhoneCapacityNode(phoneList);
-        Node<Phone> phoneColourNode = new PhoneColourNode(phoneList);
-        Node<Phone> phoneCostNode = new PhoneCostNode(phoneList);
-        Node<Phone> phoneTagNode = new PhoneTagNode(phoneList);
-        addPhoneGraph = new Graph(addPhoneStartNode, Arrays.asList(
-                new Edge(PREFIX_IDENTITY_NUM, addPhoneStartNode, phoneIdentityNumberNode),
-                new Edge(PREFIX_SERIAL_NUM, phoneIdentityNumberNode, phoneSerialNumberNode),
-                new Edge(PREFIX_PHONE_NAME, phoneSerialNumberNode, phoneNameNode),
-                new Edge(PREFIX_BRAND, phoneNameNode, phoneBrandNode),
-                new Edge(PREFIX_CAPACITY, phoneBrandNode, phoneCapacityNode),
-                new Edge(PREFIX_COLOUR, phoneCapacityNode, phoneColourNode),
-                new Edge(PREFIX_COST, phoneColourNode, phoneCostNode),
-                new Edge(PREFIX_TAG, phoneCostNode, phoneTagNode),
-                new Edge(PREFIX_TAG, phoneTagNode, phoneTagNode)
-        ));
-    }
-
-    private void setAddOrderGraph() {
-        List<Order> orderList = model.getOrderBook().getList();
-        Node<Order> addOrderStartNode = new AddOrderStartNode(orderList);
-        Node<Order> orderCustomerIndexNode = new OrderCustomerIndexNode(orderList);
-        Node<Order> orderPhoneIndexNode = new OrderPhoneIndexNode(orderList);
-        Node<Order> orderPriceNode = new OrderPriceNode(orderList);
-        Node<Order> orderTagNode = new OrderTagNode(orderList);
-        addOrderGraph = new Graph(addOrderStartNode, Arrays.asList(
-                new Edge(PREFIX_PHONE, addOrderStartNode, orderCustomerIndexNode),
-                new Edge(PREFIX_CUSTOMER, orderCustomerIndexNode, orderPhoneIndexNode),
-                new Edge(PREFIX_PRICE, orderPhoneIndexNode, orderPriceNode),
-                new Edge(PREFIX_TAG, orderPriceNode, orderTagNode),
-                new Edge(PREFIX_TAG, orderTagNode, orderTagNode)
-        ));
-    }
-
-    private void setStatisticsGraph() {
-        List<Schedule> scheduleList = model.getScheduleBook().getList();
-        Node<StatisticType> statsTypeNode = new StatsTypeNode(Arrays.asList(StatisticType.values()));
-        Node<Schedule> statsDateNode = new StatsDateNode(scheduleList);
-        Node<Schedule> statsStartNode = new StatsStartNode(scheduleList);
-        statisticsGraph = new Graph(statsStartNode, Arrays.asList(
-            new Edge(PREFIX_STAT_TYPE, statsStartNode, statsTypeNode),
-            new Edge(PREFIX_STARTING_DATE, statsTypeNode, statsDateNode),
-            new Edge(PREFIX_ENDING_DATE, statsDateNode, statsDateNode)
-        ));
+        graphs.put("add-c", generateAddCustomerGraph());
+        graphs.put("add-p",  generateAddPhoneGraph());
+        graphs.put("add-o", generateAddOrderGraph());
+        graphs.put("add-s", generateAddScheduleGraph());
+        graphs.put("delete-c", null);
+        graphs.put("delete-p", null);
+        graphs.put("delete-s", null);
+        graphs.put("edit-c", null);
+        graphs.put("edit-p", null);
+        graphs.put("edit-o", null);
+        graphs.put("edit-s", null);
+        graphs.put("switch-c", null);
+        graphs.put("switch-p", null);
+        graphs.put("switch-o", null);
+        graphs.put("switch-s", null);
+        graphs.put("list", null);
+        graphs.put("cancel", null);
+        graphs.put("exit", null);
+        graphs.put("help", null);
+        graphs.put("generate-s", generateStatisticsGraph());
     }
 
     private Optional<Graph> getGraph(String commandWord) {
-        switch (commandWord) {
-        case "add-c":
-            return Optional.of(addCustomerGraph);
-        case "add-p":
-            return Optional.of(addPhoneGraph);
-        case "add-o":
-            return Optional.of(addOrderGraph);
-        case "generate-s":
-            return Optional.of(statisticsGraph);
-        default:
-            return Optional.empty();
-        }
+        return Optional.ofNullable(graphs.get(commandWord));
+    }
+
+    private Set<String> getCommandWords() {
+        return graphs.keySet();
     }
 
     AutoCompleteResult process(String input) {
         int firstSpace = input.indexOf(" ");
         if (firstSpace == -1) { // there is no space, indicating still typing command word
-            SortedSet<String> values = new TreeSet<>(commandWords);
+            SortedSet<String> values = new TreeSet<>(getCommandWords());
             return new AutoCompleteResult(values, input);
         } else { // there is at least one space, suggesting command word is present
             String commandWord = input.substring(0, firstSpace);
@@ -184,6 +116,78 @@ class GraphGenerator {
                 return new AutoCompleteResult(new TreeSet<>(), input);
             }
         }
+    }
+
+    private Graph generateAddCustomerGraph() {
+        List<Customer> customerList = model.getCustomerBook().getList();
+        Node<Customer> addCustomerStartNode = new AddCustomerStartNode(Collections.emptyList());
+        Node<Customer> customerContactNumberNode = new CustomerContactNumberNode(customerList);
+        Node<Customer> customerEmailNode = new CustomerEmailNode(customerList);
+        Node<Customer> customerNameNode = new CustomerNameNode(customerList);
+        Node<Customer> customerTagNode = new CustomerTagNode(customerList);
+        return new Graph(addCustomerStartNode, Arrays.asList(
+                new Edge(PREFIX_NAME, addCustomerStartNode, customerNameNode),
+                new Edge(PREFIX_CONTACT, customerNameNode, customerContactNumberNode),
+                new Edge(PREFIX_EMAIL, customerContactNumberNode, customerEmailNode),
+                new Edge(PREFIX_TAG, customerEmailNode, customerTagNode),
+                new Edge(PREFIX_TAG, customerTagNode, customerTagNode)
+        ));
+    }
+
+    private Graph generateAddPhoneGraph() {
+        List<Phone> phoneList = model.getPhoneBook().getList();
+        Node<Phone> addPhoneStartNode = new AddPhoneStartNode(phoneList);
+        Node<Phone> phoneIdentityNumberNode = new PhoneIdentityNumberNode(phoneList);
+        Node<Phone> phoneSerialNumberNode = new PhoneSerialNumberNode(phoneList);
+        Node<Phone> phoneNameNode = new PhoneNameNode(phoneList);
+        Node<Phone> phoneBrandNode = new PhoneBrandNode(phoneList);
+        Node<Phone> phoneCapacityNode = new PhoneCapacityNode(phoneList);
+        Node<Phone> phoneColourNode = new PhoneColourNode(phoneList);
+        Node<Phone> phoneCostNode = new PhoneCostNode(phoneList);
+        Node<Phone> phoneTagNode = new PhoneTagNode(phoneList);
+        return new Graph(addPhoneStartNode, Arrays.asList(
+                new Edge(PREFIX_IDENTITY_NUM, addPhoneStartNode, phoneIdentityNumberNode),
+                new Edge(PREFIX_SERIAL_NUM, phoneIdentityNumberNode, phoneSerialNumberNode),
+                new Edge(PREFIX_PHONE_NAME, phoneSerialNumberNode, phoneNameNode),
+                new Edge(PREFIX_BRAND, phoneNameNode, phoneBrandNode),
+                new Edge(PREFIX_CAPACITY, phoneBrandNode, phoneCapacityNode),
+                new Edge(PREFIX_COLOUR, phoneCapacityNode, phoneColourNode),
+                new Edge(PREFIX_COST, phoneColourNode, phoneCostNode),
+                new Edge(PREFIX_TAG, phoneCostNode, phoneTagNode),
+                new Edge(PREFIX_TAG, phoneTagNode, phoneTagNode)
+        ));
+    }
+
+    private Graph generateAddOrderGraph() {
+        List<Order> orderList = model.getOrderBook().getList();
+        Node<Order> addOrderStartNode = new AddOrderStartNode(orderList);
+        Node<Order> orderCustomerIndexNode = new OrderCustomerIndexNode(orderList);
+        Node<Order> orderPhoneIndexNode = new OrderPhoneIndexNode(orderList);
+        Node<Order> orderPriceNode = new OrderPriceNode(orderList);
+        Node<Order> orderTagNode = new OrderTagNode(orderList);
+        return new Graph(addOrderStartNode, Arrays.asList(
+                new Edge(PREFIX_PHONE, addOrderStartNode, orderCustomerIndexNode),
+                new Edge(PREFIX_CUSTOMER, orderCustomerIndexNode, orderPhoneIndexNode),
+                new Edge(PREFIX_PRICE, orderPhoneIndexNode, orderPriceNode),
+                new Edge(PREFIX_TAG, orderPriceNode, orderTagNode),
+                new Edge(PREFIX_TAG, orderTagNode, orderTagNode)
+        ));
+    }
+
+    private Graph generateAddScheduleGraph() {
+        return null;
+    }
+
+    private Graph generateStatisticsGraph() {
+        List<Schedule> scheduleList = model.getScheduleBook().getList();
+        Node<StatisticType> statsTypeNode = new StatsTypeNode(Arrays.asList(StatisticType.values()));
+        Node<Schedule> statsDateNode = new StatsDateNode(scheduleList);
+        Node<Schedule> statsStartNode = new StatsStartNode(scheduleList);
+        return new Graph(statsStartNode, Arrays.asList(
+            new Edge(PREFIX_STAT_TYPE, statsStartNode, statsTypeNode),
+            new Edge(PREFIX_STARTING_DATE, statsTypeNode, statsDateNode),
+            new Edge(PREFIX_ENDING_DATE, statsDateNode, statsDateNode)
+        ));
     }
 
 }
