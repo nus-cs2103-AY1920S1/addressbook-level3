cb434737d5a09c6f21d89301f02330fd7b5064cb
Aidil F <aidilfbk@users.noreply.github.com>
2019-10-30 23:16:06 +0800

    Update version number to v1.3.0

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -41,7 +41,7 @@ import seedu.address.websocket.Cache;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 2, 1, true);
+    public static final Version VERSION = new Version(1, 3, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
