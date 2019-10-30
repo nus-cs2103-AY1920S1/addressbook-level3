343b6316f75396eee36397ae08b579b407000b05
ShaunNgTX <ngtzexuan95@gmail.com>
2019-10-30 22:04:14 +0800

    added more command_usage for autoComplete function in AddCommand.java and ListCommand.java

--- src/main/java/seedu/revision/logic/commands/main/ListCommand.java
+++ src/main/java/seedu/revision/logic/commands/main/ListCommand.java
@@ -22,6 +22,7 @@ import seedu.revision.model.answerable.predicates.DifficultyPredicate;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
+    public static final String COMPLETE_COMMAND = "list diff/(optional) cat/(optional)";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all questions which belong to the "
             + "specified category and difficulty (case-insensitive, optional) "
             + "and displays them as a list with index numbers.\n"
