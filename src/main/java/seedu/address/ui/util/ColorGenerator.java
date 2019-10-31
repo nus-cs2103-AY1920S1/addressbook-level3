7767937e3fd06e53448fac2516d72cc6a0816470
LimYiJie <e0325578@u.nus.edu>
2019-11-01 00:01:23 +0800

    Spread colors temporarily

--- src/main/java/seedu/address/ui/util/ColorGenerator.java
+++ src/main/java/seedu/address/ui/util/ColorGenerator.java
@@ -7,9 +7,9 @@ import java.util.List;
  * A class to handle generation of colors.
  */
 public class ColorGenerator {
-    private static ArrayList<String> listOfColors = new ArrayList<String>(List.of("#FF8A80", "#FF80AB", "#F06292",
-            "#EA80FC", "#B388FF", "#8C9EFF", "#80D8FF", "#18FFFF", "#64FFDA", "#00E676", "#69F0AE", "#FFFF00",
-            "#FFC400", "#FFAB40", "#FF9E80", "#90A4AE", "#BCAAA4", "66FCF1"));
+    private static ArrayList<String> listOfColors = new ArrayList<String>(List.of("#FF8A80", "#F06292",
+            "#B388FF", "#8C9EFF", "#18FFFF", "#64FFDA", "#69F0AE", "#FFC400", "#FFFF00", "#FFAB40",
+            "#FF9E80", "#90A4AE", "#BCAAA4", "66FCF1", "#FF80AB", "#EA80FC", "#80D8FF", "#00E676"));
 
     /**
      * Method to generate a random list of colors to tag each group member with a particular colour.
