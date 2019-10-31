d240d91f193e18e60fb400b4145eb6cb78378ed4
shyanjie <shyanjieteo@hotmail.com>
2019-10-31 18:31:22 +0800

    Fix travis issues

--- src/main/java/seedu/address/logic/commands/note/ListNotesCommand.java
+++ src/main/java/seedu/address/logic/commands/note/ListNotesCommand.java
@@ -1,12 +1,15 @@
 package seedu.address.logic.commands.note;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_NOTES;
+
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_NOTES;
-
+/**
+ * Lists all notes in the address book to the user.
+ */
 public class ListNotesCommand extends Command {
     public static final String COMMAND_WORD = "listnote";
 
