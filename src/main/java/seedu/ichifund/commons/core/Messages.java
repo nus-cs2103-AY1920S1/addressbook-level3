7ef867c14ba3e9b58034dc74615130ce69d0e301
Tan Wei Yang <weiyang.1996@gmail.com>
2019-11-01 01:46:16 +0800

    Update messages

--- src/main/java/seedu/ichifund/commons/core/Messages.java
+++ src/main/java/seedu/ichifund/commons/core/Messages.java
@@ -11,6 +11,8 @@ public class Messages {
     public static final String MESSAGE_TRANSACTION_FROM_REPEATER = "Cannot delete or edit transactions from repeaters";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_TRANSACTIONS_LISTED_OVERVIEW = "%1$d transactions listed!";
+    public static final String MESSAGE_INVALID_TRANSACTION_DISPLAYED_INDEX =
+            "The transaction index provided is invalid";
     public static final String MESSAGE_INVALID_REPEATER_DISPLAYED_INDEX = "The repeater index provided is invalid";
     public static final String MESSAGE_BUDGETS_LISTED_OVERVIEW = "%1$d budgets listed!";
     public static final String MESSAGE_INVALID_BUDGET_DISPLAYED_INDEX = "The budget index provided is invalid";
