3126cbde3141ede551a0a6356068c5ee1daf20b9
hua-lun <chuahualun@gmail.com>
2019-10-31 19:20:18 +0800

    update pics

--- src/test/java/dukecooks/logic/commands/ListCommandTest.java
+++ src/test/java/dukecooks/logic/commands/ListCommandTest.java
@@ -2,13 +2,11 @@ package dukecooks.logic.commands;
 
 import static dukecooks.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static dukecooks.testutil.TypicalIndexes.INDEX_FIRST_RECIPE;
-import static dukecooks.testutil.dashboard.TypicalDashboard.getTypicalDashboardRecords;
 import static dukecooks.testutil.recipe.TypicalRecipes.getTypicalRecipeBook;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import dukecooks.logic.commands.dashboard.ListTaskCommand;
 import dukecooks.logic.commands.exercise.ListExerciseCommand;
 import dukecooks.logic.commands.recipe.ListRecipeCommand;
 import dukecooks.model.Model;
@@ -22,25 +20,16 @@ public class ListCommandTest {
 
     private Model model;
     private Model expectedModel;
-    private Model modelTask;
-    private Model expectedModelTask;
 
     @BeforeEach
     public void setUp() {
         model = new ModelManager(getTypicalRecipeBook(), new UserPrefs());
         expectedModel = new ModelManager(model.getRecipeBook(), new UserPrefs());
-        modelTask = new ModelManager(getTypicalDashboardRecords(), new UserPrefs());
-        expectedModelTask = new ModelManager(modelTask.getDashboardRecords(), new UserPrefs());
     }
 
     @Test
     public void execute_recipeListIsNotFiltered_showsSameList() {
         assertCommandSuccess(new ListRecipeCommand(), model, ListRecipeCommand.MESSAGE_SUCCESS, expectedModel);
-    }
-
-    @Test
-    public void execute_dashboardListIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListTaskCommand(), modelTask, ListTaskCommand.MESSAGE_SUCCESS, expectedModelTask);
     }
 
     @Test
