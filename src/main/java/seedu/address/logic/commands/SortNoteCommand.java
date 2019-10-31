de6c6af03af304a20586e2049f6d624ebce8e0be
Jit Yong <jityong1997@hotmail.com>
2019-10-31 20:29:43 +0800

    changed prefix of sort from sb/ to by/

--- src/main/java/seedu/address/logic/commands/SortNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/SortNoteCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SORTBY;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_NOTES;
 
 import java.util.function.Predicate;
 
