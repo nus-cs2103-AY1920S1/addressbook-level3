4c59ab583da30aaa14e6d51276142c6d11d6a2ee
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-31 19:08:56 +0800

    Add DescriptionTest

---
+++ src/test/java/seedu/ichifund/model/DescriptionTest.java
@@ -0,0 +1,40 @@
+package seedu.ichifund.model;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.ichifund.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class DescriptionTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Description(null));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidDescription = "";
+        assertThrows(IllegalArgumentException.class, () -> new Description(invalidDescription));
+    }
+
+    @Test
+    public void isValidDescription() {
+        // null name
+        assertThrows(NullPointerException.class, () -> Description.isValidDescription(null));
+
+        // invalid name
+        assertFalse(Description.isValidDescription("")); // empty string
+        assertFalse(Description.isValidDescription(" ")); // spaces only
+        assertFalse(Description.isValidDescription("^")); // only non-alphanumeric characters
+        assertFalse(Description.isValidDescription("dinner*")); // contains non-alphanumeric characters
+
+        // valid name
+        assertTrue(Description.isValidDescription("dinner with myself")); // alphabets only
+        assertTrue(Description.isValidDescription("000")); // numbers only
+        assertTrue(Description.isValidDescription("2nd lunch")); // alphanumeric characters
+        assertTrue(Description.isValidDescription("Breakfast with Tiffany")); // with capital letters
+        assertTrue(Description.isValidDescription("Breakfast with Tiffany because Sabrina disappeared"));
+    }
+
+}
