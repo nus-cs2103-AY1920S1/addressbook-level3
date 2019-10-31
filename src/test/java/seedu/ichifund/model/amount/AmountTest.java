376bfde28dbecbf247d26e31a44c8620d5024dc3
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-31 18:59:49 +0800

    Update AmountTest

--- src/test/java/seedu/ichifund/model/amount/AmountTest.java
+++ src/test/java/seedu/ichifund/model/amount/AmountTest.java
@@ -5,9 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.ichifund.testutil.Assert.assertThrows;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class AmountTest {
+    private static Amount AMOUNT_POSITIVE_1 = new Amount("10.20");
+    private static Amount AMOUNT_POSITIVE_2 = new Amount("0.50");
+    private static Amount AMOUNT_POSITIVE_3 = new Amount("5");
+    private static Amount AMOUNT_NEGATIVE_1 = new Amount("-6.20");
+    private static Amount AMOUNT_NEGATIVE_2 = new Amount("-0.90");
 
     @Test
     public void constructor_null_throwsNullPointerException() {
@@ -21,7 +28,7 @@ class AmountTest {
     }
 
     @Test
-    void isValidAmount() {
+    public void isValidAmount() {
         // null amount
         assertThrows(NullPointerException.class, () -> Amount.isValidAmount(null));
 
@@ -52,15 +59,76 @@ class AmountTest {
     }
 
     @Test
-    void testToString() {
-    }
-
-    @Test
-    void getValueInCents() {
+    public void getValueInCents() {
         Amount amount = new Amount("-5.70");
         assertEquals(amount.getValueInCents(), -570);
 
         amount = new Amount("5.70");
         assertEquals(amount.getValueInCents(), 570);
+    }
+
+    @Test
+    public void isNegative() {
+        // negative amount
+        assertTrue(Amount.isNegative("-0.50"));
+        assertTrue(Amount.isNegative("-10.50"));
+        assertTrue(Amount.isNegative("-50"));
+
+        // positive amount
+        assertFalse(Amount.isNegative("0.50"));
+        assertFalse(Amount.isNegative("10.50"));
+        assertFalse(Amount.isNegative("50"));
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(new Amount("0").toString(), "0.00");
+        assertEquals(new Amount("1").toString(), "1.00");
+        assertEquals(new Amount("1.05").toString(), "1.05");
+        assertEquals(new Amount("0.09").toString(), "0.09");
+        assertEquals(new Amount("-1").toString(), "-1.00");
+        assertEquals(new Amount("-1.05").toString(), "-1.05");
+        assertEquals(new Amount("-0.09").toString(), "-0.09");
+    }
+
+    @Test
+    public void negate() {
+        assertEquals(Amount.negate(new Amount("1.50")), new Amount ("-1.50"));
+    }
+
+    @Test
+    public void add() {
+        assertEquals(Amount.add(AMOUNT_POSITIVE_2, AMOUNT_POSITIVE_1).getValueInCents(),
+                AMOUNT_POSITIVE_2.getValueInCents() + AMOUNT_POSITIVE_1.getValueInCents());
+        assertEquals(Amount.add(AMOUNT_POSITIVE_2, AMOUNT_NEGATIVE_1).getValueInCents(),
+                AMOUNT_POSITIVE_2.getValueInCents() + AMOUNT_NEGATIVE_1.getValueInCents());
+        assertEquals(Amount.add(AMOUNT_NEGATIVE_2, AMOUNT_NEGATIVE_1).getValueInCents(),
+                AMOUNT_NEGATIVE_2.getValueInCents() + AMOUNT_NEGATIVE_1.getValueInCents());
+    }
+
+    @Test
+    public void subtract() {
+        assertEquals(Amount.subtract(AMOUNT_POSITIVE_2, AMOUNT_POSITIVE_1).getValueInCents(),
+                AMOUNT_POSITIVE_2.getValueInCents() - AMOUNT_POSITIVE_1.getValueInCents());
+        assertEquals(Amount.subtract(AMOUNT_POSITIVE_2, AMOUNT_NEGATIVE_1).getValueInCents(),
+                AMOUNT_POSITIVE_2.getValueInCents() - AMOUNT_NEGATIVE_1.getValueInCents());
+        assertEquals(Amount.subtract(AMOUNT_NEGATIVE_2, AMOUNT_NEGATIVE_1).getValueInCents(),
+                AMOUNT_NEGATIVE_2.getValueInCents() - AMOUNT_NEGATIVE_1.getValueInCents());
+    }
+
+    @Test
+    public void addAll() {
+        ArrayList<Amount> amounts = new ArrayList<>();
+        amounts.add(AMOUNT_POSITIVE_1);
+        amounts.add(AMOUNT_POSITIVE_2);
+        amounts.add(AMOUNT_POSITIVE_3);
+        amounts.add(AMOUNT_NEGATIVE_1);
+        amounts.add(AMOUNT_NEGATIVE_2);
+        assertEquals(Amount.addAll(amounts).getValueInCents(),
+                AMOUNT_POSITIVE_1.getValueInCents()
+                        + AMOUNT_POSITIVE_2.getValueInCents()
+                        + AMOUNT_POSITIVE_3.getValueInCents()
+                        + AMOUNT_NEGATIVE_1.getValueInCents()
+                        + AMOUNT_NEGATIVE_2.getValueInCents());
     }
 }
