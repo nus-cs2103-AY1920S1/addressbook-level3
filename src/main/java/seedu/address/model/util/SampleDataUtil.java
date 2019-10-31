7ab35a1f30bcd3505374d9be1b9c4c264e099b63
hellodommy <dominique@u.nus.edu>
2019-10-31 22:05:45 +0800

    Update sample persons to match athlete profiles

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -41,25 +41,24 @@ public class SampleDataUtil {
 
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"), new Gender(
-                "Male"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends"), new Photo("default.png")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Gender("Female"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends"), new Photo("default.png")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Gender("Female"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours"), new Photo("default.png")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Gender("Male"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family"), new Photo("default.png")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Gender("Male"), new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates"), new Photo("default.png")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Gender("Others"), new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"), new Photo("default.png"))
+            new Person(new Name("Amanda Lim"), new Phone("87438807"), new Email("amandalim@gmail.com"),
+                new Gender("female"), new Address("180 Paya Lebar Road, #07-06, Singapore 409032"),
+                getTagSet("freestyle", "medley"), new Photo("default.png")),
+            new Person(new Name("Michael Phelps"), new Phone("3856289119"), new Email("mike@hotmail.com"),
+                new Gender("Male"), new Address("3045 Hickory Heights Drive, Linthicum Heights, Maryland 21090"),
+                getTagSet("freestyle", "olympics"), new Photo("default.png")),
+            new Person(new Name("Tao Li"), new Phone("13092646433"), new Email("tao@yahoo.com"),
+                new Gender("Female"), new Address("16号楼中单元401, 青岛市-城阳区, 山东省 266109"),
+                getTagSet("backstroke", "butterfly"), new Photo("default.png")),
+            new Person(new Name("Joseph Schooling"), new Phone("91031282"), new Email("jojo@outlook.com"),
+                new Gender("Male"), new Address("Blk 216 Bedok North Street 1, #16-43, Singapore 460216"),
+                getTagSet("butterfly", "olympics"), new Photo("default.png")),
+            new Person(new Name("Sun Yang"), new Phone("12740154825"), new Email("sunyang@gmail.com"),
+                new Gender("Male"), new Address("伟业路1号高新软件园9号楼9楼, 杭州 310000, 浙江"),
+                getTagSet("freestyle"), new Photo("default.png")),
+            new Person(new Name("Chad le Clos"), new Phone("0855590053"), new Email("chad@gmail.com"),
+                new Gender("Male"), new Address("1769 Wattle St, Maclear, Eastern Cape 5481"),
+                getTagSet("butterfly"), new Photo("default.png"))
         };
     }
 
