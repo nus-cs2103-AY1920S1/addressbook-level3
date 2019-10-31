effbce7f55ab4cf40d227eb97e7ec774b1d4db22
alxkohh <alxkohh@gmail.com>
2019-11-01 02:23:35 +0800

    Add unit tests for BookmarkCommand and BookmarkCommandFactory

---
+++ src/test/java/com/dukeacademy/logic/commands/bookmark/BookmarkCommandFactoryTest.java
@@ -0,0 +1,31 @@
+package com.dukeacademy.logic.commands.bookmark;
+
+import static com.dukeacademy.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.dukeacademy.logic.commands.exceptions.InvalidCommandArgumentsException;
+import com.dukeacademy.testutil.MockQuestionsLogic;
+
+class BookmarkCommandFactoryTest {
+
+    @Test
+    void testGetCommandWord() {
+        MockQuestionsLogic questionsLogic = MockQuestionsLogic.getMockQuestionsLogicWithTypicalQuestions();
+        BookmarkCommandFactory factory = new BookmarkCommandFactory(questionsLogic);
+        assertEquals("bookmark", factory.getCommandWord());
+    }
+
+    @Test
+    void testGetCommand() throws InvalidCommandArgumentsException {
+        MockQuestionsLogic questionsLogic = MockQuestionsLogic.getMockQuestionsLogicWithTypicalQuestions();
+        BookmarkCommandFactory factory = new BookmarkCommandFactory(questionsLogic);
+        assertTrue(factory.getCommand("1") instanceof BookmarkCommand);
+
+        assertThrows(InvalidCommandArgumentsException.class,
+                "Invalid index entered.",
+                () -> factory.getCommand("testerString"));
+    }
+}
---
+++ src/test/java/com/dukeacademy/logic/commands/bookmark/BookmarkCommandTest.java
@@ -0,0 +1,20 @@
+package com.dukeacademy.logic.commands.bookmark;
+
+import static com.dukeacademy.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.dukeacademy.logic.commands.exceptions.CommandException;
+import com.dukeacademy.testutil.MockQuestionsLogic;
+
+class BookmarkCommandTest {
+
+    @Test
+    void testExecute() throws CommandException {
+        MockQuestionsLogic questionsLogic = MockQuestionsLogic.getMockQuestionsLogicWithTypicalQuestions();
+        BookmarkCommand bookmarkCommand = new BookmarkCommand(-1, questionsLogic);
+        assertThrows(CommandException.class,
+                "Index -1 entered out of range for current list of questions.",
+                () -> bookmarkCommand.execute());
+    }
+}
