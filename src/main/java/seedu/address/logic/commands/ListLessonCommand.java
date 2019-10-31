08167be83be2c12d076744445da89bb1a285ff66
weikiat97 <42583086+weikiat97@users.noreply.github.com>
2019-11-01 05:11:10 +0800

    Update ListLessonCommand.java

--- src/main/java/seedu/address/logic/commands/ListLessonCommand.java
+++ src/main/java/seedu/address/logic/commands/ListLessonCommand.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_LESSONS;
+//import static seedu.address.model.Model.PREDICATE_SHOW_ALL_LESSONS;
 
 import seedu.address.model.Model;
 
