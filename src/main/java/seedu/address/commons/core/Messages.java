2817b3edd9c4f498177e911e3285ef0d54c20aa7
shyanjie <shyanjieteo@hotmail.com>
2019-10-31 16:20:11 +0800

    Implement Find features for notes

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -14,6 +14,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_NOTES_DISPLAYED_INDEX = "The notes index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_EARNINGS_LISTED_OVERVIEW = "%1$d earnings listed!";
+    public static final String MESSAGE_NOTES_LISTED_OVERVIEW = "%1$d notes listed!";
     public static final String MESSAGE_USERNAME_ERROR = "Username is invalid. Please try again.";
     public static final String MESSAGE_PASSWORD_ERROR = "Password is invalid. Please try again.";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
