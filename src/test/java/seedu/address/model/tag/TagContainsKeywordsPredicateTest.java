523ee54821f8f54186f8d152d2e0b9771f65b63b
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2019-10-31 23:27:29 +0800

    updated ui screenshot

--- src/test/java/seedu/address/model/tag/TagContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/tag/TagContainsKeywordsPredicateTest.java
@@ -1,6 +1,7 @@
 package seedu.address.model.tag;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
