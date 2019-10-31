a87aeb74edf69e60357f11a717e58fe1350ed2d7
ShiHaoYap <shihaoyap@gmail.com>
2019-10-31 22:53:31 +0800

    found minor bug

--- src/main/java/seedu/address/logic/commands/event/FindEventCommand.java
+++ src/main/java/seedu/address/logic/commands/event/FindEventCommand.java
@@ -2,10 +2,12 @@ package seedu.address.logic.commands.event;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.ObservableList;
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
+import seedu.address.model.event.Event;
 import seedu.address.model.event.EventNameContainsKeywordsPredicate;
 import seedu.address.ui.MainWindow;
 
@@ -19,7 +21,7 @@ public class FindEventCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all events whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " party";
 
     private final EventNameContainsKeywordsPredicate predicate;
 
@@ -30,13 +32,16 @@ public class FindEventCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        ObservableList<Event> currentEventList;
         if (MainWindow.getCurrentTabIndex() == 0) {
             model.updateFilteredEventList(predicate);
+            currentEventList = model.getFilteredEventList();
         } else {
             model.updateFilteredScheduledEventList(predicate);
+            currentEventList = model.getFilteredScheduledEventList();
         }
         return new CommandResult(
-                String.format(Messages.MESSAGE_EVENTS_LISTED_OVERVIEW, model.getFilteredEventList().size()));
+                String.format(Messages.MESSAGE_EVENTS_LISTED_OVERVIEW, currentEventList.size()));
     }
 
     @Override
