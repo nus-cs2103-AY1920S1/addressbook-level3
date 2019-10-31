08ea46e4159f09a1b1da9c63d6e9d938f7431bd6
JXKENG <jxkeng119@gmail.com>
2019-10-31 15:13:37 +0800

    Refactor CustomMode to factory to accomodate optional values for timer, category and difficulty

--- src/main/java/seedu/revision/model/quiz/CustomMode.java
+++ src/main/java/seedu/revision/model/quiz/CustomMode.java
@@ -1,47 +1,51 @@
 package seedu.revision.model.quiz;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.revision.commons.util.AppUtil.checkArgument;
-
 import seedu.revision.model.answerable.Difficulty;
+import seedu.revision.model.answerable.predicates.CategoryPredicate;
+import seedu.revision.model.answerable.predicates.DifficultyPredicate;
 import seedu.revision.model.category.Category;
-import seedu.revision.ui.bar.Timer;
 
-/**
- * Represents the mode of a quiz in the Revision Tool.
- * Guarantees: immutable; is valid as declared in {@link #isValidMode(String)}
- */
 public class CustomMode extends Mode {
+    private int time;
+    private DifficultyPredicate difficultyPredicate;
+    private CategoryPredicate categoryPredicate;
 
-    int timePerQuestion;
-    Difficulty difficulty;
-    Category category;
-
-    public CustomMode(String value, int timePerQuestion, Difficulty difficulty, Category category) {
+    public CustomMode(String value) {
         super(value);
-        this.timePerQuestion = timePerQuestion;
-        this.difficulty = difficulty;
-        this.category = category;
+        this.time = Mode.NORMAL_MODE_TIME;
+        this.categoryPredicate = Mode.NORMAL_MODE_CATEGORY_PREDICATE;
+        this.difficultyPredicate = Mode.NORMAL_MODE_DIFFCULTY_PREDICATE;
+
+    }
+
+    public CustomMode(String value, int time,
+                        CategoryPredicate categoryPredicate, DifficultyPredicate difficultyPredicate) {
+        super(value);
+        this.time = time;
+        this.categoryPredicate = categoryPredicate;
+        this.difficultyPredicate = difficultyPredicate;
+    }
+
+    public CustomMode withTime(int time) {
+        this.time = time;
+        return this;
+    }
+
+    public CustomMode withCategoryPredicate(Category category) {
+        this.categoryPredicate = new CategoryPredicate(category);
+        return this;
+    }
+
+    public CustomMode withDifficultyPredicate(Difficulty difficulty) {
+        this.difficultyPredicate = new DifficultyPredicate(difficulty);
+        return this;
+    }
+
+    public CustomMode build() {
+        return new CustomMode(value, time, categoryPredicate, difficultyPredicate);
     }
 
 
-
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof CustomMode // instanceof handles nulls
-                && value.equals(((CustomMode) other).value)); // state check
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
+}
 
 }
