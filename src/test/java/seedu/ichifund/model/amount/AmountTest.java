07c2416d726041c8350a35bf99f95ce07f7c8463
Tan Wei Yang <weiyang.1996@gmail.com>
2019-10-31 18:10:33 +0800

    Update AmountTest

--- src/test/java/seedu/ichifund/model/amount/AmountTest.java
+++ src/test/java/seedu/ichifund/model/amount/AmountTest.java
@@ -15,27 +15,40 @@ class AmountTest {
     }
 
     @Test
+    public void constructor_invalidAmount_throwsIllegalArgumentException() {
+        String invalidAmount = "";
+        assertThrows(IllegalArgumentException.class, () -> new Amount(invalidAmount));
+    }
+
+    @Test
     void isValidAmount() {
         // null amount
         assertThrows(NullPointerException.class, () -> Amount.isValidAmount(null));
 
-        // invalid amount
+        // invalid amount - alphabetical characters
+        assertFalse(Amount.isValidAmount("ab.cd"));
+
+        // invalid amount - dollars and cents format
         assertFalse(Amount.isValidAmount("50.0"));
         assertFalse(Amount.isValidAmount("50."));
         assertFalse(Amount.isValidAmount(".05"));
+
+        // invalid amount - leading zeroes
         assertFalse(Amount.isValidAmount("050"));
         assertFalse(Amount.isValidAmount("050.00"));
         assertFalse(Amount.isValidAmount("00.00"));
 
-        // valid amount
+        // valid amount - positive
         assertTrue(Amount.isValidAmount("50.00"));
-        assertTrue(Amount.isValidAmount("-49.70"));
         assertTrue(Amount.isValidAmount("50"));
-        assertTrue(Amount.isValidAmount("-50"));
-        assertTrue(Amount.isValidAmount("-0.50"));
         assertTrue(Amount.isValidAmount("0.50"));
         assertTrue(Amount.isValidAmount("0.00"));
         assertTrue(Amount.isValidAmount("0"));
+
+        // valid amount - negative
+        assertTrue(Amount.isValidAmount("-49.70"));
+        assertTrue(Amount.isValidAmount("-50"));
+        assertTrue(Amount.isValidAmount("-0.50"));
     }
 
     @Test
@@ -46,5 +59,8 @@ class AmountTest {
     void getValueInCents() {
         Amount amount = new Amount("-5.70");
         assertEquals(amount.getValueInCents(), -570);
+
+        amount = new Amount("5.70");
+        assertEquals(amount.getValueInCents(), 570);
     }
 }
