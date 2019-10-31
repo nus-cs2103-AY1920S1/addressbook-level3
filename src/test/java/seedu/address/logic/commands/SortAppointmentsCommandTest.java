a43326d595158590296b2f00f48248362541e25c
ReignOfComputer <roc@reignofcomputer.com>
2019-11-01 04:49:05 +0800

    fix: checkstyle

--- src/test/java/seedu/address/logic/commands/SortAppointmentsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortAppointmentsCommandTest.java
@@ -1,9 +1,6 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
 import java.util.function.Predicate;
