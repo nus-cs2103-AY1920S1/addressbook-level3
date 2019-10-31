aa262ac816748d70eff0b07297979d1c2ca33a08
Francis <francislow_6@hotmail.com>
2019-10-31 22:55:21 +0800

    Combined ListUpdater and ListFilter class

--- src/main/java/cs/f10/t1/nursetraverse/autocomplete/AutoCompleteListHandler.java
+++ src/main/java/cs/f10/t1/nursetraverse/autocomplete/AutoCompleteListHandler.java
@@ -1,14 +1,42 @@
 package cs.f10.t1.nursetraverse.autocomplete;
 
-import java.util.LinkedList;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
- * Filter out autocomplete words in the current list that are unrelated to the already matched words
+ * A handler class that updates list of autocomplete words to be shown according to userinput
  */
-public class AutoCompleteListFilter {
+public class AutoCompleteListHandler {
+    /**
+     * Update list of autocomplete words to be suggested according to current phrase in command box textfield
+     *
+     * @param numberOfMatchedWords number of matched words in command textfield
+     * @param segments Array of segments of the full command in textfield
+     * @param firstSegmentParts Linkedlist of parts in first segment of segments array
+     */
+    public ObservableList<AutoCompleteWord> updateList(ObservableList<AutoCompleteWord> currentList,
+                                                       int numberOfMatchedWords, String[] segments,
+                                                       LinkedList<String> firstSegmentParts) {
+        ObservableList<AutoCompleteWord> updatedList = FXCollections.observableArrayList();
+
+        LinkedList<String> combinedList = new LinkedList<>(firstSegmentParts);
+        combinedList.addAll(Arrays.asList(segments).subList(1, segments.length));
+
+        if (numberOfMatchedWords == combinedList.size()) {
+            return currentList;
+        } else {
+            for (AutoCompleteWord autoCompleteWord : currentList) {
+                if (autoCompleteWord.getSuggestedWord().startsWith(combinedList.get(numberOfMatchedWords))) {
+                    updatedList.add(autoCompleteWord);
+                }
+            }
+            return updatedList;
+        }
+    }
+
     /**
      * Filter unrelated words from either prefix or command wordlist
      *
