8a0c3d75415261e2f8f364366fd2ae9c23fceb38
Junwei <junwei.97@hotmail.com>
2019-11-01 08:43:31 +0800

    Update UG and fix image upload

--- src/main/java/dukecooks/model/util/SampleDiaryDataUtil.java
+++ src/main/java/dukecooks/model/util/SampleDiaryDataUtil.java
@@ -1,22 +1,31 @@
 package dukecooks.model.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import dukecooks.model.diary.DiaryRecords;
 import dukecooks.model.diary.ReadOnlyDiary;
 import dukecooks.model.diary.components.Diary;
 import dukecooks.model.diary.components.DiaryName;
+import dukecooks.model.diary.components.Page;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  * Contains utility methods for populating {@code DiaryRecords} with sample data.
  */
-public class DiarySampleDataUtil {
+public class SampleDiaryDataUtil {
     public static Diary[] getSampleDiaries() {
+
+        ArrayList<Page> pageList = new ArrayList<>(Arrays.asList(SamplePagesDataUtil.getSamplePages()));
+        ObservableList<Page> pages = FXCollections.observableArrayList(pageList);
         return new Diary[] {
-            new Diary(new DiaryName("Asian Cuisines")),
-            new Diary(new DiaryName("Healthy Living")),
+            new Diary(new DiaryName("Asian Cuisines"), pages),
+            new Diary(new DiaryName("Healthy Living"), pages),
             new Diary(new DiaryName("Meat Lovers")),
-            new Diary(new DiaryName("Vegan Diet")),
+            new Diary(new DiaryName("Vegan Diet"), pages),
             new Diary(new DiaryName("One Week Slimming")),
-            new Diary(new DiaryName("Core Exercises")),
+            new Diary(new DiaryName("Core Exercises"), pages),
         };
     }
 
