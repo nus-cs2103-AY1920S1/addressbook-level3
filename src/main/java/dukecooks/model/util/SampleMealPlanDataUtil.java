c33c7fb10dc5cc83c546139da26ed52bb5a85241
bakwxh <omega1653@gmail.com>
2019-11-01 08:48:23 +0800

    Fix naming issues

--- src/main/java/dukecooks/model/util/SampleMealPlanDataUtil.java
+++ src/main/java/dukecooks/model/util/SampleMealPlanDataUtil.java
@@ -15,20 +15,20 @@ import dukecooks.model.recipe.components.RecipeName;
  */
 public class SampleMealPlanDataUtil {
     public static MealPlan[] getSampleMealPlans() {
-        List<RecipeName> TEA = Arrays.asList(new RecipeName[]{new RecipeName("Tea")});
-        List<RecipeName> MILO = Arrays.asList(new RecipeName[]{new RecipeName("Milo")});
-        List<RecipeName> OMELETTE = Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")});
-        List<RecipeName> TUNA = Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")});
-        List<RecipeName> MAGGI = Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")});
-        List<RecipeName> FRIED = Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")});
+        List<RecipeName> Tea = Arrays.asList(new RecipeName[]{new RecipeName("Tea")});
+        List<RecipeName> Milo = Arrays.asList(new RecipeName[]{new RecipeName("Milo")});
+        List<RecipeName> Omelette = Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")});
+        List<RecipeName> Tuna = Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")});
+        List<RecipeName> Maggi = Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")});
+        List<RecipeName> Fried = Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")});
         return new MealPlan[] {
-            new MealPlan(new MealPlanName("Tea Plan"), TEA, TEA, TEA, TEA, TEA, TEA, TEA),
-            new MealPlan(new MealPlanName("Milo Plan"), MILO, MILO, MILO, MILO, MILO, MILO, MILO),
-            new MealPlan(new MealPlanName("Cheese Omelette Plan"), OMELETTE, OMELETTE, OMELETTE, OMELETTE, OMELETTE,
-                    OMELETTE, OMELETTE),
-            new MealPlan(new MealPlanName("Tuna Sandwich Plan"), TUNA, TUNA, TUNA, TUNA, TUNA, TUNA, TUNA),
-            new MealPlan(new MealPlanName("Chicken Maggi Plan"), MAGGI, MAGGI, MAGGI, MAGGI, MAGGI, MAGGI, MAGGI),
-            new MealPlan(new MealPlanName("Fried Chicken Plan"), FRIED, FRIED, FRIED, FRIED, FRIED, FRIED, FRIED),
+            new MealPlan(new MealPlanName("Tea Plan"), Tea, Tea, Tea, Tea, Tea, Tea, Tea),
+            new MealPlan(new MealPlanName("Milo Plan"), Milo, Milo, Milo, Milo, Milo, Milo, Milo),
+            new MealPlan(new MealPlanName("Cheese Omelette Plan"), Omelette, Omelette, Omelette, Omelette, Omelette,
+                    Omelette, Omelette),
+            new MealPlan(new MealPlanName("Tuna Sandwich Plan"), Tuna, Tuna, Tuna, Tuna, Tuna, Tuna, Tuna),
+            new MealPlan(new MealPlanName("Chicken Maggi Plan"), Maggi, Maggi, Maggi, Maggi, Maggi, Maggi, Maggi),
+            new MealPlan(new MealPlanName("Fried Chicken Plan"), Fried, Fried, Fried, Fried, Fried, Fried, Fried),
         };
     }
 
