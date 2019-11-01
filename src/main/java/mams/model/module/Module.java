1e61f8bdd5abe7854d1bf82581a77206eaef9c39
Whitebells <sky.lasts.mirror@gmail.com>
2019-11-01 18:24:46 +0800

    Added separating comma to Module#getTimeSlotAsString()

--- src/main/java/mams/model/module/Module.java
+++ src/main/java/mams/model/module/Module.java
@@ -281,6 +281,8 @@ public class Module {
         final StringBuilder builder = new StringBuilder();
         int startTimeSlot = 0;
 
+        assert(slots.length != 0);
+
         int test;
         for (int i = 0; i < slots.length; i++) {
             test = slots[i];
@@ -291,8 +293,11 @@ public class Module {
                         .append(" ")
                         .append(getTime(slots[i]))
                         .append(" to ")
-                        .append(getTime(slots[i] + 1))
-                        .append(" "); //Ends at next hour
+                        .append(getTime(slots[i] + 1)); //Ends at next hour
+                // addition of separating comma
+                if (i != slots.length - 1) {
+                    builder.append(", ");
+                }
                 startTimeSlot = 0;
             } else if (startTimeSlot == 0 && (i != slots.length - 1)) {
                 //first hour of 2/3hr sessions.
@@ -303,8 +308,11 @@ public class Module {
                         .append(" ")
                         .append(getTime(startTimeSlot))
                         .append(" to ")
-                        .append(getTime(slots[i] + 1))
-                        .append(" "); //Ends at next hour
+                        .append(getTime(slots[i] + 1)); //Ends at next hour
+                // addition of separating comma
+                if (i != slots.length - 1) {
+                    builder.append(", ");
+                }
                 startTimeSlot = 0;
             }
         }
