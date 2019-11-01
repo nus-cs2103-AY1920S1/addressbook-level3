b1d4714dbd43ced5bfa51165815af19d71ec9790
SakuraBlossom <shawnlum123@gmail.com>
2019-11-02 02:28:09 +0800

    Fixes command result messages for registering person

--- src/main/java/seedu/address/logic/commands/staff/EditStaffDetailsCommand.java
+++ src/main/java/seedu/address/logic/commands/staff/EditStaffDetailsCommand.java
@@ -38,7 +38,7 @@ public class EditStaffDetailsCommand extends ReversibleCommand {
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Staff details: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This staff member already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This staff member has already been registered.";
 
     private final Person personToEdit;
     private final Person editedPerson;
