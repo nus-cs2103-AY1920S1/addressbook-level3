2fe66048d197fe854d88cdd4a65c5192eea2305a
LimYiJie <e0325578@u.nus.edu>
2019-11-02 00:42:06 +0800

    Checkstyle issue

--- src/test/java/seedu/address/ui/uicomponent/PersonCardTest.java
+++ src/test/java/seedu/address/ui/uicomponent/PersonCardTest.java
@@ -3,25 +3,26 @@ package seedu.address.ui.uicomponent;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.util.NodeQueryUtils.hasText;
 
+import java.util.concurrent.TimeoutException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
-
 import org.testfx.framework.junit5.Start;
+import org.testfx.framework.junit5.Stop;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.testfx.framework.junit5.Stop;
+
 import seedu.address.model.display.sidepanel.PersonDisplay;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.TypicalPersons;
 import seedu.address.ui.PersonCard;
 
-import java.util.concurrent.TimeoutException;
 
 @ExtendWith(ApplicationExtension.class)
 public class PersonCardTest {
@@ -39,6 +40,10 @@ public class PersonCardTest {
         FxToolkit.cleanupStages();
     }
 
+    /**
+     * Initialise person card test with a stage.
+     * @param stage
+     */
     @Start
     public void start(Stage stage) {
         Parent sceneRoot = new PersonCard(new PersonDisplay(alice)).getRoot();
