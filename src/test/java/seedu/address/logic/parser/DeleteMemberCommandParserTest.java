b52ad491b80ba83645b523ce7ab6f6bbc556f9c5
ArunBeCoding <53741078+ArunBeCoding@users.noreply.github.com>
2019-11-01 23:32:55 +0800

    Update DeleteMemberCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import seedu.address.logic.commands.exceptions.CommandException;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIds.ID_FIRST_MEMBER;
@@ -14,12 +13,12 @@ public class DeleteMemberCommandParserTest {
     private DeleteMemberCommandParser parser = new DeleteMemberCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteMemberCommand() throws CommandException {
+    public void parse_validArgs_returnsDeleteMemberCommand() {
         assertParseSuccess(parser, "delete-member mi/GS", new DeleteMemberCommand(ID_FIRST_MEMBER));
     }
 
     @Test
-    public void parse_invalidArgs_throwsParseException() throws CommandException {
+    public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "3", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteMemberCommand.MESSAGE_USAGE));
     }
 }
