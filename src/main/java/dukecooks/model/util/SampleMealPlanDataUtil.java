60780632cbc9d5ff7c4266b0862136876dcbb6b2
bakwxh <omega1653@gmail.com>
2019-11-01 08:47:12 +0800

    Modify SampleMealPlanDataUtil

--- src/main/java/dukecooks/model/util/SampleMealPlanDataUtil.java
+++ src/main/java/dukecooks/model/util/SampleMealPlanDataUtil.java
@@ -15,55 +15,20 @@ import dukecooks.model.recipe.components.RecipeName;
  */
 public class SampleMealPlanDataUtil {
     public static MealPlan[] getSampleMealPlans() {
+        List<RecipeName> TEA = Arrays.asList(new RecipeName[]{new RecipeName("Tea")});
+        List<RecipeName> MILO = Arrays.asList(new RecipeName[]{new RecipeName("Milo")});
+        List<RecipeName> OMELETTE = Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")});
+        List<RecipeName> TUNA = Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")});
+        List<RecipeName> MAGGI = Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")});
+        List<RecipeName> FRIED = Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")});
         return new MealPlan[] {
-            new MealPlan(new MealPlanName("Tea Plan"),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tea")})),
-            new MealPlan(new MealPlanName("Milo Plan"),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Milo")})),
-            new MealPlan(new MealPlanName("Cheese Omelette Plan"),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Cheese Omelette")})),
-            new MealPlan(new MealPlanName("Tuna Sandwich Plan"),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Tuna Sandwich")})),
-            new MealPlan(new MealPlanName("Chicken Maggi Plan"),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Chicken Maggi")})),
-            new MealPlan(new MealPlanName("Fried Chicken Plan"),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")}),
-                    Arrays.asList(new RecipeName[]{new RecipeName("Fried Chicken")})),
+            new MealPlan(new MealPlanName("Tea Plan"), TEA, TEA, TEA, TEA, TEA, TEA, TEA),
+            new MealPlan(new MealPlanName("Milo Plan"), MILO, MILO, MILO, MILO, MILO, MILO, MILO),
+            new MealPlan(new MealPlanName("Cheese Omelette Plan"), OMELETTE, OMELETTE, OMELETTE, OMELETTE, OMELETTE,
+                    OMELETTE, OMELETTE),
+            new MealPlan(new MealPlanName("Tuna Sandwich Plan"), TUNA, TUNA, TUNA, TUNA, TUNA, TUNA, TUNA),
+            new MealPlan(new MealPlanName("Chicken Maggi Plan"), MAGGI, MAGGI, MAGGI, MAGGI, MAGGI, MAGGI, MAGGI),
+            new MealPlan(new MealPlanName("Fried Chicken Plan"), FRIED, FRIED, FRIED, FRIED, FRIED, FRIED, FRIED),
         };
     }
 
