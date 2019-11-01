4a850ef1e48a8c25a5cc0256c88560ca7bfa89af
Tan Wei Yang <weiyang.1996@gmail.com>
2019-11-01 21:48:03 +0800

    Add EditTransactionDescriptorBuilder

---
+++ src/test/java/seedu/ichifund/testutil/EditTransactionDescriptorBuilder.java
@@ -0,0 +1,104 @@
+package seedu.ichifund.testutil;
+
+import seedu.ichifund.logic.commands.transaction.EditTransactionCommand.EditTransactionDescriptor;
+import seedu.ichifund.model.date.Day;
+import seedu.ichifund.model.date.Month;
+import seedu.ichifund.model.date.Year;
+import seedu.ichifund.model.transaction.Category;
+import seedu.ichifund.model.transaction.TransactionType;
+import seedu.ichifund.model.Description;
+import seedu.ichifund.model.transaction.Transaction;
+import seedu.ichifund.model.amount.Amount;
+
+/**
+ * A utility class to help with building EditTransactionDescriptor objects.
+ */
+public class EditTransactionDescriptorBuilder {
+
+    private EditTransactionDescriptor descriptor;
+
+    public EditTransactionDescriptorBuilder() {
+        descriptor = new EditTransactionDescriptor();
+    }
+
+    public EditTransactionDescriptorBuilder(EditTransactionDescriptor descriptor) {
+        this.descriptor = new EditTransactionDescriptor(descriptor);
+    }
+
+    /**
+     * Returns an {@code EditTransactionDescriptor} with fields containing {@code transaction}'s details
+     */
+    public EditTransactionDescriptorBuilder(Transaction transaction) {
+        descriptor = new EditTransactionDescriptor();
+        descriptor.setDescription(transaction.getDescription());
+        descriptor.setAmount(transaction.getAmount());
+        descriptor.setDay(transaction.getDay());
+        descriptor.setMonth(transaction.getMonth());
+        descriptor.setYear(transaction.getYear());
+        descriptor.setCategory(transaction.getCategory());
+        descriptor.setTransactionType(transaction.getTransactionType());
+    }
+
+    /**
+     * Sets the {@code Description} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withDescription(String description) {
+        descriptor.setDescription(new Description(description));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Amount} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withAmount(String amount) {
+        descriptor.setAmount(new Amount(amount));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Category} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withCategory(String category) {
+        descriptor.setCategory(new Category(category));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Day} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withDay(String day) {
+        descriptor.setDay(new Day(day));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Month} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withMonth(String month) {
+        descriptor.setMonth(new Month(month));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Year} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withYear(String year) {
+        descriptor.setYear(new Year(year));
+        return this;
+    }
+
+    /**
+     * Sets the {@code TransactionType} of the {@code EditTransactionDescriptor} that we are building.
+     */
+    public EditTransactionDescriptorBuilder withTransactionType(String transactionType) {
+        descriptor.setTransactionType(new TransactionType(transactionType));
+        return this;
+    }
+
+
+
+    public EditTransactionDescriptor build() {
+        return descriptor;
+    }
+}
+
