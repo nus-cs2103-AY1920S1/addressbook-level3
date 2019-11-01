7df80d2c4d87d7b4a17efc54c8a73bcf27f56551
WEN SHUFA <SHUFA17@GMAIL.COM>
2019-11-02 00:39:43 +0800

    fixed dryRun bug: #151 and edits other messages

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,10 +11,10 @@ public class Messages {
     public static final String MESSAGE_INVALID_TIMING_COMPARE_END = "The endTiming should be after startTiming \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The index provided is invalid";
     public static final String MESSAGE_PERSON_NOT_FOUND = "The person '%1$s' is not registered";
-    public static final String MESSAGE_INVALID_REFERENCEID = "the reference id is not belong to any patient";
-    public static final String MESSAGE_INVALID_ROOM = "the room does not exist";
+    public static final String MESSAGE_INVALID_REFERENCEID = "The referenceId is not belong to any patient";
+    public static final String MESSAGE_INVALID_ROOM = "The room does not exist";
     public static final String MESSAGE_INVAILD_REFERENCE_ID =
-            "The reference ID '%1$s' does not belong to any registered person!";
+            "The referenceId '%1$s' does not belong to any registered person!";
     public static final String MESSAGE_INVALID_APPOINTMENT_DATE_TIME = "The appointment dateTime provided is invalid!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_ALL_EVENTS_LISTED_OVERVIEW = "%1$d appointments listed!";
@@ -24,9 +24,9 @@ public class Messages {
     public static final String MESSAGE_INVALID_INDEX = "The appointment index provided is not correct";
     public static final String MESSAGE_NOT_PATIENTLIST = "appointment(s) displayed do not belong to the same patient\n";
     public static final String MESSAGE_NOT_STAFFLIST =
-            "duty shift(s) displayed do not belong to the same staff doctor\n";
-    public static final String MESSAGE_NOT_MISSEDLIST = "current displaying appointment are not "
-            + "belong to missed appointment list\n" + "please type command: missedappt";
-    public static final String MESSAGE_NOTHING_SETTLE = "no missed appointment needs to settle";
+            "Duty shift(s) displayed do not belong to the same staff doctor\n";
+    public static final String MESSAGE_NOT_MISSEDLIST = "Appointments displayed are not missing appointments.\n" +
+            "Please type missappt command first, then use settleappt command again.";
+    public static final String MESSAGE_NOTHING_SETTLE = "No missed appointment needs to settle";
     public static final String MESSAGE_EVENT_NOT_FOUND = "The appointment '%1$s' is not registered";
 }
