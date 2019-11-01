3b136f77abf353a3ad924e6ec6b36b382e50edb6
Leonard Tay <leonardtay748@gmail.com>
2019-11-01 15:12:30 +0800

    More spell updates - FlashCard

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,11 +7,11 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX = "The flashcard index provided is invalid";
+    public static final String MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX = "The FlashCard index provided is invalid";
     public static final String MESSAGE_INVALID_DEADLINE_DISPLAYED_INDEX = "The deadline index provided is invalid";
     public static final String MESSAGE_INVALID_THEME =
             "The theme is not available. Existing themes: dark, light, pink, blue, hacker, nus";
-    public static final String MESSAGE_FLASHCARD_LISTED_OVERVIEW = "%1$d  flash cards listed!";
+    public static final String MESSAGE_FLASHCARD_LISTED_OVERVIEW = "%1$d  FlashCard(s) listed!";
     public static final String MESSAGE_UNKNOWN_TEST_COMMAND = "Unknown test command";
     public static final String MESSAGE_EXPORT_IO_EXCEPTION = "There was an error in writing to the file.\n"
             + "Please ensure your file path doesn't contain any illegal characters.";
