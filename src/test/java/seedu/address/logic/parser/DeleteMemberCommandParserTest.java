1493dcbe4102f47b21dc9d4a119bda69f9823cdc
ArunBeCoding <53741078+ArunBeCoding@users.noreply.github.com>
2019-11-01 23:29:04 +0800

    Update DeleteMemberCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import seedu.address.logic.commands.exceptions.CommandException;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIds.ID_FIRST_MEMBER;
