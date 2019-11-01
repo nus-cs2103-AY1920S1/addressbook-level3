3103789d0104c763011ce312b176c7fa6fe8dd15
JasmineYeo_A0185321L <jas.mint98@gmail.com>
2019-11-01 13:05:48 +0800

    More unused imports

--- src/test/java/seedu/address/logic/parser/person/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/person/EditCommandParserTest.java
@@ -36,7 +36,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.person.EditCommand;
 import seedu.address.logic.commands.person.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.parser.person.EditCommandParser;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
--- src/test/java/seedu/address/logic/parser/person/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/person/FindCommandParserTest.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.person.FindCommand;
-import seedu.address.logic.parser.person.FindCommandParser;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
