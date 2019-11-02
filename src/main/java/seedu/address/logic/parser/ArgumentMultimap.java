73db969168f263cbb9ae1bf91292d7ef9676d2e1
pohlinwei <linwei0909@hotmail.com>
2019-11-02 09:19:29 +0800

    Fix bugs

--- src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Stores mapping of prefixes to their respective arguments.
