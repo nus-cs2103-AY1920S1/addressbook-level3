e3eadb4b5eb679400f5026a2a749b65107c6fc34
Larry Law <larrylaw97@gmail.com>
2019-11-02 23:26:57 +0800

    Add larry's PPP

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -4,10 +4,8 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.logging.Logger;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
