656b2b19557d5fb24423fa930316ab6af5ea3f24
uberSaiyan <yanwl.note@gmail.com>
2019-11-02 14:44:11 +0800

    Fix add-s ct/ parameter autocomplete format

--- src/main/java/seedu/address/logic/nodes/schedule/ScheduleTimeNode.java
+++ src/main/java/seedu/address/logic/nodes/schedule/ScheduleTimeNode.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.nodes.schedule;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -19,7 +20,13 @@ public class ScheduleTimeNode extends Node<Schedule> {
     @Override
     public SortedSet<String> getValues() {
         SortedSet<String> values = new TreeSet<>();
-        backingList.forEach(schedule -> values.add(schedule.getCalendar().getTime().toString()));
+        backingList.forEach(schedule -> {
+            Calendar calendar = schedule.getCalendar();
+            String hour = String.valueOf(calendar.get(Calendar.HOUR));
+            String minute = String.valueOf(calendar.get(Calendar.MINUTE));
+            String display = String.format("%s.%s", hour, minute);
+            values.add(display);
+        });
         return values;
     }
 
