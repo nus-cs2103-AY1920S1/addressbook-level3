3b485dfdf49d88026ac2b3de0885d3f41f0f30df
Liang Boyuan <dedicus545@gmail.com>
2019-11-02 14:15:39 +0800

    ui test addded

--- src/test/java/seedu/flashcard/ui/UiPartTest.java
+++ src/test/java/seedu/flashcard/ui/UiPartTest.java
@@ -1,17 +1,16 @@
-package seedu.address.ui;
+package seedu.flashcard.ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.flashcard.testutil.Assert.assertThrows;
+
+import javafx.fxml.FXML;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import seedu.flashcard.MainApp;
 
 import java.net.URL;
 import java.nio.file.Path;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-
-import javafx.fxml.FXML;
-import seedu.address.MainApp;
 
 public class UiPartTest {
 
