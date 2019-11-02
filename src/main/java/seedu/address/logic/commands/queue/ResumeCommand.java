0bb20e88981e27fa081bdd0195ba4c65dd04ad29
Wong Shun Min <wongsm7@hotmail.com>
2019-11-02 20:04:43 +0800

    Fixes message usage for ResumeCommand

--- src/main/java/seedu/address/logic/commands/queue/ResumeCommand.java
+++ src/main/java/seedu/address/logic/commands/queue/ResumeCommand.java
@@ -17,8 +17,7 @@ public class ResumeCommand extends ReversibleCommand {
     public static final String COMMAND_WORD = "resume";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Doctor is back from his/her break. Allows patients to be directed to the doctor "
-            + "based on the room number.\n"
+            + ": Doctor is back from his/her break. Allows patients to be directed to the doctor.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
