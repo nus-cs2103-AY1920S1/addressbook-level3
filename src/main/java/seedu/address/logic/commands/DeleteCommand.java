8c6dd4201d46406bf48c1e4497a69ef82a02a689
hellodommy <dominique@u.nus.edu>
2019-11-02 16:09:49 +0800

    Correct error message

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.FLAG_EVENT;
+import static seedu.address.logic.parser.CliSyntax.FLAG_PERSON;
+import static seedu.address.logic.parser.CliSyntax.FLAG_TRAINING;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -10,7 +14,9 @@ public abstract class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
-    public static final String MESSAGE_USAGE = "Delete general usage";
+    public static final String MESSAGE_USAGE = "Use the flags "
+        + FLAG_PERSON + " or " + FLAG_EVENT + " or " + FLAG_TRAINING
+        + " to delete a person, event or training respectively.";
 
     @Override
     public abstract CommandResult execute(Model model) throws CommandException;
--- src/main/java/seedu/address/logic/commands/DeletePersonCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePersonCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.FLAG_PERSON;
 
 import java.util.List;
 
@@ -15,12 +16,10 @@ import seedu.address.model.person.Person;
  */
 public class DeletePersonCommand extends DeleteCommand {
 
-    public static final String PERSON_FLAG = "-p";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the person identified by the index number used in the displayed person list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Example: " + COMMAND_WORD + " " + FLAG_PERSON + " 1";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
