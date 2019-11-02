33909f2773b86b79e0d55fc1a3f35596abb17d12
Neo Weihong <whneo97@hotmail.com>
2019-11-02 09:09:15 +0800

    Add support for autochecking how close fontcolour and background dominant colours are. Add support to combine fontcolour and bg commands for simultaneous change.

--- src/main/java/seedu/sugarmummy/logic/parser/aesthetics/FontColourCommandParser.java
+++ src/main/java/seedu/sugarmummy/logic/parser/aesthetics/FontColourCommandParser.java
@@ -1,9 +1,14 @@
 package seedu.sugarmummy.logic.parser.aesthetics;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.sugarmummy.commons.core.Messages.MESSAGE_USE_ONLY_ONE_BACKGROUND_PREFIX;
+import static seedu.sugarmummy.logic.parser.CliSyntax.PREFIX_BACKGROUND;
 import static seedu.sugarmummy.logic.parser.ParserUtil.parseColour;
 
+import seedu.sugarmummy.logic.commands.aesthetics.BackgroundCommand;
 import seedu.sugarmummy.logic.commands.aesthetics.FontColourCommand;
+import seedu.sugarmummy.logic.parser.ArgumentMultimap;
+import seedu.sugarmummy.logic.parser.ArgumentTokenizer;
 import seedu.sugarmummy.logic.parser.Parser;
 import seedu.sugarmummy.logic.parser.exceptions.ParseException;
 import seedu.sugarmummy.model.aesthetics.Colour;
@@ -24,8 +29,24 @@ public class FontColourCommandParser implements Parser<FontColourCommand> {
         if (args.isEmpty()) {
             return new FontColourCommand();
         }
-        Colour fontColour = parseColour(args);
-        return new FontColourCommand(fontColour);
+
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_BACKGROUND);
+
+        BackgroundCommand backgroundCommand = null;
+
+        if (argMultimap.getValue(PREFIX_BACKGROUND).isPresent()) {
+            if (!argMultimap.isUniquePrefix(PREFIX_BACKGROUND)) {
+                throw new ParseException(MESSAGE_USE_ONLY_ONE_BACKGROUND_PREFIX);
+            }
+            backgroundCommand = (new BackgroundCommandParser())
+                    .parse(argMultimap.getValue(PREFIX_BACKGROUND).get());
+        }
+
+        Colour fontColour = parseColour(argMultimap.getPreamble());
+        return backgroundCommand == null
+                ? new FontColourCommand(fontColour)
+                : new FontColourCommand(fontColour, backgroundCommand);
     }
 
 }
