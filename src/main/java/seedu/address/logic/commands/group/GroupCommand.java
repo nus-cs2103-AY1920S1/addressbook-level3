392815bd211e26f9a4de0f826f338f37384deb3d
K Alages <e0309551@u.nus.edu>
2019-11-02 20:05:56 +0800

    checkstyle

--- src/main/java/seedu/address/logic/commands/group/GroupCommand.java
+++ src/main/java/seedu/address/logic/commands/group/GroupCommand.java
@@ -7,6 +7,7 @@ import seedu.address.logic.commands.Command;
  */
 public abstract class GroupCommand extends Command {
 
+    public static final String COMMAND_WORD = "group";
     protected static final String INDEX_OUT_OF_BOUNDS = "Group index input lies outside of the group.";
     protected static final String GROUP_ALREADY_EXISTS = "Group with group ID %1$s already exists";
     protected static final String STUDENT_EXISTS_IN_GROUP = "%1$s already exists in the group";
@@ -14,5 +15,5 @@ public abstract class GroupCommand extends Command {
     protected static final String GROUP_INDEX_OUT_OF_BOUNDS = "Group index number input is out of bounds";
     protected static final String GROUP_DOES_NOT_EXIST = "Group with ID '%1$s' not exist, create group first. ";
     protected static final String GROUP_ID_LEFT_EMPTY = "Group ID parameter cannot be left empty";
-    public static final String COMMAND_WORD = "group";
+
 }
