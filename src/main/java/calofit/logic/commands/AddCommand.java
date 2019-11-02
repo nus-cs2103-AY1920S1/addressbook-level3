947bfc5143ce2f5e8881f32b2e47f239618a959d
fps <universityforfang@gmail.com>
2019-11-02 11:19:14 +0800

    Add command result window message updated

--- src/main/java/calofit/logic/commands/AddCommand.java
+++ src/main/java/calofit/logic/commands/AddCommand.java
@@ -33,7 +33,7 @@ public class AddCommand extends Command {
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "Carbonara "
-            + PREFIX_CALORIES + "low calorie "
+            + PREFIX_CALORIES + "300 "
             + PREFIX_TAG + "salty";
 
     public static final String MESSAGE_SUCCESS = "New meal added to meal log: %1$s";
