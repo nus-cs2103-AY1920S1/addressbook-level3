a92801db40f62697e0c1975c9d3feea5d81dbbe7
ReignOfComputer <roc@reignofcomputer.com>
2019-11-02 17:46:04 +0800

    chore: hard-lock arguments
    Closes https://github.com/AY1920S1-CS2103T-F12-2/main/issues/163.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -6,6 +6,7 @@ package seedu.address.commons.core;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_INVALID_ARGUMENTS = "Invalid arguments after command.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_INVALID_REPORT_INDEX = "The report index provided is invalid";
--- src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAppointmentCommandParser.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.core.Messages.*;
+import static seedu.address.commons.core.Messages.MESSAGE_EMPTY;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_DAYS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DAYS;
 
 import seedu.address.commons.exceptions.IllegalValueException;
