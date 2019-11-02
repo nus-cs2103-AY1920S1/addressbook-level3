8e313c151d1b4b4ec4a3165afb228e131a5cb5be
ryanYtan <tanyuryan@gmail.com>
2019-11-02 18:03:18 +0800

    fix checkstyle errors

--- src/main/java/seedu/jarvis/ui/planner/TaskListView.java
+++ src/main/java/seedu/jarvis/ui/planner/TaskListView.java
@@ -3,7 +3,6 @@ package seedu.jarvis.ui.planner;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import seedu.jarvis.logic.Logic;
 import seedu.jarvis.model.Model;
