24d3046bee26587b5e9243243bd6cc44f4ee76fd
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-11-02 18:34:49 +0800

    Refactor ImportCommand and modify some messages

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -21,14 +21,16 @@ public class ImportCommand extends Command {
 
     public static final String COMMAND_WORD = "import";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports your flashcards from a file.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports your FlashCards from a file.\n"
             + "Parameters: "
             + PREFIX_EXPORT_PATH + "FILE_PATH\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_EXPORT_PATH + "C:\\Users\\damithc\\Documents\\CS2105_Cheat_Sheet.docx";
 
-    public static final String MESSAGE_IMPORT_SUCCESS = "Import was successful! Number of flashcards imported: %d\n";
-    public static final String MESSAGE_IMPORT_DUPLICATES = "%d duplicate FlashCards were not imported.";
+    public static final String MESSAGE_IMPORT_EMPTY_OPTIONAL = "Could not find any FlashCards to import. Are you sure you got the path correct?";
+    public static final String MESSAGE_IMPORT_DATA_CONVERSION_EXCEPTION = "There was an error in reading from the file. Perhaps it was corrupted?";
+    public static final String MESSAGE_IMPORT_SUCCESS = "Import was successful! Number of FlashCards imported: %d\n";
+    public static final String MESSAGE_IMPORT_DUPLICATES = "%d duplicate FlashCard(s) were not imported.";
     public static final String MESSAGE_IMPORT_ALL_DUPLICATES = "There are no new FlashCards to import from that file.";
 
     private final ExportPath exportPath;
@@ -51,7 +53,7 @@ public class ImportCommand extends Command {
                     exportPath.importFrom()
             );
         } catch (DataConversionException e) {
-            throw new CommandException("There was an error in reading from the file. Perhaps it was corrupted?");
+            throw new CommandException(MESSAGE_IMPORT_DATA_CONVERSION_EXCEPTION);
         } catch (UnsupportedOperationException e) {
             throw new CommandException(e.getMessage());
         } catch (CommandException e) {
@@ -68,26 +70,30 @@ public class ImportCommand extends Command {
 
     private CommandResult applyImport(Model model, Optional<List<FlashCard>> optionalList) throws CommandException {
 
-        if (optionalList.isEmpty()) {
-            throw new CommandException(
-                    "Could not find any flashcards to import. Are you sure you got the path correct?"
-            );
-        } else { //TODO can remove else
-            int successCount = 0;
-            int duplicateCount = 0;
-            List<FlashCard> list = optionalList.get();
+        verifyOptionalFlashCardListPresent(optionalList, MESSAGE_IMPORT_EMPTY_OPTIONAL);
 
-            for (FlashCard flashCard : list) {
-                if (model.hasFlashcard(flashCard)) {
-                    duplicateCount++;
-                }
+        int successCount = 0;
+        int duplicateCount = 0;
+        List<FlashCard> list = optionalList.get();
 
+        for (FlashCard flashCard : list) {
+            if (model.hasFlashcard(flashCard)) {
+                duplicateCount++;
+            } else {
                 model.addFlashCard(flashCard);
                 successCount++;
             }
+        }
 
-            return new CommandResult(
-                    formatCommandResultString(successCount, duplicateCount)
+        return new CommandResult(
+                formatCommandResultString(successCount, duplicateCount)
+        );
+    }
+
+    private void verifyOptionalFlashCardListPresent(Optional<List<FlashCard>> optionalList, String message) throws CommandException {
+        if (optionalList.isEmpty()) {
+            throw new CommandException(
+                message
             );
         }
     }
@@ -97,18 +103,18 @@ public class ImportCommand extends Command {
             return MESSAGE_IMPORT_ALL_DUPLICATES;
         }
 
-        if (duplicateCount == 0) {
-            return String.format(
-                    MESSAGE_IMPORT_SUCCESS,
-                    successCount
+        String returnValue = String.format(
+                MESSAGE_IMPORT_SUCCESS,
+                successCount
+        );
+
+        if (duplicateCount != 0) {
+            returnValue += String.format(
+                    MESSAGE_IMPORT_DUPLICATES,
+                    duplicateCount
             );
         }
 
-        return String.format(
-                MESSAGE_IMPORT_SUCCESS,
-                successCount,
-                MESSAGE_IMPORT_DUPLICATES,
-                duplicateCount
-        );
+        return returnValue;
     }
 }
