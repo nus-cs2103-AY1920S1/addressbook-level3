50c639add976f24b5f7063ed73b81e3834789c56
pohlinwei <linwei0909@hotmail.com>
2019-11-02 18:16:25 +0800

    Fix list view UI

---
+++ src/main/java/seedu/address/calendar/parser/ListCommandParser.java
@@ -0,0 +1,49 @@
+package seedu.address.calendar.parser;
+
+import seedu.address.calendar.commands.CommandUtil;
+import seedu.address.calendar.commands.ListCommand;
+import seedu.address.calendar.commands.SuggestCommand;
+import seedu.address.calendar.model.date.Date;
+import seedu.address.calendar.model.event.EventQuery;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ListCommandParser {
+    private static final String MESSAGE_INVALID_COMMAND_FORMAT = "Incorrect list command format. %s";
+    private static final Prefix[] prefixes = { CliSyntax.PREFIX_START_DAY, CliSyntax.PREFIX_START_MONTH,
+            CliSyntax.PREFIX_START_YEAR, CliSyntax.PREFIX_END_DAY, CliSyntax.PREFIX_END_MONTH,
+            CliSyntax.PREFIX_END_YEAR };
+
+    ListCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, prefixes);
+
+        if (ParserUtil.hasMultiplePrefixes(argMultimap, prefixes)) {
+            throw new ParseException(ParserUtil.MESSAGE_ARG_DUPLICATED);
+        }
+
+        boolean hasDatePrefixes = ParserUtil.arePrefixesPresent(argMultimap, CliSyntax.PREFIX_START_DAY);
+
+        if (!hasDatePrefixes) {
+            return new ListCommand();
+        }
+
+        Date startDate = DateParser.parseStartDate(argMultimap, CliSyntax.PREFIX_START_MONTH,
+                CliSyntax.PREFIX_START_YEAR, CliSyntax.PREFIX_START_DAY);
+
+        Date endDate = DateParser.parseEndDate(argMultimap, startDate, CliSyntax.PREFIX_END_MONTH,
+                CliSyntax.PREFIX_END_YEAR, CliSyntax.PREFIX_END_DAY);
+
+        boolean isValidPeriod = EventQuery.isValidEventTime(startDate, endDate);
+
+        if (!isValidPeriod) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    CommandUtil.MESSAGE_DATE_RESTRICTION));
+        }
+
+        EventQuery eventQuery = new EventQuery(startDate, endDate);
+
+        return new ListCommand(eventQuery);
+    }
+}
