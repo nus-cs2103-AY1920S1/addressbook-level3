78b38377b75b9d9605d7ed6cc1dc73572a4e8fcc
AaronLuk <chihengluk@gmail.com>
2019-11-02 17:58:37 +0800

    checkstyle

--- src/main/java/mams/logic/commands/MassApprove.java
+++ src/main/java/mams/logic/commands/MassApprove.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import mams.commons.core.Messages;
 
 import mams.logic.commands.exceptions.CommandException;
