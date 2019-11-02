211e978fc3f706f5cfb563d8f3f0c96fa43c3296
CarbonGrid <nextgentechno@hotmail.com>
2019-11-02 15:40:11 +0800

    Improved CommandBoxHistory data structure. Optimised AutoCompleter from repeated initialization of regex.

--- src/main/java/seedu/address/ui/autocomplete/AutoCompleter.java
+++ src/main/java/seedu/address/ui/autocomplete/AutoCompleter.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.HelpCommand;
@@ -90,6 +91,9 @@ public class AutoCompleter {
             ResumeCommand.COMMAND_WORD
     };
 
+    private static final Pattern HAS_FLAG = Pattern.compile("(.* )?(?<!-)\\w+\\s+$");
+    private static final Pattern CONTINUOUS_SPACES = Pattern.compile("\\s+");
+
     private final Trie trie;
     private String currentQuery;
 
@@ -108,11 +112,11 @@ public class AutoCompleter {
      * @return AutoComplete itself
      */
     public AutoCompleter update(String currentQuery) {
-        if (currentQuery.matches("(.* )?(?<!-)\\w+\\s+$")) {
+        if (HAS_FLAG.matcher(currentQuery).matches()) {
             try {
                 Set<String> result = SUPPORTED_ARGUMENTS.get(currentQuery.substring(0, currentQuery.indexOf(' ')));
                 HashSet<String> available = new HashSet<>(result);
-                available.removeAll(List.of(currentQuery.split("\\s+")));
+                available.removeAll(List.of(CONTINUOUS_SPACES.split(currentQuery)));
                 if (result.contains("-tag")) {
                     available.add("-tag");
                 }
