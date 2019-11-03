ced87b1e8002a626c83a3654714dff052acb61dc
envere <keithtxhh@gmail.com>
2019-11-03 20:16:29 +0800

    Minor refactor

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -12,7 +12,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_THEME =
             "The theme is not available. Existing themes: dark, light, pink, blue, hacker, nus";
     public static final String MESSAGE_FLASHCARD_LISTED_OVERVIEW = "%1$d  flash cards listed!";
-    public static final String MESSAGE_UNKNOWN_TEST_COMMAND = "Unknown test command!\n%1$s";
+    public static final String MESSAGE_UNKNOWN_TEST_COMMAND = "Unknown test command!\n%s";
     public static final String MESSAGE_EXPORT_IO_EXCEPTION = "There was an error in writing to the file.\n"
             + "Please ensure your file path doesn't contain any illegal characters.";
 }
