ecf224fc05f39e33f7a1ca5691803cf6c473c76e
Jonathan Tjendana <jonathan.tjendana@gmail.com>
2019-11-03 11:42:08 +0800

    Removed unused import

--- src/main/java/seedu/moneygowhere/logic/commands/ImportCommand.java
+++ src/main/java/seedu/moneygowhere/logic/commands/ImportCommand.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
