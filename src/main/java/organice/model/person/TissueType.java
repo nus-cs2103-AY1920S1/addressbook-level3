820b9642936c9ed64f188446b0cbc5d4a822314b
SimNee <chua_sim_nee@hotmail.com>
2019-11-03 22:57:36 +0800

    fix issue #106 regarding addCommand tissuetyping

--- src/main/java/organice/model/person/TissueType.java
+++ src/main/java/organice/model/person/TissueType.java
@@ -13,7 +13,7 @@ import java.util.HashSet;
 public class TissueType {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Tissue types are 6 integers from 1 to 12 separated by commas"
+            "Tissue types should only be 6 integers from 1 to 12 separated by commas."
                 + " An example will be 1,2,3,4,5,6";
     public final String value;
 
