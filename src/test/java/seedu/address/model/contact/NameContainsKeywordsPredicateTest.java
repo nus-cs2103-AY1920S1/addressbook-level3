b5de111ee2fe91b2db022c0a14fb304cd6dbfe81
weigenie <ieeweigen@hotmail.com>
2019-11-03 15:58:23 +0800

    Minor phone case fix.

--- src/test/java/seedu/address/model/contact/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/contact/NameContainsKeywordsPredicateTest.java
@@ -69,7 +69,7 @@ public class NameContainsKeywordsPredicateTest {
 
         // Keywords match phone, email and address, but does not match name
         predicate = new NameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
-        assertFalse(predicate.test(new ContactBuilder().withName("Alice").withPhone("12345")
+        assertFalse(predicate.test(new ContactBuilder().withName("Alice").withPhone("91234567")
                 .withEmail("alice@email.com").build()));
     }
 }
