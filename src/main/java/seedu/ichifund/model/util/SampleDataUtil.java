fb540ecf9885e7e8d37b4b557b81599f1a50c4d0
Tan Wei Yang <weiyang.1996@gmail.com>
2019-11-03 15:56:20 +0800

    Fix checkstyle violations

--- src/main/java/seedu/ichifund/model/util/SampleDataUtil.java
+++ src/main/java/seedu/ichifund/model/util/SampleDataUtil.java
@@ -13,11 +13,6 @@ import seedu.ichifund.model.date.Date;
 import seedu.ichifund.model.date.Day;
 import seedu.ichifund.model.date.Month;
 import seedu.ichifund.model.date.Year;
-import seedu.ichifund.model.person.Address;
-import seedu.ichifund.model.person.Email;
-import seedu.ichifund.model.person.Name;
-import seedu.ichifund.model.person.Person;
-import seedu.ichifund.model.person.Phone;
 import seedu.ichifund.model.repeater.MonthOffset;
 import seedu.ichifund.model.repeater.Repeater;
 import seedu.ichifund.model.repeater.RepeaterUniqueId;
@@ -32,54 +27,54 @@ import seedu.ichifund.model.transaction.TransactionType;
 public class SampleDataUtil {
     public static Transaction[] getSampleTransactions() {
         return new Transaction[] {
-                new Transaction(new Description("Lunch at Menya Kokoro"), new Amount("14.00"), new Category("Food"),
-                        new Date(new Day("2"), new Month("11"), new Year("2019")), new TransactionType("exp"),
-                        new RepeaterUniqueId("")),
-                new Transaction(new Description("Joker movie ticket"), new Amount("12.00"),
-                        new Category("Entertainment"), new Date(new Day("2"), new Month("11"), new Year("2019")),
-                        new TransactionType("exp"), new RepeaterUniqueId("")),
-                new Transaction(new Description("Dinner at Marche"), new Amount("21.03"), new Category("Food"),
-                        new Date(new Day("2"), new Month("11"), new Year("2019")), new TransactionType("exp"),
-                        new RepeaterUniqueId("")),
-                new Transaction(new Description("Allowance"), new Amount("1337"), new Category("Allowance"),
-                        new Date(new Day("1"), new Month("11"), new Year("2019")), new TransactionType("in"),
-                        new RepeaterUniqueId("")),
-                new Transaction(new Description("Grab to school"), new Amount("21.03"), new Category("Transportation"),
-                        new Date(new Day("31"), new Month("10"), new Year("2019")), new TransactionType("exp"),
-                        new RepeaterUniqueId("")),
-                new Transaction(new Description("LiHo honey green tea"), new Amount("2.80"), new Category("Food"),
-                        new Date(new Day("31"), new Month("10"), new Year("2019")), new TransactionType("exp"),
-                        new RepeaterUniqueId("")),
-                new Transaction(new Description("Lunch at school"), new Amount("4.80"), new Category("Food"),
-                        new Date(new Day("31"), new Month("10"), new Year("2019")), new TransactionType("exp"),
-                        new RepeaterUniqueId("")),
-                new Transaction(new Description("Grab back home"), new Amount("21.01"), new Category("Transportation"),
-                        new Date(new Day("30"), new Month("10"), new Year("2019")), new TransactionType("exp"),
-                        new RepeaterUniqueId(""))
+            new Transaction(new Description("Lunch at Menya Kokoro"), new Amount("14.00"), new Category("Food"),
+                    new Date(new Day("2"), new Month("11"), new Year("2019")), new TransactionType("exp"),
+                    new RepeaterUniqueId("")),
+            new Transaction(new Description("Joker movie ticket"), new Amount("12.00"),
+                    new Category("Entertainment"), new Date(new Day("2"), new Month("11"), new Year("2019")),
+                    new TransactionType("exp"), new RepeaterUniqueId("")),
+            new Transaction(new Description("Dinner at Marche"), new Amount("21.03"), new Category("Food"),
+                    new Date(new Day("2"), new Month("11"), new Year("2019")), new TransactionType("exp"),
+                    new RepeaterUniqueId("")),
+            new Transaction(new Description("Allowance"), new Amount("1337"), new Category("Allowance"),
+                    new Date(new Day("1"), new Month("11"), new Year("2019")), new TransactionType("in"),
+                    new RepeaterUniqueId("")),
+            new Transaction(new Description("Grab to school"), new Amount("21.03"), new Category("Transportation"),
+                    new Date(new Day("31"), new Month("10"), new Year("2019")), new TransactionType("exp"),
+                    new RepeaterUniqueId("")),
+            new Transaction(new Description("LiHo honey green tea"), new Amount("2.80"), new Category("Food"),
+                    new Date(new Day("31"), new Month("10"), new Year("2019")), new TransactionType("exp"),
+                    new RepeaterUniqueId("")),
+            new Transaction(new Description("Lunch at school"), new Amount("4.80"), new Category("Food"),
+                    new Date(new Day("31"), new Month("10"), new Year("2019")), new TransactionType("exp"),
+                    new RepeaterUniqueId("")),
+            new Transaction(new Description("Grab back home"), new Amount("21.01"), new Category("Transportation"),
+                    new Date(new Day("30"), new Month("10"), new Year("2019")), new TransactionType("exp"),
+                    new RepeaterUniqueId(""))
         };
     }
 
     public static Repeater[] getSampleRepeaters() {
         return new Repeater[] {
-                new Repeater(new RepeaterUniqueId("0"), new Description("Phone bills"), new Amount("65.00"),
-                        new Category("Utilities"), new TransactionType("exp"), new MonthOffset("1"),
-                        new MonthOffset("-1"), new Date(new Day("1"), new Month("10"), new Year("2019")),
-                        new Date(new Day("1"), new Month("12"), new Year("2019"))),
-                new Repeater(new RepeaterUniqueId("1"), new Description("Transport Concession"), new Amount("88.00"),
-                        new Category("Transportation"), new TransactionType("exp"), new MonthOffset("3"),
-                        new MonthOffset("-1"), new Date(new Day("3"), new Month("10"), new Year("2019")),
-                        new Date(new Day("1"), new Month("12"), new Year("2019")))
+            new Repeater(new RepeaterUniqueId("0"), new Description("Phone bills"), new Amount("65.00"),
+                    new Category("Utilities"), new TransactionType("exp"), new MonthOffset("1"),
+                    new MonthOffset("-1"), new Date(new Day("1"), new Month("10"), new Year("2019")),
+                    new Date(new Day("1"), new Month("12"), new Year("2019"))),
+            new Repeater(new RepeaterUniqueId("1"), new Description("Transport Concession"), new Amount("88.00"),
+                    new Category("Transportation"), new TransactionType("exp"), new MonthOffset("3"),
+                    new MonthOffset("-1"), new Date(new Day("3"), new Month("10"), new Year("2019")),
+                    new Date(new Day("1"), new Month("12"), new Year("2019")))
         };
     }
 
     public static Budget[] getSampleBudgets() {
         return new Budget[] {
-                new Budget(new Description("Lose weight gain moolah"), new Amount("300"), new Month("11"),
-                        new Year("2019"), new Category("Food")),
-                new Budget(new Description("Grab less spend less"), new Amount("200"), null, null,
-                        new Category("Transportation")),
-                new Budget(new Description("Play hard spend hard"), new Amount("50"), new Month("11"),
-                        new Year("2019"), new Category("Entertainment"))
+            new Budget(new Description("Lose weight gain moolah"), new Amount("300"), new Month("11"),
+                    new Year("2019"), new Category("Food")),
+            new Budget(new Description("Grab less spend less"), new Amount("200"), null, null,
+                    new Category("Transportation")),
+            new Budget(new Description("Play hard spend hard"), new Amount("50"), new Month("11"),
+                    new Year("2019"), new Category("Entertainment"))
         };
     }
 
@@ -101,6 +96,9 @@ public class SampleDataUtil {
         return sampleFundBook;
     }
 
+    /**
+     * Private method to add transactions to a fund book for a repeater.
+     */
     private static void createRepeaterTransactions(FundBook fundBook, Repeater repeater) {
         int currentMonth = repeater.getStartDate().getMonth().monthNumber;
         int currentYear = repeater.getStartDate().getYear().yearNumber;
