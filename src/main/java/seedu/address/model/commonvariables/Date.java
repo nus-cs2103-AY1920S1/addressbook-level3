1576255a079782831010bcf27fd335833a874b87
weigenie <ieeweigen@hotmail.com>
2019-11-03 15:59:00 +0800

    Removed unused import in Date

--- src/main/java/seedu/address/model/commonvariables/Date.java
+++ src/main/java/seedu/address/model/commonvariables/Date.java
@@ -3,7 +3,6 @@ package seedu.address.model.commonvariables;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
