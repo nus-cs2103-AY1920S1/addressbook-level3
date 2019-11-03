4b647d917881227a3dbfc24d225f4a8739ab7238
Manas Vegi <manasvegi@gmail.com>
2019-11-04 00:13:00 +0800

    Fix a bug in unit conversion in mergebought

--- src/main/java/seedu/ifridge/logic/commands/shoppinglist/MergeShoppingCommand.java
+++ src/main/java/seedu/ifridge/logic/commands/shoppinglist/MergeShoppingCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.ifridge.model.Model.PREDICATE_SHOW_ALL_GROCERY_ITEMS;
 import static seedu.ifridge.model.Model.PREDICATE_SHOW_ALL_SHOPPING_ITEMS;
 import static seedu.ifridge.model.food.Amount.getValue;
+import static seedu.ifridge.model.food.Amount.hasSameAmountUnit;
 
 import java.util.List;
 import java.util.Set;
@@ -55,6 +56,9 @@ public class MergeShoppingCommand extends Command {
             Amount shoppingAmount = shoppingItem.getAmount();
             if (!shoppingItem.getName().equals(boughtItem.getName())) {
                 continue;
+            }
+            if (!hasSameAmountUnit(shoppingAmount, boughtAmount)) {
+                boughtAmount = shoppingAmount.convertAmount(boughtAmount);
             }
             if (getValue(shoppingAmount) > getValue(boughtAmount)) {
                 Amount updatedShoppingAmount = shoppingItem.getAmount().reduceBy(boughtAmount);
