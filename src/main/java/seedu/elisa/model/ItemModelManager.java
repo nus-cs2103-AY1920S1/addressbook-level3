0567953917339d51ed20caf24cdf555d9194c39b
Siang Hwee <siang_hwee@hotmail.com>
2019-11-04 23:55:08 +0800

    Fix calendar to show number of events

--- src/main/java/seedu/elisa/model/ItemModelManager.java
+++ src/main/java/seedu/elisa/model/ItemModelManager.java
@@ -222,6 +222,7 @@ public class ItemModelManager implements ItemModel {
 
         if (wrapper.getEve() != -1) {
             eventList.addToIndex(wrapper.getEve(), wrapper.getItem());
+            calendarList.addToIndex(wrapper.getEve(), wrapper.getItem());
         }
 
         if (wrapper.getRem() != -1) {
@@ -263,6 +264,7 @@ public class ItemModelManager implements ItemModel {
         visualList.remove(item);
         taskList.remove(item);
         eventList.remove(item);
+        calendarList.remove(item);
         reminderList.remove(item);
         futureReminders.remove(item);
         activeReminders.remove(item);
@@ -279,6 +281,7 @@ public class ItemModelManager implements ItemModel {
         itemStorage.remove(item);
         taskList.remove(item);
         eventList.remove(item);
+        calendarList.remove(item);
         reminderList.remove(item);
         futureReminders.remove(item);
         activeReminders.remove(item);
@@ -373,8 +376,10 @@ public class ItemModelManager implements ItemModel {
         if ((index = eventList.indexOf(item)) >= 0) {
             if (newItem.hasEvent()) {
                 eventList.setItem(index, newItem);
+                calendarList.setItem(index, newItem);
             } else {
                 eventList.remove(index);
+                calendarList.remove(index);
             }
         }
 
@@ -439,9 +444,10 @@ public class ItemModelManager implements ItemModel {
      * Clears the 3 lists for re-populating
      * */
     public void emptyLists() {
-        this.taskList.clear();
-        this.eventList.clear();
-        this.reminderList.clear();
+        taskList.clear();
+        eventList.clear();
+        reminderList.clear();
+        calendarList.clear();
     }
 
     /**
