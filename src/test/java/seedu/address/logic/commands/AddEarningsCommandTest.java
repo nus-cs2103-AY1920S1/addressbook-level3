d3ee756be637813df549934c5046928f940bfb87
CaesarPang <47916695+caesarpjz@users.noreply.github.com>
2019-11-04 19:04:07 +0800

    Update AddEarningsCommandTest.java

--- src/test/java/seedu/address/logic/commands/AddEarningsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddEarningsCommandTest.java
@@ -151,6 +151,11 @@ public class AddEarningsCommandTest {
         }
 
         @Override
+        void setReminder(Reminder reminder, Reminder editedReminder) {
+            throw new AssertionError("This method should not be called.");
+        };
+        
+        @Override
         public Path getAddressBookFilePath() {
             throw new AssertionError("This method should not be called.");
         }
