49e83a99e4c3e4d545175cbd8333d124f1f8bdc6
TanZhanMing <e0349703@u.nus.edu>
2019-11-04 15:32:18 +0800

    Create stub for BudgetCommandTest

--- src/test/java/seedu/address/logic/commands/BudgetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BudgetCommandTest.java
@@ -16,63 +16,68 @@ import seedu.address.model.budget.Budget;
 import seedu.address.model.claim.Claim;
 import seedu.address.model.income.Income;
 
-public class BudgetCommandTest {
+public class BudgetCommandTest extends BudgetCommand {
 
     private Model model = new ModelManager(getTypicalFinSec(), new UserPrefs());
     private Model model2 = new ModelManager(getTypicalFinSecWithNoApprovedClaims(), new UserPrefs());
     private Model model3 = new ModelManager(getTypicalFinSecWithApprovedClaims(), new UserPrefs());
-    private List<Claim> claimList = model.getFilteredClaimList();
-    private List<Income> incomeList = model.getFilteredIncomeList();
-    private List<Claim> claimList2 = model2.getFilteredClaimList();
-    private List<Income> incomeList2 = model2.getFilteredIncomeList();
-    private List<Claim> claimList3 = model3.getFilteredClaimList();
-    private List<Income> incomeList3 = model3.getFilteredIncomeList();
 
     @Test
     public void execute_noData_budgetSuccess() {
-        BudgetCommand budgetCommand = new BudgetCommand();
-        budgetCommand.execute(new ModelManager());
+        CommandResult commandResult = new BudgetCommandTest().execute(new ModelManager());
         String noBudgetData = "Projected income: $0.00\n"
                 + "Projected claim value: $0.00\n"
                 + "Projected Budget: $0.00";
-        assertEquals(noBudgetData, budgetCommand.getMessage());
+        assertEquals(noBudgetData, commandResult.getFeedbackToUser());
     }
 
     @Test
     public void execute_noClaims_budgetSuccess() {
-        Budget budget = new Budget(claimList, incomeList);
-        budget.calculateBudget();
-        BudgetCommand budgetCommand = new BudgetCommand();
-        budgetCommand.execute(model);
+        CommandResult commandResult = new BudgetCommandTest().execute(model);
         String noClaimData = "Projected income: $10,100.10\n"
                 + "Projected claim value: $0.00\n"
                 + "Projected Budget: $10,100.10";
-        assertEquals(noClaimData, budgetCommand.getMessage());
+        assertEquals(noClaimData, commandResult.getFeedbackToUser());
     }
 
     @Test
     public void execute_nonApprovedClaims_budgetSuccess() {
-        Budget budget = new Budget(claimList2, incomeList2);
-        budget.calculateBudget();
-        BudgetCommand budgetCommand = new BudgetCommand();
-        budgetCommand.execute(model2);
+        CommandResult commandResult = new BudgetCommandTest().execute(model2);
         String noApprovedClaimData = "Projected income: $10,100.10\n"
                 + "Projected claim value: $0.00\n"
                 + "Projected Budget: $10,100.10";
-        assertEquals(noApprovedClaimData, budgetCommand.getMessage());
+        assertEquals(noApprovedClaimData, commandResult.getFeedbackToUser());
     }
 
     @Test
     public void execute_approvedClaims_budgetSuccess() {
-        Budget budget = new Budget(claimList3, incomeList3);
-        budget.calculateBudget();
-        BudgetCommand budgetCommand = new BudgetCommand();
-        budgetCommand.execute(model3);
+        CommandResult commandResult = new BudgetCommandTest().execute(model3);
         String approvedClaimData = "Projected income: $10,100.10\n"
                 + "Projected claim value: $10,911.29\n"
                 + "Projected Budget: -$811.19\n"
                 + "Warning, you will be over budget!";
-        assertEquals(approvedClaimData, budgetCommand.getMessage());
+        assertEquals(approvedClaimData, commandResult.getFeedbackToUser());
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        List<Claim> claimList = model.getFilteredClaimList();
+        List<Income> incomeList = model.getFilteredIncomeList();
+        Budget budget = new Budget(claimList, incomeList);
+        budget.calculateBudget();
+
+        String message;
+        message = "Projected income: "
+                + budget.getTotalIncome()
+                + "\nProjected claim value: "
+                + budget.getTotalExpenses()
+                + "\nProjected Budget: "
+                + budget.getBudgetAmount();
+
+        if (budget.isOverBudget()) {
+            message += "\nWarning, you will be over budget!";
+        }
+        return new CommandResult(message, false, false, false, false, false);
     }
 
 }
