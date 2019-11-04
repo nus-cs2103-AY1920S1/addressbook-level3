4b2ec0177559f40e61792211d4a5e5a86e96d235
ShiHaoYap <shihaoyap@gmail.com>
2019-11-04 18:10:08 +0800

    Fix code

--- src/main/java/seedu/address/model/event/EventContainsKeyDateRangePredicate.java
+++ src/main/java/seedu/address/model/event/EventContainsKeyDateRangePredicate.java
@@ -1,7 +1,10 @@
 package seedu.address.model.event;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.function.Predicate;
 
 /**
