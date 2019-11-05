b5fb421860122c05eb4063663ad8f5c48df06f0f
Manas Vegi <manasvegi@gmail.com>
2019-11-05 15:47:36 +0800

    Create test for ShoppingItem

---
+++ src/test/java/seedu/ifridge/model/food/ShoppingItemTest.java
@@ -0,0 +1,59 @@
+package seedu.ifridge.model.food;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.ifridge.testutil.TypicalBoughtList.EGG;
+import static seedu.ifridge.testutil.TypicalBoughtList.getTypicalBoughtList;
+import static seedu.ifridge.testutil.TypicalShoppingList.APPLE;
+import static seedu.ifridge.testutil.TypicalShoppingList.BANANA;
+import static seedu.ifridge.testutil.TypicalShoppingList.BOUGHT_EGGS;
+import static seedu.ifridge.testutil.TypicalShoppingList.CAKE;
+import static seedu.ifridge.testutil.TypicalShoppingList.EGGS;
+import static seedu.ifridge.testutil.TypicalShoppingList.URGENT_EGGS;
+
+class ShoppingItemTest {
+
+    @Test
+    void isBought() {
+        assertFalse(BANANA.isBought());
+        System.out.println(BOUGHT_EGGS);
+        assertTrue(BOUGHT_EGGS.isBought());
+    }
+
+    @Test
+    void isUrgent() {
+        assertFalse(APPLE.isUrgent());
+        assertTrue(URGENT_EGGS.isUrgent());
+    }
+
+    @Test
+    void setBought() {
+        assertTrue(APPLE.setBought(true).isBought());
+        assertFalse(BOUGHT_EGGS.setBought(false).isBought());
+    }
+
+    @Test
+    void setUrgent() {
+        assertFalse(URGENT_EGGS.setUrgent(false).isUrgent());
+        assertTrue(CAKE.setUrgent(true).isUrgent());
+    }
+
+    @Test
+    void getBoughtItem() {
+        System.out.println(EGGS.getBoughtItem(new Amount("12units"), new ExpiryDate("27/12/2019")));
+        System.out.println(new GroceryItem(new Name("Eggs"), new Amount("12units"), new ExpiryDate("27/12/2019"),
+                new HashSet<>()));
+        assertTrue(EGG.isSameFood(EGGS.getBoughtItem(new Amount("12units"), new ExpiryDate("03/12/2019"))));
+        assertFalse(new GroceryItem(new Name("Cake"), new Amount("10g"), new ExpiryDate("27/11/2019"),
+                new HashSet<>()).equals(CAKE.getBoughtItem(new Amount("10g"), new ExpiryDate("15/10/2019"))));
+    }
+
+    @Test
+    void isCompletelyBought() {
+        assertFalse(ShoppingItem.isCompletelyBought(APPLE, getTypicalBoughtList().getGroceryList()));
+        assertTrue(ShoppingItem.isCompletelyBought(EGGS, getTypicalBoughtList().getGroceryList()));
+    }
+}
