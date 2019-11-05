5fec66fc65cafa07aa4fa48c015e8b9bc88feda4
thwnus <54080836+thwnus@users.noreply.github.com>
2019-11-06 02:37:47 +0800

    Add script convenience functions for transactions

---
+++ src/main/java/budgetbuddy/logic/script/ScriptObjectWrapper.java
@@ -0,0 +1,142 @@
+package budgetbuddy.logic.script;
+
+import static budgetbuddy.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.lang.reflect.Array;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
+/**
+ * Wraps a Nashorn object into something more convenient to use from Java.
+ */
+@SuppressWarnings({"removal", "unchecked"})
+public class ScriptObjectWrapper {
+    public static final String MESSAGE_WRONG_TYPE =
+            "Optional argument %s is of the wrong type (expected: %s, actual: %s)";
+    public static final String MESSAGE_WRONG_ARRAY_TYPE =
+            "Optional argument %s is of the wrong type (expected: %s[], actual: %s)";
+
+    private final ScriptObjectMirror so;
+
+    public ScriptObjectWrapper(Object[] varargs) {
+        if (varargs != null && varargs.length > 0) {
+            Object obj = varargs[0];
+            if (obj instanceof ScriptObjectMirror) {
+                so = (ScriptObjectMirror) obj;
+                return;
+            }
+        }
+
+        so = null;
+    }
+
+    /**
+     * Gets the object corresponding to the key in the map if it is of the right type.
+     */
+    public <A> A get(String key, Class<A> clazz, boolean shouldThrow) {
+        requireAllNonNull(key, clazz);
+        if (!has(key)) {
+            return null;
+        }
+        Object a = so.get(key);
+        if (clazz.isInstance(a)) {
+            return (A) a;
+        } else if (a != null && shouldThrow) {
+            throw new IllegalArgumentException(String.format(MESSAGE_WRONG_TYPE, key, clazz.getSimpleName(),
+                    a.getClass().getSimpleName()));
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the object corresponding to the key in the map if it is of the right type.
+     */
+    public <A> A get(String key, Class<A> clazz) {
+        return get(key, clazz, true);
+    }
+
+    /**
+     * Gets the array corresponding to the key in the map if it is of the right type.
+     */
+    public <A> A[] getArray(String key, Class<A> clazz, boolean shouldThrow) {
+        requireAllNonNull(key, clazz);
+        if (!has(key)) {
+            return null;
+        }
+        Object a = so.get(key);
+        if (a != null && a.getClass().isArray() && a.getClass().getComponentType().equals(clazz)) {
+            return (A[]) a;
+        } else if (a instanceof ScriptObjectMirror) {
+            ScriptObjectMirror som = (ScriptObjectMirror) a;
+            if (som.isArray()) {
+                int len = (int) som.get("length");
+                A[] ret = (A[]) Array.newInstance(clazz, len);
+
+                for (int i = 0; i < len; ++i) {
+                    Object elem = som.getSlot(i);
+                    if (elem == null) {
+                        ret[i] = null;
+                        continue;
+                    }
+                    if (clazz.isInstance(elem)) {
+                        ret[i] = (A) elem;
+                        continue;
+                    }
+                    if (shouldThrow) {
+                        throw new IllegalArgumentException(
+                                String.format(MESSAGE_WRONG_ARRAY_TYPE, key, clazz.getSimpleName(),
+                                        "mixed array of types including " + elem.getClass().getSimpleName()));
+                    }
+                }
+
+                return ret;
+            } else if (shouldThrow) {
+                throw new IllegalArgumentException(
+                        String.format(MESSAGE_WRONG_ARRAY_TYPE, key, clazz.getSimpleName(),
+                                som.getClassName()));
+            }
+        }
+
+        if (a != null && shouldThrow) {
+            throw new IllegalArgumentException(
+                    String.format(MESSAGE_WRONG_ARRAY_TYPE, key, clazz.getSimpleName(),
+                            a.getClass().getSimpleName()));
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the array corresponding to the key in the map if it is of the right type.
+     */
+    public <A> A[] getArray(String key, Class<A> clazz) {
+        return getArray(key, clazz, true);
+    }
+
+    public Long getIntegral(String key) {
+        if (!has(key)) {
+            return null;
+        }
+
+        Integer retInt = get(key, Integer.class);
+        if (retInt != null) {
+            return retInt.longValue();
+        }
+
+        String retStr = get(key, String.class);
+        if (retStr != null) {
+            try {
+                return Long.parseLong(retStr, 10);
+            } catch (NumberFormatException e) {
+                // silence
+            }
+        }
+
+        return null;
+    }
+
+    public boolean has(String key) {
+        return so != null && so.containsKey(key);
+    }
+}
