5de85fa97dbdbd03c0cb30d4ea6a015bb9f4621e
1nefootstep <sheuxiang@gmail.com>
2019-11-05 11:56:43 +0800

    Finish implementation of AutoCompleteAnalyser

--- src/main/java/seedu/planner/ui/autocomplete/AutoCompleteAnalyser.java
+++ src/main/java/seedu/planner/ui/autocomplete/AutoCompleteAnalyser.java
@@ -6,8 +6,8 @@ import seedu.planner.logic.parser.Prefix;
 import seedu.planner.ui.autocomplete.exceptions.CommandNotFoundException;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 public class AutoCompleteAnalyser {
     private CommandInformation[] allCommandInformation = new CommandInformation[] {
@@ -34,29 +34,68 @@ public class AutoCompleteAnalyser {
             }
             possibilities.addAll(getPossibleRequiredPrefixSingle(commandInformation, prefixesPresent));
             possibilities.addAll(getPossibleRequiredPrefixMultiple(commandInformation));
-            possibilities.addAll(getPossibleOptionalPrefixSingle(commandInformation));
+            possibilities.addAll(getPossibleOptionalPrefixSingle(commandInformation, prefixesPresent));
             possibilities.addAll(getPossibleOptionalPrefixMultiple(commandInformation));
             return possibilities;
         }
     }
 
+    private List<String> getPossibleOptionalPrefixMultiple(CommandInformation commandInformation) {
+        return commandInformation.getOptionalPrefixWithDescriptionMultipleTime();
+    }
+
+    private List<String> getPossibleOptionalPrefixSingle(CommandInformation commandInformation,
+                                                         List<Prefix> prefixesPresent) {
+        List<String> listOfPossiblePrefixWithDescription = commandInformation
+                .getOptionalPrefixWithDescriptionSingleTime();
+        // remove all possible prefix that are already inside prefixesPresent
+        for (Prefix p : prefixesPresent) {
+            listOfPossiblePrefixWithDescription = removeMatchingPrefixFromList(
+                    listOfPossiblePrefixWithDescription, p
+            );
+        }
+        return listOfPossiblePrefixWithDescription;
+    }
+
+    private List<String> getPossibleRequiredPrefixMultiple(CommandInformation commandInformation) {
+        return commandInformation.getRequiredPrefixWithDescriptionMultipleTime();
+    }
+
     private List<String> getPossibleRequiredPrefixSingle(CommandInformation commandInformation,
                                                          List<Prefix> prefixesPresent) {
-        List<String> possibilities = new ArrayList<>();
         List<String> listOfPossiblePrefixWithDescription = commandInformation
                 .getRequiredPrefixWithDescriptionSingleTime();
+        // remove all possible prefix that are already inside prefixesPresent
         for (Prefix p : prefixesPresent) {
-
+            listOfPossiblePrefixWithDescription = removeMatchingPrefixFromList(
+                    listOfPossiblePrefixWithDescription, p
+            );
         }
-
+        return listOfPossiblePrefixWithDescription;
     }
 
-    private Optional<Integer> findMatchingPrefix(List<String> listOfPrefixWithDescription, Prefix p) {
-        for (int i = 0; i < listOfPrefixWithDescription.size(); i++) {
-            
+    private List<String> removeMatchingPrefixFromList(List<String> ls, Prefix prefix) {
+        List<String> copiedList = new ArrayList<>(ls);
+        Iterator<String> copiedListIterator = copiedList.iterator();
+        while (copiedListIterator.hasNext()) {
+            String curr = copiedListIterator.next();
+            if (checkIfPrefixMatchesString(curr, prefix)) {
+                copiedListIterator.remove();
+            }
         }
+        return copiedList;
     }
 
+    private boolean checkIfPrefixMatchesString(String stringToTest, Prefix prefix) {
+        String stringPrefix = prefix.toString();
+        if (stringPrefix.length() >= stringToTest.length()) {
+            return false;
+        } else if (stringToTest.substring(0, stringPrefix.length()).equals(stringPrefix)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     private CommandInformation findMatchingCommandInformation(String command) {
         CommandInformation matchingCommandInformation = null;
