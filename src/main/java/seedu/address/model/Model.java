7fb285e4fd28b05912888b7f6fee5a77e3d3c492
hellodommy <dominique@u.nus.edu>
2019-11-05 18:55:52 +0800

    Create methods to delete Event in Performance

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -119,6 +119,12 @@ public interface Model {
 
     boolean hasEvent(Event event);
 
+    /**
+     * Deletes the given event.
+     * The event must exist in performance.
+     */
+    void deleteEvent(Event target);
+
     ReadOnlyPerformance getPerformance();
 
     void addRecord(String eventName, Person person, Record record);
--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -259,6 +259,11 @@ public class ModelManager implements Model {
     }
 
     @Override
+    public void deleteEvent(Event target) {
+        performance.removeEvent(target);
+    }
+
+    @Override
     public ReadOnlyPerformance getPerformance() {
         return performance;
     }
