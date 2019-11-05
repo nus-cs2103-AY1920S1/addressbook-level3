c5cb107e4150534c9b411dd51c9cf921ca31b264
pohlinwei <linwei0909@hotmail.com>
2019-11-05 22:54:07 +0800

    Fix checkstyle for YearTest

--- src/test/java/seedu/address/calendar/model/date/YearTest.java
+++ src/test/java/seedu/address/calendar/model/date/YearTest.java
@@ -2,7 +2,10 @@ package seedu.address.calendar.model.date;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YearTest {
 
