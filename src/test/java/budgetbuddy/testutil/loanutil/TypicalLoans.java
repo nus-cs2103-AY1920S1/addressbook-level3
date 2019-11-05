0a0d7f70630924709c2823865418e32b04beb47b
Kenneth Fung <fungchenyu@rocketmail.com>
2019-11-05 13:17:44 +0800

    Rename incorrectly named Typical Loan

--- src/test/java/budgetbuddy/testutil/loanutil/TypicalLoans.java
+++ src/test/java/budgetbuddy/testutil/loanutil/TypicalLoans.java
@@ -19,12 +19,12 @@ public class TypicalLoans {
     public static final Loan MARY_IN_UNPAID = new LoanBuilder().withPerson("Mary").withDirection("IN")
             .withAmount(420L).withDescription("For supper.").withDate(LocalDate.ofEpochDay(3)).withStatus("UNPAID")
             .build();
-    public static final Loan ZED_OUT_PAID = new LoanBuilder().withPerson("Zed").withDirection("IN")
+    public static final Loan ZED_IN_PAID = new LoanBuilder().withPerson("Zed").withDirection("IN")
             .withAmount(66666L).withDescription("For the midnight snack.").withDate(LocalDate.ofEpochDay(4))
             .withStatus("PAID").build();
 
     public static final List<Loan> LOAN_LIST =
-            List.of(JOHN_OUT_UNPAID, MARY_IN_UNPAID, PETER_OUT_PAID, ZED_OUT_PAID);
+            List.of(JOHN_OUT_UNPAID, MARY_IN_UNPAID, PETER_OUT_PAID, ZED_IN_PAID);
 
     private TypicalLoans() {} // prevents instantiation
 }
