7e07604cd7ce09cad861d7f3f5ba2bfbd97da6d2
hellodommy <dominique@u.nus.edu>
2019-11-06 02:13:50 +0800

    Test equality for Flag

--- src/test/java/seedu/address/logic/parser/FlagTest.java
+++ src/test/java/seedu/address/logic/parser/FlagTest.java
@@ -2,6 +2,8 @@ package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.parser.CliSyntax.FLAG_EVENT;
+import static seedu.address.logic.parser.CliSyntax.FLAG_PERSON;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -27,5 +29,27 @@ public class FlagTest {
         // valid flag
         assertTrue(Flag.isValidFlag("-p")); // person flag
         assertTrue(Flag.isValidFlag("-e")); // event flag
+    }
+
+    @Test
+    public void equals() {
+        Flag firstFlag = FLAG_PERSON;
+        Flag secondFlag = FLAG_EVENT;
+
+        // same object -> returns true
+        assertTrue(firstFlag.equals(firstFlag));
+
+        // same values -> returns true
+        Flag firstFlagCopy = new Flag("-p");
+        assertTrue(firstFlag.equals(firstFlagCopy));
+
+        // different types -> returns false
+        assertFalse(firstFlag.equals(1));
+
+        // null -> returns false
+        assertFalse(firstFlag.equals(null));
+
+        // different flag -> returns false
+        assertFalse(firstFlag.equals(secondFlag));
     }
 }
