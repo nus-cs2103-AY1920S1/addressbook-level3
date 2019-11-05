fc80e7520f81530158a1ba9556baa84fe0994915
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-11-05 18:54:34 +0800

    Add new test case for DirectoryPath

--- src/test/java/seedu/address/model/export/DirectoryPathTest.java
+++ src/test/java/seedu/address/model/export/DirectoryPathTest.java
@@ -43,7 +43,8 @@ public class DirectoryPathTest {
             "../",
             "..\\",
             "/",
-            "\\"
+            "\\",
+            "GitHub\\main\\.git"
         };
 
         for (String validDirectoryPathString : validDirectoryPathStrings) {
