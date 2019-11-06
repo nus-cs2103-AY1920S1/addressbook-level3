d535c7948fda5760ce90e0a7115dbd9a394c3f43
irene-lly <irenelee278@gmail.com>
2019-11-06 16:17:27 +0800

    Add wraptext for statistics labels in fxml file

--- src/main/java/seedu/address/ui/statistics/StatsPieChart.java
+++ src/main/java/seedu/address/ui/statistics/StatsPieChart.java
@@ -25,9 +25,9 @@ public class StatsPieChart extends UiPart<Region> {
         super(FXML);
         pc.setLegendSide(Side.LEFT);
         pc.setData(data);
-        overview.setText("The total number of questions answered so far: " + totalQns + "\n"
-                + "Number of questions answered correctly: " + (int) data.get(0).getPieValue() + "\n"
-                + "Number of questions answered incorrectly: " + (int) data.get(1).getPieValue() + "\n");
+        overview.setText("The total number of attempts so far: " + totalQns + "\n"
+                + "Number of correct attempts: " + (int) data.get(0).getPieValue() + "\n"
+                + "Number of incorrect attempts: " + (int) data.get(1).getPieValue() + "\n");
     }
 
     /**
