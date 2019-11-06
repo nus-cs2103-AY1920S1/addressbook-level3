aee47725807a44ed089dd6532a7a701c586975b7
marcusteh1238 <marcusteh1238@gmail.com>
2019-11-06 15:19:22 +0800

    Clean up code

--- src/main/java/seedu/address/ics/IcsParser.java
+++ src/main/java/seedu/address/ics/IcsParser.java
@@ -31,11 +31,6 @@ public class IcsParser {
     private static final String FILE_IS_CORRUPTED = "The ICS file is corrupted!";
     private static final String TIMESTAMP_IS_INVALID = "The timestamp provided is invalid!";
 
-    /**
-     * This enum represents the different types of objects the IcsParser could be parsing at any given point in time.
-     */
-    private enum Parsing { TASK, EVENT }
-
     private File icsFile;
 
     private IcsParser(String path) throws IcsException {
