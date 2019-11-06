9703f2bd018c460dd3aeee5dd2c8e7c7ecfcef30
yhtingg <huitingxd@gmail.com>
2019-11-06 11:38:29 +0800

    Add support to delete cards by index

--- src/main/java/seedu/address/logic/commands/DeleteCardCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCardCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.card.Card;
@@ -25,9 +26,16 @@ public class DeleteCardCommand extends Command {
     public static final String MESSAGE_DELETE_CARD_SUCCESS = "Deleted Card: %1$s";
 
     private final Description description;
+    private final Index targetIndex;
 
     public DeleteCardCommand(Description description) {
         this.description = description;
+        this.targetIndex = null;
+    }
+
+    public DeleteCardCommand(Index index) {
+        this.description = null;
+        this.targetIndex = index;
     }
 
     @Override
@@ -35,11 +43,22 @@ public class DeleteCardCommand extends Command {
         requireNonNull(model);
         List<Card> lastShownList = model.getFilteredCardList();
 
-        if (!lastShownList.contains(new Card(description))) {
-            throw new CommandException(Messages.MESSAGE_INVALID_CARD_DISPLAYED);
+        Card cardToDelete;
+
+        assert((targetIndex != null) || (description != null));
+
+        if (targetIndex != null) {
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_CARD_DISPLAYED_INDEX);
+            }
+            cardToDelete = lastShownList.get(targetIndex.getZeroBased());
+        } else {
+            if (!lastShownList.contains(new Card(description))) {
+                throw new CommandException(Messages.MESSAGE_INVALID_CARD_DISPLAYED);
+            }
+            cardToDelete = lastShownList.get(lastShownList.indexOf(new Card(description)));
         }
 
-        Card cardToDelete = lastShownList.get(lastShownList.indexOf(new Card(description)));
         model.deleteCard(cardToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_CARD_SUCCESS, cardToDelete));
     }
@@ -48,6 +67,7 @@ public class DeleteCardCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteCardCommand // instanceof handles nulls
-                && description.equals(((DeleteCardCommand) other).description)); // state check
+                && description.equals(((DeleteCardCommand) other).description)
+                && targetIndex.equals(((DeleteCardCommand) other).targetIndex)); // state check
     }
 }
