6f11d2e6df8885d091642ff98264d37c947e8dd6
michelleykw <michelleykw99@gmail.com>
2019-11-06 18:31:58 +0800

    Updated SearchKey enum

---
+++ src/main/java/seedu/ezwatchlist/logic/parser/SearchKey.java
@@ -0,0 +1,20 @@
+package seedu.ezwatchlist.logic.parser;
+
+public enum SearchKey {
+    KEY_NAME("name"),
+    KEY_TYPE("type"),
+    KEY_ACTOR("actor"),
+    KEY_GENRE("genre"),
+    KEY_IS_WATCHED("is_watched"),
+    KEY_FROM_ONLINE("from_online");
+
+    public final String key;
+
+    SearchKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
