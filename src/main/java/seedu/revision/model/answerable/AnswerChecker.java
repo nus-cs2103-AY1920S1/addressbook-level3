1508d96a291b903992fbc55d962051385db97407
Neo Si Hao <neosihao@gmail.com>
2019-11-06 19:11:50 +0800

    updated saq answer checker to be more accurate

--- src/main/java/seedu/revision/model/answerable/AnswerChecker.java
+++ src/main/java/seedu/revision/model/answerable/AnswerChecker.java
@@ -11,89 +11,125 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import me.xdrop.fuzzywuzzy.FuzzySearch;
 
 /**
  * Used to check saq answers
  */
 public class AnswerChecker {
 
+    /**
+     * Get StanfordCoreNLP pipeline to process String
+     */
     private static StanfordCoreNLP pipeline = Pipeline.getPipeline();
 
     /**
-     * Used to search for keywords using parts of speech tagging
+     * Processes the String to trim trailing and leading whitespaces, covert all characters to lowercase,
+     * Checks for numbers that are in alphabets form and changes it to digits form.
+     * It will return the processed String
+     * @param stringToBeProcessed String to be processed
+     * @return processed String
      */
-    private static List<String> posTags = List.of("CC", "CD", "NN", "NNS", "JJ");
+    private static String processString(String stringToBeProcessed) {
 
-    /**
-     * Analyses the string of answer to determine keywords.
-     * It will return a list of keywords
-     * @param answer string of answer to analyse
-     * @return List of keywords in String
-     */
-    private static List<String> analyseAnswerForKeywords(String answer) {
-        List<String> keywords = new ArrayList<>();
-        CoreDocument coreDocument = new CoreDocument(answer);
+        StringBuilder processedString = new StringBuilder(stringToBeProcessed
+                .replaceAll("\\s*\\p{Punct}+\\s*$", ""));
+
+        CoreDocument coreDocument = new CoreDocument(processedString.toString());
         pipeline.annotate(coreDocument);
-
         List<CoreLabel> coreLabelList = coreDocument.tokens(); // tokenize each word
 
-        Iterator<CoreLabel> iter = coreLabelList.iterator();
-        while (iter.hasNext()) {
-            CoreLabel coreLabel = iter.next();
-            String pos = coreLabel.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-            if (!posTags.parallelStream().anyMatch(pos::contains)) {
-                iter.remove();
-            }
-        }
-
         for (CoreLabel coreLabel : coreLabelList) {
-            coreLabel.setOriginalText(coreLabel.lemma());
+
             String pos = coreLabel.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+
             if (pos.equals("CD")) {
                 Number num = NumberNormalizer.wordToNumber(coreLabel.originalText());
                 coreLabel.setOriginalText(num.toString());
             }
-
-            keywords.add(coreLabel.originalText());
         }
 
-        return keywords;
+        processedString = new StringBuilder();
+
+        for (CoreLabel coreLabel : coreLabelList) {
+            processedString.append(coreLabel.originalText()).append(" ");
+        }
+
+        return processedString.toString().toLowerCase().trim();
     }
 
     /**
-     * Analyses the string of answer to determine sentiment.
-     * It will return sentiment value (Negative, Neutral, Positive) in String format
-     * @param answer string of answer to analyse
-     * @return String that contains sentiment value
+     * Checks if user answer matches the correct answer using fuzzy matching technique.
+     * It will return true if match percentage passes the matchedPercentageThreshold
+     * @param userInput String of user's input to the question
+     * @param correctAnswer String of correct answer
+     * @return true or false
      */
-    private static String analyseAnswerForSentiment(String answer) {
-        CoreDocument coreDocument = new CoreDocument(answer);
-        pipeline.annotate(coreDocument);
+    public static boolean isMatched(String userInput, String correctAnswer) {
 
-        List<CoreSentence> sentences = coreDocument.sentences();
+        /**
+         * Used to determine if user input answer is correct or not
+         */
+        int matchedPercentageThreshold = 80;
 
-        return sentences.get(0).sentiment();
+        int matchedPercentage = FuzzySearch.weightedRatio(userInput, correctAnswer);
+
+        if (matchedPercentage > matchedPercentageThreshold) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Compares sentiment value between two Strings.
+     * It will compare sentiment value (Very negative, Negative, Neutral, Positive, Very positive) between the
+     * two Strings and return true if same sentiment and false otherwise.
+     * @param userInput String of user's input to the question
+     * @param correctAnswer String of correct answer
+     * @return true or false
+     */
+    private static boolean hasSameSentiment(String userInput, String correctAnswer) {
+
+        CoreDocument userInputDocument = new CoreDocument(userInput);
+        pipeline.annotate(userInputDocument);
+
+        CoreDocument correctAnswerDocument = new CoreDocument(correctAnswer);
+        pipeline.annotate(correctAnswerDocument);
+
+        String sentimentValueOfUserInput = userInputDocument.sentences().get(0).sentiment();
+        String sentimentValueOfCorrectAnswer = correctAnswerDocument.sentences().get(0).sentiment();
+
+        if (sentimentValueOfUserInput.equals("Very negative") || sentimentValueOfUserInput.equals("Negative")) {
+            sentimentValueOfUserInput = "Negative";
+        } else {
+            sentimentValueOfUserInput = "Positive";
+        }
+
+        if (sentimentValueOfCorrectAnswer.equals("Very negative") || sentimentValueOfCorrectAnswer.equals("Negative")) {
+            sentimentValueOfCorrectAnswer = "Negative";
+        } else {
+            sentimentValueOfCorrectAnswer = "Positive";
+        }
+
+        return sentimentValueOfUserInput.equals(sentimentValueOfCorrectAnswer);
     }
 
     /**
      * Checks if user answer is correct or not.
      * It will return true if answer is correct and false if answer is wrong
      * @param userInput user's answer to the question
-     * @param correctAnswer the correct answer to the question
+     * @param correctAnswerList the correct answer to the question
      * @return true or false
      */
-    public static boolean check(String userInput, String correctAnswer) {
-        HashSet<String> userInputKeywords = new HashSet<>(analyseAnswerForKeywords(userInput));
-        HashSet<String> correctAnswerKeywords = new HashSet<>(analyseAnswerForKeywords(correctAnswer));
+    public static boolean check(String userInput, ArrayList<Answer> correctAnswerList) {
 
-        if (userInputKeywords.equals(correctAnswerKeywords)) {
-            String userInputSentiment = analyseAnswerForSentiment(userInput);
-            String correctAnswerSentiment = analyseAnswerForSentiment(correctAnswer);
+        userInput = processString(userInput);
 
-            if (!correctAnswerSentiment.equals("Negative")) {
-                if (!userInputSentiment.equals("Negative")) {
-                    return true;
-                }
+        for (Answer answer : correctAnswerList) {
+            String correctAnswer = processString(answer.getAnswer());
+            if (hasSameSentiment(userInput, correctAnswer) == true && isMatched(userInput, correctAnswer) == true) {
+                return true;
             }
         }
 
