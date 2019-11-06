b3787cee918eda5d34c7253b053f85110c915d74
halfwhole <41856541+halfwhole@users.noreply.github.com>
2019-11-06 14:38:28 +0800

    Add getting of modules info to default study plan command test (#299)

--- src/test/java/seedu/address/logic/commands/storage/DefaultStudyPlanCommandTest.java
+++ src/test/java/seedu/address/logic/commands/storage/DefaultStudyPlanCommandTest.java
@@ -3,12 +3,20 @@ package seedu.address.logic.commands.storage;
 import static seedu.address.testutil.TypicalModulesInfo.getTypicalModulesInfo;
 import static seedu.address.testutil.TypicalStudyPlans.getTypicalModulePlanner;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.ModulesInfo;
 import seedu.address.model.UserPrefs;
+import seedu.address.storage.JsonModulesInfoStorage;
+import seedu.address.storage.ModulesInfoStorage;
 
 
 /**
@@ -25,6 +33,8 @@ public class DefaultStudyPlanCommandTest {
 
     @Test
     public void execute_commitStudyPlan_success() {
+        ModulesInfoStorage modulesInfoStorage = new JsonModulesInfoStorage(Paths.get("modules_cs.json"));
+        ModulesInfo modulesInfo = initModulesInfo(modulesInfoStorage);
         /*
         // TODO: add a full modulesinfo
         StudyPlan defaultStudyPlan = DefaultStudyPlanUtil.getDefaultStudyPlan(getTypicalModulesInfo());
@@ -38,6 +48,22 @@ public class DefaultStudyPlanCommandTest {
                 new CommandResult(DefaultStudyPlanCommand.MESSAGE_SUCCESS, true, false);
         assertCommandSuccess(command, model, expectedResult, expectedModel);
          */
+    }
+
+    /**
+     * Initialises modules info from storage.
+     */
+    protected ModulesInfo initModulesInfo(ModulesInfoStorage storage) {
+        ModulesInfo initializedModulesInfo;
+        try {
+            Optional<ModulesInfo> prefsOptional = storage.readModulesInfo();
+            initializedModulesInfo = prefsOptional.orElse(new ModulesInfo());
+        } catch (DataConversionException e) {
+            initializedModulesInfo = new ModulesInfo();
+        } catch (IOException e) {
+            initializedModulesInfo = new ModulesInfo();
+        }
+        return initializedModulesInfo;
     }
 
 }
