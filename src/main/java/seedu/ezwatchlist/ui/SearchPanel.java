961312685f93e61a671a56c28e8a0d89d631852c
tswuxia <tswuxia@gmail.com>
2019-11-06 12:44:37 +0800

    Fixed travis test 2

--- src/main/java/seedu/ezwatchlist/ui/SearchPanel.java
+++ src/main/java/seedu/ezwatchlist/ui/SearchPanel.java
@@ -2,11 +2,8 @@ package seedu.ezwatchlist.ui;
 
 import java.util.logging.Logger;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
