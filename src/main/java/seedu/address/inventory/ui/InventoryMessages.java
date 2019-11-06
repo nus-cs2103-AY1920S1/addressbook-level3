6e58128b6a3fdb244f5898ea0dda95d83a2e1675
MichaelaTSH <michaelatang99@gmail.com>
2019-11-06 16:15:46 +0800

    Fix checkstyle

--- src/main/java/seedu/address/inventory/ui/InventoryMessages.java
+++ src/main/java/seedu/address/inventory/ui/InventoryMessages.java
@@ -62,6 +62,6 @@ public class InventoryMessages {
 
     public static final String MESSAGE_NUMBER_TOO_LARGE = "Sorry, please input a number less than 10,000.";
 
-    public static final String MESSAGE_TOTAL_TOO_LARGE = "Sorry, the total cost/price is too large." +
-            " It must be less than 10,000.";
+    public static final String MESSAGE_TOTAL_TOO_LARGE = "Sorry, the total cost/price is too large."
+            + " It must be less than 10,000.";
 }
