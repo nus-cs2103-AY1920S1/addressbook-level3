246409d2af1c765e2d062f333a9eeb05ae046770
Lee Liak Ghee <liakify@gmail.com>
2019-11-07 00:51:09 +0800

    Update DG for expense and fix Amount

--- src/main/java/seedu/address/model/activity/Amount.java
+++ src/main/java/seedu/address/model/activity/Amount.java
@@ -10,16 +10,27 @@ public class Amount {
     public static final String MESSAGE_CONSTRAINTS =
             "The amount of money spent (after rounding to 2 decimal places) can only be from $0.01 to $1000000";
 
+    public static final double MIN = 0.01;
+    public static final double MAX = 1000000;
+
     public final double value;
 
     /**
      * Constructs an {@code Amount}.
+     * If the given amount is less than 0.01 but rounds to 0.01, it will be stored as 0.01.
+     * Likewise if the given amount is more than 1 million but rounds to 1 million, it will be stored as 1 million.
      *
      * @param amount A valid amount of money spent.
      */
     public Amount(double amount) {
         checkArgument(isValidAmount(amount), MESSAGE_CONSTRAINTS);
-        value = amount;
+        if (amount > MAX) {
+            value = MAX;
+        } else if (amount > 0 && amount < MIN) { //TODO: verify that this does not conflict with settlement when merged
+            value = MIN;
+        } else {
+            value = amount;
+        }
     }
 
     /**
@@ -27,7 +38,7 @@ public class Amount {
      */
     public static boolean isValidAmount(double test) {
         double d = Math.round(test * 100) / 100.0;
-        return d > 0 && d <= 1000000;
+        return d > 0 && d <= MAX;
     }
 
     @Override
