6cd1c543ef5cfbe32ce7ba97be74bf235e890a7c
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-11-07 06:01:01 +0800

    Add all Loans Models

---
+++ src/main/java/seedu/ichifund/model/loan/exceptions/LoanNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.ichifund.model.loan.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified person.
+ */
+public class LoanNotFoundException extends RuntimeException {}
