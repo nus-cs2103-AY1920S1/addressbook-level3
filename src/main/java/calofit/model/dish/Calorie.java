2ef1245d3ed1f56fd3cb719e1bd2847935d25fa0
fps <universityforfang@gmail.com>
2019-11-08 00:28:54 +0800

    Add command error message updated
    Calories only accept integers

--- src/main/java/calofit/model/dish/Calorie.java
+++ src/main/java/calofit/model/dish/Calorie.java
@@ -11,7 +11,7 @@ import calofit.commons.util.AppUtil;
 public class Calorie {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Calories should only be positive values between 0 an 2 billion";
+            "Calories should only be positive integers between 0 and 2 billion";
 
     public static final Calorie UNKNOWN_CALORIE = new Calorie(-1, true);
 
