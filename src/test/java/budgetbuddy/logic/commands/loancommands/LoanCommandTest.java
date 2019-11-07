ecf6017c17c51ac767d3d7456de2848dd145957e
Kenneth Fung <fungchenyu@rocketmail.com>
2019-11-07 20:51:26 +0800

    Test for exception and add test for duplicate loan

--- src/test/java/budgetbuddy/logic/commands/loancommands/LoanCommandTest.java
+++ src/test/java/budgetbuddy/logic/commands/loancommands/LoanCommandTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.Collections;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import budgetbuddy.logic.commands.CommandResult;
@@ -18,7 +19,12 @@ import budgetbuddy.testutil.loanutil.TypicalLoans;
 
 public class LoanCommandTest {
 
-    private Model model = new ModelManager();
+    private Model model;
+
+    @BeforeEach
+    public void initialize() {
+        model = new ModelManager();
+    }
 
     @Test
     public void constructor_nullLoan_throwsNullPointerException() {
@@ -32,6 +38,15 @@ public class LoanCommandTest {
 
         assertEquals(String.format(LoanCommand.MESSAGE_SUCCESS, validLoan), commandResult.getFeedbackToUser());
         assertEquals(Collections.singletonList(validLoan), model.getLoansManager().getLoans());
+    }
+
+    @Test
+    public void execute_duplicateLoan_throwsCommandException() {
+        Loan loan = new LoanBuilder().build();
+        Loan loanCopy = new LoanBuilder().build();
+        model.getLoansManager().addLoan(loan);
+
+        assertThrows(CommandException.class, () -> new LoanCommand(loanCopy).execute(model));
     }
 
     @Test
--- src/test/java/budgetbuddy/logic/parser/loancommandparsers/LoanUnpaidCommandParserTest.java
+++ src/test/java/budgetbuddy/logic/parser/loancommandparsers/LoanUnpaidCommandParserTest.java
@@ -3,6 +3,7 @@ package budgetbuddy.logic.parser.loancommandparsers;
 import static budgetbuddy.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static budgetbuddy.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static budgetbuddy.logic.parser.CommandParserUtil.MESSAGE_INVALID_INDEX;
+import static budgetbuddy.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import budgetbuddy.logic.commands.exceptions.CommandException;
 import budgetbuddy.logic.commands.loancommands.LoanUnpaidCommand;
 import budgetbuddy.logic.parser.commandparsers.loancommandparsers.LoanUnpaidCommandParser;
+import budgetbuddy.logic.parser.exceptions.ParseException;
 import budgetbuddy.testutil.TypicalIndexes;
 
 public class LoanUnpaidCommandParserTest {
@@ -27,5 +29,6 @@ public class LoanUnpaidCommandParserTest {
     @Test
     public void parse_invalidArgs_failure() {
         assertParseFailure(parser, "a", MESSAGE_INVALID_INDEX);
+        assertThrows(ParseException.class, () -> parser.parse("a"));
     }
 }
