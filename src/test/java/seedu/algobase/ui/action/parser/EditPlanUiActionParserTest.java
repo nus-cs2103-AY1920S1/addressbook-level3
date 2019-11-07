8671761f7a5b0512991220bb15dde8145c22a532
Weehan <deantiu56@gmail.com>
2019-11-07 15:36:16 +0800

    Add Test skeletons for UI actions and parsers

---
+++ src/test/java/seedu/algobase/ui/action/parser/EditPlanUiActionParserTest.java
@@ -0,0 +1,13 @@
+package seedu.algobase.ui.action.parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class EditPlanUiActionParserTest {
+    @Test
+    public void parse_nullUiAction_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new EditPlanUiActionParser().parse(null));
+    }
+
+}
