078e0a6a8f799fe6830f4ad8d9771c0b8215535d
zhangxuan97 <zxrox@yahoo.com.sg>
2019-11-07 15:08:50 +0800

    Update equals method in modelmanager

--- src/main/java/seedu/ifridge/model/ModelManager.java
+++ src/main/java/seedu/ifridge/model/ModelManager.java
@@ -740,6 +740,7 @@ public class ModelManager implements Model {
         ModelManager other = (ModelManager) obj;
         return groceryList.equals(other.groceryList)
                 && templateList.equals(other.templateList)
+                && wasteList.equals(other.wasteList)
                 && userPrefs.equals(other.userPrefs)
                 && filteredGroceryItems.equals(other.filteredGroceryItems)
                 && filteredTemplateList.equals(other.filteredTemplateList)
