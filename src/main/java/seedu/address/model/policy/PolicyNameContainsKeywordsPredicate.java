f1a4af632314d621ff51c2fa5f2017ad31e21366
olivercheok20 <olivercheok20@gmail.com>
2019-11-07 09:24:58 +0800

    Fix partial keyword policy name search

--- src/main/java/seedu/address/model/policy/PolicyNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/policy/PolicyNameContainsKeywordsPredicate.java
@@ -18,7 +18,7 @@ public class PolicyNameContainsKeywordsPredicate implements Predicate<Policy> {
     @Override
     public boolean test(Policy policy) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(policy.getName().toString(), keyword));
+                .anyMatch(keyword -> policy.getName().toString().toLowerCase().contains(keyword));
     }
 
     @Override
