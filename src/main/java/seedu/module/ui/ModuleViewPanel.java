f5649a2ca8cf87cbacbd2a2ca1378132b0815170
Shuming <43642522+geshuming@users.noreply.github.com>
2019-11-07 22:01:47 +0800

    Add accordions (#154)
    * Add accordions
    
    * Fix Height issues
    
    * Remove link label

--- src/main/java/seedu/module/ui/ModuleViewPanel.java
+++ src/main/java/seedu/module/ui/ModuleViewPanel.java
@@ -10,6 +10,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import seedu.module.commons.core.LogsCenter;
 import seedu.module.model.module.Module;
@@ -23,6 +24,8 @@ public class ModuleViewPanel extends UiPart<Region> {
     private static final String FXML = "ModuleViewPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ModuleListPanel.class);
 
+    @FXML
+    private VBox moduleViewVBox;
     @FXML
     private Label moduleCode;
     @FXML
@@ -40,7 +43,15 @@ public class ModuleViewPanel extends UiPart<Region> {
     @FXML
     private ListView<SemesterDetail> semesterData;
     @FXML
+    private Accordion linksAccordion;
+    @FXML
+    private TitledPane linksAccordionPane;
+    @FXML
     private FlowPane links;
+    @FXML
+    private Accordion deadlinesAccordion;
+    @FXML
+    private TitledPane deadlinesAccordionPane;
     @FXML
     private Label deadline;
 
@@ -55,10 +66,14 @@ public class ModuleViewPanel extends UiPart<Region> {
             .orElse("There are no preclusions for this module."));
         semesterData.setItems(module.getSemesterDetails().getAsObservableList());
         semesterData.setCellFactory(listView -> new ModuleSemesterDetailCell());
-        // Expands the accordion if the module is not yet tracked
-        moduleDetails.setExpandedPane(moduleDetailsPane);
+
+        // GUi to show to the user if tracked
         if (module instanceof Trackable) {
-            moduleDetails.setExpandedPane(null);
+            // Sets the accordions accordingly
+            linksAccordion.setExpandedPane(linksAccordionPane);
+            deadlinesAccordion.setExpandedPane(deadlinesAccordionPane);
+
+            // Sets the data accordingly
             Trackable trackedModule = ((Trackable) module);
             deadline.setText(trackedModule.getDeadline());
             trackedModule.getLink().stream().map(link -> new LinkButton(link))
@@ -66,7 +81,14 @@ public class ModuleViewPanel extends UiPart<Region> {
             links.setPrefWrapLength(500);
             links.setHgap(10);
             links.setVgap(5);
+
+            return;
         }
+
+        // Gui to show to the user if archived
+        moduleDetails.setExpandedPane(moduleDetailsPane);
+        moduleViewVBox.getChildren().remove(linksAccordion);
+        moduleViewVBox.getChildren().remove(deadlinesAccordion);
     }
 
     /**
