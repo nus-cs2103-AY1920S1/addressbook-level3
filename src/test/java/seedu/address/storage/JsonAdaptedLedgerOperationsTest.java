98e8aac467fb81ec3640c3420c96a343c479f451
joloong <joelloong@gmail.com>
2019-11-07 18:29:38 +0800

    Add more test cases

--- src/test/java/seedu/address/storage/JsonAdaptedLedgerOperationsTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedLedgerOperationsTest.java
@@ -3,15 +3,13 @@ package seedu.address.storage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.transaction.LedgerOperation;
 import seedu.address.testutil.LedgerOperationBuilder;
+
+import java.util.ArrayList;
 
 public class JsonAdaptedLedgerOperationsTest {
     @Test
@@ -19,5 +17,26 @@ public class JsonAdaptedLedgerOperationsTest {
         LedgerOperation ledgerOperation = new LedgerOperationBuilder().asReceiveMoney();
         JsonAdaptedLedgerOperations jsonAdaptedLedgerOperations = new JsonAdaptedLedgerOperations(ledgerOperation);
         assertEquals(ledgerOperation, jsonAdaptedLedgerOperations.toModelType());
+    }
+
+    @Test
+    public void toModelType_nullDescription_throwsIllegalValueException() {
+        JsonAdaptedLedgerOperations nullLedger = new JsonAdaptedLedgerOperations(
+            "19112019", "100", null, new ArrayList<>(), new ArrayList<>());
+        assertThrows(IllegalValueException.class, nullLedger::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullDate_throwsIllegalValueException() {
+        JsonAdaptedLedgerOperations nullLedger = new JsonAdaptedLedgerOperations(
+            null, "100", "world", new ArrayList<>(), new ArrayList<>());
+        assertThrows(IllegalValueException.class, nullLedger::toModelType);
+    }
+
+    @Test
+    public void toModelType_emptyPeople_throwsIllegalValueException() {
+        JsonAdaptedLedgerOperations nullLedger = new JsonAdaptedLedgerOperations(
+            "19112019", "100", "world", new ArrayList<>(), new ArrayList<>());
+        assertThrows(IllegalValueException.class, nullLedger::toModelType);
     }
 }
