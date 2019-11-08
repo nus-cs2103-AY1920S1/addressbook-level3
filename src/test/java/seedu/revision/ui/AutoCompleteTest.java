b14774b60080869a3ea95ae15c2578a8febceef0
ShaunNgTX <ngtzexuan95@gmail.com>
2019-11-08 13:57:08 +0800

    Remove AutoComplete test temporary

--- src/test/java/seedu/revision/ui/AutoCompleteTest.java
+++ src/test/java/seedu/revision/ui/AutoCompleteTest.java
@@ -14,19 +14,19 @@ import seedu.revision.logic.parser.exceptions.ParseException;
 import seedu.revision.model.Model;
 import seedu.revision.model.ModelManager;
 
-
-public class AutoCompleteTest {
-    private Model model = new ModelManager();
-    private Model expectedModel = new ModelManager();
-    private Label testLabel = new Label("test");
-    private CustomMenuItem testItem = new CustomMenuItem(testLabel, true);
-    private ContextMenu popUpTest = new ContextMenu();
-    private LinkedList<String> searchResult = new LinkedList<>();
-    private TextField autocomplete = new AutoComplete();
-
-    @Test
-    public void execute_autoComplete_success() throws ParseException {
-        //assertTrue(autocomplete.equals());
-
-    }
-}
+//
+//public class AutoCompleteTest {
+//    private Model model = new ModelManager();
+//    private Model expectedModel = new ModelManager();
+//    private Label testLabel = new Label("test");
+//    private CustomMenuItem testItem = new CustomMenuItem(testLabel, true);
+//    private ContextMenu popUpTest = new ContextMenu();
+//    private LinkedList<String> searchResult = new LinkedList<>();
+//    private TextField autocomplete = new AutoComplete();
+//
+//    @Test
+//    public void execute_autoComplete_success() throws ParseException {
+//        //assertTrue(autocomplete.equals());
+//
+//    }
+//}
