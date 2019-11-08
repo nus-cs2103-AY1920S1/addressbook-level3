881aa06cda927278d2fbdd67219cacd130bc5e93
uggi121 <sudharshanmadhavan@outlook.com>
2019-11-08 11:35:39 +0800

    Add InputProcessor Javadoc

--- src/main/java/com/typee/logic/interactive/parser/InputProcessor.java
+++ src/main/java/com/typee/logic/interactive/parser/InputProcessor.java
@@ -8,11 +8,20 @@ import java.util.regex.Pattern;
 import com.typee.commons.core.Messages;
 import com.typee.logic.parser.exceptions.ParseException;
 
+/**
+ * Represents a helper class that handles processing of text inputs.
+ */
 public class InputProcessor {
 
     private static final String REGEX_PATTERN_COMMAND_WORD = "[a-zA-Z]+";
     private static final String REGEX_PATTERN_PREFIX = "[a-z]/";
 
+    /**
+     * Extracts and returns the list of prefixes in the user entered input.
+     * Returns the prefixes in their order of appearance.
+     * @param commandText User entered input.
+     * @return array of prefixes in their order of appearance.
+     */
     public Prefix[] extractPrefixes(String commandText) {
         Pattern pattern = Pattern.compile("[a-zA-z]/");
         Matcher matcher = pattern.matcher(commandText);
@@ -22,6 +31,12 @@ public class InputProcessor {
         return prefixes.toArray(Prefix[]::new);
     }
 
+    /**
+     * Returns the list of prefixes matched by the input regular expression {@code Matcher}.
+     *
+     * @param matcher {@code Matcher} containing a text input and a regular expression {@code Prefix} format to match.
+     * @return list of matched prefixes.
+     */
     private List<Prefix> getMatches(Matcher matcher) {
         List<Prefix> prefixes = new ArrayList<>();
         while (matcher.find()) {
@@ -30,6 +45,13 @@ public class InputProcessor {
         return prefixes;
     }
 
+    /**
+     * Returns the command word contained in a text input.
+     *
+     * @param commandText User entered input.
+     * @return command word.
+     * @throws ParseException If there is no unique command word in the text input.
+     */
     public String getCommandWord(String commandText) throws ParseException {
         String trimmedCommandText = commandText.trim();
         List<String> commandWords = getAllCommandWords(trimmedCommandText);
@@ -42,6 +64,12 @@ public class InputProcessor {
         return commandWords.get(0);
     }
 
+    /**
+     * Returns the list of command words present in a {@code String}.
+     *
+     * @param trimmedCommandText Trimmed text input.
+     * @return list of command words.
+     */
     private List<String> getAllCommandWords(String trimmedCommandText) {
         int boundary = findBoundary(trimmedCommandText);
         Pattern pattern = Pattern.compile(REGEX_PATTERN_COMMAND_WORD);
@@ -53,6 +81,13 @@ public class InputProcessor {
         return commandWords;
     }
 
+    /**
+     * Finds and returns the index of the first {@code Prefix} that appears in the input {@code String}.
+     * Returns the length of the {@code String} if no {@code Prefix} is present.
+     *
+     * @param trimmedCommandText Trimmed text input.
+     * @return index of the first {@code Prefix}.
+     */
     private int findBoundary(String trimmedCommandText) {
         Pattern pattern = Pattern.compile(REGEX_PATTERN_PREFIX);
         Matcher matcher = pattern.matcher(trimmedCommandText);
@@ -62,6 +97,5 @@ public class InputProcessor {
         }
         return trimmedCommandText.length();
     }
-
-
+    
 }
