017f850b6bf83a230f2167c3bdac2244101822c3
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2019-11-08 08:40:40 +0800

    Add unit testing for some Storage classes (#168)
    * Add unit testing for some Storage classes
    
    * Fix visibility of attributes

--- src/test/java/seedu/scheduler/storage/JsonAdaptedSlotTest.java
+++ src/test/java/seedu/scheduler/storage/JsonAdaptedSlotTest.java
@@ -2,6 +2,8 @@ package seedu.scheduler.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.scheduler.logic.commands.CommandTestUtil.VALID_SLOT_AMY;
+import static seedu.scheduler.logic.commands.CommandTestUtil.VALID_SLOT_BOB;
+import static seedu.scheduler.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +12,34 @@ import seedu.scheduler.model.person.Slot;
 class JsonAdaptedSlotTest {
 
     @Test
-    public void toModelType_validSlotDetails_success() throws Exception {
-        JsonAdaptedSlot slot = new JsonAdaptedSlot(VALID_SLOT_AMY);
-        assertEquals(Slot.fromString(VALID_SLOT_AMY), slot.toModelType());
+    public void getSlot_validSlotString_success() {
+        JsonAdaptedSlot slotAmy = new JsonAdaptedSlot(VALID_SLOT_AMY);
+        JsonAdaptedSlot slotBob = new JsonAdaptedSlot(VALID_SLOT_BOB);
+        assertEquals(VALID_SLOT_AMY, slotAmy.getSlot());
+        assertEquals(VALID_SLOT_BOB, slotBob.getSlot());
+    }
+
+    @Test
+    public void getSlot_validSlotObject_success() {
+        Slot slotAmy = Slot.fromString(VALID_SLOT_AMY);
+        Slot slotBob = Slot.fromString(VALID_SLOT_BOB);
+        JsonAdaptedSlot jsonSlotAmy = new JsonAdaptedSlot(slotAmy);
+        JsonAdaptedSlot jsonSlotBob = new JsonAdaptedSlot(slotBob);
+        assertEquals(VALID_SLOT_AMY, jsonSlotAmy.getSlot());
+        assertEquals(VALID_SLOT_BOB, jsonSlotBob.getSlot());
+    }
+
+    @Test
+    public void toModelType_validSlot_success() throws Exception {
+        JsonAdaptedSlot slotAmy = new JsonAdaptedSlot(VALID_SLOT_AMY);
+        JsonAdaptedSlot slotBob = new JsonAdaptedSlot(VALID_SLOT_BOB);
+        assertEquals(Slot.fromString(VALID_SLOT_AMY), slotAmy.toModelType());
+        assertEquals(Slot.fromString(VALID_SLOT_BOB), slotBob.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidSlot_throwsIllegalValueException() {
+        JsonAdaptedSlot slot = new JsonAdaptedSlot("    ");
+        assertThrows(IllegalArgumentException.class, Slot.MESSAGE_CONSTRAINTS, slot::toModelType);
     }
 }
