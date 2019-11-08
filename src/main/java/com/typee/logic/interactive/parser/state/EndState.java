63cf21276ceaa5326230653776394f97a7aac1c7
uggi121 <sudharshanmadhavan@outlook.com>
2019-11-08 13:02:44 +0800

    Add State javadoc

--- src/main/java/com/typee/logic/interactive/parser/state/EndState.java
+++ src/main/java/com/typee/logic/interactive/parser/state/EndState.java
@@ -5,15 +5,23 @@ import com.typee.logic.commands.exceptions.CommandException;
 import com.typee.logic.interactive.parser.ArgumentMultimap;
 import com.typee.logic.interactive.parser.Prefix;
 
+/**
+ * Represents the final state of a finite state machine from which a {@code Command} can be built.
+ */
 public abstract class EndState extends State {
 
-    protected final String MESSAGE_END_STATE = "Cannot transition from an end state!";
-    protected final Prefix DUMMY_PREFIX = new Prefix("");
+    protected static final String MESSAGE_END_STATE = "Cannot transition from an end state!";
+    protected static final Prefix DUMMY_PREFIX = new Prefix("");
 
     protected EndState(ArgumentMultimap soFar) {
         super(soFar);
     }
 
+    /**
+     * Returns a command built using the arguments accumulated from the finite state machine.
+     * @return
+     * @throws CommandException
+     */
     public abstract Command buildCommand() throws CommandException;
 
 }
