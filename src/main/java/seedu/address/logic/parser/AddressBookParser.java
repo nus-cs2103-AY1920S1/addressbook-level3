9700800768c4a860bc0ce439bee99ac7a3bbfb46
Lee Liak Ghee <liakify@gmail.com>
2019-11-09 00:09:01 +0800

    bump test coverage

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.ActivityCommand;
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.DisinviteCommand;
@@ -58,9 +57,6 @@ public class AddressBookParser {
 
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
-
-        case ClearCommand.COMMAND_WORD:
-            return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
