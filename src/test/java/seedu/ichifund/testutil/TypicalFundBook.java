80e4e1d98f746e151de018a28a83f34d994ee44a
Tan Wei Yang <weiyang.1996@gmail.com>
2019-11-08 18:13:34 +0800

    Remove unused imports in TypicalFundBook

--- src/test/java/seedu/ichifund/testutil/TypicalFundBook.java
+++ src/test/java/seedu/ichifund/testutil/TypicalFundBook.java
@@ -1,6 +1,5 @@
 package seedu.ichifund.testutil;
 
-import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_AMOUNT_ALLOWANCE;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_AMOUNT_BUS;
@@ -10,13 +9,10 @@ import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_DAY_ALLOWANCE;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_DAY_BUS;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_DESCRIPTION_ALLOWANCE;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_DESCRIPTION_BUS;
-import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_MONTH_ALLOWANCE;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_MONTH_BUS;
-import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.ichifund.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
@@ -73,8 +69,6 @@ public class TypicalFundBook {
             .withTransactionType("exp").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Person PERSON_AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
     public static final Person PERSON_BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
