c168e04bd534e2685a15e02bbd5b25950360becf
Tan Wei Yang <weiyang.1996@gmail.com>
2019-11-08 13:38:04 +0800

    Remove AddCommand and parser

--- src/test/java/seedu/ichifund/testutil/PersonUtil.java
+++ src/test/java/seedu/ichifund/testutil/PersonUtil.java
@@ -6,21 +6,12 @@ import static seedu.ichifund.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.ichifund.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.ichifund.logic.parser.CliSyntax.PREFIX_TAG;
 
-import seedu.ichifund.logic.commands.AddCommand;
 import seedu.ichifund.model.person.Person;
 
 /**
  * A utility class for Person.
  */
 public class PersonUtil {
-
-    /**
-     * Returns an add command string for adding the {@code person}.
-     */
-    public static String getAddCommand(Person person) {
-        return AddCommand.COMMAND_WORD + " " + getPersonDetails(person);
-    }
-
     /**
      * Returns the part of command string for the given {@code person}'s details.
      */
