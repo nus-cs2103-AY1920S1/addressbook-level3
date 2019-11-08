41f5f9c953f13dbbb72fc78fd48e427194ad2941
klementtan <klementtan@gmail.com>
2019-11-09 00:23:04 +0800

    Change to alpha numeric

--- src/test/java/seedu/address/testutil/personutil/TypicalPersonDescriptor.java
+++ src/test/java/seedu/address/testutil/personutil/TypicalPersonDescriptor.java
@@ -15,42 +15,70 @@ public class TypicalPersonDescriptor {
     public static final PersonDescriptor ALICE = new PersonDescriptorBuilder()
             .withName("Alice Pauline").withAddress("123, Jurong West Ave 6, #08-111")
             .withEmail("alice@example.com")
-            .withPhone("94351253").withRemark("Alice is a loser")
-            .withTags("friends").build();
+            .withPhone("94351253")
+            .withRemark("Alice is a loser")
+            .withTags("friends")
+            .build();
 
     public static final PersonDescriptor BENSON = new PersonDescriptorBuilder()
             .withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
-            .withEmail("johnd@example.com").withPhone("98765432")
+            .withEmail("johnd@example.com")
+            .withPhone("98765432")
             .withRemark("Benson is a dumbass")
-            .withTags("owesMoney", "friends").build();
+            .withTags("owesMoney", "friends")
+            .build();
 
     public static final PersonDescriptor CARL = new PersonDescriptorBuilder()
-            .withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").withRemark("WTF CARL?!")
+            .withName("Carl Kurz")
+            .withPhone("95352563")
+            .withEmail("heinz@example.com")
+            .withAddress("wall street")
+            .withRemark("WTF CARL")
             .withTags("LALALA").build();
 
     public static final PersonDescriptor DANIEL = new PersonDescriptorBuilder()
-            .withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").withRemark("DANIEL???")
+            .withName("Daniel Meier")
+            .withPhone("87652533")
+            .withEmail("cornelia@example.com")
+            .withAddress("10th street")
+            .withRemark("LOL")
             .withTags("friends").build();
 
     public static final PersonDescriptor ELLE = new PersonDescriptorBuilder()
-            .withName("Elle Meyer").withPhone("9482224").withRemark("ELEENNNN?").withTags("friends")
-            .withEmail("werner@example.com").withAddress("michegan ave").build();
+            .withName("Elle Meyer").withPhone("9482224")
+            .withRemark("HDL?")
+            .withTags("friends")
+            .withEmail("werner@example.com")
+            .withAddress("michegan ave")
+            .build();
 
     public static final PersonDescriptor FIONA = new PersonDescriptorBuilder()
-            .withName("Fiona Kunz").withPhone("9482427").withRemark("FIONA THE SHREK?").withTags("friends")
-            .withEmail("lydia@example.com").withAddress("little tokyo").build();
+            .withName("Fiona Kunz")
+            .withPhone("9482427")
+            .withRemark("BIAP")
+            .withTags("friends")
+            .withEmail("lydia@example.com")
+            .withAddress("little tokyo")
+            .build();
 
     public static final PersonDescriptor GEORGE = new PersonDescriptorBuilder()
-            .withName("George Best").withPhone("9482442").withRemark("GEORGEEEEEEE").withTags("friends")
-            .withEmail("anna@example.com").withAddress("4th street").build();
+            .withName("George Best")
+            .withPhone("9482442")
+            .withRemark("GEORGEEEEEEE")
+            .withTags("friends")
+            .withEmail("anna@example.com")
+            .withAddress("4th street")
+            .build();
 
     public static final PersonDescriptor ZACK = new PersonDescriptorBuilder()
-            .withName("Zack").withPhone("81231236").withTags("friends")
-            .withEmail("zack@example.com").withAddress("loser street")
-            .withRemark("Im not on the list").withTags("blabla").build();
+            .withName("Zack")
+            .withPhone("81231236")
+            .withTags("friends")
+            .withEmail("zack@example.com")
+            .withAddress("loser street")
+            .withRemark("Im not on the list")
+            .withTags("blabla").build();
 
     public static final PersonDescriptor USER = new PersonDescriptorBuilder()
             .withName("User").withPhone("99999999").withTags("friends")
