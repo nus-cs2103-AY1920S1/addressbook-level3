4167c72bac4b3ccd595f2b09ee537c47c7387940
Kenneth Fung <fungchenyu@rocketmail.com>
2019-11-08 15:16:49 +0800

    Break LoanEditCommand parse() into smaller methods

--- src/main/java/budgetbuddy/logic/parser/commandparsers/loancommandparsers/LoanEditCommandParser.java
+++ src/main/java/budgetbuddy/logic/parser/commandparsers/loancommandparsers/LoanEditCommandParser.java
@@ -31,46 +31,78 @@ public class LoanEditCommandParser implements CommandParser<LoanEditCommand> {
     @Override
     public LoanEditCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultiMap =
+        ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_PERSON, PREFIX_AMOUNT, PREFIX_DESCRIPTION, PREFIX_DATE);
 
-        if (argMultiMap.getValueCount(PREFIX_PERSON) > 1
-                || argMultiMap.getValueCount(PREFIX_AMOUNT) > 1
-                || argMultiMap.getValueCount(PREFIX_DESCRIPTION) > 1
-                || argMultiMap.getValueCount(PREFIX_DATE) > 1) {
+        checkArgumentMultimap(argMultimap);
+
+        Index loanIndex = parseIndex(argMultimap.getPreamble());
+        LoanEditDescriptor loanEditDescriptor = parseOptionalArguments(argMultimap);
+
+        return new LoanEditCommand(loanIndex, loanEditDescriptor);
+    }
+
+    /**
+     * Checks if the given {@code ArgumentMultimap} is valid for parsing into a {@code LoanEditCommand}.
+     * @throws ParseException If the {@code argMultimap} is invalid.
+     */
+    private void checkArgumentMultimap(ArgumentMultimap argMultimap) throws ParseException {
+        if (argMultimap.getValueCount(PREFIX_PERSON) > 1
+                || argMultimap.getValueCount(PREFIX_AMOUNT) > 1
+                || argMultimap.getValueCount(PREFIX_DESCRIPTION) > 1
+                || argMultimap.getValueCount(PREFIX_DATE) > 1) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoanEditCommand.MESSAGE_USAGE));
         }
 
-        if (argMultiMap.getPreamble().isBlank() || argMultiMap.getPreamble().split("\\s+").length != 1) {
+        if (argMultimap.getPreamble().isBlank() || argMultimap.getPreamble().split("\\s+").length != 1) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoanEditCommand.MESSAGE_USAGE));
         }
-        Index loanIndex = CommandParserUtil.parseIndex(argMultiMap.getPreamble());
+    }
 
+    /**
+     * Parses the given string into an {@code Index}.
+     * @return The parsed {@code Index}.
+     * @throws ParseException If the string cannot be parsed into an {@code Index}.
+     */
+    private Index parseIndex(String indexStr) throws ParseException {
+        return CommandParserUtil.parseIndex(indexStr);
+    }
+
+    /**
+     * Converts the optional arguments in the given {@code ArgumentMultimap} into a {@code LoanEditDescriptor}.
+     * @return The {@code LoanEditDescriptor}.
+     * @throws ParseException If an error occurs while parsing any of the optional arguments.
+     */
+    private LoanEditDescriptor parseOptionalArguments(ArgumentMultimap argMultimap) throws ParseException {
         LoanEditDescriptor loanEditDescriptor = new LoanEditDescriptor();
-        if (argMultiMap.getValue(PREFIX_PERSON).isPresent()) {
+
+        if (argMultimap.getValue(PREFIX_PERSON).isPresent()) {
             loanEditDescriptor.setPerson(
-                    new Person(CommandParserUtil.parseName(argMultiMap.getValue(PREFIX_PERSON).get())));
+                    new Person(CommandParserUtil.parseName(argMultimap.getValue(PREFIX_PERSON).get())));
         }
-        if (argMultiMap.getValue(PREFIX_AMOUNT).isPresent()) {
-            Amount editedAmount = CommandParserUtil.parseAmount(argMultiMap.getValue(PREFIX_AMOUNT).get());
+
+        if (argMultimap.getValue(PREFIX_AMOUNT).isPresent()) {
+            Amount editedAmount = CommandParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get());
             if (editedAmount.toLong() == 0) {
                 throw new ParseException(Loan.MESSAGE_AMOUNT_POSITIVE_CONSTRAINT);
             }
             loanEditDescriptor.setAmount(editedAmount);
         }
-        if (argMultiMap.getValue(PREFIX_DESCRIPTION).isPresent()) {
+
+        if (argMultimap.getValue(PREFIX_DESCRIPTION).isPresent()) {
             loanEditDescriptor.setDescription(
-                    CommandParserUtil.parseDescription(argMultiMap.getValue(PREFIX_DESCRIPTION).get()));
+                    CommandParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get()));
         }
-        if (argMultiMap.getValue(PREFIX_DATE).isPresent()) {
+
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
             loanEditDescriptor.setDate(
-                    CommandParserUtil.parseDate(argMultiMap.getValue(PREFIX_DATE).get()));
+                    CommandParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
         }
 
         if (!loanEditDescriptor.isAnyFieldEdited()) {
             throw new ParseException(LoanEditCommand.MESSAGE_UNEDITED);
         }
 
-        return new LoanEditCommand(loanIndex, loanEditDescriptor);
+        return loanEditDescriptor;
     }
 }
