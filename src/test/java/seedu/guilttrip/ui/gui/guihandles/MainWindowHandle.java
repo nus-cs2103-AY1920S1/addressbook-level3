0d83d64f6dd5e4db383580bf65d110ea4bfc3c2e
Jessica Phua Shu Xin <jessicax941@gmail.com>
2019-11-08 13:09:01 +0800

    Add TestFX libray, GUI handle test files

---
+++ src/test/java/seedu/guilttrip/ui/gui/guihandles/MainWindowHandle.java
@@ -0,0 +1,46 @@
+package seedu.guilttrip.ui.gui.guihandles;
+
+import javafx.stage.Stage;
+
+/**
+ * Provides a handle for {@code MainWindow}.
+ */
+public class MainWindowHandle extends StageHandle {
+
+    private final EntryListPanelHandle entryListPanel;
+    private final ResultDisplayHandle resultDisplay;
+    private final CommandBoxHandle commandBox;
+    private final StatusBarFooterHandle statusBarFooter;
+    private final MainMenuHandle mainMenu;
+
+    public MainWindowHandle(Stage stage) {
+        super(stage);
+
+        entryListPanel = new EntryListPanelHandle(getChildNode(EntryListPanelHandle.ENTRY_LIST_VIEW_ID));
+        resultDisplay = new ResultDisplayHandle(getChildNode(ResultDisplayHandle.RESULT_DISPLAY_ID));
+        commandBox = new CommandBoxHandle(getChildNode(CommandBoxHandle.COMMAND_INPUT_FIELD_ID));
+        statusBarFooter = new StatusBarFooterHandle(getChildNode(StatusBarFooterHandle.STATUS_BAR_PLACEHOLDER));
+        mainMenu = new MainMenuHandle(getChildNode(MainMenuHandle.MENU_BAR_ID));
+    }
+
+    public EntryListPanelHandle getEntryListPanel() {
+        return entryListPanel;
+    }
+
+    public ResultDisplayHandle getResultDisplay() {
+        return resultDisplay;
+    }
+
+    public CommandBoxHandle getCommandBox() {
+        return commandBox;
+    }
+
+    public StatusBarFooterHandle getStatusBarFooter() {
+        return statusBarFooter;
+    }
+
+    public MainMenuHandle getMainMenu() {
+        return mainMenu;
+    }
+
+}
---
+++ src/test/java/seedu/guilttrip/ui/gui/guihandles/exceptions/NodeNotFoundException.java
@@ -0,0 +1,9 @@
+package seedu.guilttrip.ui.gui.guihandles.exceptions;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Signals that the node cannot be found.
+ */
+public class NodeNotFoundException extends NoSuchElementException {
+}
