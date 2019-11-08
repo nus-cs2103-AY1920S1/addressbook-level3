14af3ed5ca633e16226e69bc5b482908f5295b6e
uggi121 <sudharshanmadhavan@outlook.com>
2019-11-09 01:45:23 +0800

    Fix checkstyle again

--- src/main/java/com/typee/logic/interactive/parser/InteractiveParserUtil.java
+++ src/main/java/com/typee/logic/interactive/parser/InteractiveParserUtil.java
@@ -9,7 +9,6 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
