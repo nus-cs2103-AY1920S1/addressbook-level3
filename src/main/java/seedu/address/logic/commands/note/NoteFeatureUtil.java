b5be1c8fdac0e2d920b009de9676fffc6cb0187f
Sam-limyr <nacuniel@gmail.com>
2019-11-08 23:09:35 +0800

    Remove unused imports.

--- src/main/java/seedu/address/logic/commands/note/NoteFeatureUtil.java
+++ src/main/java/seedu/address/logic/commands/note/NoteFeatureUtil.java
@@ -7,12 +7,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE_FRAGMENT_START;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE_FRAGMENT_TAG;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
