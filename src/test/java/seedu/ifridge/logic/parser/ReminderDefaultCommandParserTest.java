3ae4b95e383c40015cc0130e2fa133a86dafcd05
evelynejuliet <evelynejuliet@gmail.com>
2019-11-09 17:51:12 +0800

    Add more test to ReminderDefaultCommandParserTest

--- src/test/java/seedu/ifridge/logic/parser/ReminderDefaultCommandParserTest.java
+++ src/test/java/seedu/ifridge/logic/parser/ReminderDefaultCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.ifridge.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.ifridge.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,10 @@ import seedu.ifridge.logic.parser.defaults.ReminderDefaultCommandParser;
 import seedu.ifridge.logic.parser.exceptions.ParseException;
 
 public class ReminderDefaultCommandParserTest {
+    ReminderDefaultCommandParser parser = new ReminderDefaultCommandParser();
+
     @Test
     public void parse_validValueSpecified_success() {
-        ReminderDefaultCommandParser parser = new ReminderDefaultCommandParser();
         // valid number of days
         try {
             Command command = parser.parse(" r/0");
@@ -21,6 +23,17 @@ public class ReminderDefaultCommandParserTest {
             assertEquals(expectedCommand, command);
         } catch (ParseException pe) {
             throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        // invalid number of days
+        try {
+            parser.parse(" r/-1");
+            throw new AssertionError("The expected ParseException was not thrown.");
+        } catch (ParseException pe) {
+            assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderDefaultCommand.MESSAGE_USAGE), pe.getMessage());
         }
     }
 }
