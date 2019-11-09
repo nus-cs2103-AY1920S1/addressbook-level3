4fcc9bd74e0b723a36922ad7c397f3e1d4c7e316
kelvinnharris <kelvinnharris@gmail.com>
2019-11-09 22:56:00 +0800

    Finish JsonAdaptedGroceryItemTest

--- src/test/java/seedu/ifridge/storage/JsonAdaptedGroceryItemTest.java
+++ src/test/java/seedu/ifridge/storage/JsonAdaptedGroceryItemTest.java
@@ -1,13 +1,23 @@
 package seedu.ifridge.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.ifridge.storage.JsonAdaptedGroceryItem.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.ifridge.testutil.Assert.assertThrows;
+import static seedu.ifridge.testutil.TypicalGroceryItems.BANANA;
 import static seedu.ifridge.testutil.TypicalGroceryItems.SPAGHETTI;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Test;
+
+import seedu.ifridge.commons.exceptions.IllegalValueException;
+import seedu.ifridge.model.food.Name;
+
 public class JsonAdaptedGroceryItemTest {
-    private static final String INVALID_NAME = "R@chel";
-    private static final String INVALID_TAG = "#friend";
+    private static final String INVALID_NAME = "M@ngo";
+    private static final String INVALID_TAG = "#fruit";
 
     private static final String VALID_NAME = SPAGHETTI.getName().toString();
     private static final String VALID_AMOUNT = SPAGHETTI.getAmount().toString();
@@ -16,34 +26,34 @@ public class JsonAdaptedGroceryItemTest {
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
 
-    /*@Test
-    public void toModelType_validPersonDetails_returnsPerson() throws Exception {
-        JsonAdaptedFood person = new JsonAdaptedFood(BENSON);
-        assertEquals(BENSON, person.toModelType());
-    }*/
+    @Test
+    public void toModelType_validGroceryItemDetails_returnsGroceryItem() throws Exception {
+        JsonAdaptedGroceryItem groceryItem = new JsonAdaptedGroceryItem(BANANA);
+        assertEquals(BANANA, groceryItem.toModelType());
+    }
 
-    /*@Test
+    @Test
     public void toModelType_invalidName_throwsIllegalValueException() {
-        JsonAdaptedGroceryItem person =
+        JsonAdaptedGroceryItem groceryItem =
                 new JsonAdaptedGroceryItem(INVALID_NAME, VALID_AMOUNT, VALID_EXPIRY_DATE, VALID_TAGS);
         String expectedMessage = Name.MESSAGE_CONSTRAINTS;
-        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-    }*/
+        assertThrows(IllegalValueException.class, expectedMessage, groceryItem::toModelType);
+    }
 
-    /*@Test
+    @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        JsonAdaptedGroceryItem person = new JsonAdaptedGroceryItem(null, VALID_AMOUNT, VALID_EXPIRY_DATE, VALID_TAGS);
+        JsonAdaptedGroceryItem groceryItem = new JsonAdaptedGroceryItem(null, VALID_AMOUNT, VALID_EXPIRY_DATE, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
-        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-    }*/
+        assertThrows(IllegalValueException.class, expectedMessage, groceryItem::toModelType);
+    }
 
-    /*@Test
+    @Test
     public void toModelType_invalidTags_throwsIllegalValueException() {
         List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
         invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
-        JsonAdaptedGroceryItem person =
+        JsonAdaptedGroceryItem groceryItem =
                 new JsonAdaptedGroceryItem(VALID_NAME, VALID_AMOUNT, VALID_EXPIRY_DATE, invalidTags);
-        assertThrows(IllegalValueException.class, person::toModelType);
-    }*/
+        assertThrows(IllegalValueException.class, groceryItem::toModelType);
+    }
 
 }
