ff343757e155b7f4b1ac4c6b14c416d54a2f2185
marcusteh1238 <marcusteh1238@gmail.com>
2019-11-09 15:54:56 +0800

    Add tests for IcsParser class

---
+++ src/test/java/seedu/address/ics/IcsParserTest.java
@@ -0,0 +1,97 @@
+package seedu.address.ics;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.DateTime;
+import seedu.address.model.ModelManager;
+import seedu.address.model.events.EventSource;
+import seedu.address.model.tasks.TaskSource;
+import seedu.address.model.tasks.TaskSourceBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IcsParserTest {
+    @Test
+    public void eventSourceCreationCorrectness_parseSingleEventNormalWithoutEndDateTime_valueAsExpected()
+            throws ParseException, IcsException {
+        String icsEventObjectString = "BEGIN:VEVENT\n"
+                + "UID:2019-11-09T07:32:17.516817800Z@Horo\n"
+                + "DTSTAMP:20191109T073217Z\n"
+                + "DTSTART:20191109T043400Z\n"
+                + "SUMMARY:Test Description\n"
+                + "END:VEVENT";
+        assertDoesNotThrow(() -> DateTime.fromUserString("09/11/2019 12:34"));
+        DateTime startDateTime = DateTime.fromUserString("09/11/2019 12:34");
+        String description = "Test Description";
+        EventSource expected = EventSource
+                .newBuilder(description, startDateTime)
+                .build();
+        EventSource outcome = IcsParser.parseSingleEvent(icsEventObjectString);
+        assertEquals(expected.getDescription(), outcome.getDescription());
+        assertEquals(expected.getStartDateTime(), outcome.getStartDateTime());
+    }
+
+    @Test
+    public void eventSourceCreationCorrectness_parseSingleEventNormalWithEndDateTime_valueAsExpected()
+            throws ParseException, IcsException {
+        String icsEventObjectString = "BEGIN:VEVENT\n"
+                + "UID:2019-11-09T07:32:17.516817800Z@Horo\n"
+                + "DTSTAMP:20191109T073217Z\n"
+                + "DTSTART:20191109T043400Z\n"
+                + "SUMMARY:Test Description\n"
+                + "DTEND:20191109T054500Z\n"
+                + "END:VEVENT";
+        assertDoesNotThrow(() -> DateTime.fromUserString("09/11/2019 12:34"));
+        DateTime startDateTime = DateTime.fromUserString("09/11/2019 12:34");
+        assertDoesNotThrow(() -> DateTime.fromUserString("09/11/2019 13:45"));
+        DateTime endDateTime = DateTime.fromUserString("09/11/2019 13:45");
+        String description = "Test Description";
+        EventSource expected = EventSource
+                .newBuilder(description, startDateTime)
+                .setEnd(endDateTime)
+                .build();
+        EventSource outcome = IcsParser.parseSingleEvent(icsEventObjectString);
+        assertEquals(expected.getDescription(), outcome.getDescription());
+        assertEquals(expected.getStartDateTime(), outcome.getStartDateTime());
+        assertEquals(expected.getEndDateTime(), outcome.getEndDateTime());
+    }
+
+    @Test
+    public void taskSourceCreationCorrectness_parseSingleTaskNormalWithoutDueDateTime_valueAsExpected()
+            throws IcsException {
+        String icsTaskObjectString = "BEGIN:VTODO\n"
+                + "UID:2019-11-09T07:32:17.517840600Z@Horo\n"
+                + "DTSTAMP:20191109T073217Z\n"
+                + "SUMMARY:hello\n"
+                + "END:VTODO";
+        String description = "hello";
+        TaskSource expected = TaskSource.newBuilder(description).build();
+        TaskSource outcome = IcsParser.parseSingleTask(icsTaskObjectString);
+        assertEquals(expected.getDescription(), outcome.getDescription());
+    }
+
+    @Test
+    public void taskSourceCreationCorrectness_parseSingleTaskNormalWithDueDateTime_valueAsExpected()
+            throws ParseException, IcsException {
+        String icsTaskObjectString = "BEGIN:VTODO\n"
+                + "UID:2019-11-09T07:32:17.517840600Z@Horo\n"
+                + "DTSTAMP:20191109T073217Z\n"
+                + "SUMMARY:hello\n"
+                + "DUE:20191109T043400Z\n"
+                + "END:VTODO";
+        String description = "hello";
+        assertDoesNotThrow(() -> DateTime.fromUserString("09/11/2019 12:34"));
+        DateTime dueDate = DateTime.fromUserString("09/11/2019 12:34");
+        TaskSourceBuilder builder = TaskSource.newBuilder(description);
+        builder.setDueDate(dueDate);
+        TaskSource expected = builder.build();
+        TaskSource outcome = IcsParser.parseSingleTask(icsTaskObjectString);
+        assertEquals(expected.getDescription(), outcome.getDescription());
+        assertEquals(expected.getDueDate(), outcome.getDueDate());
+    }
+}
