d617790e47dcd1a4650175dbb77a044e1f0feec7
nattanyz <natalietan8@gmail.com>
2019-11-09 21:13:45 +0800

    Remove most methods from Stats and move to UserStats, leaving only abstract startCurrentSession(), endCurrentSession() and getCurrentSession().

--- src/main/java/dream/fcard/logic/stats/DeckStats.java
+++ src/main/java/dream/fcard/logic/stats/DeckStats.java
@@ -1,10 +1,36 @@
 package dream.fcard.logic.stats;
 
+import java.util.HashMap;
+
 public class DeckStats extends Stats {
+
+    /** Data structure mapping decks to their corresponding SessionLists. */
+    private HashMap<String, SessionList> deckHashMap = new HashMap<>();
+
+    /** The current Session the user is engaging in. */
+    private Session currentSession;
 
     /** Ends the current session, and sets its score. */
     public void endCurrentSession(String score) {
         this.currentSession.setScore(score);
         endCurrentSession();
     }
+
+    @Override
+    public void startCurrentSession() {
+
+    }
+
+    @Override
+    public void endCurrentSession() {
+
+    }
+
+    @Override
+    public Session getCurrentSession() {
+        return null;
+    }
+
+    // methods relating to changes to decks
+
 }
--- src/main/java/dream/fcard/logic/stats/UserStats.java
+++ src/main/java/dream/fcard/logic/stats/UserStats.java
@@ -14,7 +14,54 @@ public class UserStats extends Stats {
 
     /** Constructs a new instance of UserStats with no stored data. */
     public UserStats() {
-        super();
+        this.sessionList = new SessionList();
+        this.currentSession = null;
         System.out.println("New UserStats object created");
+    }
+
+    /** Sets the sessionList of the current Stats object to the given newSessionList. */
+    public void setSessionList(SessionList newSessionList) {
+        this.sessionList = newSessionList;
+    }
+
+    /** Gets the list of sessions. */
+    public SessionList getSessionList() {
+        return this.sessionList;
+    }
+
+    @Override
+    public void startCurrentSession() {
+        if (this.currentSession != null) {
+            endCurrentSession(); // should not occur, but should terminate just in case
+            System.out.println("Existing current session detected. Terminating it first...");
+        }
+
+        // debug (change to Logger when implemented)
+        System.out.println("Starting a session...");
+
+        this.currentSession = new Session(); // currentSession should be null
+    }
+
+    @Override
+    public void endCurrentSession() {
+        // assert current session is not null?
+        try {
+            this.currentSession.endSession();
+            this.sessionList.addSession(currentSession);
+
+            // reset currentSession to null since this is terminated
+            this.currentSession = null;
+
+            // debug (change to Logger when implemented)
+            System.out.println("Ending the current session...");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Current session not found?");
+        }
+    }
+
+    @Override
+    public Session getCurrentSession() {
+        return this.currentSession;
     }
 }
