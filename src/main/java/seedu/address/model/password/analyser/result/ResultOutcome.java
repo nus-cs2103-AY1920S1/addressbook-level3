101ec4ecfde0a8cac0b736531b9150c0c030b269
Lee Ee Jian <e0309900@u.nus.edu>
2019-11-09 12:10:00 +0800

    added ResultOutcome class

---
+++ src/main/java/seedu/address/model/password/analyser/result/ResultOutcome.java
@@ -0,0 +1,11 @@
+package seedu.address.model.password.analyser.result;
+
+/**
+ * Represents the status of a Result
+ */
+public enum ResultStatus {
+        ACTIVE,
+        MISSING,
+        CORRUPTED
+    }
+}
