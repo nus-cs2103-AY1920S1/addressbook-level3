2bbd4b45d7499be9135eb2120ccea0846c66067c
SebastianLie <jackcass629@gmail.com>
2019-11-10 02:04:42 +0800

    Uncommented some tests

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -12,7 +12,6 @@ public class ExitCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
-
     @Test
     public void execute_exit_success() {
         CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
