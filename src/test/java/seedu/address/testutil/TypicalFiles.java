8e24213c3e2367c97a1011c02b7459d367ce9a1b
SebastianLie <jackcass629@gmail.com>
2019-11-10 01:42:48 +0800

    Add more tests

---
+++ src/test/java/seedu/address/testutil/TypicalFiles.java
@@ -0,0 +1,6 @@
+package seedu.address.testutil;
+
+public class TypicalFiles {
+    public static final String FILE_EXAMPLE_1 = "path/file.png";
+    public static final String FILE_EXAMPLE_2 = "path/file.jpg";
+}
