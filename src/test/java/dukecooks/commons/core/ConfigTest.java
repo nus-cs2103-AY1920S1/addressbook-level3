a417d658db8b296cb67b1989101613e91509394e
Junwei <junwei.97@hotmail.com>
2019-11-09 20:43:08 +0800

    Fix checkstyle errors

--- src/test/java/dukecooks/commons/core/ConfigTest.java
+++ src/test/java/dukecooks/commons/core/ConfigTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import java.nio.file.Paths;
 import java.util.logging.Level;
+
+import org.junit.jupiter.api.Test;
 
 public class ConfigTest {
 
