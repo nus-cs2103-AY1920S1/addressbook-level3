9a3cab6dd6a8793fc115898e22ae52797fef9d63
pohlinwei <linwei0909@hotmail.com>
2019-11-09 19:47:51 +0800

    Minor bug fixes

--- src/main/java/seedu/address/ui/PageType.java
+++ src/main/java/seedu/address/ui/PageType.java
@@ -4,7 +4,7 @@ package seedu.address.ui;
  * Enumerates possible page type for travezy application, for purpose of page navigation.
  */
 public enum PageType {
-    ADDRESS_BOOK, CALENDAR, DIARY, FINANCIAL_TRACKER, ITINERARY, MAIN, ACHIEVEMENTS, SAMPLE; // todo remove SAMPLE
+    ADDRESS_BOOK, CALENDAR, DIARY, FINANCIAL_TRACKER, ITINERARY, MAIN, ACHIEVEMENTS;
 
     public static final String MESSAGE_CONSTRAINTS = "Only the following arguments are allowed: "
             + "address_book, calendar, diary,financial_tracker, itinerary, main";
