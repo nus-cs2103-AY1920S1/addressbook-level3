268c0fd3d727f7929999e65d0da789c93419740d
JohnNzj <johnng1@hotmail.com>
2019-11-09 18:36:13 +0800

    Fix travis unused imports

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -22,7 +22,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ActivityBook;
 import seedu.address.model.Context;
-import seedu.address.model.ContextType;
 import seedu.address.model.InternalState;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
