9b36460b2c5ddcff8f360203a261969353c1b2c1
Tie Ying Kathleen <tieyingkathleen@gmail.com>
2019-11-10 01:49:11 +0800

    Edit AddTemplteItemCommandTest

--- src/test/java/seedu/ifridge/logic/commands/templatelist/TemplateCommandTestUtil.java
+++ src/test/java/seedu/ifridge/logic/commands/templatelist/TemplateCommandTestUtil.java
@@ -1,29 +1,36 @@
 package seedu.ifridge.logic.commands.templatelist;
 
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static seedu.ifridge.logic.parser.CliSyntax.PREFIX_AMOUNT;
 import static seedu.ifridge.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.ifridge.model.Model.PREDICATE_SHOW_ALL_TEMPLATES;
+import static seedu.ifridge.model.Model.PREDICATE_SHOW_ALL_TEMPLATES_ITEMS;
 import static seedu.ifridge.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.ifridge.commons.core.index.Index;
 import seedu.ifridge.logic.commands.Command;
 import seedu.ifridge.logic.commands.CommandResult;
+import seedu.ifridge.logic.commands.CommandTestUtil;
 import seedu.ifridge.logic.commands.EditCommand;
 import seedu.ifridge.logic.commands.exceptions.CommandException;
 import seedu.ifridge.logic.commands.templatelist.template.EditTemplateItemCommand;
 import seedu.ifridge.model.GroceryList;
 import seedu.ifridge.model.Model;
+import seedu.ifridge.model.TemplateList;
 import seedu.ifridge.model.food.Food;
 import seedu.ifridge.model.food.TemplateItem;
 import seedu.ifridge.model.food.UniqueTemplateItems;
 import seedu.ifridge.testutil.EditFoodDescriptorBuilder;
 import seedu.ifridge.testutil.EditTemplateItemDescriptorBuilder;
+import seedu.ifridge.testutil.TemplateItemBuilder;
 
 /**
  * Contains helper methods for testing commands.
@@ -34,17 +41,16 @@ public class TemplateCommandTestUtil {
     public static final String VALID_NAME_ORANGES = "Oranges";
     public static final String VALID_NAME_CHEESE = "Cheddar Cheese";
     public static final String VALID_NAME_TOMATO_JUICE = "Tomato Juice";
+    public static final String VALID_NAME_RICE = "Rice";
     public static final String VALID_AMOUNT_NUTS = "120g";
     public static final String VALID_AMOUNT_ORANGES = "15units";
     public static final String VALID_AMOUNT_CHEESE = "300g";
     public static final String VALID_AMOUNT_TOMATO_JUICE = "300ml";
+    public static final String VALID_AMOUNT_RICE = "2kg";
     public static final String VALID_EXPIRY_DATE_NUTS = "10/12/2019";
-    public static final String VALID_TAG_CARBS = "carbs";
-    public static final String VALID_TAG_VEGETABLE = "vegetable";
     public static final String INVALID_AMOUNT_FOR_VOLUME = "300g";
     public static final String INVALID_AMOUNT_FOR_QUANTITY = "300ml";
     public static final String INVALID_AMOUNT_FOR_UNIT = "300g";
-
 
     public static final String NAME_DESC_CHEESE = " " + PREFIX_NAME + VALID_NAME_CHEESE;
     public static final String NAME_DESC_TOMATO_JUICE = " " + PREFIX_NAME + VALID_NAME_TOMATO_JUICE;
@@ -56,46 +62,19 @@ public class TemplateCommandTestUtil {
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
 
-    public static final EditCommand.EditFoodDescriptor DESC_NUTS;
-    public static final EditCommand.EditFoodDescriptor DESC_ORANGES;
     public static final EditTemplateItemCommand.EditTemplateItemDescriptor DESC_TEMP_MINCED_MEAT;
     public static final EditTemplateItemCommand.EditTemplateItemDescriptor DESC_TEMP_TOMATO_JUICE;
+    public static final TemplateItem TEMPLATE_ITEM_CHEESE;
+    public static final TemplateItem TEMPLATE_ITEM_RICE;
 
     static {
-        DESC_NUTS = new EditFoodDescriptorBuilder().withName(VALID_NAME_NUTS).withAmount(VALID_AMOUNT_NUTS)
-                .withExpiryDate(VALID_EXPIRY_DATE_NUTS).withTags(VALID_TAG_VEGETABLE).build();
-        DESC_ORANGES = new EditFoodDescriptorBuilder().withName(VALID_NAME_ORANGES).withAmount(VALID_AMOUNT_ORANGES)
-                .withExpiryDate(VALID_EXPIRY_DATE_NUTS).withTags(VALID_TAG_CARBS, VALID_TAG_VEGETABLE).build();
         DESC_TEMP_MINCED_MEAT = new EditTemplateItemDescriptorBuilder().withName(VALID_NAME_CHEESE)
                 .withAmount(VALID_AMOUNT_CHEESE).build();
         DESC_TEMP_TOMATO_JUICE = new EditTemplateItemDescriptorBuilder().withName(VALID_NAME_TOMATO_JUICE)
                 .withAmount(VALID_AMOUNT_TOMATO_JUICE).build();
-    }
-
-    /**
-     * Executes the given {@code command}, confirms that <br>
-     * - the returned {@link CommandResult} matches {@code expectedCommandResult} <br>
-     * - the {@code actualModel} matches {@code expectedModel}
-     */
-    public static void assertCommandSuccess(Command command, Model actualModel, CommandResult expectedCommandResult,
-                                            Model expectedModel) {
-        try {
-            CommandResult result = command.execute(actualModel);
-            assertEquals(expectedCommandResult, result);
-            assertEquals(expectedModel, actualModel);
-        } catch (CommandException ce) {
-            throw new AssertionError("Execution of command should not fail.", ce);
-        }
-    }
-
-    /**
-     * Convenience wrapper to {@link #assertCommandSuccess(Command, Model, CommandResult, Model)}
-     * that takes a string {@code expectedMessage}.
-     */
-    public static void assertCommandSuccess(Command command, Model actualModel, String expectedMessage,
-            Model expectedModel) {
-        CommandResult expectedCommandResult = new CommandResult(expectedMessage);
-        assertCommandSuccess(command, actualModel, expectedCommandResult, expectedModel);
+        TEMPLATE_ITEM_CHEESE = new TemplateItemBuilder().withName(VALID_NAME_CHEESE)
+                .withAmount(VALID_AMOUNT_CHEESE).build();
+        TEMPLATE_ITEM_RICE = new TemplateItemBuilder().withName(VALID_NAME_RICE).withAmount(VALID_AMOUNT_RICE).build();
     }
 
     /**
@@ -107,30 +86,35 @@ public class TemplateCommandTestUtil {
     public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage) {
         // we are unable to defensively copy the model for comparison later, so we can
         // only do so by copying its components.
-        GroceryList expectedGroceryList = new GroceryList(actualModel.getGroceryList());
-        List<Food> expectedFilteredList = new ArrayList<>(actualModel.getFilteredGroceryItemList());
+        TemplateList expectedTemplateList = new TemplateList(actualModel.getTemplateList());
+        List<UniqueTemplateItems> expectedFilteredList = new ArrayList<>(actualModel.getFilteredTemplateList());
+        List<TemplateItem> expectedFilteredTemplate = new ArrayList<>(actualModel.getFilteredTemplateToBeShown());
 
         assertThrows(CommandException.class, expectedMessage, () -> command.execute(actualModel));
-        assertEquals(expectedGroceryList, actualModel.getGroceryList());
-        assertEquals(expectedFilteredList, actualModel.getFilteredGroceryItemList());
+        assertEquals(expectedTemplateList, actualModel.getTemplateList());
+        assertEquals(expectedFilteredList, actualModel.getFilteredTemplateList());
+        assertEquals(expectedFilteredTemplate, actualModel.getFilteredTemplateToBeShown());
     }
+
     /**
      * Updates {@code model}'s filtered list to show only the food at the given {@code targetIndex} in the
      * {@code model}'s address book.
      */
     public static void showItemAtIndex(Model model, Index templateIndex, Index itemIndex) {
-        // For template list
-        assertTrue(templateIndex.getZeroBased() < model.getFilteredTemplateList().size());
-        TemplateItem item = model.getFilteredTemplateList()
-                .get(templateIndex.getZeroBased()).get(itemIndex.getZeroBased());
-        final String[] splitName = item.getName().fullName.split("\\s+");
-        model.updateFilteredTemplateList(PREDICATE_SHOW_ALL_TEMPLATES);
-        assertEquals(3, model.getFilteredTemplateList().size());
+        requireNonNull(templateIndex);
 
-        // For template list
+        //Template list
         assertTrue(templateIndex.getZeroBased() < model.getFilteredTemplateList().size());
         UniqueTemplateItems template = model.getFilteredTemplateList().get(templateIndex.getZeroBased());
+        model.updateFilteredTemplateList(PREDICATE_SHOW_ALL_TEMPLATES);
+        model.updateFilteredTemplateToBeShown(PREDICATE_SHOW_ALL_TEMPLATES_ITEMS);
         assertEquals(3, model.getFilteredTemplateList().size());
-    }
+        assertEquals(0, model.getFilteredTemplateToBeShown().size());
 
+        //Template to be shown
+        if (itemIndex != null) {
+            assertTrue(itemIndex.getZeroBased() < model.getFilteredTemplateToBeShown().size());
+            TemplateItem templateItem = model.getFilteredTemplateToBeShown().get(itemIndex.getZeroBased());
+        }
+    }
 }
--- src/test/java/seedu/ifridge/logic/commands/templatelist/template/AddTemplateItemCommandTest.java
+++ src/test/java/seedu/ifridge/logic/commands/templatelist/template/AddTemplateItemCommandTest.java
@@ -5,16 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.ifridge.commons.core.Messages.MESSAGE_INVALID_TEMPLATE_DISPLAYED_INDEX;
 import static seedu.ifridge.logic.commands.CommandTestUtil.VALID_AMOUNT_CHEESE;
 import static seedu.ifridge.logic.commands.CommandTestUtil.VALID_NAME_CHEESE;
-import static seedu.ifridge.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.ifridge.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.DESC_TEMP_MINCED_MEAT;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.DESC_TEMP_TOMATO_JUICE;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.INVALID_AMOUNT_FOR_QUANTITY;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.INVALID_AMOUNT_FOR_UNIT;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.INVALID_AMOUNT_FOR_VOLUME;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.VALID_AMOUNT_ORANGES;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.VALID_NAME_ORANGES;
-import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.showItemAtIndex;
+import static seedu.ifridge.logic.commands.templatelist.TemplateCommandTestUtil.*;
 import static seedu.ifridge.model.Model.PREDICATE_SHOW_ALL_TEMPLATES;
 import static seedu.ifridge.model.food.Amount.UNIT_TYPE_QUANTITY;
 import static seedu.ifridge.model.food.Amount.UNIT_TYPE_VOLUME;
@@ -34,6 +26,7 @@ import java.util.TreeMap;
 import org.junit.jupiter.api.Test;
 
 import seedu.ifridge.commons.core.index.Index;
+import seedu.ifridge.logic.commands.CommandResult;
 import seedu.ifridge.logic.commands.templatelist.template.EditTemplateItemCommand.EditTemplateItemDescriptor;
 import seedu.ifridge.model.GroceryList;
 import seedu.ifridge.model.Model;
@@ -65,7 +58,7 @@ public class AddTemplateItemCommandTest {
         assertThrows(NullPointerException.class, () -> new AddTemplateItemCommand(null, null));
     }
 
-    /**
+    @Test
     public void execute_allFieldsSpecified_addSuccessful() throws Exception {
         TemplateItem validTemplateItem = new TemplateItemBuilder()
                 .withName(VALID_NAME_TOMATO_JUICE).withAmount(VALID_AMOUNT_TOMATO_JUICE).build();
@@ -77,11 +70,9 @@ public class AddTemplateItemCommandTest {
 
         String expectedMessage = String.format(AddTemplateItemCommand.MESSAGE_SUCCESS, validTemplateItem);
 
-        CommandResult commandResult = new AddTemplateItemCommand(INDEX_FIRST, validTemplateItem).execute(model);
-
         Model expectedModel = new ModelManager(
                 new GroceryList(model.getGroceryList()), new UserPrefs(model.getUserPrefs()),
-                new TemplateList(model.getTemplateList()), new TreeMapWasteMonth, WasteList(model.getWasteArchive()),
+                new TemplateList(model.getTemplateList()), new TreeMap<WasteMonth, WasteList>(model.getWasteArchive()),
                 new ShoppingList(model.getShoppingList()), new GroceryList(model.getBoughtList()),
                 new UnitDictionary(model.getUnitDictionary()));
         expectedModel.setTemplate(templateToEdit, editedTemplate);
@@ -91,7 +82,7 @@ public class AddTemplateItemCommandTest {
         expectedModel.commitTemplateList(templateToEdit, editedTemplate, 0);
 
         assertCommandSuccess(addCommand, model, expectedMessage, expectedModel);
-    }**/
+    }
 
     @Test
     public void execute_duplicateFoodUnfilteredList_addFailure() {
@@ -133,59 +124,25 @@ public class AddTemplateItemCommandTest {
     }
 
     @Test
-    public void execute_filteredList_success() {
-        showItemAtIndex(model, INDEX_FIRST, INDEX_FIRST);
-
-        UniqueTemplateItems filteredTemplate = model.getFilteredTemplateList().get(INDEX_FIRST.getZeroBased());
-        UniqueTemplateItems editedTemplate = new UniqueTemplateItemsBuilder(filteredTemplate).build();
-
-        TemplateItem itemInTemplate = filteredTemplate.get(INDEX_FIRST.getZeroBased());
-        TemplateItem editedItem = new TemplateItemBuilder(itemInTemplate)
-                .withName(VALID_NAME_ORANGES).withAmount(VALID_AMOUNT_ORANGES).build();
-        editedTemplate.setTemplateItem(itemInTemplate, editedItem);
-
-        EditTemplateItemCommand editCommand =
-                new EditTemplateItemCommand(INDEX_FIRST, INDEX_FIRST, new EditTemplateItemDescriptorBuilder()
-                        .withName(VALID_NAME_ORANGES).withAmount(VALID_AMOUNT_ORANGES).build());
-
-        String expectedMessage = String.format(EditTemplateItemCommand.MESSAGE_SUCCESS, itemInTemplate, editedItem);
-
-        Model expectedModel = new ModelManager(
-                new GroceryList(model.getGroceryList()), new UserPrefs(model.getUserPrefs()),
-                new TemplateList(model.getTemplateList()), new TreeMap<WasteMonth, WasteList>(model.getWasteArchive()),
-                new ShoppingList(model.getShoppingList()), new GroceryList(model.getBoughtList()),
-                new UnitDictionary(model.getUnitDictionary()));
-
-        expectedModel.setTemplate(model.getFilteredTemplateList().get(0), editedTemplate);
-        expectedModel.updateFilteredTemplateList(PREDICATE_SHOW_ALL_TEMPLATES);
-        expectedModel.commitTemplateList(filteredTemplate, editedTemplate, 0);
-        expectedModel.setShownTemplate(editedTemplate);
-        expectedModel.updateFilteredTemplateToBeShown();
-
-        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-    }
-
-    @Test
     public void execute_invalidTemplateIndexUnfilteredList_failure() {
         Index outOfBoundTemplateIndex = Index.fromOneBased(model.getFilteredTemplateList().size() + 1);
-        EditTemplateItemDescriptor descriptor = new EditTemplateItemDescriptorBuilder().withName(VALID_NAME_CHEESE)
+        TemplateItem addedItem = new TemplateItemBuilder().withName(VALID_NAME_CHEESE)
                 .withAmount(VALID_AMOUNT_CHEESE).build();
-        EditTemplateItemCommand editCommand =
-                new EditTemplateItemCommand(outOfBoundTemplateIndex, INDEX_FIRST, descriptor);
+        AddTemplateItemCommand addCommand =
+                new AddTemplateItemCommand(outOfBoundTemplateIndex, addedItem);
 
-        assertCommandFailure(editCommand, model, MESSAGE_INVALID_TEMPLATE_DISPLAYED_INDEX);
+        assertCommandFailure(addCommand, model, MESSAGE_INVALID_TEMPLATE_DISPLAYED_INDEX);
     }
 
     @Test
     public void equals() {
         // Need to set a new DESC_AMY for CommandTestUtil with a TemplateItemDescriptor
-        final EditTemplateItemCommand standardCommand =
-                new EditTemplateItemCommand(INDEX_FIRST, INDEX_FIRST, DESC_TEMP_MINCED_MEAT);
+        final TemplateItem standardItem = TEMPLATE_ITEM_CHEESE;
+        final AddTemplateItemCommand standardCommand = new AddTemplateItemCommand(INDEX_FIRST, standardItem);
 
         // same values -> returns true
-        EditTemplateItemDescriptor copyDescriptor = new EditTemplateItemDescriptor(DESC_TEMP_MINCED_MEAT);
-        EditTemplateItemCommand commandWithSameValues =
-                new EditTemplateItemCommand(INDEX_FIRST, INDEX_FIRST, copyDescriptor);
+        AddTemplateItemCommand commandWithSameValues =
+                new AddTemplateItemCommand(INDEX_FIRST, standardItem);
         assertTrue(standardCommand.equals(commandWithSameValues));
 
         // same object -> returns true
@@ -199,10 +156,10 @@ public class AddTemplateItemCommandTest {
 
         // different index -> returns false
         assertFalse(standardCommand.equals(
-                new EditTemplateItemCommand(INDEX_SECOND, INDEX_FIRST, DESC_TEMP_MINCED_MEAT)));
+                new AddTemplateItemCommand(INDEX_SECOND, standardItem)));
 
         // different descriptor -> returns false
         assertFalse(standardCommand.equals(
-                new EditTemplateItemCommand(INDEX_FIRST, INDEX_FIRST, DESC_TEMP_TOMATO_JUICE)));
+                new AddTemplateItemCommand(INDEX_FIRST, TEMPLATE_ITEM_RICE)));
     }
 }
