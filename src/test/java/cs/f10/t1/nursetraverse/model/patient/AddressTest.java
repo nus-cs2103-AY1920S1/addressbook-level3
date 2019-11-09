c8d1c701531046c2f30ad00d7fef08c1b2e19ad7
gabrielchao <fusionpol@hotmail.com>
2019-11-09 14:39:17 +0800

    Add more tests

--- src/test/java/cs/f10/t1/nursetraverse/model/patient/AddressTest.java
+++ src/test/java/cs/f10/t1/nursetraverse/model/patient/AddressTest.java
@@ -1,7 +1,9 @@
 package cs.f10.t1.nursetraverse.model.patient;
 
 import static cs.f10.t1.nursetraverse.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -32,5 +34,18 @@ public class AddressTest {
         assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
         assertTrue(Address.isValidAddress("-")); // one character
         assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
+    }
+
+    @Test
+    public void equals() {
+        assertNotEquals(new Address("Blk 456, Den Road, #01-355"), new Address("Blk 456 Den Road #01-355"));
+        assertNotEquals(new Address("-"), new Address("_"));
+        assertNotEquals(new Address("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA"),
+                new Address("LENG Inc; 1234 Market St; San Francisco CA 2349879; USA"));
+
+        assertEquals(new Address("Blk 456, Den Road, #01-355"), new Address("Blk 456, Den Road, #01-355"));
+        assertEquals(new Address("-"), new Address("-"));
+        assertEquals(new Address("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA"),
+                new Address("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA"));
     }
 }
