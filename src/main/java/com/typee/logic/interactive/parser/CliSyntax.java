4e77621894326ca8375a1ac9f5323aeaead9306f
Gi Hun Ko <rhrlgns96@gmail.com>
2019-11-09 15:01:40 +0800

    implements unit test for FromState class.

--- src/main/java/com/typee/logic/interactive/parser/CliSyntax.java
+++ src/main/java/com/typee/logic/interactive/parser/CliSyntax.java
@@ -19,7 +19,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_ORDER = new Prefix("o/");
     public static final Prefix PREFIX_CALENDAR = new Prefix("c/");
     public static final Prefix PREFIX_DATE = new Prefix("d/");
-    //Report Generation 23/10/19 Ko Gi Hun
     public static final Prefix PREFIX_LIST_INDEX = new Prefix("i/");
     public static final Prefix PREFIX_TO = new Prefix("t/");
     public static final Prefix PREFIX_FROM = new Prefix("f/");
