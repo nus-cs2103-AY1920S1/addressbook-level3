c8c8602c1d819d97e4b98f725141a0556993ce77
gabrielchao <fusionpol@hotmail.com>
2019-11-09 16:57:44 +0800

    Add a UndoCommandParser test

--- src/test/java/cs/f10/t1/nursetraverse/logic/parser/UndoCommandParserTest.java
+++ src/test/java/cs/f10/t1/nursetraverse/logic/parser/UndoCommandParserTest.java
@@ -1,5 +1,7 @@
 package cs.f10.t1.nursetraverse.logic.parser;
 
+import static cs.f10.t1.nursetraverse.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import org.junit.jupiter.api.Test;
 
 import cs.f10.t1.nursetraverse.commons.core.index.Index;
@@ -19,5 +21,11 @@ public class UndoCommandParserTest {
     public void parse_withValidArgs_returnsUndoCommand() {
         UndoCommand expectedUndoCommand = new UndoCommand(Index.fromOneBased(1));
         CommandParserTestUtil.assertParseSuccess(parser, "1", expectedUndoCommand);
+    }
+
+    @Test
+    public void parse_withInvalidArgs_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "a", String
+                .format(MESSAGE_INVALID_COMMAND_FORMAT, UndoCommand.MESSAGE_USAGE));
     }
 }
