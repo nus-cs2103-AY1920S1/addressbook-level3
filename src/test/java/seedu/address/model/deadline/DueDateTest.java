f35cf1304a866d5c57efa42a1b8ba5d42450ee8f
dalsontws <dalsontws@gmail.com>
2019-11-09 22:36:33 +0800

    remove unused import

--- src/test/java/seedu/address/model/deadline/DueDateTest.java
+++ src/test/java/seedu/address/model/deadline/DueDateTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import java.time.format.DateTimeParseException;
-
 import org.junit.jupiter.api.Test;
 
 public class DueDateTest {
