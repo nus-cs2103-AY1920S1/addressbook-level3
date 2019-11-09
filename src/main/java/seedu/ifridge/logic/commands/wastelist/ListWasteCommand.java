ff762972156e27a0332e23fd4a99bce444a8a0fc
zhangxuan97 <zxrox@yahoo.com.sg>
2019-11-09 22:34:44 +0800

    Add manual testing section fo waste list and update the general manual testing to match that of iFridge

--- src/main/java/seedu/ifridge/logic/commands/wastelist/ListWasteCommand.java
+++ src/main/java/seedu/ifridge/logic/commands/wastelist/ListWasteCommand.java
@@ -7,6 +7,8 @@ import seedu.ifridge.logic.commands.Command;
 import seedu.ifridge.logic.commands.CommandResult;
 import seedu.ifridge.logic.commands.exceptions.CommandException;
 import seedu.ifridge.model.Model;
+import seedu.ifridge.model.ReadOnlyWasteList;
+import seedu.ifridge.model.WasteList;
 import seedu.ifridge.model.waste.WasteMonth;
 
 /**
@@ -19,7 +21,7 @@ public class ListWasteCommand extends Command {
     public static final String MESSAGE_USAGE = "wlist " + COMMAND_WORD
             + ": Lists out your current food waste for a given month. "
             + "If no month is specified, the food waste for the current month is displayed.\n"
-            + "Optional Parameters: " + PREFIX_MONTH + "MONTH_OF_YEAR"
+            + "Optional Parameters: " + PREFIX_MONTH + "MONTH_OF_YEAR\n"
             + "Example: wlist " + COMMAND_WORD + " " + PREFIX_MONTH + "Sep 2019\n";
 
     public static final String MESSAGE_MONTH_RESTRICTION = "The given month must not"
@@ -52,7 +54,9 @@ public class ListWasteCommand extends Command {
         }
 
         boolean hasWasteListInArchive = model.hasWasteMonth(this.wasteMonth);
-        if (!hasWasteListInArchive) {
+        if (!hasWasteListInArchive && this.wasteMonth.equals(currentWasteMonth)) {
+            model.getWasteList();
+        } else if (!hasWasteListInArchive) {
             throw new CommandException(String.format(MESSAGE_NO_WASTE_LIST_FOUND, this.wasteMonth));
         }
 
