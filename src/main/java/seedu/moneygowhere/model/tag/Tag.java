433991f776c8a99cd5249c7a9ec7e924dd24aef5
choongyx <yongxin.choong98@gmail.com>
2019-11-09 14:30:43 +0800

    Limit tags to 15 characters

--- src/main/java/seedu/moneygowhere/model/tag/Tag.java
+++ src/main/java/seedu/moneygowhere/model/tag/Tag.java
@@ -9,8 +9,8 @@ import static seedu.moneygowhere.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric and have at most 64 characters.";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}{1,64}+";
+    public static final String MESSAGE_CONSTRAINTS = "Tag names should be alphanumeric and have at most 15 characters.";
+    public static final String VALIDATION_REGEX = "\\p{Alnum}{1,15}+";
 
     public final String tagName;
 
