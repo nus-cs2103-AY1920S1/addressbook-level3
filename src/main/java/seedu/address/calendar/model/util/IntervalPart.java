c042634b0b5c2ad3f1307a244cf3cf1cead8631b
pohlinwei <linwei0909@hotmail.com>
2019-11-09 15:32:05 +0800

    Complete Javadoc for util package

--- src/main/java/seedu/address/calendar/model/util/IntervalPart.java
+++ src/main/java/seedu/address/calendar/model/util/IntervalPart.java
@@ -1,5 +1,9 @@
 package seedu.address.calendar.model.util;
 
+/**
+ * Represents a part of an interval (i.e. the start or the end).
+ * @param <T> The type of interval part (e.g. time, date, integer, etc.)
+ */
 public interface IntervalPart<T> extends Comparable<IntervalPart<T>> {
     T copy();
 }
