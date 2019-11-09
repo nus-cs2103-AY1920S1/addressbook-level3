dc6ce8386015d40eb4c26d338bfaae6a1b42d395
Tan Wei Yang <weiyang.1996@gmail.com>
2019-11-10 02:19:28 +0800

    Extract transaction context initialization from ModelManager constructor

--- src/main/java/seedu/ichifund/model/ModelManager.java
+++ src/main/java/seedu/ichifund/model/ModelManager.java
@@ -56,22 +56,26 @@ public class ModelManager implements Model {
         filteredTransactions = new FilteredList<>(this.fundBook.getTransactionList());
         filteredRepeaters = new FilteredList<>(this.fundBook.getRepeaterList());
         filteredBudgets = new FilteredList<>(this.fundBook.getBudgetList());
+        datas = new FilteredList<>(this.fundBook.getDataList());
+        initTransactionContext();
+    }
 
+    public ModelManager() {
+        this(new FundBook(), new UserPrefs());
+    }
+
+    /**
+     * Sets up initial transaction context.
+     */
+    private void initTransactionContext() {
         Optional<Transaction> latestTransaction = this.fundBook.getLatestTransaction();
         latestTransaction.ifPresentOrElse(transaction -> logger.fine(
                 "Latest transaction found: " + transaction), () -> logger.fine("No transactions found."));
 
         TransactionContext transactionContext = new TransactionContext(latestTransaction);
-        logger.info("Setting initial transaction context as: " + transactionContext);
-
-        updateFilteredTransactionList(transactionContext.getPredicate());
         this.transactionContext = new SimpleObjectProperty<>(transactionContext);
-        datas = new FilteredList<>(this.fundBook.getDataList());
-        setTransactionContext(new TransactionContext(this.fundBook.getLatestTransaction()));
-    }
-
-    public ModelManager() {
-        this(new FundBook(), new UserPrefs());
+        updateFilteredTransactionList(transactionContext.getPredicate());
+        logger.info("Initial transaction context set as: " + transactionContext);
     }
 
     //=========== UserPrefs ==================================================================================
