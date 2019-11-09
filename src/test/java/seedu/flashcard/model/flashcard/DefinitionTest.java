0a7ff47f216777f612beebcd4c113b3bad13b1ee
lingzw <lingzw64@gmail.com>
2019-11-09 23:54:03 +0800

    -Added some model Test cases -Edited UG,DG and PPP

---
+++ src/test/java/seedu/flashcard/model/flashcard/DefinitionTest.java
@@ -0,0 +1,36 @@
+package seedu.flashcard.model.flashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.flashcard.testutil.Assert.assertThrows;
+
+public class DefinitionTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Definition(null));
+    }
+
+    @Test
+    public void constructor_invalidDefinition_throwsIllegalArgumentException() {
+        String invalidDefinition = "";
+        assertThrows(IllegalArgumentException.class, () -> new Definition(invalidDefinition));
+    }
+
+    @Test
+    public void isValidDefinition() {
+        // null Definition
+        assertThrows(NullPointerException.class, () -> Definition.isValidDefinition(null));
+
+        // invalid Definition
+        assertFalse(Definition.isValidDefinition("")); // empty string
+        assertFalse(Definition.isValidDefinition(" ")); // spaces only
+
+        // valid Definition
+        assertTrue(Definition.isValidDefinition("3"));
+        assertTrue(Definition.isValidDefinition("/")); // one character
+        assertTrue(Definition.isValidDefinition("Builds applications by combining functionalities"
+                +" packaged as programmatically")); // long answer
+    }
+}
