ac52202ed1efa0917362515a33c8c76b5b560d9f
SebastianLie <jackcass629@gmail.com>
2019-11-10 01:47:26 +0800

    Added Javadoc comment

--- src/test/java/seedu/address/testutil/TypicalFiles.java
+++ src/test/java/seedu/address/testutil/TypicalFiles.java
@@ -1,5 +1,8 @@
 package seedu.address.testutil;
 
+/**
+ * Typical, valid filenames for DisplayPic
+ */
 public class TypicalFiles {
     public static final String FILE_EXAMPLE_1 = "path/file.png";
     public static final String FILE_EXAMPLE_2 = "path/file.jpg";
