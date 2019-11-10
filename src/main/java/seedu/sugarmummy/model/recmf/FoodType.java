c7b3c420af405859119b287e0fa478b435544c11
ZhangHuafan <e0261883@u.nus.edu>
2019-11-10 10:31:52 +0800

    Update recmfood command usage message

--- src/main/java/seedu/sugarmummy/model/recmf/FoodType.java
+++ src/main/java/seedu/sugarmummy/model/recmf/FoodType.java
@@ -29,7 +29,7 @@ public enum FoodType {
         return type.abbr + "(" + type.typeName + ")";
     }
 
-    private static String getAllTypesInfo() {
+    public static String getAllTypesInfo() {
         StringBuilder stringBuilder = new StringBuilder();
         for (FoodType type : FoodType.values()) {
             stringBuilder.append(getTypeInfo(type) + " ");
