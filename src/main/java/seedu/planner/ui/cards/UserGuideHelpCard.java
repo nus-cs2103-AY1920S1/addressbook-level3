7146576ef892bf36e629f8e1fabcca1860e892fd
1nefootstep <sheuxiang@gmail.com>
2019-11-10 15:30:07 +0800

    Change text of link to guide

--- src/main/java/seedu/planner/ui/cards/UserGuideHelpCard.java
+++ src/main/java/seedu/planner/ui/cards/UserGuideHelpCard.java
@@ -4,11 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.Region;
-import seedu.planner.logic.commands.util.HelpExplanation;
 import seedu.planner.ui.UiPart;
 
 /**
@@ -17,10 +15,10 @@ import seedu.planner.ui.UiPart;
  */
 public class UserGuideHelpCard extends UiPart<Region> {
     private static final String FXML = "UserGuideHelpCard.fxml";
+    public static final String USERGUIDE_URL = "https://ay1920s1-cs2103t-t09-1.github.io/main/UserGuide.html";
+
     @FXML
     private Hyperlink hyperlink;
-
-    public static final String USERGUIDE_URL = "https://ay1920s1-cs2103t-t09-1.github.io/main/UserGuide.html";
 
     public UserGuideHelpCard() {
         super(FXML);
