aa79abb84ae5f69afdd7f137f68df8ee8c022948
limhawjia <50948002+limhawjia@users.noreply.github.com>
2019-11-11 02:14:39 +0800

    Hj dg update (#190)
    * Updated dg up till ProgramSubmissionLogicManger
    
    * Move all dg images to new folder, ammended old descriptions
    
    * Finish dg updates to logic
    
    * Fix outdated Javadoc
    
    * Add instruction to use Main class in ug, improve user feedback

--- src/main/java/com/dukeacademy/testexecutor/executor/ProgramExecutor.java
+++ src/main/java/com/dukeacademy/testexecutor/executor/ProgramExecutor.java
@@ -12,7 +12,7 @@ import com.dukeacademy.testexecutor.models.ProgramOutput;
  */
 public interface ProgramExecutor {
     /**
-     * Executes the given Java class file with the given program input.
+     * Returns a completable future that will execute the given Java class file with the given program input.
      *
      * @param program the program to be executed
      * @param input   the input to be fed into the program
@@ -21,13 +21,4 @@ public interface ProgramExecutor {
      */
     CompletableFuture<ProgramOutput> executeProgram(ClassFile program, ProgramInput input)
             throws ProgramExecutorException;
-
-    /**
-     * Executes the given Java class file with no input
-     *
-     * @param program the program to be executed
-     * @return the program output
-     * @throws ProgramExecutorException if the execution fails
-     */
-    ProgramOutput executeProgram(ClassFile program) throws ProgramExecutorException;
 }
