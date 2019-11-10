881f4492092c308f6b67c0c01d1c7fe295c1d557
AaronLuk <chihengluk@gmail.com>
2019-11-10 22:46:20 +0800

    we did it

--- src/main/java/mams/logic/commands/MassReject.java
+++ src/main/java/mams/logic/commands/MassReject.java
@@ -1,14 +1,13 @@
 package mams.logic.commands;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import mams.logic.commands.exceptions.CommandException;
 import mams.logic.history.FilterOnlyCommandHistory;
 import mams.model.Model;
 import mams.model.appeal.Appeal;
-
-
 
 /**
  * Mass rejects appeals in Mams
@@ -20,49 +19,83 @@ public class MassReject extends Reject {
     private final List<String> alreadyApproved = new ArrayList<>();
     private final List<String> alreadyRejected = new ArrayList<>();
     private final List<String> rejectedSuccessfully = new ArrayList<>();
+    private final List<String> cannotFindIdList;
 
     public MassReject(List<String> validIds, List<String> invalidIds) {
         this.validIds = validIds;
         this.invalidIds = invalidIds;
+        this.cannotFindIdList = new ArrayList<>();
     }
 
     @Override
     public CommandResult execute(Model model, FilterOnlyCommandHistory commandHistory) throws CommandException {
         List<Appeal> lastShownList = model.getFilteredAppealList();
-
+        boolean foundId;
         for (String appealId : validIds) {
+            foundId = false;
             for (Appeal appeal : lastShownList) {
                 if (appealId.equalsIgnoreCase(appeal.getAppealId())) {
-                    Appeal approvedAppeal;
-                    Appeal appealToApprove = appeal;
-                    if (appealToApprove.isResolved() == false) {
-                        approvedAppeal = new Appeal(appealToApprove.getAppealId(),
-                                appealToApprove.getAppealType(),
-                                appealToApprove.getStudentId(),
-                                appealToApprove.getAcademicYear(),
-                                appealToApprove.getStudentWorkload(),
-                                appealToApprove.getAppealDescription(),
-                                appealToApprove.getPreviousModule(),
-                                appealToApprove.getNewModule(),
-                                appealToApprove.getModuleToAdd(),
-                                appealToApprove.getModuleToDrop(),
+                    Appeal rejectedAppeal;
+                    Appeal appealToReject = appeal;
+                    if (appealToReject.isResolved() == false) {
+                        rejectedAppeal = new Appeal(appealToReject.getAppealId(),
+                                appealToReject.getAppealType(),
+                                appealToReject.getStudentId(),
+                                appealToReject.getAcademicYear(),
+                                appealToReject.getStudentWorkload(),
+                                appealToReject.getAppealDescription(),
+                                appealToReject.getPreviousModule(),
+                                appealToReject.getNewModule(),
+                                appealToReject.getModuleToAdd(),
+                                appealToReject.getModuleToDrop(),
                                 true,
                                 "REJECTED",
                                 "");
-                        model.setAppeal(appealToApprove, approvedAppeal);
+                        model.setAppeal(appealToReject, rejectedAppeal);
                         rejectedSuccessfully.add(appealId);
-                    } else if (appealToApprove.isResolved() == true
-                            && appealToApprove.getResult().equalsIgnoreCase("APPROVED")) {
+                    } else if (appealToReject.isResolved() == true
+                            && appealToReject.getResult().equalsIgnoreCase("APPROVED")) {
                         alreadyApproved.add(appeal.getAppealId());
-                    } else if (appealToApprove.isResolved() == true
-                            && appealToApprove.getResult().equalsIgnoreCase("REJECTED")) {
+                    } else if (appealToReject.isResolved() == true
+                            && appealToReject.getResult().equalsIgnoreCase("REJECTED")) {
                         alreadyRejected.add(appeal.getAppealId());
                     }
+                    foundId = true;
                     break;
                 }
             }
+            if (!foundId) {
+                cannotFindIdList.add(appealId);
+            }
+
         }
         return new CommandResult(resultGenerator());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof MassApprove)) {
+            return false;
+        }
+
+        // state check
+        MassApprove e = (MassApprove) other;
+        List<String> compareValid = e.getValidList();
+        List<String> compareInvalid = e.getInvalidIds();
+
+        Collections.sort(compareValid);
+        Collections.sort(compareInvalid);
+        Collections.sort(this.validIds);
+        Collections.sort(this.invalidIds);
+
+        return validIds.equals(compareValid);
+
     }
 
     /**
@@ -84,6 +117,9 @@ public class MassReject extends Reject {
         }
         if (!invalidIds.isEmpty()) {
             result += "\nInvalid appeal IDs: " + invalidIds.toString();
+        }
+        if (!cannotFindIdList.isEmpty()) {
+            result += "\nThese appeal IDs do not exist: " + cannotFindIdList.toString();
         }
         return result;
     }
