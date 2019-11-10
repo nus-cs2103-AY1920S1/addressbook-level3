f6e2fd36deed4aa7ad399931634617e3d1f4d468
troomtroom <kartike@DESKTOP-5DLSJQ5.localdomain>
2019-11-11 07:53:01 +0800

    Modify storage for Loans

--- src/main/java/seedu/ichifund/storage/JsonSerializableFundBook.java
+++ src/main/java/seedu/ichifund/storage/JsonSerializableFundBook.java
@@ -13,6 +13,7 @@ import seedu.ichifund.model.FundBook;
 import seedu.ichifund.model.ReadOnlyFundBook;
 import seedu.ichifund.model.budget.Budget;
 import seedu.ichifund.model.loan.Loan;
+import seedu.ichifund.model.loan.LoanId;
 import seedu.ichifund.model.repeater.Repeater;
 import seedu.ichifund.model.repeater.RepeaterUniqueId;
 import seedu.ichifund.model.transaction.Transaction;
@@ -29,9 +30,11 @@ class JsonSerializableFundBook {
     public static final String MESSAGE_DUPLICATE_LOAN = "Budgets list contains duplicate budget(s).";
 
     private final String currentRepeaterUniqueId;
+    private final String currentLoanId;
     private final List<JsonAdaptedTransaction> transactions = new ArrayList<>();
     private final List<JsonAdaptedRepeater> repeaters = new ArrayList<>();
     private final List<JsonAdaptedBudget> budgets = new ArrayList<>();
+    private final List<JsonAdaptedLoan> loans = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonSerializableFundBook} with the given person and budget.
@@ -39,13 +42,16 @@ class JsonSerializableFundBook {
     @JsonCreator
     public JsonSerializableFundBook(
             @JsonProperty("currentRepeaterUniqueId") String currentRepeaterUniqueId,
+            @JsonProperty("currentLoanId") String currentLoanId,
             @JsonProperty("transactions") List<JsonAdaptedTransaction> transactions,
             @JsonProperty("repeaters") List<JsonAdaptedRepeater> repeaters,
             @JsonProperty("loans") List<JsonAdaptedLoan> loans,
             @JsonProperty("budgets") List<JsonAdaptedBudget> budgets) {
         this.currentRepeaterUniqueId = currentRepeaterUniqueId;
+        this.currentLoanId = currentLoanId;
         this.transactions.addAll(transactions);
         this.repeaters.addAll(repeaters);
+        this.loans.addAll(loans);
         this.budgets.addAll(budgets);
     }
 
@@ -56,10 +62,12 @@ class JsonSerializableFundBook {
      */
     public JsonSerializableFundBook(ReadOnlyFundBook source) {
         currentRepeaterUniqueId = source.getCurrentRepeaterUniqueId().toString();
+        currentLoanId = source.getCurrentLoanId().toString();
         transactions.addAll(source.getTransactionList().stream().map(JsonAdaptedTransaction::new)
                 .collect(Collectors.toList()));
         repeaters.addAll(source.getRepeaterList().stream().map(JsonAdaptedRepeater::new).collect(Collectors.toList()));
         budgets.addAll(source.getBudgetList().stream().map(JsonAdaptedBudget::new).collect(Collectors.toList()));
+        loans.addAll(source.getLoanList().stream().map(JsonAdaptedLoan::new).collect(Collectors.toList()));
     }
 
     /**
@@ -80,6 +88,18 @@ class JsonSerializableFundBook {
         final RepeaterUniqueId modelCurrentRepeaterUniqueId = new RepeaterUniqueId(currentRepeaterUniqueId);
         fundBook.setCurrentRepeaterUniqueId(modelCurrentRepeaterUniqueId);
 
+
+        if (currentLoanId == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    LoanId.class.getSimpleName()));
+        }
+        if (!LoanId.isValidLoanId(currentLoanId)) {
+            throw new IllegalValueException(LoanId.MESSAGE_CONSTRAINTS);
+        }
+        final LoanId modelCurrentLoanId = new LoanId(currentLoanId);
+        fundBook.setCurrentLoanId(modelCurrentLoanId);
+
+
         for (JsonAdaptedTransaction jsonAdaptedTransaction : transactions) {
             Transaction transaction = jsonAdaptedTransaction.toModelType();
             fundBook.addTransaction(transaction);
@@ -99,6 +119,14 @@ class JsonSerializableFundBook {
                 throw new IllegalValueException(MESSAGE_DUPLICATE_BUDGET);
             }
             fundBook.addBudget(budget);
+        }
+
+        for (JsonAdaptedLoan jsonAdaptedLoan : loans) {
+            Loan loan = jsonAdaptedLoan.toModelType();
+            if (fundBook.hasLoan(loan)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_LOAN);
+            }
+            fundBook.addLoan(loan);
         }
 
         return fundBook;
