de1593ac6f477014a78874d144a9175a65ee013d
ning-sy210 <ningshy97@gmail.com>
2019-11-10 14:40:56 +0800

    Editor now has a line counter and can auto-indent
    Editor now has a line counter and can auto-indent

--- src/main/java/com/dukeacademy/ui/Editor.java
+++ src/main/java/com/dukeacademy/ui/Editor.java
@@ -2,15 +2,21 @@ package com.dukeacademy.ui;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.Key;
+import java.util.stream.Stream;
 
 import com.dukeacademy.model.question.Question;
 import com.dukeacademy.model.question.UserProgram;
 
 import com.dukeacademy.observable.Observable;
 
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -23,12 +29,10 @@ import javafx.stage.Stage;
  */
 public class Editor extends UiPart<Region> {
     private static final String FXML = "Editor.fxml";
+    private SimpleIntegerProperty numberOfLines = new SimpleIntegerProperty(1);
 
-    //    @FXML
-    //    private Button btnSave;
-    //
-    //    @FXML
-    //    private Button btnSubmit;
+    @FXML
+    private TextArea lineCounter;
 
     @FXML
     private TextArea textOutput;
@@ -57,12 +61,52 @@ public class Editor extends UiPart<Region> {
      */
     @FXML
     public void initialize() {
-        textOutput.addEventFilter(KeyEvent.KEY_PRESSED, e1 -> {
+        textOutput.addEventHandler(KeyEvent.KEY_PRESSED, e1 -> {
             if (e1.getCode() == KeyCode.TAB) {
-                String s = " ".repeat(4);
-                textOutput.insertText(textOutput.getCaretPosition(), s);
+                textOutput.insertText(textOutput.getCaretPosition(), " ".repeat(2));
                 e1.consume();
+            } else if (e1.isShiftDown() && e1.getCode() == KeyCode.CLOSE_BRACKET) {
+                int currentCaretPos = textOutput.getCaretPosition();
+                boolean isEmptyLineAtCaretPosition = true; // need further changes
+
+                if (isEmptyLineAtCaretPosition) {
+                    textOutput.deleteText(currentCaretPos - 2, currentCaretPos - 1);
+                }
             }
+        });
+
+        textOutput.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                long leftBraceCount = textOutput.textProperty().getValue().chars()
+                        .filter(ch -> ch == '{')
+                        .count();
+
+                long rightBraceCount = textOutput.textProperty().getValue().chars()
+                        .filter(ch -> ch == '}')
+                        .count();
+
+                long indentationCount = leftBraceCount - rightBraceCount;
+
+                if (indentationCount > 0) {
+                    String tab = " ".repeat(2);
+                    textOutput.insertText(textOutput.getCaretPosition(), tab.repeat((int) indentationCount));
+                }
+            }
+        });
+
+        textOutput.textProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(javafx.beans.Observable observable) {
+                numberOfLines.setValue((textOutput.getText() + " ").split("\n").length);
+                lineCounter.setText(generateLineCounterInput(numberOfLines));
+            }
+        });
+
+        Platform.runLater(() -> {
+            ScrollBar editorsb = (ScrollBar) textOutput.lookup(".scroll-bar:vertical");
+            ScrollBar lineCountersb = (ScrollBar) lineCounter.lookup(".scroll-bar:vertical");
+
+            lineCountersb.valueProperty().bind(editorsb.valueProperty());
         });
     }
 
@@ -83,19 +127,6 @@ public class Editor extends UiPart<Region> {
         fw.close();
     }
 
-
-    /**
-     * On click btn submit.
-     *
-     * @param e the e
-     * @return the string
-     */
-    @FXML
-    public String onSubmitButtonClick(ActionEvent e) {
-        System.out.println(textOutput.getText().strip());
-        return textOutput.getText().strip();
-    }
-
     /**
      * Returns the current text in the editor.
      *
@@ -103,5 +134,19 @@ public class Editor extends UiPart<Region> {
      */
     public UserProgram getUserProgram() {
         return new UserProgram("Main", textOutput.getText().strip());
+    }
+
+    private String generateLineCounterInput(SimpleIntegerProperty n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n.getValue(); i++) {
+            if (i == 1) {
+                sb.append(i);
+            } else {
+                sb.append("\n" + i);
+            }
+        }
+
+        return sb.toString();
     }
 }
