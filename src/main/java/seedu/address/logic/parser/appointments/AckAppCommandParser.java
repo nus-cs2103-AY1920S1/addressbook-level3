332b5c3b6e3c9b0db5b3afc9af4732e14af21f9a
WEN SHUFA <SHUFA17@GMAIL.COM>
2019-11-10 11:04:00 +0800

    edits appointment and shift's result message

--- src/main/java/seedu/address/logic/parser/appointments/AckAppCommandParser.java
+++ src/main/java/seedu/address/logic/parser/appointments/AckAppCommandParser.java
@@ -23,7 +23,7 @@ import seedu.address.model.events.predicates.EventMatchesRefIdPredicate;
  */
 public class AckAppCommandParser implements Parser<ReversibleActionPairCommand> {
     public static final String MESSAGE_NOTING_ACK = "there is no appointment under this patient.";
-    public static final String MESSAGE_NOT_SAME_DAY_ACK = "You can only acknowledge today's appointment!";
+    public static final String MESSAGE_NOT_SAME_DAY_ACK = "You can only acknowledge today's upcoming appointment!";
 
     public static final String MESSAGE_REFERENCEID_BELONGS_TO_STAFF =
             "Appointments should only be scheduled for patients.";
