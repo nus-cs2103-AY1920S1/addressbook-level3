42c8889313c800cef9301650c872f9afc548e8cd
alxkohh <alxkohh@gmail.com>
2019-11-10 20:04:25 +0800

    Add unit tests for deletebookmark command and its factory

---
+++ src/test/java/com/dukeacademy/logic/commands/bookmark/DeleteBookmarkCommandFactoryTest.java
@@ -0,0 +1,31 @@
+package com.dukeacademy.logic.commands.bookmark;
+
+import static com.dukeacademy.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.dukeacademy.logic.commands.exceptions.InvalidCommandArgumentsException;
+import com.dukeacademy.testutil.MockQuestionsLogic;
+
+class DeleteBookmarkCommandFactoryTest {
+
+    @Test
+    void testGetCommandWord() {
+        MockQuestionsLogic questionsLogic = MockQuestionsLogic.getMockQuestionsLogicWithTypicalQuestions();
+        DeleteBookmarkCommandFactory factory = new DeleteBookmarkCommandFactory(questionsLogic);
+        assertEquals("deletebookmark", factory.getCommandWord());
+    }
+
+    @Test
+    void testGetCommand() throws InvalidCommandArgumentsException {
+        MockQuestionsLogic questionsLogic = MockQuestionsLogic.getMockQuestionsLogicWithTypicalQuestions();
+        DeleteBookmarkCommandFactory factory = new DeleteBookmarkCommandFactory(questionsLogic);
+        assertTrue(factory.getCommand("1") instanceof DeleteBookmarkCommand);
+
+        assertThrows(InvalidCommandArgumentsException.class,
+                "Invalid index entered.",
+                () -> factory.getCommand("testerString"));
+    }
+}
---
+++ src/test/java/com/dukeacademy/logic/commands/bookmark/DeleteBookmarkCommandTest.java
@@ -0,0 +1,20 @@
+package com.dukeacademy.logic.commands.bookmark;
+
+import static com.dukeacademy.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.dukeacademy.logic.commands.exceptions.CommandException;
+import com.dukeacademy.testutil.MockQuestionsLogic;
+
+class DeleteBookmarkCommandTest {
+
+    @Test
+    void testExecute() throws CommandException {
+        MockQuestionsLogic questionsLogic = MockQuestionsLogic.getMockQuestionsLogicWithTypicalQuestions();
+        DeleteBookmarkCommand deleteBookmarkCommand = new DeleteBookmarkCommand(-1, questionsLogic);
+        assertThrows(CommandException.class,
+                "Index -1 entered out of range for current list of questions.",
+                () -> deleteBookmarkCommand.execute());
+    }
+}
