fea4743858e94262fc6f5dbf0ea9ebcd04ac5e1e
dorcastan <dorcas.tann@gmail.com>
2019-11-11 02:32:10 +0800

    Refactor: EditAutotagCommandParser to simplify #parse()
    Move validation into AutotagCommandParser#makePredicateFromMultimap()
    and call it in EditAutotagCommandParser#parse(args).

--- src/main/java/seedu/mark/logic/parser/EditAutotagCommandParser.java
+++ src/main/java/seedu/mark/logic/parser/EditAutotagCommandParser.java
@@ -18,7 +18,9 @@ import seedu.mark.model.autotag.SelectiveBookmarkTagger;
 import seedu.mark.model.predicates.BookmarkPredicate;
 
 /**
- * Parses input arguments and creates a new EditAutotagCommand object
+ * Parses input arguments and creates a new EditAutotagCommand object. An {@code EditAutotagCommandParser}
+ * makes use of the method {@link AutotagCommandParser#makePredicateFromMultimap(ArgumentMultimap)}
+ * when parsing the arguments.
  */
 public class EditAutotagCommandParser implements Parser<EditAutotagCommand> {
 
@@ -33,55 +35,18 @@ public class EditAutotagCommandParser implements Parser<EditAutotagCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_TAG, PREFIX_NAME, PREFIX_URL, PREFIX_NOT_NAME, PREFIX_NOT_URL,
                         PREFIX_FOLDER, PREFIX_NOT_FOLDER);
 
-        if (argMultimap.getPreamble().isEmpty() || argMultimap.getPreamble().split("\\s+").length > 1) {
+        String tagNameToEdit = argMultimap.getPreamble();
+        boolean hasInvalidTagName = tagNameToEdit.isEmpty() || tagNameToEdit.split("\\s+").length > 1;
+        if (hasInvalidTagName) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditAutotagCommand.MESSAGE_USAGE));
         }
 
-        String tagNameToEdit = argMultimap.getPreamble();
+        BookmarkPredicate predicate = AutotagCommandParser.makePredicateFromMultimap(argMultimap);
 
-        BookmarkPredicate predicate = new BookmarkPredicate();
+        String newTagName = getNewTagName(tagNameToEdit, argMultimap);
 
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            List<String> keywords = argMultimap.getAllValues(PREFIX_NAME);
-            checkValuesNonEmpty(keywords);
-            predicate = predicate.withNameKeywords(keywords);
-        }
-        if (argMultimap.getValue(PREFIX_URL).isPresent()) {
-            List<String> keywords = argMultimap.getAllValues(PREFIX_URL);
-            checkValuesNonEmpty(keywords);
-            predicate = predicate.withUrlKeywords(keywords);
-        }
-        if (argMultimap.getValue(PREFIX_FOLDER).isPresent()) {
-            List<String> folderNames = argMultimap.getAllValues(PREFIX_FOLDER);
-            checkValuesNonEmpty(folderNames);
-            predicate = predicate.withFolder(folderNames);
-        }
-        if (argMultimap.getValue(PREFIX_NOT_NAME).isPresent()) {
-            List<String> keywords = argMultimap.getAllValues(PREFIX_NOT_NAME);
-            checkValuesNonEmpty(keywords);
-            predicate = predicate.withoutNameKeywords(keywords);
-        }
-        if (argMultimap.getValue(PREFIX_NOT_URL).isPresent()) {
-            List<String> keywords = argMultimap.getAllValues(PREFIX_NOT_URL);
-            checkValuesNonEmpty(keywords);
-            predicate = predicate.withoutUrlKeywords(keywords);
-        }
-        if (argMultimap.getValue(PREFIX_NOT_FOLDER).isPresent()) {
-            List<String> folderNames = argMultimap.getAllValues(PREFIX_NOT_FOLDER);
-            checkValuesNonEmpty(folderNames);
-            predicate = predicate.withoutFolder(folderNames);
-        }
-
-        String newTagName = tagNameToEdit;
-        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
-            List<String> values = argMultimap.getAllValues(PREFIX_TAG);
-            if (values.size() != 1) {
-                throw new ParseException(EditAutotagCommand.MESSAGE_MULTIPLE_TAG_NAMES);
-            }
-            newTagName = values.get(0); // only one tag name
-        }
-
-        if (newTagName.equals(tagNameToEdit) && predicate.isEmpty()) {
+        boolean hasNotBeenEdited = newTagName.equals(tagNameToEdit) && predicate.isEmpty();
+        if (hasNotBeenEdited) {
             throw new ParseException(EditAutotagCommand.MESSAGE_NO_FIELD_EDITED);
         }
 
@@ -90,16 +55,15 @@ public class EditAutotagCommandParser implements Parser<EditAutotagCommand> {
         return new EditAutotagCommand(tagNameToEdit, newTagger);
     }
 
-    /**
-     * Checks that there is no empty value in the given list.
-     *
-     * @param values Strings to check.
-     * @throws ParseException if any String in {@code values} is blank.
-     */
-    private static void checkValuesNonEmpty(List<String> values) throws ParseException {
-        if (values.stream().anyMatch(String::isBlank)) {
-            throw new ParseException(EditAutotagCommand.MESSAGE_CONDITION_EMPTY);
+    private String getNewTagName(String originalTagName, ArgumentMultimap argMultimap) throws ParseException {
+        String newTagName = originalTagName;
+        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            List<String> values = argMultimap.getAllValues(PREFIX_TAG);
+            if (values.size() != 1) {
+                throw new ParseException(EditAutotagCommand.MESSAGE_MULTIPLE_TAG_NAMES);
+            }
+            newTagName = values.get(0); // only one tag name
         }
+        return newTagName;
     }
-
 }
--- src/test/java/seedu/mark/logic/parser/EditAutotagCommandParserTest.java
+++ src/test/java/seedu/mark/logic/parser/EditAutotagCommandParserTest.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.mark.logic.commands.AutotagCommand;
 import seedu.mark.logic.commands.EditAutotagCommand;
 import seedu.mark.model.autotag.SelectiveBookmarkTagger;
 import seedu.mark.model.predicates.BookmarkPredicate;
@@ -27,15 +28,15 @@ public class EditAutotagCommandParserTest {
     private static final String VALID_TAG_2 = "myTag2";
     private static final String VALID_NAME_1 = "website";
     private static final String VALID_NAME_2 = "question";
-    private static final String VALID_URL_1 = "facebook.com";
+    private static final String VALID_URL = "facebook.com";
     private static final String VALID_FOLDER = "Quiz";
 
     private static final String TAG_DESC = " " + PREFIX_TAG + VALID_TAG_2;
     private static final String NAME_DESC_1 = " " + PREFIX_NAME + VALID_NAME_1;
     private static final String NAME_DESC_2 = " " + PREFIX_NAME + VALID_NAME_2;
-    private static final String NOT_NAME_DESC_1 = " " + PREFIX_NOT_NAME + VALID_NAME_1;
-    private static final String URL_DESC_1 = " " + PREFIX_URL + VALID_URL_1;
-    private static final String NOT_URL_DESC_1 = " " + PREFIX_NOT_URL + VALID_URL_1;
+    private static final String NOT_NAME_DESC = " " + PREFIX_NOT_NAME + VALID_NAME_1;
+    private static final String URL_DESC = " " + PREFIX_URL + VALID_URL;
+    private static final String NOT_URL_DESC = " " + PREFIX_NOT_URL + VALID_URL;
     private static final String FOLDER_DESC = " " + PREFIX_FOLDER + VALID_FOLDER;
     private static final String NOT_FOLDER_DESC = " " + PREFIX_NOT_FOLDER + VALID_FOLDER;
 
@@ -53,7 +54,7 @@ public class EditAutotagCommandParserTest {
         assertParseFailure(parser, VALID_TAG, EditAutotagCommand.MESSAGE_NO_FIELD_EDITED);
 
         // empty condition
-        assertParseFailure(parser, VALID_TAG + " " + PREFIX_URL, EditAutotagCommand.MESSAGE_CONDITION_EMPTY);
+        assertParseFailure(parser, VALID_TAG + " " + PREFIX_URL, AutotagCommand.MESSAGE_CONDITION_EMPTY);
 
         // empty input
         assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
@@ -61,69 +62,41 @@ public class EditAutotagCommandParserTest {
 
     @Test
     public void parse_allFieldsSpecified_success() {
-        String userInput = VALID_TAG + TAG_DESC + NAME_DESC_1 + URL_DESC_1;
+        String userInput = VALID_TAG + TAG_DESC + NAME_DESC_1 + URL_DESC;
 
         EditAutotagCommand expectedCommand = new EditAutotagCommand(VALID_TAG,
                 new SelectiveBookmarkTagger(new Tag(VALID_TAG_2), new BookmarkPredicate()
                         .withNameKeywords(Collections.singletonList(VALID_NAME_1))
-                        .withUrlKeywords(Collections.singletonList(VALID_URL_1))));
+                        .withUrlKeywords(Collections.singletonList(VALID_URL))));
 
         assertParseSuccess(parser, userInput, expectedCommand);
     }
 
     @Test
-    public void parse_oneFieldSpecified_success() {
+    public void parse_someFieldsSpecified_success() {
         // tag name
         String userInput = VALID_TAG + TAG_DESC;
         EditAutotagCommand expectedCommand = new EditAutotagCommand(VALID_TAG,
                 new SelectiveBookmarkTagger(new Tag(VALID_TAG_2), new BookmarkPredicate()));
         assertParseSuccess(parser, userInput, expectedCommand);
 
-        // name
-        userInput = VALID_TAG + NAME_DESC_1;
+        // name + url + folder
+        userInput = VALID_TAG + NAME_DESC_1 + URL_DESC + FOLDER_DESC;
         expectedCommand = new EditAutotagCommand(VALID_TAG,
-                new SelectiveBookmarkTagger(new Tag(VALID_TAG),
-                        new BookmarkPredicate().withNameKeywords(Collections.singletonList(VALID_NAME_1))));
+                new SelectiveBookmarkTagger(new Tag(VALID_TAG), new BookmarkPredicate()
+                        .withNameKeywords(Collections.singletonList(VALID_NAME_1))
+                        .withUrlKeywords(Collections.singletonList(VALID_URL))
+                        .withFolder(Collections.singletonList(VALID_FOLDER))));
 
         assertParseSuccess(parser, userInput, expectedCommand);
 
-        // url
-        userInput = VALID_TAG + URL_DESC_1;
+        // not name + not url + not folder
+        userInput = VALID_TAG + NOT_NAME_DESC + NOT_URL_DESC + NOT_FOLDER_DESC;
         expectedCommand = new EditAutotagCommand(VALID_TAG,
-                new SelectiveBookmarkTagger(new Tag(VALID_TAG),
-                        new BookmarkPredicate().withUrlKeywords(Collections.singletonList(VALID_URL_1))));
-
-        assertParseSuccess(parser, userInput, expectedCommand);
-
-        // folder
-        userInput = VALID_TAG + FOLDER_DESC;
-        expectedCommand = new EditAutotagCommand(VALID_TAG,
-                new SelectiveBookmarkTagger(new Tag(VALID_TAG),
-                        new BookmarkPredicate().withFolder(Collections.singletonList(VALID_FOLDER))));
-
-        assertParseSuccess(parser, userInput, expectedCommand);
-
-        // not name
-        userInput = VALID_TAG + NOT_NAME_DESC_1;
-        expectedCommand = new EditAutotagCommand(VALID_TAG,
-                new SelectiveBookmarkTagger(new Tag(VALID_TAG),
-                        new BookmarkPredicate().withoutNameKeywords(Collections.singletonList(VALID_NAME_1))));
-
-        assertParseSuccess(parser, userInput, expectedCommand);
-
-        // not url
-        userInput = VALID_TAG + NOT_URL_DESC_1;
-        expectedCommand = new EditAutotagCommand(VALID_TAG,
-                new SelectiveBookmarkTagger(new Tag(VALID_TAG),
-                        new BookmarkPredicate().withoutUrlKeywords(Collections.singletonList(VALID_URL_1))));
-
-        assertParseSuccess(parser, userInput, expectedCommand);
-
-        // not folder
-        userInput = VALID_TAG + NOT_FOLDER_DESC;
-        expectedCommand = new EditAutotagCommand(VALID_TAG,
-                new SelectiveBookmarkTagger(new Tag(VALID_TAG),
-                        new BookmarkPredicate().withoutFolder(Collections.singletonList(VALID_FOLDER))));
+                new SelectiveBookmarkTagger(new Tag(VALID_TAG), new BookmarkPredicate()
+                        .withoutNameKeywords(Collections.singletonList(VALID_NAME_1))
+                        .withoutUrlKeywords(Collections.singletonList(VALID_URL))
+                        .withoutFolder(Collections.singletonList(VALID_FOLDER))));
 
         assertParseSuccess(parser, userInput, expectedCommand);
     }
