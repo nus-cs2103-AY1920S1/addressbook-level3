380fe8c848587dc59f35756f5fa7837228623afd
Chen Yi Jun <onearmy1997@gmail.com>
2019-11-11 07:48:10 +0800

    Fix undo for delete day

--- src/main/java/seedu/planner/logic/commands/deletecommand/DeleteDayCommand.java
+++ src/main/java/seedu/planner/logic/commands/deletecommand/DeleteDayCommand.java
@@ -82,6 +82,9 @@ public class DeleteDayCommand extends DeleteCommand {
             DeleteDayCommand newCommand = new DeleteDayCommand(targetIndex, dayToDelete);
             updateEventStack(newCommand, model);
         }
+        model.shiftDatesInItineraryByDayBetweenRange(-1, Index.fromOneBased(targetIndex.getOneBased() + 1),
+                Index.fromOneBased(model.getNumberOfDays() + 1));
+
         model.deleteDay(dayToDelete);
 
         return new CommandResult(
