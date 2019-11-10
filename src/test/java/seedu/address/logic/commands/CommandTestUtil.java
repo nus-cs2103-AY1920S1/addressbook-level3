08fef8288b193e110d27a70e844c53defb4011aa
SebastianLie <jackcass629@gmail.com>
2019-11-11 03:49:27 +0800

    Added full upload command tests

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -46,6 +46,8 @@ public class CommandTestUtil {
     public static final String VALID_MEDICALCONDITION_BOB = "Sinus";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    public static final String VALID_FILE_1 = "file:/C:/ping.png";
+    public static final String VALID_FILE_2 = "file:/C:/ping.jpg";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
