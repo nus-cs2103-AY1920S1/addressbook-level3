60afd8ac0480196d86532700b7331677e86a56b8
weikiat97 <42583086+weikiat97@users.noreply.github.com>
2019-11-10 21:38:34 +0800

    Update ResetPicCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/ResetPicCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ResetPicCommandParserTest.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_OBJECT;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +14,7 @@ public class ResetPicCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsResetPicCommand() {
-        assertParseSuccess(parser, "1", new ResetDisplayPictureCommand(INDEX_FIRST_STUDENT));
+        assertParseSuccess(parser, "1", new ResetDisplayPictureCommand(INDEX_FIRST_OBJECTT));
     }
 
     @Test
