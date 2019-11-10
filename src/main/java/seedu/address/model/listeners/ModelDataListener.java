49af9236d58d190ab2727995cf1478859c3b00f1
WeomuCat <weomu.cat@protonmail.com>
2019-11-10 18:45:44 +0800

    Update ModelManager DG

--- src/main/java/seedu/address/model/listeners/ModelDataListener.java
+++ src/main/java/seedu/address/model/listeners/ModelDataListener.java
@@ -2,8 +2,6 @@ package seedu.address.model.listeners;
 
 import seedu.address.model.ModelData;
 
-//@@author bruceskellator
-
 /**
  * Represents a listener that will be notified whenever the ModelData in ModelManager changes.
  */
