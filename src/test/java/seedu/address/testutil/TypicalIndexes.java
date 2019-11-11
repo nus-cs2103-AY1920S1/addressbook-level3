625a937e541036178871bbe6c62d002555f9874c
Jit Yong <jityong1997@hotmail.com>
2019-11-11 22:43:22 +0800

    resolved conflicts

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,12 +9,8 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PERSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PERSON = Index.fromOneBased(3);
-<<<<<<< HEAD
     public static final Index INDEX_FIRST = Index.fromOneBased(1);
     public static final Index INDEX_SECOND = Index.fromOneBased(2);
     public static final Index INDEX_THIRD = Index.fromOneBased(3);
-=======
-
     public static final Index INDEX_FIRST_PASSWORD = Index.fromOneBased(1);
->>>>>>> 767efeb975e26d8a3aada4cd25ea12039a6ade27
 }
