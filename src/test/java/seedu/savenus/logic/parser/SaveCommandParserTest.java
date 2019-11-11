c81e9ba8a5b62213e9aad921ab944a06ff04883f
Clarence Koh <ckoh.clarence.96@gmail.com>
2019-11-11 22:23:32 +0800

    fix checks

--- src/test/java/seedu/savenus/logic/parser/SaveCommandParserTest.java
+++ src/test/java/seedu/savenus/logic/parser/SaveCommandParserTest.java
@@ -2,22 +2,14 @@ package seedu.savenus.logic.parser;
 
 import static seedu.savenus.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.savenus.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.savenus.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.savenus.logic.commands.SaveCommand;
-import seedu.savenus.model.util.TimeStamp;
 
 //@@author fatclarence
 public class SaveCommandParserTest {
     private SaveCommandParser parser = new SaveCommandParser();
-
-    @Test
-    public void parse_validArgs_returnsTopUpCommand() {
-        assertParseSuccess(parser, "100",
-                 new SaveCommand("100", TimeStamp.generateCurrentTimeStamp()));
-    }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
