435a84cfec22e38ce171d9fdbf48ef7de7e868e1
hooncp <hooncheep@gmail.com>
2019-11-11 22:30:07 +0800

    Add new line to one command

--- src/main/java/seedu/address/logic/commands/CopyPasswordCommand.java
+++ src/main/java/seedu/address/logic/commands/CopyPasswordCommand.java
@@ -12,7 +12,7 @@ public abstract class CopyPasswordCommand extends Command {
             + "] / [" + CopyUsernameCommand.COMMAND_WORD + "/" + CopyUsernameCommand.COMMAND_WORD1
             + "]" + " : Opens and copies password/website/username identified by "
             + "the index number used in the display list. \n"
-            + "Parameters: INDEX (must be positive integer)"
+            + "Parameters: INDEX (must be positive integer)\n"
             + "Example: " + CopyPasswordCommand.COMMAND_WORD1 + " "
             + CopyPasswordValueCommand.COMMAND_WORD1 + " 1";
 }
