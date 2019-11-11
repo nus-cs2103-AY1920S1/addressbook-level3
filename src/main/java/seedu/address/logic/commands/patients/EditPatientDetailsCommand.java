2f0fff8f5614d930360f2b37f1b21a40665c28eb
Wong Shun Min <wongsm7@hotmail.com>
2019-11-11 13:31:24 +0800

    Edits message for edit commands

--- src/main/java/seedu/address/logic/commands/patients/EditPatientDetailsCommand.java
+++ src/main/java/seedu/address/logic/commands/patients/EditPatientDetailsCommand.java
@@ -42,8 +42,8 @@ public class EditPatientDetailsCommand extends ReversibleCommand {
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Patient: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This patient has already been registered.";
-    public static final String MESSAGE_PATIENT_IN_QUEUE = "You cannot edit while patient is in queue.";
-    public static final String MESSAGE_PATIENT_BEING_SERVED = "You cannot edit while patient is being served.";
+    public static final String MESSAGE_PATIENT_IN_QUEUE = "Cannot edit while patient is in queue.";
+    public static final String MESSAGE_PATIENT_BEING_SERVED = "Cannot edit while patient is being served.";
 
     private final Person personToEdit;
     private final Person editedPerson;
