621527d651d6d50a15b338929250ec2a78b1d296
oscarsu97 <zhenhuanghuang97@gmail.com>
2019-11-11 15:42:53 +0800

    Add limit to scheduling activity in same timeslot

--- src/main/java/seedu/planner/logic/commands/schedulecommand/ScheduleCommand.java
+++ src/main/java/seedu/planner/logic/commands/schedulecommand/ScheduleCommand.java
@@ -127,6 +127,15 @@ public class ScheduleCommand extends UndoableCommand {
         if (dayToEdit.getListOfActivityWithTime().contains(activityWithTimeToAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_ACTIVITY_SCHEDULED);
         }
+        int numOfOverlap = 0;
+        for (ActivityWithTime activity : dayToEdit.getListOfActivityWithTime()) {
+            if (activityWithTimeToAdd.isOverlapping(activity)) {
+                numOfOverlap++;
+            }
+        }
+        if (numOfOverlap >= MAX_LIMIT_OF_OVERLAP) {
+            throw new CommandException(MESSAGE_EXCEED_LIMIT_OF_OVERLAP);
+        }
 
         if (!isUndoRedo) {
             //Not due to undo method of UnscheduleEvent or redo method of ScheduleEvent
