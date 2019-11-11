362a102eb7b5c7e3afc10b3784f0af87990410e9
Sahilgat <nisgat@live.com>
2019-11-11 12:07:54 +0800

    Checkstyle fixes

--- src/main/java/seedu/address/model/tag/UniqueTagList.java
+++ src/main/java/seedu/address/model/tag/UniqueTagList.java
@@ -32,7 +32,7 @@ public class UniqueTagList implements Iterable<Tag> {
 
     /**
      * Adds a tag to the list.
-     * The person must not already exist in the list.
+     * The StudyBuddyProItem must not already exist in the list.
      */
     public void add(Tag toAdd) {
         requireNonNull(toAdd);
@@ -94,6 +94,10 @@ public class UniqueTagList implements Iterable<Tag> {
         internalList.setAll(tags);
     }
 
+    /**
+     * Adds all tags
+     * @param tags list of tags to be added
+     */
     public void addAllTags(List<Tag> tags) {
         for (Tag t : tags) {
             this.add(t);
