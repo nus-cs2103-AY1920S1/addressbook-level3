ad6f9b58b8d742de5e76f88f249a0e192b8afc52
dorcastan <dorcas.tann@gmail.com>
2019-11-11 15:55:07 +0800

    Fix: Failing tests
    The previous fix broke ImportCommandTest (needed to add IMPORT_FOLDER
    to expectedMark when importing).

--- src/test/java/seedu/mark/storage/StorageStub.java
+++ src/test/java/seedu/mark/storage/StorageStub.java
@@ -1,8 +1,10 @@
 package seedu.mark.storage;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 
+import seedu.mark.commons.exceptions.DataConversionException;
 import seedu.mark.model.ReadOnlyMark;
 import seedu.mark.model.ReadOnlyUserPrefs;
 import seedu.mark.model.UserPrefs;
@@ -10,7 +12,7 @@ import seedu.mark.model.UserPrefs;
 /**
  * A default Storage Stub that has all of its methods failing.
  */
-public final class StorageStub implements Storage {
+public class StorageStub implements Storage {
     @Override
     public Path getUserPrefsFilePath() {
         throw new AssertionError("This method should not be called.");
@@ -37,7 +39,7 @@ public final class StorageStub implements Storage {
     }
 
     @Override
-    public Optional<ReadOnlyMark> readMark(Path filePath) {
+    public Optional<ReadOnlyMark> readMark(Path filePath) throws IOException, DataConversionException {
         throw new AssertionError("This method should not be called.");
     }
 
