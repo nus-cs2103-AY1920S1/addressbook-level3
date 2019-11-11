956770a21e963218b1c5f53e1208dd7ad8f910a3
charliechoong <e0202817@u.nus.edu>
2019-11-11 20:36:05 +0800

    Remove redundant imports

--- src/test/java/seedu/deliverymans/model/DeliverymanTest.java
+++ src/test/java/seedu/deliverymans/model/DeliverymanTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.deliverymans.model.deliveryman.deliverymanstatus.UniqueStatusList.AVAILABLE_STATUS;
 import static seedu.deliverymans.model.deliveryman.deliverymanstatus.UniqueStatusList.UNAVAILABLE_STATUS;
-import static seedu.deliverymans.model.util.SampleDataUtil.getMenu;
 import static seedu.deliverymans.model.util.SampleDataUtil.getTagSet;
 
 import org.junit.jupiter.api.Test;
