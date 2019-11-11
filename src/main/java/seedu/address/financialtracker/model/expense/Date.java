1c524da5e4ee358a285af5f502e04e1a0217c9d4
honhaochen <haochenhon@hotmail.com>
2019-11-11 22:12:37 +0800

    final tweak

--- src/main/java/seedu/address/financialtracker/model/expense/Date.java
+++ src/main/java/seedu/address/financialtracker/model/expense/Date.java
@@ -12,7 +12,8 @@ public class Date {
     public static final String MESSAGE_CONSTRAINTS =
             "Date should only contain numerals and be written in the DDMMYYYY format.\n"
                     + "Valid formats: 25102019\n"
-                    + "Invalid formats: 25/10/2019 or 25-10-2019 or others..";
+                    + "Invalid formats: 25/10/2019 or 25-10-2019 or others..\n"
+                    + "Note: Year range is only valid from 1000 to 3999";
 
     /*
      * The first character of the description must not be a whitespace,
