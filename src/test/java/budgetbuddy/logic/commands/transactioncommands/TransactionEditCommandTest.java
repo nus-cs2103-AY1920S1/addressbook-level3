8c8dee6e03b1f395770fff951c8a502db1703d7e
cambrian-dk <daryl.kohyikai@gmail.com>
2019-11-11 23:22:40 +0800

    Add a couple of tests for transactions

---
+++ src/test/java/budgetbuddy/logic/commands/transactioncommands/TransactionEditCommandTest.java
@@ -0,0 +1,46 @@
+package budgetbuddy.logic.commands.transactioncommands;
+
+import static budgetbuddy.testutil.Assert.assertThrows;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import budgetbuddy.model.Model;
+import budgetbuddy.model.ModelManager;
+import budgetbuddy.model.attributes.Amount;
+import budgetbuddy.model.attributes.Category;
+import budgetbuddy.model.attributes.Description;
+import budgetbuddy.model.attributes.Direction;
+import budgetbuddy.model.attributes.Name;
+import budgetbuddy.model.transaction.Transaction;
+
+public class TransactionEditCommandTest {
+
+    private Model model;
+    private Transaction testTransaction;
+
+    @BeforeEach
+    public void initialize() {
+        model = new ModelManager();
+        Transaction testTransaction = new Transaction(
+                Direction.IN,
+                new Amount(5000),
+                new Description("Pocket Money"),
+                LocalDate.now(),
+                new Category("Income"));
+    }
+
+    @Test
+    public void constructor_nullTransaction_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TransactionAddCommand(null, new Name("Default")));
+    }
+
+    @Test
+    public void constructor_nullAccountName_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TransactionAddCommand(testTransaction, null));
+    }
+
+
+}
