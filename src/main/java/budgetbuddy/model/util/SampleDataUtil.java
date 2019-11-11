18f27cbdbd4785f887a6a31ea86f4f3847d2dc9c
kenneth-fung <fungchenyu@rocketmail.com>
2019-11-11 18:20:55 +0800

    Change sample data for loans

--- src/main/java/budgetbuddy/model/util/SampleDataUtil.java
+++ src/main/java/budgetbuddy/model/util/SampleDataUtil.java
@@ -20,27 +20,12 @@ public class SampleDataUtil {
 
     public static List<Loan> getSampleLoans() {
         return Arrays.asList(
-                new Loan(new Person(new Name("John")),
+                new Loan(new Person(new Name("Example Person 1")),
                         Direction.OUT, new Amount(420), LocalDate.now(),
-                        new Description("Paid for his lunch."), Status.UNPAID),
-                new Loan(new Person(new Name("Mary")),
-                        Direction.IN, new Amount(10000), LocalDate.of(2019, 9, 23),
-                        new Description("Owe them for that party."), Status.UNPAID),
-                new Loan(new Person(new Name("Bill")),
-                        Direction.OUT, new Amount(65674), LocalDate.of(2018, 12, 3),
-                        new Description("Bought his laptop for him..."), Status.UNPAID),
-                new Loan(new Person(new Name("Adam")),
-                        Direction.OUT, new Amount(8920), LocalDate.of(2019, 6, 9),
-                        new Description("Lent him for a game."), Status.PAID),
-                new Loan(new Person(new Name("Kris")),
-                        Direction.IN, new Amount(12134), LocalDate.of(2017, 7, 29),
-                        new Description("She's a kind soul."), Status.PAID),
-                new Loan(new Person(new Name("Vladimir")),
-                        Direction.IN, new Amount(345), LocalDate.of(2019, 8, 14),
-                        new Description("Hopefully he's forgotten by now."), Status.UNPAID),
-                new Loan(new Person(new Name("Charles")),
-                        Direction.OUT, new Amount(56), LocalDate.of(2011, 1, 31),
-                        new Description("Don't know if I should ask for this back."), Status.UNPAID));
+                        new Description("An example of a loan where you lent money to someone."), Status.UNPAID),
+                new Loan(new Person(new Name("Example Person 2")),
+                        Direction.IN, new Amount(1000), LocalDate.now(),
+                        new Description("An example of a loan where you borrowed money from someone."), Status.PAID));
     }
 
     public static LoansManager getSampleLoansManager() {
