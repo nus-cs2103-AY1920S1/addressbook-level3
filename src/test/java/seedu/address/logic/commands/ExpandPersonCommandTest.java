4e60f79b762bd2438fb1a54d48a7b94f4688b938
chowyiyin <yiyin.chow@gmail.com>
2019-11-11 19:54:32 +0800

    Add index out of bounds test cases to expandpersoncommandtest and expandpolicycommandtest

--- src/test/java/seedu/address/logic/commands/ExpandPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExpandPersonCommandTest.java
@@ -1,13 +1,19 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertExpandPersonCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -43,5 +49,48 @@ public class ExpandPersonCommandTest {
         assertExpandPersonCommandSuccess(new ExpandPersonCommand(INDEX_FIRST_PERSON),
             model,
             String.format(ExpandPersonCommand.MESSAGE_SUCCESS, person), expectedModel, person);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        ExpandPersonCommand expandPersonCommand = new ExpandPersonCommand(outOfBoundIndex);
+
+        assertCommandFailure(expandPersonCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        ExpandPersonCommand expandPersonCommand = new ExpandPersonCommand(outOfBoundIndex);
+
+        assertCommandFailure(expandPersonCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        ExpandPersonCommand expandFirstCommand = new ExpandPersonCommand(INDEX_FIRST_PERSON);
+        ExpandPersonCommand expandSecondCommand = new ExpandPersonCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(expandFirstCommand.equals(expandFirstCommand));
+
+        // same values -> returns true
+        ExpandPersonCommand expandPersonCommandCopy = new ExpandPersonCommand(INDEX_FIRST_PERSON);
+        assertTrue(expandFirstCommand.equals(expandPersonCommandCopy));
+
+        // different types -> returns false
+        assertFalse(expandFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(expandFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(expandFirstCommand.equals(expandSecondCommand));
     }
 }
--- src/test/java/seedu/address/logic/commands/ExpandPolicyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExpandPolicyCommandTest.java
@@ -1,14 +1,19 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertExpandPolicySuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPolicyAtIndex;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_POLICY;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_POLICY;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -41,8 +46,51 @@ public class ExpandPolicyCommandTest {
         showPolicyAtIndex(model, INDEX_FIRST_POLICY);
         showPolicyAtIndex(expectedModel, INDEX_FIRST_POLICY);
         Policy policy = model.getFilteredPolicyList().get(INDEX_FIRST_POLICY.getZeroBased());
-        assertExpandPolicySuccess(new ExpandPolicyCommand(INDEX_FIRST_PERSON),
+        assertExpandPolicySuccess(new ExpandPolicyCommand(INDEX_FIRST_POLICY),
             model,
             String.format(ExpandPolicyCommand.MESSAGE_SUCCESS, policy), expectedModel, policy);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPolicyList().size() + 1);
+        ExpandPolicyCommand expandPolicyCommand = new ExpandPolicyCommand(outOfBoundIndex);
+
+        assertCommandFailure(expandPolicyCommand, model, Messages.MESSAGE_INVALID_POLICY_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showPolicyAtIndex(model, INDEX_FIRST_POLICY);
+
+        Index outOfBoundIndex = INDEX_SECOND_POLICY;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPolicyList().size());
+
+        ExpandPolicyCommand expandPolicyCommand = new ExpandPolicyCommand(outOfBoundIndex);
+
+        assertCommandFailure(expandPolicyCommand, model, Messages.MESSAGE_INVALID_POLICY_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        ExpandPolicyCommand expandFirstCommand = new ExpandPolicyCommand(INDEX_FIRST_POLICY);
+        ExpandPolicyCommand expandSecondCommand = new ExpandPolicyCommand(INDEX_SECOND_POLICY);
+
+        // same object -> returns true
+        assertTrue(expandFirstCommand.equals(expandFirstCommand));
+
+        // same values -> returns true
+        ExpandPolicyCommand expandPersonCommandCopy = new ExpandPolicyCommand(INDEX_FIRST_POLICY);
+        assertTrue(expandFirstCommand.equals(expandPersonCommandCopy));
+
+        // different types -> returns false
+        assertFalse(expandFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(expandFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(expandFirstCommand.equals(expandSecondCommand));
     }
 }
