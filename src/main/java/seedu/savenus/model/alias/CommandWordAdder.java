7b0619a0dca6709b024741d0192bd01b85f9958b
Roby Tanama <tanamaroby@gmail.com>
2019-11-11 14:51:09 +0800

    Accommodate theme in alias

--- src/main/java/seedu/savenus/model/alias/CommandWordAdder.java
+++ src/main/java/seedu/savenus/model/alias/CommandWordAdder.java
@@ -29,6 +29,7 @@ import seedu.savenus.logic.commands.RemoveLikeCommand;
 import seedu.savenus.logic.commands.SaveCommand;
 import seedu.savenus.logic.commands.ShowCommand;
 import seedu.savenus.logic.commands.SortCommand;
+import seedu.savenus.logic.commands.ThemeCommand;
 import seedu.savenus.logic.commands.TopUpCommand;
 import seedu.savenus.logic.commands.ViewSortCommand;
 import seedu.savenus.logic.commands.WithdrawCommand;
@@ -71,6 +72,7 @@ public class CommandWordAdder {
         aliasPairList.add(new AliasPair(RemoveLikeCommand.COMMAND_WORD, emptyAliasWord));
         aliasPairList.add(new AliasPair(SortCommand.COMMAND_WORD, emptyAliasWord));
         aliasPairList.add(new AliasPair(SaveCommand.COMMAND_WORD, emptyAliasWord));
+        aliasPairList.add(new AliasPair(ThemeCommand.COMMAND_WORD, emptyAliasWord));
         aliasPairList.add(new AliasPair(TopUpCommand.COMMAND_WORD, emptyAliasWord));
         aliasPairList.add(new AliasPair(ViewSortCommand.COMMAND_WORD, emptyAliasWord));
         aliasPairList.add(new AliasPair(WithdrawCommand.COMMAND_WORD, emptyAliasWord));
